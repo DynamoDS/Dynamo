97d5589463330498d61d6a985572436d247d99a2
unknown <boyerp@WAL2UA304095H.ads.autodesk.com>
2013-02-28 10:12:28 -0500

    Updated build process to copy binaries to solution dir, all external refs should be copied local

--- DynamoRevit/Properties/AssemblyInfo.cs
+++ DynamoRevit/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DynamoRevit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("goBuild Inc.")]
+[assembly: AssemblyCompany("Autodesk, Inc.")]
 [assembly: AssemblyProduct("DynamoRevit")]
-[assembly: AssemblyCopyright("Copyright © goBuild Inc. 2012")]
+[assembly: AssemblyCopyright("Copyright © Autodesk, Inc 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyVersion("0.3.0.1")]
+[assembly: AssemblyFileVersion("0.3.0.1")]
