a432cc189f42cefe38e4a53598ce895a7ba7bda4
Peter Boyer <peter.b.boyer@gmail.com>
2013-03-01 15:13:35 -0500

    Installer robocopy

--- DynamoWIPInstall/CreateInstallers-DEBUG.bat
+++ DynamoWIPInstall/CreateInstallers-DEBUG.bat
@@ -1,5 +1,5 @@
-xcopy "..\bin\Debug\*.dll" "..\DynamoWIPInstall\bin" /Y
-xcopy "..\definitions\*.*" "..\DynamoWIPInstall\definitions" /S /I /Y /R
-xcopy "..\Samples\*.*" "..\DynamoWIPInstall\Samples" /S /I /Y /R
+robocopy ..\bin\Debug bin *.dll
+robocopy ..\definitions definitions
+robocopy ..\Samples Samples
 "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "..\DynamoWIPInstall\DynamoForVasari_WIP.iss"
 "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "..\DynamoWIPInstall\DynamoForRevit_2013_WIP.iss"
