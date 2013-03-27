dd63a7020e3315aba50ffe951577d8c0574e13af
Stephen Elliott <steell1890@gmail.com>
2013-03-27 16:32:11 -0400

    lots of user-node fixes

--- src/Dynamo/dynBench.xaml.cs
+++ src/Dynamo/dynBench.xaml.cs
@@ -699,7 +699,7 @@ namespace Dynamo.Controls
             }
             while (!error.Equals(""));
 
-            Controller.NewFunction(name, category, true);
+            Controller.NewFunction(Guid.NewGuid(), name, category, true);
         }
 
         //internal void ChangeView_Click(object sender, RoutedEventArgs e)
