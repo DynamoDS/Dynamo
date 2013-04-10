8b00304f9601f9678a1a1133891195058b883d59
Ian Keough <ian@iankeough.com>
2013-04-09 17:21:06 -0700

    Revert "Drawable interface."
    This reverts commit b99eaa792a584161e609601a0f87c392d5278dcb.

--- src/Dynamo/dynNode.cs
+++ src/Dynamo/dynNode.cs
@@ -667,18 +667,6 @@ namespace Dynamo.Nodes
         public virtual void Cleanup()
         {
         }
-
-        public IEnumerable<dynNode> DescendantsAndSelf()
-        {
-            yield return this;
-            foreach (dynNode item in Inputs.Values.
-                Where(x=>x!=null).
-                Select(x => x.Item2).
-                SelectMany(x => (x as dynNode).DescendantsAndSelf()))
-            {
-                yield return item;
-            }
-        }
     }
 
     public abstract class dynNodeWithOneOutput : dynNode
