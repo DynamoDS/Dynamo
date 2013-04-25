2eaf80aa39904aa5a4f939c373ee24528324f1df
Peter Boyer <peter.b.boyer@gmail.com>
2013-04-25 08:35:08 -0400

    More DynamoRevitTests, fixed nunit crash

--- src/DynamoRevitTests/DynamoRevitTests.cs
+++ src/DynamoRevitTests/DynamoRevitTests.cs
@@ -13,118 +13,45 @@
 //limitations under the License.
 
 using System;
-using System.Diagnostics;
 using System.Reflection;
-using System.Windows;
-using System.Windows.Forms;
-using System.Windows.Interop;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Dynamo.Applications;
-using Dynamo.Controls;
 using Dynamo.Utilities;
 using NUnit.Core;
-using IWin32Window = System.Windows.Interop.IWin32Window;
 using MessageBox = System.Windows.MessageBox;
-using Rectangle = System.Drawing.Rectangle;
 
-namespace Dynamo.Applications.Tests
+namespace Dynamo.Tests
 {
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
     internal class DynamoRevitTestsLoader : IExternalCommand
     {
-        private static dynBench _dynamoBench;
-        private UIDocument _mDoc;
-        private UIApplication _mRevit;
+        public static ExternalCommandData RevitCommandData;
 
         public Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
         {
-            if (_dynamoBench != null)
-            {
-                _dynamoBench.Focus();
-                return Result.Succeeded;
-            }
+            RevitCommandData = revit;
 
-            dynSettings.StartLogging();
-
+            // Run tests
             try
             {
-                _mRevit = revit.Application;
-                _mDoc = _mRevit.ActiveUIDocument;
+                CoreExtensions.Host.InitializeService();
+                var runner = new SimpleTestRunner();
+                var package = new TestPackage("Test");
+                string loc = Assembly.GetExecutingAssembly().Location;
+                package.Assemblies.Add(loc);
 
-                #region default level
+                TestResult result;
+                if (runner.Load(package))
+                {
+                    result = runner.Run(new NullListener(), TestFilter.Empty, true, LoggingThreshold.All);
 
-                Level defaultLevel = null;
-                var fecLevel = new FilteredElementCollector(_mDoc.Document);
-                fecLevel.OfClass(typeof (Level));
-                defaultLevel = fecLevel.ToElements()[0] as Level;
-
-                #endregion
-
-                dynRevitSettings.Revit = _mRevit;
-                dynRevitSettings.Doc = _mDoc;
-                dynRevitSettings.DefaultLevel = defaultLevel;
-
-                IdlePromise.ExecuteOnIdle(delegate
-                    {
-                        //get window handle
-                        IntPtr mwHandle = Process.GetCurrentProcess().MainWindowHandle;
-
-                        //show the window
-                        var dynamoController = new DynamoController_Revit(DynamoRevitApp.env, DynamoRevitApp.updater);
-                        _dynamoBench = dynamoController.Bench;
-
-                        //set window handle and show dynamo
-                        new WindowInteropHelper(_dynamoBench).Owner = mwHandle;
-
-                        _dynamoBench.WindowStartupLocation = WindowStartupLocation.Manual;
-
-                        Rectangle bounds = Screen.PrimaryScreen.Bounds;
-                        _dynamoBench.Left = bounds.X;
-                        _dynamoBench.Top = bounds.Y;
-                        _dynamoBench.Loaded += dynamoForm_Loaded;
-
-                        _dynamoBench.Show();
-
-                        _dynamoBench.Closed += dynamoForm_Closed;
-
-                        // Run tests
-
-                        try
-                        {
-                            CoreExtensions.Host.InitializeService();
-                            var runner = new SimpleTestRunner();
-                            var package = new TestPackage("Test");
-                            string loc = Assembly.GetExecutingAssembly().Location;
-                            package.Assemblies.Add(loc);
-
-                            TestResult result;
-                            if (runner.Load(package))
-                            {
-                                result = runner.Run(new NullListener(), TestFilter.Empty, true, LoggingThreshold.All);
-
-                                MessageBox.Show("Started dynamo");
-
-                                MessageBox.Show(result.FullName);
-                                MessageBox.Show(result.IsSuccess.ToString());
-                                MessageBox.Show(result.Message);
-                                MessageBox.Show(result.Results.Count.ToString());
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show(ex.ToString());
-                            if (dynSettings.Writer != null)
-                            {
-                                dynSettings.Writer.WriteLine(ex.Message);
-                                dynSettings.Writer.WriteLine(ex.StackTrace);
-                                dynSettings.Writer.WriteLine("Dynamo log ended " + DateTime.Now.ToString());
-                            }
-                        }
-                    });
-
+                    MessageBox.Show(result.FullName);
+                    MessageBox.Show(result.IsSuccess.ToString());
+                    MessageBox.Show(result.Message);
+                    MessageBox.Show(result.Results.Count.ToString());
+                }
             }
             catch (Exception ex)
             {
@@ -135,38 +62,10 @@ namespace Dynamo.Applications.Tests
                     dynSettings.Writer.WriteLine(ex.StackTrace);
                     dynSettings.Writer.WriteLine("Dynamo log ended " + DateTime.Now.ToString());
                 }
-                return Result.Failed;
             }
-
+        
             return Result.Succeeded;
-        }
 
-        private void dynamoForm_Closed(object sender, EventArgs e)
-        {
-            _dynamoBench = null;
-        }
-
-        private void dynamoForm_Loaded(object sender, RoutedEventArgs e)
-        {
-            ((dynBench) sender).WindowState = WindowState.Maximized;
-        }
-    }
-
-    internal class WindowHandle : IWin32Window
-    {
-        private readonly IntPtr _hwnd;
-
-        public WindowHandle(IntPtr h)
-        {
-            Debug.Assert(IntPtr.Zero != h,
-                         "expected non-null window handle");
-
-            _hwnd = h;
-        }
-
-        public IntPtr Handle
-        {
-            get { return _hwnd; }
         }
     }
 }
--- src/DynamoRevitTests/DynamoRevitTests1.cs
+++ src/DynamoRevitTests/DynamoRevitTests1.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Dynamo.Applications;
 using Dynamo.Commands;
+using Dynamo.Controls;
 using Dynamo.Nodes;
 using Dynamo.Utilities;
+using NUnit.Core;
 using NUnit.Framework;
+using MessageBox = System.Windows.MessageBox;
+using Rectangle = System.Drawing.Rectangle;
 
 namespace Dynamo.Tests
 {
@@ -17,7 +28,24 @@ namespace Dynamo.Tests
         [SetUp]
         public void Init()
         {
-            StartDynamo();
+            string tempPath = Path.GetTempPath();
+            var random = new Random();
+            string logPath = Path.Combine(tempPath, "dynamoLog" + random.Next() + ".txt");
+
+            TempFolder = Path.Combine(tempPath, "dynamoTmp");
+
+            if (!Directory.Exists(TempFolder))
+            {
+                Directory.CreateDirectory(TempFolder);
+            }
+            else
+            {
+                EmptyTempFolder();
+            }
+
+            TextWriter tw = new StreamWriter(logPath);
+            tw.WriteLine("Dynamo log started " + DateTime.Now.ToString());
+            dynSettings.Writer = tw;
         }
 
         [TearDown]
@@ -34,40 +62,72 @@ namespace Dynamo.Tests
             }
         }
 
-        private static string TempFolder;
-
-        private static void StartDynamo()
+        public void StartDynamo()
         {
+            
+            dynSettings.StartLogging();
+
             try
             {
+                _mRevit = DynamoRevitTestsLoader.RevitCommandData.Application;
+                _mDoc = _mRevit.ActiveUIDocument;
 
-                string tempPath = Path.GetTempPath();
-                var random = new Random();
-                string logPath = Path.Combine(tempPath, "dynamoLog" + random.Next() + ".txt");
+                #region default level
 
-                TempFolder = Path.Combine(tempPath, "dynamoTmp");
+                Level defaultLevel = null;
+                var fecLevel = new FilteredElementCollector(_mDoc.Document);
+                fecLevel.OfClass(typeof (Level));
+                defaultLevel = fecLevel.ToElements()[0] as Level;
 
-                if (!Directory.Exists(TempFolder))
-                {
-                    Directory.CreateDirectory(TempFolder);
-                }
-                else
-                {
-                    EmptyTempFolder();
-                }
+                #endregion
 
-                TextWriter tw = new StreamWriter(logPath);
-                tw.WriteLine("Dynamo log started " + DateTime.Now.ToString());
-                dynSettings.Writer = tw;
+                dynRevitSettings.Revit = _mRevit;
+                dynRevitSettings.Doc = _mDoc;
+                dynRevitSettings.DefaultLevel = defaultLevel;
 
-                //create a new instance of the ViewModel
-                var controller = new DynamoController(new FSchemeInterop.ExecutionEnvironment());
+                //get window handle
+                IntPtr mwHandle = Process.GetCurrentProcess().MainWindowHandle;
+
+                //show the window
+                var dynamoController = new DynamoController_Revit(DynamoRevitApp.env, DynamoRevitApp.updater);
+                _dynamoBench = dynamoController.Bench;
+
+                //set window handle and show dynamo
+                new WindowInteropHelper(_dynamoBench).Owner = mwHandle;
+
+                _dynamoBench.WindowStartupLocation = WindowStartupLocation.Manual;
+
+                Rectangle bounds = Screen.PrimaryScreen.Bounds;
+                _dynamoBench.Left = bounds.X;
+                _dynamoBench.Top = bounds.Y;
+
+                _dynamoBench.Show();
+
+                _dynamoBench.Closed += dynamoForm_Closed;
+
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.StackTrace);
+                MessageBox.Show(ex.ToString());
+                if (dynSettings.Writer != null)
+                {
+                    dynSettings.Writer.WriteLine(ex.Message);
+                    dynSettings.Writer.WriteLine(ex.StackTrace);
+                    dynSettings.Writer.WriteLine("Dynamo log ended " + DateTime.Now.ToString());
+                }
             }
+
         }
+
+        private void dynamoForm_Closed(object sender, EventArgs e)
+        {
+            // _dynamoBench = null;
+        }
+
+        private static string TempFolder;
+        private static dynBench _dynamoBench;
+        private UIDocument _mDoc;
+        private UIApplication _mRevit;
 
         public static void EmptyTempFolder()
         {
@@ -86,17 +146,21 @@ namespace Dynamo.Tests
         // OpenCommand
 
         [Test]
-        public void CanOpenGoodFile()
+        public void CanCreateNote()
         {
-            Assert.AreEqual(5, 5);
+            //create some test note data
+            var inputs = new Dictionary<string, object>();
+            inputs.Add("x", 200.0);
+            inputs.Add("y", 200.0);
+            inputs.Add("text", "This is a test note.");
+            inputs.Add("workspace", dynSettings.Controller.CurrentSpace);
+
+            dynSettings.Controller.CommandQueue.Enqueue(Tuple.Create<object, object>(DynamoCommands.AddNoteCmd, inputs));
+            dynSettings.Controller.ProcessCommandQueue();
+
+            Assert.AreEqual(dynSettings.Controller.CurrentSpace.Notes.Count, 1);
         }
 
-        [Test]
-        public void CanOpenGoodFile2()
-        {
-            Assert.AreEqual(5, 5);
-            Assert.AreEqual(5, 1);
-        }
 
     }
 }
