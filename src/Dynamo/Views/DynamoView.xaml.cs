d923b818a9a9b246f433cf27d2d3842b29af984f
Peter Boyer <peter.b.boyer@gmail.com>
2013-05-03 12:13:55 -0400

    No more home space indicator at bottom, got rid of a bunch of dead code-behind code

--- src/Dynamo/Views/DynamoView.xaml.cs
+++ src/Dynamo/Views/DynamoView.xaml.cs
@@ -138,83 +138,6 @@ namespace Dynamo.Controls
         /// <param name="e"></param>
         public void OnMouseMove(object sender, MouseEventArgs e)
         {
-            ////Canvas.SetLeft(debugPt, e.GetPosition(dynSettings.Workbench).X - debugPt.Width/2);
-            ////Canvas.SetTop(debugPt, e.GetPosition(dynSettings.Workbench).Y - debugPt.Height / 2);
-
-            ////If we are currently connecting and there is an active connector,
-            ////redraw it to match the new mouse coordinates.
-            //if (vm.IsConnecting && vm.ActiveConnector != null)
-            //{
-            //    vm.ActiveConnector.Redraw(e.GetPosition(WorkBench));
-            //}
-
-            ////If we are currently dragging elements, redraw the element to
-            ////match the new mouse coordinates.
-            //if (WorkBench.isDragInProgress)
-            //{
-            //    //IEnumerable<dynConnector> allConnectors = DynamoSelection.Instance.Selection
-            //    //                                                   .Where(x => x is dynNode)
-            //    //                                                   .Select(x => x as dynNode)
-            //    //                                                   .SelectMany(
-            //    //                                                       el => el.OutPorts
-            //    //                                                               .SelectMany(x => x.Connectors)
-            //    //                                                               .Concat(
-            //    //                                                                   el.InPorts.SelectMany(
-            //    //                                                                       x => x.Connectors))).Distinct();
-
-            //    //foreach (dynConnector connector in allConnectors)
-            //    //{
-            //    //    connector.Redraw();
-            //    //}
-            //    vm.UpdateSelectedConnectorsCommand.Execute();
-            //}
-
-            //if (isWindowSelecting)
-            //{
-            //    // When the mouse is held down, reposition the drag selection box.
-
-            //    Point mousePos = e.GetPosition(WorkBench);
-
-            //    if (mouseDownPos.X < mousePos.X)
-            //    {
-            //        Canvas.SetLeft(selectionBox, mouseDownPos.X);
-            //        selectionBox.Width = mousePos.X - mouseDownPos.X;
-            //    }
-            //    else
-            //    {
-            //        Canvas.SetLeft(selectionBox, mousePos.X);
-            //        selectionBox.Width = mouseDownPos.X - mousePos.X;
-            //    }
-
-            //    if (mouseDownPos.Y < mousePos.Y)
-            //    {
-            //        Canvas.SetTop(selectionBox, mouseDownPos.Y);
-            //        selectionBox.Height = mousePos.Y - mouseDownPos.Y;
-            //    }
-            //    else
-            //    {
-            //        Canvas.SetTop(selectionBox, mousePos.Y);
-
-            //        selectionBox.Height = mouseDownPos.Y - mousePos.Y;
-            //    }
-
-            //    if (mousePos.X > mouseDownPos.X)
-            //    {
-            //        #region contain select
-
-            //        selectionBox.StrokeDashArray = null;
-
-            //        #endregion
-            //    }
-            //    else if (mousePos.X < mouseDownPos.X)
-            //    {
-            //        #region crossing select
-
-            //        selectionBox.StrokeDashArray = new DoubleCollection {4};
-
-            //        #endregion
-            //    }
-            //}
         }
 
         /// <summary>
@@ -228,10 +151,6 @@ namespace Dynamo.Controls
 
         private void OnMouseLeftButtonDown(object sender, MouseEventArgs e)
         {
-            if (editingName && !hoveringEditBox)
-            {
-                DisableEditNameBox();
-            }
         }
 
         /// <summary>
@@ -250,86 +169,8 @@ namespace Dynamo.Controls
 
         private void OnPreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (editingName)
-            {
-                if (Keyboard.IsKeyDown(Key.Enter))
-                {
-                    //Controller.RefactorCustomNode();
-                    vm.RefactorCustomNodeCommand.Execute();
-
-                    DisableEditNameBox();
-                    e.Handled = true;
-                }
-                else if (Keyboard.IsKeyDown(Key.Escape))
-                {
-                    DisableEditNameBox();
-                    e.Handled = true;
-                }
-            }
         }
 
-        private void image1_MouseEnter(object sender, MouseEventArgs e)
-        {
-            //highlight
-            this.WorkspaceTitleContainer.Background = new SolidColorBrush(Colors.LightBlue);
-            if (beginNameEditClick && e.LeftButton == MouseButtonState.Released)
-            {
-                beginNameEditClick = false;
-            }
-        }
-
-        private void image1_MouseLeave(object sender, MouseEventArgs e)
-        {
-            this.WorkspaceTitleContainer.Background = new SolidColorBrush(Colors.Transparent);
-        }
-
-        private void image1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            vm.RefactorCustomNodeCommand.Execute();
-        }
-
-        private void image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            beginNameEditClick = true;
-        }
-
-        public void EnableEditNameBox()
-        {
-            workspaceLabel.Visibility = Visibility.Collapsed;
-
-            editNameBox.Visibility = Visibility.Visible;
-            editNameBox.IsEnabled = true;
-            editNameBox.IsHitTestVisible = true;
-            editNameBox.Focusable = true;
-            editNameBox.Focus();
-            //MVVM: editNameBox text now bound to EditName property on viewModel
-            //editNameBox.Text = Controller.CurrentSpace.Name;
-            editNameBox.SelectAll();
-
-            editingName = true;
-        }
-
-        public void DisableEditNameBox()
-        {
-            editNameBox.Visibility = Visibility.Collapsed;
-            editNameBox.IsEnabled = false;
-            editNameBox.IsHitTestVisible = false;
-            editNameBox.Focusable = false;
-
-            workspaceLabel.Visibility = Visibility.Visible;
-
-            editingName = false;
-        }
-
-        private void editNameBox_MouseEnter(object sender, MouseEventArgs e)
-        {
-            hoveringEditBox = true;
-        }
-
-        private void editNameBox_MouseLeave(object sender, MouseEventArgs e)
-        {
-            hoveringEditBox = false;
-        }
 
         private void OverlayCanvas_OnMouseMove(object sender, MouseEventArgs e)
         {
@@ -347,54 +188,6 @@ namespace Dynamo.Controls
         private void OverlayCanvas_OnMouseUp(object sender, MouseButtonEventArgs e)
         {
             
-            throw new NotImplementedException("Are we using the drag canvas any more?");
-            /*if (UILocked)
-                return;
-
-            dynNodeView el = draggedNode;
-
-            Point pos = e.GetPosition(WorkBench);
-
-            overlayCanvas.Children.Clear();
-            overlayCanvas.IsHitTestVisible = false;
-
-            draggedElementMenuItem.Visibility = Visibility.Visible;
-            draggedElementMenuItem = null;
-
-            Point outerPos = e.GetPosition(outerCanvas);
-
-            if (outerPos.X >= 0 && outerPos.X <= overlayCanvas.ActualWidth
-                && outerPos.Y >= 0 && outerPos.Y <= overlayCanvas.ActualHeight)
-            {
-                WorkBench.Children.Add(el);
-
-                Controller.Nodes.Add(el.NodeLogic);
-
-                el.NodeLogic.WorkSpace = Controller.CurrentSpace;
-
-                el.Opacity = 1;
-
-                Canvas.SetLeft(el, Math.Max(pos.X - dragOffset.X, 0));
-                Canvas.SetTop(el, Math.Max(pos.Y - dragOffset.Y, 0));
-
-                el.EnableInteraction();
-
-                if (Controller.ViewingHomespace)
-                    el.NodeLogic.SaveResult = true;
-            }
-
-            dragOffset = new Point();*/
-        }
-
-        private static void SaveRTBAsPNG(RenderTargetBitmap bmp, string filename)
-        {
-            var enc = new PngBitmapEncoder();
-            enc.Frames.Add(BitmapFrame.Create(bmp));
-
-            using (FileStream stm = File.Create(filename))
-            {
-                enc.Save(stm);
-            }
         }
 
         private void LogScroller_OnSourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
