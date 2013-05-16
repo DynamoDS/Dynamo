1194df381c65df506fd95c8a28fe9f67c2042fae
Peter Boyer <peter.b.boyer@gmail.com>
2013-05-16 16:06:48 -0400

    Fixed solar radiation optimizer (one of the dyfs was commented out :(, added a more expressive error

--- src/Dynamo/Nodes/dynFunction.cs
+++ src/Dynamo/Nodes/dynFunction.cs
@@ -66,7 +66,7 @@ namespace Dynamo
 
             void ui_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
             {
-                Controller.DynamoViewModel.ViewCustomNodeWorkspace(_def);
+                Controller.DynamoViewModel.GoToWorkspaceCommand.Execute(_def.FunctionId);
                 e.Handled = true;
             }
 
