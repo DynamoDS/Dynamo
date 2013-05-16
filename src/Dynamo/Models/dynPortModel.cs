166eb1f1bd28dca462f839c9f6d6e8054c6d9ff4
Peter Boyer <peter.b.boyer@gmail.com>
2013-05-16 12:21:40 -0400

    Updated dynPortModel

--- src/Dynamo/Models/dynPortModel.cs
+++ src/Dynamo/Models/dynPortModel.cs
@@ -2,10 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
-using Dynamo.Controls;
 using Dynamo.Nodes;
-using Dynamo.Utilities;
-using Microsoft.Practices.Prism.ViewModel;
 
 namespace Dynamo.Connectors
 {
