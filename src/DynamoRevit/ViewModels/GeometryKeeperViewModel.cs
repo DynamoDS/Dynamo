a61867bc789c58a59f1626824c868acdd3e4217b
Patrick Tierney <patrick.tierney@autodesk.com>
2013-06-07 17:05:44 -0700

    Added class to add Revit elements to GeometryKeeper

---
+++ src/DynamoRevit/ViewModels/GeometryKeeperViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Dynamo.Connectors;
+using Dynamo.Nodes;
+using Dynamo.Utilities;
+using Dynamo.Selection;
+
+namespace Dynamo.Controls
+{
+    public class GeometryKeeperViewModel : dynViewModelBase
+    {
+        public GeometryKeeperViewModel()
+        {
+            dynSettings.Controller.RunCompleted += new DynamoController.RunCompletedHandler(GeometryKeeperViewModel_RunCompleted);
+        }
+
+        public void GeometryKeeperViewModel_RunCompleted(object controller, bool success)
+        {
+
+        }
+    }
+}
