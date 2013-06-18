7aea1a1f339ca4abb8ee3ad052218fd356aa6600
Peter Boyer <peter.b.boyer@gmail.com>
2013-06-18 14:40:22 -0400

    Package manager recursive dependency view

---
+++ src/Dynamo/PackageManager/UI/PackageManagerPublishView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Dynamo.Utilities;
+
+namespace Dynamo.PackageManager
+{
+    /// <summary>
+    /// Interaction logic for PackageManagerPublishView.xaml
+    /// </summary>
+    public partial class PackageManagerPublishView : Window
+    {
+        public PackageManagerPublishView(PackageManagerPublishViewModel viewModel)
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+
+            this.Owner = dynSettings.Bench;
+            this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            this.Title.Focus();
+        }
+    }
+
+}
