90ff469645f4c54cd994d9e6fc9282a6a2a61cea
ikeough <ian@iankeough.com>
2013-06-24 10:04:37 -0700

    Add the sandbox back into the repo.

---
+++ src/DynamoSandbox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using Dynamo;
+using Dynamo.Controls;
+using Dynamo.Utilities;
+
+namespace DynamoSandbox
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            DynamoLogger.Instance.StartLogging();
+
+            try
+            {
+                var controller = new DynamoController(new Dynamo.FSchemeInterop.ExecutionEnvironment(), typeof(DynamoViewModel), Context.NONE);
+
+                //create the view
+                dynSettings.Bench = new DynamoView();
+                dynSettings.Bench.DataContext = controller.DynamoViewModel;
+                controller.UIDispatcher = dynSettings.Bench.Dispatcher;
+
+                var app = new Application();
+                app.Run(dynSettings.Bench);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
