dbbeaca2869da44a05904e30bab5c4588bf4848a
ikeough <ian@iankeough.com>
2013-07-15 00:34:20 -0700

    Move another converter into the converters file.

--- src/Dynamo/Nodes/Watch/WatchViewModel.cs
+++ src/Dynamo/Nodes/Watch/WatchViewModel.cs
@@ -72,17 +72,4 @@ namespace Dynamo.Nodes
     {
 
     }
-
-    public sealed class NullToVisibiltyConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value == null ? System.Windows.Visibility.Hidden : System.Windows.Visibility.Visible;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
