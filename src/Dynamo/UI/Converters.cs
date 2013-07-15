0351a10e33c887a46b49789132b0e99cb4a1ccae
ikeough <ian@iankeough.com>
2013-07-15 00:34:20 -0700

    Move another converter into the converters file.

--- src/Dynamo/UI/Converters.cs
+++ src/Dynamo/UI/Converters.cs
@@ -853,4 +853,17 @@ namespace Dynamo.Controls
             return value;
         }
     }
+
+    public sealed class NullToVisibiltyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? System.Windows.Visibility.Hidden : System.Windows.Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
