93b0587f3ff9004dc9111084628449a5345ca7a1
ikeough <ian@iankeough.com>
2013-07-17 06:24:50 -0700

    Cleanup.

--- src/Dynamo/Search/BrowserItem.cs
+++ src/Dynamo/Search/BrowserItem.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Dynamo.Search.SearchElements;
 using Microsoft.Practices.Prism.Commands;
