9d3b9784c6361037243a7afaaf4c276523af160f
Zach Kron <zachary.kron@autodesk.com>
2013-07-19 18:02:28 -0400

    Update search terms

--- src/DynamoRevit/Nodes/dynGeometry.cs
+++ src/DynamoRevit/Nodes/dynGeometry.cs
@@ -2636,6 +2636,7 @@ namespace Dynamo.Nodes
     [NodeName("Lines Through XYZ")]
     [NodeCategory(BuiltinNodeCategories.CREATEGEOMETRY_CURVE)]
     [NodeDescription("Create a series of linear curves through a set of points.")]
+    [NodeSearchTags("lines", "line", "through", "passing", "thread", "xyz")]
     public class dynCurvesThroughPoints : dynCurveBase
     {
         public dynCurvesThroughPoints()
