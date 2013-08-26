e637e4315724d4f29c6bee612365f44e06dac379
ikeough <ian@iankeough.com>
2013-08-26 13:22:19 -0700

    Unify surface and curve domain outputs.

--- src/DSNodes/Geometry.cs
+++ src/DSNodes/Geometry.cs
@@ -3,9 +3,9 @@
 namespace DSRevitNodes
 {
     /// <summary>
-    /// A UV domain specified by a minimum and maximum UVs.
+    /// A two dimensional UV domain specified by minimum and maximum UVs.
     /// </summary>
-    public class Domain:ObjectWithPropertyDictionary
+    public class Domain2D:ObjectWithPropertyDictionary
     {
         /// <summary>
         /// The minimum of the domain.
@@ -38,7 +38,7 @@ namespace DSRevitNodes
         /// </summary>
         /// <param name="min">The minimum UV.</param>
         /// <param name="max">The maximum UV.</param>
-        public Domain(Vector min, Vector max)
+        public Domain2D(Vector min, Vector max)
         {
             Min = min;
             Max = max;
@@ -50,7 +50,65 @@ namespace DSRevitNodes
         /// <param name="min">The minimum UV.</param>
         /// <param name="max">The maximum UV.</param>
         /// <returns></returns>
-        public static Domain ByMinimumAndMaximum(Vector min, Vector max)
+        public static Domain2D ByMinimumAndMaximum(Vector min, Vector max)
+        {
+            return new Domain2D(min, max);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Min:{0},Max:{1}", Min.ToString(), Max.ToString());
+        }
+
+        public override void Dispose()
+        {
+            //delete the elements
+
+            //unregisters with the persistence manager
+        }
+    }
+
+    /// <summary>
+    /// A one dimensional domain specified by minimum and maximum.
+    /// </summary>
+    public class Domain:ObjectWithPropertyDictionary
+    {
+        /// <summary>
+        /// The minimum of the domain.
+        /// </summary>
+        public double Min { get; set; }
+        
+        /// <summary>
+        /// The maximum of the domain.
+        /// </summary>
+        public double Max { get; set; }
+        
+        /// <summary>
+        /// The span of the domain.
+        /// </summary>
+        public double Span
+        {
+            get { return Max - Min; }
+        }
+        
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="min">The minimum.</param>
+        /// <param name="max">The maximum.</param>
+        public Domain(double min, double max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        /// <summary>
+        /// Construct a domain by minimum and maximum.
+        /// </summary>
+        /// <param name="min">The minimum.</param>
+        /// <param name="max">The maximum.</param>
+        /// <returns></returns>
+        public static Domain ByMinimumAndMaximum(double min, double max)
         {
             return new Domain(min, max);
         }
