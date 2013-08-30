47cae16bef57e7e02353d97c0e3cbc88aacdce64
Stephen Elliott <steell1890@gmail.com>
2013-08-30 13:55:40 -0400

    Added tooltip for ports displaying default value

--- src/DynamoCore/ViewModels/dynPortViewModel.cs
+++ src/DynamoCore/ViewModels/dynPortViewModel.cs
@@ -23,6 +23,11 @@ namespace Dynamo.ViewModels
             get { return _port.ToolTipContent; }
         }
 
+        public string DefaultValueTip
+        {
+            get { return _port.DefaultValueTip; }
+        }
+
         public string PortName
         {
             get
