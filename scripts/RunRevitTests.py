d9bcda24e269f1284da015378719448cfef2464a
ikeough <ian@iankeough.com>
2013-09-05 09:45:08 -0700

    Revit testing script calls sys.exit.

--- scripts/RunRevitTests.py
+++ scripts/RunRevitTests.py
@@ -83,8 +83,13 @@ def main():
 	build_dir = os.path.abspath("../../bin/AnyCPU/Debug")
 	results_start = glob.glob("DynamoRevitTestResults.xml")[0]
 	result_final = os.path.join(build_dir, os.path.basename(results_start))
-	print result_final
+
+	if os.path.exists(result_final):
+		os.remove(result_final)
+
 	os.rename(results_start, result_final)
+
+	sys.exit(0)
 
 def run_cmd( args, printOutput = True, cwd = None ):	
 	p = subprocess.Popen(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd = cwd)
