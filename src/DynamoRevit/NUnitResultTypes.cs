ffc40d90c7a1f29d69c773679add065722dd70f6
ikeough <ian@iankeough.com>
2013-09-05 15:33:59 -0700

    Add more attributes to results files.

--- src/DynamoRevit/NUnitResultTypes.cs
+++ src/DynamoRevit/NUnitResultTypes.cs
@@ -36,6 +36,17 @@ namespace Dynamo.NUnit.Tests {
         private string dateField;
         
         private string timeField;
+
+        //added for compliance with jenkins reader
+        private decimal errorsField;
+
+        private decimal inconclusiveField;
+
+        private decimal ignoredField;
+
+        private decimal skippedField;
+
+        private decimal invalidField;
         
         /// <remarks/>
         [System.Xml.Serialization.XmlElementAttribute("test-suite")]
@@ -113,6 +124,76 @@ namespace Dynamo.NUnit.Tests {
                 this.timeField = value;
             }
         }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal errors
+        {
+            get
+            {
+                return this.errorsField;
+            }
+            set
+            {
+                this.errorsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal inconclusive
+        {
+            get
+            {
+                return this.inconclusiveField;
+            }
+            set
+            {
+                this.inconclusiveField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal ignored
+        {
+            get
+            {
+                return this.ignoredField;
+            }
+            set
+            {
+                this.ignoredField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal skipped
+        {
+            get
+            {
+                return this.skippedField;
+            }
+            set
+            {
+                this.skippedField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal invalid
+        {
+            get
+            {
+                return this.invalidField;
+            }
+            set
+            {
+                this.invalidField = value;
+            }
+        }
     }
     
     /// <remarks/>
@@ -136,7 +217,11 @@ namespace Dynamo.NUnit.Tests {
         private string timeField;
         
         private string assertsField;
-        
+
+        private string typeField;
+
+        private string resultField;
+
         /// <remarks/>
         [System.Xml.Serialization.XmlArrayItemAttribute("category", IsNullable=false)]
         public categoryType[] categories {
@@ -210,6 +295,34 @@ namespace Dynamo.NUnit.Tests {
             }
             set {
                 this.assertsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string type
+        {
+            get
+            {
+                return this.typeField;
+            }
+            set
+            {
+                this.typeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string result
+        {
+            get
+            {
+                return this.resultField;
+            }
+            set
+            {
+                this.resultField = value;
             }
         }
     }
@@ -311,6 +424,8 @@ namespace Dynamo.NUnit.Tests {
         private string executedField;
         
         private string assertsField;
+
+        private string resultField;
         
         /// <remarks/>
         [System.Xml.Serialization.XmlArrayItemAttribute("category", IsNullable=false)]
@@ -398,6 +513,20 @@ namespace Dynamo.NUnit.Tests {
             }
             set {
                 this.assertsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string result
+        {
+            get
+            {
+                return this.resultField;
+            }
+            set
+            {
+                this.resultField = value;
             }
         }
     }
