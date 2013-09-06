50f47ac7f174f245a02761ac957632eb81a52dde
ikeough <ian@iankeough.com>
2013-09-06 10:45:56 -0700

    Fix for MAGN-66.

--- src/DynamoRevitTests/DynamoRevitTests.cs
+++ src/DynamoRevitTests/DynamoRevitTests.cs
@@ -1186,6 +1186,18 @@ namespace DynamoRevitTests
             Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(true));
         }
 
+        [Test]
+        public void MAGN_66()
+        {
+            var model = dynSettings.Controller.DynamoModel;
+
+            string samplePath = Path.Combine(_testPath, @".\MAGN_66.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            model.Open(testPath);
+            Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(true));
+        }
+
         /// <summary>
         /// Automated creation of regression test cases.
         /// </summary>
