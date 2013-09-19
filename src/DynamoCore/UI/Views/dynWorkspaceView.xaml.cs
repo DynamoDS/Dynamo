e2162b4214e1c66f2e4d3fd730e6bc3da779ff26
l2obin <robin.ang.wee.kiat@autodesk.com>
2013-09-19 11:06:24 +0800

    Added UI Toggle button for pan mode at top right   In pan mode, left click can also be use for panning instead of middle click.
      Added shortcut: Escape to exit from pan mode
      Modified Cursor: With hand_pan and hand_pan_active during the pan

--- src/DynamoCore/UI/Views/dynWorkspaceView.xaml.cs
+++ src/DynamoCore/UI/Views/dynWorkspaceView.xaml.cs
@@ -82,6 +82,10 @@ namespace Dynamo.Views
             ViewModel.RequestCenterViewOnElement += new NodeEventHandler(CenterViewOnElement);
             ViewModel.RequestNodeCentered += new NodeEventHandler(vm_RequestNodeCentered);
             ViewModel.RequestAddViewToOuterCanvas += new ViewEventHandler(vm_RequestAddViewToOuterCanvas);
+            ViewModel.RequestTogglePan -= new EventHandler(vm_TogglePan);
+            ViewModel.RequestTogglePan += new EventHandler(vm_TogglePan);
+            ViewModel.RequestStopPan -= new EventHandler(vm_ExitPan);
+            ViewModel.RequestStopPan += new EventHandler(vm_ExitPan);
             ViewModel.WorkspacePropertyEditRequested -= VmOnWorkspacePropertyEditRequested;
             ViewModel.WorkspacePropertyEditRequested += VmOnWorkspacePropertyEditRequested;
         }
@@ -145,6 +149,16 @@ namespace Dynamo.Views
             outerCanvas.Children.Add(view);
             Canvas.SetBottom(view, 0);
             Canvas.SetRight(view, 0);
+        }
+
+        private void vm_TogglePan(object sender, EventArgs e)
+        {
+            zoomBorder.PanMode = !zoomBorder.PanMode;
+        }
+
+        private void vm_ExitPan(object sender, EventArgs e)
+        {
+            zoomBorder.PanMode = false;
         }
 
         private double currentNodeCascadeOffset = 0.0;
---
+++ src/DynamoCore/Utilities/CursorLibrary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Resources;
+
+namespace Dynamo.Utilities
+{
+    public class CursorsLibrary
+    {
+        private static Cursor _handPan;
+        private static Cursor _handPanActive;
+
+        public static Cursor HandPan
+        {
+            get
+            {
+                if (_handPan == null)
+                    _handPan = GetCursorFromFile("hand_pan.cur");
+                return _handPan;
+            }
+        }
+
+        public static Cursor HandPanActive
+        {
+            get
+            {
+                if (_handPanActive == null)
+                    _handPanActive = GetCursorFromFile("hand_pan_active.cur");
+                return _handPanActive;
+            }
+        }
+
+        private static Cursor GetCursorFromFile(string cursorFile)
+        {
+            Uri uri = new Uri("/DynamoCore;component/UI/Images/" + cursorFile, UriKind.Relative);
+            StreamResourceInfo cursorStream = Application.GetResourceStream(uri);
+            return new Cursor(cursorStream.Stream);
+        }
+    }
+}
