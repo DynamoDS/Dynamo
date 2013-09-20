c1a6f7bfde53ea991e3bb59f743a61178913f8e8
ikeough <ian@iankeough.com>
2013-09-19 21:02:26 -0700

    MAGN-90

--- src/DynamoRevitTests/DynamoRevitTests.cs
+++ src/DynamoRevitTests/DynamoRevitTests.cs
@@ -1312,6 +1312,33 @@ namespace DynamoRevitTests
             Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(true));
         }
 
+        [Test]
+        public void Level()
+        {
+            var model = dynSettings.Controller.DynamoModel;
+
+            string samplePath = Path.Combine(_testPath, @".\Level.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            model.Open(testPath);
+            Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(true)); 
+
+            //ensure that the level count is the same
+            var levelColl = new FilteredElementCollector(dynRevitSettings.Doc.Document);
+            levelColl.OfClass(typeof (Autodesk.Revit.DB.Level));
+            Assert.AreEqual(levelColl.ToElements().Count(), 6);
+
+            //change the number and run again
+            var numNode = (DoubleInput)dynRevitSettings.Controller.DynamoModel.Nodes.First(x => x is DoubleInput);
+            numNode.Value = "0..20..2";
+            Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(true)); 
+
+            //ensure that the level count is the same
+            levelColl = new FilteredElementCollector(dynRevitSettings.Doc.Document);
+            levelColl.OfClass(typeof(Autodesk.Revit.DB.Level));
+            Assert.AreEqual(levelColl.ToElements().Count(), 11);
+        }
+
         /// <summary>
         /// Automated creation of regression test cases.
         /// </summary>
