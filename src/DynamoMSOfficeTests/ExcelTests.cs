0eacd6d9b00f5462de5a9f99c0810c3c18ad43e0
Peter Boyer <peter.b.boyer@gmail.com>
2013-09-25 13:15:22 -0400

    All file reading tests pass for Excel, including empty cells

--- src/DynamoMSOfficeTests/ExcelTests.cs
+++ src/DynamoMSOfficeTests/ExcelTests.cs
@@ -55,6 +55,7 @@ namespace DynamoMSOfficeTests
 
             Assert.AreEqual(16, list.Count());
 
+            // contents of first workbook is ascending array of numbers starting at 1
             var counter = 1;
             for (var i = 0; i < 16; i++)
             {
@@ -75,6 +76,36 @@ namespace DynamoMSOfficeTests
             Controller.DynamoModel.Open(openPath);
 
             Assert.AreEqual(6, Controller.DynamoViewModel.CurrentSpace.Nodes.Count);
+
+            var filename = (StringFilename) Controller.DynamoModel.CurrentWorkspace.FirstNodeFromWorkspace<StringFilename>();
+
+            // remap the filename as Excel requires an absolute path
+            filename.Value = filename.Value.Replace(@"..\..\..\test", GetTestDirectory());
+
+            var watch = Controller.DynamoModel.CurrentWorkspace.FirstNodeFromWorkspace<Watch>();
+
+            dynSettings.Controller.RunExpression(null);
+
+            Assert.IsTrue(watch.OldValue.IsList);
+            var list = watch.OldValue.GetListFromFSchemeValue();
+
+            Assert.AreEqual(18, list.Count());
+
+            // 18 x 3 array of numbers
+            for (var i = 0; i < 16; i++)
+            {
+                // get data returns 2d array
+                Assert.IsTrue(list[i].IsList);
+                var rowList = list[i].GetListFromFSchemeValue();
+                Assert.AreEqual(3, rowList.Count());
+
+                for (var j = 0; j < 3; j++)
+                {
+                    Assert.IsTrue(rowList[j].IsNumber);
+                }
+                
+            }
+            
         }
 
         [Test]
@@ -85,6 +116,37 @@ namespace DynamoMSOfficeTests
             Controller.DynamoModel.Open(openPath);
 
             Assert.AreEqual(6, Controller.DynamoViewModel.CurrentSpace.Nodes.Count);
+
+            var filename = (StringFilename)Controller.DynamoModel.CurrentWorkspace.FirstNodeFromWorkspace<StringFilename>();
+
+            // remap the filename as Excel requires an absolute path
+            filename.Value = filename.Value.Replace(@"..\..\..\test", GetTestDirectory());
+
+            var watch = Controller.DynamoModel.CurrentWorkspace.FirstNodeFromWorkspace<Watch>();
+
+            dynSettings.Controller.RunExpression(null);
+
+            Assert.IsTrue(watch.OldValue.IsList);
+            var list = watch.OldValue.GetListFromFSchemeValue();
+
+            Assert.AreEqual(4, list.Count());
+
+            // single column - 1, "word", 2, 3, "palabra"
+            Assert.IsTrue(list[0].IsList);
+            var rowList = list[0].GetListFromFSchemeValue();
+            Assert.AreEqual("a", rowList[0].getStringFromFSchemeValue());
+
+            Assert.IsTrue(list[1].IsList);
+            rowList = list[1].GetListFromFSchemeValue();
+            Assert.IsNull(rowList[0]);
+
+            Assert.IsTrue(list[2].IsList);
+            rowList = list[2].GetListFromFSchemeValue();
+            Assert.AreEqual("cell is", rowList[0].getStringFromFSchemeValue());
+
+            Assert.IsTrue(list[3].IsList);
+            rowList = list[3].GetListFromFSchemeValue();
+            Assert.AreEqual("missing", rowList[0].getStringFromFSchemeValue());
         }
 
         [Test]
@@ -95,9 +157,46 @@ namespace DynamoMSOfficeTests
             Controller.DynamoModel.Open(openPath);
 
             Assert.AreEqual(6, Controller.DynamoViewModel.CurrentSpace.Nodes.Count);
+
+            var filename = (StringFilename)Controller.DynamoModel.CurrentWorkspace.FirstNodeFromWorkspace<StringFilename>();
+
+            // remap the filename as Excel requires an absolute path
+            filename.Value = filename.Value.Replace(@"..\..\..\test", GetTestDirectory());
+
+            var watch = Controller.DynamoModel.CurrentWorkspace.FirstNodeFromWorkspace<Watch>();
+
+            dynSettings.Controller.RunExpression(null);
+
+            Assert.IsTrue(watch.OldValue.IsList);
+            var list = watch.OldValue.GetListFromFSchemeValue();
+
+            Assert.AreEqual(5, list.Count());
+
+            // single column - 1, "word", 2, 3, "palabra"
+            Assert.IsTrue(list[0].IsList);
+            var rowList = list[0].GetListFromFSchemeValue();
+            Assert.AreEqual(1, rowList[0].GetDoubleFromFSchemeValue());
+
+            Assert.IsTrue(list[1].IsList);
+            rowList = list[1].GetListFromFSchemeValue();
+            Assert.AreEqual("word", rowList[0].getStringFromFSchemeValue());
+
+            Assert.IsTrue(list[2].IsList);
+            rowList = list[2].GetListFromFSchemeValue();
+            Assert.AreEqual(2, rowList[0].GetDoubleFromFSchemeValue());
+
+            Assert.IsTrue(list[3].IsList);
+            rowList = list[3].GetListFromFSchemeValue();
+            Assert.AreEqual(3, rowList[0].GetDoubleFromFSchemeValue());
+
+            Assert.IsTrue(list[4].IsList);
+            rowList = list[4].GetListFromFSchemeValue();
+            Assert.AreEqual("palabra", rowList[0].getStringFromFSchemeValue());
             
         }
 
+
+        // other tests: ragged array
         #endregion
 
         #region Writing
