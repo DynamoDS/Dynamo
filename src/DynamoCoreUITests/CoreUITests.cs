46eab902e4dcc66fa9d261ef94e146663929ca98
SeanNguyen <firestormkjc.1993@gmail.com>
2013-10-01 02:39:57 +0800

    Added Some test Cases for InfoBubbleViewModel.cs

--- src/DynamoCoreUITests/CoreUITests.cs
+++ src/DynamoCoreUITests/CoreUITests.cs
@@ -278,5 +278,73 @@ namespace Dynamo.Tests
         }
 
         #endregion
+
+        #region InfoBubble
+        
+        [Test]
+        [Category("DynamoUI")]
+        public void UpdateInfoBubble_LibItem()
+        {
+            InfoBubbleViewModel infoBubble = new InfoBubbleViewModel();
+            string content = "This is the test infoBubble";
+            InfoBubbleDataPacket inputData_LibItem = new InfoBubbleDataPacket(InfoBubbleViewModel.Style.LibraryItemPreview, 
+                new Point(0, 0), new Point(0, 0), content, InfoBubbleViewModel.Direction.Left, Guid.Empty);
+
+            if (infoBubble.UpdateContentCommand.CanExecute(null))
+            {
+                infoBubble.UpdateContentCommand.Execute(inputData_LibItem);
+                Assert.AreEqual(content, infoBubble.ItemDescription);
+                Assert.AreEqual(InfoBubbleViewModel.Style.LibraryItemPreview, infoBubble.InfoBubbleStyle);
+                Assert.AreEqual(InfoBubbleViewModel.Direction.Left, infoBubble.ConnectingDirection);
+            }
+        }
+
+        [Test]
+        [Category("DynamoUI")]
+        public void UpdateInfoBubble_NodeTooltip()
+        {
+            InfoBubbleViewModel infoBubble = new InfoBubbleViewModel();
+            string content = "This is the test infoBubble";
+            InfoBubbleDataPacket inputData_NodeTooltip = new InfoBubbleDataPacket(InfoBubbleViewModel.Style.NodeTooltip,
+                new Point(0, 0), new Point(0, 0), content, InfoBubbleViewModel.Direction.Right, Guid.Empty);
+
+            if (infoBubble.UpdateContentCommand.CanExecute(null))
+            {
+                infoBubble.UpdateContentCommand.Execute(inputData_NodeTooltip);
+                Assert.AreEqual(content, infoBubble.ItemDescription);
+                Assert.AreEqual(InfoBubbleViewModel.Style.NodeTooltip, infoBubble.InfoBubbleStyle);
+                Assert.AreEqual(InfoBubbleViewModel.Direction.Right, infoBubble.ConnectingDirection);
+            }
+        }
+
+        [Test]
+        [Category("DynamoUI")]
+        public void UpdateInfoBubble_ErrorBubble()
+        {
+            InfoBubbleViewModel infoBubble = new InfoBubbleViewModel();
+            string content = "This is the test infoBubble";
+            InfoBubbleDataPacket inputData_ErrorBubble = new InfoBubbleDataPacket(InfoBubbleViewModel.Style.Error,
+                new Point(0, 0), new Point(0, 0), content, InfoBubbleViewModel.Direction.Bottom, Guid.Empty);
+
+            if (infoBubble.UpdateContentCommand.CanExecute(null))
+            {
+                infoBubble.UpdateContentCommand.Execute(inputData_ErrorBubble);
+                Assert.AreEqual(content, infoBubble.ItemDescription);
+                Assert.AreEqual(InfoBubbleViewModel.Style.Error, infoBubble.InfoBubbleStyle);
+                Assert.AreEqual(InfoBubbleViewModel.Direction.Bottom, infoBubble.ConnectingDirection);
+            }
+        }
+
+        [Test]
+        [Category("DynamoUI")]
+        public void Collapse()
+        {
+            InfoBubbleViewModel infoBubble = new InfoBubbleViewModel();
+            infoBubble.InstantCollapseCommand.Execute(null);
+            Assert.AreEqual(0, infoBubble.Opacity);
+        }
+
+	    #endregion
+        
     }
 }
