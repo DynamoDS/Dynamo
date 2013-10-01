ea50a1b8e06bf1d4524f2e396ee291ed7bb44987
Ben Goh <benglin@outlook.com>
2013-10-01 17:35:35 +0800

    Removed "DragCanvas.MyHitTestResult" method since it is not used

--- src/DynamoCore/UI/DragCanvas.cs
+++ src/DynamoCore/UI/DragCanvas.cs
@@ -768,19 +768,6 @@ namespace Dynamo.Controls
         {
             this.dragState = DragState.None;
         }
-
-        // Return the result of the hit test to the callback.
-        public HitTestResultBehavior MyHitTestResult(HitTestResult result)
-        {
-            // Add the hit test result to the list that will be processed after the enumeration.
-            if (!hitResultsList.Contains(result.VisualHit))
-            {
-                hitResultsList.Add(result.VisualHit);
-            }
-
-            // Set the behavior to return visuals at all z-order levels.
-            return HitTestResultBehavior.Continue;
-        }
     }
 
     public class OffsetData
