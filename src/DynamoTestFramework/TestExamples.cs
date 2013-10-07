30e880aa4ea0a1848d707f6c65a5a8f4c509b433
ikeough <ian@iankeough.com>
2013-10-07 16:02:59 -0700

    Find fixtures by name.

--- src/DynamoTestFramework/TestExamples.cs
+++ src/DynamoTestFramework/TestExamples.cs
@@ -2,24 +2,26 @@
 
 namespace Dynamo.Tests
 {
+    [TestFixture]
     public class TestExamples
     {
         [Test]
         public void TestOne()
         {
-
+            Assert.Fail("This failed.");
         }
 
         [Test]
         public void TestTwo()
         {
-
+            Assert.Inconclusive("This is inconclusive.");
         }
 
         [Test]
         public void TestThree()
         {
-
+            //this will pass.
+            Assert.AreEqual(0,0);
         } 
     }
     
