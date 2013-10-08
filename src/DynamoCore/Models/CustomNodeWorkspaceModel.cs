d105019799672a698468491cccec7aeb57b59e96
Peter Boyer <peter.b.boyer@gmail.com>
2013-10-08 12:11:36 -0400

    A few more changes

--- src/DynamoCore/Models/CustomNodeWorkspaceModel.cs
+++ src/DynamoCore/Models/CustomNodeWorkspaceModel.cs
@@ -79,6 +79,8 @@ namespace Dynamo.Models
             var oldPath = this.FileName;
             if (!base.SaveAs(path)) return false;
 
+            if (oldPath == null) this.FunctionDefinition.AddToSearch();
+
             // A SaveAs to an existing function id prompts the creation of a new 
             // custom node with a new function id
             if ( oldPath != path && oldPath != null )
