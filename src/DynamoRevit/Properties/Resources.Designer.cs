a7ece1bc267a1905115abdbb0a3403f533745b33
ikeough <ian@iankeough.com>
2013-10-18 11:01:51 -0700

    Update sandbox logo, Revit addin button, and add images to repo.

--- src/DynamoRevit/Properties/Resources.Designer.cs
+++ src/DynamoRevit/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace Dynamo.Applications.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap logo_square_64x64 {
-            get {
-                object obj = ResourceManager.GetObject("logo_square_64x64", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
