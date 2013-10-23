d0641b00d70749b6444d84b81250e10b24ed667b
ikeough <ian@iankeough.com>
2013-10-23 14:38:02 -0700

    Resize text input box and center text.

--- src/DynamoCore/UI/DynamoTextBox.cs
+++ src/DynamoCore/UI/DynamoTextBox.cs
@@ -74,7 +74,8 @@ namespace Dynamo.Nodes
             GotFocus += OnGotFocus;
             LostFocus += OnLostFocus;
             LostKeyboardFocus += OnLostFocus;
-
+            Height = 20;
+            Padding = new Thickness(3);
             base.Text = initialText;
             this.Pending = false;
         }
