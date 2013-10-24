469c58f3f5d006716d565420cd10360224c1b97e
Ben Goh <benglin@outlook.com>
2013-10-24 11:37:31 +0800

    1. Implemented UndoRedoCommand class for undo-redo user action recording 2. Exposed "DynamoViewModel.UndoRedoImpl" method to handle "UndoRedoCommand"

--- src/DynamoCore/UI/Commands/RecordableCommands.cs
+++ src/DynamoCore/UI/Commands/RecordableCommands.cs
@@ -96,6 +96,8 @@ namespace Dynamo.ViewModels
                         return MakeConnectionCommand.DeserializeCore(element);
                     case "DeleteModelCommand":
                         return DeleteModelCommand.DeserializeCore(element);
+                    case "UndoRedoCommand":
+                        return UndoRedoCommand.DeserializeCore(element);
                 }
 
                 string message = string.Format("Unknown command: {0}", element.Name);
@@ -509,6 +511,49 @@ namespace Dynamo.ViewModels
 
             #endregion
         }
+
+        internal class UndoRedoCommand : RecordableCommand
+        {
+            #region Public Class Methods
+
+            internal enum Operation { Undo, Redo }
+
+            internal UndoRedoCommand(Operation operation)
+            {
+                this.CmdOperation = operation;
+            }
+
+            internal static UndoRedoCommand DeserializeCore(XmlElement element)
+            {
+                XmlElementHelper helper = new XmlElementHelper(element);
+                int operation = helper.ReadInteger("CmdOperation");
+                return new UndoRedoCommand((Operation)operation);
+            }
+
+            #endregion
+
+            #region Public Command Properties
+
+            internal Operation CmdOperation { get; private set; }
+
+            #endregion
+
+            #region Protected Overridable Methods
+
+            protected override void ExecuteCore(DynamoViewModel dynamoViewModel)
+            {
+                dynamoViewModel.UndoRedoImpl(this);
+            }
+
+            protected override void SerializeCore(XmlElement element)
+            {
+                XmlElementHelper helper = new XmlElementHelper(element);
+                helper.SetAttribute("CmdOperation", ((int)this.CmdOperation));
+            }
+
+            #endregion
+        }
+
     }
 
     // internal class XxxYyyCommand : RecordableCommand
