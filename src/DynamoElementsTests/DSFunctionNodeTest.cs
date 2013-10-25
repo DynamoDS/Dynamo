d2516dc1714808f9ec8cea81d9b530ce27ee5aae
Yu Ke <yu.ke@autodesk.com>
2013-10-25 11:55:43 +0800

    Add test cases for DS function node

--- src/DynamoElementsTests/DSFunctionNodeTest.cs
+++ src/DynamoElementsTests/DSFunctionNodeTest.cs
@@ -46,5 +46,56 @@ namespace Dynamo.Tests
             StackValue value = mirror.GetData().GetStackValue();
             Assert.AreEqual(value.opdata_d, 5.0);
         }
+
+        [Test]
+        public void TestAbs()
+        {
+            var model = dynSettings.Controller.DynamoModel;
+
+            string openPath = Path.Combine(GetTestDirectory(), @"core\dsfunction\abs.dyn");
+            model.Open(openPath);
+
+            Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(null));
+
+            // get abs node
+            var absNode = model.CurrentWorkspace.NodeFromWorkspace("2c26388d-3d14-443a-ac41-c2bb0987a58e");
+            string var = (absNode.AstIdentifier as ProtoCore.AST.AssociativeAST.IdentifierNode).Name;
+            RuntimeMirror mirror = null;
+            Assert.DoesNotThrow(() => mirror = LiveRunnerServices.Instance.GetMirror(var));
+            Assert.IsNotNull(mirror);
+
+            StackValue value = mirror.GetData().GetStackValue();
+            Assert.AreEqual(value.opdata_d, 10.0);
+
+            var mulNode = model.CurrentWorkspace.NodeFromWorkspace("0c85072f-f9c5-45f3-8099-832161dfcacb");
+            var = (mulNode.AstIdentifier as ProtoCore.AST.AssociativeAST.IdentifierNode).Name;
+            mirror = null;
+            Assert.DoesNotThrow(() => mirror = LiveRunnerServices.Instance.GetMirror(var));
+            Assert.IsNotNull(mirror);
+
+            value = mirror.GetData().GetStackValue();
+            Assert.AreEqual(value.opdata_d, 100.0);
+        }
+
+        [Test]
+        public void TestCount()
+        {
+            var model = dynSettings.Controller.DynamoModel;
+
+            string openPath = Path.Combine(GetTestDirectory(), @"core\dsfunction\count.dyn");
+            model.Open(openPath);
+
+            Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(null));
+
+            // get count node
+            var count = model.CurrentWorkspace.NodeFromWorkspace("007b5942-12b0-4cea-aa05-b43531b6ccb8");
+            string var = (count.AstIdentifier as ProtoCore.AST.AssociativeAST.IdentifierNode).Name;
+            RuntimeMirror mirror = null;
+            Assert.DoesNotThrow(() => mirror = LiveRunnerServices.Instance.GetMirror(var));
+            Assert.IsNotNull(mirror);
+
+            StackValue value = mirror.GetData().GetStackValue();
+            Assert.AreEqual(value.opdata, 10);
+        }
     }
 }
