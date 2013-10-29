acd9a6f62472e8243f5b665cf36b25d93fc0b3ff
Ian Keough <ian@iankeough.com>
2013-10-29 16:54:03 -0700

    Change some revit namespaces to avoid clashes.

--- src/DSNodes/Level.cs
+++ src/DSNodes/Level.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Dynamo.Revit
+namespace DSRevitNodes
 {
     class Level
     {
--- src/DSNodes/ReferenceCurve.cs
+++ src/DSNodes/ReferenceCurve.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Autodesk.DesignScript.Interfaces;
 
-namespace Dynamo.Revit
+namespace DSRevitNodes
 {
     class ReferenceCurve
     {
