16debc5cdc196515f817b7c62b97315e84d8d5b6
Ian Keough <ian@iankeough.com>
2013-10-28 19:33:58 -0700

    Begin stubbing out ProtoRevit.

---
+++ src/DSNodes/Floor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Curve = Autodesk.DesignScript.Geometry.Curve;
+
+namespace Dynamo.Revit
+{
+    class Floor
+    {
+        static Floor ByOutline(List<Curve> outline, Level level)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DSNodes/StucturalFraming.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Autodesk.Revit.DB;
+using Point = Autodesk.DesignScript.Geometry.Point;
+
+namespace Dynamo.Revit
+{
+    class StucturalFraming
+    {
+        static FamilyInstance ByStartEndPoints(Point start, Point end, FamilySymbol framing_type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
