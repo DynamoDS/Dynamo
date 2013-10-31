d83c81cb5a4d27a039dfe1cf89ee65b8bc9e52ef
Luke Church <luke@church.name>
2013-10-30 22:21:26 -0700

    Factoring of shared dispose behaviour into the AbstractGeometry, and migration of Revit specific functionality from Point -> ReferencePoint
    Basic implementation without trace of the point, with property update, transaction management and dispose.

--- src/DSNodes/Point.cs
+++ src/DSNodes/Point.cs
@@ -8,85 +8,11 @@ using RevitServices.Transactions;
 
 namespace DSRevitNodes
 {
-    /// <summary>
-    /// A sample geometry class
-    /// Node that it needs to be reistered for trace in order to be able to
-    /// rebind itself
-    /// </summary>
-    [RegisterForTrace]
+
     public class Point : AbstractGeometry
     {
-        protected Autodesk.Revit.DB.ReferencePoint RefPoint;
-        protected ElementId ID;
 
-        private Point()
-        {
-            
-        }
+        //This is now a placeholder for a Proto-Point
 
-        /// <summary>
-        /// A sample property
-        /// </summary>
-        public double X
-        {
-            get
-            {
-                return RefPoint.Position.X;
-            }
-
-            set
-            {
-                XYZ xyz = new XYZ(value, Y, Z);
-                var transManager = new TransactionManager();
-                var transaction = transManager.StartTransaction(Document);
-
-                RefPoint.Position = xyz;
-
-                transaction.CommitTransaction();
-            }
-
-        }
-        public double Y { get; set; }
-        public double Z { get; set; }
-
-        /// <summary>
-        /// A sample constructor
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="z"></param>
-        /// <returns></returns>
-        public static Point ByCoordinates(double x, double y, double z)
-        {
-            var transManager = new TransactionManager();
-            var transaction = transManager.StartTransaction(Document);
-
-            Autodesk.Revit.DB.ReferencePoint refPoint = Document.FamilyCreate.NewReferencePoint(new XYZ(x, y, z));
-            ElementId elementId = refPoint.Id;
-
-            transaction.CommitTransaction();
-
-            //@TODO(Luke): Add a test that does readback verification to make sure this is safe, it's almost certainly faster
-            Point pt = new Point();
-            pt.Y = y;
-            pt.Z = z;
-            pt.RefPoint = refPoint;
-            pt.ID = elementId;
-
-            return pt;
-        }
-
-
-        public override void Dispose()
-        {
-            //TODO(Luke): Move this into persistence
-            var transManager = new TransactionManager();
-            var transaction = transManager.StartTransaction(Document);
-
-            Document.Delete(ID);
-
-            transaction.CommitTransaction();
-
-        }
     }
 }
