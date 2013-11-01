2f85299a5903f0f7867fac6a4bafc0f87f87ed86
Ian Keough <ian@iankeough.com>
2013-11-01 12:41:21 -0700

    Separation and creation of many new curve types.

---
+++ src/DynamoRevit/Nodes/Arc.cs
@@ -0,0 +1,104 @@
+﻿using Autodesk.Revit.DB;
+using Dynamo.Models;
+using Dynamo.Utilities;
+using Microsoft.FSharp.Collections;
+
+namespace Dynamo.Nodes
+{
+    [NodeName("Arc by Start, Middle, End")]
+    [NodeCategory(BuiltinNodeCategories.GEOMETRY_CURVE_CREATE)]
+    [NodeDescription("Creates a geometric arc given start, middle and end points in XYZ.")]
+    [NodeSearchTags("arc", "circle", "start", "middle", "end", "3 point", "three")]
+    public class ArcStartMiddleEnd : GeometryBase
+    {
+        public ArcStartMiddleEnd()
+        {
+            InPortData.Add(new PortData("start", "Start XYZ", typeof(FScheme.Value.Container)));
+            InPortData.Add(new PortData("mid", "XYZ on Curve", typeof(FScheme.Value.Container)));
+            InPortData.Add(new PortData("end", "End XYZ", typeof(FScheme.Value.Container)));
+            OutPortData.Add(new PortData("arc", "Arc", typeof(FScheme.Value.Container)));
+
+            RegisterAllPorts();
+        }
+
+        public override FScheme.Value Evaluate(FSharpList<FScheme.Value> args)
+        {
+
+            Arc a = null;
+
+            var ptA = ((FScheme.Value.Container)args[0]).Item;//start
+            var ptB = ((FScheme.Value.Container)args[1]).Item;//middle
+            var ptC = ((FScheme.Value.Container)args[2]).Item;//end
+
+            if (ptA is XYZ)
+            {
+
+                a = dynRevitSettings.Doc.Application.Application.Create.NewArc(
+                   (XYZ)ptA, (XYZ)ptC, (XYZ)ptB //start, end, middle 
+                );
+
+
+            }
+            else if (ptA is ReferencePoint)
+            {
+                a = dynRevitSettings.Doc.Application.Application.Create.NewArc(
+                   (XYZ)((ReferencePoint)ptA).Position, (XYZ)((ReferencePoint)ptB).Position, (XYZ)((ReferencePoint)ptC).Position //start, end, middle 
+                );
+
+            }
+
+            return FScheme.Value.NewContainer(a);
+        }
+    }
+
+    [NodeName("Arc by Center, Normal, Radius")]
+    [NodeCategory(BuiltinNodeCategories.GEOMETRY_CURVE_CREATE)]
+    [NodeDescription("Creates a geometric arc given a center point and two end parameters. Start and End Values may be between 0 and 2*PI in Radians")]
+    [NodeSearchTags("arc", "circle", "center", "radius")]
+    public class ArcCenter : GeometryBase
+    {
+        public ArcCenter()
+        {
+            InPortData.Add(new PortData("center", "center XYZ or Coordinate System", typeof(FScheme.Value.Container)));
+            InPortData.Add(new PortData("radius", "Radius", typeof(FScheme.Value.Number)));
+            InPortData.Add(new PortData("start", "Start Param", typeof(FScheme.Value.Number)));
+            InPortData.Add(new PortData("end", "End Param", typeof(FScheme.Value.Number)));
+            OutPortData.Add(new PortData("arc", "Arc", typeof(FScheme.Value.Container)));
+
+            RegisterAllPorts();
+        }
+
+        public override FScheme.Value Evaluate(FSharpList<FScheme.Value> args)
+        {
+            var ptA = ((FScheme.Value.Container)args[0]).Item;
+            var radius = (double)((FScheme.Value.Number)args[1]).Item;
+            var start = (double)((FScheme.Value.Number)args[2]).Item;
+            var end = (double)((FScheme.Value.Number)args[3]).Item;
+
+            Arc a = null;
+
+            if (ptA is XYZ)
+            {
+                a = dynRevitSettings.Doc.Application.Application.Create.NewArc(
+                   (XYZ)ptA, radius, start, end, XYZ.BasisX, XYZ.BasisY
+                );
+            }
+            else if (ptA is ReferencePoint)
+            {
+                a = dynRevitSettings.Doc.Application.Application.Create.NewArc(
+                   (XYZ)((ReferencePoint)ptA).Position, radius, start, end, XYZ.BasisX, XYZ.BasisY
+                );
+            }
+            else if (ptA is Transform)
+            {
+                Transform trf = ptA as Transform;
+                XYZ center = trf.Origin;
+                a = dynRevitSettings.Doc.Application.Application.Create.NewArc(
+                             center, radius, start, end, trf.BasisX, trf.BasisY
+                );
+            }
+
+            return FScheme.Value.NewContainer(a);
+        }
+    }
+}
---
+++ src/DynamoRevit/Nodes/Line.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Reflection;
+using Autodesk.Revit.DB;
+using Dynamo.Models;
+using Dynamo.Revit;
+using Dynamo.Utilities;
+using Microsoft.FSharp.Collections;
+
+namespace Dynamo.Nodes
+{
+    [NodeName("Line by Endpoints")]
+    [NodeCategory(BuiltinNodeCategories.GEOMETRY_CURVE_CREATE)]
+    [NodeDescription("Creates a geometric line.")]
+    [NodeSearchTags("curve", "two point", "line")]
+    public class LineBound : GeometryBase
+    {
+        public LineBound()
+        {
+            InPortData.Add(new PortData("start", "Start XYZ", typeof(FScheme.Value.Container)));
+            InPortData.Add(new PortData("end", "End XYZ", typeof(FScheme.Value.Container)));
+            //InPortData.Add(new PortData("bound?", "Boolean: Is this line bounded?", typeof(bool)));
+
+            OutPortData.Add(new PortData("line", "Line", typeof(FScheme.Value.Container)));
+
+            RegisterAllPorts();
+        }
+
+        public override FScheme.Value Evaluate(FSharpList<FScheme.Value> args)
+        {
+            var ptA = ((FScheme.Value.Container)args[0]).Item;
+            var ptB = ((FScheme.Value.Container)args[1]).Item;
+
+            Line line = null;
+
+            if (ptA is XYZ)
+            {
+
+                line = dynRevitSettings.Doc.Application.Application.Create.NewLineBound(
+                  (XYZ)ptA, (XYZ)ptB
+                  );
+
+
+            }
+            else if (ptA is ReferencePoint)
+            {
+                line = dynRevitSettings.Doc.Application.Application.Create.NewLineBound(
+                  (XYZ)((ReferencePoint)ptA).Position, (XYZ)((ReferencePoint)ptB).Position
+               );
+
+            }
+
+            return FScheme.Value.NewContainer(line);
+        }
+    }
+
+    [NodeName("Line by Origin and Direction")]
+    [NodeCategory(BuiltinNodeCategories.GEOMETRY_CURVE_CREATE)]
+    [NodeDescription("Creates a line in the direction of an XYZ normal.")]
+    public class LineVectorfromXyz : NodeWithOneOutput
+    {
+        public LineVectorfromXyz()
+        {
+            InPortData.Add(new PortData("normal", "Normal Point (XYZ)", typeof(FScheme.Value.Container)));
+            InPortData.Add(new PortData("origin", "Origin Point (XYZ)", typeof(FScheme.Value.Container)));
+            OutPortData.Add(new PortData("curve", "Curve", typeof(FScheme.Value.Container)));
+
+            RegisterAllPorts();
+        }
+
+        public override FScheme.Value Evaluate(FSharpList<FScheme.Value> args)
+        {
+            var ptA = (XYZ)((FScheme.Value.Container)args[0]).Item;
+            var ptB = (XYZ)((FScheme.Value.Container)args[1]).Item;
+
+            // CurveElement c = MakeLine(this.UIDocument.Document, ptA, ptB);
+            CurveElement c = MakeLineCBP(dynRevitSettings.Doc.Document, ptA, ptB);
+
+            return FScheme.Value.NewContainer(c);
+        }
+
+
+        public Autodesk.Revit.DB.ModelCurve MakeLine(Document doc, XYZ ptA, XYZ ptB)
+        {
+            Autodesk.Revit.ApplicationServices.Application app = doc.Application;
+            // Create plane by the points
+            Line line = app.Create.NewLine(ptA, ptB, true);
+            XYZ norm = ptA.CrossProduct(ptB);
+            double length = norm.GetLength();
+            if (length == 0) norm = XYZ.BasisZ;
+            Autodesk.Revit.DB.Plane plane = app.Create.NewPlane(norm, ptB);
+            Autodesk.Revit.DB.SketchPlane skplane = doc.FamilyCreate.NewSketchPlane(plane);
+            // Create line here
+            Autodesk.Revit.DB.ModelCurve modelcurve = doc.FamilyCreate.NewModelCurve(line, skplane);
+            return modelcurve;
+        }
+
+        public Autodesk.Revit.DB.CurveByPoints MakeLineCBP(Document doc, XYZ ptA, XYZ ptB)
+        {
+            ReferencePoint sunRP = doc.FamilyCreate.NewReferencePoint(ptA);
+            ReferencePoint originRP = doc.FamilyCreate.NewReferencePoint(ptB);
+            ReferencePointArray sunRPArray = new ReferencePointArray();
+            sunRPArray.Append(sunRP);
+            sunRPArray.Append(originRP);
+            Autodesk.Revit.DB.CurveByPoints sunPath = doc.FamilyCreate.NewCurveByPoints(sunRPArray);
+            return sunPath;
+        }
+    }
+
+    [NodeName("Bisector Line")]
+    [NodeCategory(BuiltinNodeCategories.REVIT_REFERENCE)]
+    [NodeDescription("Creates bisector of two lines")]
+    [DoNotLoadOnPlatforms(Context.REVIT_2013, Context.REVIT_2014, Context.VASARI_2013)]
+    public class Bisector : RevitTransactionNodeWithOneOutput
+    {
+        public Bisector()
+        {
+            InPortData.Add(new PortData("line1", "First Line", typeof(FScheme.Value.Container)));
+            InPortData.Add(new PortData("line2", "Second Line", typeof(FScheme.Value.Container)));
+            OutPortData.Add(new PortData("bisector", "Bisector Line", typeof(FScheme.Value.Container)));
+
+            RegisterAllPorts();
+        }
+        public override FScheme.Value Evaluate(FSharpList<FScheme.Value> args)
+        {
+            Line line1 = (Line)((FScheme.Value.Container)args[0]).Item;
+            Line line2 = (Line)((FScheme.Value.Container)args[1]).Item;
+
+            Type LineType = typeof(Autodesk.Revit.DB.Line);
+
+            MethodInfo[] lineInstanceMethods = LineType.GetMethods(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
+
+            System.String nameOfMethodCreateBisector = "CreateBisector";
+            Line result = null;
+
+            foreach (MethodInfo m in lineInstanceMethods)
+            {
+                if (m.Name == nameOfMethodCreateBisector)
+                {
+                    object[] argsM = new object[1];
+                    argsM[0] = line2;
+
+                    result = (Line)m.Invoke(line1, argsM);
+
+                    break;
+                }
+            }
+
+            return FScheme.Value.NewContainer(result);
+        }
+    }
+}
