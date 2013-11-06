f3211288b0cbb131e4640fc4ee797b57e2c92820
Ian Keough <ian@iankeough.com>
2013-11-06 05:48:10 -0800

    Add some more revit python test stuff. Hold off adding to input file until we can get them to run.

--- src/DynamoCore/ViewModels/SearchViewModel.cs
+++ src/DynamoCore/ViewModels/SearchViewModel.cs
@@ -237,7 +237,7 @@ namespace Dynamo.ViewModels
             MaxNumSearchResults = 20;
             Visible = false;
             _SearchText = "";
-            IncludeRevitAPIElements = false; // revit api
+            IncludeRevitAPIElements = true; // revit api
 
             _topResult = this.AddRootCategory("Top Result");
             this.AddRootCategory(BuiltinNodeCategories.CORE);
