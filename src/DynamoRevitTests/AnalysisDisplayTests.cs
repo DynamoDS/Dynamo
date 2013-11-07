b154c231f2e08334dbb0306ada64d0cdf074f804
Ian Keough <ian@iankeough.com>
2013-11-07 13:07:03 -0800

    Add basic analysis display tests and batch file.

--- src/DynamoRevitTests/AnalysisDisplayTests.cs
+++ src/DynamoRevitTests/AnalysisDisplayTests.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NUnit.Framework;
 
 namespace Dynamo.Tests
 {
-    class AnalysisDisplayTests
+    [TestFixture]
+    class AnalysisDisplayTests : DynamoRevitUnitTestBase
     {
+        [Test]
+        public void AnalysisDisplayPoints()
+        {
+            OpenAndRun(@".\AnalysisDisplay\AD_Points.dyn");
+        }
+
+        [Test]
+        public void AnalysisDisplaySurface()
+        {
+            OpenAndRun(@".\AnalysisDisplay\AD_Surface.dyn");
+        }
+
+        [Test]
+        public void AnalysisDisplayVectors()
+        {
+            OpenAndRun(@".\AnalysisDisplay\AD_Vector.dyn");
+        }
     }
 }
