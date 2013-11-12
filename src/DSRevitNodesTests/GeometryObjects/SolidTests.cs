b286f2e07ca5780a6c3c607e32873d03188b57c7
Peter Boyer <peter.b.boyer@gmail.com>
2013-11-12 12:20:25 -0500

    Unit tests for DSSolid.ByExtrusion

--- src/DSRevitNodesTests/GeometryObjects/SolidTests.cs
+++ src/DSRevitNodesTests/GeometryObjects/SolidTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Autodesk.DesignScript.Geometry;
+using DSRevitNodes;
 using DSRevitNodes.GeometryObjects;
 using NUnit.Framework;
 
@@ -11,18 +12,58 @@ namespace DSRevitNodesTests.GeometryObjects
     [TestFixture]
     public class SolidTests
     {
-
-
         [Test]
         public void ByExtrusion_ValidArgs()
         {
-            // make a rectangle
+          
+            // construct a unit rectangle
+            var pts1 = new[]
+            {
+                Point.ByCoordinates(0,0,0),
+                Point.ByCoordinates(0.4,0,0),
+                Point.ByCoordinates(0.8,0,0),
+                Point.ByCoordinates(1,0,0),
+            };
 
-            
+            var pts2 = new[]
+            {
+                Point.ByCoordinates(1,0,0),
+                Point.ByCoordinates(1,0.4,0),
+                Point.ByCoordinates(1,0.8,0),
+                Point.ByCoordinates(1,1,0)
+            };
 
+            var pts3 = new[]
+            {
+                Point.ByCoordinates(1,1,0),
+                Point.ByCoordinates(0.8,0.8,0),
+                Point.ByCoordinates(0.4,0.4,0),
+                Point.ByCoordinates(0.0,0,0)
+            };
 
+            var wts = new double[]
+            {
+                1,1,1,1
+            };
 
-            Assert.Inconclusive();
+            var crvs = new[]
+            {
+                DSNurbSpline.ByControlPointsAndWeights(pts1, wts),
+                DSNurbSpline.ByControlPointsAndWeights(pts2, wts),
+                DSNurbSpline.ByControlPointsAndWeights(pts3, wts)
+            };
+
+            // construct the curveloop
+            var curveloop = DSCurveLoop.ByCurves(crvs);
+
+            var dir = Vector.ByCoordinates(0, 0, 1);
+            var dist = 5;
+            var extrusion = DSSolid.ByExtrusion(curveloop, dir, dist);
+
+            Assert.NotNull(extrusion);
+            Assert.AreEqual(2.5, extrusion.Volume);
+            Assert.AreEqual(5 + 5 + 2.5 + 2.5 + Math.Sqrt(2) * 5, extrusion.SurfaceArea);
+
         }
     }
 }
