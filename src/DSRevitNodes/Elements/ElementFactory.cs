b83e2a03263a60f0b6baad5fc4db66700182994f
Peter Boyer <peter.b.boyer@gmail.com>
2013-11-12 10:16:05 -0500

    Obtain all elements of type using DocumentManager, Unit tests for ElementFactory

--- src/DSRevitNodes/Elements/ElementFactory.cs
+++ src/DSRevitNodes/Elements/ElementFactory.cs
@@ -21,19 +21,29 @@ namespace DSRevitNodes.Elements
         {
             var ele = InternalGetElementById(id);
 
-            // return the appropriate type
-            if (ele is Autodesk.Revit.DB.FamilyInstance)
+            if (ele != null)
             {
-                return new DSFamilyInstance(ele as Autodesk.Revit.DB.FamilyInstance);
+                return InternalGetDSTypeFromElement(ele);
             }
             
-            if (ele is Autodesk.Revit.DB.Family)
+            throw new Exception("Could not get the element from the document");
+        }
+
+        /// <summary>
+        /// A factory method for looking up and obtaining elements
+        /// from the revit project
+        /// </summary>
+        /// <returns></returns>
+        public static AbstractElement ByUniqueId(string uniqueId)
+        {
+            var ele = InternalGetElementByUniqueId(uniqueId);
+
+            if (ele != null)
             {
-                return new DSFamily(ele as Autodesk.Revit.DB.Family);
+                return InternalGetDSTypeFromElement(ele);
             }
 
-            throw new Exception("The element is of an unknown type.");
-
+            throw new Exception("Could not get the element from the document");
         }
 
         /// <summary>
@@ -48,10 +58,53 @@ namespace DSRevitNodes.Elements
 
             if (!AbstractElement.Document.TryGetElement(eleId, out ele))
             {
-                throw new Exception("Could not obtain element from the current document.  The id may not be valid");
+                throw new Exception("Could not obtain element from the current document!  The id may not be valid.");
             }
 
             return ele;
+        }
+
+        /// <summary>
+        /// Obtain an element from the current document given the element's unique id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private static Autodesk.Revit.DB.Element InternalGetElementByUniqueId(string uniqueId)
+        {
+            Element ele;
+
+            if (!AbstractElement.Document.TryGetElement(uniqueId, out ele))
+            {
+                throw new Exception("Could not obtain element from the current document!  The unique id may not be valid.");
+            }
+
+            return ele;
+        }
+
+        /// <summary>
+        /// Get the DS wrapper object given a Revit element
+        /// </summary>
+        /// <param name="ele"></param>
+        /// <returns></returns>
+        private static AbstractElement InternalGetDSTypeFromElement(Autodesk.Revit.DB.Element ele)
+        {
+
+            if (ele is Autodesk.Revit.DB.FamilyInstance)
+            {
+                return new DSFamilyInstance(ele as Autodesk.Revit.DB.FamilyInstance);
+            }
+
+            if (ele is Autodesk.Revit.DB.FamilySymbol)
+            {
+                return new DSFamilySymbol(ele as Autodesk.Revit.DB.FamilySymbol);
+            }
+
+            if (ele is Autodesk.Revit.DB.Family)
+            {
+                return new DSFamily(ele as Autodesk.Revit.DB.Family);
+            }
+
+            throw new Exception("The element is of an unknown type.");
         }
 
     }
