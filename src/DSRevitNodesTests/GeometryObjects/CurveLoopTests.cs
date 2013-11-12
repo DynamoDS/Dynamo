9fef2864a69e203961269e2273b6d071dfe4ef4c
Peter Boyer <peter.b.boyer@gmail.com>
2013-11-12 12:09:47 -0500

    More tests for CurveLoop

--- src/DSRevitNodesTests/GeometryObjects/CurveLoopTests.cs
+++ src/DSRevitNodesTests/GeometryObjects/CurveLoopTests.cs
@@ -66,5 +66,128 @@ namespace DSRevitNodesTests.GeometryObjects
             Assert.IsTrue(curveloop.IsPlanar);
             Assert.IsTrue(curveloop.IsClosed);
         }
+
+        [Test]
+        public void TriangleByCurves_ValidArgs()
+        {
+            // construct a unit rectangle
+            var pts1 = new[]
+            {
+                Point.ByCoordinates(0,0,0),
+                Point.ByCoordinates(0.4,0,0),
+                Point.ByCoordinates(0.8,0,0),
+                Point.ByCoordinates(1,0,0),
+            };
+
+            var pts2 = new[]
+            {
+                Point.ByCoordinates(1,0,0),
+                Point.ByCoordinates(1,0.4,0),
+                Point.ByCoordinates(1,0.8,0),
+                Point.ByCoordinates(1,1,0)
+            };
+
+            var pts3 = new[]
+            {
+                Point.ByCoordinates(1,1,0),
+                Point.ByCoordinates(0.8,0.8,0),
+                Point.ByCoordinates(0.4,0.4,0),
+                Point.ByCoordinates(0.0,0,0)
+            };
+
+            var wts = new double[]
+            {
+                1,1,1,1
+            };
+
+            var crvs = new[]
+            {
+                DSNurbSpline.ByControlPointsAndWeights(pts1, wts),
+                DSNurbSpline.ByControlPointsAndWeights(pts2, wts),
+                DSNurbSpline.ByControlPointsAndWeights(pts3, wts)
+            };
+
+            // construct the curveloop
+            var curveloop = DSCurveLoop.ByCurves(crvs);
+            Assert.NotNull(curveloop);
+            Assert.IsTrue(curveloop.IsPlanar);
+            Assert.IsTrue(curveloop.IsClosed);
+        }
+
+        [Test]
+        public void OpenCurveByCurves_ValidArgs()
+        {
+            // construct a unit rectangle
+            var pts1 = new[]
+            {
+                Point.ByCoordinates(0,0,0),
+                Point.ByCoordinates(0.4,0,0),
+                Point.ByCoordinates(0.8,0,0),
+                Point.ByCoordinates(1,0,0),
+            };
+
+            var pts2 = new[]
+            {
+                Point.ByCoordinates(1,0,0),
+                Point.ByCoordinates(1,0.4,0),
+                Point.ByCoordinates(1,0.8,0),
+                Point.ByCoordinates(1,1,0)
+            };
+
+            var wts = new double[]
+            {
+                1,1,1,1
+            };
+
+            var crvs = new[]
+            {
+                DSNurbSpline.ByControlPointsAndWeights(pts1, wts),
+                DSNurbSpline.ByControlPointsAndWeights(pts2, wts)
+            };
+
+            // construct the curveloop
+            var curveloop = DSCurveLoop.ByCurves(crvs);
+            Assert.NotNull(curveloop);
+            Assert.IsTrue(curveloop.IsPlanar);
+            Assert.IsFalse(curveloop.IsClosed);
+        }
+
+        [Test]
+        public void NonPlanarOpenCurveByCurves_ValidArgs()
+        {
+            // construct a unit rectangle
+            var pts1 = new[]
+            {
+                Point.ByCoordinates(0,0,0),
+                Point.ByCoordinates(0.4,0,0),
+                Point.ByCoordinates(0.8,0,0),
+                Point.ByCoordinates(1,0,0),
+            };
+
+            var pts2 = new[]
+            {
+                Point.ByCoordinates(1,0,0),
+                Point.ByCoordinates(1,0.4,0.4),
+                Point.ByCoordinates(1,0.8,0.8),
+                Point.ByCoordinates(1,1,1)
+            };
+
+            var wts = new double[]
+            {
+                1,1,1,1
+            };
+
+            var crvs = new[]
+            {
+                DSNurbSpline.ByControlPointsAndWeights(pts1, wts),
+                DSNurbSpline.ByControlPointsAndWeights(pts2, wts)
+            };
+
+            // construct the curveloop
+            var curveloop = DSCurveLoop.ByCurves(crvs);
+            Assert.NotNull(curveloop);
+            Assert.IsFalse(curveloop.IsPlanar);
+            Assert.IsFalse(curveloop.IsClosed);
+        }
     }
 }
