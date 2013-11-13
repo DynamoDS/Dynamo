6870f338a767e6d14f6e513166d42e7fbbf3735d
Yu Ke <yu.ke@autodesk.com>
2013-11-13 12:42:33 +0800

    Add regression test case for MANG-561

--- src/DynamoElementsTests/DSEvaluationTest.cs
+++ src/DynamoElementsTests/DSEvaluationTest.cs
@@ -22,18 +22,28 @@ namespace Dynamo.Tests
             Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(null));
         }
 
-        private void AssertValue(string guid, object value)
+        private string GetVarName(string guid)
         {
             var model = dynSettings.Controller.DynamoModel;
             var node = model.CurrentWorkspace.NodeFromWorkspace(guid);
             Assert.IsNotNull(node);
+            return  node.VariableToPreview;
+        }
 
+        private RuntimeMirror GetRuntimeMirror(string varName)
+        {
             RuntimeMirror mirror = null;
-            string var = node.VariableToPreview;
-            Assert.DoesNotThrow(() => mirror = EngineController.Instance.GetMirror(var));
+            Assert.DoesNotThrow(() => mirror = EngineController.Instance.GetMirror(varName));
             Assert.IsNotNull(mirror);
+            return mirror;
+        }
 
-            Console.WriteLine(var + " = " + mirror.GetStringData());
+        private void AssertValue(string guid, object value)
+        {
+            string varname = GetVarName(guid);
+            var mirror = GetRuntimeMirror(varname);
+
+            Console.WriteLine(varname + " = " + mirror.GetStringData());
             StackValue svValue = mirror.GetData().GetStackValue();
             if (value is double)
             {
@@ -46,20 +56,40 @@ namespace Dynamo.Tests
             else if (value is IEnumerable<int>)
             {
                 var values = (value as IEnumerable<int>).ToList().Select(v => (object)v).ToList();
-                Assert.IsTrue(mirror.GetUtils().CompareArrays(var, values, typeof(Int64)));
+                Assert.IsTrue(mirror.GetUtils().CompareArrays(varname, values, typeof(Int64)));
             }
             else if (value is IEnumerable<double>)
             {
                 var values = (value as IEnumerable<double>).ToList().Select(v => (object)v).ToList();
-                Assert.IsTrue(mirror.GetUtils().CompareArrays(var, values, typeof(double)));
+                Assert.IsTrue(mirror.GetUtils().CompareArrays(varname, values, typeof(double)));
             }
+        }
+
+        private void AssertIsPointer(string guid)
+        {
+            string varname = GetVarName(guid);
+            var mirror = GetRuntimeMirror(varname);
+
+            StackValue svValue = mirror.GetData().GetStackValue();
+            Assert.IsTrue(StackUtils.IsValidPointer(svValue));
         }
 
         [Test]
         public void TestCodeBlockNode01()
         {
+            // (1..5) + 1;
             RunModel(@"core\dsevaluation\cbn_nolhs.dyn");
             AssertValue("4e88b4a9-397a-422e-be13-f9ffcb27bc75", new int[] { 2, 3, 4, 5, 6});
+        }
+
+        [Test]
+        public void Regress561()
+        {
+            // 1; ----> x
+            // 2; ----> y Point.ByCoordinates(x, y, z);
+            // 3; ----> z
+            RunModel(@"core\dsevaluation\regress561.dyn");
+            AssertIsPointer("8774296c-5269-450b-959d-ce4020ddbf80");
         }
     }
 }
