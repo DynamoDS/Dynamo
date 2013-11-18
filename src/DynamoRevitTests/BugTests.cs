9a402c07a3cbeccbbe3d5d0dba0407c899e56ed7
riteshchandawar <ritesh.chandawar@autodesk.com>
2013-11-18 16:26:57 +0800

    Added 2 test cases related to Defects. Also created batch file to run Defect related test cases.

--- src/DynamoRevitTests/BugTests.cs
+++ src/DynamoRevitTests/BugTests.cs
@@ -18,5 +18,39 @@ namespace Dynamo.Tests
             model.Open(testPath);
             Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(true));
         }
+
+        [Test]
+        public void MAGN_102()
+        {
+            var model = dynSettings.Controller.DynamoModel;
+
+            string samplePath = Path.Combine(_testPath, @".\\Bugs\MAGN_102_projectPointsToFace_selfContained.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            model.Open(testPath);
+
+            // check all the nodes and connectors are loaded
+            Assert.AreEqual(12, model.CurrentWorkspace.Nodes.Count);
+            Assert.AreEqual(11, model.CurrentWorkspace.Connectors.Count);
+
+            Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(true));
+        }
+
+        [Test]
+        public void MAGN_122()
+        {
+            var model = dynSettings.Controller.DynamoModel;
+
+            string samplePath = Path.Combine(_testPath, @".\\Bugs\MAGN_122_wallsAndFloorsAndLevels.dyn");
+            string testPath = Path.GetFullPath(samplePath);
+
+            model.Open(testPath);
+
+            // check all the nodes and connectors are loaded
+            Assert.AreEqual(17, model.CurrentWorkspace.Nodes.Count);
+            Assert.AreEqual(20, model.CurrentWorkspace.Connectors.Count);
+
+            Assert.DoesNotThrow(() => dynSettings.Controller.RunExpression(true));
+        }
     }
 }
