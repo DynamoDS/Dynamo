003296964689439399a7f284e4ce67b24aafe2b0
Peter Boyer <peter.b.boyer@gmail.com>
2013-11-20 15:06:18 -0500

    Sort packages by most recent update, by default sort packages by most recent update in search

--- src/DynamoCore/Properties/AssemblyInfo.cs
+++ src/DynamoCore/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DynaoElements")]
+[assembly: AssemblyTitle("DynamoCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk Inc.")]
