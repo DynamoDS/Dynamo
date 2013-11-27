6a56f36510c59930943b0ffc5dc5b43cf169407f
Peter Boyer <peter.b.boyer@gmail.com>
2013-11-27 16:40:05 -0500

    Implemented DSFloorType, DSFloor

--- src/DSRevitNodes/Elements/DSFloor.cs
+++ src/DSRevitNodes/Elements/DSFloor.cs
@@ -15,6 +15,9 @@ namespace DSRevitNodes
     /// </summary>
     public class DSFloor : AbstractElement
     {
+        /// <summary>
+        /// An internal handle on the Revit floor
+        /// </summary>
         internal Autodesk.Revit.DB.Floor InternalFloor
         {
             get; private set;
@@ -48,7 +51,13 @@ namespace DSRevitNodes
             this.InternalUniqueId = floor.UniqueId;
         }
 
-        public static DSFloor ByOutline( Autodesk.DesignScript.Geometry.Curve[] outline, DSLevel level)
+        /// <summary>
+        /// Create a Revit Floor given it's curve outline and Level
+        /// </summary>
+        /// <param name="outline"></param>
+        /// <param name="level"></param>
+        /// <returns>The floor</returns>
+        public static DSFloor ByOutline( Autodesk.DesignScript.Geometry.Curve[] outline, DSFloorType floorType, DSLevel level)
         {
             if (outline == null)
             {
@@ -68,75 +77,7 @@ namespace DSRevitNodes
             var ca = new CurveArray();
             outline.ToList().ForEach(x => ca.Append(x.ToRevitType())); 
 
-            return new DSFloor(ca, level.InternalLevel );
+            return new DSFloor(ca, floorType.InternalFloorType, level.InternalLevel );
         }
     }
 }
-/*
-
-[NodeName("Floor By Outline")]
-[NodeCategory(BuiltinNodeCategories.REVIT_DOCUMENT)]
-[NodeDescription("WARNING!  Recreated, not modified on change.  Create a floor given a list of curves, a floor type and a level.")]
-public class FloorByOutlineLevelAndOffset : RevitTransactionNodeWithOneOutput
-{
-    public FloorByOutlineLevelAndOffset()
-    {
-        InPortData.Add(new PortData("curves", "A list of curves representing the edges of the floor.", typeof(Value.List)));
-        InPortData.Add(new PortData("floor type", "The floor type to use for floor creation.", typeof(Value.Container)));
-        InPortData.Add(new PortData("level", "A level to associate this floor with.", typeof(Value.Container)));
-
-        OutPortData.Add(new PortData("floor", "The floor.", typeof(Value.Container)));
-
-        RegisterAllPorts();
-
-    }
-
-    public override FScheme.Value Evaluate(Microsoft.FSharp.Collections.FSharpList<FScheme.Value> args)
-    {
-        //if we're in a family document, don't even try to add a floor
-        if (dynRevitSettings.Doc.Document.IsFamilyDocument)
-        {
-            throw new Exception("Floors can not be created in family documents.");
-        }
-
-        var edges = ((Value.List)args[0]).Item;
-        var floorType = (FloorType)((Value.Container)args[1]).Item;
-        var level = (Autodesk.Revit.DB.Level)((Value.Container)args[2]).Item;
-
-        Autodesk.Revit.DB.Floor floor = null;
-
-        //convert the edges to a curveArray
-        if (edges.Count() < 3)
-        {
-            throw new Exception("The edge list provided does not have an adequate number of edges to create a floor.");
-        }
-
-        if (this.Elements.Any())
-        {
-            if (dynUtils.TryGetElement(this.Elements[0], out floor))
-            {
-                //Delete the existing floor. Revit API does not allow update of floor sketch.
-                dynRevitSettings.Doc.Document.Delete(floor.Id);
-            }
-
-            floor = CreateFloor(edges, floorType, level);
-            this.Elements[0] = floor.Id;
-        }
-        else
-        {
-            floor = CreateFloor(edges, floorType, level);
-            Elements.Add(floor.Id);
-        }
-
-        return Value.NewContainer(floor);
-    }
-
-    private static Autodesk.Revit.DB.Floor CreateFloor(IEnumerable<Value> edges, FloorType floorType, Autodesk.Revit.DB.Level level)
-    {
-        var ca = new CurveArray();
-        edges.ToList().ForEach(x => ca.Append((Curve)((Value.Container)x).Item));
-        var floor = dynRevitSettings.Doc.Document.Create.NewFloor(ca, floorType, level, false);
-        return floor;
-    }
-}
-*/
