8ffaef37561197a1bfaa7ea63a9389eeb04abe33
Peter Boyer <peter.b.boyer@gmail.com>
2013-11-27 14:45:48 -0500

    Added DSReferencePlane

--- src/DSRevitNodes/Elements/DSLevel.cs
+++ src/DSRevitNodes/Elements/DSLevel.cs
@@ -15,3 +15,57 @@ namespace DSRevitNodes
         }
     }
 }
+
+/*
+[NodeName("Level")]
+[NodeCategory(BuiltinNodeCategories.REVIT_DATUMS)]
+[NodeDescription("Creates a level datum")]
+public class Level : RevitTransactionNodeWithOneOutput
+{
+    public Level()
+    {
+        InPortData.Add(new PortData("elevation", "The elevation of the level.", typeof(FScheme.Value.Number)));
+        InPortData.Add(new PortData("name", "The name of the level.", typeof(FScheme.Value.String)));
+
+        OutPortData.Add(new PortData("level", "The level.", typeof(FScheme.Value.Container)));
+
+        RegisterAllPorts();
+    }
+
+    public override FScheme.Value Evaluate(FSharpList<FScheme.Value> args)
+    {
+        //Level elements take in one double for the z elevation (height)f
+        var h = (double)((FScheme.Value.Number)args[0]).Item;
+        var name = ((FScheme.Value.String)args[1]).Item;
+
+        Autodesk.Revit.DB.Level lev;
+
+        if (this.Elements.Any())
+        {
+            if (dynUtils.TryGetElement(this.Elements[0], out lev))
+            {
+                lev.Elevation = h;
+                lev.Name = name;
+            }
+            else
+            {
+                lev = this.UIDocument.Document.IsFamilyDocument
+                    ? this.UIDocument.Document.FamilyCreate.NewLevel(h)
+                    : this.UIDocument.Document.Create.NewLevel(h);
+                lev.Name = name;
+                this.Elements[0] = lev.Id;
+            }
+        }
+        else
+        {
+            lev = this.UIDocument.Document.IsFamilyDocument
+                ? this.UIDocument.Document.FamilyCreate.NewLevel(h)
+                : this.UIDocument.Document.Create.NewLevel(h);
+            lev.Name = name;
+            this.Elements.Add(lev.Id);
+        }
+
+        return FScheme.Value.NewContainer(lev);
+    }
+}
+*/
--- src/DSRevitNodes/Elements/DSReferencePlane.cs
+++ src/DSRevitNodes/Elements/DSReferencePlane.cs
@@ -1,18 +1,145 @@
 ﻿using System;
-using Autodesk.DesignScript.Geometry;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using DSNodeServices;
+using DSRevitNodes.Elements;
+using DSRevitNodes.GeometryConversion;
+using RevitServices.Persistence;
+using RevitServices.Transactions;
+using Line = Autodesk.DesignScript.Geometry.Line;
+using Point = Autodesk.DesignScript.Geometry.Point;
 
 namespace DSRevitNodes
 {
-    class DSReferencePlane
+    [RegisterForTrace]
+    public class DSReferencePlane : AbstractElement
     {
-        static Autodesk.Revit.DB.ReferencePlane ByLine(Line line)
+
+        private Autodesk.Revit.DB.ReferencePlane InternalReferencePlane;
+
+        #region Private constructors
+
+        private DSReferencePlane(XYZ bubbleEnd, XYZ freeEnd)
         {
-            throw new NotImplementedException();
+            //Phase 1 - Check to see if the object exists and should be rebound
+            var oldEle =
+                ElementBinder.GetElementFromTrace<Autodesk.Revit.DB.ReferencePlane>(Document);
+
+            //There was an element, bind & mutate
+            if (oldEle != null)
+            {
+                InternalSetReferencePlane(oldEle);
+                if (InternalSetEndpoints(bubbleEnd, freeEnd))
+                {
+                    return;
+                }
+
+                // delete the old element, we couldn't update it for some reason
+                DocumentManager.GetInstance().DeleteElement(oldEle.Id);
+            }
+
+            //Phase 2- There was no existing element, create new
+            TransactionManager.GetInstance().EnsureInTransaction(Document);
+
+            Autodesk.Revit.DB.ReferencePlane refPlane;
+
+            if (Document.IsFamilyDocument)
+            {
+                refPlane = Document.FamilyCreate.NewReferencePlane(
+                    bubbleEnd,
+                    freeEnd,
+                    XYZ.BasisZ,
+                    Document.ActiveView);
+            }
+            else
+            {
+                refPlane = Document.Create.NewReferencePlane(
+                    bubbleEnd,
+                    freeEnd,
+                    XYZ.BasisZ,
+                    Document.ActiveView );
+            }
+
+            InternalSetReferencePlane(refPlane);
+
+            TransactionManager.GetInstance().TransactionTaskDone();
+
+            ElementBinder.SetElementForTrace(this.InternalElementId);
+
         }
 
-        static Autodesk.Revit.DB.ReferencePlane ByStartPtEndPt(Point start, Point end)
+        #endregion
+
+        #region Private mutators
+
+        private void InternalSetReferencePlane(Autodesk.Revit.DB.ReferencePlane rp)
         {
-            throw new NotImplementedException();
+            this.InternalReferencePlane = rp;
+            this.InternalElementId = rp.Id;
+            this.InternalUniqueId = rp.UniqueId;
         }
+
+        private bool InternalSetEndpoints(XYZ bubbleEnd, XYZ freeEnd)
+        {
+            TransactionManager.GetInstance().EnsureInTransaction(Document);
+
+            var refPlane = this.InternalReferencePlane;
+
+            XYZ oldBubbleEnd = refPlane.BubbleEnd;
+            XYZ oldFreeEnd = refPlane.FreeEnd;
+            XYZ midPointOld = 0.5 * (oldBubbleEnd + oldFreeEnd);
+
+            XYZ midPoint = 0.5 * (bubbleEnd + freeEnd);
+            XYZ moveVec = XYZ.BasisZ.DotProduct(midPoint - midPointOld) * XYZ.BasisZ;
+
+            // (sic) From Dynamo Legacy
+            var success = true;
+            try
+            {
+                ElementTransformUtils.MoveElement(Document, refPlane.Id, moveVec);
+                refPlane.BubbleEnd = bubbleEnd;
+                refPlane.FreeEnd = freeEnd;
+            }
+            catch
+            {
+                success = false;
+            }
+
+            TransactionManager.GetInstance().TransactionTaskDone();
+
+            return success;
+        }
+
+        #endregion
+
+        #region Public static constructors
+
+        public static DSReferencePlane ByLine(Autodesk.DesignScript.Geometry.Line line)
+        {
+            if (line == null)
+            {
+                throw new ArgumentNullException("line");
+            }
+
+            return new DSReferencePlane(line.StartPoint.ToXyz(), line.EndPoint.ToXyz());
+        }
+
+        public static DSReferencePlane ByStartPtEndPt(Point start, Point end)
+        {
+            if (start == null)
+            {
+                throw new ArgumentNullException("start");
+            }
+
+            if (end == null)
+            {
+                throw new ArgumentNullException("end");
+            }
+
+            return new DSReferencePlane(start.ToXyz(), end.ToXyz());
+        }
+
+        #endregion
+
     }
 }
