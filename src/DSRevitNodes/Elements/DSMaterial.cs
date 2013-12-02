41cca44cbe8d1572177f6b2ac2abe5cfff90e46f
Peter Boyer <peter.b.boyer@gmail.com>
2013-12-02 12:26:30 -0500

    Implemented DSMaterial

--- src/DSRevitNodes/Elements/DSMaterial.cs
+++ src/DSRevitNodes/Elements/DSMaterial.cs
@@ -1,49 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Deployment.Internal;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using DSNodeServices;
+using DSRevitNodes.Elements;
+using RevitServices.Persistence;
 
 namespace DSRevitNodes
 {
-    class DSMaterial
+    [RegisterForTrace]
+    public class DSMaterial : AbstractElement
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        internal Autodesk.Revit.DB.Material InternalMaterial
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// Private constructor for DSMaterial
+        /// </summary>
+        /// <param name="material"></param>
+        private DSMaterial(Autodesk.Revit.DB.Material material)
+        {
+            InternalSetMaterial(material);
+        }
+
+        /// <summary>
+        /// Set the internal Element, ELementId, and UniqueId
+        /// </summary>
+        /// <param name="material"></param>
+        private void InternalSetMaterial(Autodesk.Revit.DB.Material material)
+        {
+            this.InternalMaterial = material;
+            this.InternalElementId = material.Id;
+            this.InternalUniqueId = material.UniqueId;
+        }
+        
+        /// <summary>
+        /// Select a material from the current document by the name
+        /// </summary>
+        /// <param name="name">The name of the material</param>
+        /// <returns></returns>
+        public DSMaterial ByName(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            var mat = DocumentManager.GetInstance().ElementsOfType<Autodesk.Revit.DB.Material>()
+                .FirstOrDefault(x => x.Name == name);
+
+            if (mat == null)
+            {
+                throw new Exception("A Material with the given name does not exist in the current Document");
+            }
+
+            return new DSMaterial(mat)
+            {
+                IsRevitOwned = true
+            };
+        }
+
+        /// <summary>
+        /// Wrap an element in the associated DS type
+        /// </summary>
+        /// <param name="material">The material</param>
+        /// <returns></returns>
+        internal DSMaterial FromExisting(Autodesk.Revit.DB.Material material)
+        {
+            return new DSMaterial(material)
+            {
+                IsRevitOwned = true
+            };
+        }
 
     }
 }
-
-/*
- *  [NodeName("Get Material by Name")]
-    [NodeCategory(BuiltinNodeCategories.REVIT_DOCUMENT)]
-    [NodeDescription("Get a material from the active Revit document by name.")]
-    public class Material : NodeWithOneOutput
-    {
-        public Material()
-        {
-            InPortData.Add(new PortData("name", "Material name.", typeof(FScheme.Value.String)));
-            OutPortData.Add(new PortData("id", "The Element Id of the selected material.", typeof(FScheme.Value.Container)));
-
-            RegisterAllPorts();
-        }
-
-        public override FScheme.Value Evaluate(Microsoft.FSharp.Collections.FSharpList<FScheme.Value> args)
-        {
-            var matName = ((FScheme.Value.String) args[0]).Item;
-
-            var fec = new FilteredElementCollector(dynRevitSettings.Doc.Document);
-
-            Autodesk.Revit.DB.Material foundMat;
-
-            fec.OfClass(typeof (Autodesk.Revit.DB.Material));
-            try
-            {
-                foundMat = (Autodesk.Revit.DB.Material)fec.ToElements().First(x => x.Name == matName);
-            }
-            catch (InvalidOperationException)
-            {
-                throw new Exception("A material with that name could not be found.");
-            }
-
-            return FScheme.Value.NewContainer(foundMat.Id);
-        }
-    }
- * */
