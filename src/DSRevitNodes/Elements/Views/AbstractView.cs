fc8648edac5b322d9d0d59fc645212903e2354ba
Peter Boyer <peter.b.boyer@gmail.com>
2013-12-09 13:20:27 -0500

    Add DSSheet, add packing methods to DSRevitNodes

--- src/DSRevitNodes/Elements/Views/AbstractView.cs
+++ src/DSRevitNodes/Elements/Views/AbstractView.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
+using Autodesk.Revit.DB;
 
 namespace DSRevitNodes.Elements
 {
@@ -19,6 +22,30 @@ namespace DSRevitNodes.Elements
             {
                 return (Autodesk.Revit.DB.View) InternalElement;
             }
+        }
+
+        /// <summary>
+        /// Export the view as an image to the given path as a PNG
+        /// </summary>
+        /// <param name="path">A valid path for the image</param>
+        /// <returns>The image</returns>
+        public System.Drawing.Image ExportAsImage(string path)
+        {
+            var options = new ImageExportOptions
+            {
+                ExportRange = ExportRange.SetOfViews,
+                FilePath = path,
+                HLRandWFViewsFileType = ImageFileType.PNG,
+                ImageResolution = ImageResolution.DPI_72,
+                ZoomType = ZoomFitType.Zoom,
+                ShadowViewsFileType = ImageFileType.PNG
+            };
+
+            options.SetViewsAndSheets(new List<ElementId> { InternalView.Id });
+
+            Document.ExportImage(options);
+
+            return Image.FromFile(path + ".png");
         }
     }
 }
