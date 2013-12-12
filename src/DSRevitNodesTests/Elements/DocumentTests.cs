f1dabe58d79d3c02012464b8d7acc6196eec837a
Peter Boyer <peter.b.boyer@gmail.com>
2013-12-11 20:03:42 -0500

    PointAnalysisDisplayTests, VectorAnalysisDisplayTests

---
+++ src/DSRevitNodesTests/Elements/DocumentTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DSRevitNodes.Application;
+using DSRevitNodes.Elements;
+using NUnit.Framework;
+
+namespace DSRevitNodesTests.Elements
+{
+    [TestFixture]
+    public class DocumentTests
+    {
+
+        [Test]
+        public void Current()
+        {
+            var doc = DSDocument.Current;
+            Assert.NotNull(doc);
+            Assert.NotNull(doc.ActiveView);
+            Assert.IsTrue(doc.IsFamilyDocument);
+        }
+
+    }
+}
