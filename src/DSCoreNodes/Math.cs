b84fe2d51a5ef7ecd8475f8b6aa8f14d7494c9f5
Stephen Elliott <steell1890@gmail.com>
2013-12-17 13:09:18 -0500

    namespace and file location cleanup

--- src/DSCoreNodes/Math.cs
+++ src/DSCoreNodes/Math.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace DSCoreNodes.Core
+namespace DSCoreNodes
 {
     /// <summary>
     /// Methods for performing Mathematical operations.
