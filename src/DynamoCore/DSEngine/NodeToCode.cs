d20b2e422391ebb2b88f6e4d9dc2ebd4926c7966
Yu Ke <yu.ke@autodesk.com>
2013-12-18 16:27:13 +0800

    Not passing IAstContainer instance to AstBuilder for non delta execution

--- src/DynamoCore/DSEngine/NodeToCode.cs
+++ src/DynamoCore/DSEngine/NodeToCode.cs
@@ -9,35 +9,12 @@ using ProtoScript.Runners;
 
 namespace Dynamo.DSEngine
 {
-    public class ASTNodesToCode : IAstNodeContainer
-    {
-        public List<AssociativeNode> AstNodes { get; set; }
-
-        public ASTNodesToCode()
-        {
-            AstNodes = new List<AssociativeNode>();
-        }
-
-        public void OnAstNodeBuilding(Guid node)
-        {
-
-        }
-
-        public void OnAstNodeBuilt(Guid node, IEnumerable<AssociativeNode> astNodes)
-        {
-            AstNodes.AddRange(astNodes);
-        }
-    }
-
     public static class NodeToCodeUtils
     {
         public static string ConvertNodesToCode(IEnumerable<NodeModel> nodeList)
         {
-            var astnodesContainer = new ASTNodesToCode();
-            var astBuilder = new AstBuilder(astnodesContainer);
-            astBuilder.CompileToAstNodes(nodeList, false);
-
-            var astNodes = astnodesContainer.AstNodes;
+            var astBuilder = new AstBuilder(null);
+            var astNodes = astBuilder.CompileToAstNodes(nodeList, false);
 
             string code = GraphToDSCompiler.GraphUtilities.ASTListToCode(astNodes);
             return code;
