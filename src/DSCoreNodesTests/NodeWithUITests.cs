9e9dcbee21c280a2c9a4f3338ec0cf05d75e9c7c
Stephen Elliott <steell1890@gmail.com>
2014-01-01 12:55:24 -0500

    removed redundant NodeWithUI abstract class

--- src/DSCoreNodesTests/NodeWithUITests.cs
+++ src/DSCoreNodesTests/NodeWithUITests.cs
@@ -1,6 +1,4 @@
-﻿using DSCoreNodes;
-using NUnit.Framework;
-// NOTE: The following namespace has been updated. This test use to fail in unit 
+﻿// NOTE: The following namespace has been updated. This test use to fail in unit 
 // test, the reason is that LibGNet.dll could not be loaded. That was because the
 // right "AppDomain.CurrentDomain.AssemblyResolve" callback has not been set to 
 // resolve the library path when this test is run.
@@ -13,9 +11,13 @@ using NUnit.Framework;
 // while "NodeSampleTests" resides in "Dynamo.Nodes". So by relocating the 
 // "NodeSampleTests" to "Dynamo.Tests" helps NUnit locating the "Setup" class. 
 // 
+using System.Collections.Generic;
+using System.Linq;
+using DSCoreNodes;
+using NUnit.Framework;
 using ProtoCore.AST.AssociativeAST;
 
-namespace NodeWithUITests
+namespace DSCoreNodesTests
 {
     [TestFixture]
     class NodeWithUITests
@@ -24,10 +26,11 @@ namespace NodeWithUITests
         public void SliderASTGeneration()
         {
             var sliderNode = new NumberSlider { Value = 10 };
-            var buildOutput = sliderNode.BuildAst();
+            var buildOutput = sliderNode.BuildOutputAst(new List<AssociativeNode>());
 
-            Assert.IsInstanceOf<DoubleNode>(buildOutput);
-            Assert.AreEqual(new DoubleNode("10").value, (buildOutput as DoubleNode).value);
+            Assert.AreEqual(
+                new DoubleNode("10").value,
+                ((DoubleNode)((BinaryExpressionNode)buildOutput.First()).RightNode).value);
         }
     }
 }
