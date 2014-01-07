6f101f0cc0c7bb864a2be188bc12dfbb2b2859bd
Stephen Elliott <steell1890@gmail.com>
2014-01-07 14:18:38 -0500

    implemented If node
    this requires testing for laziness

---
+++ src/DSCoreNodesUI/Logic/If.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dynamo.Models;
+using ProtoCore.AST.AssociativeAST;
+
+namespace DSCoreNodesUI.Logic
+{
+    public class If : NodeModel
+    {
+        public If()
+        {
+            InPortData.Add(new PortData("test", "Test block"));
+            InPortData.Add(new PortData("true", "True block"));
+            InPortData.Add(new PortData("false", "False block"));
+
+            OutPortData.Add(new PortData("result", "Result"));
+
+            RegisterAllPorts();
+
+            //TODO: Default Values
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            return new[]
+            {
+                AstFactory.BuildAssignment(
+                    GetAstIdentifierForOutputIndex(0),
+                    AstFactory.BuildConditionalNode(
+                        inputAstNodes[0],
+                        inputAstNodes[1],
+                        inputAstNodes[2]))
+            };
+        }
+    }
+}
