4faf214e7cdb019f8cdd633bc6e8ee7af6b7da66
Ian Keough <ian@iankeough.com>
2014-01-15 15:45:52 -0800

    Add update manager assets from designscript.

---
+++ src/DynamoCore/UpdateManager/ILoggerWrapper.cs
@@ -0,0 +1,13 @@
+﻿namespace Dynamo.UpdateManager
+{
+    public interface ILoggerWrapper
+    {
+        void EnableLogging(bool enable);
+        void LogInfo(string tag, string data);
+        void LogDebug(string tag, string data);
+        void LogPerf(string tag, string data);
+        void LogError(string tag, string data);
+        void FORCE_Log(string tag, string data);
+        void CancelProcessing();
+    }
+}
