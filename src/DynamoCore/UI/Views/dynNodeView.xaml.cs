694e01479b6d6ef1c87e15bd5dbc2f913a8dbddf
Peter Boyer <peter.b.boyer@gmail.com>
2014-01-21 15:03:15 -0500

    Fixed issue where Custom Node Help window would come up multiple times

--- src/DynamoCore/UI/Views/dynNodeView.xaml.cs
+++ src/DynamoCore/UI/Views/dynNodeView.xaml.cs
@@ -116,10 +116,9 @@ namespace Dynamo.Controls
             // ViewModel to be valid earlier (e.g. OnSizeChanged is called before
             // OnNodeViewLoaded, and it needs ViewModel for size computation).
             // 
-            // ViewModel = this.DataContext as NodeViewModel;
-
+            // ViewModel = this.DataContext as NodeViewModel
             ViewModel.NodeLogic.DispatchedToUI += new DispatchedToUIThreadHandler(NodeLogic_DispatchedToUI);
-            ViewModel.RequestShowNodeHelp += new NodeViewModel.NodeHelpEventHandler(ViewModel_RequestShowNodeHelp);
+            ViewModel.RequestShowNodeHelp += ViewModel_RequestShowNodeHelp;
             ViewModel.RequestShowNodeRename += new EventHandler(ViewModel_RequestShowNodeRename);
             ViewModel.RequestsSelection += new EventHandler(ViewModel_RequestsSelection);
 
@@ -179,6 +178,10 @@ namespace Dynamo.Controls
 
         void ViewModel_RequestShowNodeHelp(object sender, NodeHelpEventArgs e)
         {
+            if (e.Handled) return;
+
+            e.Handled = true;
+
             var helpDialog = new NodeHelpPrompt(e.Model);
             helpDialog.Show();
         }
