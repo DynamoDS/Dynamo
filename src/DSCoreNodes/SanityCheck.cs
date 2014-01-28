740e65a0fb24f1e0606288752ad78c2985772428
Ian Keough <ian@iankeough.com>
2014-01-28 15:40:00 -0800

    Add sanity check method to test function calls.

---
+++ src/DSCoreNodes/SanityCheck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSCoreNodes
+{
+    public class SanityCheck
+    {
+        public static double ANumber()
+        {
+            return 42.0;
+        }
+    }
+}
