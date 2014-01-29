dbfde6ad260eca07f76440771b851ba47bfde5c3
Stephen Elliott <steell1890@gmail.com>
2014-01-29 14:41:18 -0500

    Implemented boolean XOR

---
+++ src/DSCoreNodes/Logic.cs
@@ -0,0 +1,19 @@
+﻿namespace DSCore
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Logic
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool Xor(bool a, bool b)
+        {
+            return a ^ b;
+        }
+    }
+}
