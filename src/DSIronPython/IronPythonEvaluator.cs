4db878e7389137dadca0858e4be5f6e0372dae1b
Stephen Elliott <steell1890@gmail.com>
2014-01-31 16:00:40 -0500

    added test script node for debugging purposes

--- src/DSIronPython/IronPythonEvaluator.cs
+++ src/DSIronPython/IronPythonEvaluator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,10 @@ namespace DSIronPython
     /// </summary>
     public class IronPythonEvaluator
     {
+        private IronPythonEvaluator() { }
+
+        public static string TestCode { get { return "OUT = 0"; } }
+
         /// <summary>
         /// 
         /// </summary>
