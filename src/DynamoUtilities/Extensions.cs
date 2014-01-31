ee618489e7dfc778186520342cefcd436636f29b
Stephen Elliott <steell1890@gmail.com>
2014-01-31 15:57:42 -0500

    more build fixes

--- src/DynamoUtilities/Extensions.cs
+++ src/DynamoUtilities/Extensions.cs
@@ -7,12 +7,12 @@ namespace DynamoUtilities
 {
     public static class Extensions
     {
-        public static string GetFullName(this Delegate del)
-        {
-            if (del.Method.DeclaringType == null)
-                throw new ArgumentException("Delegate has no declaring type.", @"del");
+        //public static string GetFullName(this Delegate del)
+        //{
+        //    if (del.Method.DeclaringType == null)
+        //        throw new ArgumentException("Delegate has no declaring type.", @"del");
 
-            return string.Format("{0}.{1}", del.Method.DeclaringType.FullName, del.Method.Name);
-        }
+        //    return string.Format("{0}.{1}", del.Method.DeclaringType.FullName, del.Method.Name);
+        //}
     }
 }
