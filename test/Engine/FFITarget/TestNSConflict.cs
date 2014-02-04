fc2c89b111536638095e6011be522a7bc8ec1408
Luke Church <luke@church.name>
2014-02-04 05:38:17 -0500

    Refactoring test suites

---
+++ test/Engine/FFITarget/TestNSConflict.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSNodeTarget
+{
+    namespace A
+    {
+
+
+
+        public class DupTargetTest
+        {
+            public int Foo()
+            {
+                return 1;
+            }
+
+        }
+    }
+
+
+    namespace B
+    {
+
+
+
+        public class DupTargetTest
+        {
+            public int Foo()
+            {
+                return 2;
+            }
+
+        }
+    }
+
+
+
+}
