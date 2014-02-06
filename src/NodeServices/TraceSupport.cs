38569fea37af1e0459c5f4e6f7156642831db9cb
Luke Church <luke@church.name>
2014-02-06 06:50:41 -0500

    Trace test commit
    Test current fails as the call site objects are not stable between multiple executions

--- src/NodeServices/TraceSupport.cs
+++ src/NodeServices/TraceSupport.cs
@@ -37,8 +37,7 @@ namespace DSNodeServices
         /// <param name="value"></param>
         public static void SetTraceData(string key, ISerializable value)
         {
-            // PB: do not store data in TLS until the VM is able to handle this
-            // Thread.SetData(Thread.GetNamedDataSlot(key), value);
+            Thread.SetData(Thread.GetNamedDataSlot(key), value);
         }
     }
 }
