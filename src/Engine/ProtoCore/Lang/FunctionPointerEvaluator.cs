8498b99c199262541811579f36890d47014dd80a
Yu Ke <yu.ke@autodesk.com>
2014-02-07 10:56:44 +0800

    Fix FunctionPointerEvaluator for static member function and constructor

--- src/Engine/ProtoCore/Lang/FunctionPointerEvaluator.cs
+++ src/Engine/ProtoCore/Lang/FunctionPointerEvaluator.cs
@@ -21,15 +21,18 @@ namespace ProtoCore.Lang
             Core core = dsi.runtime.Core;
 
             int fptr = (int)pointer.opdata;
-            ProtoCore.DSASM.FunctionPointerNode fptrNode;
+            FunctionPointerNode fptrNode;
+            int classScope = Constants.kGlobalScope;
+
             if (core.FunctionPointerTable.functionPointerDictionary.TryGetByFirst(fptr, out fptrNode))
             {
                 int blockId = fptrNode.blockId;
                 int procId = fptrNode.procId;
-                mProcNode = dsi.runtime.exe.procedureTable[blockId].procList[procId];
+                classScope = fptrNode.classScope;
+                mProcNode = dsi.runtime.GetProcedureNode(blockId, classScope, procId);
             }
 
-            mCallSite = new ProtoCore.CallSite(ProtoCore.DSASM.Constants.kGlobalScope, Name, core.FunctionTable, core.Options.ExecutionMode);
+            mCallSite = new ProtoCore.CallSite(classScope, Name, core.FunctionTable, core.Options.ExecutionMode);
         }
 
         public StackValue Evaluate(List<StackValue> args, StackFrame stackFrame)
