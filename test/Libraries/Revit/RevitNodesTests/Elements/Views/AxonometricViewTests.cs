80f98e6ffc76b386bd8c84456317b2b4beba16b4
Ben Goh <benglin@outlook.com>
2014-02-11 15:34:38 +0800

    Added static constructor "AxonometricView.ByEyePointAndTarget"

--- test/Libraries/Revit/RevitNodesTests/Elements/Views/AxonometricViewTests.cs
+++ test/Libraries/Revit/RevitNodesTests/Elements/Views/AxonometricViewTests.cs
@@ -16,6 +16,55 @@ namespace DSRevitNodesTests
     class AxonometricViewTests
     {
         [Test]
+        public void ByEyePointAndTarget_ValidBoundingBox()
+        {
+            var eye = Point.ByCoordinates(100, 100, 100);
+            var target = Point.ByCoordinates(0, 1, 2);
+            var name = "treeView";
+
+            var famSym = FamilySymbol.ByName("Kousa Dogwood - 10'");
+            var pt = Point.ByCoordinates(0, 1, 2);
+            var famInst = FamilyInstance.ByPoint(famSym, pt);
+
+            object element = famInst.BoundingBox;
+            var view = AxonometricView.ByEyePointAndTarget(eye, target, element, name, false);
+
+            Assert.NotNull(view);
+            Assert.IsTrue(DocumentManager.GetInstance().ElementExistsInDocument(view.InternalElement.Id));
+        }
+
+        [Test]
+        public void ByEyePointAndTarget_ValidAbstractElement()
+        {
+            var eye = Point.ByCoordinates(100, 100, 100);
+            var target = Point.ByCoordinates(0, 1, 2);
+            var name = "treeView";
+
+            var famSym = FamilySymbol.ByName("Kousa Dogwood - 10'");
+            var pt = Point.ByCoordinates(0, 1, 2);
+            var famInst = FamilyInstance.ByPoint(famSym, pt);
+
+            object element = famInst;
+            var view = AxonometricView.ByEyePointAndTarget(eye, target, element, name, false);
+
+            Assert.NotNull(view);
+            Assert.IsTrue(DocumentManager.GetInstance().ElementExistsInDocument(view.InternalElement.Id));
+        }
+
+        [Test]
+        public void ByEyePointAndTarget_BadArgs0()
+        {
+            var eye = Point.ByCoordinates(100, 100, 100);
+            var target = Point.ByCoordinates(0, 1, 2);
+            var name = "treeView";
+
+            Assert.Throws(typeof(ArgumentNullException), () =>
+            {
+                AxonometricView.ByEyePointAndTarget(eye, target, null, name, false);
+            });
+        }
+
+        [Test]
         public void ByEyePointTargetAndBoundingBox_ValidArgs()
         {
             var eye = Point.ByCoordinates(100, 100, 100);
