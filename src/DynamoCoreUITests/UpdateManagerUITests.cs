9005de28d6599160d0dcc712c660e4f157ae8459
Ian Keough <ian@iankeough.com>
2014-02-10 22:19:52 -0800

    Refactor UpdateManagerUITests to use testing flag and Init method.

--- src/DynamoCoreUITests/UpdateManagerUITests.cs
+++ src/DynamoCoreUITests/UpdateManagerUITests.cs
@@ -20,20 +20,18 @@ namespace DynamoCoreUITests
     [TestFixture]
     public class UpdateManagerNotUpToDateTests : DynamoTestUI
     {
-        [SetUp]
-        public void Start()
+        private void Init(IUpdateManager updateManager)
         {
             AppDomain.CurrentDomain.AssemblyResolve += AssemblyHelper.CurrentDomain_AssemblyResolve;
 
-            var um_mock = new Mock<IUpdateManager>();
-            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
-            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("1.1.1.1"));
-
             var env = new ExecutionEnvironment();
-            Controller = new DynamoController(env, typeof(DynamoViewModel), "None", null, um_mock.Object);
+            Controller = new DynamoController(env, typeof(DynamoViewModel), "None", null, updateManager)
+            {
+                Testing = true
+            };
 
             //create the view
-            Ui = new DynamoView {DataContext = Controller.DynamoViewModel};
+            Ui = new DynamoView { DataContext = Controller.DynamoViewModel };
             Vm = Controller.DynamoViewModel;
             Controller.UIDispatcher = Ui.Dispatcher;
             Ui.Show();
@@ -54,7 +52,7 @@ namespace DynamoCoreUITests
         }
 
         [TearDown]
-        public void Exit()
+        public void Shutdown()
         {
             if (Ui.IsLoaded)
                 Ui.Close();
@@ -63,112 +61,42 @@ namespace DynamoCoreUITests
         [Test]
         public void UpdateButtonNotCollapsedIfNotUpToDate()
         {
+            var um_mock = new Mock<IUpdateManager>();
+            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
+            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("1.1.1.1"));
+
+            Init(um_mock.Object);
+
             var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
             var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
             var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
             Assert.AreEqual(Visibility.Visible, updateControl.Visibility);
         }
-    }
-
-    [TestFixture]
-    public class UpdateManagerUpToDateTests : DynamoTestUI
-    {
-        [SetUp]
-        public void Start()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += AssemblyHelper.CurrentDomain_AssemblyResolve;
-
-            var um_mock = new Mock<IUpdateManager>();
-            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString("1.1.1.1"));
-            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
-
-            var env = new ExecutionEnvironment();
-            Controller = new DynamoController(env, typeof(DynamoViewModel), "None", null, um_mock.Object);
-
-            //create the view
-            Ui = new DynamoView {DataContext = Controller.DynamoViewModel};
-            Vm = Controller.DynamoViewModel;
-            Controller.UIDispatcher = Ui.Dispatcher;
-            Ui.Show();
-
-            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
-
-            string tempPath = Path.GetTempPath();
-            TempFolder = Path.Combine(tempPath, "dynamoTmp");
-
-            if (!Directory.Exists(TempFolder))
-            {
-                Directory.CreateDirectory(TempFolder);
-            }
-            else
-            {
-                EmptyTempFolder();
-            }
-        }
-
-        [TearDown]
-        public void Exit()
-        {
-            if (Ui.IsLoaded)
-                Ui.Close();
-        }
 
         [Test]
         public void UpdateButtonCollapsedIfUpToDate()
         {
+            var um_mock = new Mock<IUpdateManager>();
+            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString("1.1.1.1"));
+            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
+
+            Init(um_mock.Object);
+
             var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
             var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
             var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
             Assert.AreEqual(Visibility.Collapsed, updateControl.Visibility);
         }
-    }
-
-    [TestFixture]
-    public class UpdateManagerDisconnectedTests : DynamoTestUI
-    {
-        [SetUp]
-        public void Start()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += AssemblyHelper.CurrentDomain_AssemblyResolve;
-
-            var um_mock = new Mock<IUpdateManager>();
-            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString(""));
-            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
-
-            var env = new ExecutionEnvironment();
-            Controller = new DynamoController(env, typeof(DynamoViewModel), "None", null, um_mock.Object);
-
-            //create the view
-            Ui = new DynamoView {DataContext = Controller.DynamoViewModel};
-            Vm = Controller.DynamoViewModel;
-            Controller.UIDispatcher = Ui.Dispatcher;
-            Ui.Show();
-
-            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
-
-            string tempPath = Path.GetTempPath();
-            TempFolder = Path.Combine(tempPath, "dynamoTmp");
-
-            if (!Directory.Exists(TempFolder))
-            {
-                Directory.CreateDirectory(TempFolder);
-            }
-            else
-            {
-                EmptyTempFolder();
-            }
-        }
-
-        [TearDown]
-        public void Exit()
-        {
-            if (Ui.IsLoaded)
-                Ui.Close();
-        }
 
         [Test]
         public void UpdateButtonCollapsedIfNotConnected()
         {
+            var um_mock = new Mock<IUpdateManager>();
+            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString(""));
+            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
+
+            Init(um_mock.Object);
+
             var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
             var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
             var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
