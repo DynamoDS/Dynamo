a8e39eea444ef733ac459f36154e1dfd0ea4a6f0
Ian Keough <ian@iankeough.com>
2014-02-17 22:12:29 -0800

    Update DTF batch files to take 2 command line arguments.

--- test/DynamoTestFramework/RunFixtureByName.bat
+++ test/DynamoTestFramework/RunFixtureByName.bat
@@ -1,4 +1,6 @@
-set arg=%1
-set arg=%arg:"=%
-start /wait python ..\..\src\DynamoTestFramework\RunRevitTests.py -i DynamoRevitTests.xml -r %arg%Results.xml -f %arg%
-start %arg%Results.xml
+set arg1=%1
+set arg2=%2
+set arg1=%arg1:"=%
+set arg2=%arg2:"=%
+start /wait python ..\..\DynamoTestFramework\RunRevitTests.py -i %arg1% -r %arg2%Results.xml -f %arg2%
+start %arg2%Results.xml
--- test/DynamoTestFramework/RunSingleTestByNameDS.bat
+++
@@ -1,4 +0,0 @@
-set arg=%1
-set arg=%arg:"=%
-start /wait python ..\..\src\DynamoTestFramework\RunRevitTests.py -i DSRevitNodesTests.xml -r %arg%Result.xml -n %arg%
-start %arg%Result.xml
