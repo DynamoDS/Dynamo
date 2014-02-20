0f3e60652ef2b635c239f676bb9d39c98a5f3291
Ian Keough <ian@iankeough.com>
2014-02-20 15:07:37 -0800

    Make numbers and watch update immediately when number format is changed.

--- src/DynamoCore/Nodes/dynBaseTypes.cs
+++ src/DynamoCore/Nodes/dynBaseTypes.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
