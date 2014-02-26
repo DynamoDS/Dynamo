76930a519a12d0a0e48b44fbbb967da814c93a15
Luke Church <luke@church.name>
2014-02-26 19:25:17 +0800

    Dispose dispatcher not collecting from LR
    Test to show correct language behaviour

---
+++ test/Engine/FFITarget/AbstractDisposeTracert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FFITarget
+{
+    public abstract class AbstractDisposeTracert : IDisposable
+    {
+        static public int DisposeCount { get; set; }
+
+        public virtual void Dispose()
+        {
+            DisposeCount++;
+        }
+    }
+
+
+
+    public class AbstractDerivedDisposeTracer2 : AbstractDisposeTracert
+        {
+        public AbstractDerivedDisposeTracer2()
+        {
+                
+        }
+
+
+        }
+
+}
