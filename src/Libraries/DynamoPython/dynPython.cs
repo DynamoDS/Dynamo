51feaa13fdba75390a25d1c073bde74cb3e5a549
ellensi <ellensi.chandra@autodesk.com>
2014-02-28 17:48:08 +0800

    Added dummy migration function

--- src/Libraries/DynamoPython/dynPython.cs
+++ src/Libraries/DynamoPython/dynPython.cs
@@ -222,6 +222,18 @@ namespace Dynamo.Nodes
         }
 
         #endregion
+
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            NodeMigrationData migrationData = new NodeMigrationData(data.Document);
+
+            XmlElement oldNode = data.MigratedNodes.ElementAt(0);
+            XmlElement dummyNode = MigrationManager.CreateDummyNode(oldNode, 1, 1);
+            migrationData.AppendNode(dummyNode);
+
+            return migrationData;
+        }
     }
 
     [NodeName("LEGACY Python Script With Variable Number of Inputs")]
