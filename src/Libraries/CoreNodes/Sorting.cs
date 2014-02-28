3628f8ffb4cc31ed7e0a6d62cf03b0b9e68d8140
Yu Ke <yu.ke@autodesk.com>
2014-02-28 16:53:38 +0800

    Checking a class is static based on isSealed and isAbstract property

--- src/Libraries/CoreNodes/Sorting.cs
+++ src/Libraries/CoreNodes/Sorting.cs
@@ -10,7 +10,7 @@ namespace DSCore
     /// Utility methods for sorting by keys. These should be suppressed from becoming nodes, instead
     /// they will be wrapped by DS implementations that accept a key mapping function.
     /// </summary>
-    public class Sorting
+    public static class Sorting
     {
         public static object minByKey(IList list, IList keys)
         {
