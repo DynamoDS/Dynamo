432a247c94f570a0a3d87a7aef8bb3ad15b22772
Peter Boyer <peter.b.boyer@gmail.com>
2014-03-03 09:15:19 -0500

    Require curveReference input for AdaptiveComponent.ByPointsOnCurve, add base class for Revit tests requiring ProtoGeometry, fix AdaptiveComponent.ByPointsOnCurve test

---
+++ test/Libraries/Revit/RevitNodesTests/ProtoGeometryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.DesignScript.Geometry;
+using NUnit.Framework;
+
+namespace DSRevitNodesTests
+{
+    [TestFixture]
+    class ProtoGeometryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            HostFactory.Instance.StartUp();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            HostFactory.Instance.ShutDown();
+        }
+
+    }
+}
