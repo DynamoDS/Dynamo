9814a422fea64fd9793fffccf05de6681b4347a8
Ian Keough <ian@iankeough.com>
2014-03-06 14:54:10 -0800

    Add setup method on UpdateManagerUITests.

--- test/DynamoCoreUITests/UpdateManagerUITests.cs
+++ test/DynamoCoreUITests/UpdateManagerUITests.cs
@@ -20,84 +20,90 @@ namespace DynamoCoreUITests
     [TestFixture]
     public class UpdateManagerUITests : DynamoTestUI
     {
-        //private void Init(IUpdateManager updateManager)
-        //{
-        //    AppDomain.CurrentDomain.AssemblyResolve += AssemblyHelper.CurrentDomain_AssemblyResolve;
+        private void Init(IUpdateManager updateManager)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += AssemblyHelper.CurrentDomain_AssemblyResolve;
 
-        //    Controller = new DynamoController(typeof(DynamoViewModel), "None", null, updateManager, new DefaultWatchHandler(), new PreferenceSettings());
-        //    DynamoController.IsTestMode = true;
+            Controller = new DynamoController(typeof(DynamoViewModel), "None", null, updateManager, new DefaultWatchHandler(), new PreferenceSettings());
+            DynamoController.IsTestMode = true;
 
-        //    //create the view
-        //    Ui = new DynamoView { DataContext = Controller.DynamoViewModel };
-        //    Vm = Controller.DynamoViewModel;
-        //    Controller.UIDispatcher = Ui.Dispatcher;
-        //    Ui.Show();
+            //create the view
+            Ui = new DynamoView { DataContext = Controller.DynamoViewModel };
+            Vm = Controller.DynamoViewModel;
+            Controller.UIDispatcher = Ui.Dispatcher;
+            Ui.Show();
 
-        //    SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
 
-        //    string tempPath = Path.GetTempPath();
-        //    TempFolder = Path.Combine(tempPath, "dynamoTmp");
+            string tempPath = Path.GetTempPath();
+            TempFolder = Path.Combine(tempPath, "dynamoTmp");
 
-        //    if (!Directory.Exists(TempFolder))
-        //    {
-        //        Directory.CreateDirectory(TempFolder);
-        //    }
-        //    else
-        //    {
-        //        EmptyTempFolder();
-        //    }
-        //}
+            if (!Directory.Exists(TempFolder))
+            {
+                Directory.CreateDirectory(TempFolder);
+            }
+            else
+            {
+                EmptyTempFolder();
+            }
+        }
 
-        //[TearDown]
-        //public void Shutdown()
-        //{
-        //    if (Ui.IsLoaded)
-        //        Ui.Close();
-        //}
+        [SetUp]
+        public void Setup()
+        {
+            //Do nothing.
+        }
 
-        //[Test]
-        //public void UpdateButtonNotCollapsedIfNotUpToDate()
-        //{
-        //    var um_mock = new Mock<IUpdateManager>();
-        //    um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
-        //    um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("1.1.1.1"));
+        [TearDown]
+        public void Shutdown()
+        {
+            if (Ui.IsLoaded)
+                Ui.Close();
+        }
 
-        //    Init(um_mock.Object);
+        [Test]
+        public void UpdateButtonNotCollapsedIfNotUpToDate()
+        {
+            var um_mock = new Mock<IUpdateManager>();
+            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
+            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("1.1.1.1"));
 
-        //    var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
-        //    var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
-        //    var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
-        //    Assert.AreEqual(Visibility.Visible, updateControl.Visibility);
-        //}
+            Init(um_mock.Object);
 
-        //[Test]
-        //public void UpdateButtonCollapsedIfUpToDate()
-        //{
-        //    var um_mock = new Mock<IUpdateManager>();
-        //    um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString("1.1.1.1"));
-        //    um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
+            var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
+            var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
+            var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
+            Assert.AreEqual(Visibility.Visible, updateControl.Visibility);
+        }
 
-        //    Init(um_mock.Object);
+        [Test]
+        public void UpdateButtonCollapsedIfUpToDate()
+        {
+            var um_mock = new Mock<IUpdateManager>();
+            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString("1.1.1.1"));
+            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
 
-        //    var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
-        //    var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
-        //    var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
-        //    Assert.AreEqual(Visibility.Collapsed, updateControl.Visibility);
-        //}
+            Init(um_mock.Object);
 
-        //[Test]
-        //public void UpdateButtonCollapsedIfNotConnected()
-        //{
-        //    var um_mock = new Mock<IUpdateManager>();
-        //    um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString(""));
-        //    um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
+            var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
+            var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
+            var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
+            Assert.AreEqual(Visibility.Collapsed, updateControl.Visibility);
+        }
 
-        //    Init(um_mock.Object);
+        [Test]
+        public void UpdateButtonCollapsedIfNotConnected()
+        {
+            var um_mock = new Mock<IUpdateManager>();
+            um_mock.Setup(um => um.AvailableVersion).Returns(BinaryVersion.FromString(""));
+            um_mock.Setup(um => um.ProductVersion).Returns(BinaryVersion.FromString("9.9.9.9"));
 
-        //    var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
-        //    var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
-        //    var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
-        //    Assert.AreEqual(Visibility.Collapsed, updateControl.Visibility);
-        //}
+            Init(um_mock.Object);
+
+            var stb = (ShortcutToolbar)Ui.shortcutBarGrid.Children[0];
+            var sbgrid = (Grid)stb.FindName("ShortcutToolbarGrid");
+            var updateControl = (GraphUpdateNotificationControl)sbgrid.FindName("UpdateControl");
+            Assert.AreEqual(Visibility.Collapsed, updateControl.Visibility);
+        }
     }
 }
