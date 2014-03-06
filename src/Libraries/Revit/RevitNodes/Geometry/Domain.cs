a4522fe8007946a575e635706bbfc423daabd043
Peter Boyer <peter.b.boyer@gmail.com>
2014-03-06 11:20:02 -0500

    Provide ability to disable ElementBinder, fix WallTests

--- src/Libraries/Revit/RevitNodes/Geometry/Domain.cs
+++ src/Libraries/Revit/RevitNodes/Geometry/Domain.cs
@@ -113,31 +113,3 @@ namespace Revit.Geometry
         }
     }
 }
-
-/* Moved old Domain node to avoid circular assembly reference
-namespace Dynamo.Nodes
-{
-    [NodeName("Domain")]
-    [NodeCategory(BuiltinNodeCategories.GEOMETRY_SURFACE_UV)]
-    [NodeDescription("Create a domain specifying the Minimum and Maximum UVs.")]
-    public class Domain : NodeWithOneOutput
-    {
-        public Domain()
-        {
-            InPortData.Add(new PortData("min", "The minimum of the domain.", typeof(FScheme.Value.Number)));
-            InPortData.Add(new PortData("max", "The maximum of the domain.", typeof(FScheme.Value.Number)));
-            OutPortData.Add(new PortData("domain", "A domain.", typeof(FScheme.Value.Container)));
-
-            RegisterAllPorts();
-        }
-
-        public override FScheme.Value Evaluate(FSharpList<FScheme.Value> args)
-        {
-            var min = ((FScheme.Value.Number)args[0]).Item;
-            var max = ((FScheme.Value.Number)args[1]).Item;
-
-            return FScheme.Value.NewContainer(DSCoreNodes.Domain.ByMinimumAndMaximum(min, max));
-        }
-    }
-}
-*/
