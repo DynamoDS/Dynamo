4d80d8c8d5f5d4896c89161c2394bddfab1b38c9
Ian Keough <ian@iankeough.com>
2014-03-07 10:41:46 -0800

    Add exception handling for CurveByPoints and add test for the same.

---
+++ test/Libraries/Revit/RevitNodesTests/Elements/CurveByPointsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Revit.Elements;
+using RevitServices.Persistence;
+
+namespace DSRevitNodesTests.Elements
+{
+    [TestFixture]
+    public class CurveByPointsTests
+    {
+        [Test]
+        public void ByReferencePoints_ValidArgs()
+        {
+            ElementBinder.IsEnabled = false;
+
+            var p1 = ReferencePoint.ByCoordinates(0, 0, 0);
+            var p2 = ReferencePoint.ByCoordinates(1, 1, 1);
+            var p3 = ReferencePoint.ByCoordinates(2, 2, 2);
+
+            Assert.NotNull(p1);
+            Assert.NotNull(p2);
+            Assert.NotNull(p3);
+
+            var curveByPoints = CurveByPoints.ByReferencePoints(new List<ReferencePoint>{p1,p2,p3});
+            Assert.NotNull(curveByPoints);
+
+            var curveRef = curveByPoints.CurveReference;
+            Assert.NotNull(curveRef);
+        }
+    }
+}
