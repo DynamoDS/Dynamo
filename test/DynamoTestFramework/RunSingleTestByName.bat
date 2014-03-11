fe7516105f1f878d28af182b10ded1aa2d4addb3
Ian Keough <ian@iankeough.com>
2014-03-11 11:05:52 -0700

    Add debug flag to test framework.

--- test/DynamoTestFramework/RunSingleTestByName.bat
+++ test/DynamoTestFramework/RunSingleTestByName.bat
@@ -3,5 +3,5 @@ set arg2=%2
 set arg1=%arg1:"=%
 set arg2=%arg2:"=%
 
-start /wait python ..\..\DynamoTestFramework\RunRevitTests.py -i %arg1% -r %arg2%Result.xml -n %arg2%
+start /wait python ..\..\DynamoTestFramework\RunRevitTests.py -i %arg1% -r %arg2%Result.xml -n %arg2% -d true
 start %arg2%Result.xml
