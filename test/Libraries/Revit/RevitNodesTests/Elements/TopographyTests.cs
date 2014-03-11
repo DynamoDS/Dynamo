0430a07aadbd9065a33d00c628894f5d1bbe8830
Ian Keough <ian@iankeough.com>
2014-03-10 23:53:44 -0700

    Force regenerate of document to get topo points. Turn on Dynamo for tests.

--- test/Libraries/Revit/RevitNodesTests/Elements/TopographyTests.cs
+++ test/Libraries/Revit/RevitNodesTests/Elements/TopographyTests.cs
@@ -2,7 +2,6 @@
 using Autodesk.DesignScript.Geometry;
 using NUnit.Framework;
 using Revit.Elements;
-using RevitServices.Persistence;
 
 namespace DSRevitNodesTests.Elements
 {
@@ -11,9 +10,7 @@ namespace DSRevitNodesTests.Elements
     {
         [Test]
         public void ByPoints_ValidArgs()
-        {
-            //ElementBinder.IsEnabled = false;
-
+        {   
             var p1 = Point.ByCoordinates(0, 0, 0);
             var p2 = Point.ByCoordinates(1, 3, 4);
             var p3 = Point.ByCoordinates(27, 5, 12);
@@ -27,17 +24,23 @@ namespace DSRevitNodesTests.Elements
             var topoSurf = Topography.ByPoints(new List<Point> { p1,p2,p3,p4,p5 });
             Assert.NotNull(topoSurf);
 
-            //p1.ShouldBeApproximately(topoSurf.Points[0]);
-            //p2.ShouldBeApproximately(topoSurf.Points[1]);
-            //p3.ShouldBeApproximately(topoSurf.Points[2]);
-            //p4.ShouldBeApproximately(topoSurf.Points[3]);
-            //p5.ShouldBeApproximately(topoSurf.Points[4]);
+            topoSurf.Points[0].ShouldBeApproximately(p1);
+            topoSurf.Points[1].ShouldBeApproximately(p2);
+            topoSurf.Points[2].ShouldBeApproximately(p3);
+            topoSurf.Points[3].ShouldBeApproximately(p4);
+            topoSurf.Points[4].ShouldBeApproximately(p5);
         }
 
         [Test]
         public void ByPoints_InvalidArgs()
         {
+            var p1 = Point.ByCoordinates(0, 0, 0);
+            var p2 = Point.ByCoordinates(1, 3, 4);
 
+            Assert.NotNull(p1);
+            Assert.NotNull(p2);
+
+            Assert.Throws<System.Exception>(()=>Topography.ByPoints(new List<Point> { p1, p2 }));
         }
     }
 }
