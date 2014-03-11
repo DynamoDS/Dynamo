2ae30d17ff22dc795527f063d03fc3f64bab6dc5
Ian Keough <ian@iankeough.com>
2014-03-11 00:09:14 -0700

    Add mutation for topo surface.

--- test/Libraries/Revit/RevitNodesTests/Elements/TopographyTests.cs
+++ test/Libraries/Revit/RevitNodesTests/Elements/TopographyTests.cs
@@ -42,5 +42,28 @@ namespace DSRevitNodesTests.Elements
 
             Assert.Throws<System.Exception>(()=>Topography.ByPoints(new List<Point> { p1, p2 }));
         }
+
+        [Test]
+        public void ByPoints_Mutation()
+        {
+            var p1 = Point.ByCoordinates(0, 0, 0);
+            var p2 = Point.ByCoordinates(1, 3, 4);
+            var p3 = Point.ByCoordinates(27, 5, 12);
+            var p4 = Point.ByCoordinates(-10, 8, -3);
+            var p5 = Point.ByCoordinates(.005, 0.01, -10);
+
+            Assert.NotNull(p1);
+            Assert.NotNull(p2);
+            Assert.NotNull(p3);
+
+            var topoSurf = Topography.ByPoints(new List<Point> { p1, p2, p3, p4, p5 });
+            Assert.NotNull(topoSurf);
+
+            var p6 = Point.ByCoordinates(10, 42, -3.3);
+            topoSurf = Topography.ByPoints(new List<Point> {p1, p2, p3, p4, p6});
+            Assert.NotNull(topoSurf);
+
+            topoSurf.Points[4].ShouldBeApproximately(p6);
+        }
     }
 }
