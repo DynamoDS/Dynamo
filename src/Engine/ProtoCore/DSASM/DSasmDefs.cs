e12d9aa14cae2f23bb65ac2deac2c17900b01dff
Jun Mendoza <jun.mendoza@autodesk.com>
2014-03-14 14:32:17 +0800

    Split class heirarchy traversal in 2 passes
    1. Traverse the immediate base class in a first pass
    2. Traverse all the base classes in the the class hierarchy in a second
    pass
    3. Test cases for 4-level class hierarchy

--- src/Engine/ProtoCore/DSASM/DSasmDefs.cs
+++ src/Engine/ProtoCore/DSASM/DSasmDefs.cs
@@ -544,7 +544,8 @@ namespace ProtoCore.DSASM
     public enum AssociativeCompilePass
     {
         kClassName,
-        kClassHeirarchy,
+        kClassBaseClass,
+        kClassHierarchy,
         kClassMemVar,
         
         kClassMemFuncSig,
