f3b76e260aabdad90712cd82c4f0709ffe5e4783
sharadkjaiswal <sharad.jaiswal@autodesk.com>
2014-03-15 02:16:30 +0800

    Marshal IEnumerable as arbitrary rank collection
    IEnumerable, ICollection or IDictionary data types are marshaled as
    arbitrary rank collection so that nested collection can be marshaled
    thru these enumerables.
    
    However if IEnumerable<T> is not marshaled as arbitrary rank collection
    because all the element in the collection will be of type T.
    
    Fixes http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-2192

--- test/Libraries/CoreNodesTests/ListTests.cs
+++ test/Libraries/CoreNodesTests/ListTests.cs
@@ -219,7 +219,7 @@ namespace DSCoreNodesTests
         [Test]
         public static void RemoveValueFromList()
         {
-            Assert.AreEqual(new List<int> { 0, 1, 3, 4 }, List.RemoveItemAtIndex(new List<int> { 0, 1, 2, 3, 4 }, 2));
+            Assert.AreEqual(new List<int> { 0, 1, 3, 4 }, List.RemoveItemAtIndex(new List<int> { 0, 1, 2, 3, 4 }, new int[]{2}));
         }
 
         [Test]
@@ -227,7 +227,16 @@ namespace DSCoreNodesTests
         {
             Assert.AreEqual(
                 new List<int> { 0, 4 },
-                List.RemoveItemAtIndex(new List<int> { 0, 1, 2, 3, 4 }, new List<int> { 1, 2, 3 }));
+                List.RemoveItemAtIndex(new List<int> { 0, 1, 2, 3, 4 }, new int[] { 1, 2, 3 }));
+        }
+
+        [Test]
+        public static void RemoveMultipleValuesFromNestedList()
+        {
+            var strings = new List<string> { "one", "two" };
+            Assert.AreEqual(
+                new List<object> { 0, 4 },
+                List.RemoveItemAtIndex(new List<object> { 0, 1, strings, new List<object> { 1, strings }, 4 }, new int[] { 1, 2, 3 }));
         }
 
         [Test]
