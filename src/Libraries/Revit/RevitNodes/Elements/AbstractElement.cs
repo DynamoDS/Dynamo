27467ad21193248ac1ae881a43367fe3689a8413
Ian Keough <ian@iankeough.com>
2014-03-17 11:44:43 -0700

    Make abtract element implement IGraphicItem to thwart GraphicDataProvider property inspection.

--- src/Libraries/Revit/RevitNodes/Elements/AbstractElement.cs
+++ src/Libraries/Revit/RevitNodes/Elements/AbstractElement.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Autodesk.DesignScript.Interfaces;
 using Autodesk.Revit.DB;
+using DSCore;
 using DSNodeServices;
 using Revit.GeometryConversion;
 using RevitServices.Persistence;
@@ -13,7 +15,7 @@ namespace Revit.Elements
     /// Superclass of all Revit element wrappers
     /// </summary>
     //[SupressImportIntoVM]
-    public abstract class AbstractElement : IDisposable
+    public abstract class AbstractElement : IDisposable, IGraphicItem
     {
         /// <summary>
         /// A reference to the current Document.
@@ -164,6 +166,13 @@ namespace Revit.Elements
             return InternalElement.ToString();
         }
 
+        public void Tessellate(IRenderPackage package)
+        {
+            // Do nothing. We implement this method only to prevent the GraphicDataProvider from
+            // attempting to interrogate the public properties, some of which may require regeneration
+            // or transactions and which must necessarily be threaded in a specific way.
+        }
+
         /// <summary>
         /// Set one of the element's parameters.
         /// </summary>
@@ -226,6 +235,28 @@ namespace Revit.Elements
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Override the element's color in the active view.
+        /// </summary>
+        /// <param name="color">The color to apply to a solid fill on the element.</param>
+        public void OverrideColorInView(DSColor color)
+        {
+            TransactionManager.Instance.EnsureInTransaction(DocumentManager.Instance.CurrentDBDocument);
+
+            var view = DocumentManager.Instance.CurrentUIDocument.ActiveView;
+            var ogs = new OverrideGraphicSettings();
+
+            var patternCollector = new FilteredElementCollector(DocumentManager.Instance.CurrentDBDocument);
+            patternCollector.OfClass(typeof(FillPatternElement));
+            FillPatternElement solidFill = patternCollector.ToElements().Cast<FillPatternElement>().First(x => x.GetFillPattern().Name == "Solid fill");
+
+            ogs.SetProjectionFillColor(new Autodesk.Revit.DB.Color(color.Red, color.Green, color.Blue));
+            ogs.SetProjectionFillPatternId(solidFill.Id);
+            view.SetElementOverrides(this.InternalElementId, ogs);
+
+            TransactionManager.Instance.TransactionTaskDone();
         }
 
         #region dynamic parameter setting methods
