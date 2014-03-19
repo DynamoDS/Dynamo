670782754599e7093d4b87493244a0a7e69dbe44
ellensi <ellensi.chandra@autodesk.com>
2014-03-19 18:27:26 +0800

    Ported and migrated "String.ToNumber"

--- src/Migrations/CoreNodes/dynBaseTypes.cs
+++ src/Migrations/CoreNodes/dynBaseTypes.cs
@@ -2350,13 +2350,7 @@ namespace Dynamo.Nodes
         [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
-            NodeMigrationData migrationData = new NodeMigrationData(data.Document);
-
-            XmlElement oldNode = data.MigratedNodes.ElementAt(0);
-            XmlElement dummyNode = MigrationManager.CreateDummyNode(oldNode, 1, 1);
-            migrationData.AppendNode(dummyNode);
-
-            return migrationData;
+            return MigrateToDsFunction(data, "DSCoreNodes.dll", "String.ToNumber", "String.ToNumber@string");
         }
     }
 
