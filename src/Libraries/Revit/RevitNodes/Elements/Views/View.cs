a81962e44cd2535ad3dec22d6399305f19ae41ed
Peter Boyer <peter.b.boyer@gmail.com>
2014-03-26 11:28:01 -0400

    Improve View.ExportAsImage - respect passed file name, support more file output types, return file

--- src/Libraries/Revit/RevitNodes/Elements/Views/View.cs
+++ src/Libraries/Revit/RevitNodes/Elements/Views/View.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Mime;
 using System.Text;
@@ -32,23 +33,88 @@ namespace Revit.Elements.Views
         /// </summary>
         /// <param name="path">A valid path for the image</param>
         /// <returns>The image</returns>
-        public System.Drawing.Image ExportAsImage(string path)
+        public System.Drawing.Image ExportAsImage(string fullPath)
         {
+            string pathName = fullPath;
+            string extension = null;
+
+            var fileType = ImageFileType.PNG;
+            if (Path.HasExtension(fullPath))
+            {
+                extension = Path.GetExtension(fullPath).ToLower();
+                switch (extension)
+                {
+                    case ".jpg":
+                        fileType = ImageFileType.JPEGLossless;
+                        break;
+                    case ".png":
+                        fileType = ImageFileType.PNG;
+                        break;
+                    case ".bmp":
+                        fileType = ImageFileType.BMP;
+                        break;
+                    case ".tga":
+                        fileType = ImageFileType.TARGA;
+                        break;
+                    case ".tif":
+                        fileType = ImageFileType.TIFF;
+                        break;
+                }
+                pathName = Path.Combine(
+                    Path.GetDirectoryName(fullPath),
+                    Path.GetFileNameWithoutExtension(fullPath));
+            }
+
+            extension = (extension ?? ".png");
+
             var options = new ImageExportOptions
             {
                 ExportRange = ExportRange.SetOfViews,
-                FilePath = path,
-                HLRandWFViewsFileType = ImageFileType.PNG,
+                FilePath = pathName,
+                HLRandWFViewsFileType = fileType,
                 ImageResolution = ImageResolution.DPI_72,
                 ZoomType = ZoomFitType.Zoom,
-                ShadowViewsFileType = ImageFileType.PNG
+                ShadowViewsFileType = fileType
             };
 
             options.SetViewsAndSheets(new List<ElementId> { InternalView.Id });
 
             Document.ExportImage(options);
 
-            return Image.FromFile(path + ".png");
+            // Revit outputs file with a bunch of crap in the file name, let's construct that
+            var actualFn = string.Format("{0} - {1} - {2}{3}", pathName, ViewTypeString(InternalView.ViewType),
+                InternalView.ViewName, extension);
+
+            // and the intended destination
+            var destFn = pathName + extension;
+
+            // rename the file
+            if (File.Exists(destFn)) File.Delete(destFn);
+            File.Move(actualFn, destFn);
+
+            return Image.FromFile(destFn);
+        }
+
+
+        private string ViewTypeString(ViewType vt)
+        {
+            switch (vt)
+            {
+                case ViewType.ThreeD:
+                    return "3D View";
+                case ViewType.AreaPlan:
+                    return "Area Plan";
+                case ViewType.CeilingPlan:
+                    return "Ceiling Plan";
+                case ViewType.EngineeringPlan:
+                    return "Engineering Plan";
+                case ViewType.FloorPlan:
+                    return "Floor Plan";
+                case ViewType.Elevation:
+                    return "Elevation";
+                default:
+                    return "Section View";
+            }
         }
     }
 }
