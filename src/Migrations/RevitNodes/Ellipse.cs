69d76c084339a392c3d7ccc6bb77cff3a596a065
ellensi <ellensi.chandra@autodesk.com>
2014-03-27 19:22:42 +0800

    Fixed migration function

--- src/Migrations/RevitNodes/Ellipse.cs
+++ src/Migrations/RevitNodes/Ellipse.cs
@@ -1,9 +1,16 @@
-﻿using Migrations;
+﻿using Dynamo.Models;
+using Migrations;
 
 namespace Dynamo.Nodes
 {
     public class Ellipse : MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "ProtoGeometry.dll", "Ellipse.ByOriginRadii",
+                "Ellipse.ByOriginRadii@Point,double,double");
+        }
     }
 
     public class EllipticalArc : MigrationNode
