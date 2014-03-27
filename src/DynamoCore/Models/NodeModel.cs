169a43584ae30666b1a740317487015d21e48130
Yu Ke <yu.ke@autodesk.com>
2014-03-27 12:31:42 +0800

    Not setting node state if it is in warning/error state

--- src/DynamoCore/Models/NodeModel.cs
+++ src/DynamoCore/Models/NodeModel.cs
@@ -920,8 +920,11 @@ namespace Dynamo.Models
                     // if there are inputs without connections
                     // mark as dead; otherwise, if the original state is dead,
                     // update it as active.
-                    if (inPorts.Any(x => !x.Connectors.Any() && !(x.UsingDefaultValue && x.DefaultValueEnabled)))
-                        State = ElementState.Dead;
+                    if (State == ElementState.Active)
+                    {
+                        if (inPorts.Any(x => !x.Connectors.Any() && !(x.UsingDefaultValue && x.DefaultValueEnabled)))
+                            State = ElementState.Dead;
+                    }
                     else if (State == ElementState.Dead)
                         State = ElementState.Active;
                 });
