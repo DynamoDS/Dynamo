9cbf56befccc26e517741548f62487ebfcf9d25c
Zhengzi <flymaple1213@gmail.com>
2014-03-28 17:17:43 +0800

    Change 0.7 -> 07 Alpha

--- src/DynamoRevit/Resource_en_us.Designer.cs
+++ src/DynamoRevit/Resource_en_us.Designer.cs
@@ -61,7 +61,7 @@ namespace Dynamo.Applications {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Visual Programming DS 0.7.
+        ///   Looks up a localized string similar to Visual Programming DS 0.7 Alpha.
         /// </summary>
         internal static string App_Description {
             get {
@@ -70,7 +70,7 @@ namespace Dynamo.Applications {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Dynamo 0.7.
+        ///   Looks up a localized string similar to Dynamo 0.7 Alpha.
         /// </summary>
         internal static string App_Name {
             get {
