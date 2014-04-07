7103bd04660da76808775ffdbb7dd43731a5c98b
Peter Boyer <peter.b.boyer@gmail.com>
2014-04-07 15:25:34 -0400

    Migration method for CurvesThroughPoints -> PolyCurve.ByPoints

--- src/Migrations/RevitNodes/Curve.cs
+++ src/Migrations/RevitNodes/Curve.cs
@@ -41,6 +41,12 @@ namespace Dynamo.Nodes
 
     public class CurvesThroughPoints : MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "ProtoGeometry.dll",
+                "PolyCurve.ByPoints", "PolyCurve.ByPoints@Point[],bool");
+        }
     }
 
     public class CurveByPoints : MigrationNode
