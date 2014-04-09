2438b8a2763f193dd4490ee6a2d6e8aec3633f92
Zhengzi <flymaple1213@gmail.com>
2014-04-09 16:46:06 +0800

    migration

--- src/Migrations/CoreNodes/dynBaseTypes.cs
+++ src/Migrations/CoreNodes/dynBaseTypes.cs
@@ -2358,7 +2358,7 @@ namespace Dynamo.Nodes
         [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
-            return MigrateToDsFunction(data, "DSCoreNodes.dll", "String.FromObject", "String.FromObject@var");
+            return MigrateToDsFunction(data, "", "ToString", "ToString@var[]..[]");
         }
     }
 
