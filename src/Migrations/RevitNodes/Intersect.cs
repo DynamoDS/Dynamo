434523a8b94e7401c2c5da8ad4cf60bab12885a7
Peter Boyer <peter.b.boyer@gmail.com>
2014-04-09 14:45:56 -0400

    Refactor and migrate Face-Face Intersection -> Face.Intersect(face)

--- src/Migrations/RevitNodes/Intersect.cs
+++ src/Migrations/RevitNodes/Intersect.cs
@@ -105,6 +105,12 @@ namespace Dynamo.Nodes
 
     public class FaceFaceIntersection : MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "RevitNodes.dll",
+                "Face.Intersect", "Face.Intersect@Face");
+        }
     }
 
     public class CurvePlaneIntersection : MigrationNode
