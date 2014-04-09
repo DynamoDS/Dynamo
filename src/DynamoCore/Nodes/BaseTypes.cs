fcb534ec5f8d4280de9bbe852aa335e4e48a12dc
Zhengzi <flymaple1213@gmail.com>
2014-04-09 11:18:37 +0800

    migration

--- src/DynamoCore/Nodes/BaseTypes.cs
+++ src/DynamoCore/Nodes/BaseTypes.cs
@@ -1655,6 +1655,8 @@ namespace Dynamo.Nodes
 
         #endregion
 
+        /* disable the migration path from number node to CBN.
+
         [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
@@ -1701,6 +1703,8 @@ namespace Dynamo.Nodes
             return migrationData;
         }
 
+        */
+        
         public static List<IDoubleSequence> ParseValue(string text, char[] seps, List<string> identifiers, ConversionDelegate convertToken)
         {
             var idSet = new HashSet<string>(identifiers);
--- src/Migrations/RevitNodes/SimplexNoise.cs
+++ src/Migrations/RevitNodes/SimplexNoise.cs
@@ -1,16 +1,32 @@
 ﻿using Migrations;
+using Dynamo.Models;
 
 namespace Dynamo.Nodes
 {
     public class Simplex1D: MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "SimplexNoise.dll", "SimplexNoise.Generate", "SimplexNoise.Generate@double");
+        }
     }
 
     public class Simplex2D: MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "SimplexNoise.dll", "Simplex2D", "NurbsCurve.ByPoints@Point[]");
+        }
     }
 
     public class Simplex3D: MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "SimplexNoise.dll", "Simplex3D", "NurbsCurve.ByPoints@Point[]");
+        }
     }
 }
