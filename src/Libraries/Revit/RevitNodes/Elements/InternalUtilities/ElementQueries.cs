d5d86deee854c1c91cad578a556c44dd4db91249
Ian Keough <ian@iankeough.com>
2014-04-14 15:31:32 -0700

    Make AllElementsOf... nodes UI nodes so we can respond to doc changes.

---
+++ src/Libraries/Revit/RevitNodes/Elements/InternalUtilities/ElementQueries.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.DB;
+using RevitServices.Persistence;
+
+namespace Revit.Elements.InternalUtilities
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public static class ElementQueries
+    {
+        public static IList<Element> OfFamilyType(FamilySymbol familyType)
+        {
+            var instanceFilter = new ElementClassFilter(typeof(Autodesk.Revit.DB.FamilyInstance));
+            var fec = new FilteredElementCollector(DocumentManager.Instance.CurrentDBDocument);
+
+            var familyInstances = fec.WherePasses(instanceFilter).ToElements().Cast<Autodesk.Revit.DB.FamilyInstance>()
+                .Where(x => x.Symbol.IsSimilarType(familyType.InternalFamilySymbol.Id));
+
+            var instances = familyInstances
+                .Select(x => ElementSelector.ByElementId(x.Id.IntegerValue)).ToList();
+            return instances;
+        }
+
+        public static IList<Element> OfCategory(Category category)
+        {
+            var catFilter = new ElementCategoryFilter(category.InternalCategory.Id);
+            var fec = new FilteredElementCollector(DocumentManager.Instance.CurrentDBDocument);
+            var instances = fec.WherePasses(catFilter)
+                .ToElementIds()
+                .Select(id => ElementSelector.ByElementId(id.IntegerValue))
+                .ToList();
+            return instances;
+        }
+    }
+}
