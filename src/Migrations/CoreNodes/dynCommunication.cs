bc877f13556976bdaf98ace3ef232a9fa5d405e7
Ian Keough <ian@iankeough.com>
2014-04-20 05:35:38 -0700

    Add web request file (whoops) and migration.

--- src/Migrations/CoreNodes/dynCommunication.cs
+++ src/Migrations/CoreNodes/dynCommunication.cs
@@ -1,9 +1,15 @@
-﻿using Migrations;
+﻿using Dynamo.Models;
+using Migrations;
 
 namespace Dynamo.Nodes
 {
     public class WebRequest : MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "DSCoreNodes.dll", "WebRequest.ByUrl", "WebRequest.ByUrl@string");
+        }
     }
 
     public class UdpListener : MigrationNode
