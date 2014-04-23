9dd9c7cdf7e72ae9294c6e1cef251f6124e3ac6b
Ian Keough <ian@iankeough.com>
2014-04-23 11:49:55 -0700

    Add two simple tests for web request.

---
+++ test/Libraries/CoreNodesTests/WebTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using DSCore;
+using NUnit.Framework;
+
+namespace DSCoreNodesTests
+{
+    [TestFixture]
+    class WebTests
+    {
+        [Test]
+        public void WebRequest_ValidArgs()
+        {
+            var result = Web.WebRequestByUrl("http://www.google.com");
+            Assert.IsNotNullOrEmpty(result);
+        }
+
+        [Test]
+        public void WebRequest_BadArgs()
+        {
+            Assert.Throws<WebException>(()=>Web.WebRequestByUrl("http://www.somesitethatdoesnotexist.com"));
+        }
+    }
+}
