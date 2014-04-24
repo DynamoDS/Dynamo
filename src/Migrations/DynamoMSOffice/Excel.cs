3c8ffb8afe76d2756749bfe2bfbc352c38bb1e7d
ellensi <ellensi.chandra@autodesk.com>
2014-04-24 11:15:54 +0800

    Cleanup

--- src/Migrations/DynamoMSOffice/Excel.cs
+++ src/Migrations/DynamoMSOffice/Excel.cs
@@ -50,9 +50,6 @@ namespace Dynamo.Nodes
         [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
-            NodeMigrationData migrationData = new NodeMigrationData(data.Document);
-            XmlElement oldNode = data.MigratedNodes.ElementAt(0);
-
             return MigrateToDsFunction(data, "DSOffice.dll", "Excel.GetDataFromExcelWorksheet",
                 "Excel.GetDataFromExcelWorksheet@var");
         }
