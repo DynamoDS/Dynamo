9aa00e0ef4d4ca9fb4482c6e7206f9da5e824523
Stephen Elliott <steell1890@gmail.com>
2014-04-24 11:23:18 -0400

    ported and migrated PointsOnDividedPath

--- src/Migrations/RevitNodes/DividedCurve.cs
+++ src/Migrations/RevitNodes/DividedCurve.cs
@@ -34,6 +34,15 @@ namespace Dynamo.Nodes
 
     public class PointsOnDividedPath : MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(
+                data,
+                "RevitNodes.dll",
+                "DividedPath.Points",
+                "DividedPath.Points@DividedPath");
+        }
     }
 
 }
