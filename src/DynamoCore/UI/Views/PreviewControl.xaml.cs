bfff33af8287e338af341dc334b8ba25543f7129
Benglin (Ben) Goh <benglin@outlook.com>
2014-04-25 16:13:41 +0800

    Added "PreviewControl" skeleton user control

---
+++ src/DynamoCore/UI/Views/PreviewControl.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dynamo.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for PreviewControl.xaml
+    /// </summary>
+    public partial class PreviewControl : UserControl
+    {
+        #region Private Class Data Members
+
+        public enum State
+        {
+            Hidden, Condensed, Expanded
+        }
+
+        private enum Transition
+        {
+            None, FadingIn, Expanding, Condensing, FadingOut
+        }
+
+        private State currentState = State.Hidden;
+        private Transition currentTransition = Transition.None;
+        private Queue<State> queuedRequest = new Queue<State>();
+
+        #endregion
+
+        #region Public Class Operational Methods
+
+        public PreviewControl()
+        {
+            InitializeComponent();
+        }
+
+        public void TransitionToState(State nextState)
+        {
+            queuedRequest.Enqueue(nextState);
+        }
+
+        #endregion
+
+        #region Public Class Properties
+
+        public State CurrentState { get { return currentState; } }
+
+        #endregion
+    }
+}
