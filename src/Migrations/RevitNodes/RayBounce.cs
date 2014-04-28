de06a324e11c6986ed89a7dc8cecc1ba208fedb4
Ian Keough <ian@iankeough.com>
2014-04-28 14:42:55 -0700

    Migrate RayBounce.

--- src/Migrations/RevitNodes/RayBounce.cs
+++ src/Migrations/RevitNodes/RayBounce.cs
@@ -1,8 +1,16 @@
-﻿using Migrations;
+﻿using Dynamo.Models;
+using Migrations;
 
 namespace Dynamo.Nodes
 {
     class RayBounce : MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "RevitNodes.dll",
+                "RayBounce.ByOriginDirection",
+                "Revit.References.RayBounce.ByOriginDirection@Autodesk.DesignScript.Geometry.Point,Autodesk.DesignScript.Geometry.Vector,int,Revit.Elements.Views.View3D");
+        }
     }
 }
