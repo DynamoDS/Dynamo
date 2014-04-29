067704b3b4fd864028e7ab6b67f0c11294016727
Ian Keough <ian@iankeough.com>
2014-04-28 17:47:08 -0700

    Start work on port of SunPath.

---
+++ src/DynamoRevit/SunPathUpdater.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Autodesk.Revit.DB;
+
+namespace Dynamo.Applications
+{
+    public class SunPathUpdater : IUpdater
+    {
+        public void Execute(UpdaterData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UpdaterId GetUpdaterId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ChangePriority GetChangePriority()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUpdaterName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAdditionalInformation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
