19594a68f47103e0de9d57904aab257ea2407dd7
ellensi <ellensi.chandra@autodesk.com>
2014-04-30 11:19:36 +0800

    Migrate SunPath Direction

--- src/Migrations/RevitNodes/SunPath.cs
+++ src/Migrations/RevitNodes/SunPath.cs
@@ -1,9 +1,19 @@
-﻿
+﻿using System.Linq;
+using Dynamo.Models;
 using Migrations;
 
 namespace Dynamo.Nodes
 {
     public class SunPathDirection : MigrationNode
     {
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            NodeMigrationData migrationData = new NodeMigrationData(data.Document);
+            migrationData.AppendNode(MigrationManager.CloneAndChangeName(
+                data.MigratedNodes.ElementAt(0), "DSRevitNodesUI.SunPathDirection", "SunPath Direction"));
+
+            return migrationData;
+        }
     }
 }
