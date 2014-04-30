c49cb764925dd87d74f3ed7cee06b3fad1f05e66
ellensi <ellensi.chandra@autodesk.com>
2014-04-30 12:16:09 +0800

    Add type conversion to migration

--- src/Migrations/RevitNodes/SunPath.cs
+++ src/Migrations/RevitNodes/SunPath.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Xml;
 using Dynamo.Models;
 using Migrations;
 
@@ -10,8 +11,23 @@ namespace Dynamo.Nodes
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
             NodeMigrationData migrationData = new NodeMigrationData(data.Document);
-            migrationData.AppendNode(MigrationManager.CloneAndChangeName(
-                data.MigratedNodes.ElementAt(0), "DSRevitNodesUI.SunPathDirection", "SunPath Direction"));
+            XmlElement oldNode = data.MigratedNodes.ElementAt(0);
+
+            // Create nodes
+            var vectorAsPoint = MigrationManager.CreateFunctionNodeFrom(oldNode);
+            MigrationManager.SetFunctionSignature(vectorAsPoint, "ProtoGeometry.dll",
+                "Vector.AsPoint", "Vector.AsPoint");
+            migrationData.AppendNode(vectorAsPoint);
+
+            XmlElement sunPathNode = MigrationManager.CloneAndChangeName(
+                oldNode, "DSRevitNodesUI.SunPathDirection", "SunPath Direction");
+            sunPathNode.SetAttribute("guid", System.Guid.NewGuid().ToString());
+            sunPathNode.SetAttribute("x", (System.Convert.ToDouble(oldNode.GetAttribute("x")) - 230).ToString());
+            
+            migrationData.AppendNode(sunPathNode);
+
+            // Update connectors
+            data.CreateConnector(sunPathNode, 0, vectorAsPoint, 0);
 
             return migrationData;
         }
