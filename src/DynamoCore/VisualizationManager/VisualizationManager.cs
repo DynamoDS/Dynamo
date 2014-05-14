bc6e8022bcbad813937551dad007118477fe4169
Ian Keough <ian@iankeough.com>
2014-05-13 18:07:03 -0700

    Don't render after un-pausing.

--- src/DynamoCore/VisualizationManager/VisualizationManager.cs
+++ src/DynamoCore/VisualizationManager/VisualizationManager.cs
@@ -54,7 +54,7 @@ namespace Dynamo
             {
                 if (updatingPaused && value == false)
                 {
-                    Render();
+                    AggregateUpstreamRenderPackages(new RenderTag(CurrentTaskId, null));
                 }
                 Debug.WriteLine("Updating paused = " + value.ToString());
                 updatingPaused = value;
@@ -338,6 +338,9 @@ namespace Dynamo
                     }
                 }
 
+                watch.Stop();
+                Debug.WriteLine(String.Format("RENDER: {0} ellapsed for aggregating geometry for background preview.", watch.Elapsed));
+
                 if (packages.Any())
                 {
                     // if there are packages, send any that aren't empty
@@ -356,14 +359,16 @@ namespace Dynamo
             }
             else
             {
+                watch.Stop();
+                Debug.WriteLine(String.Format("RENDER: {0} ellapsed for aggregating geometry for branch {1}.", watch.Elapsed, tag.Node.GUID));
+
                 //send back renderables for the branch
                 packages = GetUpstreamPackages(tag.Node.Inputs).ToList();
                 if (packages.Any())
                     OnResultsReadyToVisualize(this, new VisualizationEventArgs(packages.Where(x => ((RenderPackage)x).IsNotEmpty()).Cast<RenderPackage>(), tag.Node.GUID.ToString(),tag.TaskId));
             }
 
-            watch.Stop();
-            Debug.WriteLine(String.Format("RENDER: {0} ellapsed for aggregating geometry for watch.", watch.Elapsed));
+            
 
             //LogVisualizationUpdateData(rd, watch.Elapsed.ToString());
         }
