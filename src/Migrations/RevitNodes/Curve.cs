c342f060b0c3913fc122603668cd30a421fbea41
ellensi <ellensi.chandra@autodesk.com>
2014-05-14 17:45:28 +0800

    Migrate "Curve By Points By Curve"

--- src/Migrations/RevitNodes/Curve.cs
+++ src/Migrations/RevitNodes/Curve.cs
@@ -66,13 +66,8 @@ namespace Dynamo.Nodes
         [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
-            NodeMigrationData migrationData = new NodeMigrationData(data.Document);
-
-            XmlElement oldNode = data.MigratedNodes.ElementAt(0);
-            XmlElement dummyNode = MigrationManager.CreateDummyNode(oldNode, 3, 1);
-            migrationData.AppendNode(dummyNode);
-
-            return migrationData;
+            return MigrateToDsFunction(data, "RevitNodes.dll",
+                "ModelCurve.ByCurve", "ModelCurve.ByCurve@Autodesk.DesignScript.Geometry.Curve");
         }
     }
 
