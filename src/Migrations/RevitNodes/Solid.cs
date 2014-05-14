75f7cb4654ad01be4efc6d2f47b3febf987829b9
Zhengzi <flymaple1213@gmail.com>
2014-05-14 11:32:31 +0800

    migrate SkinCurveLoops

--- src/Migrations/RevitNodes/Solid.cs
+++ src/Migrations/RevitNodes/Solid.cs
@@ -586,6 +586,12 @@ namespace Dynamo.Nodes
     public class SkinCurveLoops : MigrationNode
     {
         // PB: deprecated
+        [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
+        public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
+        {
+            return MigrateToDsFunction(data, "ProtoGeometry.dll",
+                "Solid.ByLoft", "Solid.ByLoft@Autodesk.DesignScript.Geometry.PolyCurve[]");
+        }
     }
 
     public class VolumeMeasure : MigrationNode
