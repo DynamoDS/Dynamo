e261db90a12821802858f70e0d41b953d0ec671a
Benglin (Ben) Goh <benglin@outlook.com>
2014-05-24 17:09:31 +0800

    Added "glcorearb.h" and removed the reference to "gl.h"

--- src/Extensions/Dynamorph.Cpp/stdafx.h
+++ src/Extensions/Dynamorph.Cpp/stdafx.h
@@ -10,6 +10,6 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 #include <windows.h>
-#include <gl/GL.h>
+// #include <gl/GL.h>
 #include <string>
 #include <vector>
