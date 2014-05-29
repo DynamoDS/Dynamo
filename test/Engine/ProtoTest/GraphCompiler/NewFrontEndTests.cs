faa9cc5d23e9df19e8be79751bf4e7e2e7fb151e
Luke Church <luke@church.name>
2014-05-29 15:44:31 +0300

    Hotfix for MAGN-3603
    Have the function call node emit parens, to be replaced by a proper fix in 072

---
+++ test/Engine/ProtoTest/GraphCompiler/NewFrontEndTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProtoTest.GraphCompiler
+{
+    [TestFixture]
+    public class NewFrontEndTests
+    {
+        [Test]
+        public void ReproMAGN3603()
+        {
+            List<String> outStrings;
+            GraphToDSCompiler.GraphUtilities.CompileExpression(
+@"a = 1 + (2 * 3);
+b = (1 + 2) * 3;
+c = 1 + 2 * 3;
+", out  outStrings);
+
+
+            Assert.IsTrue(outStrings[0].Trim() == "a = 1 + (2 * 3);");
+            Assert.IsTrue(outStrings[1].Trim() == "b = (1 + 2) * 3;");
+            Assert.IsTrue(outStrings[2].Trim() == "c = 1 + (2 * 3);");
+
+
+
+        }
+
+
+    }
+}
