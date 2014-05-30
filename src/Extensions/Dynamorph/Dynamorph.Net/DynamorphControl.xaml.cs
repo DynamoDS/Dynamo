b7140d41121d7848afd8ae2f0412968585579a50
Benglin (Ben) Goh <benglin@outlook.com>
2014-05-30 23:24:06 +0800

    Added GraphVisualHost into the loading sequence of DynamorphControl

--- src/Extensions/Dynamorph/Dynamorph.Net/DynamorphControl.xaml.cs
+++ src/Extensions/Dynamorph/Dynamorph.Net/DynamorphControl.xaml.cs
@@ -17,8 +17,9 @@ namespace Dynamorph
 {
     public partial class DynamorphControl : UserControl
     {
-        private SynthesizedGraph currentGraph = null;
-        private Dynamorph.VisualizerHwndHost visualizer = null;
+        private SynthesizedGraph pendingGraph = null;
+        private VisualizerHwndHost visualizer = null;
+        private GraphVisualHost graphVisualHost = null;
 
         #region Public Operational Class Methods
 
@@ -38,7 +39,16 @@ namespace Dynamorph
             var nextGraph = graph as SynthesizedGraph;
             nextGraph.BuildGraphStructure();
 
-            this.currentGraph = nextGraph; // Finally, update the internal graph.
+            if (this.graphVisualHost == null)
+            {
+                // No visual host yet, set it later when it gets created.
+                this.pendingGraph = nextGraph;
+            }
+            else
+            {
+                // The graph visual host is ready to update.
+                this.graphVisualHost.RefreshGraph(nextGraph);
+            }
         }
 
         #endregion
@@ -60,6 +70,17 @@ namespace Dynamorph
 
         private void OnDynamorphControlLoaded(object sender, RoutedEventArgs e)
         {
+            this.graphVisualHost = new GraphVisualHost();
+            this.GraphCanvas.Children.Add(graphVisualHost);
+
+            if (this.pendingGraph != null)
+            {
+                // A graph was sent before GraphVisualHost got created, now 
+                // that we have the visual host, set the graph for display.
+                this.graphVisualHost.RefreshGraph(this.pendingGraph);
+                this.pendingGraph = null;
+            }
+
             if (visualizer == null)
             {
                 var b = VisualizerHostElement;
---
+++ src/Extensions/Dynamorph/Dynamorph.Net/GraphVisualHost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Dynamorph
+{
+    class GraphVisualHost : FrameworkElement
+    {
+        private VisualCollection childVisuals = null;
+        private DrawingVisual nodeVisuals = new DrawingVisual();
+        private DrawingVisual edgeVisuals = new DrawingVisual();
+
+        internal GraphVisualHost()
+        {
+            childVisuals = new VisualCollection(this);
+            childVisuals.Add(edgeVisuals);
+            childVisuals.Add(nodeVisuals);
+        }
+
+        internal void RefreshGraph(SynthesizedGraph graph)
+        {
+        }
+    }
+}
