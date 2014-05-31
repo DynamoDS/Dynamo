940a731ac1dc7b51e22bb0455c2e4d20a4242dc0
Luke Church <luke@church.name>
2014-05-31 18:10:30 +0300

    Revert "Revert "Merge pull request #1691 from junmendoza/CottonCandyJun""
    This reverts commit f62737897a78f413d7363e91bc2e01e8f80b7e79.

---
+++ src/DynamoRevit/DynamoRunner_Revit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Autodesk.Revit.DB;
+using Dynamo.Revit;
+using Dynamo.Utilities;
+using Dynamo.ViewModels;
+using RevitServices.Persistence;
+using RevitServices.Transactions;
+
+namespace Dynamo.Applications
+{
+    class DynamoRunner_Revit : Dynamo.Core.DynamoRunner
+    {
+        private DynamoController_Revit controllerRevit;
+
+        public DynamoRunner_Revit(DynamoController_Revit controllerRevit)
+        {
+            this.controllerRevit = controllerRevit;
+        }
+
+
+        protected override void OnRunCancelled(bool error)
+        {
+            base.OnRunCancelled(error);
+
+            if (controllerRevit.transaction != null && controllerRevit.transaction.Status == TransactionStatus.Started)
+                controllerRevit.transaction.CancelTransaction();
+        }
+
+
+        protected override void Evaluate()
+        {
+            //DocumentManager.Instance.CurrentDBDocument = DocumentManager.Instance.CurrentUIDocument.Document;
+
+            if (controllerRevit.DynamoViewModel.RunInDebug)
+            {
+                controllerRevit.TransMode = TransactionMode.Debug; //Debug transaction control
+                dynSettings.DynamoLogger.Log("Running expression in debug.");
+            }
+            else
+            {
+                // As we use a generic function node to represent all functions,
+                // we don't know if a node has something to do with Revit or 
+                // not, neither we know that the re-execution of a dirty node
+                // will trigger the update for a Revit related node. Now just
+                // run the execution in the idle thread until we find out a 
+                // way to control the execution.
+                controllerRevit.TransMode = TransactionMode.Automatic; //Automatic transaction control
+                Debug.WriteLine("Adding a run to the idle stack.");
+            }
+
+            //Run in idle thread no matter what
+            RevitServices.Threading.IdlePromise.ExecuteOnIdleSync(base.Evaluate);
+        }
+        
+
+    }
+}
