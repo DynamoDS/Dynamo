41c884a86a6f5e7ccae2e54b59257b9e11eb5bcd
Benglin (Ben) Goh <benglin@outlook.com>
2014-05-31 15:28:21 +0800

    Added edge rendering logic

--- src/Extensions/Dynamorph/Dynamorph.Net/GraphVisualHost.cs
+++ src/Extensions/Dynamorph/Dynamorph.Net/GraphVisualHost.cs
@@ -72,6 +72,10 @@ namespace Dynamorph
 
             foreach (var node in graph.Nodes)
             {
+                PathGeometry geom;
+                PathFigure figure;
+                BezierSegment s = new BezierSegment();
+
                 drawingContext.DrawRectangle(Brushes.Black, null, node.Rect);
             }
 
@@ -80,6 +84,48 @@ namespace Dynamorph
 
         private void RefreshGraphEdges()
         {
+            if (graph == null || (graph.Edges.Any() == false))
+                return; // There is no graph or it has no edges.
+
+            var startPoints = new PointCollection();
+            var endPoints = new PointCollection();
+
+            var nodes = this.graph.Nodes;
+            foreach (var edge in this.graph.Edges)
+            {
+                var startNode = nodes.First(n => n.Identifier == edge.StartNodeId);
+                var endNode = nodes.First(n => n.Identifier == edge.EndNodeId);
+                startPoints.Add(startNode.OutputPoint);
+                endPoints.Add(endNode.InputPoint);
+            }
+
+            var drawingContext = edgeVisuals.RenderOpen();
+            for (int index = 0; index < startPoints.Count; ++index)
+            {
+                PathGeometry pathGeometry = new PathGeometry();
+                PathFigure pathFigure = new PathFigure()
+                {
+                    StartPoint = startPoints[index]
+                };
+
+                AddBezier(pathFigure, startPoints[index], endPoints[index]);
+                pathGeometry.Figures.Add(pathFigure);
+                drawingContext.DrawGeometry(null, new Pen(Brushes.Red, 1.0), pathGeometry);
+            }
+
+            drawingContext.Close();
+        }
+
+        private void AddBezier(PathFigure figure, Point start, Point end)
+        {
+            var offset = ((end.X - start.X) * 0.25);
+            figure.StartPoint = start;
+
+            BezierSegment bezierSegment = new BezierSegment(
+                new Point(start.X + offset, start.Y),
+                new Point(end.X - offset, end.Y), end, true);
+
+            figure.Segments.Add(bezierSegment);
         }
 
         #endregion
