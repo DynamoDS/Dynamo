5c08177c5ea77cf71ef8077b0007d1ec19308953
Benglin (Ben) Goh <benglin@outlook.com>
2014-06-11 14:59:51 +0800

    Added implementation for "CanvasCheckBox.OnApplyTemplate" method

--- src/DynamoCore/UI/Controls/CanvasCheckBox.cs
+++ src/DynamoCore/UI/Controls/CanvasCheckBox.cs
@@ -8,6 +8,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -31,5 +32,46 @@ namespace Dynamo.UI.Controls
         public static readonly DependencyProperty ImageStripProperty =
             DependencyProperty.Register("ImageStrip", typeof(ImageSource),
             typeof(CanvasCheckBox), new UIPropertyMetadata(null));
+
+        /// <summary>
+        /// You can't use dynamic resource references or data binding expressions
+        /// to set Storyboard or animation property values. That's because 
+        /// everything inside a ControlTemplate must be thread-safe, and the 
+        /// timing system must Freeze Storyboard objects to make them thread-safe.
+        /// A Storyboard cannot be frozen if it or its child timelines contain 
+        /// dynamic resource references or data binding expressions. 
+        /// 
+        /// See: http://msdn.microsoft.com/en-us/library/ms742868.aspx
+        /// 
+        /// </summary>
+        /// 
+        public override void OnApplyTemplate()
+        {
+            var animationNames = new string[]
+            {
+                "normalAnimation",
+                "hoverOverAnimation",
+                "pressedAnimation",
+                "checkedAnimation"
+            };
+
+            var offsetValues = new double[]
+            {
+                0.0 * this.Height,
+                -1.0 * this.Height,
+                -2.0 * this.Height,
+                -3.0 * this.Height
+            };
+
+            for (int index = 0; index < animationNames.Length; ++index)
+            {
+                var animation = Template.FindName(animationNames[index], this);
+                var doubleAnimation = animation as DoubleAnimation;
+                if (doubleAnimation != null)
+                    doubleAnimation.To = offsetValues[index];
+            }
+
+            base.OnApplyTemplate();
+        }
     }
 }
