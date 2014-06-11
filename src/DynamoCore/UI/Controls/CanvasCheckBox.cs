0e88079f8a3f25d62bd11ed46ba15af6670dc1c1
Benglin (Ben) Goh <benglin@outlook.com>
2014-06-11 13:07:39 +0800

    Implemented a new class "CanvasCheckBox" for in-canvas buttons

---
+++ src/DynamoCore/UI/Controls/CanvasCheckBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dynamo.UI.Controls
+{
+    public class CanvasCheckBox : CheckBox
+    {
+        // static CanvasCheckBox()
+        // {
+        //     DefaultStyleKeyProperty.OverrideMetadata(typeof(CanvasCheckBox),
+        //         new FrameworkPropertyMetadata(typeof(CanvasCheckBox)));
+        // }
+
+        public ImageSource ImageStrip
+        {
+            get { return (ImageSource)GetValue(ImageStripProperty); }
+            set { SetValue(ImageStripProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageStripProperty =
+            DependencyProperty.Register("ImageStrip", typeof(ImageSource),
+            typeof(CanvasCheckBox), new UIPropertyMetadata(null));
+    }
+}
