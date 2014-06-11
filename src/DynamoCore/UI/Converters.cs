6f94727650cb99dbccf091bf0bba994c73c291ba
Benglin (Ben) Goh <benglin@outlook.com>
2014-06-11 18:25:25 +0800

    Updated "InverseBooleanConverter" to be two-way converter

--- src/DynamoCore/UI/Converters.cs
+++ src/DynamoCore/UI/Converters.cs
@@ -698,16 +698,16 @@ namespace Dynamo.Controls
         public object Convert(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            if (targetType != typeof(bool))
+            if (targetType != typeof(bool) && (targetType != typeof(bool?)))
                 throw new InvalidOperationException("The target must be a boolean");
 
-            return !(bool)value;
+            return !((bool)value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            throw new NotSupportedException();
+            return !((bool)value);
         }
 
         #endregion
