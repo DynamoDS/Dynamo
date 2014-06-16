c941e15ae39f5742e9abfe0dfa498b35bece610f
Yu Ke <yu.ke@autodesk.com>
2014-06-16 18:13:46 +0800

    Add ScopedNodeModel

---
+++ src/DynamoCore/Models/ScopedNodeModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamo.Models
+{
+    /// <summary>
+    /// ScopedNodeModel will be put all its children inside its scope so that 
+    /// they won't get compiled in global scope.
+    /// </summary>
+    public class ScopedNodeModel: NodeModel
+    {
+        private bool IsNodeInScope(NodeModel node, IEnumerable<NodeModel> scopes)
+        {
+            foreach (var index in Enumerable.Range(0, node.OutPortData.Count))
+            {
+                HashSet<Tuple<int, NodeModel>> outputTuples = null;
+                if (TryGetOutput(index, out outputTuples))
+                {
+                    if (!outputTuples.All(t => scopes.Contains(t.Item2)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// If the input port is the scoped port or not. 
+        /// </summary>
+        /// <param name="portIndex"></param>
+        /// <returns></returns>
+        protected virtual bool IsScopedInPort(int portIndex)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Get all nodes that in its input ports's scope. A node is in its 
+        /// scope if it is one of its upstream nodes and all of that node's 
+        /// downstream nodes are in this node's scope.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<NodeModel> GetScopedChildren(int portIndex)
+        {
+            // The related test cases are in test fixture ScopedNodeTest.
+
+            var scopedNodes = new HashSet<NodeModel>();
+
+            Tuple<int, NodeModel> inputTuple = null;
+            if (!IsScopedInPort(portIndex) || 
+                !this.TryGetInput(portIndex, out inputTuple))
+            {
+                return scopedNodes;
+            }
+
+            scopedNodes.Add(this);
+            var inputNode = inputTuple.Item2;
+            var workingList = new Queue<NodeModel>();
+            if (IsNodeInScope(inputNode, scopedNodes))
+            {
+                workingList.Enqueue(inputNode);
+                scopedNodes.Add(inputNode);
+            }
+
+            while (workingList.Count() > 0)
+            {
+                var currentNode = workingList.Dequeue();
+                if (currentNode is ScopedNodeModel)
+                {
+                    continue;
+                }
+
+                foreach (int index in Enumerable.Range(0, currentNode.InPortData.Count))
+                {
+                    if (currentNode.TryGetInput(index, out inputTuple))
+                    {
+                        inputNode = inputTuple.Item2;
+                        if (IsNodeInScope(inputNode, scopedNodes))
+                        {
+                            workingList.Enqueue(inputNode);
+                            scopedNodes.Add(inputNode);
+                        }
+                    }
+                }
+            }
+
+            return scopedNodes.Skip(1);
+        }
+    }
+}
---
+++ test/DynamoCoreTests/ScopedNodeTest.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Dynamo.Models;
+using Dynamo.Nodes;
+using Dynamo.Tests;
+using NUnit.Framework;
+
+namespace Dynamo.Tests
+{
+    [TestFixture]
+    internal class ScopedNodeTest : DSEvaluationUnitTest
+    {
+        string testFolder { get { return Path.Combine(GetTestDirectory(), "core", "scopednode"); } }
+
+        // test class
+        private class TwoScopedInputs: ScopedNodeModel
+        {
+            public TwoScopedInputs()
+            {
+                InPortData.Add(new PortData("port1", "Port1 block"));
+                InPortData.Add(new PortData("port2", "Port2 block"));
+                OutPortData.Add(new PortData("result", "Result"));
+
+                RegisterAllPorts();
+            }
+        }
+
+        [Test]
+        public void TestScopedNode1()
+        {
+            //
+            // n1 -> n2 --> n4 --> s1 --> n6 --> n7 --> s2
+            //            /      /                            
+            //          n3      n5                           
+            // 
+            // For s1, n1, n2, n3, n4 in its scope (for input port 0)
+            //         n5 in its scope (for input port 1)
+            // For s2, s1, n6, n7 in its scope for input port 0
+            DynamoModel model = Controller.DynamoModel;
+            Func<string, CodeBlockNodeModel> createCBN = 
+                s => new CodeBlockNodeModel(s, Guid.NewGuid(), model.CurrentWorkspace, 0, 0);
+
+            var cbn1 = createCBN("n1;");
+
+            var cbn2 = createCBN("n2;");
+            cbn2.ConnectInput(0, 0, cbn1);
+
+            var cbn3 = createCBN("n3;");
+
+            var cbn4 = createCBN("n4=x+y;");
+            cbn4.ConnectInput(0, 0, cbn2);
+            cbn4.ConnectInput(1, 0, cbn3);
+
+            var cbn5 = createCBN("n5;");
+
+            var s1 = new TwoScopedInputs();
+            s1.ConnectInput(0, 0, cbn4);
+            s1.ConnectInput(1, 0, cbn5);
+
+            var scopedNodes = s1.GetScopedChildren(0);
+            Assert.AreEqual(4, scopedNodes.Count());
+            Assert.IsTrue(scopedNodes.Contains(cbn1));
+            Assert.IsTrue(scopedNodes.Contains(cbn2));
+            Assert.IsTrue(scopedNodes.Contains(cbn3));
+            Assert.IsTrue(scopedNodes.Contains(cbn4));
+
+            scopedNodes = s1.GetScopedChildren(1);
+            Assert.AreEqual(1, scopedNodes.Count());
+            Assert.IsTrue(scopedNodes.Contains(cbn5));
+
+            var cbn6 = createCBN("n6;");
+            cbn6.ConnectInput(0, 0, s1);
+
+            var cbn7 = createCBN("n7;");
+            cbn7.ConnectInput(0, 0, cbn6);
+
+            var s2 = new TwoScopedInputs();
+            s2.ConnectInput(0, 0, cbn7);
+
+            scopedNodes = s2.GetScopedChildren(0);
+            Assert.AreEqual(3, scopedNodes.Count());
+            Assert.IsTrue(scopedNodes.Contains(cbn6));
+            Assert.IsTrue(scopedNodes.Contains(cbn7));
+            Assert.IsTrue(scopedNodes.Contains(s1));
+        }
+
+        [Test]
+        public void TestScopedNodeModel02()
+        {
+            //                n5
+            //               /
+            // n1 -> n2 --> n4 --> s1
+            //            /                                  
+            //          n3                                 
+            // 
+            // For s1, none is in its scope 
+            DynamoModel model = Controller.DynamoModel;
+            Func<string, CodeBlockNodeModel> createCBN =
+                s => new CodeBlockNodeModel(s, Guid.NewGuid(), model.CurrentWorkspace, 0, 0);
+
+            var cbn1 = createCBN("n1;");
+
+            var cbn2 = createCBN("n2;");
+            cbn2.ConnectInput(0, 0, cbn1);
+
+            var cbn3 = createCBN("n3;");
+
+            var cbn4 = createCBN("n4=x+y;");
+            cbn4.ConnectInput(0, 0, cbn2);
+            cbn4.ConnectInput(1, 0, cbn3);
+
+            var cbn5 = createCBN("n5;");
+            cbn5.ConnectInput(0, 0, cbn4);
+
+            var s1 = new TwoScopedInputs();
+            s1.ConnectInput(0, 0, cbn4);
+
+            var scopedNodes = s1.GetScopedChildren(0);
+            Assert.AreEqual(0, scopedNodes.Count());
+        }
+
+        [Test]
+        public void TestScopedNodeModel03()
+        {
+            //         n5       
+            //        /       
+            // n1 -> n2 --> n4 --> s1
+            //            /                                  
+            //          n3                                 
+            // 
+            // For s1, n3, n4 are in its scope
+            DynamoModel model = Controller.DynamoModel;
+            Func<string, CodeBlockNodeModel> createCBN =
+                s => new CodeBlockNodeModel(s, Guid.NewGuid(), model.CurrentWorkspace, 0, 0);
+
+            var cbn1 = createCBN("n1;");
+
+            var cbn2 = createCBN("n2;");
+            cbn2.ConnectInput(0, 0, cbn1);
+
+            var cbn3 = createCBN("n3;");
+
+            var cbn4 = createCBN("n4=x+y;");
+            cbn4.ConnectInput(0, 0, cbn2);
+            cbn4.ConnectInput(1, 0, cbn3);
+
+            var cbn5 = createCBN("n5;");
+            cbn5.ConnectInput(0, 0, cbn2);
+
+            var s1 = new TwoScopedInputs();
+            s1.ConnectInput(0, 0, cbn4);
+
+            var scopedNodes = s1.GetScopedChildren(0);
+            Assert.AreEqual(2, scopedNodes.Count());
+            Assert.IsTrue(scopedNodes.Contains(cbn3));
+            Assert.IsTrue(scopedNodes.Contains(cbn4));
+        }
+    }
+}
