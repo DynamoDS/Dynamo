abc7a0b80678acc32826719cb0a6886a18b79b2c
Ian Keough <ian@iankeough.com>
2014-06-16 11:46:22 -0700

    Add DynamoAddinGenerator tool.
    Consolidates logic for finding installed versions of Dynamo and Revit
    and generating the appropriate addins.

---
+++ tools/DynamoAddinGenerator/Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Autodesk.RevitAddIns;
+
+namespace DynamoAddinGenerator
+{
+    class Program
+    {
+        private const string addinName = "Dynamo.addin";
+        private const string versionSelectorName = "DynamoVersionSelector.addin";
+
+        private static string dynamo_063 = @"C:\Autodesk\Dynamo";
+        private static string dynamo_071_x86 = Path.Combine(Environment.SpecialFolder.ProgramFilesX86.ToString(),"Dynamo071");
+        private static string dynamo_071_bad = Path.Combine(Environment.SpecialFolder.ProgramFiles.ToString(),"Dynamo071");
+        private static string dynamo_post_071 = Path.Combine(Environment.SpecialFolder.ProgramFiles.ToString(), "Dynamo");
+
+        static void Main(string[] args)
+        {
+            var installedProds = GetInstalledProducts();
+
+            var prods = GetValidProducts(installedProds).ToList();
+
+            if (!prods.Any())
+            {
+                Console.WriteLine("There were no Revit products found.");
+                return;
+            }
+
+            DeleteAddins();
+
+            
+
+            var dynamos = GetInstalledDynamos();
+
+            // Search each addin path for an existing Dynamo.addin
+            // These are from installs without multiple versions
+            GenerateAddins(prods, dynamos);
+        }
+
+        private static IEnumerable<IRevitProduct> GetInstalledProducts()
+        {
+            var installedProds = RevitProductUtility.GetAllInstalledRevitProducts()
+                .ToList()
+                .Select(p => new DynamoRevitProduct(p));
+
+            return installedProds;
+        }
+
+        private static bool HasDynamo063Install()
+        {
+            if (Directory.Exists(dynamo_063))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool HasDynamo071BadInstall()
+        {
+            if (Directory.Exists(dynamo_071_bad))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool HasDynamo071InstallInx86()
+        {
+            if (Directory.Exists(dynamo_071_x86))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool HasDynamo071Install()
+        {
+            if (Directory.Exists(dynamo_post_071))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static IEnumerable<IDynamoInstall> GetInstalledDynamos()
+        {
+            var dynamos = new List<IDynamoInstall>();
+
+            if (HasDynamo063Install())
+            {
+                
+            }
+
+            if (HasDynamo071BadInstall())
+            {
+                
+            }
+
+            if (HasDynamo071InstallInx86())
+            {
+                
+            }
+
+            if (HasDynamo071Install())
+            {
+                
+            }
+
+            return dynamos;
+        }
+
+        internal static IEnumerable<IRevitProduct> GetValidProducts(IEnumerable<IRevitProduct> products)
+        {
+            var validProds = new List<IRevitProduct>();
+
+            foreach (var prod in products)
+            {
+                if (prod.VersionString == RevitVersion.Revit2011.ToString() ||
+                    prod.VersionString == RevitVersion.Revit2012.ToString() ||
+                    prod.VersionString == RevitVersion.Revit2013.ToString())
+                {
+                    continue;
+                }
+
+                validProds.Add(prod);
+            }
+
+            return validProds;
+        }
+
+        internal static void BackupOldAddins(IEnumerable<IRevitProduct> products)
+        {
+            foreach (var product in products)
+            {
+                var oldAddinPath = Path.Combine(product.AddinPath, "Dynamo.addin");
+                var newAddinPath = Path.Combine(product.AddinPath, "Dynamo.OLD");
+
+                if (File.Exists(oldAddinPath))
+                {
+                    File.Move(oldAddinPath,newAddinPath);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deletes all existing Dynamo addins.
+        /// </summary>
+        internal static void DeleteAddins()
+        {
+            
+        }
+
+        /// <summary>
+        /// Generates new Dynamo addins.
+        /// </summary>
+        /// <param name="products">A collection of revit installs.</param>
+        /// <param name="dynamos">A collection of dynamo installs.</param>
+        internal static void GenerateAddins(IEnumerable<IRevitProduct> products, IEnumerable<IDynamoInstall> dynamos)
+        {
+            // If there is only one version of revit installed, we will
+            // create a Dynamo.addin, and return.
+            if (products.Count() == 1)
+            {
+                // Generate a dynamo.addin and return
+                GenerateDynamoAddin(products.FirstOrDefault());
+                return;
+            }
+
+            // If there are multiple versions of Revit installed, we will
+            // create a DynamoVersionSelector.addin.
+            foreach (var prod in products)
+            {
+                GenerateVersionSelectorAddin(prod);
+            }
+        }
+
+        /// <summary>
+        /// Generate a single-version Dynamo addin.
+        /// </summary>
+        /// <param name="product"></param>
+        internal static void GenerateDynamoAddin(IRevitProduct product)
+        {
+            
+            
+        }
+
+        /// <summary>
+        /// Generate a multiple-version Dynamo addin.
+        /// </summary>
+        /// <param name="product"></param>
+        internal static void GenerateVersionSelectorAddin(IRevitProduct product)
+        {
+
+            
+        }
+    }
+
+    public class DynamoRevitProduct : IRevitProduct
+    {
+        public string ProductName { get; set; }
+        public string AddinPath { get; set; }
+        public string InstallLocation { get; set; }
+        public string VersionString { get; set; }
+        public string CurrentDynamoAddinPath { get; set; }
+
+        public DynamoRevitProduct(RevitProduct product)
+        {
+            ProductName = product.Name;
+            AddinPath = product.AllUsersAddInFolder;
+            InstallLocation = product.InstallLocation;
+            VersionString = product.Version.ToString();
+
+            var oldAddinPath = Path.Combine(AddinPath, "Dynamo.addin");
+            var newAddinPath = Path.Combine(AddinPath, "DynamoVersionSelector.addin");
+
+            if (File.Exists(oldAddinPath))
+            {
+                CurrentDynamoAddinPath = oldAddinPath;
+            }
+            else if (File.Exists(newAddinPath))
+            {
+                CurrentDynamoAddinPath = newAddinPath;
+            }
+        }
+    }
+
+    public class DynamoInstall:IDynamoInstall
+    {
+        public string Path { get; set; }
+
+        public DynamoInstall(string path)
+        {
+            Path = path;
+        }
+    }
+
+    public interface IRevitProduct
+    {
+        string ProductName { get; set; }
+        string AddinPath { get; set; }
+        string InstallLocation { get; set; }
+        string VersionString { get; set; }
+        string CurrentDynamoAddinPath { get; set; }
+    }
+
+    public interface IDynamoInstall
+    {
+        string Path { get; set; }
+    }
+}
---
+++ tools/DynamoAddinGenerator/Tests.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autodesk.RevitAddIns;
+using Moq;
+using NUnit.Framework;
+
+namespace DynamoAddinGenerator
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void GetValidProducts_FindsValidProducts()
+        {
+            var prods = SomeValidProductsWithDynamo();
+            var validProds = Program.GetValidProducts(prods);
+            Assert.AreEqual(3, validProds.Count());
+        }
+
+        [Test]
+        public void GetValidProducts_FindsNoValidProducts()
+        {
+            var prods = NoValidProductsWithNoDynamo();
+            var validProds = Program.GetValidProducts(prods);
+            Assert.AreEqual(0, validProds.Count());
+        }
+
+        [Test]
+        public void BackupOldAddins_BacksUpOldAddins()
+        {
+            
+
+        }
+
+        public void BackupOldAddins_DoesNothing()
+        {
+            
+        }
+
+        private IEnumerable<IRevitProduct> NoValidProductsWithNoDynamo()
+        {
+            // Two Revit 2014 products we should handle
+            var p1 = new Mock<IRevitProduct>();
+            p1.Setup(p => p.ProductName).Returns("Revit1");
+            p1.Setup(p => p.InstallLocation).Returns(@"C:\Revit1\");
+            p1.Setup(p => p.AddinPath).Returns(@"C:\addins1\");
+            p1.Setup(p => p.VersionString).Returns(RevitVersion.Revit2011.ToString);
+
+            var p2 = new Mock<IRevitProduct>();
+            p2.Setup(p => p.ProductName).Returns("Revit2");
+            p2.Setup(p => p.InstallLocation).Returns(@"C:\Revit2\");
+            p2.Setup(p => p.AddinPath).Returns(@"C:\addins2\");
+            p2.Setup(p => p.VersionString).Returns(RevitVersion.Revit2012.ToString);
+
+            return new List<IRevitProduct>() { p1.Object, p2.Object};
+        }
+
+        private IEnumerable<IRevitProduct> SomeValidProductsWithNoDynamo()
+        {
+            // Two Revit 2014 products we should handle
+            var p1 = new Mock<IRevitProduct>();
+            p1.Setup(p => p.ProductName).Returns("Revit1");
+            p1.Setup(p => p.InstallLocation).Returns(@"C:\Revit1\");
+            p1.Setup(p => p.AddinPath).Returns(@"C:\addins1\");
+            p1.Setup(p => p.VersionString).Returns(RevitVersion.Revit2014.ToString);
+
+            var p2 = new Mock<IRevitProduct>();
+            p2.Setup(p => p.ProductName).Returns("Revit2");
+            p2.Setup(p => p.InstallLocation).Returns(@"C:\Revit2\");
+            p2.Setup(p => p.AddinPath).Returns(@"C:\addins2\");
+            p2.Setup(p => p.VersionString).Returns(RevitVersion.Revit2014.ToString);
+
+            // A Revit 2015 product we should  handle
+            var p3 = new Mock<IRevitProduct>();
+            p3.Setup(p => p.ProductName).Returns("Revit3");
+            p3.Setup(p => p.InstallLocation).Returns(@"C:\Revit3\");
+            p3.Setup(p => p.AddinPath).Returns(@"C:\addins3\");
+            p3.Setup(p => p.VersionString).Returns(RevitVersion.Revit2015.ToString);
+
+            // A Revit 2011 product we should not handle
+            var p4 = new Mock<IRevitProduct>();
+            p4.Setup(p => p.ProductName).Returns("Revit4");
+            p4.Setup(p => p.InstallLocation).Returns(@"C:\Revit4\");
+            p4.Setup(p => p.AddinPath).Returns(@"C:\addins4\");
+            p4.Setup(p => p.VersionString).Returns(RevitVersion.Revit2011.ToString);
+
+            return new List<IRevitProduct>() {p1.Object, p2.Object, p3.Object, p4.Object};
+        }
+
+        private IEnumerable<IRevitProduct> SomeValidProductsWithDynamo()
+        {
+            // Two Revit 2014 products we should handle
+            var p1 = new Mock<IRevitProduct>();
+            p1.Setup(p => p.ProductName).Returns("Revit1");
+            p1.Setup(p => p.InstallLocation).Returns(@"C:\Revit1\");
+            p1.Setup(p => p.AddinPath).Returns(@"C:\addins1\");
+            p1.Setup(p => p.VersionString).Returns(RevitVersion.Revit2014.ToString);
+            p1.Setup(p => p.CurrentDynamoAddinPath).Returns(@"C:\addins1\Dynamo.addin");
+
+            var p2 = new Mock<IRevitProduct>();
+            p2.Setup(p => p.ProductName).Returns("Revit2");
+            p2.Setup(p => p.InstallLocation).Returns(@"C:\Revit2\");
+            p2.Setup(p => p.AddinPath).Returns(@"C:\addins2\");
+            p2.Setup(p => p.VersionString).Returns(RevitVersion.Revit2014.ToString);
+            p2.Setup(p => p.CurrentDynamoAddinPath).Returns(@"C:\addins2\Dynamo.addin");
+
+            // A Revit 2015 product we should  handle
+            var p3 = new Mock<IRevitProduct>();
+            p3.Setup(p => p.ProductName).Returns("Revit3");
+            p3.Setup(p => p.InstallLocation).Returns(@"C:\Revit3\");
+            p3.Setup(p => p.AddinPath).Returns(@"C:\addins3\");
+            p3.Setup(p => p.VersionString).Returns(RevitVersion.Revit2015.ToString);
+            p3.Setup(p => p.CurrentDynamoAddinPath).Returns(@"C:\addins3\Dynamo.addin");
+
+            // A Revit 2011 product we should not handle
+            var p4 = new Mock<IRevitProduct>();
+            p4.Setup(p => p.ProductName).Returns("Revit4");
+            p4.Setup(p => p.InstallLocation).Returns(@"C:\Revit4\");
+            p4.Setup(p => p.AddinPath).Returns(@"C:\addins4\");
+            p4.Setup(p => p.VersionString).Returns(RevitVersion.Revit2011.ToString);
+
+            return new List<IRevitProduct>() { p1.Object, p2.Object, p3.Object, p4.Object };
+        }
+    }
+}
