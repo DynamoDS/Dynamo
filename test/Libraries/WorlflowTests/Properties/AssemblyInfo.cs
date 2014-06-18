16af1632b8d52375b5a39447963b73cda8d6d06c
riteshchandawar <ritesh.chandawar@autodesk.com>
2014-06-18 09:59:35 +0800

    Addressed comments provided by Luke

--- test/Libraries/WorlflowTests/Properties/AssemblyInfo.cs
+++ test/Libraries/WorlflowTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GeometryTests")]
+[assembly: AssemblyTitle("WorkflowTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GeometryTests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("WorkflowTests")]
+[assembly: AssemblyCopyright("Copyright Autodesk 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
