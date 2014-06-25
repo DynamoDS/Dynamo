28f97e5bd014a862a25bb5c9f78e98b1b15ba816
Yu Ke <yu.ke@autodesk.com>
2014-06-25 11:28:18 +0800

    Fix mang-3188: dynamo hangs in opening for some custom node

--- src/DynamoCore/Core/DynamoController.cs
+++ src/DynamoCore/Core/DynamoController.cs
@@ -311,6 +311,7 @@ namespace Dynamo
             DisposeLogic.IsShuttingDown = false;
 
             EngineController = new EngineController(this);
+            CustomNodeManager.RecompileAllNodes(EngineController);
 
             //This is necessary to avoid a race condition by causing a thread join
             //inside the vm exec
@@ -395,6 +396,7 @@ namespace Dynamo
             }
 
             EngineController = new EngineController(this);
+            CustomNodeManager.RecompileAllNodes(EngineController);
         }
 
         public void RequestRedraw()
