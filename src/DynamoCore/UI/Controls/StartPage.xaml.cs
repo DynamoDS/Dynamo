d0da7f7781fbd71fd6889250e91a070fdb6f9fc9
Benglin (Ben) Goh <benglin@outlook.com>
2014-06-25 13:49:18 +0800

    Added list box styles and implemented initial start page list boxes

--- src/DynamoCore/UI/Controls/StartPage.xaml.cs
+++ src/DynamoCore/UI/Controls/StartPage.xaml.cs
@@ -22,6 +22,35 @@ namespace Dynamo.UI.Controls
         public StartPage()
         {
             InitializeComponent();
+            this.Loaded += OnStartPageLoaded;
+        }
+
+        void OnStartPageLoaded(object sender, RoutedEventArgs e)
+        {
+            this.filesListBox.Items.Add("New");
+            this.filesListBox.Items.Add("Open");
+
+            this.recentListBox.Items.Add("ImportUsingSat");
+            this.recentListBox.Items.Add("door_movable-copy");
+            this.recentListBox.Items.Add("door_movable");
+            this.recentListBox.Items.Add("test file");
+            this.recentListBox.Items.Add("doormovable");
+
+            this.samplesListBox.Items.Add("Abstract cubes");
+            this.samplesListBox.Items.Add("Attractor circles");
+            this.samplesListBox.Items.Add("Parametric bridge");
+            this.samplesListBox.Items.Add("Rotated bricks");
+            this.samplesListBox.Items.Add("Shading devices");
+
+            this.askListBox.Items.Add("Discussion forum");
+            this.askListBox.Items.Add("email team@dynamobim.org");
+            this.askListBox.Items.Add("Visit www.dynamobim.org");
+
+            this.referenceListBox.Items.Add("Dynamo reference");
+            this.referenceListBox.Items.Add("Video tutorials");
+
+            this.codeListBox.Items.Add("Github repository");
+            this.codeListBox.Items.Add("Send issues");
         }
     }
 }
