d434db46026b6d3e25b6eb9537e5238ef550a8d7
vtaran <vtaran@sitrusllc.com.ua>
2014-06-25 17:09:27 +0300

    1) Implement ComputationResult response with the node data which was changed after execution of RunCancelCommand (values, geometry) 2) Add REST service to retrieve static data (for now it is the list of Node Models) 3) Update models drawing logic to avoid Dynamo crash on subsequent client updates

--- src/DynamoCore/UI/Commands/RecordableCommands.cs
+++ src/DynamoCore/UI/Commands/RecordableCommands.cs
@@ -8,6 +8,10 @@ using Dynamo.Utilities;
 using Newtonsoft.Json;
 using System.Runtime.Serialization;
 using Newtonsoft.Json.Serialization;
+using System.Text;
+using Autodesk.DesignScript.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
 namespace Dynamo.ViewModels
 {
     partial class DynamoViewModel
@@ -30,8 +34,8 @@ namespace Dynamo.ViewModels
             /// <summary>
             /// Settings that is used for serializing commands
             /// </summary>
-            static JsonSerializerSettings jsonSettings;
-            
+            protected static JsonSerializerSettings jsonSettings;
+
             /// <summary>
             /// Initialize commands serializing settings
             /// </summary>
@@ -39,7 +43,7 @@ namespace Dynamo.ViewModels
             {
                 jsonSettings = new JsonSerializerSettings()
                 {
-                    TypeNameHandling = TypeNameHandling.All,
+                    TypeNameHandling = TypeNameHandling.Objects,
                     ContractResolver = new CamelCasePropertyNamesContractResolver()
                 };
             }
@@ -71,6 +75,16 @@ namespace Dynamo.ViewModels
 
                 Tag = tag;
                 IsInPlaybackMode = false;
+            }
+
+            /// <summary>
+            /// Send the result of executing command to subscriber. 
+            /// </summary>
+            public event ShowMessage ShowResult;
+            protected void OnShowResult(string result)
+            {
+                if (ShowResult != null)
+                    ShowResult(result);
             }
 
             /// <summary>
@@ -288,6 +302,12 @@ namespace Dynamo.ViewModels
         }
 
         /// <summary>
+        /// The delegate used for sending the result of executing command to subscriber. 
+        /// </summary>
+        /// <param name="message">Result message</param>
+        public delegate void ShowMessage(string message);
+
+        /// <summary>
         /// This class is base for those RecordableCommands that should have 
         /// Guid NodeId that causes the problems during deserialization
         /// </summary>
@@ -491,6 +511,140 @@ namespace Dynamo.ViewModels
         [DataContract]
         public class RunCancelCommand : RecordableCommand
         {
+            /// <summary>
+            /// The class that represents calculated result for a node
+            /// </summary>
+            [DataContract]
+            public class ExecutedNode
+            {
+                /// <summary>
+                /// The class that represents data for drawing a graphic primitive 
+                /// </summary>
+                public class Primitive
+                {
+                    /// <summary>
+                    /// Name of the graphic primitive
+                    /// </summary>
+                    [DataMember]
+                    public string PrimitiveType { get; private set; }
+
+                    /// <summary>
+                    /// Data that is needed for drawing this primitive. For example
+                    /// coordinates of a point
+                    /// </summary>
+                    [DataMember]
+                    public string PrimitiveData { get; private set; }
+
+                    public Primitive(string type, string data)
+                    {
+                        PrimitiveType = type;
+                        PrimitiveData = data;
+                    }
+                }
+
+                /// <summary>
+                /// Guid of the specified node
+                /// </summary>
+                [DataMember]
+                public string NodeID { get; private set; }
+
+                /// <summary>
+                /// State of the node after executing
+                /// </summary>
+                [DataMember]
+                public string State { get; private set; }
+
+                /// <summary>
+                /// State description. It is empty when state has Active or Dead value
+                /// </summary>
+                [DataMember]
+                public string StateMessage { get; private set; }
+
+                /// <summary>
+                /// String representing of the result object
+                /// </summary>
+                [DataMember]
+                public string Data { get; private set; }
+
+                /// <summary>
+                /// Indicates whether the result object should be drawn on the canvas
+                /// </summary>
+                [DataMember]
+                public bool IsGraphic { get; private set; }
+
+                /// <summary>
+                /// List of the graphic primitives that result object consist of.
+                /// It is empty for nongraphic objects
+                /// </summary>
+                [DataMember]
+                public List<Primitive> GraphicPrimitives { get; private set; }
+
+                public ExecutedNode(string id, string state, string stateMessage,
+                    string data, List<IRenderPackage> packages)
+                {
+                    this.NodeID = id;
+                    this.State = state;
+                    this.StateMessage = stateMessage;
+                    this.Data = data;
+                    GeneratePrimitives(packages);
+                }
+
+                private void GeneratePrimitives(List<IRenderPackage> packages)
+                {
+                    IsGraphic = packages != null && packages.Any();
+
+                    if (IsGraphic)
+                    {
+                        GraphicPrimitives = new List<Primitive>();
+                        foreach (var package in packages)
+                        {
+                            // Add points
+                            GraphicPrimitives.AddRange(GeneratePoints(package.PointVertices));
+
+                            var points = GeneratePoints(package.LineStripVertices);
+                            // Add lines
+                            GraphicPrimitives.AddRange(ConcatPoints(points, 2, "Line"));
+
+                            points = GeneratePoints(package.TriangleVertices);
+                            // Add triangles
+                            GraphicPrimitives.AddRange(ConcatPoints(points, 3, "Triangle"));
+                        }
+                    }
+                }
+
+                private List<Primitive> GeneratePoints(List<double> coordinates)
+                {
+                    if (coordinates == null)
+                        return null;
+                    string name = "Point";
+                    string data;
+                    var points = new List<Primitive>();
+                    for (int i = 2; i < coordinates.Count; i += 3)
+                    {
+                        data = "(" + coordinates[i - 2] + ";" + coordinates[i - 1] + ";" + coordinates[i] + ")";
+                        points.Add(new Primitive(name, data));
+                    }
+                    return points;
+                }
+
+                private List<Primitive> ConcatPoints(List<Primitive> points, int count, string newName)
+                {
+                    string data;
+                    var concatPoints = new List<Primitive>();
+                    while (points.Count >= count)
+                    {
+                        data = string.Empty;
+                        for (int j = 0; j < count; j++)
+                        {
+                            data += points[j].PrimitiveData;
+                        }
+                        points.RemoveRange(0, count);
+                        concatPoints.Add(new Primitive(newName, data));
+                    }
+                    return concatPoints;
+                }
+            }
+
             #region Public Class Methods
 
             public RunCancelCommand(bool showErrors, bool cancelRun)
@@ -530,7 +684,24 @@ namespace Dynamo.ViewModels
 
             protected override void ExecuteCore(DynamoViewModel dynamoViewModel)
             {
+                dynSettings.Controller.VisualizationManager.RenderComplete += VisualizationManager_RenderComplete;
                 dynamoViewModel.RunCancelImpl(this);
+            }
+
+            private void VisualizationManager_RenderComplete(object sender, RenderCompletionEventArgs e)
+            {
+                var nodes = new List<ExecutedNode>();
+                foreach (var item in dynSettings.Controller.DynamoModel.NodeMap)
+                {
+                    var execNode = new ExecutedNode(item.Key.ToString(), item.Value.State.ToString(), item.Value.ToolTipText,
+                        (item.Value.CachedValue.Data == null && !item.Value.CachedValue.IsCollection ? "null" : item.Value.CachedValue.IsCollection ? "Array" : item.Value.CachedValue.Data.ToString()),
+                        item.Value.RenderPackages);
+                    nodes.Add(execNode);
+                }
+
+                string nodesInfoMessage = JsonConvert.SerializeObject(nodes, jsonSettings);
+                OnShowResult(nodesInfoMessage);
+                dynSettings.Controller.VisualizationManager.RenderComplete -= VisualizationManager_RenderComplete;
             }
 
             protected override void SerializeCore(XmlElement element)
