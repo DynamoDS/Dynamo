2118ecf06099d98212d697cce0e89200300239b6
Peter Boyer <peter.b.boyer@gmail.com>
2014-06-27 18:09:42 -0400

    Added unit test for setting Element parameter using Element argument, added some additional assembly resolvers

---
+++ test/Libraries/Revit/RevitNodesTests/Elements/ElementTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.Revit.DB;
+
+using NUnit.Framework;
+
+using Revit.Elements;
+
+using RevitServices.Persistence;
+
+using RTF.Framework;
+
+namespace DSRevitNodesTests.Elements
+{
+    [TestFixture]
+    class ElementTests : RevitNodeTestBase
+    {
+        [Test]
+        [TestModel(@".\materials.rvt")]
+        public void SetParameterByName_Element_CanSuccessfullySetMaterialByElement()
+        {
+
+            var mat = Revit.Elements.Material.ByName("Glass");
+
+            var ele = ElementSelector.ByType<Autodesk.Revit.DB.FamilyInstance>(true).First();
+
+            var paramName = "Body Material";
+            var elemId0 = ele.GetParameterValueByName(paramName);
+
+            Assert.AreNotEqual( mat.Id, elemId0 );
+
+            ele.SetParameterByName(paramName, mat);
+
+            DocumentManager.Regenerate();
+
+            var elemId1 = ele.GetParameterValueByName(paramName);
+
+            Assert.AreEqual(mat.InternalElement.Id, elemId1);
+
+        }
+
+    }
+}
