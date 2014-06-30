243467aab250b555555c3eab7e7a0cf6d19b723e
Luke Church <luke@church.name>
2014-06-30 20:41:55 +0200

    Add CSharpAnalytics Library

---
+++ extern/CSharpAnalytics/Source/Vendor/CSharpAnalytics.Tests/Network/HttpWebRequesterTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using CSharpAnalytics.Network;
+#if WINDOWS_STORE || WINDOWS_PHONE
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+namespace CSharpAnalytics.Test.Network
+{
+    [TestClass]
+    public class HttpWebRequesterTests
+    {
+        private const string BaseUriString = "http://localhost/csharpanalytics";
+
+        [TestMethod]
+        public void HttpWebRequester_CreateRequest_Uses_Get_Under_2000_Bytes()
+        {
+            var shortUri = new Uri(BaseUriString);
+            var request = HttpWebRequester.CreateRequest(shortUri);
+
+            Assert.AreEqual("GET", request.Method);
+            Assert.AreEqual(shortUri.AbsoluteUri, request.RequestUri.AbsoluteUri);
+            Assert.AreEqual(-1, request.ContentLength);
+        }
+
+        [TestMethod]
+        public void HttpWebRequester_CreateRequest_Uses_Get_When_2000_Bytes()
+        {
+            var longUri = new Uri(BaseUriString + "?" + TestHelpers.RandomChars(2000));
+            var borderlineUri = new Uri(longUri.AbsoluteUri.Substring(0, 2000));
+
+            var request = HttpWebRequester.CreateRequest(borderlineUri);
+
+            Assert.AreEqual("GET", request.Method);
+            Assert.AreEqual(borderlineUri.AbsoluteUri, request.RequestUri.AbsoluteUri);
+            Assert.AreEqual(-1, request.ContentLength);
+        }
+
+        [TestMethod]
+        public void HttpWebRequester_CreateMessage_Uses_Post_Over_2000_Bytes()
+        {
+            var baseUri = new Uri(BaseUriString);
+            var longUri = new Uri(baseUri.AbsoluteUri + "?" + TestHelpers.RandomChars(2000));
+
+            var request = HttpWebRequester.CreateRequest(longUri, false);
+
+            Assert.AreEqual("POST", request.Method);
+            Assert.AreEqual(baseUri.AbsoluteUri, request.RequestUri.AbsoluteUri);
+            Assert.IsTrue(request.ContentLength > 100);
+        }
+    }
+}
---
+++ extern/CSharpAnalytics/Source/Vendor/Samples/CSharpAnalytics.Sample.Windows81/App.xaml.cs
@@ -0,0 +1,142 @@
+﻿using Windows.UI.ApplicationSettings;
+using CSharpAnalytics.Sample.Windows81.Common;
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Grid App template is documented at http://go.microsoft.com/fwlink/?LinkId=234226
+
+namespace CSharpAnalytics.Sample.Windows81
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton Application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override async void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            // CSharpAnalytics initialization
+            AutoMeasurement.DebugWriter = d => System.Diagnostics.Debug.WriteLine(d);
+            AutoMeasurement.Start(new MeasurementConfiguration("UA-319000-8"), e);
+
+#if DEBUG
+            // Show graphics profiling information while debugging.
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // Display the current frame rate counters
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+                //Associate the frame with a SuspensionManager key                                
+                SuspensionManager.RegisterFrame(rootFrame, "AppFrame");
+                // Set the default language
+                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // Restore the saved session state only when appropriate
+                    try
+                    {
+                        await SuspensionManager.RestoreAsync();
+                    }
+                    catch (SuspensionManagerException)
+                    {
+                        //Something went wrong restoring state.
+                        //Assume there is no state and continue
+                    }
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(typeof(GroupedItemsPage), e.Arguments);
+            }
+
+            // CSharpAnalytics init complete
+            AutoMeasurement.Attach(rootFrame);
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await SuspensionManager.SaveAsync();
+            deferral.Complete();
+        }
+
+        /// <summary>
+        /// Fires when the Window is created and you should sign up for charm notifications.
+        /// </summary>
+        /// <param name="args">Arguments relating to which window was created.</param>
+        protected override void OnWindowCreated(WindowCreatedEventArgs args)
+        {
+            base.OnWindowCreated(args);
+            SettingsPane.GetForCurrentView().CommandsRequested += SettingsCommandsRequested;
+        }
+
+        /// <summary>
+        /// Settings charm is requesting commands, register the Options flyout.
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="args">Application Commands object to register settings with.</param>
+        private static void SettingsCommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            args.Request.ApplicationCommands
+                .Add(new SettingsCommand("options", "Options", _ => new OptionsSettingsFlyout().Show()));
+        }
+    }
+}
