a87b663350fec1c7c1488b0aaa87fac094e6e102
Michael Kirschner <mjk.kirschner@gmail.com>
2014-06-29 23:37:05 -0400

    everything working, except extracting members from dynamic types is not simple

--- src/Libraries/CoreNodesUI/Inspector.cs
+++ src/Libraries/CoreNodesUI/Inspector.cs
@@ -5,63 +5,72 @@ using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using Dynamo.Controls;
 using Dynamo.Models;
 using Dynamo.UI;
 using Dynamo.Utilities;
 using ProtoCore.AST.AssociativeAST;
-using Dynamo.UI.Commands;
-using Dynamo.ViewModels;
 using Dynamo.Nodes;
 using System.Reflection;
 using System.Windows.Data;
 using System.Windows.Controls.Primitives;
+using System.Dynamic;
 
+using rt =  Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
 
 namespace DSCoreNodesUI
-{   
+{
     [IsDesignScriptCompatible]
     [NodeName("Inspector")]
     [NodeCategory(BuiltinNodeCategories.CORE_VIEW)]
-    [NodeDescription("Get a color given a color range.")]
+    [NodeDescription("Displays Members of Any Object - Useful for debugging.")]
     public class Inspector : VariableInputNode, IWpfNode
     {
-
-       public class combobox_selected_index_wrapper: INotifyPropertyChanged{
+        /// <summary>
+        /// wraps a combox box selexted index
+        /// </summary>
+        public class combobox_selected_index_wrapper : INotifyPropertyChanged
+        {
 
             public combobox_selected_index_wrapper()
-            { 
-            
-            }
-
-        private int selectedIndex = 0;
-        public int SelectedIndex
-        {
-            get { return selectedIndex; }
-            set
             {
-                
-                selectedIndex = value;
-                NotifyPropertyChanged("SelectedIndex");
+
             }
-        }
-        private void NotifyPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
+
+            private int selectedIndex = 0;
+            public int SelectedIndex
             {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                get { return selectedIndex; }
+                set
+                {
+
+                    selectedIndex = value;
+                    NotifyPropertyChanged("SelectedIndex");
+                }
             }
+            private void NotifyPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-
-         }
-       
-
+        /// <summary>
+        /// value that keeps track of desired number of dropdowns
+        /// </summary>
         private int numDropDowns = 1;
+        /// <summary>
+        /// collections of items - populated from public properties in PopulateItems()
+        /// </summary>
         private ObservableCollection<DynamoDropDownItem> items = new ObservableCollection<DynamoDropDownItem>();
         public ObservableCollection<DynamoDropDownItem> Items
         {
@@ -72,6 +81,9 @@ namespace DSCoreNodesUI
                 RaisePropertyChanged("Items");
             }
         }
+        /// <summary>
+        /// collection of index wrappers
+        /// </summary>
         private ObservableCollection<combobox_selected_index_wrapper> indicies = new ObservableCollection<combobox_selected_index_wrapper>();
         public ObservableCollection<combobox_selected_index_wrapper> Indicies
         {
@@ -82,14 +94,16 @@ namespace DSCoreNodesUI
                 RaisePropertyChanged("Indicies");
             }
         }
-       
-      
 
-        
 
+
+
+        /// <summary>
+        /// list of comboboxes - can use this to track existing number of comboboxes
+        /// </summary>
         public List<ComboBox> comboboxes = new List<ComboBox>();
-
-
+        public List<combobox_selected_index_wrapper> previous_indicies = new List<combobox_selected_index_wrapper>();
+       // public List<TextBox> tboxes = new List<TextBox>();
 
 
         public event EventHandler RequestSelectChange;
@@ -100,17 +114,17 @@ namespace DSCoreNodesUI
         }
 
         public Inspector()
-        {  
+        {
             InPortData.Add(new PortData("object", "Object to Inspect"));
             OutPortData.Add(new PortData("object", "The Object"));
 
             RegisterAllPorts();
-            
+
             // subscribe this method to send an update event whenever any property on this object changes...
             // but that handler checks if IsUpdated is fired
             this.PropertyChanged += Inspector_PropertyChanged;
-            
-            
+
+
         }
 
         void Inspector_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -121,9 +135,9 @@ namespace DSCoreNodesUI
             if (InPorts.Any(x => x.Connectors.Count == 0))
                 return;
 
-           // OnRequestSelectChange(this, EventArgs.Empty);
+            OnRequestSelectChange(this, EventArgs.Empty);
         }
-        
+
         public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
         {
 
@@ -134,12 +148,22 @@ namespace DSCoreNodesUI
             };
         }
 
+        
+        
+        public static object GetDynamicValue(dynamic ob, string name)
+        {
+            CallSite<Func<CallSite, object, object>> site
+                = CallSite<Func<CallSite, object, object>>.Create
+                (rt.Binder.GetMember(rt.CSharpBinderFlags.None, name,
+                 typeof(IDynamicMetaObjectProvider), new rt.CSharpArgumentInfo[] { rt.CSharpArgumentInfo.Create(rt.CSharpArgumentInfoFlags.None, null) }));
+            return site.Target(site, ob);
+        }
 
 
         public void PopulateItems()
         {
             // getting the input object song and dance
-            if (Inputs.Count > 0)
+            if (InPorts.All(x => x.Connectors.Count != 0))
             {
 
                 var inputnode = InPorts[0].Connectors[0].Start.Owner;
@@ -149,41 +173,101 @@ namespace DSCoreNodesUI
 
                 var mirror = dynSettings.Controller.EngineController.GetMirror(inputId);
 
-                object objectinput = null;
-
-
-                if (mirror.GetData().IsCollection)
+                if (mirror != null)
                 {
-                    objectinput = mirror.GetData().GetElements().Select(x => x.Data).FirstOrDefault();
+                    object objectinput = null;
+
+
+                    if (mirror.GetData().IsCollection)
+                    {
+                        objectinput = mirror.GetData().GetElements().Select(x => x.Data).FirstOrDefault();
+                    }
+                    else
+                    {
+                        objectinput = mirror.GetData().Data;
+                    }
+
+                    objectinput = objectinput as object;
+
+                    //before clearing the items and forcing updates
+                    // of the selections, lets grab all current selected indicies
+                    previous_indicies.Clear();
+                    foreach (combobox_selected_index_wrapper wrapper in Indicies)
+                    {
+                        var x = new combobox_selected_index_wrapper();
+                        x.SelectedIndex = wrapper.SelectedIndex;
+                        previous_indicies.Add(x);
+                    }
+
+                    Items.Clear();
+
+                    // check the type of this object, if it's dynamic have to parse membernames and then get values
+                    if (objectinput is IDynamicMetaObjectProvider)
+                    {
+                        var names = new List<string>();
+                        var dynobj = objectinput as IDynamicMetaObjectProvider;
+                        if (dynobj != null)
+                        {
+                            names.AddRange(dynobj.GetMetaObject(System.Linq.Expressions.Expression.Constant(dynobj)).GetDynamicMemberNames());
+                        }
+
+
+                        foreach (var name in names)
+                        {
+                            var val = GetDynamicValue(objectinput, name);
+
+                            if (val != null)
+                            {
+                                items.Add(new DynamoDropDownItem(string.Format("{0}:{1}", name, val), val));
+                            }
+
+
+                            /* object val = null;
+                             MemberInfo mem = objectinput.GetType().GetMember(name, BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy).FirstOrDefault();
+
+                             if (mem.MemberType == MemberTypes.Field)
+                             {
+                                 val = ((FieldInfo)mem).GetValue(objectinput);
+                             }
+                             else if (mem.MemberType == MemberTypes.Property)
+                             {
+
+                                 val = ((PropertyInfo)mem).GetValue(objectinput, null);
+                             }
+                             if (val != null){
+                             items.Add(new DynamoDropDownItem(string.Format("{0}:{1}", name, val), val));
+                             }
+                         }
+                             */
+
+                        }
+
+                    }
+
+                    // if object was not dynamic    
+                    else
+                    {
+                        var propertyInfos = objectinput.GetType().GetProperties(
+                   BindingFlags.Public | BindingFlags.NonPublic // Get public and non-public
+                 | BindingFlags.Static | BindingFlags.Instance  // Get instance + static
+                 | BindingFlags.FlattenHierarchy); // Search up the hierarchy
+
+
+
+                        foreach (var prop in propertyInfos.ToList())
+                        {
+                            var val = prop.GetValue(objectinput, null);
+                            items.Add(new DynamoDropDownItem(string.Format("{0}:{1}", prop.Name, val), val));
+                        }
+
+                    }
+
+                    //Items = Items.OrderBy(x => x.Name).ToObservableCollection();
+
                 }
-                else
-                {
-                    objectinput = mirror.GetData().Data;
-                }
-
-                objectinput = objectinput as object;
-
-
-                Items.Clear();
-
-                var propertyInfos = objectinput.GetType().GetProperties(
-           BindingFlags.Public | BindingFlags.NonPublic // Get public and non-public
-         | BindingFlags.Static | BindingFlags.Instance  // Get instance + static
-         | BindingFlags.FlattenHierarchy); // Search up the hierarchy
-
-
-                foreach (var prop in propertyInfos.ToList())
-                {
-                    var val = prop.GetValue(objectinput, null);
-                    items.Add(new DynamoDropDownItem(string.Format("{0}:{1}", prop.Name, val), val));
-                }
-
-                Items = Items.OrderBy(x => x.Name).ToObservableCollection();
 
             }
-
         }
-
         protected override void AddInput()
         {
             // instead of actually adding an input
@@ -191,8 +275,8 @@ namespace DSCoreNodesUI
             // create a new indexwrapper and store it
             // then call setupcustomUIelements on the UIdispatch thread.
             numDropDowns = numDropDowns + 1;
-            combobox_selected_index_wrapper new_selected_index = new combobox_selected_index_wrapper();
-            indicies.Add(new_selected_index);
+            // combobox_selected_index_wrapper new_selected_index = new combobox_selected_index_wrapper();
+            //indicies.Add(new_selected_index);
             RequiresRecalc = true;
             OnRequestSelectChange(this, EventArgs.Empty);
         }
@@ -203,8 +287,8 @@ namespace DSCoreNodesUI
             {
                 numDropDowns = numDropDowns - 1;
                 // not sure about this actually deleting the wrapper
-                var lastindex = indicies[indicies.Count - 1];
-                indicies.RemoveAt(indicies.Count - 1);
+                // var lastindex = indicies[indicies.Count - 1];
+                //indicies.RemoveAt(indicies.Count - 1);
 
                 RequiresRecalc = true;
                 OnRequestSelectChange(this, EventArgs.Empty);
@@ -220,7 +304,7 @@ namespace DSCoreNodesUI
         {
             return "object" + index;
         }
-        
+
         protected override int GetInputIndex()
         {
             return InPortData.Count;
@@ -239,10 +323,41 @@ namespace DSCoreNodesUI
 
         public ComboBox gen_and_setup_combobox(WrapPanel wp)
         {
-
+            // make a new index wrapper to bind to
             combobox_selected_index_wrapper new_selected_index = new combobox_selected_index_wrapper();
+            //store it
             indicies.Add(new_selected_index);
-           int index_into_index = indicies.IndexOf(new_selected_index);
+            //we just added this so this is the index
+            int index_into_index = indicies.Count - 1;
+
+/*
+
+            var tbox = new TextBox
+            {
+                Width = System.Double.NaN,
+                MinWidth = 100,
+                Height = Configurations.PortHeightInPixels,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Center
+
+            };
+            wp.Children.Add(tbox);
+           
+
+            //text box for testing the index binding
+            var tindexBinding = new Binding()
+            {
+                Path = new PropertyPath("SelectedIndex"),
+                Mode = BindingMode.OneWay,
+                Source = new_selected_index
+            };
+            tbox.SetBinding(TextBox.TextProperty, tindexBinding);
+
+            */
+
+
+
+
             var combo = new ComboBox
                         {
                             Width = System.Double.NaN,
@@ -251,58 +366,76 @@ namespace DSCoreNodesUI
                             HorizontalAlignment = HorizontalAlignment.Stretch,
                             VerticalAlignment = VerticalAlignment.Center
                         };
-                        wp.Children.Add(combo);
-                        comboboxes.Add(combo);
-                        //System.Windows.Controls.Grid.SetColumn(combo, 0);
-                        //System.Windows.Controls.Grid.SetRow(combo, 0);
-                        
-                        combo.DropDownOpened += combo_DropDownOpened;
-                      /*  combo.SelectionChanged += delegate
-                        {
-                            if (combo.SelectedIndex != -1)
-                            {
-                                RequiresRecalc = true;
-                                int index = comboboxes.IndexOf(combo);
-                                SelectedIndicies[index] = combo.SelectedIndex;
-                            }
-                        };
-                       */
-                        combo.DropDownClosed += delegate
-                        {
-                            //disallow selection of nothing
-                            if (combo.SelectedIndex == -1)
-                            {
-                              
-                                
-                            }
-                        };
-                        
-                        combo.DataContext = this;
-                        //bind this combo box to the selected item hash
+            wp.Children.Add(combo);
+            comboboxes.Add(combo);
+          //  tboxes.Add(tbox);
+            //System.Windows.Controls.Grid.SetColumn(combo, 0);
+            //System.Windows.Controls.Grid.SetRow(combo, 0);
 
-                        var bindingVal = new System.Windows.Data.Binding("Items") { Mode = BindingMode.TwoWay, Source = this };
-                        combo.SetBinding(ItemsControl.ItemsSourceProperty, bindingVal);
+            combo.DropDownOpened += combo_DropDownOpened;
+            combo.SelectionChanged += delegate
+            {   // if we are changing the selection
+                // this might occur because we've changed the selection
+                // or because items has been refreshed
+                // if items is refreshed - index will goto -1
+                // and in that case we'll hold onto the current index
+                if (combo.SelectedIndex == -1)
+                {
+                    //RequiresRecalc = true;
+                    int index_of_this_box = comboboxes.IndexOf(combo);
+                    //indicies[index_of_this_box] = previous_indicies[index_of_this_box];
+                    
+                    // if this combo box has been removed from the combobox list, then 
+                    // the selected index should refer to the last remaining item in the list.
+                    // actually this may be unture... might need to just delete the combobox, could do it here or where it is removed.
+                    if (index_of_this_box > -1)
+                    {   
+                    
+                    combo.SelectedIndex = previous_indicies[index_of_this_box].SelectedIndex;
+                    }
+                }
+            };
 
-                        //bind the selected index to the 
-                        var indexBinding = new Binding()
-                        {  Path= new PropertyPath("Indicies[index_into_index].SelectedIndex"),
-                            Mode = BindingMode.TwoWay,
-                            Source = this
-                        };
-                        combo.SetBinding(Selector.SelectedIndexProperty, indexBinding);
-                       
-            return combo;            
+            combo.DropDownClosed += delegate
+            {
+                //disallow selection of nothing
+                if (combo.SelectedIndex == -1)
+                {
+
+
+                }
+            };
+
+            combo.DataContext = this;
+            //bind this combo box to the selected item hash
+
+            var bindingVal = new System.Windows.Data.Binding("Items") { Mode = BindingMode.TwoWay, Source = this };
+            combo.SetBinding(ItemsControl.ItemsSourceProperty, bindingVal);
+
+            //bind the selected index to the 
+            var indexBinding = new Binding()
+            {
+                Path = new PropertyPath("SelectedIndex"),
+                Mode = BindingMode.TwoWay,
+                Source = new_selected_index
+            };
+            combo.SetBinding(Selector.SelectedIndexProperty, indexBinding);
+
+            return combo;
         }
 
         public ComboBox remove_combo(WrapPanel wp)
         {
             // find the last combobox
             var last = comboboxes[comboboxes.Count - 1];
+           // var lasttbox = tboxes[tboxes.Count-1];
             //remove it from the wrap panel
             wp.Children.Remove(last);
             comboboxes.Remove(last);
+          //  wp.Children.Remove(lasttbox);
+           // tboxes.Remove(lasttbox);
             // remove it's index from the list of indicies
-            indicies.RemoveAt(comboboxes.Count-1);
+            indicies.RemoveAt(Indicies.Count - 1);
             return last;
 
         }
@@ -342,7 +475,7 @@ namespace DSCoreNodesUI
 
             // add the first combobox
             var firstcombo = gen_and_setup_combobox(wp);
-           
+
 
             RequestSelectChange += delegate
             {
@@ -371,6 +504,6 @@ namespace DSCoreNodesUI
             return false; // Previews are not shown for this node type.
         }
 
-     
+
     }
 }
