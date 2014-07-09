6c4d3f8aae70029dc54802dce60849516ff98725
MikeKirschner <mjk.kirschner@gmail.com>
2014-07-09 14:55:24 -0400

    converted dynamo graphs for aligning surfaces to c# - units tests coming in next commits.  This has been simply tested on simple cube, unfolds each pair of faces correctly over shared edge.

--- src/Unfold/AlignPlanarFaces.cs
+++ src/Unfold/AlignPlanarFaces.cs
@@ -6,42 +6,66 @@ using System.Threading.Tasks;
 using Autodesk.DesignScript.Geometry;
 using Autodesk.DesignScript.Interfaces;
 using Autodesk.DesignScript.Runtime;
-using Unfold.Unfold_Planar;
+using Unfold;
 
 namespace Unfold
 {
-   public static class AlignPlanarFaces
+    public static class AlignPlanarFaces
     {
 
+        /// <summary>
+        /// method that searches for an edge in a set of edges that shares the same start point as another edge but is not that edge
+        /// </summary>
+        /// <param name="edges"></param>
+        /// <param name="StartPointOnSharedEdge"></param>
+        /// <param name="sharedEdge"></param>
+        /// <returns></returns>
+        public static List<Point> FindPointsOfEdge_StartingAtPoint(List<Unfold_Planar.EdgeLikeEntity> edges, 
+            Point StartPointOnSharedEdge, Unfold_Planar.EdgeLikeEntity sharedEdge)
+        {
+            Unfold_Planar.EdgeLikeEntity found_edge = null;
 
-       public static List<Point> FindPointsOfEdge_StartingAtPoint(List<Unfold_Planar.EdgeLikeEntity> edges, Point StartPointOnSharedEdge, Unfold_Planar.EdgeLikeEntity sharedEdge)
-       {
-           Unfold_Planar.EdgeLikeEntity found_edge = null;
+            Point Start = null;
+            Point End = null;
 
-           Point Start = null;
-           Point End = null;
-           
-           // we are searching for an edge that has the same start point as the shared edge, but is not the shared edge
-           foreach (var edge in edges)
-           {
-               if ((edge.End.IsAlmostEqualTo(StartPointOnSharedEdge)) || ( edge.Start.IsAlmostEqualTo(StartPointOnSharedEdge))){
-                   if (!(edge.Start.IsAlmostEqualTo(StartPointOnSharedEdge)&&(edge.End.IsAlmostEqualTo(sharedEdge.End)))){
-                       found_edge = edge;
-                   }
-               }
-           }
+            // we are searching for an edge that has the same start point as the shared edge, but is not the shared edge
+            foreach (var edge in edges)
+            {
+                if ((edge.End.IsAlmostEqualTo(StartPointOnSharedEdge)) || (edge.Start.IsAlmostEqualTo(StartPointOnSharedEdge)))
+                {
+                    if (!(edge.Start.IsAlmostEqualTo(StartPointOnSharedEdge) && (edge.End.IsAlmostEqualTo(sharedEdge.End))))
+                    {
+                        found_edge = edge;
+                    }
+                }
+            }
 
-           //reverse if needed
-           if (!found_edge.Start.IsAlmostEqualTo(sharedEdge.Start)){
+            //reverse if needed
+            if (!found_edge.Start.IsAlmostEqualTo(sharedEdge.Start))
+            {
 
-               Start = found_edge.End;
-               End = found_edge.Start;
-           }
+                Start = found_edge.End;
+                End = found_edge.Start;
+            }
+            else
+            {
+                Start = found_edge.Start;
+                End = found_edge.End;
+            }
+            return new List<Point>() { Start, End };
 
-           return new List<Point>() { Start, End };
-
-       }
-        public static Dictionary<string, object> CheckNormalConsistency(Unfold_Planar.FaceLikeEntity facetoRotate, Unfold_Planar.FaceLikeEntity referenceFace, Unfold_Planar.EdgeLikeEntity sharedEdge)
+        }
+        
+        /// <summary>
+        /// Method that returns and integer signifying if the normals between the two faces were as expected or if they were facing opposite directions as defined by the
+        /// shared edge betweeen them
+        /// </summary>
+        /// <param name="facetoRotate"></param>
+        /// <param name="referenceFace"></param>
+        /// <param name="sharedEdge"></param>
+        /// <returns></returns>
+        public static int CheckNormalConsistency(Unfold_Planar.FaceLikeEntity facetoRotate, 
+            Unfold_Planar.FaceLikeEntity referenceFace, Unfold_Planar.EdgeLikeEntity sharedEdge)
         {
             // assumption that face A is rotation face
             // and face B is reference face.
@@ -49,13 +73,17 @@ namespace Unfold
             //C is a vert on B
             //Point A is the start point of the shared edge
 
+            int rotFaceNormalOK = -1;
+            int refFaceNormalOK = -1;
+
+
             Curve sharedcurve = sharedEdge.Curve;
 
             Surface refsurface = referenceFace.SurfaceEntity;
             Surface rotsurface = facetoRotate.SurfaceEntity;
 
             List<Unfold_Planar.EdgeLikeEntity> refedegs = referenceFace.EdgeLikeEntities;
-            List<Unfold_Planar.EdgeLikeEntity> rotedges = referenceFace.EdgeLikeEntities;
+            List<Unfold_Planar.EdgeLikeEntity> rotedges = facetoRotate.EdgeLikeEntities;
 
             Vector AB = Vector.ByTwoPoints(sharedcurve.StartPoint, sharedcurve.EndPoint); //Edge from A TO B// this is the shared edge
             Vector ABnorm = AB.Normalized();
@@ -72,8 +100,66 @@ namespace Unfold
 
           Vector  rotFaceNormal = rotsurface.NormalAtParameter(.5, .5);
 
+
            double abxad_dot_normal_rot_face =  firstCross.Dot(rotFaceNormal);
 
+            // replace this with almost equal
+           if (abxad_dot_normal_rot_face == -1.0)
+           {
+               rotFaceNormalOK = 1;
+           }
+
+           List<Point> ACedgePoints = AlignPlanarFaces.FindPointsOfEdge_StartingAtPoint(refedegs,sharedEdge.Start , sharedEdge);
+           Point ACstart = ACedgePoints[0];
+           Point ACend = ACedgePoints[1];
+
+           Vector ACVector = Vector.ByTwoPoints(ACstart, ACend);
+           Vector ACnorm = ACVector.Normalized();
+
+           Vector secondCross = ACnorm.Cross(ABnorm);
+
+           Vector refFaceNormal = refsurface.NormalAtParameter(.5, .5);
+
+           double acxab_dot_normal_ref_face = secondCross.Dot(refFaceNormal);
+
+           if (acxab_dot_normal_ref_face == -1)
+           {
+               refFaceNormalOK = 1;
+           }
+
+           int result = refFaceNormalOK * rotFaceNormalOK;
+
+           return result;
+
+        }
+        /// <summary>
+        /// method that rotates one surface to be coplanar with another, around a shared edge.
+        /// </summary>
+        /// <param name="normalconsistency"></param> des
+        /// <param name="facetoRotate"></param>
+        /// <param name="referenceFace"></param>
+        /// <param name="sharedEdge"></param>
+        /// <returns></returns>
+        public static Geometry MakeGeometryCoPlanarAroundEdge( int normalconsistency, Unfold_Planar.FaceLikeEntity facetoRotate,
+            Unfold_Planar.FaceLikeEntity referenceFace, Unfold_Planar.EdgeLikeEntity sharedEdge)
+        {
+
+            Vector rotFaceNorm = facetoRotate.SurfaceEntity.NormalAtParameter(.5,.5);
+            Vector refFaceNorm = referenceFace.SurfaceEntity.NormalAtParameter(.5,.5);
+
+            Vector BXACrossedNormals = refFaceNorm.Cross(rotFaceNorm);
+
+
+            var PlaneRotOrigin = Plane.ByOriginNormal(sharedEdge.End,BXACrossedNormals);
+
+            var radians = -1.0 * Math.Asin(BXACrossedNormals.Length) * normalconsistency;
+
+            var degrees = radians * (180 / Math.PI);
+
+            Geometry rotatedFace = facetoRotate.SurfaceEntity.Rotate(PlaneRotOrigin, degrees);
+
+            return rotatedFace;
+                
         }
     }
 }
