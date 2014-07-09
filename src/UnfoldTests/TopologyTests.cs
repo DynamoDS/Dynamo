28c6ae83bdd74b9952ce40d8999f921951d9856f
MikeKirschner <mjk.kirschner@gmail.com>
2014-07-09 19:45:43 -0400

    creating unfold test project and stubbing out some tests

---
+++ src/UnfoldTests/TopologyTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+using NUnit.Framework;
+using Autodesk.DesignScript.Interfaces;
+namespace UnfoldTests
+{
+    [TestFixture]
+    public class TopologyTests
+    {
+
+        public class InitialGraphTests
+        {
+            [Test]
+            public void GraphHasVertsForEachFace()
+            {
+                //
+            }
+
+            [Test]
+            public void EveryFaceIsReachable()
+            {
+                //
+            }
+            [Test]
+            public void NoRepeatVerts()
+            {
+                //
+
+            }
+
+
+        }
+
+        public class BFSTreeTests
+        {
+            [Test]
+            public void TreeIsAcyclic()
+            {
+                //
+            }
+
+           [Test]
+            public void TreeContainsAllFaces()
+            {
+               //
+            }
+
+            [Test]
+           public void TreeContainsNoRepeatEdges()
+           {
+                //
+           }
+
+            [Test]
+            public void AllFinishingTimesSet()
+            {
+                //
+            }
+
+        }
+
+
+    }
+}
