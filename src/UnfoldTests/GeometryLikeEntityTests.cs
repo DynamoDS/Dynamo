9a012151da7e9d90c85c8dde355c4ee1290278b7
MikeKirschner <mjk.kirschner@gmail.com>
2014-07-10 13:27:41 -0400

    moving machines

--- src/UnfoldTests/GeometryLikeEntityTests.cs
+++ src/UnfoldTests/GeometryLikeEntityTests.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+using NUnit.Framework;
+using Autodesk.DesignScript.Interfaces;
+using Unfold;
 
 namespace UnfoldTests
 {   
-    class GeometryLikeEntityTests
+
+    /// <summary>
+    /// This text fixture will run tests to assert that geometry wrapper types implement hashing and equality correctly.
+    /// </summary>
+    [TestFixture]
+    
+   public static class GeometryLikeEntityTests
     {
+
+
+
+
     }
 }
