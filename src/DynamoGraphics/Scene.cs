1564c9dfefa2f115995cec5c267a98701aac1a60
Benglin (Ben) Goh <benglin@outlook.com>
2014-07-14 16:46:05 +0800

    Added "DynamoGraphics" project with some empty classes

---
+++ src/DynamoGraphics/Scene.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.Graphics
+{
+    public class NodeData
+    {
+    }
+
+    public class Scene
+    {
+        #region Private Class Data Members
+
+        // There can only be one scene in a session.
+        private static Scene currentScene = null;
+
+        // Instance class data members.
+        private Visualizer visualizer = null;
+        private Dictionary<string, NodeData> nodeData = null;
+
+        #endregion
+
+        #region Public Class Operational Methods
+
+        public static Scene CreateScene()
+        {
+            if (currentScene == null)
+                currentScene = new Scene();
+
+            return currentScene;
+        }
+
+        public static void DestroyScene()
+        {
+            if (currentScene != null)
+                currentScene.Destroy();
+
+            currentScene = null;
+        }
+
+        #endregion
+
+        #region Private Class Helper Methods
+
+        private Scene()
+        {
+            this.nodeData = new Dictionary<string, NodeData>();
+            this.visualizer = new Visualizer();
+        }
+
+        private void Destroy()
+        {
+            this.visualizer.Destroy();
+            this.visualizer = null;
+        }
+
+        #endregion
+    }
+}
---
+++ src/DynamoGraphics/Visualizer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.Graphics
+{
+    class Visualizer
+    {
+        #region Public Class Operational Methods
+
+        internal void Destroy()
+        {
+        }
+
+        #endregion
+    }
+}
