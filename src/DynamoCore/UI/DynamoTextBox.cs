f0f2ac018be9cd08229e7f6483508f246bbf6cfe
Anna Osyatnik <aosyatnik@sitrusllc.com.ua>
2014-07-24 11:13:05 +0300

    Initial commit for tooltip

--- src/DynamoCore/UI/DynamoTextBox.cs
+++ src/DynamoCore/UI/DynamoTextBox.cs
@@ -417,4 +417,76 @@ namespace Dynamo.UI.Controls
             set { SetValue(AttachmentSideProperty, value); }
         }
     }
+
+    public class DynamoPopup : Popup
+    {
+        public static readonly DependencyProperty AttachmentSidePopupProperty =
+            DependencyProperty.Register("AttachmentSidePopup",
+            typeof(DynamoPopup.Side), typeof(DynamoPopup),
+            new PropertyMetadata(DynamoPopup.Side.Left));
+
+        public enum Side
+        {
+            Left, Top, Right, Bottom
+        }
+
+        public Side AttachmentSide
+        {
+            get { return ((Side)GetValue(AttachmentSidePopupProperty)); }
+            set { SetValue(AttachmentSidePopupProperty, value); }
+        }
+        public DynamoPopup()
+        {
+            this.Placement = PlacementMode.Custom;
+            this.AllowsTransparency = true;
+           // this.Opacity = 0;
+            this.CustomPopupPlacementCallback = new CustomPopupPlacementCallback(PlacementCallback);           
+        }
+
+        private CustomPopupPlacement[] PlacementCallback(Size popup, Size target, Point offset)
+        {
+            double x = 0, y = 0;
+            double gap = Configurations.ToolTipTargetGapInPixels;
+            PopupPrimaryAxis primaryAxis = PopupPrimaryAxis.None;
+
+            switch (this.AttachmentSide)
+            {
+                case Side.Left:
+                    x = -(popup.Width + gap);
+                    y = (target.Height - popup.Height) * 0.5;
+                    primaryAxis = PopupPrimaryAxis.Horizontal;
+                    break;
+
+                case Side.Right:
+                    x = target.Width + 3*gap - 50;
+                    y = (target.Height - popup.Height) * 0.5;
+                    primaryAxis = PopupPrimaryAxis.Horizontal;
+                    break;
+
+                case Side.Top:
+                    x = (target.Width - popup.Width) * 0.5;
+                    y = -(popup.Height + gap);
+                    primaryAxis = PopupPrimaryAxis.Vertical;
+                    break;
+
+                case Side.Bottom:
+                    x = (target.Width - popup.Width) * 0.5;
+                    y = target.Height + gap;
+                    primaryAxis = PopupPrimaryAxis.Vertical;
+                    break;
+            }
+
+            return new CustomPopupPlacement[]
+            {
+                new CustomPopupPlacement()
+                {
+                    Point = new Point(x, y),
+                    PrimaryAxis = primaryAxis
+                }
+            };
+        }
+
+
+        
+    }
 }
