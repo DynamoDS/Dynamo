fecbe03dcb102a22bd4e58c70b39f380ed2eec04
Michael Kirschner <mjk.kirschner@gmail.com>
2014-07-27 23:01:14 -0400

    debug commit - adding BFS method to unfold dynamo nodes to debug BFS traversal

--- src/Libraries/CoreNodes/Unfold.cs
+++ src/Libraries/CoreNodes/Unfold.cs
@@ -39,7 +39,23 @@ namespace DSCore
             var unfoldsurfaces = UnfoldPlanar.PlanarUnfolder.DSPLanarUnfold(trisurfaces);
             return unfoldsurfaces;
         }
-       
+
+        public static object BFSTestTesselation(List<Surface> surfaces)
+        {
+
+            //handle tesselation here
+            var pointtuples = Tessellate.Tesselate(surfaces);
+            //convert triangles to surfaces
+            List<Surface> trisurfaces = pointtuples.Select(x => Surface.ByPerimeterPoints(new List<Point>() { x[0], x[1], x[2] })).ToList();
+
+            var graph = UnfoldPlanar.ModelTopology.GenerateTopologyFromSurfaces(trisurfaces);
+
+            //perform BFS on the graph and get back the tree
+            var nodereturn = UnfoldPlanar.ModelGraph.BFS<UnfoldPlanar.EdgeLikeEntity, UnfoldPlanar.FaceLikeEntity>(graph);
+            object treegeo = nodereturn["tree geo"];
+
+            return treegeo;
+        }
 
 
     }
