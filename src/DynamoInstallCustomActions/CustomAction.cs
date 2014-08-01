afa3e83c19e51920bb29061a5f00c132e96e43cb
Ian Keough <ian.keough@autodesk.com>
2014-07-31 22:30:44 -0700

    Add a custom actions project.

---
+++ src/DynamoInstallCustomActions/CustomAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace DynamoInstallCustomActions
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CustomAction1(Session session)
+        {
+            session.Log("Begin CustomAction1");
+
+            return ActionResult.Success;
+        }
+    }
+}
