d1ebc3beb7c760252020e6d4a7c618b9062071b5
Michael Kirschner <mjk.kirschner@gmail.com>
2014-08-02 01:13:23 -0400

    geometry for text is created when text objects come back as polylines, might need dynamic dispatch for different type conversions

--- src/TextAsGeometry/StringToGeometry.cs
+++ src/TextAsGeometry/StringToGeometry.cs
@@ -15,20 +15,42 @@ namespace TextAsGeometry
 {
     public class StringToGeometry
     {
-        private Autodesk.DesignScript.Geometry.Line LineSegmentsToProtoLine (LineSegment lineseg1, LineSegment lineseg2){
 
-        var startwp = lineseg1.Point;
-        var endwp = lineseg2.Point;
 
-            var start = Autodesk.DesignScript.Geometry.Point.ByCoordinates(startwp.X,startwp.Y,0);
+        private static Autodesk.DesignScript.Geometry.Point WindowsPoint2DSPoint (System.Windows.Point point){
 
-            var end = Autodesk.DesignScript.Geometry.Point.ByCoordinates(endwp.X,endwp.Y,0);
+            var result = Autodesk.DesignScript.Geometry.Point.ByCoordinates(point.X, point.Y, 0);
+            return result;
+        }
+
+        private static Autodesk.DesignScript.Geometry.PolyCurve PolyLineSegmentsToProtoLine(PolyLineSegment polyline)
+        {
+
+            var points = polyline.Points;
+
+            var convertedPoints = points.Select(x => WindowsPoint2DSPoint(x)).ToList();
+
+            return Autodesk.DesignScript.Geometry.PolyCurve.ByPoints(convertedPoints);
+          
+
+        }
+
+
+        private static Autodesk.DesignScript.Geometry.Line LineSegmentsToProtoLine(LineSegment lineseg1, LineSegment lineseg2)
+        {
+
+            var startwp = lineseg1.Point;
+            var endwp = lineseg2.Point;
+
+            var start = Autodesk.DesignScript.Geometry.Point.ByCoordinates(startwp.X, startwp.Y, 0);
+
+            var end = Autodesk.DesignScript.Geometry.Point.ByCoordinates(endwp.X, endwp.Y, 0);
 
             return Autodesk.DesignScript.Geometry.Line.ByStartPointEndPoint(start, end);
 
-    }
+        }
 
-        private System.Windows.Media.Geometry TextGeoFromString(string inputstring)
+        public static System.Windows.Media.Geometry TextGeoFromString(string inputstring)
         {
 
             System.Windows.FontStyle fontStyle = FontStyles.Normal;
@@ -50,22 +72,42 @@ namespace TextAsGeometry
 
 
 
-        private List<List<Autodesk.DesignScript.Geometry.Line>> TextGeoToProtoGeo(System.Windows.Media.Geometry textGeometry)
+        public static List<Autodesk.DesignScript.Geometry.PolyCurve> TextGeoToProtoGeo(System.Windows.Media.Geometry textGeometry)
         {
+            PathFigureCollection pfc = null;
+
+            // Update UI component here
+
+
             // flatten the geometry
-            var flatten = textGeometry.GetFlattenedPathGeometry();
+            var copy = textGeometry.Clone();
+            var flatten = copy.GetFlattenedPathGeometry();
             // get the figure collection from the flattened geometry
-            PathFigureCollection pfc = flatten.Figures;
+            pfc = flatten.Figures;
             //iterate all path figures
 
             // convert all the linesegments into protolines nested foreach figure
 
-           var  lineCollector = new List<List<LineSegment>>();
 
-            lineCollector = pfc.Select(pf => pf.Segments.OfType<LineSegment>().ToList()).ToList();
+            var lineCollector = new List<List<PolyLineSegment>>();
 
-            var zipped = lineCollector.Select(x=>x.Zip(x.Skip(1), LineSegmentsToProtoLine).ToList()).ToList();
+            foreach (PathFigure pf in pfc)
+            {
+                var figure = new List<PolyLineSegment>();
+                foreach (PathSegment ps in pf.Segments)
+                {
+                    
+                    if (ps is PolyLineSegment)
+                    {
+                        figure.Add(ps as PolyLineSegment);
+                    }
+                }
+                lineCollector.Add(figure);
+            }
 
+            //var zipped = lineCollector.Select(x => x.Zip(x.Skip(1), LineSegmentsToProtoLine).ToList()).ToList();
+
+            var zipped = lineCollector.Select(x => PolyLineSegmentsToProtoLine(x.First())).ToList();
 
             return zipped;
 
