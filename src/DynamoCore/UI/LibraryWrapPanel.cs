046f005e0ca41c8d1763df6068daf2433477b9a3
vmoyseenko <vmoyseenko@sitrusllc.com.ua>
2014-08-08 22:00:31 +0300

    Adding LibraryView prototype files into DynamoCore project

---
+++ src/DynamoCore/UI/LibraryWrapPanel.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Dynamo.Models;
+
+namespace Dynamo.Controls
+{
+    public class LibraryWrapPanel : WrapPanel
+    {
+        private int selectedItemIndex = -1;
+        private const double ClassObjectWidth = 96.0;
+        private const double ClassObjectHeight = 96.0;
+        private ObservableCollection<ClassObjectBase> collection;
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            // ListView should never be null.
+            var classListView = FindTypedParent<ListView>(this);
+            collection = classListView.ItemsSource as ObservableCollection<ClassObjectBase>;
+            classListView.SelectionChanged += OnClassViewSelectionChanged;
+
+            base.OnInitialized(e);
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            if (sizeInfo.WidthChanged) // Only recorder when width changes.
+                OrderListItems();
+
+            base.OnRenderSizeChanged(sizeInfo);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (UIElement child in this.Children)
+                child.Measure(availableSize);
+
+            return new Size(100, 100);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if (this.Children == null || this.Children.Count == 0)
+                return finalSize;
+
+            double x = 0, y = 0, currentRowHeight = 0;
+            foreach (UIElement child in this.Children)
+            {
+                var desiredSize = child.DesiredSize;
+                if ((x + desiredSize.Width) > finalSize.Width)
+                {
+                    x = 0;
+                    y = y + currentRowHeight;
+                    currentRowHeight = 0;
+                }
+
+                child.Arrange(new Rect(x, y, desiredSize.Width, desiredSize.Height));
+                x = x + desiredSize.Width;
+                if (desiredSize.Height > currentRowHeight)
+                    currentRowHeight = desiredSize.Height;
+            }
+
+            return finalSize;
+        }
+
+        protected override bool HasLogicalOrientation
+        {
+            get { return false; } // Arrange items in two dimension.
+        }
+
+        private static T FindTypedParent<T>(DependencyObject initial)
+            where T : DependencyObject
+        {
+            DependencyObject current = initial;
+
+            while (current != null)
+            {
+                if (current.GetType() == typeof(T))
+                    return current as T;
+
+                current = VisualTreeHelper.GetParent(current);
+            }
+
+            return null;
+        }
+
+        private void OnClassViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var index = ((sender as ListView).SelectedIndex);
+            selectedItemIndex = TranslateSelectionIndex(index);
+            OrderListItems(); // Selection change, we may need to reorder items.
+        }
+
+        private int TranslateSelectionIndex(int selection)
+        {
+            if (selection < GetClassDetailsIndex())
+                return selection;
+
+            return selection - 1;
+        }
+
+        private int GetClassDetailsIndex()
+        {
+            var query = collection.Select(c => c).Where(c => c is ClassDetails);
+            var classObjectBase = query.ElementAt(0);
+            return collection.IndexOf(classObjectBase);
+        }
+
+        private void OrderListItems()
+        {
+            if (double.IsNaN(this.ActualWidth))
+                return;
+            if (collection == null || (collection.Count <= 1))
+                return;
+
+            // Find out where ClassDetails object is positioned in collection.
+            var currentClassDetailsIndex = GetClassDetailsIndex();
+            var classObjectBase = collection[currentClassDetailsIndex];
+
+            // If there is no selection, then mark the class details as hidden.
+            var classDetails = classObjectBase as ClassDetails;
+            if (classDetails != null && (selectedItemIndex == -1))
+            {
+                classDetails.ClassDetailsVisibility = Visibility.Collapsed;
+                return;
+            }
+
+            // Otherwise, if we get here it means the class details is shown!
+            classDetails.ClassDetailsVisibility = Visibility.Visible;
+
+            // When we know the number of items on a single row, through selected 
+            // item index we will find out where the expanded StandardPanel sit.
+            var itemsPerRow = ((int)Math.Floor(ActualWidth / ClassObjectWidth));
+            var d = ((double)selectedItemIndex) / itemsPerRow;
+            var selectedItemRow = ((int)Math.Floor(d));
+
+            // Calculate the correct index where ClassDetails object should go 
+            // in the collection. If the selected item is on the first row (i.e. 
+            // row #0), then the ClassDetails object should be at the index 
+            // 'itemsPerRow'. Similarly, if the selected item is on row #N, then
+            // ClassDetails object should be at the index '(N + 1) * itemsPerRow'.
+            var correctClassDetailsIndex = ((selectedItemRow + 1) * itemsPerRow);
+
+            if (correctClassDetailsIndex == currentClassDetailsIndex)
+                return; // No repositioning is required.
+
+            // We need to move the ClassDetails object to the right index.
+            classObjectBase = collection[currentClassDetailsIndex];
+            collection.RemoveAt(currentClassDetailsIndex);
+            collection.Insert(correctClassDetailsIndex, classObjectBase);
+        }
+    }
+}
