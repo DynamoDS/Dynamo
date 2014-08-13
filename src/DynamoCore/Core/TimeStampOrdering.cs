9e57f1bec77c31c195c8af56122f3dfdb136da06
Benglin (Ben) Goh <benglin@outlook.com>
2014-08-13 11:24:17 +0800

    Implemented a new "TimeStamp" struct for time keeping.

--- src/DynamoCore/Core/TimeStampOrdering.cs
+++ src/DynamoCore/Core/TimeStampOrdering.cs
@@ -1,8 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+using System;
+using System.Diagnostics;
+
 using Dynamo.Interfaces;
+
+#if true
+
+namespace Dynamo.Core.Threading
+{
+    public struct TimeStamp
+    {
+        /// <summary>
+        /// The unique identifier of the time-stamp. This identifier is 
+        /// generated from a centralized TimeStampGenerator class that ensures 
+        /// the identifier is unique for each generated TimeStamp object.
+        /// </summary>
+        private readonly long identifier;
+
+        /// <summary>
+        /// The high resolution tick count obtained from Stopwatch.GetTimestamp.
+        /// This tick count is different from 'identifier' in the way that this 
+        /// represents real elapsed 
+        /// </summary>
+        private readonly long tickCount;
+
+        internal TimeStamp(long identifier)
+        {
+            this.tickCount = Stopwatch.GetTimestamp();
+            this.identifier = identifier;
+        }
+
+        internal long Identifier
+        {
+            get { return identifier; }
+        }
+
+        internal long TickCount
+        {
+            get { return tickCount; }
+        }
+
+        /// <summary>
+        /// The public usage of time stamps should be restricted to these
+        /// methods used to ensure an ordering on timestamps
+        /// </summary>
+        /// <param name="timeStamp0">The first time stamp in comparison.</param>
+        /// <param name="timeStamp1">The second time stamp in comparison.</param>
+        /// <returns>Return true if the first time stamp was created later than 
+        /// the second time steamp, or false otherwise.</returns>
+        /// 
+        public static bool operator >(TimeStamp timeStamp0, TimeStamp timeStamp1)
+        {
+            return timeStamp0.identifier > timeStamp1.identifier;
+        }
+
+        /// <summary>
+        /// The public usage of time stamps should be restricted to these
+        /// methods used to ensure an ordering on timestamps
+        /// </summary>
+        /// <param name="timeStamp0">The first time stamp in comparison.</param>
+        /// <param name="timeStamp1">The second time stamp in comparison.</param>
+        /// <returns>Return true if the first time stamp was created earlier than 
+        /// the second time steamp, or false otherwise.</returns>
+        /// 
+        public static bool operator <(TimeStamp timeStamp0, TimeStamp timeStamp1)
+        {
+            return timeStamp0.identifier < timeStamp1.identifier;
+        }
+    }
+}
+
+#else
 
 namespace Dynamo.Core
 {
@@ -149,3 +217,5 @@ namespace Dynamo.Core
         }
     }
 }
+
+#endif
