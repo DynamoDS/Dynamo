5152cd1758ad93c622bf738debee4d38a184b349
Benglin (Ben) Goh <benglin@outlook.com>
2014-08-14 21:54:02 +0800

    Added skeleton implementation of "UpdateGraphAsyncTask" class

---
+++ src/DynamoCore/Core/Threading/UpdateGraphAsyncTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Dynamo.DSEngine;
+using Dynamo.Models;
+
+namespace Dynamo.Core.Threading
+{
+    class UpdateGraphAsyncTask : AsyncTask
+    {
+        internal UpdateGraphAsyncTask(DynamoScheduler scheduler, Action<AsyncTask> callback)
+            : base(scheduler, callback)
+        {
+        }
+
+        internal void Initialize(EngineController controller, IEnumerable<NodeModel> updatedNodes)
+        {
+        }
+
+        protected override void ExecuteCore()
+        {
+        }
+
+        protected override void HandleTaskCompletionCore()
+        {
+        }
+    }
+}
