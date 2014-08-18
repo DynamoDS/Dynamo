1a6deeb581971b4831f8be0a4c9f2605bc3a82a4
Benglin (Ben) Goh <benglin@outlook.com>
2014-08-18 22:35:15 +0800

    Implemented "UpdateGraphAsyncTask" class

--- src/DynamoCore/Core/Threading/UpdateGraphAsyncTask.cs
+++ src/DynamoCore/Core/Threading/UpdateGraphAsyncTask.cs
@@ -6,25 +6,85 @@ using System.Text;
 using Dynamo.DSEngine;
 using Dynamo.Models;
 
+using GraphLayout;
+
+using ProtoCore.BuildData;
+
+using ProtoScript.Runners;
+
+using BuildWarning = ProtoCore.BuildData.WarningEntry;
+using RuntimeWarning = ProtoCore.RuntimeData.WarningEntry;
+
 namespace Dynamo.Core.Threading
 {
+#if ENABLE_DYNAMO_SCHEDULER
+
     class UpdateGraphAsyncTask : AsyncTask
     {
+        private GraphSyncData graphSyncData;
+        private EngineController engineController;
+
+        #region Public Class Operational Methods
+
         internal UpdateGraphAsyncTask(DynamoScheduler scheduler, Action<AsyncTask> callback)
             : base(scheduler, callback)
         {
         }
 
-        internal void Initialize(EngineController controller, IEnumerable<NodeModel> updatedNodes)
+        /// <summary>
+        /// This method is called by codes that intent to start a graph update.
+        /// This method is called on the main thread where node collection in a 
+        /// WorkspaceModel can be safely accessed.
+        /// </summary>
+        /// <param name="controller">Reference to an instance of EngineController 
+        /// to assist in generating GraphSyncData object for the given set of nodes.
+        /// </param>
+        /// <param name="updatedNodes">The list of nodes that were updated so their 
+        /// GraphSyncData can be generated.</param>
+        /// <returns>Returns true if there is any GraphSyncData, or false otherwise
+        /// (in which case there will be no need to schedule UpdateGraphAsyncTask 
+        /// for execution).</returns>
+        /// 
+        internal bool Initialize(EngineController controller, IEnumerable<NodeModel> updatedNodes)
         {
+            try
+            {
+                engineController = controller;
+                graphSyncData = engineController.ComputeSyncData(updatedNodes);
+                return graphSyncData != null;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
+
+        #endregion
+
+        #region Protected Overridable Methods
 
         protected override void ExecuteCore()
         {
+            // Updating graph in the context of ISchedulerThread.
+            engineController.UpdateGraphImmediate(graphSyncData);
         }
 
         protected override void HandleTaskCompletionCore()
         {
+            // Retrieve warnings in the context of ISchedulerThread.
+            BuildWarnings = engineController.GetBuildWarnings();
+            RuntimeWarnings = engineController.GetRuntimeWarnings();
         }
+
+        #endregion
+
+        #region Public Class Properties
+
+        internal IDictionary<Guid, List<BuildWarning>> BuildWarnings { get; private set; }
+        internal IDictionary<Guid, List<RuntimeWarning>> RuntimeWarnings { get; private set; }
+
+        #endregion
     }
+
+#endif
 }
