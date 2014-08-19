8b5a469852ddf75ba0d92e4547ef26c6e4744685
Vladimir Moyseenko <vmoyseenko@sitrusllc.com.ua>
2014-08-19 13:11:40 +0300

    Added Library header and search functionality to LibraryViewContainer

--- src/DynamoCore/UI/Views/DynamoView.xaml.cs
+++ src/DynamoCore/UI/Views/DynamoView.xaml.cs
@@ -300,7 +300,9 @@ namespace Dynamo.Controls
 #endif
             #region Search initialization
 #if ENABLE_NEW_LIBRARY_VIEW
-            var search = new LibraryContainerView();
+            var search = new LibraryContainerView(
+                this.dynamoViewModel.SearchViewModel,
+                this.dynamoViewModel);
             sidebarGrid.Children.Add(search);
             this.dynamoViewModel.SearchViewModel.Visible = true;
 #else
@@ -1005,25 +1007,25 @@ namespace Dynamo.Controls
             collapseIcon.Source = hover;
         }
 
-		private void Button_Click(object sender, EventArgs e)
+        private void Button_Click(object sender, EventArgs e)
         {
-            SearchView sv = (SearchView)this.sidebarGrid.Children[0];
-            if (sv.Visibility == Visibility.Collapsed)
+            UserControl view = (UserControl)this.sidebarGrid.Children[0];
+            if (view.Visibility == Visibility.Collapsed)
             {
                 //this.sidebarGrid.Width = restoreWidth;
-                sv.Width = double.NaN;
-                sv.HorizontalAlignment = HorizontalAlignment.Stretch;
-                sv.Height = double.NaN;
-                sv.VerticalAlignment = VerticalAlignment.Stretch;
+                view.Width = double.NaN;
+                view.HorizontalAlignment = HorizontalAlignment.Stretch;
+                view.Height = double.NaN;
+                view.VerticalAlignment = VerticalAlignment.Stretch;
 
                 this.mainGrid.ColumnDefinitions[0].Width = new System.Windows.GridLength(restoreWidth);
                 this.verticalSplitter.Visibility = Visibility.Visible;
-                sv.Visibility = Visibility.Visible;
+                view.Visibility = Visibility.Visible;
                 this.sidebarGrid.Visibility = Visibility.Visible;
                 this.collapsedSidebar.Visibility = Visibility.Collapsed;
             }
-            //SearchView sv = (SearchView)this.sidebarGrid.Children[0];
-            //sv.Width = double.NaN;
+            //UserControl view = (UserControl)this.sidebarGrid.Children[0];
+            //view.Width = double.NaN;
             //this.sidebarGrid.Width = 250;
             //this.collapsedSidebar.Visibility = Visibility.Collapsed;
         }
@@ -1054,10 +1056,10 @@ namespace Dynamo.Controls
             this.mainGrid.ColumnDefinitions[0].Width = new System.Windows.GridLength(0.0);
             this.verticalSplitter.Visibility = System.Windows.Visibility.Collapsed;
             this.sidebarGrid.Visibility = System.Windows.Visibility.Collapsed;
-            
+
             this.horizontalSplitter.Width = double.NaN;
-            SearchView sv = (SearchView)this.sidebarGrid.Children[0];
-            sv.Visibility = Visibility.Collapsed;
+            UserControl view = (UserControl)this.sidebarGrid.Children[0];
+            view.Visibility = Visibility.Collapsed;
 
             this.sidebarGrid.Visibility = Visibility.Collapsed;
             this.collapsedSidebar.Visibility = Visibility.Visible;
--- src/DynamoCore/UI/Views/LibraryContainerView.xaml.cs
+++ src/DynamoCore/UI/Views/LibraryContainerView.xaml.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Dynamo.Selection;
+using Dynamo.Utilities;
+using Dynamo.ViewModels;
 
 namespace Dynamo.UI.Views
 {
@@ -19,9 +16,291 @@ namespace Dynamo.UI.Views
     /// </summary>
     public partial class LibraryContainerView : UserControl
     {
-        public LibraryContainerView()
+        //TODO: use LibraryContainerViewModel when it will be ready
+        private readonly SearchViewModel viewModel;
+        private readonly DynamoViewModel dynamoViewModel;
+
+        public LibraryContainerView(SearchViewModel searchViewModel, DynamoViewModel dynamoViewModel)
         {
+            this.viewModel = searchViewModel;
+            this.dynamoViewModel = dynamoViewModel;
+
             InitializeComponent();
+
+            this.Loaded += LibraryContainerViewLoaded;
+            this.Dispatcher.ShutdownStarted += DispatcherShutdownStarted;
+        }
+
+        #region LibraryContainerView event handlers
+
+        private void LibraryContainerViewLoaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = this.viewModel;
+
+            this.MouseEnter += LibraryContainerViewMouseEnter;
+            this.MouseLeave += LibraryContainerViewMouseLeave;
+            this.PreviewKeyDown += LibraryContainerViewKeyHandler;
+
+            this.viewModel.RequestFocusSearch += SearchViewModelRequestFocusSearch;
+            this.viewModel.RequestReturnFocusToSearch += SearchViewModelRequestReturnFocusToSearch;
+        }
+
+        private void LibraryContainerViewMouseLeave(object sender, MouseEventArgs e)
+        {
+            viewModel.SearchScrollBarVisibility = false;
+        }
+
+        private void LibraryContainerViewMouseEnter(object sender, MouseEventArgs e)
+        {
+            viewModel.SearchScrollBarVisibility = true;
+        }
+
+        /// <summary>
+        ///     A KeyHandler method used by SearchView, increments decrements and executes based on input.
+        /// </summary>
+        /// <param name="sender">Originating object for the KeyHandler </param>
+        /// <param name="e">Parameters describing the key push</param>
+        private void LibraryContainerViewKeyHandler(object sender, KeyEventArgs e)
+        {
+
+            // ignore the key command if modifiers are present
+            if (e.KeyboardDevice.IsKeyDown(Key.LeftCtrl) ||
+                e.KeyboardDevice.IsKeyDown(Key.RightCtrl) ||
+                e.KeyboardDevice.IsKeyDown(Key.LeftAlt) ||
+                e.KeyboardDevice.IsKeyDown(Key.RightAlt))
+            {
+                return;
+            }
+
+            switch (e.Key)
+            {
+                case Key.Return:
+                    viewModel.Execute();
+                    break;
+
+                case Key.Delete:
+                    if (DynamoSelection.Instance.Selection.Count > 0)
+                    {
+                        e.Handled = true;
+                        this.dynamoViewModel.DeleteCommand.Execute(null);
+                    }
+
+                    //if there are no nodes being selected, the delete key should 
+                    //delete the text in the search box of library preview
+                    else
+                    {
+
+                        //if there is no text, then jump out of the switch
+                        if (String.IsNullOrEmpty(SearchTextBox.Text))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            int cursorPosition = SearchTextBox.SelectionStart;
+                            string searchBoxText = SearchTextBox.Text;
+
+                            //if some piece of text is seleceted by users.
+                            //delete this piece of text
+                            if (SearchTextBox.SelectedText != "")
+                            {
+                                searchBoxText = searchBoxText.Remove(cursorPosition,
+                                    SearchTextBox.SelectionLength);
+                            }
+
+                            //if there is no text selected, delete the character after the cursor
+                            else
+                            {
+
+                                //the cursor is at the end of this text string
+                                if (cursorPosition == searchBoxText.Length)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    searchBoxText = searchBoxText.Remove(cursorPosition, 1);
+                                }
+                            }
+
+                            //update the SearchTextBox's text and the cursor position
+                            SearchTextBox.Text = searchBoxText;
+                            SearchTextBox.SelectionStart = cursorPosition;
+                        }
+                    }
+                    break;
+
+                case Key.Tab:
+                    viewModel.PopulateSearchTextWithSelectedResult();
+                    break;
+
+                case Key.Down:
+                    viewModel.SelectNext();
+                    break;
+
+                case Key.Up:
+                    viewModel.SelectPrevious();
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region SearchViewModel event handlers
+
+        private void SearchViewModelRequestFocusSearch(object sender, EventArgs e)
+        {
+            SearchTextBox.Focus();
+        }
+
+        private void SearchViewModelRequestReturnFocusToSearch(object sender, EventArgs e)
+        {
+            if (this.Visibility != Visibility.Collapsed)
+                Keyboard.Focus(SearchTextBox);
+            else
+                MoveFocusToNextUIElement();
+        }
+
+        #endregion
+
+        #region Library Expander event handlers
+
+        private void LibraryExpanderMouseEnter(object sender, MouseEventArgs e)
+        {
+            Button b = (Button)sender;
+            Grid g = (Grid)b.Parent;
+            Label lb = (Label)(g.Children[0]);
+            var bc = new BrushConverter();
+            lb.Foreground = (Brush)bc.ConvertFromString("#cccccc");
+            Image collapsestate = (Image)(b).Content;
+            var collapsestateSource = new Uri(@"pack://application:,,,/DynamoCore;component/UI/Images/expand_hover.png");
+            BitmapImage bmi = new BitmapImage(collapsestateSource);
+            RotateTransform rotateTransform = new RotateTransform(-90, 16, 16);
+            collapsestate.Source = new BitmapImage(collapsestateSource);
+
+            this.Cursor = CursorLibrary.GetCursor(CursorSet.LinkSelect);
+        }
+
+        private void LibraryExpanderMouseLeave(object sender, MouseEventArgs e)
+        {
+            Button b = (Button)sender;
+            Grid g = (Grid)b.Parent;
+            Label lb = (Label)(g.Children[0]);
+            var bc = new BrushConverter();
+            lb.Foreground = (Brush)bc.ConvertFromString("#aaaaaa");
+            Image collapsestate = (Image)(b).Content;
+            var collapsestateSource = new Uri(@"pack://application:,,,/DynamoCore;component/UI/Images/expand_normal.png");
+            collapsestate.Source = new BitmapImage(collapsestateSource);
+
+            this.Cursor = null;
+        }
+
+        private void LibraryExpanderClick(object sender, RoutedEventArgs e)
+        {
+            this.dynamoViewModel.OnSidebarClosed(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region SearchTextBox Grid event handlers
+
+        private void SearchTextBoxGridMouseEnter(object sender, MouseEventArgs e)
+        {
+            var searchIconSource = new Uri(@"pack://application:,,,/DynamoCore;component/UI/Images/search_hover.png");
+            SearchIcon.Source = new BitmapImage(searchIconSource);
+        }
+
+        private void SearchTextBoxGridMouseLeave(object sender, MouseEventArgs e)
+        {
+            var searchIconSource = new Uri(@"pack://application:,,,/DynamoCore;component/UI/Images/search_normal.png");
+            SearchIcon.Source = new BitmapImage(searchIconSource);
+        }
+
+        #endregion
+
+        #region SearchTextBox event handlers
+
+        private void SearchTextBoxIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (SearchTextBox.IsVisible)
+            {
+                this.viewModel.SearchCommand.Execute(null);
+                Keyboard.Focus(this.SearchTextBox);
+                var view = Application.Current.MainWindow;
+                SearchTextBox.InputBindings.AddRange(view.InputBindings);
+            }
+        }
+
+        private void SearchTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            BindingExpression binding = ((TextBox)sender).GetBindingExpression(TextBox.TextProperty);
+            if (binding != null)
+                binding.UpdateSource();
+
+            this.viewModel.SearchCommand.Execute(null);
+        }
+
+        private void SearchTextBoxGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (viewModel != null)
+                viewModel.SearchIconAlignment = System.Windows.HorizontalAlignment.Left;
+        }
+
+        private void SearchTextBoxLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (viewModel != null)
+            {
+                if (string.IsNullOrEmpty(viewModel.SearchText))
+                    viewModel.SearchIconAlignment = System.Windows.HorizontalAlignment.Center;
+                else
+                    viewModel.SearchIconAlignment = System.Windows.HorizontalAlignment.Left;
+            }
+        }
+
+        private void SearchTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            bool handleIt = false;
+            e.Handled = handleIt;
+
+            if (e.Key == Key.Escape)
+            {
+                ClearSearchBox();
+            }
+        }
+
+        private void SearchTextBoxPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            bool handleIt = false;
+            e.Handled = handleIt;
+        }
+
+        #endregion
+
+        private void SearchCancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            ClearSearchBox();
+        }
+
+        private void DispatcherShutdownStarted(object sender, EventArgs e)
+        {
+            this.viewModel.RequestFocusSearch -= SearchViewModelRequestFocusSearch;
+            this.viewModel.RequestReturnFocusToSearch -= SearchViewModelRequestReturnFocusToSearch;
+        }
+
+        private void MoveFocusToNextUIElement()
+        {
+            // Gets the element with keyboard focus.
+            UIElement elementWithFocus = Keyboard.FocusedElement as UIElement;
+
+            // Change keyboard focus.
+            if (elementWithFocus != null)
+                elementWithFocus.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+        }
+
+        private void ClearSearchBox()
+        {
+            SearchTextBox.Text = "";
+            Keyboard.Focus(SearchTextBox);
         }
     }
 }
