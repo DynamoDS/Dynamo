ab7c2026e4d018ed5281da6209695372a6676a27
Benglin (Ben) Goh <benglin@outlook.com>
2014-08-26 16:17:25 +0800

    Implemented "CompileCustomNodeAsyncTask" class

---
+++ src/DynamoCore/Core/Threading/CompileCustomNodeAsyncTask.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using Dynamo.DSEngine;
+using Dynamo.Models;
+
+using ProtoCore.AST.AssociativeAST;
+
+namespace Dynamo.Core.Threading
+{
+#if ENABLE_DYNAMO_SCHEDULER
+
+    class CompileCustomNodeParams
+    {
+        internal EngineController EngineController { get; set; }
+        internal CustomNodeDefinition Definition { get; set; }
+        internal IEnumerable<string> Parameters { get; set; }
+        internal IEnumerable<AssociativeNode> Outputs { get; set; }
+    }
+
+    class CompileCustomNodeAsyncTask : AsyncTask
+    {
+        #region Public Class Operational Methods
+
+        internal CompileCustomNodeAsyncTask(DynamoScheduler scheduler, Action<AsyncTask> callback)
+            : base(scheduler, callback)
+        {
+        }
+
+        internal bool Initialize(CompileCustomNodeParams initParams)
+        {
+            return false;
+        }
+
+        #endregion
+
+        #region Protected Overridable Methods
+
+        protected override void ExecuteCore()
+        {
+        }
+
+        protected override void HandleTaskCompletionCore()
+        {
+        }
+
+        #endregion
+    }
+
+#endif
+}
