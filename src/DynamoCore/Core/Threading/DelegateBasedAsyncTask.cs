be5ef1b87508c09e51d0fdd07ca8918cad922879
Benglin (Ben) Goh <benglin@outlook.com>
2014-09-03 14:48:08 +0800

    Implemented DelegateBasedAsyncTask (replacement of IdlePromise)

---
+++ src/DynamoCore/Core/Threading/DelegateBasedAsyncTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamo.Core.Threading
+{
+    internal class DelegateBasedAsyncTask : AsyncTask
+    {
+        private Action actionToPerform;
+
+        #region Public Class Operational Methods
+
+        internal DelegateBasedAsyncTask(DynamoScheduler scheduler, Action<AsyncTask> callback)
+            : base(scheduler, callback)
+        {
+        }
+
+        internal void Initialize(Action action)
+        {
+            if (action == null)
+                throw new ArgumentNullException("action");
+
+            actionToPerform = action;
+        }
+
+        #endregion
+
+        #region Protected Overridable Methods
+
+        protected override void ExecuteCore()
+        {
+            actionToPerform();
+        }
+
+        protected override void HandleTaskCompletionCore()
+        {
+        }
+
+        #endregion
+    }
+}
