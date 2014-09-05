f2b9c921ae654e88c5972d2fd26f8683de30e1f4
aosyatnik@sitrusllc.com.ua <aosyatnik@sitrusllc.com.ua>
2014-09-05 11:58:19 +0300

    same for large icons

--- src/DynamoCore/Search/SearchElements/DSFunctionNodeSearchElement.cs
+++ src/DynamoCore/Search/SearchElements/DSFunctionNodeSearchElement.cs
@@ -62,9 +62,12 @@ namespace Dynamo.Search.SearchElements
                 }
                 case ResourceType.LargeIcon:
                 {
-                    if (this.FullCategoryName == "Operators")
-                        return FunctionDescriptor.Name;
-                    return FunctionDescriptor.QualifiedName;
+                    var name = Dynamo.Nodes.Utilities.NormalizeAsResourceName(FunctionDescriptor.QualifiedName);
+
+                    // This case for operators. Operators should use FunctionDescriptor.Name property.
+                    if (string.IsNullOrEmpty(name))
+                        name = Dynamo.Nodes.Utilities.NormalizeAsResourceName(FunctionDescriptor.Name);
+                    return name;
                 }
             }
 
