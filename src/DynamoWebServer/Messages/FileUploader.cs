885286e50c9e282a22f992c03a93c2724aa45afd
marimano <maria.manoylo@autodesk.com>
2014-09-08 14:50:48 +0300

    Save file with correct name, get it from the file content

--- src/DynamoWebServer/Messages/FileUploader.cs
+++ src/DynamoWebServer/Messages/FileUploader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using Dynamo.Models;
 using Dynamo.Utilities;
 using Dynamo.ViewModels;
@@ -22,22 +23,20 @@ namespace DynamoWebServer.Messages
             NodesToCreate = new List<NodeToCreate>();
             ConnectorsToCreate = new List<ConnectorToCreate>();
         }
+
         internal bool ProcessFileData(UploadFileMessage uploadFileMessage, DynamoViewModel dynamoViewModel)
         {
             try
             {
-                var path = Directory.GetCurrentDirectory() + "\\Uploaded";
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-                var fileName = path + "\\MyWorkspace.dyn";
+                var content = uploadFileMessage.FileContent;
+                var filePath = Path.GetTempPath() + "\\" + GetFileName(content);
+                
+                File.WriteAllBytes(filePath, content);
 
-
-                File.WriteAllBytes(fileName, uploadFileMessage.FileContent);
-
-                dynamoViewModel.ExecuteCommand(new DynamoViewModel.OpenFileCommand(fileName));
+                dynamoViewModel.ExecuteCommand(new DynamoViewModel.OpenFileCommand(filePath));
                 IsCustomNode = dynamoViewModel.Model.CurrentWorkspace is CustomNodeWorkspaceModel;
+
+                File.Delete(filePath);
 
                 NodesToCreate.Clear();
                 ConnectorsToCreate.Clear();
@@ -55,6 +54,47 @@ namespace DynamoWebServer.Messages
             var nodeToCreate = new NodeToCreate(node, data);
             NodesToCreate.Add(nodeToCreate);
             ConnectorsToCreate.AddRange(ConnectorToCreate.GetComingOutConnectors(node));
+        }
+
+        /// <summary>
+        /// Gets the file name from the file content
+        /// </summary>
+        /// <param name="content">The file content</param>
+        /// <returns>The file name with correct extension</returns>
+        private string GetFileName(byte[] content)
+        {
+            var xmlDoc = new XmlDocument();
+            using (MemoryStream ms = new MemoryStream(content))
+            {
+                xmlDoc.Load(ms);
+            }
+
+            string name = null;
+
+            var topNode = xmlDoc.GetElementsByTagName("Workspace");
+
+            // legacy support
+            if (topNode.Count == 0)
+            {
+                topNode = xmlDoc.GetElementsByTagName("dynWorkspace");
+            }
+
+            // find workspace name
+            foreach (XmlNode node in topNode)
+            {
+                foreach (XmlAttribute att in node.Attributes)
+                {
+                    if (att.Name.Equals("Name"))
+                        name = att.Value;
+                    else if (att.Name.Equals("ID"))
+                        IsCustomNode = !string.IsNullOrEmpty(att.Value);
+                }
+            }
+
+            if (!IsCustomNode)
+                return "Home.dyn";
+            else
+                return name + ".dyf";
         }
     }
 }
