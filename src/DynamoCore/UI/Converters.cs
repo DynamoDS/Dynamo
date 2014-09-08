0ea643012009a99622f1e7b04e1e38c6c75bde4b
aparajit <aparajit.pratap@autodesk.com>
2014-09-08 14:41:16 +0800

    Reworked Code block editor using user control to fix port alignment and watermark issues

--- src/DynamoCore/UI/Converters.cs
+++ src/DynamoCore/UI/Converters.cs
@@ -1593,4 +1593,19 @@ namespace Dynamo.Controls
             return null;
         }
     }
+
+    public class StringLengthToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty((string)value))
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
 }
