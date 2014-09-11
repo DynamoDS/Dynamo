88ce3ee85f718499e655a499f2f798ae0936173f
aparajit <aparajit.pratap@autodesk.com>
2014-09-11 11:56:25 +0800

    minor fix

--- src/DynamoCore/Models/PortModel.cs
+++ src/DynamoCore/Models/PortModel.cs
@@ -315,6 +315,7 @@ namespace Dynamo.Models
             ToolTipString = toolTipString;
             DefaultValue = defaultValue;
             VerticalMargin = 0;
+            Height = 0;
         }
 
         public bool HasDefaultValue
