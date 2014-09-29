a171be854cbe7960781115b342599fd510a71fbe
aparajit <aparajit.pratap@autodesk.com>
2014-09-29 13:05:49 +0800

    Moved static highlighting rule function to CodeBlockUtils class

--- src/DynamoCore/UI/Controls/CodeBlockEditor.xaml.cs
+++ src/DynamoCore/UI/Controls/CodeBlockEditor.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Dynamo.Nodes;
+using Dynamo.Utilities;
 using Dynamo.ViewModels;
 using ICSharpCode.AvalonEdit;
 using ICSharpCode.AvalonEdit.CodeCompletion;
@@ -51,35 +52,6 @@ namespace Dynamo.UI.Controls
             this.Loaded += (obj, args) => this.InnerTextEditor.TextArea.Focus();
 
             InitializeSyntaxHighlighter();
-        }
-
-        public static HighlightingRule CreateDigitRule()
-        {
-            var digitRule = new HighlightingRule();
-
-            Color color = (Color)ColorConverter.ConvertFromString("#2585E5");
-            digitRule.Color = new HighlightingColor()
-            {
-                Foreground = new CustomizedBrush(color)
-            };
-
-            // These Regex's must match with the grammars in the DS ATG for digits
-            // Refer to the 'number' and 'float' tokens in Start.atg
-            //*******************************************************************************
-            // number = digit {digit} .
-            // float = digit {digit} '.' digit {digit} [('E' | 'e') ['+'|'-'] digit {digit}].
-            //*******************************************************************************
-
-            string digit = @"(-?\b\d+)";
-            string floatingPoint = @"(\.[0-9]+)";
-            string numberWithOptionalDecimal = digit + floatingPoint + "?";
-
-            string exponent = @"([eE][+-]?[0-9]+)";
-            string numberWithExponent = digit + floatingPoint + exponent;
-
-            digitRule.Regex = new Regex(numberWithExponent + "|" + numberWithOptionalDecimal);
-
-            return digitRule;
         }
 
         #region Generic Properties
@@ -154,7 +126,7 @@ namespace Dynamo.UI.Controls
 
             // Highlighting Digits
             var rules = this.InnerTextEditor.SyntaxHighlighting.MainRuleSet.Rules;
-            rules.Add(CreateDigitRule());
+            rules.Add(CodeBlockUtils.CreateDigitRule());
         }
 
         private void OnRequestReturnFocusToSearch()
