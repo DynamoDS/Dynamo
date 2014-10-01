87d273311c2b73dbc7319929a6a90214638b7e33
Ian Keough <ian.keough@autodesk.com>
2014-10-01 14:41:10 -0700

    Add testable parsing methods.

--- test/DynamoCoreUITests/DropDownTests.cs
+++ test/DynamoCoreUITests/DropDownTests.cs
@@ -1,14 +1,8 @@
 ﻿using System.Collections.Generic;
-using System.Xml;
 
 using DSCoreNodesUI;
-using Dynamo.Models;
+
 using Dynamo.Nodes;
-
-using DynamoCoreUITests;
-
-using Microsoft.Practices.Prism;
-
 using NUnit.Framework;
 
 namespace DynamoCoreUITests
@@ -17,45 +11,63 @@ namespace DynamoCoreUITests
     public class DropDownTests : DynamoTestUIBase
     {
         [Test]
+        public void Save_NothingSelected()
+        {
+            Assert.AreEqual(
+                "-1",
+                DSDropDownBase.SaveSelectedIndex(-1, TestList()));
+        }
+
+        [Test]
         public void Save_SelectedIndex()
         {
-            //var node = new TestDropDown(ViewModel.CurrentSpace, "test");
-
-            //node.SelectedIndex = 2;
-
-            //var xmlDoc = new XmlDocument();
-            //var dynEl = xmlDoc.CreateElement(node.GetType().ToString());
-            //xmlDoc.AppendChild(dynEl);
-            //node.Save(xmlDoc, dynEl, SaveContext.File);
-
+            Assert.AreEqual(
+                "2:banana:blueberry",
+                DSDropDownBase.SaveSelectedIndex(2, TestList()));
         }
 
         [Test]
-        public void Save_SelectedIndexAndName()
+        public void Load_NothingSelected()
         {
-            //var node = new TestDropDown(ViewModel.CurrentSpace, "test");
-
-            //node.SelectedIndex = 2;
-
-            //var xmlDoc = new XmlDocument();
-            //var dynEl = xmlDoc.CreateElement(node.GetType().ToString());
-            //        xmlDoc.AppendChild(dynEl);
-            //        node.Save(xmlDoc, dynEl, SaveContext.File);
-
+            Assert.AreEqual(-1, DSDropDownBase.ParseSelectedIndex("-1", TestList()));
         }
 
         [Test]
-        public void Load_SemiColons()
+        public void Load_Selection()
         {
-            
+            Assert.AreEqual(2, DSDropDownBase.ParseSelectedIndex("2:banana:blueberry", TestList()));
         }
 
         [Test]
-        public void Load_SpecialCharacters()
+        public void Load_SelectionIndexOnly()
         {
-            
+            Assert.AreEqual(2, DSDropDownBase.ParseSelectedIndex("2", TestList()));
         }
 
+        [Test]
+        public void Load_SelectionIndexOutOfRange()
+        {
+            Assert.AreEqual(-1, DSDropDownBase.ParseSelectedIndex("12", TestList()));
+        }
+
+        [Test]
+        public void Load_SelectionIndexNoNameMatch()
+        {
+            Assert.AreEqual(-1, DSDropDownBase.ParseSelectedIndex("2:foo", TestList()));
+        }
+
+        private static List<DynamoDropDownItem> TestList()
+        {
+            var items = new List<DynamoDropDownItem>
+            {
+                new DynamoDropDownItem("cat", "cat"),
+                new DynamoDropDownItem("dog", "dog"),
+                new DynamoDropDownItem("banana:blueberry", "stuff"),
+                new DynamoDropDownItem("!@#$%%%^&*()", "craziness")
+            };
+
+            return items;
+        }
 
     }
 }
