e29edfe604b7d09a32c9e67efb26e3c1ff981354
pbidenko <pavel.bidenko@gmail.com>
2014-10-02 14:33:38 +0300

    REACH-70 Turn GetDefinitionLineIndexMap method into static helper method in CodeBlockUtils.

--- src/DynamoCore/Nodes/CodeBlockNode.cs
+++ src/DynamoCore/Nodes/CodeBlockNode.cs
@@ -128,34 +128,6 @@ namespace Dynamo.Nodes
             return -1;
         }
 
-        /// <summary>
-        /// Returns a list of defined variables, along with the line number on which 
-        /// they are defined last. A variable can be defined multiple times in a single 
-        /// code block node, but the output port is only shown on the last definition.
-        /// </summary>
-        /// <returns>Returns a map between defined variables and the line index on 
-        /// which they are defined last.</returns>
-        public IOrderedEnumerable<KeyValuePair<string, int>> GetDefinitionLineIndexMap()
-        {
-            // Get all defined variables and their locations
-            var definedVars = codeStatements.Select(s => new KeyValuePair<Variable, int>(s.FirstDefinedVariable, s.StartLine))
-                                            .Where(pair => pair.Key != null)
-                                            .Select(pair => new KeyValuePair<string, int>(pair.Key.Name, pair.Value))
-                                            .OrderBy(pair => pair.Key)
-                                            .GroupBy(pair => pair.Key);
-
-            // Calc each variable's last location of definition
-            var locationMap = new Dictionary<string, int>();
-            foreach (var defs in definedVars)
-            {
-                var name = defs.FirstOrDefault().Key;
-                var loc = defs.Select(p => p.Value).Max<int>();
-                locationMap[name] = loc;
-            }
-
-            return locationMap.OrderBy(p => p.Value);
-        }
-
         #endregion
 
         #region Properties
@@ -242,6 +214,11 @@ namespace Dynamo.Nodes
         public List<string> TempVariables
         {
             get { return tempVariables; }
+        }
+
+        public List<Statement> CodeStatements
+        {
+            get { return codeStatements; }
         }
 
         #endregion
@@ -576,7 +553,7 @@ namespace Dynamo.Nodes
 
         private void SetOutputPorts()
         {
-            var allDefs = GetDefinitionLineIndexMap();
+            var allDefs = CodeBlockUtils.GetDefinitionLineIndexMap(codeStatements);
 
             if (allDefs.Any() == false)
                 return;
