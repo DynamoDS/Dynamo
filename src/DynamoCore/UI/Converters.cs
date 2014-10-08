0f2c44ddceaa38304b016ba12c74a6a375e94501
Anna Osyatnik <anna.osyatnik@autodesk.com>
2014-10-08 11:39:01 +0300

    Binding added. Converter hasn't been done yet.

--- src/DynamoCore/UI/Converters.cs
+++ src/DynamoCore/UI/Converters.cs
@@ -1785,4 +1785,19 @@ namespace Dynamo.Controls
             throw new NotImplementedException();
         }
     }
+
+    public class DataContextToMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(
+            object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
