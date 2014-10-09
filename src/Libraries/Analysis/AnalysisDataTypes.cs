b8f5257bbad20fd0e1263b97409a1c79519e95f3
Ian Keough <ian.keough@autodesk.com>
2014-10-09 14:20:28 -0700

    Documentation for AnalysisData types.

--- src/Libraries/Analysis/AnalysisDataTypes.cs
+++ src/Libraries/Analysis/AnalysisDataTypes.cs
@@ -4,12 +4,31 @@ using Autodesk.DesignScript.Geometry;
 
 namespace Analysis
 {
+    /// <summary>
+    /// A class for storing structured surface analysis data.
+    /// </summary>
     public class SurfaceAnalysisData : ISurfaceAnalysisData<UV, double>
     {
+        /// <summary>
+        /// The surface which contains the calculation locations.
+        /// </summary>
         public Surface Surface { get; set; }
+
+        /// <summary>
+        /// A list of calculation locations.
+        /// </summary>
         public IEnumerable<UV> CalculationLocations { get; set; }
+
+        /// <summary>
+        /// A dictionary of results.
+        /// </summary>
         public Dictionary<string, IList<double>> Results { get; set; }
 
+        /// <summary>
+        /// Create a SurfaceAnalysisData object.
+        /// </summary>
+        /// <param name="surface">The surface which contains the calculation locations.</param>
+        /// <param name="calculationLocations">A set of results keyed by the name of the result type.</param>
         public SurfaceAnalysisData(Surface surface, IEnumerable<UV> calculationLocations)
         {
             Surface = surface;
@@ -17,12 +36,20 @@ namespace Analysis
             Results = new Dictionary<string, IList<double>>();
         }
 
+        /// <summary>
+        /// Create a SurfaceAnalysisData object.
+        /// </summary>
+        /// <param name="surface">The surface which contains the calculation locations.</param>
+        /// <param name="calculationLocations">UV coordinates on the surface.</param>
+        /// <param name="results">A set of results keyed by the name of the result type.</param>
         public SurfaceAnalysisData(Surface surface, IEnumerable<UV> calculationLocations, Dictionary<string, IList<double>> results)
         {
             Surface = surface;
             CalculationLocations = CullCalculationLocations(surface, calculationLocations);
             Results = results;
         }
+
+        #region private methods
 
         /// <summary>
         /// Cull calculation locations that aren't within 1e-6 of the surface.
@@ -44,19 +71,40 @@ namespace Analysis
 
             return pts;
         }
+
+        #endregion
     }
 
+    /// <summary>
+    /// A class for storing structured vector analysis data.
+    /// </summary>
     public class VectorAnalysisData : IAnalysisData<Point, Vector>
     {
-        public IEnumerable<Point> CalculationLocations { get; set; }
-        public Dictionary<string, IList<Vector>> Results { get; set; }
+        /// <summary>
+        /// A list of calculation locations.
+        /// </summary>
+        public IEnumerable<Point> CalculationLocations { get; internal set; }
 
+        /// <summary>
+        /// A dictionary of results.
+        /// </summary>
+        public Dictionary<string, IList<Vector>> Results { get; internal set; }
+
+        /// <summary>
+        /// Create a VectorAnalysisData object.
+        /// </summary>
+        /// <param name="points">A list of calculation locations.</param>
         public VectorAnalysisData(IEnumerable<Point> points)
         {
             CalculationLocations = points;
             Results = new Dictionary<string, IList<Vector>>();
         }
 
+        /// <summary>
+        /// Create a VectorAnalysisData object.
+        /// </summary>
+        /// <param name="points">A list of calculation locations.</param>
+        /// <param name="results">A set of results keyed by the name of the result type.</param>
         public VectorAnalysisData(IEnumerable<Point> points, Dictionary<string, IList<Vector>> results)
         {
             CalculationLocations = points;
@@ -64,17 +112,36 @@ namespace Analysis
         }
     }
 
+    /// <summary>
+    /// A class for storing structure point analysis data.
+    /// </summary>
     public class PointAnalysisData : IAnalysisData<Point, double>
     {
-        public IEnumerable<Point> CalculationLocations { get; set; }
-        public Dictionary<string, IList<double>> Results { get; set; }
+        /// <summary>
+        /// A list of calculation locations.
+        /// </summary>
+        public IEnumerable<Point> CalculationLocations { get; internal set; }
 
+        /// <summary>
+        /// A dictionary of results.
+        /// </summary>
+        public Dictionary<string, IList<double>> Results { get; internal set; }
+
+        /// <summary>
+        /// Create a PointAnalysisData object.
+        /// </summary>
+        /// <param name="points">A list of calculation locations.</param>
         public PointAnalysisData(IEnumerable<Point> points)
         {
             CalculationLocations = points;
             Results = new Dictionary<string, IList<double>>();
         }
 
+        /// <summary>
+        /// Create a PointAnalysisData object.
+        /// </summary>
+        /// <param name="points">A list of calculation locations.</param>
+        /// <param name="results">A set of resutls keyed by the name of the result type.</param>
         public PointAnalysisData(IEnumerable<Point> points, Dictionary<string, IList<double>> results)
         {
             CalculationLocations = points;
