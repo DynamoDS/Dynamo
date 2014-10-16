42823b5589e7c0615aec4a39bf197e1d5f0d553c
Steve Elliott <steell1890@gmail.com>
2014-10-16 11:31:40 -0400

    implemented file and directory library

--- src/Libraries/CoreNodes/Files.cs
+++ src/Libraries/CoreNodes/Files.cs
@@ -1,72 +1,231 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using Autodesk.DesignScript.Runtime;
 using System.IO;
-using System.Text;
-using System;
 
-namespace DSCore
+namespace DSCore.IO
 {
-    public static class File
+    /// <summary>
+    ///     Methods for operating on strings representing file paths.
+    /// </summary>
+    public static class FilePath
     {
         /// <summary>
-        ///     Load a bitmap from a file path.
+        ///     Combines multiple strings into a single file path.
         /// </summary>
-        /// <param name="path">The path to the image file.</param>
-        /// <returns name="bitmap">Bitmap</returns>
-        public static Bitmap LoadImageFromPath(string path)
+        /// <param name="paths">String to combine into a path.</param>
+        public static string Combine(params string[] paths)
         {
-            if (!System.IO.File.Exists(path)) return null;
+            return Path.Combine(paths);
+        }
 
-            var bmp = new Bitmap(path);
-            return bmp;
+        /// <summary>
+        ///     Gets the extension from a file path.
+        /// </summary>
+        /// <param name="path">Path to get extension of.</param>
+        public static string Extension(string path)
+        {
+            return Path.GetExtension(path);
+        }
+
+        /// <summary>
+        ///     Changes the extension of a file path.
+        /// </summary>
+        /// <param name="path">Path to change extension of.</param>
+        /// <param name="newExtension">New extension.</param>
+        public static string ChangeExtension(string path, string newExtension)
+        {
+            return Path.ChangeExtension(path, newExtension);
+        }
+
+        /// <summary>
+        ///     Gets the directory name of a file path.
+        /// </summary>
+        /// <param name="path">Path to get directory information of.</param>
+        public static string DirectoryName(string path)
+        {
+            return Path.GetDirectoryName(path);
+        }
+
+        /// <summary>
+        ///     Gets the file name of a file path.
+        /// </summary>
+        /// <param name="path">Path to get the file name of.</param>
+        /// <param name="withExtension">Determines whether or not the extension is included in the result, defaults to true.</param>
+        public static string FileName(string path, bool withExtension=true)
+        {
+            return withExtension ? Path.GetFileName(path) : Path.GetFileNameWithoutExtension(path);
+        }
+
+        /// <summary>
+        ///     Determines whether or not a file path contains an extension.
+        /// </summary>
+        /// <param name="path">Path to check for an extension.</param>
+        public static bool HasExtension(string path)
+        {
+            return Path.HasExtension(path);
+        }
+    }
+
+    /// <summary>
+    ///     Methods for operating on Image Bitmaps.
+    /// </summary>
+    public static class Image
+    {
+        /// <summary>
+        ///     Loads the file as a bitmap.
+        /// </summary>
+        /// <param name="file">File object to load image from.</param>
+        /// <returns name="bitmap">Bitmap</returns>
+        public static Bitmap FromFile(FileInfo file)
+        {
+            using (var fs = new FileStream(file.FullName, FileMode.Open))
+                return new Bitmap(System.Drawing.Image.FromStream(fs));
         }
 
         /// <summary>
         ///     Reads an image file and returns the color values at the specified grid locations.
         /// </summary>
-        /// <param name="filePath">Path to the image file.</param>
-        /// <param name="numX">Number of sample grid points in the X direction.</param>
-        /// <param name="numY">Number of sample grid points in the Y direction.</param>
+        /// <param name="image">The image to read.</param>
+        /// <param name="xSamples">Number of sample grid points in the X direction.</param>
+        /// <param name="ySamples">Number of sample grid points in the Y direction.</param>
         /// <returns name="colors">Colors at the specified grid points.</returns>
         /// <search>read,image,bitmap,png,jpg,jpeg</search>
-        public static IList ReadImage(string filePath, int numX, int numY)
+        public static IList Pixels(Bitmap image, int? xSamples=null, int? ySamples=null)
         {
-            if (System.IO.File.Exists(filePath) == false)
-                return List.Empty;
+            var numX = xSamples ?? image.Width;
+            var numY = ySamples ?? image.Height;
 
-            IList result = List.Empty;
-            var bmp = new Bitmap(filePath);
+            return
+                Enumerable.Range(0, numY)
+                    .Select(
+                        y =>
+                            Enumerable.Range(0, numX)
+                                .Select(x => 
+                                     Color.ByColor(image.GetPixel(x * (image.Width / numX), y * (image.Height / numY))))
+                                .ToList())
+                    .ToList();
+        }
 
-            for (int y = 0; y < numY; y++)
+        /// <summary>
+        ///     The height of an image.
+        /// </summary>
+        /// <param name="image">An image.</param>
+        public static int Width(Bitmap image)
+        {
+            return image.Width;
+        }
+
+        /// <summary>
+        ///     The width of an image.
+        /// </summary>
+        /// <param name="image">An image.</param>
+        public static int Height(Bitmap image)
+        {
+            return image.Height;
+        }
+
+        /// <summary>
+        ///     Write the image to a path, given the specified file name.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="image">The image to write</param>
+        /// <returns name="ok">It is successful or not.</returns>
+        /// <search>write image,image,file</search>
+        public static void WriteToFile(string path, Bitmap image)
+        {
+            image.Save(path);
+        }
+    }
+
+    /// <summary>
+    ///     Methods for working with Directories.
+    /// </summary>
+    public static class Directory
+    {
+        [IsVisibleInDynamoLibrary(false)]
+        public static DirectoryInfo FromPath(string path)
+        {
+            if (!Exists(path))
+                System.IO.Directory.CreateDirectory(path);
+            return new DirectoryInfo(path);
+        }
+        
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="newPath"></param>
+        public static void Move(string path, string newPath)
+        {
+            System.IO.Directory.Move(path, newPath);
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="recursive"></param>
+        public static void Delete(string path, bool recursive=false)
+        {
+            System.IO.Directory.Delete(path, recursive);
+        }
+
+        /// <summary>
+        ///     Gets all of the contents of a given directory.
+        /// </summary>
+        /// <param name="directory">Directory to get contents of.</param>
+        /// <param name="searchString">Search string used to filter results. Defaults to "*" (displays all contents).</param>
+        [MultiReturn("files", "directories")]
+        public static Dictionary<string, IList> Contents(DirectoryInfo directory, string searchString="*")
+        {
+            return new Dictionary<string, IList>
             {
-                for (int x = 0; x < numX; x++)
-                {
-                    int xParam = x * (bmp.Width / numX);
-                    int yParam = y * (bmp.Height / numY);
+                { "files", directory.EnumerateFiles(searchString).Select(x => x.FullName).ToList() },
+                { "directories", directory.EnumerateDirectories(searchString).Select(x => x.FullName).ToList() }
+            };
+        }
 
-                    // Insert new color at the front of the list.
-                    var c = bmp.GetPixel(xParam, yParam);
-                    result.Insert(0, Color.ByARGB(c.A,c.R,c.G,c.B));
-                }
-            }
+        /// <summary>
+        ///     Determines if a directory exists at the given path.
+        /// </summary>
+        /// <param name="path"></param>
+        public static bool Exists(string path)
+        {
+            return System.IO.Directory.Exists(path);
+        }
+    }
 
-            return result;
+    /// <summary>
+    ///     Methods for working with Files.
+    /// </summary>
+    public static class File
+    {
+        [IsVisibleInDynamoLibrary(false)]
+        public static FileInfo FromPath(string path)
+        {
+            return new FileInfo(path);
         }
 
         /// <summary>
         ///     Reads a text file and returns the contents as a string.
         /// </summary>
-        /// <param name="filePath">Path to the text file.</param>
         /// <returns name="str">Contents of the text file.</returns>
         /// <search>read file,text,file</search>
-        public static string ReadText(string filePath)
+        public static string ReadText(FileInfo file)
         {
-            if (System.IO.File.Exists(filePath) == false)
-                return string.Empty;
+            return System.IO.File.ReadAllText(file.FullName);
+        }
 
-            return System.IO.File.ReadAllText(filePath);
+        /// <summary>
+        ///     Determines if a file exists at the given path.
+        /// </summary>
+        /// <param name="path"></param>
+        public static bool Exists(string path)
+        {
+            return System.IO.File.Exists(path);
         }
 
         /// <summary>
@@ -77,80 +236,33 @@ namespace DSCore
         /// <param name="data">List of lists to write into CSV</param>
         /// <returns name="str">Contents of the text file.</returns>
         /// <search>write,text,file</search>
-        public static bool ExportToCSV(string filePath, double[][] data)
+        public static void ExportToCSV(string filePath, object[][] data)
         {
-            try
+            using (var writer = new StreamWriter(filePath))
             {
-                System.IO.StreamWriter writer = new StreamWriter(filePath);
-
-                for (int i = 0; i < data.GetLength(0); i++)
+                foreach (var line in data)
                 {
-                    StringBuilder line = new StringBuilder();
-
-                    for (int j = 0; j < data[i].Length; j++)
+                    int count = 0;
+                    foreach (var entry in line)
                     {
-                        line.Append(data[i][j]);
-                        line.Append(", ");
+                        writer.Write(entry);
+                        if (++count < line.Length)
+                            writer.Write(", ");
                     }
-
-                    string lineOut = line.ToString();
-                    lineOut = lineOut.Substring(0, lineOut.LastIndexOf(','));
-                    writer.WriteLine(lineOut);
+                    writer.WriteLine();
                 }
-                writer.Flush();
-                writer.Close();
             }
-            catch (Exception)
-            {
-                return false;
-            }
-            return true;
         }
 
         /// <summary>
         ///     Write the text content to a file specified by the path
         /// </summary>
         /// <param name="filePath">Path to write to</param>
-        /// <param name="data">Text content</param>
-        /// <returns name="ok">It is successful or not.</returns>
+        /// <param name="text">Text content</param>
         /// <search>write file,text,file</search>
-        public static bool WriteText(string filePath, string text)
+        public static void WriteText(string filePath, string text)
         {
-            try
-            {
-                System.IO.File.WriteAllText(filePath, text);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("The text is not written to the file successfully!", e);
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        ///     Write the image to a path, given the specified file name.
-        ///     The file name will be appended with .png. 
-        /// </summary>
-        /// <param name="filePath">Path to write to</param>
-        /// <param name="fileName">File name to save as</param>
-        /// <param name="image">The image to write</param>
-        /// <returns name="ok">It is successful or not.</returns>
-        /// <search>write image,image,file</search>
-        public static bool WriteImage(string filePath, string fileName, System.Drawing.Bitmap image)
-        {
-            string pathName = Path.Combine(filePath, fileName + ".png");
-
-            try
-            {
-                image.Save(pathName);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("The image is not saved successfully!", e);
-            }
-
-            return true;
+            System.IO.File.WriteAllText(filePath, text);
         }
     }
 }
