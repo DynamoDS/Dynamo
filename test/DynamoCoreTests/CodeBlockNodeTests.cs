10adbe6fda6e44ad133469ff4e649ab869912b98
aparajit <aparajit.pratap@autodesk.com>
2014-10-17 17:36:22 +0800

    Fixed autocomplete test case failures

--- test/DynamoCoreTests/CodeBlockNodeTests.cs
+++ test/DynamoCoreTests/CodeBlockNodeTests.cs
@@ -868,7 +868,7 @@ b = c[w][x][y][z];";
 
         private void AssertCompletions(IEnumerable<StaticMirror> members, string[] expected)
         {
-            var actual = members.OrderBy(n => n.Name).Select(x => x.ToString()).ToArray();
+            var actual = members.OrderBy(n => n.Name).Select(x => x.Name).ToArray();
             Assert.AreEqual(expected, actual);
         }
     }
