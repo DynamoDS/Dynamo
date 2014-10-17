80aa3d6670e4631b78efab506f5e6b171bf9d6ca
Ian Keough <ian.keough@autodesk.com>
2014-10-17 11:19:53 -0700

    Stub in tests for Analysis data types.

---
+++ test/Libraries/AnalysisTests/AnalysisTests.cs
@@ -0,0 +1,103 @@
+﻿using Analysis.DataTypes;
+
+using Autodesk.DesignScript.Geometry;
+
+using NUnit.Framework;
+
+namespace AnalysisTests
+{
+    [TestFixture]
+    public class AnalysisTests
+    {
+        [Test, Category("UnitTests")]
+        public void SurfaceAnalysisDataBySurfaceAndPoints_ValidArgs()
+        {
+            var points = new[]
+            {
+                Point.ByCoordinates(0, 0, 0),
+                Point.ByCoordinates(0, 1, 0),
+                Point.ByCoordinates(1, 1, 0),
+                Point.ByCoordinates(1, 0, 0)
+            };
+
+            var uvs = new[]
+            {
+                UV.ByCoordinates(0, 0),
+                UV.ByCoordinates(1, 1)
+            };
+
+            var srf = Surface.ByPerimeterPoints(points);
+
+            var sad = SurfaceAnalysisData.BySurfaceAndPoints(srf, uvs);
+            Assert.NotNull(sad);
+        }
+
+        [Test, Category("UnitTests")]
+        public void SurfaceAnalysisDataBySurfaceAndPoints_BadArgs()
+        {
+            
+        }
+
+        [Test, Category("UnitTests")]
+        public void SurfaceAnalysisDataBySurfacePointsAndResults_ValidArgs()
+        {
+            
+        }
+
+        [Test, Category("UnitTests")]
+        public void SurfaceAnalysisDataBySurfacePointAndResults_BadArgs()
+        {
+            
+        }
+
+        [Test, Category("UnitTests")]
+        public void PointAnalysisDataByPoints_ValidArgs()
+        {
+            
+        }
+
+        [Test, Category("UnitTests")]
+        public void PointAnalysisDataByPoints_BadArgs()
+        {
+            
+        }
+
+        [Test, Category("UnitTests")]
+        public void PointAnalysisDataByPointsAndResults_ValidArgs()
+        {
+
+        }
+
+        [Test, Category("UnitTests")]
+        public void PointAnalysisDataByPointsAndResults_BadArgs()
+        {
+
+        }
+
+        [Test, Category("UnitTests")]
+        public void VectorAnalysisDataByPoints_ValidArgs()
+        {
+            
+        }
+
+        [Test, Category("UnitTests")]
+        public void VectorAnalysisdataByPoint_BadArgs()
+        {
+            
+        }
+
+        [Test, Category("UnitTests")]
+        public void VectorAnalysisDataByPointsAndResults_ValidArgs()
+        {
+
+        }
+
+        [Test, Category("UnitTests")]
+        public void VectorAnalysisDataByPointsAndResults_BadArgs()
+        {
+
+        }
+
+
+    }
+}
