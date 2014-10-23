ace2c660eac406383387ec1c0260cd04d0a70bc9
Anna Osyatnik <anna.osyatnik@autodesk.com>
2014-10-23 16:31:15 +0300

    Added comment.

--- src/DynamoCore/UI/Views/LibrarySearchView.xaml.cs
+++ src/DynamoCore/UI/Views/LibrarySearchView.xaml.cs
@@ -245,6 +245,8 @@ namespace Dynamo.UI.Views
 
             // Show all members of this group.
             memberGroupContext.ExpandAllMembers();
+
+            // Refresh members.
             var members = WPF.FindChild<ListBox>(memberGroup, "MembersListBox");
             BindingOperations.GetBindingExpressionBase(members, ListBox.ItemsSourceProperty).UpdateTarget();
  
