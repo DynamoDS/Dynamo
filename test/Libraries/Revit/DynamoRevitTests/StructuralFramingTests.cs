f99951fb3cf7cecd6dd19a91d187e428b47a8845
Ian Keough <ian.keough@autodesk.com>
2014-10-28 10:58:27 -0700

    Add system tests for structural framing.

---
+++ test/Libraries/Revit/DynamoRevitTests/StructuralFramingTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using RTF.Framework;
+
+namespace Dynamo.Tests
+{
+    [TestFixture]
+    public class StructuralFramingTests : DynamoRevitUnitTestBase
+    {
+        [Test, TestModel(@".\StructuralFraming\StructuralFraming.rvt")]
+        public void StructuralFraming_Beam()
+        {
+            OpenAndRun(@".\StructuralFraming\Beam.dyn");
+        }
+
+        [Test, TestModel(@".\StructuralFraming\StructuralFraming.rvt")]
+        public void StructuralFraming_Brace()
+        {
+            OpenAndRun(@".\StructuralFraming\Brace.dyn");
+        }
+
+        [Test, TestModel(@".\StructuralFraming\StructuralFraming.rvt")]
+        public void StructuralFraming_Column()
+        {
+            OpenAndRun(@".\StructuralFraming\Column.dyn");
+        }
+    }
+}
