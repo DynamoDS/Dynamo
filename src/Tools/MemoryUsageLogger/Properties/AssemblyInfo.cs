5ada7564fe00869798711635772811631bcf8465
Yu Ke <yu.ke@autodesk.com>
2014-11-03 16:57:15 +0800

    Change assembly information

--- src/Tools/MemoryUsageLogger/Properties/AssemblyInfo.cs
+++ src/Tools/MemoryUsageLogger/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyProfiler")]
+[assembly: AssemblyTitle("MemoryUsageLogger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MyProfiler")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyProduct("MemoryusageLogger")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
