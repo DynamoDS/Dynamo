DynamoDS/Dynamo

commit 06bcf1128571048f25eb6b018358ee76d500e301

Fixed deadlock in Watch3DView.VisualizationManagerRenderComplete

src/DynamoCore/UI/Views/Watch3DView.xaml.cs
@@ -256,7 +256,7 @@ namespace Dynamo.Controls
         /// <param name="e"></param>
         private void VisualizationManagerRenderComplete(object sender, RenderCompletionEventArgs e)
         {
-            Dispatcher.Invoke(new Action(delegate
+            Dispatcher.BeginInvoke(new Action(delegate
             {
                 var vm = (IWatchViewModel) DataContext;
 

src/DynamoCore/VisualizationManager/VisualizationManager.cs
@@ -411,6 +411,8 @@ namespace Dynamo
         public void RequestBranchUpdate(NodeModel node)
         {
             var scheduler = dynamoModel.Scheduler;
+            if (scheduler == null) // Shutdown has begun.
+                return;
 
             // Schedule a AggregateRenderPackageAsyncTask here so that the 
             // background geometry preview gets refreshed.

commit b82aa31239115bed4274bb65a681f3619c514336

Fixed regression from 06bcf1128571048f25eb6b018358ee76d500e301

src/DynamoCore/UI/Views/Watch3DView.xaml.cs
@@ -256,15 +256,17 @@ namespace Dynamo.Controls
         /// <param name="e"></param>
         private void VisualizationManagerRenderComplete(object sender, RenderCompletionEventArgs e)
         {
-            Dispatcher.BeginInvoke(new Action(delegate
+            var executeCommand = new Action(delegate
             {
-                var vm = (IWatchViewModel) DataContext;
-
+                var vm = (IWatchViewModel)DataContext;
                 if (vm.GetBranchVisualizationCommand.CanExecute(e.TaskId))
-                {
                     vm.GetBranchVisualizationCommand.Execute(e.TaskId);
-                }
-            }));
+            });
+
+            if (CheckAccess())
+                executeCommand();
+            else
+                Dispatcher.BeginInvoke(executeCommand);
         }
 
         /// <summary>