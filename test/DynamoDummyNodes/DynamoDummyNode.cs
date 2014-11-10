dc249d8e8e0559e089a1493f2063f5957570a3e3
Yu Ke <yu.ke@autodesk.com>
2014-11-10 11:17:08 +0800

    Add DynamoDummyNodes project for UI node testing

---
+++ test/DynamoDummyNodes/DynamoDummyNode.cs
@@ -0,0 +1,29 @@
+﻿using Dynamo.Models;
+using ProtoCore.AST.AssociativeAST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamo.Nodes
+{
+    [NodeName("DynamoDummyErrorNode")]
+    [NodeCategory("DynamoDummy")]
+    [NodeDescription("Dynamo dummy node which will throuw an excpetion when is compiled to AST.")]
+    [IsDesignScriptCompatible]
+    public class DynamoDummyErrorNode: NodeModel
+    {
+        public DynamoDummyErrorNode(WorkspaceModel workspaceModel)
+            : base(workspaceModel)
+        {
+            InPortData.Add(new PortData("dummyInput", "dummy"));
+            OutPortData.Add(new PortData("dummyOutput", "Result"));
+            RegisterAllPorts();
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            throw new Exception("Dummy error in building dummy node");
+        }
+    }
+}
