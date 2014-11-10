079cb7a2504650b82f1ad5ce56462dbdf4d00df0
Anna Osyatnik <anna.osyatnik@autodesk.com>
2014-11-10 17:12:45 +0200

    Generation of inputs ad outputs parameters added.

--- src/DynamoCore/Search/SearchElements/CustomNodeSearchElement.cs
+++ src/DynamoCore/Search/SearchElements/CustomNodeSearchElement.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
+using System.Linq;
 
 using Dynamo.UI.Commands;
 using Dynamo.Utilities;
@@ -22,6 +24,80 @@ namespace Dynamo.Search.SearchElements
         }
 
         public override string Type { get { return "Custom Node"; } }
+
+        private XmlNode xmlNode;
+
+        protected override List<Tuple<string, string>> GenerateInputParameters()
+        {
+            List<Tuple<string, string>> inputPar = new List<Tuple<string, string>>();
+            List<string> inputs = new List<string>();
+
+            if (xmlNode == null)
+            {
+                xmlNode = TryToLoadXmlNode();
+
+                // If we couldn't load xml with information about node, just return none.
+                if (xmlNode == null)
+                {
+                    inputPar.Add(Tuple.Create("", "none"));
+                    return inputPar;
+                }
+            }
+
+            foreach (XmlNode elNode in xmlNode.ChildNodes)
+            {
+                foreach (var subNode in
+                    elNode.ChildNodes.Cast<XmlNode>()
+                        .Where(subNode =>
+                            ((subNode.Name == "Symbol") && 
+                            (subNode.ParentNode.Name == "Dynamo.Nodes.Symbol")||
+                            (subNode.ParentNode.Name == "Dynamo.Nodes.dynSymbol"))))
+                {
+                    inputs.Add(subNode.Attributes[0].Value);
+                }
+            }
+
+            foreach (var parameter in inputs)
+            {
+                if (parameter != string.Empty)
+                    inputPar.Add(Tuple.Create(parameter, ""));
+            }
+
+            return inputPar;
+        }
+
+        protected override List<string> GenerateOutputParameters()
+        {
+            List<string> outputPar = new List<string>();
+
+            if (xmlNode == null)
+            {
+                xmlNode = TryToLoadXmlNode();
+
+                // If we couldn't load xml with information about node, just return none.
+                if (xmlNode == null)
+                {
+                    outputPar.Add("none");
+                    return outputPar;
+                }
+            }
+
+            foreach (XmlNode elNode in xmlNode.ChildNodes)
+            {
+                foreach (var subNode in
+                    elNode.ChildNodes.Cast<XmlNode>()
+                        .Where(subNode =>
+                            ((subNode.Name == "Symbol") &&
+                            (subNode.ParentNode.Name == "Dynamo.Nodes.Output") ||
+                            (subNode.ParentNode.Name == "Dynamo.Nodes.dynOutput"))))
+                {
+                    if (subNode.Attributes[0].Value != string.Empty)
+                        outputPar.Add(subNode.Attributes[0].Value);
+                }
+            }
+
+            return outputPar;
+        }
 
         public CustomNodeSearchElement(CustomNodeInfo info, SearchElementGroup group)
             : base(info.Name, info.Description, new List<string>(), group)
@@ -67,5 +143,24 @@ namespace Dynamo.Search.SearchElements
             return other is CustomNodeSearchElement && this.Equals(other as CustomNodeSearchElement);
         }
 
+        private XmlNode TryToLoadXmlNode()
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(Path);
+                XmlNodeList elNodes = xmlDoc.GetElementsByTagName("Elements");
+
+                if (elNodes.Count == 0)
+                    elNodes = xmlDoc.GetElementsByTagName("dynElements");
+
+                XmlNode elNodesList = elNodes[0];
+                return elNodesList;
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
