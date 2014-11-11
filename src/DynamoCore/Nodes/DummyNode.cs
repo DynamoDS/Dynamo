5a580b3b5bae66dcff63c0ae467e3af605df6ee2
Stephen Elliott <steell1890@gmail.com>
2014-11-11 10:53:01 -0500

    moved dummynode to core

--- src/DynamoCore/Nodes/DummyNode.cs
+++ src/DynamoCore/Nodes/DummyNode.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Xml;
 using Autodesk.DesignScript.Runtime;
 using Dynamo.Models;
 
-namespace DSCoreNodesUI
+namespace Dynamo.Nodes
 {
     [NodeName("Legacy Node")]
     [NodeDescription("This is an obsolete node")]
@@ -14,6 +16,7 @@ namespace DSCoreNodesUI
     [IsVisibleInDynamoLibrary(false)]
     [NodeSearchable(false)]
     [IsDesignScriptCompatible]
+    [AlsoKnownAs("DSCoreNodesUI.DummyNode")]
     public class DummyNode : NodeModel
     {
         public enum Nature
@@ -21,55 +24,52 @@ namespace DSCoreNodesUI
             Deprecated, Unresolved
         }
 
-        public DummyNode(WorkspaceModel workspace)
-            : base()
+        public DummyNode()
         {
-            this.LegacyNodeName = "DSCoreNodesUI.DummyNode";
-            this.LegacyAssembly = string.Empty;
-            this.NodeNature = Nature.Unresolved;
+            LegacyNodeName = "DSCoreNodesUI.DummyNode";
+            LegacyAssembly = string.Empty;
+            NodeNature = Nature.Unresolved;
         }
 
-        protected override void LoadNode(XmlNode nodeElement)
+        protected override void LoadNode(XmlElement nodeElement)
         {
             var inputCount = nodeElement.Attributes["inputCount"];
             var outputCount = nodeElement.Attributes["outputCount"];
             var legacyName = nodeElement.Attributes["legacyNodeName"];
 
-            this.InputCount = Int32.Parse(inputCount.Value);
-            this.OutputCount = Int32.Parse(outputCount.Value);
-            this.LegacyNodeName = legacyName.Value;
+            InputCount = Int32.Parse(inputCount.Value);
+            OutputCount = Int32.Parse(outputCount.Value);
+            LegacyNodeName = legacyName.Value;
 
-            if (nodeElement.ChildNodes != null) 
-            {
-                foreach (XmlNode childNode in nodeElement.ChildNodes)
-                    if (childNode.Name.Equals("OriginalNodeContent"))
-                        this.OriginalNodeContent = (XmlElement)nodeElement.FirstChild.FirstChild;
-            }
-            
+            OriginalNodeContent =
+                nodeElement.ChildNodes.Cast<XmlNode>()
+                    .First(childNode => childNode.Name.Equals("OriginalNodeContent"))
+                    .FirstChild;
+
             var legacyAsm = nodeElement.Attributes["legacyAssembly"];
             if (legacyAsm != null)
-                this.LegacyAssembly = legacyAsm.Value;
+                LegacyAssembly = legacyAsm.Value;
 
             var nodeNature = nodeElement.Attributes["nodeNature"];
             if (nodeNature != null)
             {
                 var nature = Enum.Parse(typeof(Nature), nodeNature.Value);
-                this.NodeNature = ((Nature)nature);
+                NodeNature = ((Nature)nature);
             }
 
-            for (int input = 0; input < this.InputCount; input++)
+            for (int input = 0; input < InputCount; input++)
             {
                 var name = string.Format("Port {0}", input + 1);
                 InPortData.Add(new PortData(name, ""));
             }
 
-            for (int output = 0; output < this.OutputCount; output++)
+            for (int output = 0; output < OutputCount; output++)
             {
                 var name = string.Format("Port {0}", output + 1);
                 OutPortData.Add(new PortData(name, ""));
             }
 
-            this.RegisterAllPorts();
+            RegisterAllPorts();
         }
 
         protected override void SaveNode(XmlDocument xmlDoc,
@@ -79,24 +79,26 @@ namespace DSCoreNodesUI
             {
                 //Dump all the information into memory
 
-                nodeElement.SetAttribute("inputCount", this.InputCount.ToString());
-                nodeElement.SetAttribute("outputCount", this.OutputCount.ToString());
-                nodeElement.SetAttribute("legacyNodeName", this.LegacyNodeName);
-                nodeElement.SetAttribute("legacyAssembly", this.LegacyAssembly);
-                nodeElement.SetAttribute("nodeNature", this.NodeNature.ToString());
+                nodeElement.SetAttribute("inputCount", InputCount.ToString());
+                nodeElement.SetAttribute("outputCount", OutputCount.ToString());
+                nodeElement.SetAttribute("legacyNodeName", LegacyNodeName);
+                nodeElement.SetAttribute("legacyAssembly", LegacyAssembly);
+                nodeElement.SetAttribute("nodeNature", NodeNature.ToString());
 
-                if(this.OriginalNodeContent != null)
+                if (OriginalNodeContent != null && nodeElement.OwnerDocument != null)
                 {
                     XmlElement originalNode = xmlDoc.CreateElement("OriginalNodeContent");
-                    XmlElement nodeContent = nodeElement.OwnerDocument.CreateElement(this.OriginalNodeContent.Name);
+                    XmlElement nodeContent = nodeElement.OwnerDocument.CreateElement(OriginalNodeContent.Name);
 
-                    foreach (XmlAttribute attribute in this.OriginalNodeContent.Attributes)
+                    var attributes = OriginalNodeContent.Attributes as IEnumerable
+                        ?? Enumerable.Empty<object>();
+
+                    foreach (XmlAttribute attribute in attributes)
                         nodeContent.SetAttribute(attribute.Name, attribute.Value);
 
-                    for (int i = 0; i < this.OriginalNodeContent.ChildNodes.Count; i++)
+                    foreach (XmlNode childNode in OriginalNodeContent.ChildNodes)
                     {
-                        XmlNode child = 
-                            nodeContent.OwnerDocument.ImportNode(this.OriginalNodeContent.ChildNodes[i], true);
+                        XmlNode child = nodeContent.OwnerDocument.ImportNode(childNode, true);
                         nodeContent.AppendChild(child.CloneNode(true));
                     }
 
@@ -109,10 +111,11 @@ namespace DSCoreNodesUI
             {
                 //When save files, only save the original node's content, 
                 //instead of saving the dummy node.
-                if (this.OriginalNodeContent != null)
+                if (OriginalNodeContent != null)
                 {
-                    XmlElement originalNode = nodeElement.OwnerDocument.CreateElement(this.OriginalNodeContent.Name);
-                    foreach (XmlAttribute attribute in this.OriginalNodeContent.Attributes)
+                    XmlElement originalNode =
+                        nodeElement.OwnerDocument.CreateElement(OriginalNodeContent.Name);
+                    foreach (XmlAttribute attribute in OriginalNodeContent.Attributes)
                         originalNode.SetAttribute(attribute.Name, attribute.Value);
 
                     //overwrite the guid/x/y value of the original node.
@@ -120,21 +123,24 @@ namespace DSCoreNodesUI
                     originalNode.SetAttribute("x", nodeElement.GetAttribute("x"));
                     originalNode.SetAttribute("y", nodeElement.GetAttribute("y"));
 
-                    for (int i = 0; i < this.OriginalNodeContent.ChildNodes.Count; i++)
+                    for (int i = 0; i < OriginalNodeContent.ChildNodes.Count; i++)
                     {
                         XmlNode child =
-                            originalNode.OwnerDocument.ImportNode(this.OriginalNodeContent.ChildNodes[i], true);
+                            originalNode.OwnerDocument.ImportNode(
+                                OriginalNodeContent.ChildNodes[i],
+                                true);
                         originalNode.AppendChild(child.CloneNode(true));
                     }
 
                     nodeElement.ParentNode.ReplaceChild(originalNode, nodeElement);
                 }
-                else {
-                    nodeElement.SetAttribute("inputCount", this.InputCount.ToString());
-                    nodeElement.SetAttribute("outputCount", this.OutputCount.ToString());
-                    nodeElement.SetAttribute("legacyNodeName", this.LegacyNodeName);
-                    nodeElement.SetAttribute("legacyAssembly", this.LegacyAssembly);
-                    nodeElement.SetAttribute("nodeNature", this.NodeNature.ToString());
+                else
+                {
+                    nodeElement.SetAttribute("inputCount", InputCount.ToString());
+                    nodeElement.SetAttribute("outputCount", OutputCount.ToString());
+                    nodeElement.SetAttribute("legacyNodeName", LegacyNodeName);
+                    nodeElement.SetAttribute("legacyAssembly", LegacyAssembly);
+                    nodeElement.SetAttribute("nodeNature", NodeNature.ToString());
                 }
             }
         }
@@ -144,7 +150,7 @@ namespace DSCoreNodesUI
         protected override void SerializeCore(XmlElement element, SaveContext context)
         {
             base.SerializeCore(element, context);
-            this.SaveNode(element.OwnerDocument, element, context);
+            SaveNode(element.OwnerDocument, element, context);
         }
 
         protected override void DeserializeCore(XmlElement element, SaveContext context)
@@ -153,7 +159,7 @@ namespace DSCoreNodesUI
             OutPortData.Clear(); // LoadNode, clear them so they don't accumulate.
 
             base.DeserializeCore(element, context);
-            this.LoadNode(element);
+            LoadNode(element);
         }
 
         #endregion
@@ -165,40 +171,41 @@ namespace DSCoreNodesUI
 
         public string GetDescription()
         {
-            if (this.NodeNature == Nature.Deprecated)
+            if (NodeNature == Nature.Deprecated)
             {
-                if (string.IsNullOrEmpty(this.LegacyAssembly))
+                if (string.IsNullOrEmpty(LegacyAssembly))
                 {
-                    var format = "Node of type '{0}' is now deprecated";
-                    return string.Format(format, this.LegacyNodeName);
+                    const string format = "Node of type '{0}' is now deprecated";
+                    return string.Format(format, LegacyNodeName);
                 }
                 else
                 {
-                    var format = "Node of type '{0}' ({1}) is now deprecated";
-                    return string.Format(format, this.LegacyNodeName, this.LegacyAssembly);
+                    const string format = "Node of type '{0}' ({1}) is now deprecated";
+                    return string.Format(format, LegacyNodeName, LegacyAssembly);
                 }
             }
-            else if (this.NodeNature == Nature.Unresolved)
+            
+            if (NodeNature == Nature.Unresolved)
             {
-                if (string.IsNullOrEmpty(this.LegacyAssembly))
+                if (string.IsNullOrEmpty(LegacyAssembly))
                 {
-                    var format = "Node of type '{0}' cannot be resolved";
-                    return string.Format(format, this.LegacyNodeName);
+                    const string format = "Node of type '{0}' cannot be resolved";
+                    return string.Format(format, LegacyNodeName);
                 }
                 else
                 {
-                    var format = "Node of type '{0}' ({1}) cannot be resolved";
-                    return string.Format(format, this.LegacyNodeName, this.LegacyAssembly);
+                    const string format = "Node of type '{0}' ({1}) cannot be resolved";
+                    return string.Format(format, LegacyNodeName, LegacyAssembly);
                 }
             }
 
-            var message = "Unhandled 'DummyNode.NodeNature' value: {0}";
-            throw new InvalidOperationException(string.Format(message, NodeNature.ToString()));
+            const string message = "Unhandled 'DummyNode.NodeNature' value: {0}";
+            throw new InvalidOperationException(string.Format(message, NodeNature));
         }
 
         public override string Description
         {
-            get { return this.GetDescription(); }
+            get { return GetDescription(); }
             set { base.Description = value; }
         }
 
@@ -207,6 +214,6 @@ namespace DSCoreNodesUI
         public string LegacyNodeName { get; private set; }
         public string LegacyAssembly { get; private set; }
         public Nature NodeNature { get; private set; }
-        public XmlElement OriginalNodeContent { get; private set; }
+        public XmlNode OriginalNodeContent { get; private set; }
     }
 }
