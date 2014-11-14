93a3983f69f3e5ab573fbc44fa18a8a09a487f03
Anna Osyatnik <anna.osyatnik@autodesk.com>
2014-11-14 10:17:02 +0200

    ConvertersTest.cs created.

---
+++ test/DynamoCoreTests/ConvertersTest.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+
+namespace Dynamo
+{
+    class ConvertersTest
+    {
+        [Test]
+        public void SearchResultsToVisibilityConverterTest()
+        {
+        }
+
+        [Test]
+        public void FullyQualifiedNameToDisplayConverterTest()
+        {
+        }
+
+        [Test]
+        public void InOutParamTypeConverterTest()
+        {
+        }
+
+        [Test]
+        public void BrowserRootElementToSubclassesConverterTest()
+        {
+        }
+
+        [Test]
+        public void DisplayModeToTextDecorationsConverterTest()
+        {
+        }
+
+        [Test]
+        public void ViewModeToVisibilityConverterTest()
+        {
+        }
+
+        [Test]
+        public void ElementTypeToBoolConverterTest()
+        {
+        }
+
+        [Test]
+        public void NodeTypeToColorConverterTest()
+        {
+        }
+
+        [Test]
+        public void RootElementToBoolConverterTest()
+        {
+        }
+
+        [Test]
+        public void HasParentRootElementTest()
+        {
+        }
+
+        [Test]
+        public void MultiBoolToVisibilityConverterTest()
+        {
+        }
+
+        [Test]
+        public void NullValueToCollapsedConverterTest()
+        {
+        }
+
+        [Test]
+        public void FullCategoryNameToMarginConverterTest()
+        {
+        }
+
+        [Test]
+        public void IntToVisibilityConverterTest()
+        {
+        }
+
+        [Test]
+        public void SearchHighlightMarginConverterTest()
+        {
+        }
+    }
+}
