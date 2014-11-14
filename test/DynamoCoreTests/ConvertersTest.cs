7784fd30224ac541a8ccb97e10f0c2981b7ef575
Anna Osyatnik <anna.osyatnik@autodesk.com>
2014-11-14 11:45:18 +0200

    SearchResultsToVisibilityConverterTest added.

--- test/DynamoCoreTests/ConvertersTest.cs
+++ test/DynamoCoreTests/ConvertersTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Dynamo.Controls;
+using NUnit.Framework;
+using System.Windows;
 
 namespace Dynamo
 {
@@ -7,6 +9,73 @@ namespace Dynamo
         [Test]
         public void SearchResultsToVisibilityConverterTest()
         {
+            SearchResultsToVisibilityConverter converter = new SearchResultsToVisibilityConverter();
+            int numberOfFoundSearchCategories = 0;
+            bool addonsVisibility = false;
+            string searchText = "";
+
+            object[] array = { numberOfFoundSearchCategories, addonsVisibility, searchText };
+
+            //1. There are no found search categories. Addons are invisible. Search text is empty.
+            //2. There are no found search categories. Addons are invisible. Search text is not empty.
+            //3. There are no found search categories. Addons are visible. Search text is empty.
+            //4. There are no found search categories. Addons are visible. Search text is not empty.
+            //5. There are some search categories. Addons are invisible. Search text is not empty.
+            //6. There are some search categories. Addons are invisible. Search text is empty.
+            //7. There are some search categories. Addons are visible. Search text is not empty.
+            //8. There are some search categories. Addons are visible. Search text is empty.
+
+            // 1 case
+            object result = converter.Convert(array, null, null, null);
+            Assert.AreEqual(Visibility.Collapsed, result);
+
+            // 2 case
+            searchText = "search text";
+            array[2] = searchText;
+            result = converter.Convert(array, null, null, null);
+            Assert.AreEqual(Visibility.Visible, result);
+
+            // 3 case
+            searchText = "";
+            array[2] = searchText;
+            addonsVisibility = true;
+            array[1] = addonsVisibility;
+            result = converter.Convert(array, null, null, null);
+            Assert.AreEqual(Visibility.Collapsed, result);
+
+            // 4 case
+            searchText = "search text";
+            array[2] = searchText;
+            result = converter.Convert(array, null, null, null);
+            Assert.AreEqual(Visibility.Collapsed, result);
+
+            // 5 case
+            numberOfFoundSearchCategories = 5;
+            array[0] = numberOfFoundSearchCategories;
+            addonsVisibility = false;
+            array[1] = addonsVisibility;
+            result = converter.Convert(array, null, null, null);
+            Assert.AreEqual(Visibility.Collapsed, result);
+
+            // 6 case
+            searchText = "";
+            array[2] = searchText;
+            result = converter.Convert(array, null, null, null);
+            Assert.AreEqual(Visibility.Collapsed, result);
+
+            // 7 case
+            addonsVisibility = true;
+            array[1] = addonsVisibility;
+            searchText = "search text";
+            array[2] = searchText;
+            result = converter.Convert(array, null, null, null);
+            Assert.AreEqual(Visibility.Collapsed, result);
+
+            // 8 case
+            searchText = "";
+            array[2] = searchText;
+            result = converter.Convert(array, null, null, null);
+            Assert.AreEqual(Visibility.Collapsed, result);
         }
 
         [Test]
