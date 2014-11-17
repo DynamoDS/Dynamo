971af47be74960914d30ccc8cfe61f5918369117
Yu Ke <yu.ke@autodesk.com>
2014-11-17 12:59:05 +0800

    Add String.FromObject and String.FromArray UI nodes

---
+++ src/Libraries/CoreNodesUI/String.cs
@@ -0,0 +1,58 @@
+﻿using Dynamo.Models;
+using ProtoCore.AST.AssociativeAST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSCoreNodesUI
+{
+    [NodeName("String.FromObject")]
+    [NodeDescription("Convert an object to a string representation")]
+    [NodeCategory("Core.String")]
+    [IsDesignScriptCompatible]
+    public class StringFromObject: NodeModel
+    {
+        public StringFromObject(WorkspaceModel workspace) : base(workspace)
+        {
+            ArgumentLacing = LacingStrategy.Disabled;
+            InPortData.Add(new PortData("obj", "Object to be serialized"));
+            OutPortData.Add(new PortData("str", "String representation of the object"));
+            RegisterAllPorts();
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            var functionCall = AstFactory.BuildFunctionCall("__ToStringForObject", inputAstNodes);
+            return new[]
+            {
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), functionCall)
+            };
+        }
+    }
+
+    [NodeName("String.FromArray")]
+    [NodeDescription("Convert an array to a string representation")]
+    [NodeCategory("Core.String")]
+    [IsDesignScriptCompatible]
+    public class StringFromArray : NodeModel
+    {
+        public StringFromArray(WorkspaceModel workspace) : base(workspace)
+        {
+            ArgumentLacing = LacingStrategy.Disabled;
+            InPortData.Add(new PortData("arr", "The array of object to be serialized"));
+            OutPortData.Add(new PortData("str", "String representation of the array"));
+            RegisterAllPorts();
+        }
+
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            var functionCall = AstFactory.BuildFunctionCall("__ToStringForArray", inputAstNodes);
+            return new[]
+            {
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), functionCall)
+            };
+        }
+    }
+}
