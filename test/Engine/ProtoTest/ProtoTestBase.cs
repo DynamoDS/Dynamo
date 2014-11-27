421023d6306fab1b001eafc04d53f7613a5d6d8b
Yu Ke <yu.ke@autodesk.com>
2014-11-27 16:36:59 +0800

    Add ProtoTestBase to handle common SetUp and TearDown event

---
+++ test/Engine/ProtoTest/ProtoTestBase.cs
@@ -0,0 +1,31 @@
+﻿using ProtoTestFx.TD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProtoTest
+{
+    abstract class ProtoTestBase
+    {
+        public ProtoCore.Core core;
+        public TestFrameWork thisTest = new TestFrameWork();
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            core = new ProtoCore.Core(new ProtoCore.Options());
+            core.Executives.Add(ProtoCore.Language.kAssociative, new ProtoAssociative.Executive(core));
+            core.Executives.Add(ProtoCore.Language.kImperative, new ProtoImperative.Executive(core));
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            core.Cleanup();
+            thisTest.CleanUp();
+        }
+
+    }
+}
