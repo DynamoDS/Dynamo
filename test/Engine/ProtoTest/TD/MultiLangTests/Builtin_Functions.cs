38f6e9ed1d3bd174d26f517c62c7a49ca24fc43d
Yu Ke <yu.ke@autodesk.com>
2014-11-28 15:41:59 +0800

    Remove dependency on ProtoGeometry.dll and DSCoreNodes.dll

--- test/Engine/ProtoTest/TD/MultiLangTests/Builtin_Functions.cs
+++ test/Engine/ProtoTest/TD/MultiLangTests/Builtin_Functions.cs
@@ -3000,13 +3000,13 @@ d = { };
         }
 
         [Test]
-        [Category("ProtoGeometry")] [Ignore] [Category("PortToCodeBlocks")]
+        [Category("PortToCodeBlocks")]
         public void BIM53_RemoveDuplicates_geoemtry_1467447()
         {//1467446
             String code =
             @"
-            import(""ProtoGeometry.dll"");
-            pt = Point.ByCoordinates(1, 1, 1);
+            import(""FFITarget.dll"");
+            pt = DummyPoint.ByCoordinates(1, 1, 1);
             input = { pt, pt};
             removeDuplicatesSetInsert = RemoveDuplicates(input);
             count = Count(removeDuplicatesSetInsert);
