409e090ff665f2178ac6d5c8809c10ce671d4d9a
Peter Boyer <peter.b.boyer@gmail.com>
2014-12-08 16:20:30 -0500

    Remove PresentationCore from DynamoCore

--- src/DynamoCoreWpf/UI/VisualConfigurations.cs
+++ src/DynamoCoreWpf/UI/VisualConfigurations.cs
@@ -9,6 +9,8 @@ namespace Dynamo.Wpf.UI
 {
     public static class VisualConfigurations
     {
+        // TODO(Peter): Clean up configurations, these are just of the few moved out of DynamoCore in order
+        //              to remove the PresentationCore dependency http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-5608
         public static FontWeight LibraryTooltipTextFontWeight = FontWeights.Normal;
         public static FontWeight NodeTooltipTextFontWeight = FontWeights.Light;
         public static FontWeight ErrorTextFontWeight = FontWeights.Normal;
