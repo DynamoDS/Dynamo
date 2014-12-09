3990a5dfb4316e2cbf0e82d7999116b9ae54979d
Stephen Elliott <steell1890@gmail.com>
2014-12-09 17:29:46 -0500

    replaced jarowinkler implementation

--- src/DynamoCore/Search/SearchDictionary.cs
+++ src/DynamoCore/Search/SearchDictionary.cs
@@ -324,120 +324,110 @@ namespace Dynamo.Search
                     .Select(entryAndWeight => entryAndWeight.Entry);
         }
 
+
+        //http://stackoverflow.com/questions/19123506/jaro-winkler-distance-algorithm-in-c-sharp
         public static class JaroWinkler
         {
+            /* The Winkler modification will not be applied unless the 
+             * percent match was at or above the mWeightThreshold percent 
+             * without the modification. 
+             * Winkler's paper used a default value of 0.7
+             */
+            private const double M_WEIGHT_THRESHOLD = 0.7;
+
+            /* Size of the prefix to be concidered by the Winkler modification. 
+             * Winkler's paper used a default value of 4
+             */
+            private const int M_NUM_CHARS = 4;
+
+
             /// <summary>
-            ///     Calculates a distance metric between two strings, on a scale of [0, 1]. 0 indicates
-            ///     that there is no similarity between the strings, 1 indicates that there is an exact
-            ///     match.
+            /// Returns the Jaro-Winkler distance between the specified  
+            /// strings. The distance is symmetric and will fall in the 
+            /// range 0 (perfect match) to 1 (no match). 
             /// </summary>
-            /// <param name="firstWord"></param>
-            /// <param name="secondWord"></param>
-            public static double StringDistance(string firstWord, string secondWord)
+            /// <param name="aString1">First String</param>
+            /// <param name="aString2">Second String</param>
+            /// <returns></returns>
+            public static double StringDistance(string aString1, string aString2)
             {
-                const double prefixAdustmentScale = 0.1;
-
-                if ((firstWord == null) || (secondWord == null)) 
-                    return 0.0;
-
-                double dist = GetJaroDistance(firstWord, secondWord);
-                int prefixLength = GetPrefixLength(firstWord, secondWord);
-                return dist + prefixLength * prefixAdustmentScale * (1.0 - dist);
+                return 1.0 - Proximity(aString1, aString2);
             }
 
-            private static double GetJaroDistance(string firstWord, string secondWord)
+
+            /// <summary>
+            /// Returns the Jaro-Winkler distance between the specified  
+            /// strings. The distance is symmetric and will fall in the 
+            /// range 0 (no match) to 1 (perfect match). 
+            /// </summary>
+            /// <param name="aString1">First String</param>
+            /// <param name="aString2">Second String</param>
+            /// <returns></returns>
+            private static double Proximity(string aString1, string aString2)
             {
-                const double defaultMismatchScore = 0;
+                int lLen1 = aString1.Length;
+                int lLen2 = aString2.Length;
+                if (lLen1 == 0)
+                    return lLen2 == 0 ? 1.0 : 0.0;
 
-                if ((firstWord == null) || (secondWord == null)) 
-                    return defaultMismatchScore;
+                int lSearchRange = Math.Max(0, Math.Max(lLen1, lLen2) / 2 - 1);
 
-                //get half the length of the string rounded up 
-                //(this is the distance used for acceptable transpositions)
-                int halflen = Math.Min(firstWord.Length, secondWord.Length) / 2 + 1;
-
-                //get common characters
-                StringBuilder common1 = GetCommonCharacters(firstWord, secondWord, halflen);
-                int commonMatches = common1.Length;
-
-                //check for zero in common
-                if (commonMatches == 0)
+                var lMatched1 = new bool[lLen1];
+                for (int i = 0; i < lMatched1.Length; i++)
                 {
-                    return defaultMismatchScore;
+                    lMatched1[i] = false;
                 }
 
-                StringBuilder common2 = GetCommonCharacters(secondWord, firstWord, halflen);
-
-                //check for same length common strings returning 0.0f is not the same
-                if (commonMatches != common2.Length)
+                var lMatched2 = new bool[lLen2];
+                for (int i = 0; i < lMatched2.Length; i++)
                 {
-                    return defaultMismatchScore;
+                    lMatched2[i] = false;
                 }
 
-                //get the number of transpositions
-                int transpositions = 0;
-                for (int i = 0; i < commonMatches; i++)
+                int lNumCommon = 0;
+                for (int i = 0; i < lLen1; ++i)
                 {
-                    if (common1[i] != common2[i])
+                    int lStart = Math.Max(0, i - lSearchRange);
+                    int lEnd = Math.Min(i + lSearchRange + 1, lLen2);
+                    for (int j = lStart; j < lEnd; ++j)
                     {
-                        transpositions++;
-                    }
-                }
-
-                //calculate jaro metric
-                transpositions /= 2;
-                double tmp1 = commonMatches / (3.0 * firstWord.Length)
-                    + commonMatches / (3.0 * secondWord.Length)
-                    + (commonMatches - transpositions) / (3.0 * commonMatches);
-                return tmp1;
-            }
-
-            private static StringBuilder GetCommonCharacters(string firstWord, string secondWord, int distanceSep)
-            {
-                if ((firstWord == null) || (secondWord == null)) 
-                    return null;
-
-                var returnCommons = new StringBuilder();
-                var copy = new StringBuilder(secondWord);
-                for (int i = 0; i < firstWord.Length; i++)
-                {
-                    char ch = firstWord[i];
-                    bool foundIt = false;
-                    for (int j = Math.Max(0, i - distanceSep);
-                         !foundIt && j < Math.Min(i + distanceSep, secondWord.Length);
-                         j++)
-                    {
-                        if (copy[j] != ch)
+                        if (lMatched2[j]) continue;
+                        if (aString1[i] != aString2[j])
                             continue;
-
-                        foundIt = true;
-                        returnCommons.Append(ch);
-                        copy[j] = '#';
+                        lMatched1[i] = true;
+                        lMatched2[j] = true;
+                        ++lNumCommon;
+                        break;
                     }
                 }
+                if (lNumCommon == 0) return 0.0;
 
-                return returnCommons;
-            }
-
-            private static int GetPrefixLength(string firstWord, string secondWord)
-            {
-                const int minPrefixTestLength = 4;
-
-                if ((firstWord == null) || (secondWord == null)) 
-                    return minPrefixTestLength;
-                
-                int n =
-                    Math.Min(
-                        minPrefixTestLength,
-                        Math.Min(firstWord.Length, secondWord.Length));
-
-                for (int i = 0; i < n; i++)
+                int lNumHalfTransposed = 0;
+                int k = 0;
+                for (int i = 0; i < lLen1; ++i)
                 {
-                    if (firstWord[i] != secondWord[i])
-                        return i;
+                    if (!lMatched1[i]) continue;
+                    while (!lMatched2[k]) ++k;
+                    if (aString1[i] != aString2[k])
+                        ++lNumHalfTransposed;
+                    ++k;
                 }
+                // System.Diagnostics.Debug.WriteLine("numHalfTransposed=" + numHalfTransposed);
+                int lNumTransposed = lNumHalfTransposed / 2;
 
-                return n;
+                // System.Diagnostics.Debug.WriteLine("numCommon=" + numCommon + " numTransposed=" + numTransposed);
+                double lNumCommonD = lNumCommon;
+                double lWeight = (lNumCommonD / lLen1
+                                 + lNumCommonD / lLen2
+                                 + (lNumCommon - lNumTransposed) / lNumCommonD) / 3.0;
+
+                if (lWeight <= M_WEIGHT_THRESHOLD) return lWeight;
+                int lMax = Math.Min(M_NUM_CHARS, Math.Min(aString1.Length, aString2.Length));
+                int lPos = 0;
+                while (lPos < lMax && aString1[lPos] == aString2[lPos])
+                    ++lPos;
+                if (lPos == 0) return lWeight;
+                return lWeight + 0.1 * lPos * (1.0 - lWeight);
             }
         }
     }
