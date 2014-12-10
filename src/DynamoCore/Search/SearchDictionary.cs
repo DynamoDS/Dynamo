feb98dbe65292f4bbf302eedc7dd5f0c3a9b374a
Stephen Elliott <steell1890@gmail.com>
2014-12-09 20:28:09 -0500

    replaced jarowinkler implementation... again

--- src/DynamoCore/Search/SearchDictionary.cs
+++ src/DynamoCore/Search/SearchDictionary.cs
@@ -18,7 +18,10 @@ namespace Dynamo.Search
         /// <summary>
         ///     All the current entries in search.
         /// </summary>
-        public IEnumerable<V> SearchEntries { get { return entryDictionary.Keys; } } 
+        public IEnumerable<V> SearchEntries
+        {
+            get { return entryDictionary.Keys; }
+        }
 
         /// <summary>
         ///     The number of tags in the dictionary
@@ -33,13 +36,11 @@ namespace Dynamo.Search
         /// </summary>
         public int NumElements
         {
-            get
-            {
-                return entryDictionary.Count;
-            }
+            get { return entryDictionary.Count; }
         }
 
         public event Action<V> EntryAdded;
+
         protected virtual void OnEntryAdded(V entry)
         {
             var handler = EntryAdded;
@@ -47,6 +48,7 @@ namespace Dynamo.Search
         }
 
         public event Action<V> EntryRemoved;
+
         protected virtual void OnEntryRemoved(V entry)
         {
             var handler = EntryRemoved;
@@ -59,7 +61,7 @@ namespace Dynamo.Search
         /// <param name="value"> The object to add  </param>
         /// <param name="tag"> The string to identify it in search </param>
         /// <param name="weight"></param>
-        public void Add(V value, string tag, double weight=1)
+        public void Add(V value, string tag, double weight = 1)
         {
             Add(value, tag.AsSingleton(), weight);
         }
@@ -70,7 +72,7 @@ namespace Dynamo.Search
         /// <param name="values"> List of objects to add  </param>
         /// <param name="tag"> The string to identify it in search </param>
         /// <param name="weight"></param>
-        public void Add(IEnumerable<V> values, string tag, double weight=1)
+        public void Add(IEnumerable<V> values, string tag, double weight = 1)
         {
             foreach (var value in values)
                 Add(value, tag, weight);
@@ -82,7 +84,7 @@ namespace Dynamo.Search
         /// <param name="value"> The object to add  </param>
         /// <param name="tags"> The list of strings to identify it in search </param>
         /// <param name="weight"></param>
-        public void Add(V value, IEnumerable<string> tags, double weight=1)
+        public void Add(V value, IEnumerable<string> tags, double weight = 1)
         {
             Dictionary<string, double> keys;
             if (!entryDictionary.TryGetValue(value, out keys))
@@ -101,7 +103,7 @@ namespace Dynamo.Search
         /// <param name="values"> The objects to add. Must have the same cardinality as the second parameter</param>
         /// <param name="tags"> The list of strings to identify it in search. Must have the same cardinality as the first parameter </param>
         /// <param name="weight"></param>
-        public void Add(IEnumerable<V> values, IEnumerable<string> tags, double weight=1)
+        public void Add(IEnumerable<V> values, IEnumerable<string> tags, double weight = 1)
         {
             var tagList = tags as IList<string> ?? tags.ToList();
             foreach (var value in values)
@@ -126,7 +128,7 @@ namespace Dynamo.Search
         /// <param name="value"> The object to remove </param>
         public bool Remove(V value)
         {
-            if (!entryDictionary.Remove(value)) 
+            if (!entryDictionary.Remove(value))
                 return false;
             OnEntryRemoved(value);
             return true;
@@ -176,9 +178,9 @@ namespace Dynamo.Search
         public int Remove(V value, IEnumerable<string> tags)
         {
             Dictionary<string, double> keys;
-            if (!entryDictionary.TryGetValue(value, out keys)) 
+            if (!entryDictionary.TryGetValue(value, out keys))
                 return 0;
-            
+
             var count = tags.Count(tag => keys.Remove(tag));
             if (!keys.Any())
                 Remove(value);
@@ -225,9 +227,9 @@ namespace Dynamo.Search
         private static string SanitizeQuery(string query)
         {
             return query.Trim()
-                        .Replace("\\", "\\\\")
-                        .Replace(".", "\\.")
-                        .Replace("*", "\\*");
+                .Replace("\\", "\\\\")
+                .Replace(".", "\\.")
+                .Replace("*", "\\*");
         }
 
         private static string[] SplitOnWhiteSpace(string s)
@@ -252,8 +254,8 @@ namespace Dynamo.Search
             string query)
         {
             yield return kv => kv.Contains(query);
-            
-            if (!ContainsSpecialCharacters(query)) 
+
+            if (!ContainsSpecialCharacters(query))
                 yield break;
             var regexPattern = MakePattern(SplitOnWhiteSpace(SanitizeQuery(query)));
             yield return kv => MatchWithQuerystring(kv, regexPattern);
@@ -265,7 +267,7 @@ namespace Dynamo.Search
         /// <param name="query">The search query</param>
         public IEnumerable<V> Search(string query)
         {
-            return 
+            return
                 entryDictionary.AsParallel()
                     
                     //Begin the search by "flattening" the entryDictionary.
@@ -285,7 +287,8 @@ namespace Dynamo.Search
                     //Group together matching search tags. Each search tag now has an enumerable sequence of entries and associated weight
                     .GroupBy(
                         tagWeightAndEntry => tagWeightAndEntry.Tag,
-                        tagWeightAndEntry => new { tagWeightAndEntry.Entry, tagWeightAndEntry.Weight })
+                        tagWeightAndEntry =>
+                            new { tagWeightAndEntry.Entry, tagWeightAndEntry.Weight })
 
                     //For each grouping, we calculate a "match closeness" based on the difference between the search tag and the query
                     .Select(
@@ -304,18 +307,22 @@ namespace Dynamo.Search
                                     new
                                     {
                                         entryAndWeight.Entry,
-                                        Weight = entryAndWeight.Weight*closenessAndEntries.MatchCloseness
+                                        Weight =
+                                    entryAndWeight.Weight*closenessAndEntries.MatchCloseness
                                     }))
                     
                     //Filter out all entries with a weight of approximately zero
                     .Where(entryAndWeight => Math.Abs(entryAndWeight.Weight) >= double.Epsilon)
                     
                     //Group together all results consisting of the same entry.
-                    .GroupBy(entryAndWeight => entryAndWeight.Entry, entryAndWeight => entryAndWeight.Weight)
+                    .GroupBy(
+                        entryAndWeight => entryAndWeight.Entry,
+                        entryAndWeight => entryAndWeight.Weight)
                     
                     //We remove all duplicates, using the largest available weight
                     .Select(
-                        entryAndWeights => new { Entry = entryAndWeights.Key, Weight = entryAndWeights.Max() })
+                        entryAndWeights =>
+                            new { Entry = entryAndWeights.Key, Weight = entryAndWeights.Max() })
                     
                     //Sort results in descending order by weight.
                     .OrderByDescending(entryAndWeight => entryAndWeight.Weight)
@@ -325,109 +332,105 @@ namespace Dynamo.Search
         }
 
 
-        //http://stackoverflow.com/questions/19123506/jaro-winkler-distance-algorithm-in-c-sharp
         public static class JaroWinkler
         {
-            /* The Winkler modification will not be applied unless the 
-             * percent match was at or above the mWeightThreshold percent 
-             * without the modification. 
-             * Winkler's paper used a default value of 0.7
-             */
-            private const double M_WEIGHT_THRESHOLD = 0.7;
-
-            /* Size of the prefix to be concidered by the Winkler modification. 
-             * Winkler's paper used a default value of 4
-             */
-            private const int M_NUM_CHARS = 4;
-
-
             /// <summary>
-            /// Returns the Jaro-Winkler distance between the specified  
-            /// strings. The distance is symmetric and will fall in the 
-            /// range 0 (perfect match) to 1 (no match). 
+            /// Gets or sets the current value of the threshold used for adding the Winkler bonus.
+            /// Set to a negative value to get the Jaro distance. The default value is 0.7.
             /// </summary>
-            /// <param name="aString1">First String</param>
-            /// <param name="aString2">Second String</param>
-            /// <returns></returns>
-            public static double StringDistance(string aString1, string aString2)
+            private const float THRESHOLD = 0.7f;
+
+            private static int[] Matches(String s1, String s2)
             {
-                return 1.0 - Proximity(aString1, aString2);
-            }
+                string max, min;
 
-
-            /// <summary>
-            /// Returns the Jaro-Winkler distance between the specified  
-            /// strings. The distance is symmetric and will fall in the 
-            /// range 0 (no match) to 1 (perfect match). 
-            /// </summary>
-            /// <param name="aString1">First String</param>
-            /// <param name="aString2">Second String</param>
-            /// <returns></returns>
-            private static double Proximity(string aString1, string aString2)
-            {
-                int lLen1 = aString1.Length;
-                int lLen2 = aString2.Length;
-                if (lLen1 == 0)
-                    return lLen2 == 0 ? 1.0 : 0.0;
-
-                int lSearchRange = Math.Max(0, Math.Max(lLen1, lLen2) / 2 - 1);
-
-                var lMatched1 = new bool[lLen1];
-                for (int i = 0; i < lMatched1.Length; i++)
+                if (s1.Length > s2.Length)
                 {
-                    lMatched1[i] = false;
+                    max = s1;
+                    min = s2;
+                }
+                else
+                {
+                    max = s2;
+                    min = s1;
                 }
 
-                var lMatched2 = new bool[lLen2];
-                for (int i = 0; i < lMatched2.Length; i++)
-                {
-                    lMatched2[i] = false;
-                }
+                var range = Math.Max(max.Length/2 - 1, 0);
+                var matchIndexes = new int[min.Length];
 
-                int lNumCommon = 0;
-                for (int i = 0; i < lLen1; ++i)
+                for (var i = 0; i < matchIndexes.Length; i++)
+                    matchIndexes[i] = -1;
+
+                var matchFlags = new bool[max.Length];
+                var matches = 0;
+
+                for (var mi = 0; mi < min.Length; mi++)
                 {
-                    int lStart = Math.Max(0, i - lSearchRange);
-                    int lEnd = Math.Min(i + lSearchRange + 1, lLen2);
-                    for (int j = lStart; j < lEnd; ++j)
+                    var c1 = min[mi];
+                    for (int xi = Math.Max(mi - range, 0),
+                        xn = Math.Min(mi + range + 1, max.Length);
+                         xi < xn;
+                         xi++)
                     {
-                        if (lMatched2[j]) continue;
-                        if (aString1[i] != aString2[j])
-                            continue;
-                        lMatched1[i] = true;
-                        lMatched2[j] = true;
-                        ++lNumCommon;
+                        if (matchFlags[xi] || c1 != max[xi]) continue;
+
+                        matchIndexes[mi] = xi;
+                        matchFlags[xi] = true;
+                        matches++;
                         break;
                     }
                 }
-                if (lNumCommon == 0) return 0.0;
 
-                int lNumHalfTransposed = 0;
-                int k = 0;
-                for (int i = 0; i < lLen1; ++i)
+                var ms1 = new char[matches];
+                var ms2 = new char[matches];
+
+                for (int i = 0, si = 0; i < min.Length; i++)
                 {
-                    if (!lMatched1[i]) continue;
-                    while (!lMatched2[k]) ++k;
-                    if (aString1[i] != aString2[k])
-                        ++lNumHalfTransposed;
-                    ++k;
+                    if (matchIndexes[i] != -1)
+                    {
+                        ms1[si] = min[i];
+                        si++;
+                    }
                 }
-                // System.Diagnostics.Debug.WriteLine("numHalfTransposed=" + numHalfTransposed);
-                int lNumTransposed = lNumHalfTransposed / 2;
 
-                // System.Diagnostics.Debug.WriteLine("numCommon=" + numCommon + " numTransposed=" + numTransposed);
-                double lNumCommonD = lNumCommon;
-                double lWeight = (lNumCommonD / lLen1
-                                 + lNumCommonD / lLen2
-                                 + (lNumCommon - lNumTransposed) / lNumCommonD) / 3.0;
+                for (int i = 0, si = 0; i < max.Length; i++)
+                {
+                    if (matchFlags[i])
+                    {
+                        ms2[si] = max[i];
+                        si++;
+                    }
+                }
 
-                if (lWeight <= M_WEIGHT_THRESHOLD) return lWeight;
-                int lMax = Math.Min(M_NUM_CHARS, Math.Min(aString1.Length, aString2.Length));
-                int lPos = 0;
-                while (lPos < lMax && aString1[lPos] == aString2[lPos])
-                    ++lPos;
-                if (lPos == 0) return lWeight;
-                return lWeight + 0.1 * lPos * (1.0 - lWeight);
+                var transpositions = ms1.Where((t, mi) => t != ms2[mi]).Count();
+
+                var prefix = 0;
+                for (var mi = 0; mi < min.Length; mi++)
+                {
+                    if (s1[mi] == s2[mi])
+                    {
+                        prefix++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                return new[] { matches, transpositions/2, prefix, max.Length };
+            }
+
+            public static float StringDistance(String s1, String s2)
+            {
+                var mtp = Matches(s1, s2);
+                var m = (float)mtp[0];
+
+                if (Math.Abs(m) < 0.0001)
+                    return 0f;
+
+                float j = ((m/s1.Length + m/s2.Length + (m - mtp[1])/m))/3;
+                float jw = j < THRESHOLD ? j : j + Math.Min(0.1f, 1f/mtp[3])*mtp[2]*(1 - j);
+                return jw;
             }
         }
     }
