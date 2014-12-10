500be83780503eb1c5d1a22ce4779b0389ccf26c
Vladimir Moyseenko <vladimir.moyseenko@autodesk.com>
2014-12-10 12:15:04 +0200

    Added default parameter to ImportLibarry

--- test/DynamoCoreTests/DSLibraryTest.cs
+++ test/DynamoCoreTests/DSLibraryTest.cs
@@ -36,7 +36,7 @@ namespace Dynamo.Tests
             libraryServices.LibraryLoadFailed += (sender, e) => Assert.Fail("Failed to load library: " + e.LibraryPath); 
 
             string libraryPath = Path.Combine(GetTestDirectory(), @"core\library\Dummy.ds");
-            libraryServices.ImportLibrary(libraryPath, ViewModel.Model.Logger, SearchModel.ElementType.CustomDll);
+            libraryServices.ImportLibrary(libraryPath, ViewModel.Model.Logger);
             Assert.IsTrue(libraryLoaded);
 
             var functions = libraryServices.GetFunctionGroups(libraryPath);
@@ -53,7 +53,7 @@ namespace Dynamo.Tests
 
             // library should be able to load
             string libraryPath = Path.Combine(GetTestDirectory(), @"core\library\Test.ds");
-            libraryServices.ImportLibrary(libraryPath, ViewModel.Model.Logger, SearchModel.ElementType.CustomDll);
+            libraryServices.ImportLibrary(libraryPath, ViewModel.Model.Logger);
             Assert.IsTrue(libraryLoaded);
 
             // open dyn file which uses node in that library
