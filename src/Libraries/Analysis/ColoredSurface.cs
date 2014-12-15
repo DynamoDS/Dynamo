b296551e2d5af67b2f4507bac75e0edf88fe6473
Ian Keough <ian.keough@autodesk.com>
2014-12-14 23:02:08 -0800

    Cleanup unused classes.

--- src/Libraries/Analysis/ColoredSurface.cs
+++ src/Libraries/Analysis/ColoredSurface.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 using Autodesk.DesignScript.Geometry;
 using Autodesk.DesignScript.Interfaces;
@@ -10,134 +8,10 @@ using Autodesk.DesignScript.Runtime;
 
 using DSCore;
 
-using MIConvexHull;
-
 using Point = Autodesk.DesignScript.Geometry.Point;
 
 namespace Analysis
 {
-    // This is a duplication of the Vertex2 class in the Tesselation
-    // library. We need to re-implement here in order to avoid marking 
-    // that class as public.
-    internal class Vertex2 : IVertex, IGraphicItem
-    {
-        public object Tag { get; set; }
-
-        public static Vertex2 FromUV(UV uv)
-        {
-            return new Vertex2(uv.U, uv.V);
-        }
-
-        public Vector AsVector()
-        {
-            return Vector.ByCoordinates(Position[0], Position[1], 0);
-        }
-
-        public Point AsPoint()
-        {
-            return Point.ByCoordinates(Position[0], Position[1]);
-        }
-
-        public Vertex2(double x, double y)
-        {
-            Position = new[] { x, y };
-        }
-
-        public double[] Position { get; set; }
-
-        public void Tessellate(IRenderPackage package, double tol = -1, int maxGridLines = 512)
-        {
-            AsVector().Tessellate(package, tol, maxGridLines);
-        }
-    }
-
-    internal class Vertex2EqualityComparer : IEqualityComparer<Vertex2>
-    {
-        public bool Equals(Vertex2 x, Vertex2 y)
-        {
-            var v1 = x.AsVector();
-            var v2 = y.AsVector();
-
-            return v1.IsAlmostEqualTo(v2);
-        }
-
-        public int GetHashCode(Vertex2 obj)
-        {
-            int hCode = obj.Position[0].GetHashCode() ^ obj.Position[1].GetHashCode();
-            return hCode.GetHashCode();
-        }
-    }
-
-    // This is a duplication of the Cell2 class in the Tesselation.
-    // library. We need to re-implement here in order to avoid marking 
-    // that class as public.
-    internal class Cell2 : TriangulationCell<Vertex2, Cell2>
-    {
-        Point GetCircumcenter()
-        {
-            // From MathWorld: http://mathworld.wolfram.com/Circumcircle.html
-
-            var points = Vertices;
-
-            var m = new double[3, 3];
-
-            // x, y, 1
-            for (int i = 0; i < 3; i++)
-            {
-                m[i, 0] = points[i].Position[0];
-                m[i, 1] = points[i].Position[1];
-                m[i, 2] = 1;
-            }
-            var a = StarMath.determinant(m);
-
-            // size, y, 1
-            for (int i = 0; i < 3; i++)
-            {
-                m[i, 0] = StarMath.norm2(points[i].Position, 2, true);
-            }
-            var dx = -StarMath.determinant(m);
-
-            // size, x, 1
-            for (int i = 0; i < 3; i++)
-            {
-                m[i, 1] = points[i].Position[0];
-            }
-            var dy = StarMath.determinant(m);
-
-            // size, x, y
-            for (int i = 0; i < 3; i++)
-            {
-                m[i, 2] = points[i].Position[1];
-            }
-
-            var s = -1.0 / (2.0 * a);
-            return Point.ByCoordinates(s * dx, s * dy);
-        }
-
-        Point GetCentroid()
-        {
-            return Point.ByCoordinates(Vertices.Select(v => v.Position[0]).Average(), Vertices.Select(v => v.Position[1]).Average());
-        }
-
-        Point circumCenter;
-        public Point Circumcenter
-        {
-            get
-            {
-                return circumCenter = circumCenter ?? GetCircumcenter();
-            }
-        }
-
-        Point centroid;
-        public Point Centroid
-        {
-            get
-            {
-                return centroid = centroid ?? GetCentroid();
-            }
-        }
-    }
-
     public class ColoredSurface : IGraphicItem
     {
         private Surface surface;
