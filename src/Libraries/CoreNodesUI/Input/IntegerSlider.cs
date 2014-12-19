74fa8d82423d8aefca2b42b186a21c8184ea894a
Ian Keough <ian.keough@autodesk.com>
2014-12-19 14:31:59 -0800

    Split slider classes again.
    The previous failed commit, which created one base class for sliders,
    did not account for the treatment of the double and integer types as
    they were loaded from files, causing files using integer sliders with
    the previous build to fail to load their saved values. This commit
    removes the shared slider base class in favor of an ISlider interface,
    and adds additional checks to ensure that slider mins, maxes, and steps,
    automatically reset to appropriate values during changes, and handle
    culture invariant formatting correctly.

--- src/Libraries/CoreNodesUI/Input/IntegerSlider.cs
+++ src/Libraries/CoreNodesUI/Input/IntegerSlider.cs
@@ -1,4 +1,8 @@
-﻿using Autodesk.DesignScript.Runtime;
+﻿using System;
+using System.Globalization;
+using System.Xml;
+
+using Autodesk.DesignScript.Runtime;
 
 using DSCoreNodesUI.Input;
 
@@ -11,8 +15,12 @@ namespace Dynamo.Nodes
     [NodeDescription("A slider that produces integer values.")]
     [SupressImportIntoVM]
     [IsDesignScriptCompatible]
-    public class IntegerSlider : SliderBase
+    public class IntegerSlider : DSCoreNodesUI.Integer, ISlider<int>
     {
+        private int max;
+        private int min;
+        private int step;
+
         public IntegerSlider(WorkspaceModel workspace)
             : base(workspace)
         {
@@ -23,5 +31,166 @@ namespace Dynamo.Nodes
             Step = 1;
             Value = 0;
         }
+
+        public int Max
+        {
+            get { return max; }
+            set
+            {
+                max = value;
+                RaisePropertyChanged("Max");
+            }
+        }
+
+        public int Min
+        {
+            get { return min; }
+            set
+            {
+                min = value;
+                RaisePropertyChanged("Min");
+            }
+        }
+
+        public int Step
+        {
+            get { return step; }
+            set
+            {
+                step = value;
+                RaisePropertyChanged("Step");
+            }
+        }
+
+        protected override bool ShouldDisplayPreviewCore
+        {
+            get { return false; }
+        }
+
+        protected override bool UpdateValueCore(string name, string value)
+        {
+            switch (name)
+            {
+                case "Min":
+                case "MinText":
+                    Min = SliderViewModel<int>.ConvertStringToInt(value);
+                    if (Min > Max)
+                    {
+                        Max = Min;
+                        Value = Max;
+                    }
+                    return true; // UpdateValueCore handled.
+                case "Max":
+                case "MaxText":
+                    Max = SliderViewModel<int>.ConvertStringToInt(value);
+                    if (Max < Min)
+                    {
+                        Min = Max;
+                        Value = Min;
+                    }
+                    return true; // UpdateValueCore handled.
+                case "Value":
+                case "ValueText":
+                    Value = SliderViewModel<int>.ConvertStringToInt(value);
+                    if (Value >= Max)
+                    {
+                        this.Max = Value;
+                    }
+                    if (Value <= Min)
+                    {
+                        this.Min = Value;
+                    }
+                    return true; // UpdateValueCore handled.
+                case "Step":
+                case "StepText":
+                    Step = SliderViewModel<int>.ConvertStringToInt(value);
+                    return true;
+            }
+
+            return base.UpdateValueCore(name, value);
+        }
+
+        #region Load/Save
+
+        protected override void SaveNode(XmlDocument xmlDoc, XmlElement nodeElement, SaveContext context)
+        {
+            base.SaveNode(xmlDoc, nodeElement, context);
+
+            XmlElement outEl = xmlDoc.CreateElement("Range");
+            outEl.SetAttribute("min", Min.ToString(CultureInfo.InvariantCulture));
+            outEl.SetAttribute("max", Max.ToString(CultureInfo.InvariantCulture));
+            outEl.SetAttribute("step", Step.ToString(CultureInfo.InvariantCulture));
+            nodeElement.AppendChild(outEl);
+        }
+
+        protected override void LoadNode(XmlNode nodeElement)
+        {
+            base.LoadNode(nodeElement);
+
+            foreach (XmlNode subNode in nodeElement.ChildNodes)
+            {
+                if (!subNode.Name.Equals("Range"))
+                    continue;
+
+                if (subNode.Attributes == null) continue;
+
+                foreach (XmlAttribute attr in subNode.Attributes)
+                {
+                    if (attr.Name.Equals("min"))
+                        Min = Convert.ToInt16(attr.Value, CultureInfo.InvariantCulture);
+                    else if (attr.Name.Equals("max"))
+                        Max = Convert.ToInt16(attr.Value, CultureInfo.InvariantCulture);
+                    else if (attr.Name.Equals("step"))
+                        Step = Convert.ToInt16(attr.Value, CultureInfo.InvariantCulture);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Serialization/Deserialization Methods
+
+        protected override void SerializeCore(XmlElement element, SaveContext context)
+        {
+            base.SerializeCore(element, context); // Base implementation must be called.
+
+            if (context != SaveContext.Undo) return;
+
+            var xmlDocument = element.OwnerDocument;
+            XmlElement subNode = xmlDocument.CreateElement("Range");
+            subNode.SetAttribute("min", Min.ToString(CultureInfo.InvariantCulture));
+            subNode.SetAttribute("max", Max.ToString(CultureInfo.InvariantCulture));
+            subNode.SetAttribute("step", Step.ToString(CultureInfo.InvariantCulture));
+            element.AppendChild(subNode);
+        }
+
+        protected override void DeserializeCore(XmlElement element, SaveContext context)
+        {
+            base.DeserializeCore(element, context); //Base implementation must be called.
+
+            if (context != SaveContext.Undo) return;
+
+            foreach (XmlNode subNode in element.ChildNodes)
+            {
+                if (!subNode.Name.Equals("Range"))
+                    continue;
+                if (subNode.Attributes == null || (subNode.Attributes.Count <= 0))
+                    continue;
+
+                foreach (XmlAttribute attr in subNode.Attributes)
+                {
+                    if (attr.Name.Equals("min"))
+                        Min = Convert.ToInt16(attr.Value, CultureInfo.InvariantCulture);
+                    else if (attr.Name.Equals("max"))
+                        Max = Convert.ToInt16(attr.Value, CultureInfo.InvariantCulture);
+                    else if (attr.Name.Equals("step"))
+                        Step = Convert.ToInt16(attr.Value, CultureInfo.InvariantCulture);
+                }
+
+                break;
+            }
+        }
+
+        #endregion
     }
 }
