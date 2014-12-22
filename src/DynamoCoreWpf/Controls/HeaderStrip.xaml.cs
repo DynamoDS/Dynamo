994ecfe7f9bebda9749548497432b27a979f98a3
Vladimir Moyseenko <vladimir.moyseenko@autodesk.com>
2014-12-22 11:46:35 +0200

    HeaderStrip control is added

---
+++ src/DynamoCoreWpf/Controls/HeaderStrip.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Dynamo.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for HeaderStrip.xaml
+    /// </summary>
+    public partial class HeaderStrip : UserControl
+    {
+        public HeaderStrip()
+        {
+            InitializeComponent();
+        }
+    }
+}
