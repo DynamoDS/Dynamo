c3bcda69cd40dec6e47de566a7f787c447214179
Yu Ke <yu.ke@autodesk.com>
2014-12-28 22:35:29 +0800

    Rename Apply() to __Apply()

--- test/Engine/ProtoTest/LiveRunnerTests/MicroFeatureTests.cs
+++ test/Engine/ProtoTest/LiveRunnerTests/MicroFeatureTests.cs
@@ -2174,7 +2174,7 @@ r = Equals(x, {41, 42});
             astLiveRunner.ResetVMAndResyncGraph(new List<string> { "FunctionObject.ds" });
             string code = @"
  def foo(x,y ) { return = x + y; }
- f = _SingleFunctionObject(foo, 2, {1}, {null, 42}, true); r = Apply(f, 3);
+ f = _SingleFunctionObject(foo, 2, {1}, {null, 42}, true); r = __Apply(f, 3);
  ";
 
             Guid guid = System.Guid.NewGuid();
