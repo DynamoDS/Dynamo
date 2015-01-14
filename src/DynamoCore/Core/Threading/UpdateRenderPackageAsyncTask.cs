bde95625d943058f132f2cb512144acc8c12c948
aparajit <aparajit.pratap@autodesk.com>
2015-01-14 13:06:47 +0800

    removed unnecessary checks

--- src/DynamoCore/Core/Threading/UpdateRenderPackageAsyncTask.cs
+++ src/DynamoCore/Core/Threading/UpdateRenderPackageAsyncTask.cs
@@ -76,7 +76,7 @@ namespace Dynamo.Core.Threading
                 throw new ArgumentNullException("initParams.DrawableIds");
 
             var nodeModel = initParams.Node;
-            if (!nodeModel.IsUpdated && !nodeModel.ExecutionHint.HasFlag(NodeModel.ExecutionHints.GenerateAst))
+            if (!nodeModel.IsUpdated)
                 return false; // Not has not been updated at all.
 
             // If a node is in either of the following states, then it will not 
