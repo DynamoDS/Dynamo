bd1220017611bf89aef12bbf8b22272e458edfbe
Sriram Pollachisubburaman <reachsriram82@gmail.com>
2015-02-02 08:46:00 -0500

    Changes for Graph Annotation
    Changes for Graph Annotation

--- src/DynamoCoreWpf/Views/Core/AnnotationView.xaml.cs
+++ src/DynamoCoreWpf/Views/Core/AnnotationView.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Dynamo.Selection;
 using Dynamo.UI;
-using Dynamo.UI.Prompts;
 using Dynamo.Utilities;
 using Dynamo.ViewModels;
 using DynCmd = Dynamo.Models.DynamoModel;
@@ -100,6 +91,6 @@ namespace Dynamo.Nodes
         {
             String test = "test";
         }
-        
+
     }
 }
