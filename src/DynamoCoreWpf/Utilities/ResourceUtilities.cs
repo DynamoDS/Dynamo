ccfab23a824239d0c20f74447af11e1ac63b34dc
aparajit <aparajit.pratap@autodesk.com>
2015-02-04 17:13:52 +0800

    disposing unmanaged Intptr object

--- src/DynamoCoreWpf/Utilities/ResourceUtilities.cs
+++ src/DynamoCoreWpf/Utilities/ResourceUtilities.cs
@@ -4,10 +4,13 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
+
 namespace Dynamo.Utilities
 {
     public static class ResourceUtilities
     {
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
 
         public static ImageSource ConvertToImageSource(System.Drawing.Bitmap bmp)
         {
@@ -18,11 +21,18 @@ namespace Dynamo.Utilities
             // TODO - watch out for memory leaks using system.drawing.bitmaps in managed code, see here 
             // http://social.msdn.microsoft.com/Forums/en/csharpgeneral/thread/4e213af5-d546-4cc1-a8f0-462720e5fcde
             // need to call Dispose manually somewhere, or perhaps use a WPF native structure instead of bitmap?
-
+            ImageSource imageSource;
             var hbitmap = bmp.GetHbitmap();
-            var imageSource = Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty,
-                BitmapSizeOptions.FromWidthAndHeight(bmp.Width, bmp.Height));
-            
+            try
+            {
+                imageSource = Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty,
+                    BitmapSizeOptions.FromWidthAndHeight(bmp.Width, bmp.Height));
+            }
+            finally
+            {
+                DeleteObject(hbitmap);
+            }
+
             return imageSource;
         }
     }
