5f3d62289efdb2b2325fe1e573bb56b722af34da
Sriram Pollachisubburaman <reachsriram82@gmail.com>
2015-02-06 13:39:44 -0500

    Changes for dynamo conversion
    Added new node customization view.

---
+++ src/Libraries/CoreNodeModelsWpf/NodeViewCustomizations/ConverterNodeViewCustomization.cs
@@ -0,0 +1,17 @@
+﻿using DSCoreNodesUI.Input;
+using Dynamo.Controls;
+using Dynamo.Wpf.Controls;
+
+namespace Dynamo.Wpf.NodeViewCustomizations
+{
+    class ConverterNodeViewCustomization : INodeViewCustomization<Convert>
+    {       
+        public void CustomizeView(Convert model, NodeView nodeView)
+        {
+            var converter = new DynamoConverterControl(model,nodeView)
+            {
+                DataContext = new SliderViewModel<double>(model)
+            }
+        }
+    }
+}
