028f3dcff7c57a1e624d61d1253d435e623d66e5
aparajit <aparajit.pratap@autodesk.com>
2015-02-12 17:44:26 +0800

    removed extraneous code

--- src/DynamoCoreWpf/ViewModels/Core/DynamoViewModelBranding.cs
+++ src/DynamoCoreWpf/ViewModels/Core/DynamoViewModelBranding.cs
@@ -8,15 +8,5 @@ namespace Dynamo.ViewModels
     partial class DynamoViewModel 
     {
         public IBrandingResourceProvider BrandingResourceProvider { get; private set; }
-
-        public ImageSource AboutBoxIcon
-        {
-            get
-            {
-                return new BitmapImage(
-                        new Uri(@"pack://application:,,,/DynamoCoreWpf;component/UI/Images/AboutWindow/logo_about.png",
-                            UriKind.Absolute));
-            }
-        }
     }
 }
