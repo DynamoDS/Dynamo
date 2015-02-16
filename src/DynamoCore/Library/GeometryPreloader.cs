16cfe46cf566c8fec3838cc0b377a6861999f428
Benglin (Ben) Goh <benglin@outlook.com>
2015-02-16 18:21:20 +0800

    Implemented "GeometryPreloader.Preload" method

--- src/DynamoCore/Library/GeometryPreloader.cs
+++ src/DynamoCore/Library/GeometryPreloader.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Dynamo.Interfaces;
+using DynamoUtilities;
 using ProtoScript.Runners;
 
 namespace Dynamo.Library
@@ -14,6 +17,23 @@ namespace Dynamo.Library
                 throw new ArgumentNullException("configuration");
 
             this.configuration = configuration;
+        }
+
+        internal bool Preload()
+        {
+            if (!configuration.PreloadShapeManager)
+                return false;
+
+            var assemblyPath = Assembly.GetCallingAssembly().Location;
+            var directory = Path.GetDirectoryName(assemblyPath);
+
+            var version = ((int) configuration.Version);
+            var libGFolder = string.Format("libg_{0}", version);
+            var preloaderLocation = Path.Combine(directory, libGFolder);
+
+            var asmLocation = configuration.ShapeManagerPath;
+            Utils.PreloadAsmFromPath(preloaderLocation, asmLocation);
+            return true;
         }
     }
 }
