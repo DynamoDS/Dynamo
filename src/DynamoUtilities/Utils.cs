3d702ce9ed19f4b56d5dee28fe11d552c57e01aa
Benglin (Ben) Goh <benglin@outlook.com>
2015-02-16 16:37:22 +0800

    Implemented utility method "Utils.GetInstalledAsmVersion"

---
+++ src/DynamoUtilities/Utils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DynamoUtilities
+{
+    public class Utils
+    {
+        /// <summary>
+        /// Call this method to determine the version of ASM that is installed 
+        /// on the user machine. The method scans through a list of known Autodesk 
+        /// product folders for ASM binaries with the targeted version.
+        /// </summary>
+        /// <param name="versions">A list of version numbers to check for in order 
+        /// of preference. For example, { 221, 220, 219 }.</param>
+        /// <param name="location">The full path of the directory in which targeted
+        /// ASM binaries are found.</param>
+        /// <returns>Returns a zero based index into the versions list if any 
+        /// installed ASM is found, or -1 otherwise.</returns>
+        /// 
+        public static int GetInstalledAsmVersion(IEnumerable<int> versions, ref string location)
+        {
+            if ((versions == null) || !versions.Any())
+                throw new ArgumentNullException("versions");
+            if (location == null)
+                throw new ArgumentNullException("location");
+
+            location = string.Empty;
+
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            var baseSearchDirectory = Path.Combine(programFiles, "Autodesk");
+
+            try
+            {
+                var root = new DirectoryInfo(baseSearchDirectory);
+                var subDirs = root.GetDirectories();
+                var directories = subDirs.Where(d => d.Name.Contains("Revit")
+                    || d.Name.Contains("Vasari")).ToList();
+
+                if (!directories.Any())
+                    return -1;
+
+                foreach (var version in versions)
+                {
+                    var assemblyName = string.Format("ASMAHL{0}A.DLL", version);
+
+                    foreach (var directoryInfo in directories)
+                    {
+                        if (directoryInfo.GetFiles(assemblyName).Length <= 0)
+                            continue;
+
+                        location = directoryInfo.FullName;
+                        return version;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+
+            return -1;
+        }
+    }
+}
