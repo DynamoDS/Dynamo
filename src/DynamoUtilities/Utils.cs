a8234e516ad72041c2e3c2c5200ec98b9e56f500
Benglin (Ben) Goh <benglin@outlook.com>
2015-02-16 17:08:28 +0800

    Implemented "PreloadAsmFromPath" utility method

--- src/DynamoUtilities/Utils.cs
+++ src/DynamoUtilities/Utils.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 
 namespace DynamoUtilities
 {
@@ -14,9 +15,10 @@ namespace DynamoUtilities
         /// product folders for ASM binaries with the targeted version.
         /// </summary>
         /// <param name="versions">A list of version numbers to check for in order 
-        /// of preference. For example, { 221, 220, 219 }.</param>
+        /// of preference. For example, { 221, 220, 219 }. This argument cannot be 
+        /// null or empty.</param>
         /// <param name="location">The full path of the directory in which targeted
-        /// ASM binaries are found.</param>
+        /// ASM binaries are found. This argument cannot be null.</param>
         /// <returns>Returns a zero based index into the versions list if any 
         /// installed ASM is found, or -1 otherwise.</returns>
         /// 
@@ -62,6 +64,41 @@ namespace DynamoUtilities
             }
 
             return -1;
+        }
+
+        /// <summary>
+        /// Call this method to preload ASM binaries from a specific location. This 
+        /// method does not have a return value, any failures in loading ASM binaries
+        /// will result in an exception being thrown.
+        /// </summary>
+        /// <param name="preloaderPath">Full path to LibG.AsmPreloader.Managed.dll
+        /// assembly. This argument must represent a valid path to the loader.</param>
+        /// <param name="asmLocation">Full path of the folder that contains ASM 
+        /// binaries to load. This argument cannot be null or empty.</param>
+        /// 
+        public static void PreloadAsmFromPath(string preloaderPath, string asmLocation)
+        {
+            if (string.IsNullOrEmpty(preloaderPath) || !File.Exists(preloaderPath))
+                throw new ArgumentException("preloadedPath");
+            if (string.IsNullOrEmpty(asmLocation) || !Directory.Exists(asmLocation))
+                throw new ArgumentException("asmLocation");
+
+            Debug.WriteLine("ASM Preloader: {0}", preloaderPath);
+            Debug.WriteLine("ASM Location: {0}", asmLocation);
+
+            var libG = Assembly.LoadFrom(preloaderPath);
+            var preloadType = libG.GetType("Autodesk.LibG.AsmPreloader");
+
+            var preloadMethod = preloadType.GetMethod("PreloadAsmLibraries",
+                BindingFlags.Public | BindingFlags.Static);
+
+            if (preloadMethod == null)
+                throw new MissingMethodException(@"Method ""PreloadAsmLibraries"" not found");
+
+            var methodParams = new object[] { asmLocation };
+            preloadMethod.Invoke(null, methodParams);
+
+            Debug.WriteLine("Successfully loaded ASM binaries");
         }
     }
 }
