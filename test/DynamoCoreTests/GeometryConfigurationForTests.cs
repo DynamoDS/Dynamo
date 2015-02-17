fa6bc41cbc882cbae5c971e02e463cfe610ed58e
Benglin (Ben) Goh <benglin@outlook.com>
2015-02-17 15:25:10 +0800

    Introduced a new test utility class "GeometryConfigurationForTests"

---
+++ test/DynamoCoreTests/GeometryConfigurationForTests.cs
@@ -0,0 +1,44 @@
+﻿using Dynamo.Interfaces;
+using DynamoUtilities;
+
+namespace Dynamo
+{
+    public class GeometryConfigurationForTests : IGeometryConfiguration
+    {
+        private readonly string shapeManagerPath;
+        private readonly LibraryVersion libraryVersion = LibraryVersion.None;
+
+        public GeometryConfigurationForTests()
+        {
+            var versions = new[]
+            {
+                ((int) LibraryVersion.Version219),
+                ((int) LibraryVersion.Version220),
+                ((int) LibraryVersion.Version221),
+            };
+
+            var location = string.Empty;
+            var installed = Utils.GetInstalledAsmVersion(versions, ref location);
+            if (installed >= 0)
+            {
+                libraryVersion = ((LibraryVersion)versions[installed]);
+                shapeManagerPath = location;
+            }
+        }
+
+        public LibraryVersion Version
+        {
+            get { return libraryVersion; }
+        }
+
+        public string ShapeManagerPath
+        {
+            get { return shapeManagerPath; }
+        }
+
+        public bool PreloadShapeManager
+        {
+            get { return true; }
+        }
+    }
+}
