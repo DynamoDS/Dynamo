8b3c922c869fd445a37e76ee3e5b1e92825851de
Ian Keough <ian.keough@autodesk.com>
2015-02-17 17:59:32 -0800

    Add some code behind for periodic stop/start.

--- src/DynamoCoreWpf/Controls/RunSettingsControl.xaml.cs
+++ src/DynamoCoreWpf/Controls/RunSettingsControl.xaml.cs
@@ -1,4 +1,11 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+using Dynamo.Controls;
+using Dynamo.Models;
+using Dynamo.Utilities;
+using Dynamo.ViewModels;
+using Dynamo.Wpf.ViewModels;
 
 namespace Dynamo.Wpf.Controls
 {
@@ -7,9 +14,60 @@ namespace Dynamo.Wpf.Controls
     /// </summary>
     public partial class RunSettingsControl : UserControl
     {
+        private RunSettingsViewModel vm;
+
         public RunSettingsControl()
         {
             InitializeComponent();
+            Loaded += RunSettingsControl_Loaded;
+        }
+
+        void RunSettingsControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            vm = DataContext as RunSettingsViewModel;
+
+            EnumComboBox.SelectionChanged += EnumComboBox_OnSelectionChanged;
+        }
+
+        /// <summary>
+        /// Use the KeyDown event handler to set the binding to update
+        /// when the enter key is pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UIElement_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+
+            var bindingExpr = RunPeriodTextBox.GetBindingExpression(TextBox.TextProperty);
+            bindingExpr.UpdateSource();
+        }
+
+        /// <summary>
+        /// Use the SelectionChanged event handler to trigger a run
+        /// depending on what type of run is set.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EnumComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var dynamoView = WpfUtilities.FindUpVisualTree<DynamoView>(this);
+            var dynamoVm = dynamoView.DataContext as DynamoViewModel;
+
+            dynamoVm.StopPeriodicTimerCommand.Execute(null);
+            switch (vm.RunType)
+            {
+                case RunType.Manual:
+                    return;
+                case RunType.Automatic:
+                    dynamoVm.RunExpressionCommand.Execute(true);
+                    return;
+                case RunType.Periodic:
+                    dynamoVm.StartPeriodicTimerCommand.Execute(null);
+                    return;
+            }
+            
+            
         }
     }
 }
