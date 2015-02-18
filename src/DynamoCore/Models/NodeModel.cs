8b7e3e967338586f04362555be6edeb80f60b68f
Yu Ke <yu.ke@autodesk.com>
2015-02-18 13:56:18 +0800

    Update tooltip content for PortModel

--- src/DynamoCore/Models/NodeModel.cs
+++ src/DynamoCore/Models/NodeModel.cs
@@ -1142,7 +1142,7 @@ namespace Dynamo.Models
                             p.UsingDefaultValue = true;
                             p.DefaultValueEnabled = true;
                         }
-
+                        p.ToolTipContent = data.ToolTipString;
                         return p;
                     }
 
--- src/DynamoCore/Models/PortModel.cs
+++ src/DynamoCore/Models/PortModel.cs
@@ -91,7 +91,7 @@ namespace Dynamo.Models
             }
         }
 
-        public string ToolTipContent { get; private set; }
+        public string ToolTipContent { get; set; }
 
         public string DefaultValueTip
         {
