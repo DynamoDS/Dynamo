b94c88cf24549b9cf0e6e4ab07789d74cbb87f80
Benglin (Ben) Goh <benglin@outlook.com>
2015-02-25 21:14:41 +0800

    Added UserDefinitions, CommonDefinitions to IPathManager

--- src/DynamoCore/Core/PathManager.cs
+++ src/DynamoCore/Core/PathManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using ProtoCore.AST;
@@ -8,13 +9,28 @@ namespace Dynamo.Core
 {
     public interface IPathManager
     {
+        /// <summary>
+        /// The local directory that contains custom nodes created by the user.
+        /// </summary>
+        string UserDefinitions { get; }
+
+        /// <summary>
+        /// The local directory that contains custom nodes created by all users.
+        /// </summary>
+        string CommonDefinitions { get; }
+
         IEnumerable<string> NodeDirectories { get; }
     }
 
     class PathManager : IPathManager
     {
         private readonly HashSet<string> nodeDirectories;
-        private readonly string dynamoCoreDir; // DynamoCore.dll directory.
+        private readonly string dynamoCoreDir;
+        private readonly string userDataDir;
+        private readonly string commonDataDir;
+
+        private readonly string userDefinitions;
+        private readonly string commonDefinitions;
 
         internal PathManager()
         {
@@ -29,13 +45,68 @@ namespace Dynamo.Core
                     "PathManager starts off within an invalid assembly.");
             }
 
-            nodeDirectories = new HashSet<string>();
-            nodeDirectories.Add(Path.Combine(dynamoCoreDir, "nodes"));
+            userDataDir = CreateFolder(GetUserDataFolder());
+            commonDataDir = CreateFolder(GetCommonDataFolder());
+
+            userDefinitions = CreateFolder(Path.Combine(userDataDir, "definitions"));
+            commonDefinitions = CreateFolder(Path.Combine(commonDataDir, "definitions"));
+
+            nodeDirectories = new HashSet<string>
+            {
+                Path.Combine(dynamoCoreDir, "nodes")
+            };
+        }
+
+        public string UserDefinitions
+        {
+            get { return userDefinitions; }
+        }
+
+        public string CommonDefinitions
+        {
+            get { return commonDefinitions; }
         }
 
         public IEnumerable<string> NodeDirectories
         {
             get { return nodeDirectories; }
+        }
+
+        private string GetUserDataFolder()
+        {
+            var folder = Environment.SpecialFolder.ApplicationData;
+            return GetDynamoDataFolder(folder);
+        }
+
+        private string GetCommonDataFolder()
+        {
+            var folder = Environment.SpecialFolder.CommonApplicationData;
+            return GetDynamoDataFolder(folder);
+        }
+
+        private string GetDynamoDataFolder(Environment.SpecialFolder folder)
+        {
+            var assemblyPath = Path.Combine(dynamoCoreDir, "DynamoCore.dll");
+
+            if (!File.Exists(assemblyPath))
+            {
+                throw new Exception("Dynamo's core path could not be found. " +
+                    "If you are running Dynamo from a test, try specifying the " +
+                    "Dynamo core location in the DynamoBasePath variable in " +
+                    "TestServices.dll.config.");
+            }
+
+            var v = FileVersionInfo.GetVersionInfo(assemblyPath);
+            return Path.Combine(Environment.GetFolderPath(folder), "Dynamo",
+                String.Format("{0}.{1}", v.FileMajorPart, v.FileMinorPart));
+        }
+
+        private static string CreateFolder(string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            return folderPath;
         }
     }
 }
