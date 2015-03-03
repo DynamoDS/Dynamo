938ce082456c8cfe09035e4d5772731a1f545604
Sriram Pollachisubburaman <reachsriram82@gmail.com>
2015-03-03 09:48:18 -0500

    set the workspace changed value to true
    set the workspace changed value to true

--- src/Libraries/CoreNodeModelsWpf/Controls/DynamoSlider.xaml.cs
+++ src/Libraries/CoreNodeModelsWpf/Controls/DynamoSlider.xaml.cs
@@ -45,12 +45,8 @@ namespace Dynamo.Wpf.Controls
 
         private void Slider_OnDragCompleted(object sender, DragCompletedEventArgs e)
         {
-            nodeModel.MarkNodeAsModified(true);           
-            ui.ViewModel.DynamoViewModel.ExecuteCommand(
-               new DynamoModel.UpdateModelValueCommand(
-                    ui.ViewModel.WorkspaceViewModel.Model.Guid,
-                    ui.ViewModel.NodeModel.GUID, "SliderValue",
-                    this.slider.Value.ToString(CultureInfo.InvariantCulture)));
+            nodeModel.MarkNodeAsModified(true);
+            ui.ViewModel.WorkspaceViewModel.HasUnsavedChanges = true;          
         }
 
         protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
