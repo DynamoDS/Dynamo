11e3c267ff659e5592ba0ff26a9dfbc52114512f
Minh Nguyen <minhnb3192@gmail.com>
2015-03-05 16:32:50 +0800

    fix missing README

--- tools/install/CreateInstallers.bat
+++ tools/install/CreateInstallers.bat
@@ -38,15 +38,11 @@ XmlDocumentationsUtility.exe %cwd%\..\..\bin\%OPT_Platform%\%OPT_CONFIGURATION%\
 
 REM Localized resource assemblies
 for %%L in (cs-CZ, de-DE, en-US, es-ES, fr-FR, it-IT, ja-JP, ko-KR, pl-PL, pt-BR, ru-RU, zh-CN, zh-TW) do (
-    robocopy %cwd%\..\..\bin\%OPT_Platform%\%OPT_CONFIGURATION%\%%L %cwd%\temp\bin\lang\%%L License.rtf
+    robocopy %cwd%\..\..\bin\%OPT_Platform%\%OPT_CONFIGURATION%\%%L %cwd%\temp\bin\lang\%%L License.rtf README.txt
     robocopy %cwd%\..\..\bin\%OPT_Platform%\%OPT_CONFIGURATION%\%%L %cwd%\temp\bin\lang\%%L *.dll *.xml *.po *.mo /e
 )
 
-set OPT_Language=en-US
-robocopy %cwd%\..\..\ %cwd%\temp\bin\%OPT_Language% README.md
-pushd %cwd%\temp\bin\%OPT_Language%\
-rename README.md README.txt
-popd 
+
 
 robocopy %cwd%\..\..\doc\distrib\migration_nodes %cwd%\temp\definitions /e
 robocopy %cwd%\..\..\bin\%OPT_Platform%\%OPT_CONFIGURATION%\samples %cwd%\temp\samples /s
