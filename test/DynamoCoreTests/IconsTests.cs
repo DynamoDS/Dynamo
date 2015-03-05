9af07560c35593354a815647a1b59950fed1602f
Anna Osyatnik <anna.osyatnik@autodesk.com>
2015-03-05 17:35:42 +0200

    Paths loaded. DynamoModel created.

---
+++ test/DynamoCoreTests/IconsTests.cs
@@ -0,0 +1,66 @@
+﻿using Dynamo.Models;
+using DynamoShapeManager;
+using DynamoUtilities;
+
+using NUnit.Framework;
+
+using System;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+
+namespace Dynamo.Tests
+{
+    [TestFixture]
+    internal class IconsTests
+    {
+        private static string geometryFactoryPath = "";
+        private IEnumerable searchEntries;
+
+        static readonly string AssemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        static readonly string AnyCPUPath = Directory.GetParent(AssemblyPath).FullName;
+        static readonly string binPath = Directory.GetParent(AnyCPUPath).FullName;
+        static readonly string DynamoPath = Directory.GetParent(binPath).FullName;
+
+        static readonly string ResourcePath = Path.Combine(DynamoPath, @"src\Resources");
+
+        [SetUp]
+        public void PreloadShapeManager()
+        {
+            var exePath = Assembly.GetExecutingAssembly().Location;
+            var rootFolder = Path.GetDirectoryName(exePath);
+
+            var versions = new[]
+            {
+                LibraryVersion.Version219,
+                LibraryVersion.Version220,
+                LibraryVersion.Version221
+            };
+
+            var preloader = new Preloader(rootFolder, versions);
+            preloader.Preload();
+            geometryFactoryPath = preloader.GeometryFactoryPath;
+        }
+
+
+
+        [Test]
+        [Category("UnitTests")]
+        public void SearchForPNGFiles()
+        {
+            var model = DynamoModel.Start(
+                new DynamoModel.StartConfiguration()
+                {
+                    GeometryFactoryPath = geometryFactoryPath,
+                    Preferences = PreferenceSettings.Load()
+                });
+
+            searchEntries = model.SearchModel.SearchEntries;
+
+            foreach (var entry in searchEntries)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
