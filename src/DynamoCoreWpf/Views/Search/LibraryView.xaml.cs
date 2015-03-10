d532fd08472f03fdb9bcddfe45f3e35be304752d
Anna Osyatnik <anna.osyatnik@autodesk.com>
2015-03-10 12:59:51 +0200

    Check for NodeCategoryViewModel

--- src/DynamoCoreWpf/Views/Search/LibraryView.xaml.cs
+++ src/DynamoCoreWpf/Views/Search/LibraryView.xaml.cs
@@ -166,7 +166,7 @@ namespace Dynamo.UI.Views
         // here to ignore the immediate mouse-enter event.
         private void OnExpanderButtonMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if ((sender as Button).DataContext is NodeSearchElement)
+            if ((sender as Button).DataContext is NodeCategoryViewModel)
             {
                 libraryToolTipPopup.SetDataContext(null, true);
                 ignoreMouseEnter = true;
