8b94a17506e2cd35e2550c273760110e0764aa2c
Ian Keough <ian.keough@autodesk.com>
2015-03-11 12:30:21 -0700

    Fix AreaFromString and VolumeFromString tests...
    Area and Volume now have the same behavior as length, which is that
    their AreaUnit or VolumeUnit are set at the time of construction and not
    set conditionally based on the parsed value.

--- src/Libraries/DynamoUnits/Units.cs
+++ src/Libraries/DynamoUnits/Units.cs
@@ -1014,11 +1014,6 @@ namespace DynamoUnits
             double sq_mm, sq_cm, sq_m, sq_in, sq_ft;
             Utils.ParseAreaFromString(value, out sq_in, out sq_ft, out sq_mm, out sq_cm, out sq_m);
 
-            if (sq_mm != 0 || sq_cm != 0 || sq_m != 0)
-                AreaUnit = AreaUnit.SquareMeter;
-            else
-                AreaUnit = AreaUnit.SquareFoot;
-
             total += sq_mm / ToSquareMillimeters;
             total += sq_cm / ToSquareCentimeters;
             total += sq_m;
@@ -1371,11 +1366,6 @@ namespace DynamoUnits
 
             double cu_mm, cu_cm, cu_m, cu_in, cu_ft;
             Utils.ParseVolumeFromString(value, out cu_in, out cu_ft, out cu_mm, out cu_cm, out cu_m);
-
-            if (cu_mm != 0 || cu_cm != 0 || cu_m != 0)
-                VolumeUnit = VolumeUnit.CubicMeter;
-            else
-                VolumeUnit = VolumeUnit.CubicFoot;
 
             total += cu_mm / ToCubicMillimeter;
             total += cu_cm / ToCubicCentimeter;
