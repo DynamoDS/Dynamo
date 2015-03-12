50767a34acba28bdd12679e771e6e1b0c28104e2
Anna Osyatnik <anna.osyatnik@autodesk.com>
2015-03-12 11:06:44 +0200

    Revit stuff deleted.

--- src/DynamoCore/Nodes/NodeCategories.cs
+++ src/DynamoCore/Nodes/NodeCategories.cs
@@ -82,18 +82,6 @@ namespace Dynamo.Nodes
         public const string GEOMETRY_EXPERIMENTAL_MODIFY = "Geometry.Experimental.Modify";
         public const string GEOMETRY_EXPERIMENTAL_VIEW = "Geometry.Experimental.View";
 
-        public const string REVIT = "Revit";
-        public const string REVIT_DOCUMENT = "Revit.Document";
-        public const string REVIT_DATUMS = "Revit.Datums";
-        public const string REVIT_FAMILIES = "Revit.Families";
-        public const string REVIT_SELECTION = "Revit.Selection";
-        public const string REVIT_VIEW = "Revit.View";
-        public const string REVIT_REFERENCE = "Revit.Reference";
-        public const string REVIT_PARAMETERS = "Revit.Parameters";
-        public const string REVIT_BAKE = "Revit.Bake";
-        public const string REVIT_API = "Revit.API";
-        public const string REVIT_ELEMENTS_DIVIDEDPATH_ACTION = "Revit.Elements.DividedPath.Actions";
-
         public const string ANALYZE = "Analyze";
         public const string ANALYZE_MEASURE = "Analyze.Measure";
         public const string ANALYZE_DISPLAY = "Analyze.Display";
