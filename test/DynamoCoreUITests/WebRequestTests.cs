abdc3fb7dc7ab8328620c4728cd195eaf21e0b1d
Ian Keough <ian.keough@autodesk.com>
2015-03-13 11:28:45 -0700

    Add a system test for WebRequest

---
+++ test/DynamoCoreUITests/WebRequestTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+using SystemTestServices;
+
+using DSCoreNodesUI;
+
+using Dynamo.Models;
+using Dynamo.Tests;
+
+using NUnit.Framework;
+
+namespace DynamoCoreUITests
+{
+    [TestFixture]
+    public class WebRequestTests : SystemTestBase
+    {
+        [Test]
+        public void WebRequest()
+        {
+            var openPath = Path.Combine(GetTestDirectory(ExecutingDirectory), @"core\web_request\WebRequest.dyn");
+            ViewModel.OpenCommand.Execute(openPath);
+
+            var ws = ViewModel.Model.CurrentWorkspace as HomeWorkspaceModel;
+            var webRequestNode = ws.FirstNodeFromWorkspace<WebRequest>();
+            Assert.NotNull(webRequestNode);
+
+            var response = GetPreviewValue(webRequestNode.GUID.ToString()).ToString();
+            Assert.True(response.Contains("Search the world's information"));
+        }
+    }
+}
