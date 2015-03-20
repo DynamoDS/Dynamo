c0aa6432c5ff83e13c2910e1bd9eb783b57a4284
Anna Osyatnik <anna.osyatnik@autodesk.com>
2015-03-20 14:51:02 +0200

    Private member is at the beginning of document.

--- src/DynamoCore/Library/DocumentationServices.cs
+++ src/DynamoCore/Library/DocumentationServices.cs
@@ -27,6 +27,58 @@ namespace Dynamo.DSEngine
                 _cached.Clear();
         }
 
+        private static XmlReader GetForAssembly(string assemblyPath, IPathManager pathManager)
+        {
+            if (_triedPaths.ContainsKey(assemblyPath))
+            {
+                return _triedPaths[assemblyPath] ? _cached[assemblyPath] : null;
+            }
+
+            var documentationPath = "";
+            if (ResolveForAssembly(assemblyPath, pathManager, ref documentationPath))
+            {
+                var c = XmlReader.Create(documentationPath);
+                _triedPaths.Add(assemblyPath, true);
+                _cached.Add(assemblyPath, c);
+                return c;
+            }
+
+            _triedPaths.Add(assemblyPath, false);
+            return null;
+        }
+
+        private static bool ResolveForAssembly(string assemblyLocation,
+            IPathManager pathManager, ref string documentationPath)
+        {
+            if (pathManager != null)
+                pathManager.ResolveLibraryPath(ref assemblyLocation);
+
+            if (!File.Exists(assemblyLocation))
+            {
+                return false;
+            }
+
+            var assemblyPath = Path.GetFullPath(assemblyLocation);
+
+            var baseDir = Path.GetDirectoryName(assemblyPath);
+            var xmlFileName = Path.GetFileNameWithoutExtension(assemblyPath) + ".xml";
+
+            var language = System.Threading.Thread.CurrentThread.CurrentUICulture.ToString();
+            var localizedResPath = Path.Combine(baseDir, language);
+            documentationPath = Path.Combine(localizedResPath, xmlFileName);
+
+            if (File.Exists(documentationPath))
+                return true;
+
+            localizedResPath = Path.Combine(baseDir, UI.Configurations.FallbackUiCulture);
+            documentationPath = Path.Combine(localizedResPath, xmlFileName);
+            if (File.Exists(documentationPath))
+                return true;
+
+            documentationPath = Path.Combine(baseDir, xmlFileName);
+            return File.Exists(documentationPath);
+        }
+
         public static string GetSummary(this FunctionDescriptor member)
         {
             var assemblyName = member.Assembly;
@@ -119,58 +171,6 @@ namespace Dynamo.DSEngine
             Summary,
             Parameter,
             SearchTags
-        }
-
-        private static XmlReader GetForAssembly(string assemblyPath, IPathManager pathManager)
-        {
-            if (_triedPaths.ContainsKey(assemblyPath))
-            {
-                return _triedPaths[assemblyPath] ? _cached[assemblyPath] : null;
-            }
-
-            var documentationPath = "";
-            if (ResolveForAssembly(assemblyPath, pathManager, ref documentationPath))
-            {
-                var c = XmlReader.Create(documentationPath);
-                _triedPaths.Add(assemblyPath, true);
-                _cached.Add(assemblyPath, c);
-                return c;
-            }
-
-            _triedPaths.Add(assemblyPath, false);
-            return null;
-        }
-
-        private static bool ResolveForAssembly(string assemblyLocation,
-            IPathManager pathManager, ref string documentationPath)
-        {
-            if (pathManager != null)
-                pathManager.ResolveLibraryPath(ref assemblyLocation);
-
-            if (!File.Exists(assemblyLocation))
-            {
-                return false;
-            }
-
-            var assemblyPath = Path.GetFullPath(assemblyLocation);
-
-            var baseDir = Path.GetDirectoryName(assemblyPath);
-            var xmlFileName = Path.GetFileNameWithoutExtension(assemblyPath) + ".xml";
-
-            var language = System.Threading.Thread.CurrentThread.CurrentUICulture.ToString();
-            var localizedResPath = Path.Combine(baseDir, language);
-            documentationPath = Path.Combine(localizedResPath, xmlFileName);
-
-            if (File.Exists(documentationPath))
-                return true;
-
-            localizedResPath = Path.Combine(baseDir, UI.Configurations.FallbackUiCulture);
-            documentationPath = Path.Combine(localizedResPath, xmlFileName);
-            if (File.Exists(documentationPath))
-                return true;
-
-            documentationPath = Path.Combine(baseDir, xmlFileName);
-            return File.Exists(documentationPath);
         }
 
         #region Fully qualified name creation
