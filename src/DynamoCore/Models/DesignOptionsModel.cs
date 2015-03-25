c46330cfad2980ee9ee5b89e631fa6d5f56b3168
Michael Kirschner <mjk.kirschner@gmail.com>
2015-03-25 13:43:58 -0400

    add method on Design Options Model Set to load an options set at workspace load time - currently all design options population and commands have been added to workspace model but these should probably live on home workspace model, I'm not sure that custom nodes should have individual options sets, instead they should probably just be set from parameters.

--- src/DynamoCore/Models/DesignOptionsModel.cs
+++ src/DynamoCore/Models/DesignOptionsModel.cs
@@ -39,14 +39,14 @@ namespace Dynamo.Models
         //but not actually write this xml to a file until the graph is saved
         protected override void SerializeCore(System.Xml.XmlElement element, SaveContext context)
         {
-            
+
             foreach (var state in designStates)
             {
                 var parent = element.OwnerDocument.CreateElement("DesignOptionsState");
                 element.AppendChild(parent);
                 parent.SetAttribute("Name", state.Name);
                 parent.SetAttribute("Description", state.Description);
-                
+
                 //the states are already serialized
                 foreach (var serializedNode in state.SerializedNodes)
                 {
@@ -54,7 +54,7 @@ namespace Dynamo.Models
                     var importNode = parent.OwnerDocument.ImportNode(serializedNode, true);
                     parent.AppendChild(importNode);
                 }
-               
+
             }
         }
 
@@ -68,19 +68,66 @@ namespace Dynamo.Models
         /// <summary>
         /// method to create and add a new state to this design options set
         /// </summary>
-        public void CreateNewState(string name, string description,List<NodeModel> currentSelection)
+        public void CreateNewState(string name, string description, List<NodeModel> currentSelection)
         {
-          //TODO filter current selection down to inputs before passing it on
+            //TODO filter current selection down to inputs before passing it on
             //var inputs = currentSelection.Where(x=>supportedInputTypes.Contains(x.GetType())).ToList();
             var inputs = currentSelection;
-          //then create a new designstate and store it in the dictionary looked up via it's name
-          //TODO should use a GUID instead?
+            //then create a new designstate and store it in the dictionary looked up via it's name
+            //TODO should use a GUID instead?
             var newstate = new DesignOptionsState(name, description, inputs);
             designStates.Add(newstate);
         }
         #endregion
 
 
-       
+
+
+        internal static DesignOptionsSetModel LoadFromXml(XmlDocument xmlDoc, NodeGraph nodegraph)
+        {
+            var loadedStateSet = new DesignOptionsSetModel();
+
+            //create a new state inside the set foreach state present in the xmldoc
+
+            foreach (XmlElement element in xmlDoc.ChildNodes)
+            {
+                if (element.Name == typeof(DesignOptionsSetModel).ToString())
+                {
+                    foreach (XmlElement stateNode in element.ChildNodes)
+                    {
+                        var name = stateNode.GetAttribute("Name");
+                        var des = stateNode.GetAttribute("Description");
+                        var nodes = new List<NodeModel>();
+
+                        //now find the nodes we're looking for by their guids in the loaded nodegraph
+                        //it's possible they may no longer be present, and we must not fail to set the
+                        //TODO//rest of the nodes but log this to the console.
+
+                        //iterate each actual saved nodemodel in each state
+                        foreach (XmlElement node in stateNode.ChildNodes)
+                        {
+                            var ID = Guid.Parse(stateNode.GetAttribute("guid"));
+                            var nodename = stateNode.GetAttribute("nickname");
+                            var nodebyGuid = nodegraph.Nodes.Where(x => x.GUID == ID).ToList();
+                            if (nodebyGuid.Count > 0)
+                            {
+                                nodes.Add(nodebyGuid.First());
+
+                            }
+                            else
+                            {
+                                //TODO possibly hookup to dynamologger
+                                Console.WriteLine(nodename + ID.ToString() + " could not be found in the loaded dyn");
+                            }
+
+                        }
+
+                        loadedStateSet.CreateNewState(name, des, nodes);
+                    }
+                }
+            }
+            return loadedStateSet;
+        }
+
     }
 }
