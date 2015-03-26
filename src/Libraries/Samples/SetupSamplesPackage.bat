e1f3f1aabdc1481c12a7b34a24bfed6d28ba2f23
Ian Keough <ian.keough@autodesk.com>
2015-03-26 15:02:51 -0700

    Cleanup samples.
    - Build the samples to local build directories.
    - Include a script to wrap all samples into a package.
    - Don't used the shared assembly info for the UI samples. It breaks
    custom UI.

--- src/Libraries/Samples/SetupSamplesPackage.bat
+++ src/Libraries/Samples/SetupSamplesPackage.bat
@@ -3,15 +3,18 @@ set binDir=%packagesDir%\bin
 set nodesDir=%packagesDir%\bin\nodes
 set dyfDir=%packagesDir%\dyf
 set extraDir=%packagesDir%\extra
+set resourcesDir=%binDir%\en-US
 
 set sampleLibraryTestsDir=%cd%\SampleLibraryTests\bin\Debug
 set sampleLibraryUIDir=%cd%\SampleLibraryUI\bin\Debug
+set sampleLibraryUIResourcesDir=%cd%\SampleLibraryUI\bin\Debug\en-us
 set sampleLibraryZeroTouchDir=%cd%\SampleLibraryZeroTouch\bin\Debug
 
 if not exist "%packagesDir%" mkdir "%packagesDir%"
 if not exist "%binDir%" mkdir "%binDir%"
 if not exist "%dyfDir%" mkdir "%dyfDir%"
 if not exist "%extraDir%" mkdir "%extraDir%"
+if not exist "%resourcesDir%" mkdir "%resourcesDir%"
 
 xcopy "%sampleLibraryTestsDir%\*.dll" "%binDir%" /Y
 xcopy "%sampleLibraryTestsDir%\*.pdb" "%binDir%" /Y
@@ -20,6 +23,7 @@ xcopy "%sampleLibraryTestsDir%\*.dyn" "%extraDir%" /Y
 xcopy "%sampleLibraryUIDir%\*.dll" "%binDir%" /Y
 xcopy "%sampleLibraryUIDir%\*.pdb" "%binDir%" /Y
 xcopy "%sampleLibraryUIDir%\*.xml" "%binDir%" /Y
+xcopy "%sampleLibraryUIResourcesDir%\*" "%resourcesDir%" /Y
 
 xcopy "%sampleLibraryZeroTouchDir%\*.dll" "%binDir%" /Y
 xcopy "%sampleLibraryZeroTouchDir%\*.pdb" "%binDir%" /Y
