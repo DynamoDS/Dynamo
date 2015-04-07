fb9d580ba306c9e2c3a2969442d8880e7fdf6381
Patrick Tierney <patrick.tierney@autodesk.com>
2015-04-07 08:21:02 -0700

    Added two new projects for ExportToSAT with units dropdown node

---
+++ src/Libraries/GeometryUIWpf/Controls/ExportAsSATControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Dynamo.Controls;
+using DSCoreNodesUI;
+using Dynamo.UI.Commands;
+using System.Xml;
+
+namespace GeometryUIWpf.Controls
+{
+    /// <summary>
+    /// Interaction logic for ExportAsSATControl.xaml
+    /// </summary>
+    public partial class ExportAsSATControl : UserControl
+    {
+        public ExportAsSATControl(DynamoConvert Model, NodeView nodeView)
+        {
+            InitializeComponent();
+        }
+    }
+}
