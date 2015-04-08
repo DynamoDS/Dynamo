2f654a3118dd7625eb4c1a3d9feb5fba8fa30e07
Yu Ke <yu.ke@autodesk.com>
2015-04-08 12:05:18 +0800

    Update test cases for topological sort

--- test/DynamoCoreTests/AstBuilderTest.cs
+++ test/DynamoCoreTests/AstBuilderTest.cs
@@ -267,5 +267,263 @@ namespace Dynamo.Tests
                 Assert.IsTrue(nodePosMap[7] > nodePosMap[5]);
             }
         }
+
+        [Test]
+        public void TestTopologicalSortForGraph1()
+        {
+            // The connections of CBNs are
+            // 
+            //  1 <----> 2
+            //
+            var model = ViewModel.Model;
+
+            string openPath = Path.Combine(TestDirectory, @"core\astbuilder\cyclic.dyn");
+            ViewModel.OpenCommand.Execute(openPath);
+
+            var sortedNodes = AstBuilder.TopologicalSortForGraph(model.CurrentWorkspace.Nodes);
+            Assert.AreEqual(sortedNodes.Count(), 2);
+        }
+
+        [Test]
+        public void TestTopologicalSortForGraph2()
+        {
+            // The connections of CBNs are
+            // 
+            //      +----> 2
+            //     /
+            //   1 ----> 3
+            //     \    
+            //      +----> 4
+            // 
+            var model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\astbuilder\multioutputs.dyn");
+            ViewModel.OpenCommand.Execute(openPath);
+            var nodes = model.CurrentWorkspace.Nodes.ToList();
+
+            int shuffleCount = 10;
+            var shuffle = new ShuffleUtil<NodeModel>(nodes);
+
+            for (int i = 0; i < shuffleCount; ++i)
+            {
+                var sortedNodes = AstBuilder.TopologicalSortForGraph(shuffle.ShuffledList).ToList();
+                Assert.AreEqual(sortedNodes.Count(), 4);
+
+                List<int> nickNames = sortedNodes.Select(node => Int32.Parse(node.NickName)).ToList();
+
+                Dictionary<int, int> nodePosMap = new Dictionary<int, int>();
+                for (int idx = 0; idx < nickNames.Count; ++idx)
+                {
+                    nodePosMap[nickNames[idx]] = idx;
+                }
+
+                // no matter input nodes in whatever order, there invariants 
+                // should hold
+                Assert.IsTrue(nodePosMap[2] > nodePosMap[1]);
+                Assert.IsTrue(nodePosMap[3] > nodePosMap[1]);
+                Assert.IsTrue(nodePosMap[4] > nodePosMap[1]);
+            }
+        }
+
+        [Test]
+        public void TestTopologicalSortForGraph3()
+        {
+            // The connections of CBNs are
+            // 
+            //   1 ----+ 
+            //          \
+            //   2 ----> 4
+            //          /
+            //   3 ----+
+            // 
+            var model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\astbuilder\multiinputs.dyn");
+            ViewModel.OpenCommand.Execute(openPath);
+            var nodes = model.CurrentWorkspace.Nodes.ToList();
+
+            int shuffleCount = 10;
+            var shuffle = new ShuffleUtil<NodeModel>(nodes);
+
+            for (int i = 0; i < shuffleCount; ++i)
+            {
+                var sortedNodes = AstBuilder.TopologicalSortForGraph(shuffle.ShuffledList).ToList();
+                Assert.AreEqual(sortedNodes.Count(), 4);
+
+                List<int> nickNames = sortedNodes.Select(node => Int32.Parse(node.NickName)).ToList();
+
+                Dictionary<int, int> nodePosMap = new Dictionary<int, int>();
+                for (int idx = 0; idx < nickNames.Count; ++idx)
+                {
+                    nodePosMap[nickNames[idx]] = idx;
+                }
+
+                // no matter input nodes in whatever order, there invariants 
+                // should hold
+                Assert.IsTrue(nodePosMap[1] < nodePosMap[4]);
+                Assert.IsTrue(nodePosMap[2] < nodePosMap[4]);
+                Assert.IsTrue(nodePosMap[3] < nodePosMap[4]);
+                Assert.IsTrue(nodePosMap[1] < nodePosMap[2]);
+                Assert.IsTrue(nodePosMap[2] < nodePosMap[3]);
+                Assert.IsTrue(nodePosMap[3] < nodePosMap[4]);
+            }
+        }
+
+        [Test]
+        public void TestTopologicalSortForGraph4()
+        {
+            // The connections of CBNs are
+            //   
+            //  +---------------+    
+            //  |               |
+            //  |               v
+            //  2 ----> 3 ----> 1
+            // 
+            var model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\astbuilder\tri.dyn");
+            ViewModel.OpenCommand.Execute(openPath);
+            var nodes = model.CurrentWorkspace.Nodes.ToList();
+
+            int shuffleCount = 10;
+            var shuffle = new ShuffleUtil<NodeModel>(nodes);
+
+            for (int i = 0; i < shuffleCount; ++i)
+            {
+                var sortedNodes = AstBuilder.TopologicalSortForGraph(shuffle.ShuffledList).ToList();
+                Assert.AreEqual(sortedNodes.Count(), 3);
+
+                List<int> nickNames = sortedNodes.Select(node => Int32.Parse(node.NickName)).ToList();
+
+                Dictionary<int, int> nodePosMap = new Dictionary<int, int>();
+                for (int idx = 0; idx < nickNames.Count; ++idx)
+                {
+                    nodePosMap[nickNames[idx]] = idx;
+                }
+
+                // no matter input nodes in whatever order, there invariants 
+                // should hold
+                Assert.IsTrue(nodePosMap[2] < nodePosMap[3]);
+                Assert.IsTrue(nodePosMap[3] < nodePosMap[1]);
+            }
+        }
+
+
+        [Test]
+        public void TestTopologicalSortForGraph5()
+        {
+            // The connections of CBNs are
+            //   
+            // 1 <---- 2 <----> 3 <---- 4
+            //
+            var model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\astbuilder\linear.dyn");
+            ViewModel.OpenCommand.Execute(openPath);
+            var nodes = model.CurrentWorkspace.Nodes.ToList();
+
+            int shuffleCount = 10;
+            var shuffle = new ShuffleUtil<NodeModel>(nodes);
+
+            for (int i = 0; i < shuffleCount; ++i)
+            {
+                var sortedNodes = AstBuilder.TopologicalSortForGraph(shuffle.ShuffledList).ToList();
+                Assert.AreEqual(sortedNodes.Count(), 4);
+
+                List<int> nickNames = sortedNodes.Select(node => Int32.Parse(node.NickName)).ToList();
+
+                Dictionary<int, int> nodePosMap = new Dictionary<int, int>();
+                for (int idx = 0; idx < nickNames.Count; ++idx)
+                {
+                    nodePosMap[nickNames[idx]] = idx;
+                }
+
+                // no matter input nodes in whatever order, there invariants 
+                // should hold
+                Assert.IsTrue(nodePosMap[4] < nodePosMap[3]);
+                Assert.IsTrue(nodePosMap[4] < nodePosMap[2]);
+                Assert.IsTrue(nodePosMap[3] < nodePosMap[1]);
+                Assert.IsTrue(nodePosMap[2] < nodePosMap[1]);
+            }
+        }
+
+        [Test]
+        public void TestTopologicalSortForGraph6()
+        {
+            // The connections of CBNs are
+            //
+            //                   1
+            //                   ^
+            //                   |
+            //                   2
+            //                   ^
+            //                   |
+            //  6 <---- 4 <----> 3 <----> 5 ----> 7          8 ----> 9
+            // 
+            var model = ViewModel.Model;
+
+            string openPath = Path.Combine(TestDirectory, @"core\astbuilder\complex.dyn");
+            ViewModel.OpenCommand.Execute(openPath);
+
+            var nodes = model.CurrentWorkspace.Nodes.ToList();
+            int shuffleCount = 10;
+            var shuffle = new ShuffleUtil<NodeModel>(nodes);
+
+            for (int i = 0; i < shuffleCount; ++i)
+            {
+                nodes = shuffle.ShuffledList;
+
+                var sortedNodes = AstBuilder.TopologicalSortForGraph(nodes).ToList();
+                Assert.AreEqual(sortedNodes.Count(), 9);
+
+                var nickNames = sortedNodes.Select(node => Int32.Parse(node.NickName)).ToList();
+                var nodePosMap = new Dictionary<int, int>();
+                for (int idx = 0; idx < nickNames.Count; ++idx)
+                {
+                    nodePosMap[nickNames[idx]] = idx;
+                }
+
+                // no matter input nodes in whatever order, there invariants 
+                // should hold
+                Assert.IsTrue(nodePosMap[1] > nodePosMap[2]);
+                Assert.IsTrue(nodePosMap[6] > nodePosMap[4]);
+                Assert.IsTrue(nodePosMap[7] > nodePosMap[5]);
+                Assert.IsTrue(nodePosMap[5] > nodePosMap[3]
+                    || nodePosMap[2] > nodePosMap[3]
+                    || nodePosMap[4] > nodePosMap[3]);
+                Assert.IsTrue(nodePosMap[9] > nodePosMap[8]);
+            }
+        }
+
+        [Test]
+        public void TestTopologicalSortForGraph7()
+        {
+            // The connections of CBNs are
+            //
+            //     1 ----> 2 ----> 3 ----+
+            //                           |
+            //     4 ----> 5 ----> 6 ----+
+            //                           |
+            //                           +----> 13
+            //                           |
+            //     7 ----> 8 ----> 9 ----+
+            //                           |
+            //    10 ---> 11 ---->12 ----+
+            var model = ViewModel.Model;
+
+            string openPath = Path.Combine(TestDirectory, @"core\astbuilder\multiinputs2.dyn");
+            ViewModel.OpenCommand.Execute(openPath);
+
+            var nodes = model.CurrentWorkspace.Nodes.ToList();
+            int shuffleCount = 10;
+            var shuffle = new ShuffleUtil<NodeModel>(nodes);
+
+            for (int i = 0; i < shuffleCount; ++i)
+            {
+                nodes = shuffle.ShuffledList;
+
+                var sortedNodes = AstBuilder.TopologicalSortForGraph(nodes).ToList();
+                Assert.AreEqual(sortedNodes.Count(), 13);
+
+                var nickNames = sortedNodes.Select(node => Int32.Parse(node.NickName)).ToList();
+                Assert.IsTrue(nickNames.SequenceEqual(Enumerable.Range(1, 13))); 
+            }
+        }
     }
 }
