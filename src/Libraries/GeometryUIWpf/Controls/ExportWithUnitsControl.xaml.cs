cc52382c96dc253cf515b38ef257ba27baeab9ce
Patrick Tierney <patrick.tierney@autodesk.com>
2015-04-08 21:49:23 -0700

    Stubbed out node, in WPF and NodeModel. Does not do actual export

---
+++ src/Libraries/GeometryUIWpf/Controls/ExportWithUnitsControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Dynamo.Controls;
+using DSCoreNodesUI;
+
+using Dynamo.Nodes;
+using Dynamo.UI.Commands;
+using System.Xml;
+
+using GeometryUI;
+
+
+namespace Dynamo.Wpf.Controls
+{
+    /// <summary>
+    /// Interaction logic for ExportAsSATControl.xaml
+    /// </summary>
+    public partial class ExportWithUnitsControl : UserControl
+    {
+        public ExportWithUnitsControl(ExportWithUnits Model, NodeView nodeView)
+        {
+            InitializeComponent();
+        }
+    }
+}
