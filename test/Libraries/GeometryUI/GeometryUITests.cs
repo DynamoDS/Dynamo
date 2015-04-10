835108c6c9690ee4fc89f5364047ab28146eaf19
Patrick Tierney <patrick.tierney@autodesk.com>
2015-04-09 20:47:20 -0700

    Commented out and removed the test project, as Dynamo doesn't appear to start up the geometry engine

--- test/Libraries/GeometryUI/GeometryUITests.cs
+++ test/Libraries/GeometryUI/GeometryUITests.cs
@@ -1,21 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Autodesk.DesignScript.Geometry;
+
+using DynamoConversions;
 
 using NUnit.Framework;
 
 namespace GeometryUITests
 {
-    [TestFixture]
-    public class GeometryUITests
-    {
-        [Test]
-        [Category("UnitTests")]
-        public void ExportAsSAT_ExportsWithCorrectUnits()
-        {
+    //[TestFixture]
+    //public class GeometryUITests
+    //{
+    //    [Test]
+    //    [Category("UnitTests")]
+    //    public void ExportAsSAT_ExportsWithCorrectUnits()
+    //    {
+    //        var temp_file = System.IO.Path.GetTempFileName();
+    //        var c = Cuboid.ByLengths(3, 4, 5);
 
-        }
-    }
+    //        var testUnit = ConversionUnit.Feet;
+    //        var conversionFactor = Conversions.ConversionDictionary[testUnit]*1000;
+
+    //        c.ExportToSAT(temp_file, conversionFactor);
+
+    //        int i = 0;
+
+    //        foreach (string line in File.ReadLines(temp_file))
+    //        {
+    //            ++i;
+
+    //            if (i != 3)
+    //                continue;
+
+    //            var fields = line.Split(' ');
+
+    //            var unitsString = fields[0];
+
+    //            var unit = Convert.ToDouble(unitsString);
+
+    //            Assert.IsTrue(Math.Abs(unit) - conversionFactor < 0.0001);
+
+    //            break;
+    //        }
+    //    }
+    //}
 }
