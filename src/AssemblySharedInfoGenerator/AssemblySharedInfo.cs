6e30b857e5c5101013a599a7c0abeff2c18d58d4
Peter Boyer <peter.b.boyer@gmail.com>
2015-04-17 11:29:22 -0400

    Fix merge conflict

--- src/AssemblySharedInfoGenerator/AssemblySharedInfo.cs
+++ src/AssemblySharedInfoGenerator/AssemblySharedInfo.cs
@@ -64,8 +64,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-<<<<<<< HEAD
+
 [assembly: AssemblyFileVersion("0.8.1.1056")]
-=======
-[assembly: AssemblyFileVersion("0.8.1.1046")]
->>>>>>> upstream/master
