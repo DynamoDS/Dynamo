f4b6160661190b3bc3fada361a7de25f678e35d0
Ian Keough <ian.keough@autodesk.com>
2015-04-20 06:57:46 -0700

    Make color resetting methods static.

--- src/Libraries/CoreNodes/GeometryColor.cs
+++ src/Libraries/CoreNodes/GeometryColor.cs
@@ -5,8 +5,6 @@ using Autodesk.DesignScript.Geometry;
 using Autodesk.DesignScript.Interfaces;
 using Autodesk.DesignScript.Runtime;
 
-using Geometry = Autodesk.DesignScript.Geometry.Geometry;
-
 namespace DSCore
 {
     public class Display :  IGraphicItem
@@ -53,9 +51,9 @@ namespace DSCore
 
             geometry.Tessellate(package, tol, maxGridLines);
 
-            SetColorOnArray(package.LineStripVertexColors, color, lineStripStartIndex);
-            SetColorOnArray(package.PointVertexColors, color, pointVertexStartIndex);
-            SetColorOnArray(package.TriangleVertexColors, color, triangleVertexStartIndex);
+            package.LineStripVertexColors = ResetColorOnArray(package.LineStripVertexColors, color, lineStripStartIndex);
+            package.PointVertexColors = ResetColorOnArray(package.PointVertexColors, color, pointVertexStartIndex);
+            package.TriangleVertexColors = ResetColorOnArray(package.TriangleVertexColors, color, triangleVertexStartIndex);
 
             var surf = geometry as Surface;
             if (surf != null)
@@ -68,7 +66,7 @@ namespace DSCore
                                 tol,
                                 maxGridLines));
                 var end = package.LineStripVertexColors.Count - 1;
-                ReColorVerticesFromTo(start, end, package);
+                ReColorLineVerticesFromTo(start, end, package);
             }
 
             var solid = geometry as Solid;
@@ -82,53 +80,62 @@ namespace DSCore
                                 tol,
                                 maxGridLines));
                 var end = package.LineStripVertexColors.Count - 1;
-                ReColorVerticesFromTo(start, end, package);
+                ReColorLineVerticesFromTo(start, end, package);
             }
 
-            for (var i = 0; i < package.TriangleVertices.Count; i += 3)
+            var existingVerts = package.TriangleVertices;
+            var existingNormals = package.TriangleNormals;
+
+            var newVerts = new List<double>();
+            for (var i = 0; i < existingVerts.Count; i += 3)
             {
-                NudgeVertexAlongVector(package.TriangleVertices, package.TriangleNormals, i, 0.001);
+                newVerts.AddRange(NudgeVertexAlongVector(existingVerts, existingNormals, i, 0.001));
             }
-
+            package.TriangleVertices = newVerts;
         }
 
-        private void NudgeVertexAlongVector(IList<double> vertices, IList<double> normals, int i, double amount)
+        private static IEnumerable<double> NudgeVertexAlongVector(IList<double> vertices, IList<double> normals, int i, double amount)
         {
             var x = (float)vertices[i];
             var y = (float)vertices[i + 1];
             var z = (float)vertices[i + 2];
             var v = Vector.ByCoordinates(x, y, z);
 
-            var nx = (float)vertices[i];
-            var ny = (float)vertices[i + 1];
-            var nz = (float)vertices[i + 2];
+            var nx = (float)normals[i];
+            var ny = (float)normals[i + 1];
+            var nz = (float)normals[i + 2];
             var n = Vector.ByCoordinates(nx, ny, nz);
 
             var nudge = v.Add(n.Normalized().Scale(amount));
-            vertices[i] = nudge.X;
-            vertices[i + 1] = nudge.Y;
-            vertices[i + 2] = nudge.Z;
+
+            return new [] { nudge.X, nudge.Y, nudge.Z };
         }
 
-        private void ReColorVerticesFromTo(int start, int end, IRenderPackage package)
+        private void ReColorLineVerticesFromTo(int start, int end, IRenderPackage package)
         {
+            var colors = package.LineStripVertexColors;
             for (var i = start; i < end; i += 4)
             {
-                package.LineStripVertexColors[i] = color.Red;
-                package.LineStripVertexColors[i + 1] = color.Green;
-                package.LineStripVertexColors[i + 2] = color.Blue;
+                colors[i] = color.Red;
+                colors[i + 1] = color.Green;
+                colors[i + 2] = color.Blue;
+                colors[i + 3] = color.Alpha;
             }
+            package.LineStripVertexColors = null;
+            package.LineStripVertexColors = colors;
         }
 
-        private void SetColorOnArray(IList<byte> array, Color color, int startIndex)
+        private static List<byte> ResetColorOnArray(List<byte> array, Color color, int startIndex)
         {
-            for (int i = startIndex; i < array.Count; i += 4)
+            var colors = array;
+            for (var i = startIndex; i < array.Count; i += 4)
             {
-                array[i] = color.Red;
-                array[i + 1] = color.Green;
-                array[i + 2] = color.Blue;
-                array[i + 3] = color.Alpha;
+                colors[i] = color.Red;
+                colors[i + 1] = color.Green;
+                colors[i + 2] = color.Blue;
+                colors[i + 3] = color.Alpha;
             }
+            return colors;
         }
     }
 }
