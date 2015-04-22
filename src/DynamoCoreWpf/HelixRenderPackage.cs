277a1174099690c07b5ac9553ec4d7b2f3ad5d39
Ian Keough <ian.keough@autodesk.com>
2015-04-22 06:55:19 -0700

    Update for removal of NativeRenderPackage...
    This property has been removed from ProtoInterface.IRenderPackage.

--- src/DynamoCoreWpf/HelixRenderPackage.cs
+++ src/DynamoCoreWpf/HelixRenderPackage.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 using Autodesk.DesignScript.Interfaces;
@@ -387,14 +386,6 @@ namespace Dynamo.Wpf
         public bool IsDisplayingLabels { get; set; }
 
         public bool RequiresPerVertexColoration { get; set; }
-
-        public IntPtr NativeRenderPackage
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
 
         #endregion
 
