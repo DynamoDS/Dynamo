757bf7f1f088162ae72c95f88fa236396174252a
Ian Keough <ian.keough@autodesk.com>
2015-04-26 14:05:04 -0700

    Start stubbing out tests for reconciliation.

---
+++ test/DynamoCoreTests/CallsiteTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+using Dynamo.DSEngine;
+using Dynamo.Models;
+using Dynamo.Nodes;
+using Dynamo.ViewModels;
+
+using DynamoShapeManager;
+
+using NUnit.Framework;
+
+using TestServices;
+
+namespace Dynamo.Tests
+{
+    [TestFixture]
+    public class CallsiteTests : DynamoModelTestBase
+    {
+        private readonly string callsiteDir = @"core\callsite";
+
+        [Test]
+        public void Callsite_MultiDimensionDecreaseDimensionOnOpenAndRun()
+        {
+            CurrentDynamoModel.EngineController.TraceReconcliationComplete += MultiDimensionCheck;
+
+            var ws = Open<HomeWorkspaceModel>(TestDirectory, callsiteDir, "RebindingSingleDimension.dyn");
+            var numNode = ws.FirstNodeFromWorkspace<DoubleInput>();
+
+            // File is saved with a value of 5 (elements)
+            numNode.Value = "3";
+
+            BeginRun();
+        }
+
+        private static void MultiDimensionCheck(TraceReconciliationEventArgs args)
+        {
+            Assert.AreEqual(args.OrphanedSerializables.Count, 2);
+        }
+
+        [Test]
+        public void CallSite_MultiDimensionIncreaseDimensionOnOpenAndRun()
+        {
+            
+        }
+
+        [Test]
+        public void Callsite_SingleDimensionDecreaseDimensionOnOpenAndRun()
+        {
+        }
+
+        [Test]
+        public void Callsite_SingleDimensionIncreaseDimensionOnOpenAndRun()
+        {
+            
+        }
+
+        [Test]
+        public void Callsite_DeleteNodeBeforeRun()
+        {
+            
+        }
+    }
+}
