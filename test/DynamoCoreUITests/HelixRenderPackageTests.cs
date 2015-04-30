f617e59d5a02a0f3ff68f79a395a602134bbb7fd
Ian Keough <ian.keough@autodesk.com>
2015-04-30 15:12:07 -0700

    Tiny cleanup.

--- test/DynamoCoreUITests/HelixRenderPackageTests.cs
+++ test/DynamoCoreUITests/HelixRenderPackageTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+﻿using System.Linq;
 
 using Autodesk.DesignScript.Interfaces;
 
