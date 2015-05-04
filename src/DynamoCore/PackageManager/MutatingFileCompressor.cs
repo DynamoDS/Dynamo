e9c6040489965365530d425d7f42ce9020c07280
Peter Boyer <peter.b.boyer@gmail.com>
2015-05-04 15:17:59 -0400

    Ensure directory exists before zipping

--- src/DynamoCore/PackageManager/MutatingFileCompressor.cs
+++ src/DynamoCore/PackageManager/MutatingFileCompressor.cs
@@ -1,4 +1,6 @@
-﻿using Dynamo.PackageManager.Interfaces;
+﻿using System;
+using System.IO;
+using Dynamo.PackageManager.Interfaces;
 
 namespace Dynamo.PackageManager
 {
@@ -9,6 +11,13 @@ namespace Dynamo.PackageManager
     {
         public IFileInfo Zip(IDirectoryInfo directory)
         {
+            if (directory == null) throw new ArgumentNullException("directory");
+
+            if (!(new MutatingFileSystem()).DirectoryExists(directory.FullName))
+            {
+                throw new DirectoryNotFoundException(directory.FullName);
+            }
+
             return new RealFileInfo(Greg.Utility.FileUtilities.Zip(directory.FullName));
         }
     }
