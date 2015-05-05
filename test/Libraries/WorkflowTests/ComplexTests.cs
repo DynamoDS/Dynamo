c64e2ac13cb36f816282d2038be85603181efc44
zora-wang <t_yanz@SQALABSMT.acadqasrf.autodesk.com>
2015-05-05 17:19:50 +0800

    add test case for codeblock

--- test/Libraries/WorkflowTests/ComplexTests.cs
+++ test/Libraries/WorkflowTests/ComplexTests.cs
@@ -2,6 +2,8 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using Autodesk.DesignScript.Geometry;
+using Dynamo.Models;
+using System.Linq;
 
 namespace Dynamo.Tests
 {
@@ -56,5 +58,68 @@ namespace Dynamo.Tests
 
         }
         #endregion
+
+
+        //add test cases that are related to CodeBlocks.
+        #region CodeBlocks test
+        [Test]
+        public void Test_CodeBlocksReference()
+        {
+            // Create automation for Dynamo file : Code Blocks Reference.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\04 Code blocks\Code Blocks Reference.dyn");
+            RunModel(openPath);
+
+            //check preview values of CBNs
+            var cbn = "27c2f333-f51f-4a0c-9f71-70dc64f2ecbe";
+            AssertPreviewValue(cbn, 3.142);
+            var strCbn = "4c8ddee8-e2b1-4472-9470-a4142f56ac97";
+            string str = "Less is more.";
+            AssertPreviewValue(strCbn, str);
+            var pointCbn = "4ea1d0d8-4882-4f6f-b659-2dcb297db34e";
+            var x = GetPreviewValue(pointCbn) as Point;
+            Assert.AreEqual(0, x.X);
+            Assert.AreEqual(0, x.Y);
+            Assert.AreEqual(0, x.Z);
+            var ptCBN = "613c6d53-0eab-414f-9aeb-fc7c56de6900";
+            AssertPreviewValue(ptCBN, 10);
+            
+            //check preview values of code block nodes
+            var CBN1 = "34a4b2fc-a1c5-4c18-a0fe-48b1c3b8f711";
+            var CBN1Val = GetPreviewValue(CBN1) as Point;
+            var CBN2 = "a1142e89-89e4-4fa2-a63d-d58489af044d";
+            var CBN2Val = GetPreviewValue(CBN2);
+            Assert.AreEqual(CBN1Val, CBN2Val);
+
+            //check preview value of code block node, which contains a list
+            var listCBN = "4c049b29-2792-4a66-ab89-d8fac3161e7d";
+            AssertPreviewCount(listCBN, 4);
+
+            //check preview value of CBN, which is a list and not null
+            var notNullCBN = "d71e5f04-9928-437a-863c-5bf34666370b";
+            AssertPreviewCount(notNullCBN, 5);
+            for (int i = 0; i < 5; i++)
+            {
+                var element = GetPreviewValueAtIndex(notNullCBN, i) as string;
+                Assert.IsNotNull(element);
+            }
+
+            //check the preview value of CBN, which value is null
+            var nullCBN = "f973a3ec-70ee-475b-ad7d-50e5f21c1e04";
+            AssertPreviewValue(nullCBN, null);
+
+            //check the CBN which contains a list of nurbsCurve
+            var nurbCBN = "8dce8e89-1d9d-4ffe-b6ea-e7251ab1368b";
+            AssertPreviewCount(nurbCBN, 24);
+            for (int i = 0; i<24; i++)
+            {
+                var nurbCBNVal = GetPreviewValueAtIndex(nurbCBN, i) as NurbsCurve;
+                Assert.IsNotNull(nurbCBNVal);
+            }
+            
+        }
+        #endregion
+
     }
 }
