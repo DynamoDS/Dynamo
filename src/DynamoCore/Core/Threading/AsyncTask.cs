f9d59ffe1bb8b59575706d76cbd64b4f1ca2bf2b
Benglin (Ben) Goh <benglin@outlook.com>
2015-05-05 17:25:51 +0800

    Removed redundant namespaces

--- src/DynamoCore/Core/Threading/AsyncTask.cs
+++ src/DynamoCore/Core/Threading/AsyncTask.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
-using System.Windows.Media.Animation;
-
-using Dynamo.Services;
 
 namespace Dynamo.Core.Threading
 {
