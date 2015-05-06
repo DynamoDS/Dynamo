2d0546465aaf9ec420664cdb870a0c9c64294e44
Ian Keough <ian.keough@autodesk.com>
2015-05-06 11:52:10 -0700

    Comments about returning string.Empty.

--- test/DynamoCoreTests/DynamoModelTestBase.cs
+++ test/DynamoCoreTests/DynamoModelTestBase.cs
@@ -101,14 +101,16 @@ namespace Dynamo
         protected virtual string GetUserUserDataRootFolder()
         {
             // Override in derived classed to provide a custom
-            // UserAppDataRootFolder
+            // UserAppDataRootFolder. Returning an empty string
+            // here will cause the PathManager to use its default.
             return string.Empty;
         }
 
         protected virtual string GetCommonDataRootFolder()
         {
             // Override in derived classed to provide a custom
-            // CommonAppDataRootFolder
+            // CommonAppDataRootFolder. Returning an empty string
+            // here will cause the PathManager to use its default.
             return string.Empty;
         }
 
