10247eff1be747172cca942d91a81a48edb13b49
zora-wang <t_yanz@SQALABSMT.acadqasrf.autodesk.com>
2015-05-06 13:04:46 +0800

    add dynamo node test cases

--- test/Libraries/WorkflowTests/ComplexTests.cs
+++ test/Libraries/WorkflowTests/ComplexTests.cs
@@ -8,13 +8,17 @@ using System.Linq;
 namespace Dynamo.Tests
 {
     [TestFixture]
-    class WorkflowTests : DSEvaluationViewModelUnitTest
+    class ComplexTests : DSEvaluationViewModelUnitTest
     {
         protected override void GetLibrariesToPreload(List<string> libraries)
         {
-            libraries.Add("ProtoGeometry.dll");
-            libraries.Add("DSCoreNodes.dll");
+            libraries.Add("VMDataBridge.dll");  // Required for Watch node.
+            libraries.Add("ProtoGeometry.dll"); // Required for Surface.
+            libraries.Add("DSCoreNodes.dll");   // Required for built-in nodes.
+            libraries.Add("DSIronPython.dll");  // Required for Python tests.
+            libraries.Add("FunctionObject.ds"); // Required for partially applied nodes
             base.GetLibrariesToPreload(libraries);
+            
         }
 
         // Note: Only add test cases those are related to ASM Geometry.
@@ -29,6 +33,8 @@ namespace Dynamo.Tests
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\RandomModel_V3.dyn");
             
             RunModel(openPath);
+
+            AssertNoDummyNodes();
 
             var solidNodeId = "ff0b24b2-73e5-4da6-8081-a1883ff9ad72";
             AssertPreviewCount(solidNodeId, 66);
@@ -70,6 +76,8 @@ namespace Dynamo.Tests
             DynamoModel model = ViewModel.Model;
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\04 Code blocks\Code Blocks Reference.dyn");
             RunModel(openPath);
+
+            AssertNoDummyNodes();
 
             //check preview values of CBNs
             var cbn = "27c2f333-f51f-4a0c-9f71-70dc64f2ecbe";
@@ -121,5 +129,120 @@ namespace Dynamo.Tests
         }
         #endregion
 
+        //add test cases that are related to Geometry.
+        #region Geometry test
+        [Test]
+        public void Test_Basket1()
+        {
+            // Create automation for Dynamo file : Basket1.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\05 Geometry\Basket1.dyn");
+            RunModel(openPath);
+
+            AssertNoDummyNodes();
+
+            // check the number of nodes and connectors
+            Assert.AreEqual(31, model.CurrentWorkspace.Connectors.Count());
+            Assert.AreEqual(25, model.CurrentWorkspace.Nodes.Count);
+
+            // check Geometry.Rotate
+            var rotate = "dfdfa7b1-533b-4dca-afa2-1d9d62233b7f";
+            AssertPreviewCount(rotate, 12);
+            for (int i = 0; i < 12; i++)
+            {
+                var element = GetPreviewValueAtIndex(rotate, i) as Solid;
+                Assert.IsNotNull(element);
+            }
+        }
+
+
+        [Test]
+        public void Test_Basket2()
+        {
+            // Create automation for Dynamo file : Basket2.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\05 Geometry\Basket2.dyn");
+            RunModel(openPath);
+
+            AssertNoDummyNodes();
+
+            // check the number of nodes and connectors
+            Assert.AreEqual(53, model.CurrentWorkspace.Connectors.Count());
+            Assert.AreEqual(45, model.CurrentWorkspace.Nodes.Count);
+
+            // check Curve.ExtrudeAsSolid
+            var solid = "9349f9d5-aa7c-43d9-a336-5610004ed7ef";
+            AssertPreviewCount(solid, 63);
+            for (int i = 0; i < 63; i++)
+            {
+                var element = GetPreviewValueAtIndex(solid, i) as Solid;
+                Assert.IsNotNull(element);
+            }
+        }
+
+
+        [Test]
+        public void Test_Basket3()
+        {
+            // Create automation for Dynamo file : Basket3.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\05 Geometry\Basket3.dyn");
+            RunModel(openPath);
+
+            AssertNoDummyNodes();
+
+            // check the number of nodes and connectors
+            Assert.AreEqual(199, model.CurrentWorkspace.Connectors.Count());
+            Assert.AreEqual(141, model.CurrentWorkspace.Nodes.Count);
+
+            // check Surface.Thicken
+            var solid1 = "db78b128-fec5-40b9-9e59-0ca371a4de43";
+            AssertPreviewCount(solid1, 4);
+            for (int i = 0; i < 4; i++)
+            {
+                var element = GetPreviewValueAtIndex(solid1, i) as Solid;
+                Assert.IsNotNull(element);
+            }
+
+            // check Surface.Thicken
+            var solid2 = "bd7ffff8-b6dd-4a01-8caf-9a59682a3288";
+            AssertPreviewCount(solid2, 5);
+            for (int i = 0; i < 5; i++)
+            {
+                var element = GetPreviewValueAtIndex(solid2, i) as Solid;
+                Assert.IsNotNull(element);
+            }
+        }
+        #endregion
+
+        //add test cases that are related to Python.
+        #region Python test
+        [Test]
+        public void Test_Core_Python()
+        {
+            // Create automation for Dynamo file : Core_Python.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\06 Python\Core_Python.dyn");
+            RunModel(openPath);
+
+            AssertNoDummyNodes();
+
+            // check the number of nodes and connectors
+            Assert.AreEqual(13, model.CurrentWorkspace.Connectors.Count());
+            Assert.AreEqual(15, model.CurrentWorkspace.Nodes.Count);
+
+            //check List.GetItemAtIndex
+            var getItemAtIndex1 = "b1be50ed-f93c-4d9c-b7c4-55d97e820672";
+            AssertPreviewValue(getItemAtIndex1, 55);   
+
+            //check List.GetItemAtIndex
+            var getItemAtIndex2 = "e14a4953-9115-4344-ac65-e7243e4975e3";
+            AssertPreviewValue(getItemAtIndex2, new int[] { 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 });    
+        }
+        #endregion
     }
 }
