584ddbc5d17c914d21dcb9a55da6c24b372330d1
zora-wang <t_yanz@SQALABSMT.acadqasrf.autodesk.com>
2015-05-07 14:19:18 +0800

    add automation test cases for dynamo

--- test/Libraries/WorkflowTests/ComplexTests.cs
+++ test/Libraries/WorkflowTests/ComplexTests.cs
@@ -8,12 +8,15 @@ using System.Linq;
 namespace Dynamo.Tests
 {
     [TestFixture]
-    class WorkflowTests : DSEvaluationViewModelUnitTest
+    class ComplexTests : DSEvaluationViewModelUnitTest
     {
         protected override void GetLibrariesToPreload(List<string> libraries)
         {
-            libraries.Add("ProtoGeometry.dll");
-            libraries.Add("DSCoreNodes.dll");
+            libraries.Add("VMDataBridge.dll");  // Required for Watch node.
+            libraries.Add("ProtoGeometry.dll"); // Required for Surface.
+            libraries.Add("DSCoreNodes.dll");   // Required for built-in nodes.
+            libraries.Add("DSIronPython.dll");  // Required for Python tests.
+            libraries.Add("FunctionObject.ds"); // Required for partially applied nodes.
             base.GetLibrariesToPreload(libraries);
         }
 
@@ -26,9 +29,9 @@ namespace Dynamo.Tests
         {
             // This will test user workflow which contains many nodes, final output is Solid using
             // sweep.
-            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\RandomModel_V3.dyn");
-            
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\RandomModel_V3.dyn");          
             RunModel(openPath);
+            AssertNoDummyNodes();
 
             var solidNodeId = "ff0b24b2-73e5-4da6-8081-a1883ff9ad72";
             AssertPreviewCount(solidNodeId, 66);
@@ -39,8 +42,8 @@ namespace Dynamo.Tests
                 var sweptSolid = GetPreviewValueAtIndex(solidNodeId, i) as Solid;
                 Assert.IsNotNull(sweptSolid);
             }
-
         }
+
 
         [Test]
         public void ListLacing()
@@ -50,6 +53,8 @@ namespace Dynamo.Tests
             DynamoModel model = ViewModel.Model;
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\01 Dynamo Basics\02 List Lacing.dyn");
             RunModel(openPath);
+            AssertNoDummyNodes();
+
             // check list lacing 
             var shortesListLacing = "e93fee37-1901-4162-8f73-6b5e98c1167f";
             AssertPreviewCount(shortesListLacing, 6);
@@ -60,6 +65,7 @@ namespace Dynamo.Tests
 
         }
 
+
          [Test]
         public void AttractorPoint()
         {
@@ -67,14 +73,22 @@ namespace Dynamo.Tests
             // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
             DynamoModel model = ViewModel.Model;
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\01 Dynamo Basics\03 Attractor Point.dyn");
+            RunModel(openPath); 
+            AssertNoDummyNodes();
+             // check Point.X
+            var pointX = "d16627d1-695f-4997-a7d2-3f8754f19589";
+            AssertPreviewCount(pointX, 100);      
 
-            RunModel(openPath);
-            // check the number of cylinders
+            // check Cylinder.ByPointsRadius
             var cylinders = "ef3eaed0-7a8e-47a9-b06e-416bb30ec72f";
             AssertPreviewCount(cylinders, 100);
-           
-
+            for (int i = 0; i < 100; i++)
+            {
+                var cylinder = GetPreviewValueAtIndex(cylinders, i) as Cylinder;
+                Assert.IsNotNull(cylinder);
+            }      
         }
+
 
         [Test]
         public void RangeSyntax()
@@ -83,8 +97,9 @@ namespace Dynamo.Tests
             // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
             DynamoModel model = ViewModel.Model;
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\01 Dynamo Basics\04 Range Syntax.dyn");
-
             RunModel(openPath);
+            AssertNoDummyNodes();
+
             // check the preview value of Number Range node
             var numberRange = "5892dcad-9e46-46b5-97f2-5e18e17ca7db";
             AssertPreviewCount(numberRange, 6);
@@ -106,6 +121,7 @@ namespace Dynamo.Tests
   
         }
 
+
          [Test]
         public void WriteToExcel()
         {
@@ -113,9 +129,9 @@ namespace Dynamo.Tests
             // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
             DynamoModel model = ViewModel.Model;
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\01 Dynamo Basics\05 Write To Excel.dyn");
-
             RunModel(openPath);
-           
+            AssertNoDummyNodes();
+
             //check the number of nodes and connectors
             Assert.AreEqual(14, model.CurrentWorkspace.Connectors.Count());
             Assert.AreEqual(12, model.CurrentWorkspace.Nodes.Count);
@@ -126,6 +142,7 @@ namespace Dynamo.Tests
 
         }
 
+
         [Test]
         public void ReadFromCSV ()
         {
@@ -133,8 +150,8 @@ namespace Dynamo.Tests
             // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
             DynamoModel model = ViewModel.Model;
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\01 Dynamo Basics\06 Read from CSV.dyn");
-
             RunModel(openPath);
+            AssertNoDummyNodes();
 
             //check the number of nodes and connectors
             Assert.AreEqual(6, model.CurrentWorkspace.Connectors.Count());
@@ -151,6 +168,7 @@ namespace Dynamo.Tests
 
         }
 
+
         [Test]
         public void PassingFunctions()
         {
@@ -160,6 +178,7 @@ namespace Dynamo.Tests
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\01 Dynamo Basics\07 Passing Functions.dyn");
 
             RunModel(openPath);
+            AssertNoDummyNodes();
 
             //check the number of nodes and connectors
             Assert.AreEqual(16, model.CurrentWorkspace.Connectors.Count());
@@ -171,10 +190,8 @@ namespace Dynamo.Tests
             //flat the two dimensional list
             var flat = GetFlattenedPreviewValues(curveExtrude);
             Assert.AreEqual(flat.Count, 10);
-
-          
-
         }
+
 
         [Test]
         public void Math()
@@ -185,6 +202,7 @@ namespace Dynamo.Tests
             string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\01 Dynamo Basics\08 Math.dyn");
 
             RunModel(openPath);
+            AssertNoDummyNodes();
 
             //check the number of nodes and connectors
             Assert.AreEqual(201, model.CurrentWorkspace.Connectors.Count());
@@ -201,8 +219,139 @@ namespace Dynamo.Tests
             //check remainder
             string remainder = "5ae3c356-3e83-43a9-a126-95b82cdcacc1";
             AssertPreviewValue(remainder, 1);
-
         }
+
+
+        [Test]
+        public void Combine()
+        {
+            // Create automation for Dynamo file : Combine.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\ListManagementMisc\Combine.dyn");
+
+            RunModel(openPath);
+            AssertNoDummyNodes();
+
+            //check the number of nodes and connectors
+            Assert.AreEqual(44, model.CurrentWorkspace.Connectors.Count());
+            Assert.AreEqual(36, model.CurrentWorkspace.Nodes.Count);
+
+            //check List.Map
+            var map1 = "34986114-3561-4feb-993b-3c53c9ef352f";
+            AssertPreviewCount(map1, 15);
+            for (int i = 0; i < 15; i++)
+            {
+                var nurbscurve = GetPreviewValueAtIndex(map1, i) as NurbsCurve;
+                Assert.IsNotNull(nurbscurve);
+            }
+            var map2 = "7ec7b343-6e0c-46ba-b89a-7df17e6a0833";
+            for (int i = 0; i < 15; i++)
+            {
+                var point = GetPreviewValueAtIndex(map2, i) as Point;
+                Assert.IsNotNull(point);
+                Assert.AreEqual(point.X, 0);
+            }
+
+            //check List.Combine
+            var combine = "ce5a6237-f69b-4a84-9838-245272c8fccf";
+            AssertPreviewCount(combine, 15);
+            var flatCombine = GetFlattenedPreviewValues(combine);
+            foreach (var element in flatCombine)
+            {
+                Assert.IsNotNull(element);
+            }
+
+            // check Surface.ByLoft
+            var surface = "cd8135dc-2d29-4466-bc0d-476016f811cb";
+            AssertPreviewCount(surface, 15);
+            for (int i = 0; i < 15; i++)
+            {
+                var sur = GetPreviewValueAtIndex(surface, i) as Surface;
+                Assert.IsNotNull(sur);
+            }         
+        }
+
+
+        [Test]
+        public void Count()
+        {
+            // Create automation for Dynamo file : Count.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\ListManagementMisc\Count.dyn");
+
+            RunModel(openPath);
+            AssertNoDummyNodes();
+
+            //check the number of nodes and connectors
+            Assert.AreEqual(6, model.CurrentWorkspace.Connectors.Count());
+            Assert.AreEqual(6, model.CurrentWorkspace.Nodes.Count);
+
+            //check Count
+            var count = "6e2bc5c1-a774-4c54-b7e4-9f1dc87360d1";
+            AssertPreviewValue(count, 43);
+           
+            // check List.Combine
+            var combine = "2595c54d-2d72-4050-93ff-457ad3003bab";
+            for (int i = 0; i < 43; i++)
+            {
+                var value = GetPreviewValueAtIndex(combine, i) as string;
+                Assert.AreEqual(value, "foo"+i.ToString());
+            }     
+        }
+
+
+        [Test]
+        public void Flatten()
+        {
+            // Create automation for Dynamo file : Flatten.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\ListManagementMisc\Flatten.dyn");
+
+            RunModel(openPath);
+            AssertNoDummyNodes();
+
+            //check the number of nodes and connectors
+            Assert.AreEqual(13, model.CurrentWorkspace.Connectors.Count());
+            Assert.AreEqual(10, model.CurrentWorkspace.Nodes.Count);
+
+            //check Flatten
+            var count = "9348354d-1aae-49bf-b1cd-33f650dd42dd";
+            AssertPreviewCount(count, 18);
+            int[] list = new int[] { 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5 };
+            AssertPreviewValue(count, list);           
+        }
+
+
+        [Test]
+        public void CreateList()
+        {
+            // Create automation for Dynamo file : CreateList.dyn
+            // http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7214
+            DynamoModel model = ViewModel.Model;
+            string openPath = Path.Combine(TestDirectory, @"core\WorkflowTestFiles\ListManagementMisc\CreateList.dyn");
+
+            RunModel(openPath);
+            AssertNoDummyNodes();
+
+            //check the number of nodes and connectors
+            Assert.AreEqual(4, model.CurrentWorkspace.Connectors.Count());
+            Assert.AreEqual(5, model.CurrentWorkspace.Nodes.Count);
+
+            //check List.Create
+            var count = "cd7d56c0-3e0d-46c7-9dad-777008030045";
+            AssertPreviewCount(count, 3);
+            var flatList = GetFlattenedPreviewValues(count);
+            Assert.AreEqual(flatList.Count, 4);
+            flatList[0].ToString().Equals("42");
+            flatList[1].ToString().Equals("The answer to everything");
+            flatList[2].ToString().Equals("foo");
+            flatList[3].ToString().Equals("bar");
+        }
+
+
         [Test]
        public void IndexOutsideBounds_3399()
         {
