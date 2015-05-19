07dcf6a59d216e323c6dc278c6fa90ae581fae0a
Jun Mendoza <jun.mendoza@autodesk.com>
2015-05-19 16:51:30 +0800

    Remove multiple nesting

--- src/Engine/ProtoCore/CodeGen.cs
+++ src/Engine/ProtoCore/CodeGen.cs
@@ -505,47 +505,51 @@ namespace ProtoCore
         //      fnode.updatedArgProps.push(proplist)
         //  end
         //
-        protected ProtoCore.AssociativeGraph.UpdateNodeRef AutoGenerateUpdateArgumentReference(
+        protected void AutoGenerateUpdateArgumentReference(
             ProtoCore.AssociativeGraph.UpdateNodeRef nodeRef, ProtoCore.AssociativeGraph.GraphNode graphNode)
         {
 
             ProtoCore.DSASM.SymbolNode firstSymbol = null;
 
+            // This is only valid within a function as we are dealing with function args
+            Validity.Assert(null != localProcedure);
 
-            // Check if we are inside a procedure
-            if (null != localProcedure)
+            
+            // Check if there are at least 2 symbols in the list
+            if (nodeRef.nodeList.Count < 2)
             {
-                // Check if there are at least 2 symbols in the list
-                if (nodeRef.nodeList.Count >= 2)
-                {
-                    firstSymbol = nodeRef.nodeList[0].symbol;
-                    if (null != firstSymbol && nodeRef.nodeList[0].nodeType != ProtoCore.AssociativeGraph.UpdateNodeType.kMethod)
-                    {
-                        // Now check if the first element of the identifier list is an argument
-                        foreach (ProtoCore.DSASM.ArgumentInfo argInfo in localProcedure.argInfoList)
-                        {
-                            if (argInfo.Name == firstSymbol.name)
-                            {
-                                nodeRef.nodeList.RemoveAt(0);
+                return;
+            }
 
-                                List<ProtoCore.AssociativeGraph.UpdateNodeRef> refList = null;
-                                bool found = localProcedure.updatedArgumentProperties.TryGetValue(argInfo.Name, out refList);
-                                if (found)
-                                {
-                                    refList.Add(nodeRef);
-                                }
-                                else
-                                {
-                                    refList = new List<ProtoCore.AssociativeGraph.UpdateNodeRef>();
-                                    refList.Add(nodeRef);
-                                    localProcedure.updatedArgumentProperties.Add(argInfo.Name, refList);
-                                }
-                            }
-                        }
+            firstSymbol = nodeRef.nodeList[0].symbol;
+
+            bool isValidNodeRef = null != firstSymbol && nodeRef.nodeList[0].nodeType != ProtoCore.AssociativeGraph.UpdateNodeType.kMethod;
+            if (!isValidNodeRef)
+            {
+                return;
+            }
+            
+            // Now check if the first element of the identifier list is an argument
+            foreach (ProtoCore.DSASM.ArgumentInfo argInfo in localProcedure.argInfoList)
+            {
+                if (argInfo.Name == firstSymbol.name)
+                {
+                    nodeRef.nodeList.RemoveAt(0);
+
+                    List<ProtoCore.AssociativeGraph.UpdateNodeRef> refList = null;
+                    bool found = localProcedure.updatedArgumentProperties.TryGetValue(argInfo.Name, out refList);
+                    if (found)
+                    {
+                        refList.Add(nodeRef);
+                    }
+                    else
+                    {
+                        refList = new List<ProtoCore.AssociativeGraph.UpdateNodeRef>();
+                        refList.Add(nodeRef);
+                        localProcedure.updatedArgumentProperties.Add(argInfo.Name, refList);
                     }
                 }
             }
-            return nodeRef;
         }
 
         //  
@@ -2670,8 +2674,7 @@ namespace ProtoCore
                         graphNode.updateNodeRefList.Add(nodeRef);
                         graphNode.IsLHSIdentList = true;
 
-                        ProtoCore.AssociativeGraph.UpdateNodeRef leftNodeArgRef = AutoGenerateUpdateArgumentReference(nodeRef, graphNode);
-                        //graphNode.updateNodeRefList.Add(leftNodeArgRef);
+                        AutoGenerateUpdateArgumentReference(nodeRef, graphNode);
                     }
                 }
             }
