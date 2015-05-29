a17c52188161b8ac629f180487c723bfa05ee8b2
Sancerio <lsancerio@gmail.com>
2015-05-29 15:41:56 +0800

    add unit test for parsing gallery

---
+++ test/DynamoCoreWpfTests/GalleryTests.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Dynamo;
+using Dynamo.Wpf.ViewModels.Core;
+using NUnit.Framework;
+
+namespace DynamoCoreWpfTests
+{
+    [TestFixture]
+    public class GalleryTests : UnitTestBase
+    {
+        [Test]
+        public void LoadGalleryContents()
+        {
+            //Test loading of xml gallery contents
+            var galleryFilePath = Path.Combine(Directory.GetCurrentDirectory(),
+                @"..\..\..\test\core\gallery\NormalGalleryContents.xml");
+            var contents = GalleryContents.Load(galleryFilePath).GalleryUiContents;
+            Assert.IsTrue(contents.Count == 6);
+
+            for (int i = 0; i < contents.Count; i++)
+            {
+                Assert.IsTrue(contents[i].ImagePath == string.Format("{0}.png", i + 1));
+                Assert.IsTrue(contents[i].Header == string.Format("Library items are now draggable {0}", i + 1));
+                Assert.IsTrue(contents[i].Body == string.Format("Give me some random texts inside {0}", i + 1));
+            }
+
+            //Test loading of an empty xml file
+            galleryFilePath = Path.Combine(Directory.GetDirectoryRoot(galleryFilePath),
+                "EmptyGalleryContents.xml");
+            contents = GalleryContents.Load(galleryFilePath).GalleryUiContents;
+            Assert.IsTrue(contents.Count == 0);
+        }
+    }
+}
