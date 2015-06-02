59e9fd32fefe82f6efa5c86bdcc108f9e5c8597c
Michael Kirschner <mjk.kirschner@gmail.com>
2015-06-02 10:39:38 -0400

    before modifying any node when setting the state of the graph, we can set the state x,y of the node to the current x,y so the node is not moved
    add a test for this behavior

--- src/DynamoCore/Models/WorkspaceModel.cs
+++ src/DynamoCore/Models/WorkspaceModel.cs
@@ -804,6 +804,7 @@ namespace Dynamo.Models
             //start an undoBeginGroup
             using (var undoGroup = this.undoRecorder.BeginActionGroup())
             {
+               
                //reload each node, and record each each modification in the undogroup
                 foreach (var node in state.Nodes)
                 {
@@ -811,9 +812,15 @@ namespace Dynamo.Models
                     //otherwise bail on this node
                     if (nodes.Contains(node))
                     {
+                        var originalpos = node.Position;
                         var serializedNode = state.SerializedNodes.ToList().Find(x => Guid.Parse(x.GetAttribute("guid")) == node.GUID);
+                        //overwrite the xy coords of the serialized node with the current position, so the node is not moved
+                        serializedNode.SetAttribute("x", originalpos.X.ToString());
+                        serializedNode.SetAttribute("y", originalpos.Y.ToString());
+
                         this.undoRecorder.RecordModificationForUndo(node);
                         this.ReloadModel(serializedNode);
+                        
                     }
                 }
                 //select all the modified nodes in the UI
