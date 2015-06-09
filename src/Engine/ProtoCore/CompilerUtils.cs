a7be28bb40b78f93bdf55ea58cb475d215315081
aparajit <aparajit.pratap@autodesk.com>
2015-06-09 13:43:06 +0800

    rewrite default argument expressions with fully qualified names

--- src/Engine/ProtoCore/CompilerUtils.cs
+++ src/Engine/ProtoCore/CompilerUtils.cs
@@ -304,7 +304,7 @@ namespace ProtoCore.Utils
                 // partial classnames with their fully qualified names in ASTs
                 // before passing them for pre-compilation. If partial class is not found in map, 
                 // update Resolution map in elementResolver with fully resolved name from compiler.
-                var reWrittenNodes = ElementRewriter.RewriteElementNames(core.ClassTable, parseParams.ElementResolver, astNodes);
+                var reWrittenNodes = ElementRewriter.RewriteElementNames(core, parseParams.ElementResolver, astNodes);
 
                 // Clone a disposable copy of AST nodes for PreCompile() as Codegen mutates AST's
                 // while performing SSA transforms and we want to keep the original AST's
---
+++ test/Engine/FFITarget/TestDefaultArgumentAttributes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.DesignScript.Runtime;
+
+namespace FFITarget
+{
+    public class ArgumentClass
+    {
+        public static float GetFloat()
+        {
+            return 2.5F;
+        }    
+    }
+
+    public class TestDefaultArgumentAttributes
+    {
+        public static double GetCircleArea([DefaultArgument("ArgumentClass.GetFloat()")]double radius)
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+}
