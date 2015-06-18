b01c761c644469b900effe4ae8d6363a575dca89
Minh Nguyen <minhnb3192@gmail.com>
2015-06-18 14:45:31 +0800

    migrate Rectangle.ByWidthHeight to Rectangle.ByWidthLength

--- src/Migrations/RevitNodes/Rectangle.cs
+++ src/Migrations/RevitNodes/Rectangle.cs
@@ -8,8 +8,8 @@ namespace Dynamo.Nodes
         [NodeMigration(from: "0.6.3.0", to: "0.7.0.0")]
         public static NodeMigrationData Migrate_0630_to_0700(NodeMigrationData data)
         {
-            return MigrateToDsFunction(data, "ProtoGeometry.dll", "Rectangle.ByWidthHeight",
-                "Rectangle.ByWidthHeight@Autodesk.DesignScript.Geometry.CoordinateSystem,double,double");
+            return MigrateToDsFunction(data, "ProtoGeometry.dll", "Rectangle.ByWidthLength",
+                "Rectangle.ByWidthLength@Autodesk.DesignScript.Geometry.CoordinateSystem,double,double");
         }
     }
 }
