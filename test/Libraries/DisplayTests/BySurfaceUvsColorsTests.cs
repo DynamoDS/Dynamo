2197c487c3a5f0229f7c2e70ecf0c496073682fa
Ian Keough <ian.keough@autodesk.com>
2015-06-26 10:10:42 -0700

    Change Display.BySurfaceUVsColors to Display.BySurfaceColors

--- test/Libraries/DisplayTests/BySurfaceUvsColorsTests.cs
+++ test/Libraries/DisplayTests/BySurfaceUvsColorsTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Autodesk.DesignScript.Geometry;
 using DSCore;
 using NUnit.Framework;
@@ -13,50 +12,29 @@ namespace DisplayTests
         [Test]
         public void BySurfaceUVsColors_Construction_AllGood()
         {
-            Assert.DoesNotThrow(()=>Display.Display.BySurfaceUvsColors(
-                CreateOneSurface(), CreateThreeUvs(), CreateThreeColors())); 
+            Assert.DoesNotThrow(() => Display.Display.BySurfaceColors(
+                CreateOneSurface(), CreateTwoRowsOfColors()));
         }
 
         [Test]
         public void BySurfaceUvsColors_Construction_NullSurface_ThrowsException()
         {
-            Assert.Throws<ArgumentNullException>(() => Display.Display.BySurfaceUvsColors(
-                null, CreateThreeUvs(), CreateThreeColors()));
+            Assert.Throws<ArgumentNullException>(() => Display.Display.BySurfaceColors(
+                null, CreateTwoRowsOfColors()));
         }
 
         [Test]
-        public void BySurfaceUvsColors_Construction_NullUvs_ThrowsException()
+        public void BySurfaceUvsColors_Construction_NullColors_ThrowsException()
         {
-            Assert.Throws<ArgumentNullException>(() => Display.Display.BySurfaceUvsColors(
-                CreateOneSurface(), null, CreateThreeColors())); 
+            Assert.Throws<ArgumentNullException>(() => Display.Display.BySurfaceColors(
+                CreateOneSurface(), null));
         }
 
         [Test]
-        public void BySurfaceUvsColors_Construction_EmptyUvs_ThrowsException()
+        public void BySurfaceUvsColors_Construction_NoColors_ThrowsException()
         {
-            Assert.Throws<ArgumentException>(() => Display.Display.BySurfaceUvsColors(
-                CreateOneSurface(), new UV[]{}, CreateThreeColors())); 
-        }
-
-        [Test]
-        public void BySurfaceUvsColors_Construction_PrecisionTooLow_ThrowsException()
-        {
-            Assert.Throws<Exception>(() => Display.Display.BySurfaceUvsColors(
-                CreateOneSurface(), CreateThreeUvs(), CreateThreeColors(), -42.0)); 
-        }
-
-        [Test]
-        public void BySurfaceUvsColors_Construction_PrecisionTooHigh_ThrowsException()
-        {
-            Assert.Throws<Exception>(() => Display.Display.BySurfaceUvsColors(
-                CreateOneSurface(), CreateThreeUvs(), CreateThreeColors(), 42.0)); 
-        }
-
-        [Test]
-        public void BySurfaceUvsColors_Construction_UvsColorsCountMismatch_ThrowsException()
-        {
-            Assert.Throws<Exception>(() => Display.Display.BySurfaceUvsColors(
-                CreateOneSurface(), CreateTwoUvs(), CreateThreeColors(), 42.0)); 
+            Assert.Throws<ArgumentException>(() => Display.Display.BySurfaceColors(
+                CreateOneSurface(), new Color[][]{}));
         }
 
         private static Surface CreateOneSurface()
@@ -66,36 +44,22 @@ namespace DisplayTests
             return surface;
         }
 
-        private static UV[] CreateThreeUvs()
-        {
-            var uvs = new[]
-            {
-                UV.ByCoordinates(0.2, 0.5),
-                UV.ByCoordinates(0.1, 0.9),
-                UV.ByCoordinates(0.9, 0.3)
-            };
-
-            return uvs;
-        }
-
-        private static UV[] CreateTwoUvs()
-        {
-            var uvs = new[]
-            {
-                UV.ByCoordinates(0.2, 0.5),
-                UV.ByCoordinates(0.1, 0.9),
-            };
-
-            return uvs;
-        }
-
-        private static Color[] CreateThreeColors()
+        private static Color[][] CreateTwoRowsOfColors()
         {
             var colors = new[]
             {
-                Color.ByARGB(255, 255, 0, 0),
-                Color.ByARGB(255, 0, 255, 0),
-                Color.ByARGB(0, 0, 255)
+                new []
+                {
+                    Color.ByARGB(255, 255, 0, 0),
+                    Color.ByARGB(255, 0, 255, 0),
+                    Color.ByARGB(0, 0, 255)
+                },
+                new []
+                {
+                    Color.ByARGB(0, 0, 255),
+                    Color.ByARGB(255, 0, 255, 0),
+                    Color.ByARGB(255, 255, 0, 0),
+                }
             };
 
             return colors;
