dbf916d6d0e666b3ff88fb05e2e7bc841931f954
Ian Keough <ian.keough@autodesk.com>
2015-06-29 09:50:37 -0700

    Add DateTime selection node.

---
+++ src/Libraries/CoreNodeModels/Input/DateTime.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Dynamo.Models;
+using Dynamo.Nodes;
+using ProtoCore.AST.AssociativeAST;
+
+namespace DSCoreNodesUI
+{
+    [NodeName("Date Time")]
+    [NodeDescription("Create a DateTime object by selecting a date and time.")]
+    [NodeCategory(BuiltinNodeCategories.CORE_INPUT)]
+    [IsDesignScriptCompatible]
+    public class DateTime : NodeModel
+    {
+        private System.DateTime dateTimeCore;
+
+        public System.DateTime DateTimeCore
+        {
+            get
+            {
+                return dateTimeCore;
+            }
+            set
+            {
+                dateTimeCore = value; 
+                OnNodeModified();
+            }
+        }
+
+        public DateTime()
+        {
+            OutPortData.Add(new PortData("date/time", "A DateTime object."));
+            RegisterAllPorts();
+
+            ArgumentLacing = LacingStrategy.Disabled;
+            ShouldDisplayPreviewCore = false;
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            var yearNode = AstFactory.BuildIntNode(DateTimeCore.Year);
+            var monthNode = AstFactory.BuildIntNode(DateTimeCore.Month);
+            var dayNode = AstFactory.BuildIntNode(DateTimeCore.Day);
+            var hourNode = AstFactory.BuildIntNode(DateTimeCore.Hour);
+            var minuteNode = AstFactory.BuildIntNode(DateTimeCore.Minute);
+            var secondNode = AstFactory.BuildIntNode(DateTimeCore.Second);
+            var msNode = AstFactory.BuildIntNode(DateTimeCore.Millisecond);
+
+            var funcNode =
+                AstFactory.BuildFunctionCall(new Func<int, int, int, int, int, int, int, System.DateTime>(DSCore.DateTime.ByDateAndTime),
+                    new List<AssociativeNode>
+                    {
+                        yearNode,
+                        monthNode,
+                        dayNode,
+                        hourNode,
+                        minuteNode,
+                        secondNode,
+                        msNode
+                    });
+
+            return new[]
+            {
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), funcNode)
+            };
+        }
+    }
+}
