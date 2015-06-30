084880df981e9efd903f15f35be4cd6caaede704
Minh Nguyen <minhnb3192@gmail.com>
2015-06-30 10:21:31 +0800

    localize Unit Conversion node replace CR in resource localize FunctionNamePrompt tooltips localize PresetPrompt tooltips get enum message based on culture localize select node select button localize Engine exception replace default message of ArgumentNullException

--- src/Libraries/CoreNodeModelsWpf/Converters/SelectionToTextConverter.cs
+++ src/Libraries/CoreNodeModelsWpf/Converters/SelectionToTextConverter.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using DynamoConversions;
+using Dynamo.Wpf.Properties;
 
-namespace Dynamo.Wpf.Converters
+namespace Dynamo.Controls
 {
-    public class SelectionToTextConverter : IValueConverter
+    public class UnitToTextConverter : IValueConverter
     {
         // parameter is the data context
         // value is the selection
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value.ToString();
+            if (value is ConversionUnit)
+            {
+                ConversionUnit st = (ConversionUnit) value;
+                string localizedEnum = Resources.ResourceManager.GetString("Unit" + st, culture);
+                return localizedEnum ?? string.Empty;
+            }
+            return string.Empty;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return null;
+            return value;
         }
+
     }
 }
