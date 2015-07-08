25d740f682c4f4f62f51fa23ac58709a5dc550ae
ramramps <sriram.pollachisubburaman@autodesk.com>
2015-07-08 13:26:26 -0400

    Updated Max Length and Added a test
    Updated Max Length and Added a test

--- src/DynamoCoreWpf/UI/Prompts/PresetPrompt.xaml.cs
+++ src/DynamoCoreWpf/UI/Prompts/PresetPrompt.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using Dynamo.Controls;
 using Dynamo.Utilities;
 using System;
+using System.Windows.Controls;
 
 namespace Dynamo.Nodes
 {
@@ -18,8 +19,13 @@ namespace Dynamo.Nodes
 
             this.Owner = WpfUtilities.FindUpVisualTree<DynamoView>(this);
             this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-
+            this.Loaded +=PresetPrompt_Loaded;
             this.nameBox.Focus();
+        }
+
+        private void PresetPrompt_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.TextRemaining.Content = Wpf.Properties.Resources.PresetTextRemaining + " " + (this.nameBox.MaxLength - this.nameBox.Text.Length);
         }
 
         void OK_Click(object sender, RoutedEventArgs e)
@@ -34,7 +40,15 @@ namespace Dynamo.Nodes
 
         public string Text
         {
-            get { return this.nameBox.Text; }
+            get
+            {
+                return this.nameBox.Text;
+            }
+
+            set
+            {
+                this.nameBox.Text = value;
+            }
         }
 
         public string Description
@@ -42,6 +56,10 @@ namespace Dynamo.Nodes
             get { return this.DescriptionInput.Text; }
         }
 
-      
+
+        private void NameBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.TextRemaining.Content = Wpf.Properties.Resources.PresetTextRemaining  + " " + (this.nameBox.MaxLength - this.nameBox.Text.Length);
+        }
     }
 }
