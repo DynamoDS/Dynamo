56473d7540ef051b62b06b4ee0fce9b9a667aee4
ramramps <sriram.pollachisubburaman@autodesk.com>
2015-07-08 13:28:11 -0400

    Updated resource file
    Updated resource file

---
+++ test/DynamoCoreWpfTests/PresetViewModelTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+using SystemTestServices;
+
+using Dynamo;
+using Dynamo.Controls;
+using Dynamo.Models;
+using Dynamo.Nodes;
+using Dynamo.Selection;
+using Dynamo.Services;
+using Dynamo.Tests;
+using Dynamo.ViewModels;
+using NUnit.Framework;
+
+namespace DynamoCoreWpfTests
+{
+    [TestFixture]
+    public class PresetViewModelTests : DynamoViewModelUnitTest
+    {
+        [Test]
+        public void TestCharacterLength()
+        {
+           //create a preset prompt
+           var prompt = new PresetPrompt();
+           
+           //Enter the text in the textbox - more than 50 characters
+           prompt.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaqqq";
+
+           //check for the characters in the textbox - characters after the max length
+           //should get cut off.
+           Assert.AreEqual("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+               "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        }
+    }
+}
