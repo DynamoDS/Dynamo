f11f6079ea761ea1dc4140a4e59b69e75f6828e9
Minh Nguyen <minhnb3192@gmail.com>
2015-07-09 15:08:16 +0800

    remove redundant test case

--- test/DynamoCoreTests/Settings.cs
+++ test/DynamoCoreTests/Settings.cs
@@ -10,28 +10,7 @@ namespace Dynamo.Tests
     class PreferenceSettingTests : DynamoModelTestBase
     {
         public string SettingDirectory { get { return Path.Combine(TestDirectory, "settings"); } }
-
-        [Test]
-        public void LoadMultiplePackageLocationsFromSetting()
-        {
-            var filePath = Path.Combine(SettingDirectory, "duplicatePaths_DynamoSettings.xml");
-            var settings = PreferenceSettings.Load(filePath);
-
-            Assert.NotNull(settings);
-            Assert.AreEqual(4, settings.PackageFolders.Count);
-            Assert.AreEqual(3, settings.CustomNodeFolders.Count);
-
-            var expectedPackageFolders = new List<string> {@"C:\21", @"C:\46", @"C:\55", @"C:\17"};
-
-            IEnumerable<bool> comparisonResult = settings.PackageFolders.Zip(expectedPackageFolders, string.Equals);
-            Assert.IsFalse(comparisonResult.Any(isEqual => !isEqual));
-
-            var expectedCustomNodeFolders = new List<string> {@"D:\17", @"D:\46", @"D:\55"};
-
-            comparisonResult = settings.CustomNodeFolders.Zip(expectedCustomNodeFolders, string.Equals);
-            Assert.IsFalse(comparisonResult.Any(isEqual => !isEqual));
-        }
-
+        
         [Test]
         public void LoadInvalidLocationsFromSetting()
         {
