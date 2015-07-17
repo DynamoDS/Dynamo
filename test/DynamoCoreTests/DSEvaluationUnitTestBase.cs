8fec33ab0e6b5224f076c89b23c3dd503df46200
Ian Keough <ian.keough@autodesk.com>
2015-07-16 20:59:25 -0700

    Add DateTime input test files and first test.

--- test/DynamoCoreTests/DSEvaluationUnitTestBase.cs
+++ test/DynamoCoreTests/DSEvaluationUnitTestBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 using Dynamo.Models;
---
+++ test/DynamoCoreWpfTests/DateTimeTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Dynamo;
+using Dynamo.Tests;
+using NUnit.Framework;
+using DateTime = DSCoreNodesUI.DateTime;
+
+namespace DynamoCoreWpfTests
+{
+    class DateTimeTests : DynamoModelTestBase
+    {
+        protected override void GetLibrariesToPreload(List<string> libraries)
+        {
+            libraries.Add("DSCoreNodes.dll");
+            base.GetLibrariesToPreload(libraries);
+        }
+
+        [Test]
+        public void DateTimeNode_Open_DeserializesCorrectly()
+        {
+            string path = Path.Combine(TestDirectory, "core", "dateTime", "DateTime.dyn");
+            RunModel(path);
+
+            var node = CurrentDynamoModel.CurrentWorkspace.FirstNodeFromWorkspace<DateTime>();
+
+            var dt = (System.DateTime)GetPreviewValue(node.GUID.ToString());
+
+            Assert.AreEqual(string.Format("{0:" + PreferenceSettings.DefaultDateFormat + "}", dt), "January 01, 2150 3:33 PM");
+        }
+    }
+}
