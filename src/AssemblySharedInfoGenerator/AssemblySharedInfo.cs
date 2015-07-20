a988b69e51d99f37ac210b11d40ad16415bdebc9
ramramps <sriram.pollachisubburaman@autodesk.com>
2015-07-20 11:19:03 -0400

    Updated as part of merge
    Updated as part of merge

--- src/AssemblySharedInfoGenerator/AssemblySharedInfo.cs
+++ src/AssemblySharedInfoGenerator/AssemblySharedInfo.cs
@@ -66,8 +66,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-<<<<<<< HEAD
-[assembly: AssemblyFileVersion("0.8.2.1946")]
-=======
+
 [assembly: AssemblyFileVersion("0.8.2.1957")]
->>>>>>> f5f094aafe0d608f326273561eaa6f6292451fe0
+
