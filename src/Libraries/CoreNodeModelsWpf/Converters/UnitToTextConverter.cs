83dffbc6d0cf8cb7f9faebeba00bbb49d7ec930f
zora-wang <t_yanz@SQALABSMT.acadqasrf.autodesk.com>
2015-07-23 12:16:03 +0800

    localize issue

--- src/Libraries/CoreNodeModelsWpf/Converters/UnitToTextConverter.cs
+++ src/Libraries/CoreNodeModelsWpf/Converters/UnitToTextConverter.cs
@@ -11,11 +11,17 @@ namespace Dynamo.Controls
         // parameter is the data context
         // value is the selection
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
+        {         
             if (value is ConversionUnit)
             {
                 ConversionUnit st = (ConversionUnit) value;
-                string localizedEnum = Resources.ResourceManager.GetString("Unit" + st, culture);
+                string localizedEnum = Resources.ResourceManager.GetString("Unit" + st.ToString(), CultureInfo.CurrentUICulture);
+                return localizedEnum ?? string.Empty;
+            }
+            else if (value is ConversionMetricUnit)
+            {
+                ConversionMetricUnit st = (ConversionMetricUnit)value;
+                string localizedEnum = Resources.ResourceManager.GetString("Unit" + st.ToString(), CultureInfo.CurrentUICulture);
                 return localizedEnum ?? string.Empty;
             }
             return string.Empty;
