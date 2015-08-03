018002ed749484f2b7fd961f33dc8fa468851e8a
Yu Ke <yu.ke@autodesk.com>
2015-08-03 14:49:27 +0800

    Add UI test case for node to code

---
+++ test/DynamoCoreWpfTests/NodeToCodeTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using Dynamo.Tests;
+using Dynamo.Selection;
+using Dynamo.Nodes;
+
+namespace DynamoCoreWpfTests
+{
+    [TestFixture]
+    public class NodeToCodeTests : DynamoViewModelUnitTest
+    {
+        [Test]
+        public void TestNodeToCodeCommandState()
+        {
+            DynamoSelection.Instance.ClearSelection();
+            Assert.IsFalse(ViewModel.CurrentSpaceViewModel.NodeToCodeCommand.CanExecute(null));
+
+            var node = new DoubleInput();
+            ViewModel.Model.CurrentWorkspace.AddAndRegisterNode(node, false);
+            DynamoSelection.Instance.Selection.Add(node);
+            Assert.IsTrue(ViewModel.CurrentSpaceViewModel.NodeToCodeCommand.CanExecute(null));
+
+            DynamoSelection.Instance.ClearSelection();
+            Assert.IsFalse(ViewModel.CurrentSpaceViewModel.NodeToCodeCommand.CanExecute(null));
+        }
+    }
+}
