d180ab5179ae70b8ae868a93e28cb74f636d97f2
Yu Ke <yu.ke@autodesk.com>
2015-08-05 13:48:58 +0800

    Add InputNodeLoader to handle namespace resolution

---
+++ src/DynamoCore/Models/NodeLoaders/InputNodeLoader.cs
@@ -0,0 +1,29 @@
+﻿using Dynamo.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Dynamo.Models.NodeLoaders
+{
+    public class InputNodeLoader : INodeLoader<Symbol>, INodeFactory<Symbol>
+    {
+        public InputNodeLoader()
+        {
+        }
+
+        public Symbol CreateNodeFromXml(XmlElement elNode, SaveContext context, ProtoCore.Namespace.ElementResolver resolver)
+        {
+            var node = CreateNode();
+            node.ElementResolver = resolver;
+            node.Deserialize(elNode, context);
+            return node;
+        }
+
+        public Symbol CreateNode()
+        {
+            return new Symbol(); 
+        }
+    }
+}
