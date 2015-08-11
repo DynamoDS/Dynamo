1d9b6c5a5bc3ddc66e56f566471ffa0bb3d73a5e
Jun Mendoza <jun.mendoza@autodesk.com>
2015-08-11 12:52:39 +0800

    Add LR testcase

--- test/Engine/ProtoTest/LiveRunnerTests/MicroFeatureTests.cs
+++ test/Engine/ProtoTest/LiveRunnerTests/MicroFeatureTests.cs
@@ -5727,6 +5727,113 @@ k = __TryGetValueFromNestedDictionaries(i, ""a"");
             AssertValue("j", 1);
             AssertValue("k", 3);
         }
+
+        [Test]
+        public void RegressMAGN7759()
+        {
+            // Tracked in: http://adsk-oss.myjetbrains.com/youtrack/issue/MAGN-7759
+
+            // Simulate behavior in MAGN-7759
+            List<string> codes = new List<string>() 
+            {
+                
+// =================Run 1===============
+// guid1
+@"
+import(""FFITarget.dll"");
+",
+
+// Add code 
+// guid2
+@"
+y = 2;          
+",
+// guid3
+@"
+s = 0;
+",
+// guid4
+@"
+rad = s;
+x = 1 / rad;
+",
+// guid5
+@"
+pt = DummyPoint.ByCoordinates(x, y, 0);
+",
+ 
+ 
+// =================Run 2===============
+// Modify from y from 2 to 3
+// The UI sends Point.ByCoordinates twice
+
+// guid5 
+@"
+pt = DummyPoint.ByCoordinates(x, 0, 0);
+",
+ 
+// =================Run 3===============
+// guid2
+@"
+y = 3;
+",
+
+// guid5
+@" 
+pt = DummyPoint.ByCoordinates(x, y, 0);
+",
+
+ 
+// =================Run 4===============
+// Update the input from 0 to 1
+// guid3
+@" 
+s = 1;
+"
+            };
+
+            Guid guid1 = System.Guid.NewGuid();
+            Guid guid2 = System.Guid.NewGuid();
+            Guid guid3 = System.Guid.NewGuid();
+            Guid guid4 = System.Guid.NewGuid();
+            Guid guid5 = System.Guid.NewGuid();
+
+            // Run 1
+            List<Subtree> added = new List<Subtree>();
+            added.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid1, codes[0]));
+            added.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid2, codes[1]));
+            added.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid3, codes[2]));
+            added.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid4, codes[3]));
+            added.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid5, codes[4]));
+
+            var syncData = new GraphSyncData(null, added, null);
+            liveRunner.UpdateGraph(syncData);
+
+
+            // Modfiy 2 to 3
+            // Run 2
+            List<Subtree> modified = new List<Subtree>();
+            modified.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid5, codes[5]));
+            syncData = new GraphSyncData(null, null, modified);
+            liveRunner.UpdateGraph(syncData);
+
+            // Run 3
+            modified = new List<Subtree>();
+            modified.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid2, codes[6]));
+            modified.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid5, codes[7]));
+            syncData = new GraphSyncData(null, null, modified);
+            liveRunner.UpdateGraph(syncData);
+
+
+            // Run 4
+            modified = new List<Subtree>();
+            modified.Add(ProtoTestFx.TD.TestFrameWork.CreateSubTreeFromCode(guid3, codes[8]));
+            syncData = new GraphSyncData(null, null, modified);
+            liveRunner.UpdateGraph(syncData);
+
+            // There should be no warnings
+            Assert.AreEqual(0, liveRunner.RuntimeCore.RuntimeStatus.WarningCount);
+        }
      
     }
 
