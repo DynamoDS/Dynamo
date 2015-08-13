d1ef782c6e9f8178647ae2125438d0cad641c91f
Peter Boyer <peter.b.boyer@gmail.com>
2015-08-13 13:33:38 -0400

    Clean up extension tests, fix broken PackageDirectoryBuilderTests

--- test/DynamoCoreTests/ExtensionTests.cs
+++ test/DynamoCoreTests/ExtensionTests.cs
@@ -6,13 +6,11 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using Dynamo.Extensions;
 using System.IO;
-using Dynamo.PackageManager;
-using System.Reflection;
 using Dynamo.Models;
 using Moq;
 using Dynamo.Interfaces;
 
-namespace PackageManagerTests
+namespace Dynamo.Tests
 {
     class ExtensionTests
     {
@@ -38,16 +36,6 @@ namespace PackageManagerTests
         public void DirectoryExists()
         {
             Assert.IsTrue(Directory.Exists(extensionsPath));
-        }
-
-        [Test]
-        public void ExtensionsAreExtracted()
-        {
-            var extensionManager = new ExtensionManager();
-            var extensions = extensionManager.ExtensionLoader.LoadDirectory(extensionsPath);
-            Assert.Greater(extensions.Count(), 0);
-
-            Assert.AreEqual(extensions.OfType<PackageManagerExtension>().Count(), 1);
         }
 
         [Test]
