49d8a98c64952455901a0bbe1da926b2d54696b2
Jun Mendoza <jun.mendoza@autodesk.com>
2015-08-17 12:32:56 +0800

    Remove redundant test

--- test/Engine/ProtoTest/Associative/SimpleVerification.cs
+++ test/Engine/ProtoTest/Associative/SimpleVerification.cs
@@ -118,51 +118,5 @@ namespace ProtoTest.Associative
                 );
         }
 
-
-        [Test]
-        public void TestSyntaxError01()
-        {
-            String code =
-            @"
-// No semicolon
-a = 10 
-";
-            thisTest.RunAndVerifySyntaxError(code);
-        }
-
-        [Test]
-        public void TestCompileWarning01()
-        {
-            const string code = @"
-a = f();
-";
-            thisTest.RunAndVerifyBuildWarning(
-                code, 
-                ProtoCore.BuildData.WarningID.kFunctionNotFound
-                );
-        }
-
-        [Test]
-        public void TestRuntimeWarning01()
-        {
-            string code =
-
-@"
-a = 2 % 0;
-b = 2.1 % 0;
-";
-
-            thisTest.RunAndVerifyRuntimeWarning(code, ProtoCore.Runtime.WarningID.kModuloByZero);
-        }
-
-        [Test]
-        public void TestStaticCyclicDependency01()
-        {
-            string code = @"
-a = 1;
-b = a;
-a = b;";
-            thisTest.RunAndVerifyBuildWarning(code, ProtoCore.BuildData.WarningID.kInvalidStaticCyclicDependency);
-        }
     }
 }
