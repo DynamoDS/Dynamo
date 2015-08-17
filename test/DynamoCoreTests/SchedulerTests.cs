c4b90078e551cf4d402b9a47e831182c172079b7
Ian Keough <ian.keough@autodesk.com>
2015-08-17 13:00:26 -0700

    Fix scheduler tests.

--- test/DynamoCoreTests/SchedulerTests.cs
+++ test/DynamoCoreTests/SchedulerTests.cs
@@ -951,29 +951,27 @@ namespace Dynamo.Tests
             var tasksToSchedule = new List<AsyncTask>()
             {
                 // Query value for a given named variable.
-                MakeQueryMirrorDataAsyncTask("variableOne"),
+                MakeQueryMirrorDataAsyncTask("variableOne"), //0
 
                 // This older task is kept because it wasn't re-scheduled.
-                MakeUpdateRenderPackageAsyncTask(nodes[0].GUID),
+                MakeUpdateRenderPackageAsyncTask(nodes[0].GUID), // 1
 
                 // These older tasks are to be dropped.
-                MakeUpdateRenderPackageAsyncTask(nodes[1].GUID),
-                MakeUpdateRenderPackageAsyncTask(nodes[2].GUID),
-
-                // These older tasks are to be dropped.
+                MakeUpdateRenderPackageAsyncTask(nodes[1].GUID), //2
+                MakeUpdateRenderPackageAsyncTask(nodes[2].GUID), //3
 
                 // This higher priority task moves to the front.
-                MakeUpdateGraphAsyncTask(),
+                MakeUpdateGraphAsyncTask(), //4
 
                 // Query value for a given named variable.
-                MakeQueryMirrorDataAsyncTask("variableOne"),
+                MakeQueryMirrorDataAsyncTask("variableOne"), //5
 
                 // These newer tasks will be kept.
-                MakeUpdateRenderPackageAsyncTask(nodes[1].GUID),
-                MakeUpdateRenderPackageAsyncTask(nodes[2].GUID),
+                MakeUpdateRenderPackageAsyncTask(nodes[1].GUID), //6
+                MakeUpdateRenderPackageAsyncTask(nodes[2].GUID), //7
 
                 // This higher priority task moves to the front.
-                MakeUpdateGraphAsyncTask(),
+                MakeUpdateGraphAsyncTask(), //8 
             };
 
             // Due to defaulting to auto-run mode, multiple UpdateGraphAsyncTask
@@ -992,14 +990,12 @@ namespace Dynamo.Tests
 
             var expected = new List<string>
             {
-                "FakeUpdateGraphAsyncTask: 10",
+                "FakeUpdateGraphAsyncTask: 8",
                 "FakeQueryMirrorDataAsyncTask: 0",
                 "FakeUpdateRenderPackageAsyncTask: 1",
-                "FakeQueryMirrorDataAsyncTask: 7",
-                "FakeUpdateRenderPackageAsyncTask: 8",
-                "FakeUpdateRenderPackageAsyncTask: 9",
-                "FakeNotifyRenderPackagesReadyAsyncTask: 11",
-                "FakeAggregateRenderPackageAsyncTask: 12",
+                "FakeQueryMirrorDataAsyncTask: 5",
+                "FakeUpdateRenderPackageAsyncTask: 6",
+                "FakeUpdateRenderPackageAsyncTask: 7",
             };
 
             Assert.AreEqual(expected.Count, results.Count);
@@ -1038,9 +1034,9 @@ namespace Dynamo.Tests
                 "FakeSetTraceDataAsyncTask: 0",
                 "FakeCompileCustomNodeAsyncTask: 1",
                 "FakeUpdateGraphAsyncTask: 2",
-                "FakeDelegateBasedAsyncTask: 4",
-                "FakeQueryMirrorDataAsyncTask: 5",
-                "FakeUpdateRenderPackageAsyncTask: 7"
+                "FakeDelegateBasedAsyncTask: 3",
+                "FakeQueryMirrorDataAsyncTask: 4",
+                "FakeUpdateRenderPackageAsyncTask: 5"
             };
 
             Assert.AreEqual(expected.Count, results.Count);
@@ -1076,12 +1072,12 @@ namespace Dynamo.Tests
 
             var expected = new List<string>
             {
-                "FakeSetTraceDataAsyncTask: 7",
-                "FakeUpdateGraphAsyncTask: 5",
-                "FakeCompileCustomNodeAsyncTask: 6",
+                "FakeSetTraceDataAsyncTask: 5",
+                "FakeUpdateGraphAsyncTask: 3",
+                "FakeCompileCustomNodeAsyncTask: 4",
                 "FakeUpdateRenderPackageAsyncTask: 0",
-                "FakeQueryMirrorDataAsyncTask: 2",
-                "FakeDelegateBasedAsyncTask: 3"
+                "FakeQueryMirrorDataAsyncTask: 1",
+                "FakeDelegateBasedAsyncTask: 2"
             };
 
             Assert.AreEqual(expected.Count, results.Count);
@@ -1101,13 +1097,13 @@ namespace Dynamo.Tests
             // Everything is scheduled in reversed order of priority.
             var tasksToSchedule = new List<AsyncTask>()
             {
-                MakeUpdateRenderPackageAsyncTask(Guid.NewGuid()),   // Normal
-                MakeDelegateBasedAsyncTask(),                       // Normal
-                MakeQueryMirrorDataAsyncTask("variableName"),       // Normal
-                MakeQueryMirrorDataAsyncTask("variableName"),       // Normal
-                MakeUpdateGraphAsyncTask(),                         // Above normal
-                MakeCompileCustomNodeAsyncTask(),                   // Above normal
-                MakeSetTraceDataAsyncTask(),                        // Highest
+                MakeUpdateRenderPackageAsyncTask(Guid.NewGuid()),   // Normal 0
+                MakeDelegateBasedAsyncTask(),                       // Normal 1
+                MakeQueryMirrorDataAsyncTask("variableName"),       // Normal 2
+                MakeQueryMirrorDataAsyncTask("variableName"),       // Normal 3
+                MakeUpdateGraphAsyncTask(),                         // Above normal 4
+                MakeCompileCustomNodeAsyncTask(),                   // Above normal 5
+                MakeSetTraceDataAsyncTask(),                        // Highest 6
             };
 
             var scheduler = dynamoModel.Scheduler;
@@ -1120,12 +1116,13 @@ namespace Dynamo.Tests
 
             var expected = new List<string>
             {
-                "FakeSetTraceDataAsyncTask: 11",
-                "FakeUpdateGraphAsyncTask: 9",
-                "FakeCompileCustomNodeAsyncTask: 10",
-                "FakeDelegateBasedAsyncTask: 2",
-                "FakeQueryMirrorDataAsyncTask: 5",
-                "FakeQueryMirrorDataAsyncTask: 8",
+                "FakeSetTraceDataAsyncTask: 6",
+                "FakeUpdateGraphAsyncTask: 4",
+                "FakeCompileCustomNodeAsyncTask: 5",
+                "FakeUpdateRenderPackageAsyncTask: 0",
+                "FakeDelegateBasedAsyncTask: 1",
+                "FakeQueryMirrorDataAsyncTask: 2",
+                "FakeQueryMirrorDataAsyncTask: 3",
             };
 
             Assert.AreEqual(expected.Count, results.Count);
