a55944a2bfb6b0f7ee338295e35945ad03c1fe71
ramramps <sriram.pollachisubburaman@autodesk.com>
2015-08-26 07:24:05 -0400

    Added a test project for testing
    Added a test project for testing

---
+++ tools/XmlDocToMarkdown/DocumentationTestLibrary/TestDocumentation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.DocumentationTestLibrary
+{
+    public class TestDocumentation
+    {
+        /// <summary>
+        /// The length property
+        /// </summary>
+        public double Length { get; set; }
+
+        /// <summary>
+        /// The breadth property
+        /// </summary>
+        public double Breadth { get; set; }
+
+        /// <summary>
+        /// The height property
+        /// </summary>
+        public double Height { get; set; }
+
+        /// <summary>
+        /// Sets the length.
+        /// </summary>
+        /// <param name="len">The length.</param>
+        public void SetLength(double len)
+        {
+            Length = len;
+        }
+
+        /// <summary>
+        /// Sets the breadth.
+        /// </summary>
+        /// <param name="bre">The bre.</param>
+        public void SetBreadth(double bre)
+        {
+            Breadth = bre;
+        }
+
+        /// <summary>
+        /// Sets the height.
+        /// </summary>
+        /// <param name="hei">The hei.</param>
+        public void SetHeight(double hei)
+        {
+            Height = hei;
+        }
+
+        /// <summary>
+        /// Gets the volume.
+        /// </summary>
+        /// <returns></returns>
+        public double GetVolume()
+        {
+            return Length * Breadth * Height;
+        }
+    }
+}
