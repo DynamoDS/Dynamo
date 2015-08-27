6ae8e606c7055832c2546cb25731e5c27a185f17
Ian Keough <ian.keough@autodesk.com>
2015-08-27 11:51:19 -0700

    Hide ConnectorHandler

--- src/DynamoCore/Models/DynamoModelDelegates.cs
+++ src/DynamoCore/Models/DynamoModelDelegates.cs
@@ -7,7 +7,7 @@ namespace Dynamo.Models
     public delegate void CleanupHandler(DynamoModel dynamoModel);
     public delegate void DynamoModelHandler(DynamoModel model);
     public delegate void NodeHandler(NodeModel node);
-    public delegate void ConnectorHandler(ConnectorModel connector);
+    internal delegate void ConnectorHandler(ConnectorModel connector);
     public delegate void WorkspaceHandler(WorkspaceModel model);   
     public delegate void ActionHandler(Action action);
     public delegate void NodeEventHandler(object sender, EventArgs e);
