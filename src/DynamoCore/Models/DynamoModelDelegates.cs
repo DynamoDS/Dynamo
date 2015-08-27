a40c3ca637a42a42d499dedde558902f5228122e
Ian Keough <ian.keough@autodesk.com>
2015-08-27 11:54:00 -0700

    Remove two unused handlers.

--- src/DynamoCore/Models/DynamoModelDelegates.cs
+++ src/DynamoCore/Models/DynamoModelDelegates.cs
@@ -4,10 +4,8 @@ using ProtoScript.Runners;
 
 namespace Dynamo.Models
 {
-    public delegate void CleanupHandler(DynamoModel dynamoModel);
     public delegate void DynamoModelHandler(DynamoModel model);
     public delegate void NodeHandler(NodeModel node);
-    internal delegate void ConnectorHandler(ConnectorModel connector);
     public delegate void WorkspaceHandler(WorkspaceModel model);   
     public delegate void ActionHandler(Action action);
     public delegate void NodeEventHandler(object sender, EventArgs e);
