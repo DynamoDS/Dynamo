3538d12f997c4adab684530700d8108caf4074e5
ramramps <sriram.pollachisubburaman@autodesk.com>
2015-09-04 10:13:18 -0400

    Added appveyor path to the script
    Added appveyor path to the script

--- tools/XmlDocToMarkdown/XmlDocToMarkdown/BuildScript/BuildScript.ps1
+++ tools/XmlDocToMarkdown/XmlDocToMarkdown/BuildScript/BuildScript.ps1
@@ -1,7 +1,7 @@
 ﻿Write-Host "Running Xml Markdown"
 
-$app = "C:\projects\tools\XmlDocToMarkdown\XmlDocToMarkdown\bin\Debug\XmlDocToMarkdown.exe"
-$arg1 = "C:\projects\bin\Release\DynamoCore.dll"
-$arg2 = "C:\projects\bin\Release\DynamoCore.xml"
+$app = "C:\projects\dynamo\tools\XmlDocToMarkdown\XmlDocToMarkdown\bin\Debug\XmlDocToMarkdown.exe"
+$arg1 = "C:\projects\dynamo\bin\Release\DynamoCore.dll"
+$arg2 = "C:\projects\dynamo\bin\Release\DynamoCore.xml"
 
 & $app $arg1 $arg2
