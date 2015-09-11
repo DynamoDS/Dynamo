7062c766dd759a9064a512a7a81fe4b1839bf533
ramramps <sriram.pollachisubburaman@autodesk.com>
2015-09-11 12:12:18 -0400

    publish doc check1
    publish doc check1

--- tools/XmlDocToMarkdown/XmlDocToMarkdown/BuildScript/PublishDocs.ps1
+++ tools/XmlDocToMarkdown/XmlDocToMarkdown/BuildScript/PublishDocs.ps1
@@ -7,18 +7,28 @@ $pipArg2 ="mkdocs"
 	
 Write-Host "Copying MkDocs"
 $mkDocs = "C:\Python27\Scripts\mkdocs.exe"
-$mkDocsDest = "C:\projects\dynamo\tools\XmlDocToMarkdown\XmlDocToMarkdown\bin\Release"
+$mkDocsDest = "C:\projects\dynamo\DocumentTesting"
 
 Copy-Item $mkDocs  $mkDocsDest
 
+Write-Host "Copying yml"
+$ymlLocation = "C:\projects\dynamo\tools\XmlDocToMarkdown\XmlDocToMarkdown\bin\Release\mkdocs.yml"
+$ymlDest = "C:\projects\dynamo\DocumentTesting"
+
+Copy-Item $ymlLocation  $ymlDest
+
+Write-Host "Copying Docs folder"
+$docLocation = "C:\projects\dynamo\tools\XmlDocToMarkdown\XmlDocToMarkdown\bin\Release\docs"
+$docDest = "C:\projects\dynamo\DocumentTesting"
+
+Copy-Item $docLocation  $docDest -recurse
+
 Write-Host "Running MkDocs"
-$mkDocsLoc = "C:\projects\dynamo\tools\XmlDocToMarkdown\XmlDocToMarkdown\bin\Release"
+$mkDocsLoc = "C:\projects\dynamo\DocumentTesting"
 
 Set-Location -Path $mkDocsLoc
-Get-Location
-$mkDocsFile = "C:\projects\dynamo\tools\XmlDocToMarkdown\XmlDocToMarkdown\bin\Release\mkDocs.exe"
+$mkDocsFile = "C:\projects\dynamo\DocumentTesting\mkDocs.exe"
 $mkarg1 = "gh-deploy"
-$mkarg2 = "--remote-branch"
-$mkarg3 = "gh-pages"
+$mkarg2 = "--clean"
 
-& $mkDocsFile $mkarg1 $mkarg2 $mkarg3
+& $mkDocsFile $mkarg1 $mkarg2
