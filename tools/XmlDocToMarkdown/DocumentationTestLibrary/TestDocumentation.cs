0d9e8395d7878654821701f9cac6acc1887fea19
ramramps <sriram.pollachisubburaman@autodesk.com>
2015-09-15 16:50:49 -0400

    Updated to convert the special char on xml
    Updated to convert the special char on xml

--- tools/XmlDocToMarkdown/DocumentationTestLibrary/TestDocumentation.cs
+++ tools/XmlDocToMarkdown/DocumentationTestLibrary/TestDocumentation.cs
@@ -15,15 +15,32 @@ using System.IO;
 
 namespace Dynamo.DocumentationTestLibrary
 {
+    #region Interface
+    /// <summary>
+    ///  Has an event that produces items.
+    /// </summary>
+    /// <typeparam name="TItem">Type of items produced.</typeparam>
+    public interface ISource<out TItem>
+    {
+        /// <summary>
+        ///     Produces items, potentially asynchronously.
+        /// </summary>
+        event Action<TItem> ItemProduced;
+    }
+
+    #endregion
+
     public class TestDocumentation
     {
+
+
         #region properties
 
         /// <summary>
         /// Testing property
         /// </summary>
         public int property1 { get; set; }
-      
+
         #endregion
 
         #region events
--- tools/XmlDocToMarkdown/XmlDocToMarkdown/Program.cs
+++ tools/XmlDocToMarkdown/XmlDocToMarkdown/Program.cs
@@ -106,11 +106,13 @@ namespace Dynamo.Docs
 
         private static void GenerateMarkdownDocumentForType(Type t, string folder, XDocument xml)
         {
+           var typeName = t.Name.Replace("``1", "").Replace("``0", "").Replace("`1", "").Replace("`0", "");
+            
             var members = xml.Root.Element("members").Elements("member");
 
             //get all the generic members. Generic methods in XML has special characters 
             // List<T> in xml is List``1. So, replace them with correct values. Here, instead
-            // of angular brackets we use [ ]
+            // of angular brackets we use [ ]             
             var genericMembers = members.Where(x => x.Attribute("name").Value.Contains("``1") ||
                                                     x.Attribute("name").Value.Contains("``0") ||
                                                     x.Attribute("name").Value.Contains("`1") ||
@@ -123,17 +125,21 @@ namespace Dynamo.Docs
                     if (typeParamelem != null)
                     {
                         var typeParamName = typeParamelem.Attribute("name").Value;
-                        var text = genericMember.Attribute("name").Value;
-                        text = text.Replace("``1", "<*" + typeParamName + "*>");
-                        text = text.Replace("``0", "<*" + typeParamName + "*>");
-
+                        var text = ConvertGenericParameterName(genericMember.Attribute("name").Value, typeParamName);                    
                         genericMember.Attribute("name").Value = text;
                     }
+                }
+
+                //if there is no typeparam (possible if it is not documented correctly, then just replace it by T.
+                else
+                {
+                    var text = ConvertGenericParameterName(genericMember.Attribute("name").Value);                    
+                    genericMember.Attribute("name").Value = text;
                 }
             }
             var sb = new StringBuilder();
 
-            sb.AppendLine("#" + t.Name);
+            sb.AppendLine("#" + typeName);
             sb.Append(GetMarkdownForType(members, t.FullName));
             sb.AppendLine("---");
 
@@ -165,7 +171,8 @@ namespace Dynamo.Docs
             sb.AppendLine("##Properties  ");
             foreach (var property in t.GetProperties())
             {
-                var current = GetMarkdownForProperty(members, t.FullName + "." + property.Name);               
+                var propertyNameSpace = ConvertGenericParameterName(t.FullName);
+                var current = GetMarkdownForProperty(members, propertyNameSpace + "." + property.Name);               
                 sb.Append(current);
             }
             sb.AppendLine("---");
@@ -173,17 +180,26 @@ namespace Dynamo.Docs
             sb.AppendLine("##Events  ");
             foreach (var e in t.GetEvents())
             {
-                var current = GetMarkdownForEvent(members, t.FullName + "." + e.Name);                
+                var eventNameSpace = ConvertGenericParameterName(t.FullName);                
+                var current = GetMarkdownForEvent(members, eventNameSpace + "." + e.Name);                
                 sb.Append(current);
             }
             sb.AppendLine("---");
 
-            var fileName = t.Name + ".md";
-            var filePath = Path.Combine(folder, t.Name + ".md");
+            var fileName = typeName + ".md";
+            var filePath = Path.Combine(folder, fileName);
             System.IO.File.WriteAllText(filePath, sb.ToString());
         }
 
-      
+        private static string ConvertGenericParameterName(string text , string typeParamName = "T")
+        {
+           text = text.Replace("``1", "<*" + typeParamName + "*>");
+           text = text.Replace("``0", "<*" + typeParamName + "*>");
+           text = text.Replace("`1", "<*" + typeParamName + "*>");
+           text = text.Replace("`0", "<*" + typeParamName + "*>");
+
+            return text;
+        }
         private static string GetMarkdownForMethod(IEnumerable<XElement> members, string methodName)
         {
             return GetMarkdownForMember(members, string.Format("M:{0}", methodName));
@@ -258,16 +274,18 @@ namespace Dynamo.Docs
                 });
 
 
-        private static Func<string,XElement, string[]> dType =
-            new Func<string, XElement, string[]>((att1,node) =>            
+        private static Func<string, string, XElement,string[]> dType =
+            new Func<string, string, XElement, string[]>((att1,att2,node) =>            
          
             {
                 var methodName = node.Attribute(att1).Value.Split('.').Last();
                 methodName = CheckAndAppendStability(node, methodName);
-               
+
+                var convertedMethodName = ConvertGenericParameters(node, methodName, att2);
+
                 return new[]
                 {
-                    methodName,
+                    convertedMethodName,
                     node.Nodes().ToMarkDown()
                 };
             });         
@@ -276,55 +294,11 @@ namespace Dynamo.Docs
             new Func<string, string,  XElement, string[]>((att1, att2,node) =>
             {              
                 var methodName = node.Attribute(att1).Value.Split(':').Last();
-                //Seperate the method name and paramaeters (ex: Func(a,b) = [Func] + [a,b])
-                var methodParams = methodName.Split('(', ')');
-                var stringList = new CommaDelimitedStringCollection();
-                if (methodParams.Count() > 1)
-                {
-                    //Store the method name. the method name is Dynamo.Test.Test(int).
-                    //so splitting by . to get the method name as Test(int)
-                    methodName = methodParams[0].Split('.').Last(); 
-                    //Split the Parameters (ex: (a,b) = [a], [b]) 
-                    methodParams = methodParams[1].Split(',');
-                    int i = 0;
-                    foreach (var param in node.Elements(att2))
-                    {
-                        //when you add a parameter to a function, there is a possibility that comment is 
-                        //not updated immediately. In that case add the param name to only those parameters 
-                        //in the method name
-                        if (methodParams.Count() > i)
-                        {
-                            //Extract only the classname , not the entire namespace
-                            var className = string.Empty;
-                            if (methodParams[i].Contains("Dynamo"))
-                            {
-                                className = methodParams[i].Split('.').Last();
-                                var url = ConstructUrl(methodParams[i]) + "/" + className;
-                                className = "[" + className + "]" + "(" + url + ")";
-                            }
-                            else
-                            {
-                                className = methodParams[i].Split('.').Last();
-                            }
-
-                            stringList.Add(className + " " + param.Attribute("name").Value);
-                        }
-                        i++;
-                    }
-                    if (stringList.Count > 0)
-                    {
-                        methodName = methodName + "(" + stringList.ToString() + ")";
-                        
-                    }
-
-                    //add api_stability as super script. If there is no stability tag
-                    //then add a default value.
-                    methodName = CheckAndAppendStability(node, methodName);
-                }
-
+                var convertedMethodName = ConvertGenericParameters(node, methodName, att2);
+                
                 return new[]
                 {
-                    methodName.Contains("(")? methodName : methodName + "()", 
+                    convertedMethodName.Contains("(")? methodName : methodName + "()", 
                     node.Nodes().ToMarkDown()
                 };
             });
@@ -346,6 +320,57 @@ namespace Dynamo.Docs
             return methodName;
         }
 
+        public static string ConvertGenericParameters(XElement node,string methodName,string attribute)
+        {
+            //Seperate the method name and paramaeters (ex: Func(a,b) = [Func] + [a,b])
+             var methodParams = methodName.Split('(', ')');
+             var stringList = new CommaDelimitedStringCollection();
+            if (methodParams.Count() > 1)
+            {
+                //Store the method name. the method name is Dynamo.Test.Test(int).
+                //so splitting by . to get the method name as Test(int)
+                methodName = methodParams[0].Split('.').Last();
+                //Split the Parameters (ex: (a,b) = [a], [b]) 
+                methodParams = methodParams[1].Split(',');
+                int i = 0;
+                foreach (var param in node.Elements(attribute))
+                {
+                    //when you add a parameter to a function, there is a possibility that comment is 
+                    //not updated immediately. In that case add the param name to only those parameters 
+                    //in the method name
+                    if (methodParams.Count() > i)
+                    {
+                        //Extract only the classname , not the entire namespace
+                        var className = string.Empty;
+                        if (methodParams[i].Contains("Dynamo"))
+                        {
+                            className = methodParams[i].Split('.').Last();
+                            var url = ConstructUrl(methodParams[i]) + "/" + className;
+                            className = "[" + className + "]" + "(" + url + ")";
+                        }
+                        else
+                        {
+                            className = methodParams[i].Split('.').Last();
+                        }
+
+                        stringList.Add(className + " " + param.Attribute("name").Value);
+                    }
+                    i++;
+                }
+                if (stringList.Count > 0)
+                {
+                    methodName = methodName + "(" + stringList.ToString() + ")";
+
+                }
+
+                //add api_stability as super script. If there is no stability tag
+                //then add a default value.
+                methodName = CheckAndAppendStability(node, methodName);
+            }
+
+            return methodName;
+        }
+
         private static Dictionary<string, Func<XElement, IEnumerable<string>>> methods =
             new Dictionary<string, Func<XElement, IEnumerable<string>>>
                 {
@@ -353,11 +378,11 @@ namespace Dynamo.Docs
                         x.Element("assembly").Element("name").Value,
                         x.Element("members").Elements("member").ToMarkDown()
                     }},
-                    {"type", x=>dType("name", x)},
+                    {"type", x=>dType("name","param", x)},
                     {"field", x=> d("name", x)},
-                    {"property", x=> dType("name", x)},
+                    {"property", x=> dType("name","param", x)},
                     {"method",x=>mType("name", "param", x)},
-                    {"event", x=>dType("name", x)},
+                    {"event", x=>dType("name", "param",  x)},
                     {"summary", x=> new[]{ x.Nodes().ToMarkDown() }},
                     {"remarks", x => new[]{x.Nodes().ToMarkDown()}},
                     {"example", x => new[]{x.Value.ToCodeBlock()}},
