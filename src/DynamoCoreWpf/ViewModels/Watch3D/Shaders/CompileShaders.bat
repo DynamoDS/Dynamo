effae50bfd4bbc20ba32db6a4d301dafae4c9ef2
Ian Keough <ian.keough@autodesk.com>
2015-10-02 15:30:02 -0700

    Remove optimization flags from shader compilation.

--- src/DynamoCoreWpf/ViewModels/Watch3D/Shaders/CompileShaders.bat
+++ src/DynamoCoreWpf/ViewModels/Watch3D/Shaders/CompileShaders.bat
@@ -1,6 +1,6 @@
 @echo off
 call "%DXSDK_DIR%Utilities\bin\dx_setenv.cmd"
 pushd %~dp0
-fxc /Od /Zi /T fx_5_0 /I ./../ /Fo ..\Resources\_dynamo.bfx Default.fx
+fxc /T fx_5_0 /I ./../ /Fo ..\Resources\_dynamo.bfx Default.fx
 popd
 pause
