1963f1b523e96657074d2e92c0f1958849e8d917
Yu Ke <yu.ke@autodesk.com>
2015-10-20 16:14:40 +0800

    Check if EngineController is diposed or not

--- src/DynamoCore/Core/Threading/UpdateGraphAsyncTask.cs
+++ src/DynamoCore/Core/Threading/UpdateGraphAsyncTask.cs
@@ -95,11 +95,15 @@ namespace Dynamo.Core.Threading
         protected override void HandleTaskExecutionCore()
         {
             // Updating graph in the context of ISchedulerThread.
-            engineController.UpdateGraphImmediate(graphSyncData);
+            if (!engineController.IsDisposed)
+                engineController.UpdateGraphImmediate(graphSyncData);
         }
 
         protected override void HandleTaskCompletionCore()
         {
+            if (engineController.IsDisposed)
+                return;
+
             // Retrieve warnings in the context of ISchedulerThread.
             BuildWarnings = engineController.GetBuildWarnings();
             RuntimeWarnings = engineController.GetRuntimeWarnings();
