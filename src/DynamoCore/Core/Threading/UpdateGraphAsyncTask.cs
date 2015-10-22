69d35dafbc68f36a94e842cf67d14d243f9dfc0a
Yu Ke <yu.ke@autodesk.com>
2015-10-22 15:43:15 +0800

    Initialize BuildWarnings and RuntimeWarnings

--- src/DynamoCore/Core/Threading/UpdateGraphAsyncTask.cs
+++ src/DynamoCore/Core/Threading/UpdateGraphAsyncTask.cs
@@ -105,27 +105,32 @@ namespace Dynamo.Core.Threading
         protected override void HandleTaskCompletionCore()
         {
             if (engineController.IsDisposed)
-                return;
-
-            // Retrieve warnings in the context of ISchedulerThread.
-            BuildWarnings = engineController.GetBuildWarnings();
-            RuntimeWarnings = engineController.GetRuntimeWarnings();
-
-            // Mark all modified nodes as being updated (if the task has been 
-            // successfully scheduled, executed and completed, it is expected 
-            // for "modifiedNodes" to be both non-null and non-empty.
-            // 
-            // In addition to marking modified nodes as being updated, their 
-            // warning states are cleared (which include the tool-tip). Any node
-            // that has build/runtime warnings assigned to it will properly be 
-            // restored to warning state when task completion handler sets the 
-            // corresponding build/runtime warning on it.
-            // 
-            foreach (var modifiedNode in ModifiedNodes)
             {
-                modifiedNode.WasInvolvedInExecution = true;
-                if (modifiedNode.State == ElementState.Warning)
-                    modifiedNode.ClearRuntimeError();
+                BuildWarnings = new Dictionary<Guid, List<BuildWarning>>();
+                RuntimeWarnings = new Dictionary<Guid, List<RuntimeWarning>>();
+            }
+            else
+            {
+                // Retrieve warnings in the context of ISchedulerThread.
+                BuildWarnings = engineController.GetBuildWarnings();
+                RuntimeWarnings = engineController.GetRuntimeWarnings();
+
+                // Mark all modified nodes as being updated (if the task has been 
+                // successfully scheduled, executed and completed, it is expected 
+                // for "modifiedNodes" to be both non-null and non-empty.
+                // 
+                // In addition to marking modified nodes as being updated, their 
+                // warning states are cleared (which include the tool-tip). Any node
+                // that has build/runtime warnings assigned to it will properly be 
+                // restored to warning state when task completion handler sets the 
+                // corresponding build/runtime warning on it.
+                // 
+                foreach (var modifiedNode in ModifiedNodes)
+                {
+                    modifiedNode.WasInvolvedInExecution = true;
+                    if (modifiedNode.State == ElementState.Warning)
+                        modifiedNode.ClearRuntimeError();
+                }
             }
         }
 
