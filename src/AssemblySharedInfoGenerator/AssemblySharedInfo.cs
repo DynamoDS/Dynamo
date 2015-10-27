81a955a5ddb3874d6ac8dcdbc49d953a101acb8f
Ian Keough <ian.keough@autodesk.com>
2015-10-27 10:16:28 -0700

    AssemblySharedBuggerAll.

--- src/AssemblySharedInfoGenerator/AssemblySharedInfo.cs
+++ src/AssemblySharedInfoGenerator/AssemblySharedInfo.cs
@@ -45,11 +45,7 @@ using System.Runtime.InteropServices;
 // to distinguish one build from another. AssemblyFileVersion is specified
 // in AssemblyVersionInfo.cs so that it can be easily incremented by the
 // automated build process.
-<<<<<<< HEAD
-[assembly: AssemblyVersion("0.9.1.2981")]
-=======
-[assembly: AssemblyVersion("0.9.1.2985")]
->>>>>>> master
+[assembly: AssemblyVersion("0.9.1.2995")]
 
 
 // By default, the "Product version" shown in the file properties window is
@@ -68,8 +64,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-<<<<<<< HEAD
-[assembly: AssemblyFileVersion("0.9.1.2981")]
-=======
-[assembly: AssemblyFileVersion("0.9.1.2985")]
->>>>>>> master
+[assembly: AssemblyFileVersion("0.9.1.2995")]
