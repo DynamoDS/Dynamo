6a194b780c8cdb0321bc80be794d6d5e8444459d
Ian Keough <ian.keough@autodesk.com>
2015-11-05 10:30:48 -0800

    AssemblySharedBeetlejuice.

--- src/ShapewaysClient/Properties/AssemblyInfo.cs
+++ src/ShapewaysClient/Properties/AssemblyInfo.cs
@@ -35,5 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("DynamoCore")]
 [assembly: InternalsVisibleTo("DynamoCoreWpf")]
