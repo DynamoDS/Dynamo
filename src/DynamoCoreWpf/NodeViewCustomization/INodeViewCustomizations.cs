4b4f6c9d8b74ecfa6b2d9c20bebe5af890432c6d
Ian Keough <ian.keough@autodesk.com>
2015-11-09 13:32:39 -0800

    Add comment.

--- src/DynamoCoreWpf/NodeViewCustomization/INodeViewCustomizations.cs
+++ src/DynamoCoreWpf/NodeViewCustomization/INodeViewCustomizations.cs
@@ -7,6 +7,11 @@ namespace Dynamo.Wpf
 {
     public interface INodeViewCustomizations
     {
+        /// <summary>
+        /// Get a dictionary of collections of INodeViewCustomization types.
+        /// </summary>
+        /// <param name="logger">An ILogger used for logging exceptions when loading INodeViewCustomizations from assemblies.</param>
+        /// <returns>A dictionary of collections of INodeViewCustomization types.</returns>
         IDictionary<Type, IEnumerable<Type>> GetCustomizations(ILogger logger);
     }
 }
