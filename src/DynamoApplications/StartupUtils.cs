e80f1707f4a9ee513f255c9297ca26aeace1cbe1
Anna Osyatnik <anna.osyatnik@autodesk.com>
2015-11-19 14:48:09 +0200

    Remove greg dlls.

--- src/DynamoApplications/StartupUtils.cs
+++ src/DynamoApplications/StartupUtils.cs
@@ -9,7 +9,6 @@ using System.Reflection;
 using System.IO;
 using Dynamo.Models;
 using Dynamo.Updates;
-using Greg.AuthProviders;
 using Microsoft.Win32;
 using System.Diagnostics;
 using System.Threading;
@@ -188,8 +187,7 @@ namespace Dynamo.Applications
 
             config.UpdateManager = CLImode ? null : InitializeUpdateManager();
             config.StartInTestMode = CLImode ? true : false;
-            config.PathResolver = CLImode ? new CLIPathResolver(preloaderLocation) as IPathResolver : new SandboxPathResolver(preloaderLocation) as IPathResolver ;
-            config.AuthProvider = new OxygenProvider("https://accounts-staging.autodesk.com/");
+            config.PathResolver = CLImode ? new CLIPathResolver(preloaderLocation) as IPathResolver : new SandboxPathResolver(preloaderLocation) as IPathResolver ;            
 
             var model = DynamoModel.Start(config);
             return model;
