b095ae4eea2a3ca64b6d8e31b2a20f26b7deb60d
Yu Ke <yu.ke@autodesk.com>
2015-11-24 13:32:03 +0800

    Update debugger property

--- src/Engine/ProtoCore/ClassTable.cs
+++ src/Engine/ProtoCore/ClassTable.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 namespace ProtoCore.DSASM
 {
-    [System.Diagnostics.DebuggerDisplay("{Name}, classId = {ClassID}")]
+    [System.Diagnostics.DebuggerDisplay("{Name}, classId = {ID}")]
     public class ClassNode
     {
         public string Name { get; set; }
