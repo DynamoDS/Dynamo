e60db4b39ab1547f11bc3d74b37475f56b42c01f
Yu Ke <yu.ke@autodesk.com>
2015-11-30 11:02:24 +0800

    Add more test cases

--- test/Engine/ProtoTest/UtilsTests/ImperativeAstVistorTest.cs
+++ test/Engine/ProtoTest/UtilsTests/ImperativeAstVistorTest.cs
@@ -65,30 +65,189 @@ namespace ProtoTest.UtilsTests
             return imperativeCodeBlockNode;
         }
 
-        [Test]
-        public void TestReplaceIdentifierNode()
+        private void TestMapping(string originalCode, string expectedCode)
         {
-            var result = ParserUtils.Parse(@"
-[Imperative]
-{
-    y = foo + x;
-}");
-            var cbn = GetCodeBlockNode(result);
+            var originalResult = ParserUtils.Parse(originalCode);
+            var cbn = GetCodeBlockNode(originalResult);
             var mappedCBN = cbn.Accept(replacer) as CodeBlockNode;
             Assert.IsNotNull(mappedCBN);
             Assert.IsTrue(mappedCBN.Body.Any());
 
-            var expectedResult = ParserUtils.Parse(@"
-[Imperative]
-{
-    y = bar + x;
-}");
+            var expectedResult = ParserUtils.Parse(expectedCode);
             var expectedCBN = GetCodeBlockNode(expectedResult);
             Assert.IsNotNull(expectedCBN);
             Assert.IsTrue(expectedCBN.Body.Any());
 
-            Assert.IsTrue(expectedCBN.Equals(mappedCBN));
+            Assert.IsTrue(expectedCBN.Equals(mappedCBN), mappedCBN.ToString());
         }
 
+        [Test]
+        public void TestBinaryExpression()
+        {
+            TestMapping(
+@"
+[Imperative]
+{
+    y = foo + x;
+}",
+
+@"
+[Imperative]
+{
+    y = bar + x;
+}");
+        }
+
+        [Test]
+        public void TestReplaceArrayIndexing()
+        {
+            TestMapping(@"
+[Imperative]
+{
+    r = foo[a][b][c][d];
+}",
+@"
+[Imperative]
+{
+    r = bar[a1][b1][c1][d];
+}");
+        }
+
+        [Test]
+        public void TestExpressionList()
+        {
+            TestMapping(@"
+[Imperative]
+{
+    foo = {a, b, c, d, e};
+}",
+@"
+[Imperative]
+{
+    bar = {a1, b1, c1, d, e};
+}");
+        }
+
+        [Test]
+        public void TestFunctionParameter()
+        {
+            TestMapping(@"
+[Imperative]
+{
+    t = foo(a, b, c, d);
+}",
+@"
+[Imperative]
+{
+    t = bar(a1, b1, c1, d);
+}");
+        }
+
+        [Test]
+        public void TestInlineCondition()
+        {
+            TestMapping(@"
+[Imperative]
+{
+    foo = a ? b : c + d;
+}",
+@"
+[Imperative]
+{
+    bar = a1 ? b1 : c1 + d;
+}");
+        }
+
+        [Test]
+        public void TestRangeExpression()
+        {
+            TestMapping(@"
+[Imperative]
+{
+    foo = a..b..#c;
+}",
+@"
+[Imperative]
+{
+    bar = a1..b1..#c1;
+}");
+        }
+
+        [Test]
+        public void TestIfElse()
+        {
+            TestMapping(@"
+[Imperative]
+{
+    if (a)
+    {
+        foo = 10;
+    }
+    else if (b)
+    {
+        foo = 20;
+    }
+    else if (c)
+    {
+        foo = 30;
+    }
+}",
+@"
+[Imperative]
+{
+    if (a1)
+    {
+        bar = 10;
+    }
+    else if (b1)
+    {
+        bar = 20;
+    }
+    else if (c1)
+    {
+        bar = 30;
+    }
+}");
+        }
+
+        public void TestForLoop()
+        {
+            TestMapping(@"
+[Imperative]
+{
+    for (a in (0..100))
+    {
+        foo = foo + b;
+    }
+}",
+@"
+[Imperative]
+{
+    for (a1 in (0..100))
+    {
+        bar = bar + b1;
+    }
+}");
+        }
+
+        public void TestWhileLoop()
+        {
+            TestMapping(@"
+[Imperative]
+{
+    a = 1;
+    while (a < 100) {
+        foo = foo + a;
+    }
+}",
+@"
+[Imperative]
+{
+    a1 = 1;
+    while (a1 < 100) {
+        bar = bar + a1;
+    }
+}");
+        }
     }
 }
