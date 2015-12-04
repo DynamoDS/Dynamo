6c92ccccc763294be7b31feb0369d3f3b859b6d4
Yu Ke <yu.ke@autodesk.com>
2015-12-04 10:57:36 +0800

    Add OutputEditor

---
+++ src/DynamoCoreWpf/Views/Output/OutputEditor.cs
@@ -0,0 +1,44 @@
+﻿using Dynamo.Graph.Nodes.CustomNodes;
+using Dynamo.Controls;
+
+namespace Dynamo.UI.Controls
+{
+    /// <summary>
+    /// Auto-completion control for input parameter.
+    /// </summary>
+    public class OutputEditor : CodeCompletionEditor
+    {
+        private Output output;
+
+        /// <summary>
+        /// Create input editor by the view of symbol node.
+        /// </summary>
+        /// <param name="view"></param>
+        public OutputEditor(NodeView view) : base(view)
+        {
+            output = nodeViewModel.NodeModel as Output;
+        }
+
+        protected override void OnEscape()
+        {
+            var text = InnerTextEditor.Text;
+            if (output.Symbol != null && text.Equals(output.Symbol))
+            {
+                ReturnFocus();
+            }
+            else
+            {
+                InnerTextEditor.Text = output.Symbol;
+            }
+        }
+
+        protected override void OnCommitChange()
+        {
+            var lastInput = output.Symbol;
+            if (lastInput.Equals(InnerTextEditor.Text))
+                return;
+
+            UpdateNodeValue("Symbol");
+        }
+    }
+}
