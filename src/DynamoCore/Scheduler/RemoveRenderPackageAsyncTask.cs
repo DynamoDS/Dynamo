11dfca1b2f4318f9509fef5482e23fa3e0a80e1f
Yu Ke <yu.ke@autodesk.com>
2016-02-23 09:54:49 +0800

    Add RemoveRenderPackageAsyncTask

---
+++ src/DynamoCore/Scheduler/RemoveRenderPackageAsyncTask.cs
@@ -0,0 +1,29 @@
+﻿using Dynamo.Graph.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.Scheduler
+{
+    class RemoveRenderPackageAsyncTask: AsyncTask
+    {
+        public RemoveRenderPackageAsyncTask(IScheduler scheduler):base(scheduler)
+        {
+        }
+
+        public override TaskPriority Priority
+        {
+            get { return TaskPriority.Normal; }
+        }
+
+        protected override void HandleTaskCompletionCore()
+        {
+        }
+
+        protected override void HandleTaskExecutionCore()
+        {
+        }
+    }
+}
