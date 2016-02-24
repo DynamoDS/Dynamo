2a13a3286a6680f32deb1345d7ee20c59df1b65b
Yu Ke <yu.ke@autodesk.com>
2016-02-24 11:36:25 +0800

    Add test case

--- test/Engine/ProtoTest/TD/Associative/ReplicationGuide.cs
+++ test/Engine/ProtoTest/TD/Associative/ReplicationGuide.cs
@@ -3442,5 +3442,25 @@ r = foo(xs<1>, ys, zs);
             thisTest.RunScriptSource(code);
             thisTest.Verify("r", new object[] { "1-foo-bar", "2-foo-bar", "3-foo-bar" });
         }
+
+        [Test]
+        [Category("Replication")]
+        public void TestReplicationGuideTakesPriotry2()
+        {
+            // Verify it always replicates on the first parameter
+            string code = @"
+def foo(xs:var[], ys:var, zs:var)
+{
+    return = Sum(xs) + ys + zs;
+};
+xs = {1, 2, 3};
+ys = ""-foo"";
+zs = ""-bar"";
+r = foo(xs<1L>, ys<1L>, zs<1L>);
+            ";
+
+            thisTest.RunScriptSource(code);
+            thisTest.Verify("r", new object[] { "1-foo-bar", "2-foo-bar", "3-foo-bar" });
+        }
     }
 }
