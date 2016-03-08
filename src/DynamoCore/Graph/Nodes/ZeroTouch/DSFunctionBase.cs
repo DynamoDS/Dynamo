ca2c8d3cb66d1c0e1b2079a36edf9bc4257f24ec
Yu Ke <yu.ke@autodesk.com>
2016-03-08 14:04:15 +0800

    Get indexed identifier if function has multi return key

--- src/DynamoCore/Graph/Nodes/ZeroTouch/DSFunctionBase.cs
+++ src/DynamoCore/Graph/Nodes/ZeroTouch/DSFunctionBase.cs
@@ -40,11 +40,19 @@ namespace Dynamo.Graph.Nodes.ZeroTouch
 
         public override IdentifierNode GetAstIdentifierForOutputIndex(int outputIndex)
         {
-            return Controller.ReturnKeys != null && Controller.ReturnKeys.Any()
-                ? base.GetAstIdentifierForOutputIndex(outputIndex)
-                : (OutPortData.Count == 1
-                    ? AstIdentifierForPreview
-                    : base.GetAstIdentifierForOutputIndex(outputIndex));
+            if (Controller.ReturnKeys != null && Controller.ReturnKeys.Any())
+            {
+                string id = AstIdentifierBase + "_out" + outputIndex;
+                return AstFactory.BuildIdentifier(id);
+            }
+            else if (OutPortData.Count == 1)
+            {
+                return AstIdentifierForPreview;
+            }
+            else
+            {
+                return base.GetAstIdentifierForOutputIndex(outputIndex); 
+            }
         }
 
         /// <summary>
