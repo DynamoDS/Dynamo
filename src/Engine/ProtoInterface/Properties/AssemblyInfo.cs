8d4082513e7f970a9dc521ed330b06e66e051b4e
Sharad <sharad.jaiswal@autodesk.com>
2016-03-15 11:15:29 +0800

    ProtoInterface library cleanup

---
+++ src/Engine/ProtoInterface/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProtoInterface")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("D60CD8AA-3BBF-424A-8F6C-5A4757351F24")]
