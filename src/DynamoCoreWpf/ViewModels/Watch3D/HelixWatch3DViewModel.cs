596a7b03810c34fe0f715775838f2cc2ad2c1d16
Michael Kirschner <michaelkirschner@Michaels-iMac.local>
2016-03-19 23:28:48 -0400

    remove errant dispatcher call

--- src/DynamoCoreWpf/ViewModels/Watch3D/HelixWatch3DViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Watch3D/HelixWatch3DViewModel.cs
@@ -1664,8 +1664,7 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
         private DynamoGeometryModel3D CreateDynamoGeometryModel3D(HelixRenderPackage rp)
         {
             DynamoGeometryModel3D meshGeometry3D = null;
-            Dispatcher.CurrentDispatcher.Invoke(() =>
-            {
+           
                 meshGeometry3D = new DynamoGeometryModel3D(renderTechnique)
                 {
                     Transform = new MatrixTransform3D(rp.Transform),
@@ -1674,10 +1673,7 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
                     RequiresPerVertexColoration = rp.RequiresPerVertexColoration,
                     IsSelected = rp.IsSelected
                 };
-            }
-            );
-           
-
+            
             if (rp.Colors != null)
             {
                 var pf = PixelFormats.Bgra32;
