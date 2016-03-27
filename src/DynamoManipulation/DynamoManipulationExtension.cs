96c80b4730f82fc4588638a1b30317c2881b21d9
aparajit-pratap <aparajit.pratap@autodesk.com>
2016-03-27 22:30:12 +0800

    do not create manipulators for nodes in error state

--- src/DynamoManipulation/DynamoManipulationExtension.cs
+++ src/DynamoManipulation/DynamoManipulationExtension.cs
@@ -305,8 +305,8 @@ namespace Dynamo.Manipulation
                 node.PropertyChanged += OnNodePropertyChanged;
             }
             
-            //No manipulator for frozen node
-            if (node.IsFrozen) return false;
+            //No manipulator for frozen node or node in state of error
+            if (node.IsFrozen || node.IsInErrorState) return false;
 
             //If the node already has a manipulator, then skip creating new one.
             if (manipulatorDaemon.HasNodeManipulator(node)) return false;
