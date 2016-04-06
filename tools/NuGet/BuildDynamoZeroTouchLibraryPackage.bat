d4c287060ab1cf67c4e26103c65a701b29abed2e
rk_lakshmanan <rk_lakshmanan@hotmail.com>
2016-04-06 14:50:42 +0800

    modified ZeroTouch NuGet to include DynamoServices and exclude ProtoInterface

--- tools/NuGet/BuildDynamoZeroTouchLibraryPackage.bat
+++ tools/NuGet/BuildDynamoZeroTouchLibraryPackage.bat
@@ -16,10 +16,4 @@ mkdir .\%zt%\tools
 copy %base%\ProtoGeometry.dll .\%zt%\lib\%framework%\ProtoGeometry.dll
 copy %base%\ProtoGeometry.xml .\%zt%\build\%framework%\ProtoGeometry.xml
 
-copy %base%\ProtoInterface.dll .\%zt%\lib\%framework%\ProtoInterface.dll
-copy %base%\ProtoInterface.xml .\%zt%\build\%framework%\ProtoInterface.xml
-
-copy %base%\DynamoServices.dll .\%zt%\lib\%framework%\DynamoServices.dll
-copy %base%\DynamoServices.xml .\%zt%\build\%framework%\DynamoServices.xml
-
 nuget pack .\%zt%\ZeroTouchLibrary.nuspec
