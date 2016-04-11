db3f70a4f0eca00a9723ce6909f78ad35275bbcf
Shannon <sh4nnongoh@gmail.com>
2016-04-11 14:34:41 +0800

    Removed Redundant Code.

--- src/DynamoSandbox/DynamoCoreSetup.cs
+++ src/DynamoSandbox/DynamoCoreSetup.cs
@@ -10,7 +10,6 @@ using Dynamo.Interfaces;
 using Dynamo.Models;
 using Dynamo.ViewModels;
 using DynamoShapeManager;
-//using Greg.AuthProviders;
 using Microsoft.Win32;
 using Dynamo.Configuration;
 using Dynamo.Updates;
@@ -18,7 +17,6 @@ using Dynamo.Logging;
 using Dynamo.Core;
 using Dynamo.Wpf.ViewModels.Watch3D;
 using Dynamo.DynamoSandbox;
-using Dynamo.Applications;
 
 namespace DynamoSandbox
 {
@@ -83,118 +81,12 @@ namespace DynamoSandbox
             get { return string.Empty; }
         }
     }
-    /*
-    class DynamoProPathResolver : IPathResolver
-    {
-        private readonly HashSet<string> additionalNodeDirectories;
-        private readonly HashSet<string> additionalResolutionPaths;
-        private readonly HashSet<string> preloadedLibraryPaths;
-        private readonly string userDataRootFolder;
-        private readonly string commonDataRootFolder;
-
-        internal DynamoProPathResolver(string userDataFolder, string commonDataFolder)
-        {
-            // The executing assembly is DynamoStudio.exe, locate Dynamo Studio folder
-            var currentAssemblyPath = Assembly.GetExecutingAssembly().Location;
-            var currentAssemblyDir = Path.GetDirectoryName(currentAssemblyPath);
-
-            var nodesDirectory = Path.Combine(currentAssemblyDir, "nodes");
-           // var translationDll = Path.Combine(currentAssemblyDir, "Translation.dll");
-
-            // Add an additional node processing folder
-            additionalNodeDirectories = new HashSet<string>();
-
-            if (Directory.Exists(nodesDirectory))
-            {
-                additionalNodeDirectories.Add(nodesDirectory);
-            }
-
-            // Add the DynamoStudio folder for assembly resolution
-            additionalResolutionPaths = new HashSet<string> { currentAssemblyDir };
-
-            preloadedLibraryPaths = new HashSet<string>
-            {
-                "VMDataBridge.dll",
-                "ProtoGeometry.dll",
-                "DSCoreNodes.dll",
-                "DSOffice.dll",
-                "DSIronPython.dll",
-                "FunctionObject.ds",
-                "Optimize.ds",
-                "DynamoConversions.dll",
-                "DynamoUnits.dll",
-                "Tessellation.dll",
-                "Analysis.dll",
-                "Display.dll",
-             //   translationDll
-            };
-            this.userDataRootFolder = userDataFolder;
-            this.commonDataRootFolder = commonDataFolder;
-        }
-
-        internal void AddAdditionalResolutionPath(string directory)
-        {
-            if (!additionalResolutionPaths.Contains(directory))
-                additionalResolutionPaths.Add(directory);
-        }
-
-        public IEnumerable<string> AdditionalNodeDirectories
-        {
-            get { return additionalNodeDirectories; }
-        }
-
-        public IEnumerable<string> AdditionalResolutionPaths
-        {
-            get { return additionalResolutionPaths; }
-        }
-
-        public IEnumerable<string> PreloadedLibraryPaths
-        {
-            get { return preloadedLibraryPaths; }
-        }
-
-        public string UserDataRootFolder
-        {
-            get { return userDataRootFolder; }
-        }
-
-        public string CommonDataRootFolder
-        {
-            get { return commonDataRootFolder; }
-        }
-    }
-    
-#if DYNAMO_LICENSING
-    internal class DynamoProLicensing
-    {
-        private static ClwLib licensing;
-
-        private DynamoProLicensing() { }
-
-        public static ClwLib Licensing
-        {
-            get 
-            { 
-                if (licensing == null)
-                {
-                    licensing = new ClwLib();
-                }
-                return licensing;
-            }
-        }
-    }
-#endif
-    */
 
     class DynamoCoreSetup
     {
         private SandboxPathResolver pathResolver;
-        //private DynamoProPathResolver pathResolver;
         private SettingsMigrationWindow migrationWindow;
         private DynamoViewModel viewModel = null;
-        private const string DownloadSourcePath = "http://dyn-studio-data.s3.amazonaws.com/";
-        private const string SignatureSourcePath = "http://dyn-studio-data-sig.s3.amazonaws.com/";
-        private const string InstallerNameBase = "DynamoStudio";
         private CommandLineArguments argumentSettings;
 
         public DynamoCoreSetup(CommandLineArguments args)
@@ -215,75 +107,31 @@ namespace DynamoSandbox
 
                 DynamoModel.RequestMigrationStatusDialog += MigrationStatusDialogRequested;
 
-                /*
-                // TODO: Update DownloadLink and TeamEmail when they come
-                Configurations.DynamoSiteLink = "http://dynamobim.org/design/";
-                Configurations.DynamoWikiLink = "http://dynamobim.org/design/source/git/wiki/";
-                Configurations.DynamoBimForum = "http://dynamobim.org/design/forum/";
-                Configurations.DynamoTeamEmail = "";
-                Configurations.DynamoVideoTutorials = "http://dynamobim.org/design/learn/video/";
-                Configurations.DynamoPrimer = "http://dynamoprimer.com/";
-                Configurations.DynamoDownloadLink = "";
-                Configurations.GitHubDynamoLink = "http://dynamobim.org/design/source/git/";
-                Configurations.GitHubBugReportingLink = "http://dynamobim.org/design/source/git/issues/";
-                */
-                //var model = Dynamo.Applications.StartupUtils.MakeModel(false);
-         
                 var model = DynamoModel.Start(
-                        new DynamoModel.DefaultStartConfiguration()
-                        {
-                            PathResolver = pathResolver,
-                            DynamoCorePath = Program.DynamoCorePath,
-                            DynamoHostPath = Path.GetDirectoryName(exePath),
-                            GeometryFactoryPath = geometryFactoryPath,
-                            UpdateManager = InitializeUpdateManager(),
-                            //AuthProvider = authProvider,
-                            ProcessMode = Dynamo.Scheduler.TaskProcessMode.Asynchronous
-                        });
+                    new DynamoModel.DefaultStartConfiguration()
+                    {
+                        PathResolver = pathResolver,
+                        DynamoCorePath = Program.DynamoCorePath,
+                        DynamoHostPath = Path.GetDirectoryName(exePath),
+                        GeometryFactoryPath = geometryFactoryPath,
+                        UpdateManager = InitializeUpdateManager(),
+                        ProcessMode = Dynamo.Scheduler.TaskProcessMode.Asynchronous
+                    });
 
-                var startconfig = new DynamoViewModel.StartConfiguration
-                {
-                    CommandFilePath = argumentSettings.CommandFilePath,
-                    DynamoModel = model,
-                    Watch3DViewModel = HelixWatch3DViewModel.TryCreateHelixWatch3DViewModel(new Watch3DViewModelStartupParams(model), model.Logger),
-                    ShowLogin = true
-                };
+                viewModel = DynamoViewModel.Start(
+                    new DynamoViewModel.StartConfiguration()
+                    {
+                        CommandFilePath = argumentSettings.CommandFilePath,
+                        DynamoModel = model,
+                        Watch3DViewModel = HelixWatch3DViewModel.TryCreateHelixWatch3DViewModel(new Watch3DViewModelStartupParams(model), model.Logger),
+                        ShowLogin = true
+                    });
 
-                viewModel = DynamoViewModel.Start(startconfig);
-                //viewModel = DynamoViewModel.Start();
-
-                //var startUpFilePath = argumentSettings.StartUpDynFilePath;
                 var view = new DynamoView(viewModel);
                 view.Loaded += (sender, args) => CloseMigrationWindow();
-                /*
-                view.Loaded += (sender, a) =>
-                {
-                    CloseMigrationWindow();
-                    view.Dispatcher.BeginInvoke(new Action(() =>
-                    {
-                        // If there is a start-up file path, open it first.
-                        if (!string.IsNullOrEmpty(startUpFilePath))
-                            viewModel.OpenCommand.Execute(startUpFilePath);
-                    }));
-                };
-                */
-#if (DYNAMO_LICENSING)
 
-                var rootFolder = Path.GetDirectoryName(exePath);
-
-                DynamoProLicensing.Licensing.Initialize("en-US", SaveWorkspaceAlertAndClose);
-
-                if (DynamoProLicensing.Licensing.CheckOut())
-                {
-                    app.Run(view);
-
-                    DynamoProLicensing.Licensing.CheckIn();
-                    DynamoProLicensing.Licensing.CleanUp();
-                }
-#else
-                //var app = new Application();
                 app.Run(view);
-#endif
+
                 DynamoModel.RequestMigrationStatusDialog -= MigrationStatusDialogRequested;
                
             }
@@ -330,23 +178,6 @@ namespace DynamoSandbox
             Debug.Assert(cfg.DynamoLookUp != null);
             return um;
         }
-       
-        /*
-        private UpdateManagerConfiguration GetUpdateManagerConfiguration()
-        {
-
-            var config = UpdateManagerConfiguration.GetSettings(new SandboxLookUp());
-            if (string.IsNullOrEmpty(config.ConfigFilePath))
-            {
-                config.DownloadSourcePath = DownloadSourcePath;
-                config.SignatureSourcePath = SignatureSourcePath;
-                config.InstallerNameBase = InstallerNameBase;
-            };
-
-            Debug.Assert(config.DynamoLookUp != null);
-            return config;
-        }
-        */
 
         private void CloseMigrationWindow()
         {
@@ -372,41 +203,6 @@ namespace DynamoSandbox
         
         private void PreloadShapeManager(ref string geometryFactoryPath, ref string preloaderLocation)
         {
-            /*
-            var exePath = Assembly.GetExecutingAssembly().Location;
-            var rootFolder = Path.GetDirectoryName(exePath);
-
-            var versions = new[]
-            {
-                LibraryVersion.Version220,
-                LibraryVersion.Version221,
-                LibraryVersion.Version222,
-            };
-
-            var preloader = new Preloader(Program.DynamoCorePath, versions);
-            preloader.Preload();
-            geometryFactoryPath = preloader.GeometryFactoryPath;
-            */
-            //preloaderLocation = preloader.PreloaderLocation;
-            /*
-            var exePath = Assembly.GetExecutingAssembly().Location;
-            var rootFolder = Path.GetDirectoryName(exePath);
-
-            const LibraryVersion version = LibraryVersion.Version222;
-
-            var binaryFileName = string.Format("ASMAHL{0}A.dll", ((int)version));
-
-            var shapeManagerPath = Path.Combine(rootFolder,
-                string.Format("libg_{0}", ((int)version)));
-
-            if (!File.Exists(Path.Combine(shapeManagerPath, binaryFileName)))
-            {
-                // First look under 'libg_xxx' folder for ASM binaries. If not 
-                // found, try looking for them directly under 'rootFolder'.
-                // 
-                shapeManagerPath = rootFolder;
-            }
-            */
             var versions = new[]
             {
                 LibraryVersion.Version220,
@@ -419,19 +215,6 @@ namespace DynamoSandbox
             preloader.Preload();
             geometryFactoryPath = preloader.GeometryFactoryPath;
             preloaderLocation = preloader.PreloaderLocation;
-
-            /*
-            var userDataFolder = string.Empty; //Let's use default location location from Dynamo Core
-            var commonDataFolder = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-                "Dynamo", "Dynamo Revit");
-            */
-            
-
-            //pathResolver = new DynamoProPathResolver(userDataFolder, commonDataFolder);
-
-            //pathResolver.AddAdditionalResolutionPath(preloader.PreloaderLocation);
-            
         }
         
     }
@@ -450,21 +233,4 @@ namespace DynamoSandbox
                 (s) => regKey.OpenSubKey(s).GetValue("InstallLocation") as string);
         }
     }
-    /*
-    internal class StudioLookUp : DynamoLookUp
-    {
-        public override IEnumerable<string> GetDynamoInstallLocations()
-        {
-            const string regKey64 = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\";
-            //Open HKLM for 64bit registry
-            var regKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
-            //Open Windows/CurrentVersion/Uninstall registry key
-            regKey = regKey.OpenSubKey(regKey64);
-
-            //Get "InstallLocation" value as string for all the subkey that starts with "Dynamo"
-            return regKey.GetSubKeyNames().Where(s => s.StartsWith("Autodesk Dynamo Studio")).Select(
-                (s) => regKey.OpenSubKey(s).GetValue("InstallLocation") as string);
-        }
-    }
-    */
 }
--- src/DynamoSandbox/Program.cs
+++ src/DynamoSandbox/Program.cs
@@ -91,38 +91,28 @@ namespace DynamoSandbox
         [STAThread]
         static void Main(string[] args)
         {
-            /*
-            var splashWindow = new SplashWindow();
-            splashWindow.Show();
-            */
+
             AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
             
             //Include Dynamo Core path in System Path variable for helix to load properly.
             UpdateSystemPathForProcess();
-            /*
-            var cmdLineArgs = StartupUtils.CommandLineArguments.Parse(args);
-            var locale = StartupUtils.SetLocale(cmdLineArgs);
-            _putenv(locale);
-            */
+
             var cmdLineArgs = CommandLineArguments.FromArguments(args);
 
             var setup = new DynamoCoreSetup(cmdLineArgs);
             var app = new Application();
             setup.RunApplication(app);
 
-
         }
 
         /// <summary>
-        /// Finds the Dynamo Core path by looking into registery or potentially a config file.
+        /// Finds the Dynamo Core path by looking into registery.
         /// </summary>
         /// <returns>The root folder path of Dynamo Core.</returns>
         private static string GetDynamoCorePath()
         {
             var assembly = Assembly.GetExecutingAssembly();
             var version = assembly.GetName().Version;
-            var dynamoRevitRootDirectory = Path.GetDirectoryName(assembly.Location);
-            var dynamoRoot = GetDynamoRoot(dynamoRevitRootDirectory);
 
             var installs = DynamoInstallDetective.DynamoProducts.FindDynamoInstallations();
             if (installs == null) return string.Empty;
@@ -131,21 +121,6 @@ namespace DynamoSandbox
                 .Where(p => p.VersionInfo.Item1 == version.Major && p.VersionInfo.Item2 == version.Minor)
                 .Select(p => p.InstallLocation)
                 .FirstOrDefault();
-        }
-
-        /// <summary>
-        /// Returns Dynamo Core path from the given Dynamo Studio for debugging 
-        /// purpose.
-        /// </summary>
-        /// <param name="dynamoStudioRootDirectory"></param>
-        /// <returns></returns>
-        private static string GetDynamoRoot(string dynamoRevitRootDirectory)
-        {
-            //TODO: use config file to setup Dynamo Path for debug builds.
-
-            //For now just replace DynamoPro with Dynamo, assuming Dynamo Studio
-            //is built at DynamoPro\bin.
-            return dynamoRevitRootDirectory.Replace("Dynamo Revit", "Dynamo");
         }
 
         /// <summary>
@@ -196,14 +171,5 @@ namespace DynamoSandbox
             Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.Process);
         }
     }
-    /*
-    class DynamoProApp : Application
-    {
-        public DynamoProApp(SplashWindow splash)
-        {
-            this.Startup += (sender, args) => splash.Close();
-        }
 
-    }
-    */
 }
