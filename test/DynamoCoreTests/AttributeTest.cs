9620281692cb6c66371fb190d74890ebd3c27b96
Yu Ke <yu.ke@autodesk.com>
2016-04-20 10:44:22 +0800

    Update test case TestKeepReferenceThisAttribute

--- test/DynamoCoreTests/AttributeTest.cs
+++ test/DynamoCoreTests/AttributeTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using Dynamo.Graph.Connectors;
 using System.IO;
+using Dynamo.Graph.Nodes.ZeroTouch;
 
 namespace Dynamo.Tests
 {
@@ -18,10 +19,14 @@ namespace Dynamo.Tests
         }
 
         [Test]
-        public void TestAttributeKeepReferenceThis()
+        public void TestKeepReferenceThisAttribute()
         {
             var dynFilePath = Path.Combine(TestDirectory, @"core\dsevaluation\referenceThis.dyn");
             OpenModel(dynFilePath);
+            var node1 = CurrentDynamoModel.CurrentWorkspace.Nodes.OfType<Graph.Nodes.CustomNodes.Function>().FirstOrDefault();
+            var node2 = CurrentDynamoModel.CurrentWorkspace.Nodes.OfType<DSFunction>().FirstOrDefault();
+            ConnectorModel.Make(node1, node2, 0, 0);
+            RunCurrentModel();
             AssertPreviewValue("763c4e98-dbe0-4bb7-b00f-69e5b79b09b0", new object[] { false, false });
         }
     }
