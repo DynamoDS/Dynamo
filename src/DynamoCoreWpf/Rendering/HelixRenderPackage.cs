d6429dc3e19b473f78aaaeed07abf0eb45eb221d
Michael Kirschner <michaelkirschner@Michaels-iMac.local>
2016-04-27 15:17:00 -0400

    fix bug

--- src/DynamoCoreWpf/Rendering/HelixRenderPackage.cs
+++ src/DynamoCoreWpf/Rendering/HelixRenderPackage.cs
@@ -671,7 +671,7 @@ namespace Dynamo.Wpf.Rendering
             return new double[] {mat.M11,mat.M12,mat.M13,mat.M14,
                 mat.M21,mat.M22,mat.M23,mat.M24,
                 mat.M31,mat.M32,mat.M33,mat.M34,
-                mat.OffsetX,mat.OffsetY,mat.OffsetZ,mat.M14,
+                mat.OffsetX,mat.OffsetY,mat.OffsetZ,mat.M44,
             };
 
         }
