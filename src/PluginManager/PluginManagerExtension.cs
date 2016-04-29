f2bd383b1c2fb56d998805c41d05e7419f29ec1a
rk_lakshmanan <rk_lakshmanan@hotmail.com>
2016-04-29 18:34:59 +0800

    initial linking of PluginManager to Dynamo

--- src/PluginManager/PluginManagerExtension.cs
+++ src/PluginManager/PluginManagerExtension.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
-namespace PluginManager
+namespace Dynamo.PluginManager
 {
     public class PluginManagerExtension : IViewExtension, ILogSource
     {
