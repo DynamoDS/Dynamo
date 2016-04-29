73ccbd3f72dbd3679826c1ecbdbb886c19fccae7
rk_lakshmanan <rk_lakshmanan@hotmail.com>
2016-04-29 17:38:12 +0800

    initial comm of plugin manager

---
+++ src/PluginManager/PluginManagerExtension.cs
@@ -0,0 +1,95 @@
+﻿using Dynamo.Graph.Workspaces;
+using Dynamo.Logging;
+using Dynamo.Wpf.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PluginManager
+{
+    public class PluginManagerExtension : IViewExtension, ILogSource
+    {
+        private ViewStartupParams startupParams;
+        private ViewLoadedParams loadedParams;
+
+        private Menu dynamoMenu;
+        private MenuItem loadPythonScriptMenuItem;
+        private Separator separator = new Separator();
+
+        #region IViewExtension implementation
+
+
+        public string Name { get { return "DynamoPluginManager"; } }
+
+        public string UniqueId
+        {
+            get { return "7c936911-bd25-4ae5-bc78-5b96e86bb30d"; }
+        }
+
+        public void Startup(ViewStartupParams p)
+        {
+            this.startupParams = p;
+        }
+
+        public void Loaded(ViewLoadedParams p)
+        {
+            this.loadedParams = p;
+
+            dynamoMenu = p.dynamoMenu;
+            loadPythonScriptMenuItem = GenerateMenuItem();
+            p.AddMenuItem(MenuBarType.File, loadPythonScriptMenuItem, 11);
+
+            p.CurrentWorkspaceChanged += CurrentWorkspaceChanged;
+
+        }
+
+        private void CurrentWorkspaceChanged(IWorkspaceModel ws)
+        {
+
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            loadedParams.CurrentWorkspaceChanged -= CurrentWorkspaceChanged;
+
+         //   ClearMenuItem(extensionMenuItem, inviteMenuItem, manageCustomizersMenuItem, separator);
+        }
+        #endregion
+
+        #region ILogSource implementation
+        public event Action<ILogMessage> MessageLogged;
+
+        private void OnMessageLogged(ILogMessage msg)
+        {
+            if (this.MessageLogged != null)
+            {
+                this.MessageLogged(msg);
+            }
+        }
+
+        #endregion
+        #region Helper methods
+        private MenuItem GenerateMenuItem()
+        {
+            MenuItem item = new MenuItem();
+            item.Header = "Load Python Script";
+           
+
+           // item.Click += { ImportPythonScript};
+           return item;
+        }
+
+        #endregion
+
+
+    }
+}
