e62ca5c92a89c513db3df128a90105e9193e6ad6
laksh <rkplakshmanan@gmail.com>
2016-05-11 01:52:57 +0800

    added promp class and removed accidentally change to DynamoView

---
+++ src/PluginManager/TextBoxPromptDialog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PluginManager
+{
+   public static class TextBoxPromptDialog
+    {
+       public static string ShowDialog(string text, string caption)
+       {
+           Form prompt = new Form()
+           {
+               Width = 500,
+               Height = 150,
+               FormBorderStyle = FormBorderStyle.FixedDialog,
+               Text = caption,
+               StartPosition = FormStartPosition.CenterScreen
+           };
+           Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+           TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+           Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+           confirmation.Click += (sender, e) => { prompt.Close(); };
+           prompt.Controls.Add(textBox);
+           prompt.Controls.Add(confirmation);
+           prompt.Controls.Add(textLabel);
+           prompt.AcceptButton = confirmation;
+
+           return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+       }
+    }
+}
