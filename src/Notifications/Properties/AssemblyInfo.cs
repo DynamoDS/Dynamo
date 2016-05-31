d3af365b26484e338e05ec13ef3e2630125e7dc8
Michael Kirschner <mjk.kirschner@gmail.com>
2016-05-31 17:00:00 -0400

    move project to notifications folder

--- src/Notifications/Properties/AssemblyInfo.cs
+++ src/Notifications/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PrettyExceptionExtension")]
+[assembly: AssemblyTitle("Notifications")]
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
