de234812863fe05181f194910defc9f0c8609318
Yu Ke <yu.ke@autodesk.com>
2016-06-09 16:52:43 +0800

    Add Reorder() test case

--- test/Engine/ProtoTest/Associative/BuiltinMethodsTest.cs
+++ test/Engine/ProtoTest/Associative/BuiltinMethodsTest.cs
@@ -411,6 +411,19 @@ r = c[2];
         }
 
         [Test]
+        public void Reorder2()
+        {
+            String code =
+@"
+x = {1, 2, 3};
+y = {-1, -2, -3};
+z = Reorder(x,y);
+";
+            thisTest.RunScriptSource(code);
+            thisTest.Verify("z", new[] { 3, 2, 1 });
+        }
+
+        [Test]
         //Test "IsUniformDepth"
         public void BIM19_IsUniformDepth()
         {
