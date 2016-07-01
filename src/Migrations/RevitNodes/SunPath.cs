d561d956f5ee1e0a2e5856c29d96ec8a2d558e62
rk_lakshmanan <rk_lakshmanan@hotmail.com>
2016-07-01 19:13:07 +0800

    remove references and revert SunPath.cs

---
+++ src/Migrations/RevitNodes/SunPath.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using Dynamo.Graph;
+using Dynamo.Graph.Nodes;
+using Dynamo.Models;
+using Dynamo.Migration;
+
+namespace Dynamo.Nodes
+{
+    public class SunPathDirection : MigrationNode
+    {
+        [NodeMigration(from: "0.7.0.0", to: "0.7.3.0")]
+        public static NodeMigrationData Migrate_0700_to_0730(NodeMigrationData data)
+        {
+            var migrationData = new NodeMigrationData(data.Document);
+            var oldNode = data.MigratedNodes.ElementAt(0);
+
+            var sunSettingNode = MigrationManager.CloneAndChangeName(
+                oldNode,
+                "DSRevitNodesUI.SunSettings",
+                "SunSettings.Current");
+            var sunSettingsNodeId = Guid.NewGuid().ToString();
+            sunSettingNode.SetAttribute("guid", sunSettingsNodeId);
+            migrationData.AppendNode(sunSettingNode);
+
+            var sunPathNode = MigrationManager.CreateFunctionNodeFrom(oldNode);
+            MigrationManager.SetFunctionSignature(
+                sunPathNode,
+                "RevitNodes.dll",
+                "SunSettings.SunDirection",
+                "SunSettings.SunDirection@var");
+            migrationData.AppendNode(sunPathNode);
+
+            migrationData.CreateConnector(sunSettingNode, 0, sunPathNode, 0);
+
+            var oldConnector = data.FindFirstConnector(
+                new PortId(MigrationManager.GetGuidFromXmlElement(oldNode), 0, PortType.Output));
+
+            if (oldConnector != null)
+            {
+                migrationData.ReconnectToPort(
+                    oldConnector,
+                    new PortId(
+                        MigrationManager.GetGuidFromXmlElement(sunPathNode),
+                        0,
+                        PortType.Output));
+            }
+
+            return migrationData;
+        }
+    }
+}
+
+namespace DSRevitNodesUI
+{
+    public class SunPathDirection : MigrationNode
+    {
+        [NodeMigration(from: "0.7.0.0", to: "0.7.3.0")]
+        public static NodeMigrationData Migrate_0700_to_0730(NodeMigrationData data)
+        {
+            return Dynamo.Nodes.SunPathDirection.Migrate_0700_to_0730(data);
+        }
+    }
+}
