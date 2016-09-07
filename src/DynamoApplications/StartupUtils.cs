12b29509571e6a522ff4910d87db5e68becdd1f6
QilongTang <173288704@qq.com>
2016-09-07 18:35:27 -0400

    Clean the usings

--- src/DynamoApplications/StartupUtils.cs
+++ src/DynamoApplications/StartupUtils.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Dynamo.Interfaces;
-using Dynamo.Scheduler;
-using DynamoShapeManager;
-using System.Reflection;
-using System.IO;
-using Dynamo.Models;
-using Dynamo.Updates;
-using Microsoft.Win32;
 using System.Diagnostics;
-using System.Threading;
 using System.Globalization;
-using NDesk.Options;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using Dynamo.Interfaces;
+using Dynamo.Models;
+using Dynamo.Scheduler;
+using Dynamo.Updates;
 using DynamoApplications.Properties;
+using DynamoShapeManager;
+using Microsoft.Win32;
+using NDesk.Options;
 
 namespace Dynamo.Applications
 {
