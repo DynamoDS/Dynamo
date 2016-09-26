10c66f6845d593220f0ec2380ceb02a62f6a1559
aparajit-pratap <aparajit.pratap@autodesk.com>
2016-09-26 10:05:36 +0800

    Updated failing test case after fix to CBN autocompletion behaviour (#7198)
    * MAGN-10642: InvalidOperationException is thrown from AvalonEdit (#7189)
    
    * Enable/disable t-spline nodes only on relaunch of Dynamo (#6915)
    
    * Track Startup Analytics (#6866)
    
    * LibG Binaries Update (#6868)
    
    * LibG/ASM220,a7ca002
    
    * LibG/ASM221,7bcf505
    
    * LibG/ASM222,e20aa15
    
    * Fix in deserialization of trace data for element binding regressions (#6882)
    
    * fix in deserialization of trace data for element binding
    
    * reverting assemblysharedinfo.cs
    
    * enable/disable t-spline nodes only on relaunch of Dynamo
    
    * Revert "Enable/disable t-spline nodes only on relaunch of Dynamo (#6915)"
    
    This reverts commit b2ea255bf22920ad45c382a823f18e34650f46dd.
    
    * filter empty classnodes from autocompletion and syntax highlighting
    
    * updated failing test case after fix to CBN autocompletion behaviour

--- test/DynamoCoreTests/CodeBlockNodeTests.cs
+++ test/DynamoCoreTests/CodeBlockNodeTests.cs
@@ -1461,9 +1461,9 @@ var06 = g;
             var completions = codeCompletionServices.SearchCompletions(code, Guid.Empty);
 
             // Expected 5 completion items
-            Assert.AreEqual(5, completions.Count());
+            Assert.AreEqual(4, completions.Count());
 
-            string[] expected = { "Decimal", "Imperative", "ImportFromCSV", "Minimal", "MinimalTracedClass" };
+            string[] expected = { "Imperative", "ImportFromCSV", "Minimal", "MinimalTracedClass" };
             var actual = completions.Select(x => x.Text).OrderBy(x => x);
 
             Assert.AreEqual(expected, actual);
