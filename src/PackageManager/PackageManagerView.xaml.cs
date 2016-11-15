38f5170d683f2fad58e5304abbe06ecf60a662b0
Jitendra Nevase <jit_tat@yahoo.com>
2016-11-15 16:51:27 +0530

    Dynamo CEF initial setup

---
+++ src/PackageManager/PackageManagerView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PackageManager
+{
+    /// <summary>
+    /// Interaction logic for PackageManagerView.xaml
+    /// </summary>
+    public partial class PackageManagerView : Window
+    {
+        public PackageManagerView()
+        {
+            InitializeComponent();
+            this.Height = (System.Windows.SystemParameters.PrimaryScreenHeight * 0.95);
+            this.Width = (System.Windows.SystemParameters.PrimaryScreenWidth * 0.75);
+        }
+    }
+}
---
+++ src/PackageManager/Utilities/PackageManagerCefHelper.cs
@@ -0,0 +1,23 @@
+﻿using CefSharp;
+using Dynamo.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using static Dynamo.PackageManager.PackageDownloadHandle;
+
+namespace Dynamo.PackageManager.Utilities
+{
+    internal class PackageManagerCefHelper : CefHelper
+    {
+
+        public PackageManagerCefHelper(DynamoViewModel dynamoViewModel, PackageLoader model, PackageManagerViewModel pkgManagerViewModel) : base(dynamoViewModel, model, pkgManagerViewModel)
+        {
+        }
+
+        
+    }
+}
