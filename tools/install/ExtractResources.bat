38a5101f8513b9a6abdce4fd56be16dd993d911a
monikaprabhu <monika.prabhu@autodesk.com>
2016-12-08 11:37:29 +0800

    Split the extractresources file for Localization into two parts
    1) Script required for Localization from DynamoCore
    2) Script required for Localization from DynamoRevit
    
    This file will contain only the script for DynamoCore

--- tools/install/ExtractResources.bat
+++ tools/install/ExtractResources.bat
@@ -21,6 +21,11 @@ echo .
 rd /s/q %wwlroot%
 mkdir %wwlroot%
 
+rem Sanitize xml file
+SET PATH=%PATH%;%cwd%\..\..\src\Tools\XmlDocumentationsUtility\bin\%OPT_CONFIGURATION%
+echo %cwd%
+XmlDocumentationsUtility.exe %cwd%\..\..\bin\%OPT_Platform%\%OPT_CONFIGURATION%\
+
 rem Copy localizable resources
 robocopy "%binroot%\en-US"                    "%wwlroot%\en-US"                     *.resources.dll *.xml
 robocopy "%binroot%\libg_locale\en_US"        "%wwlroot%\libg_locale\en_US"         /e
@@ -33,26 +38,6 @@ rem required for translation tool to even open up localizable resource assemblie
 robocopy "%binroot%"                          "%wwlroot%"                           *.dll *.exe -XF *tests.dll *.customization.dll 
 robocopy "%binroot%\nodes"                    "%wwlroot%\nodes"                     *.dll
 
-IF EXIST "%binroot%\Revit_2016" (
-robocopy "%binroot%\Revit_2016"               "%wwlroot%\Revit_2016"                *.dll *.exe -XF *tests.dll *.customization.dll 
-robocopy "%binroot%\Revit_2016\en-US"         "%wwlroot%\Revit_2016\en-US"          *.resources.dll *.xml
-robocopy "%binroot%\Revit_2016\nodes"         "%wwlroot%\Revit_2016\nodes"          *.dll
-robocopy "%binroot%\Revit_2016\nodes\en-US"   "%wwlroot%\Revit_2016\nodes\en-US"    *.resources.dll *.xml
-)
-
-IF EXIST "%binroot%\Revit_2017" (
-robocopy "%binroot%\Revit_2017"               "%wwlroot%\Revit_2017"                *.dll *.exe -XF *tests.dll *.customization.dll 
-robocopy "%binroot%\Revit_2017\en-US"         "%wwlroot%\Revit_2017\en-US"          *.resources.dll *.xml
-robocopy "%binroot%\Revit_2017\nodes"         "%wwlroot%\Revit_2017\nodes"          *.dll
-robocopy "%binroot%\Revit_2017\nodes\en-US"   "%wwlroot%\Revit_2017\nodes\en-US"    *.resources.dll *.xml
-)
-
-IF EXIST "%binroot%\Revit_2018" (
-robocopy "%binroot%\Revit_2018"               "%wwlroot%\Revit_2018"                *.dll *.exe -XF *tests.dll *.customization.dll 
-robocopy "%binroot%\Revit_2018\en-US"         "%wwlroot%\Revit_2018\en-US"          *.resources.dll *.xml
-robocopy "%binroot%\Revit_2018\nodes"         "%wwlroot%\Revit_2018\nodes"          *.dll
-robocopy "%binroot%\Revit_2018\nodes\en-US"   "%wwlroot%\Revit_2018\nodes\en-US"    *.resources.dll *.xml
-)
 
 rem Reset error codes of "1" returned from "robocopy" for downstream scripts.
 if %ERRORLEVEL% equ 1 ( set errorlevel=0 ) else if %ERRORLEVEL% equ 3 ( set errorlevel=0 )
