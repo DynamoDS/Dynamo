82a9e49fe8da143b04beca2288e5c77c9f045f5e
yee xin <yeexinc@gmail.com>
2017-02-24 14:50:45 +0800

    ProcessStartInfo for external links

--- src/DynamoCoreWpf/ViewModels/Core/DynamoViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Core/DynamoViewModel.cs
@@ -792,7 +792,7 @@ namespace Dynamo.ViewModels
 
         public static void ReportABug(object parameter)
         {
-            Process.Start(Configurations.GitHubBugReportingLink);
+            Process.Start(new ProcessStartInfo("explorer.exe", Configurations.GitHubBugReportingLink));
         }
 
         public static void ReportABug()
@@ -802,7 +802,7 @@ namespace Dynamo.ViewModels
 
         internal static void DownloadDynamo()
         {
-            Process.Start(Configurations.DynamoDownloadLink);
+            Process.Start(new ProcessStartInfo("explorer.exe", Configurations.DynamoDownloadLink));
         }
 
         internal bool CanReportABug(object parameter)
@@ -2056,7 +2056,7 @@ namespace Dynamo.ViewModels
 
         public void GoToWiki(object parameter)
         {
-            Process.Start(Configurations.DynamoWikiLink);
+            Process.Start(new ProcessStartInfo("explorer.exe", Configurations.DynamoWikiLink));
         }
 
         internal bool CanGoToWiki(object parameter)
@@ -2066,7 +2066,7 @@ namespace Dynamo.ViewModels
 
         public void GoToSourceCode(object parameter)
         {
-            Process.Start(Configurations.GitHubDynamoLink);
+            Process.Start(new ProcessStartInfo("explorer.exe", Configurations.GitHubDynamoLink));
         }
 
         internal bool CanGoToSourceCode(object parameter)
@@ -2076,7 +2076,7 @@ namespace Dynamo.ViewModels
 
         public void GoToDictionary(object parameter)
         {
-            Process.Start(Configurations.DynamoDictionary);
+            Process.Start(new ProcessStartInfo("explorer.exe", Configurations.DynamoDictionary));
         }
 
         internal bool CanGoToDictionary(object parameter)
