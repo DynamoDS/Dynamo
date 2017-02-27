5753dcedd871a91a5979fbed0cd325ebc6cb7549
Gytaco <sheatherbim@gmail.com>
2017-02-27 22:31:23 +1000

    DateTime Palette
    Date Time Palette PR from UIplus plus toolkit. Includes Unit Test

---
+++ src/Libraries/CoreNodeModelsWpf/Controls/DateTimePalette.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CoreNodeModelsWpf.Controls
+{
+    /// <summary>
+    /// Interaction logic for DateTimePalette.xaml
+    /// </summary>
+    public partial class DateTimePaletteUI : UserControl
+    {
+        public DateTimePaletteUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Libraries/CoreNodeModelsWpf/NodeViewCustomizations/DateTimePalette.cs
@@ -0,0 +1,36 @@
+﻿using Dynamo.Controls;
+using Dynamo.Core;
+using Dynamo.Wpf;
+
+using CoreNodeModelsWpf.Controls;
+using CoreNodeModels.Input;
+
+namespace CoreNodeModelsWpf.Nodes
+{
+    public class DateTimePaletteNodeViewCustomization : NotificationObject, INodeViewCustomization<DateTimePalette>
+    {
+        /// <summary>
+        ///     WPF Control.
+        /// </summary>
+        public DateTimePaletteUI DateTimePaletteUINode;
+
+        /// <summary>
+        ///     Customize View.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="nodeView"></param>
+        public void CustomizeView(DateTimePalette model, NodeView nodeView)
+        {
+            DateTimePaletteUINode = new DateTimePaletteUI();
+            nodeView.inputGrid.Width = 250;
+            nodeView.inputGrid.Children.Add(DateTimePaletteUINode);
+            DateTimePaletteUINode.DataContext = model;
+        }
+
+        /// <summary>
+        ///     Dispose.
+        /// </summary>
+        public void Dispose() { }
+    }
+}
+
