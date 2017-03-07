b13f9bfc5a2f53d5da507bb2a44fb64f26f63a28
yee xin <yeexinc@gmail.com>
2017-03-07 16:51:43 +0800

    Removed libraryServices property from NodeModel

--- src/DynamoCore/Graph/Nodes/NodeModel.cs
+++ src/DynamoCore/Graph/Nodes/NodeModel.cs
@@ -46,7 +46,6 @@ namespace Dynamo.Graph.Nodes
         private string persistentWarning = "";
         private bool areInputPortsRegistered;
         private bool areOutputPortsRegistered;
-        private LibraryServices libraryServices;
 
         ///A flag indicating whether the node has been explicitly frozen.
         internal bool isFrozenExplicitly;
