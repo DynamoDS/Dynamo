7b624f2d1477d7137a1e8bd4b7f69861f28d5dee
Gytaco <sheatherbim@gmail.com>
2017-03-26 07:02:10 +1000

    Revert "Undorecorder added"
    This reverts commit 1b3ea0b7ef82645e0dfd9814be5092b9d5cf537c.

--- src/Libraries/CoreNodeModelsWpf/Controls/ColorPalette.xaml.cs
+++ src/Libraries/CoreNodeModelsWpf/Controls/ColorPalette.xaml.cs
@@ -1,33 +1,15 @@
 ﻿using System.Windows.Controls;
-using CoreNodeModelsWpf.Nodes;
-using Dynamo.Graph.Nodes;
-using Dynamo.Graph.Workspaces;
-using Dynamo.Models;
-using Dynamo.UI;
-using Dynamo.ViewModels;
 
 namespace CoreNodeModelsWpf.Controls
-
 {
     /// <summary>
     /// Interaction logic for ColorPalette.xaml
     /// </summary>
     public partial class ColorPaletteUI : UserControl
     {
-        readonly NodeModel nodeModel;
-        private IViewModelView<NodeViewModel> ui;
-        public ColorPaletteUI(NodeModel model, IViewModelView<NodeViewModel> nodeUI)
+        public ColorPaletteUI()
         {
             InitializeComponent();
-            nodeModel = model;
-            ui = nodeUI;
         }
-        
-        private void On_Change(object sender, System.Windows.RoutedPropertyChangedEventArgs<System.Windows.Media.Color?> e)
-        {
-            var undoRecorder = ui.ViewModel.WorkspaceViewModel.Model.UndoRecorder;           
-            WorkspaceModel.RecordModelForModification(nodeModel, undoRecorder);      
-        }
-        
     }
 }
