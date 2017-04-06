12c67c0546ab4654e550bdc4153945e5d34e6cad
Randy-Ma <zhaoyang.ma@autodesk.com>
2017-04-06 12:17:45 +0800

    Consolidate Display nodes for re-categorization

--- test/DynamoCoreTests/NodeMigrationTests.cs
+++ test/DynamoCoreTests/NodeMigrationTests.cs
@@ -140,6 +140,18 @@ namespace Dynamo.Tests
         }
 
         [Test]
+        public void TestMigration_Display_ByGeometryColor()
+        {
+            TestMigration("TestMigration_Display_ByGeometryColor.dyn");
+        }
+
+        [Test]
+        public void TestMigration_Display_BySurfaceColors()
+        {
+            TestMigration("TestMigration_Display_BySurfaceColors.dyn");
+        }
+
+        [Test]
         public void TestStringInput()
         {
             OpenModel(GetDynPath("TestStringInput.dyn"));
