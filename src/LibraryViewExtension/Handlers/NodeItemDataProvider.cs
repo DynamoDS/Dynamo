4e3ed4f81a5920eab2a7d5a3035772bc4c88394c
sharadkjaiswal <sharad.jaiswal@autodesk.com>
2017-04-27 16:52:51 +0800

    Add search keywords to LoadedTypes

--- src/LibraryViewExtension/Handlers/NodeItemDataProvider.cs
+++ src/LibraryViewExtension/Handlers/NodeItemDataProvider.cs
@@ -8,17 +8,24 @@ using Newtonsoft.Json;
 
 namespace Dynamo.LibraryUI.Handlers
 {
-    class LoadedItemTypeData
+    class NodeData
+    {
+        public string creationName { get; set; }
+        public string module { get; set; }
+    }
+
+    class LoadedTypeItem
     {
         public string fullyQualifiedName { get; set; }
         public string iconUrl { get; set; }
         public string contextData { get; set; }
         public string itemType { get; set; }
+        public string keywords { get; set; }
     }
 
     class LoadedTypeData
     {
-        public List<LoadedItemTypeData> loadedTypes { get; set; }
+        public List<LoadedTypeItem> loadedTypes { get; set; }
     }
 
     /// <summary>
@@ -49,14 +56,15 @@ namespace Dynamo.LibraryUI.Handlers
         {
             var data = new LoadedTypeData();
             data.loadedTypes = searchEntries
-                .Where(e => !e.ElementType.HasFlag(ElementTypes.Packaged))
+                //.Where(e => !e.ElementType.HasFlag(ElementTypes.Packaged))
                 .Select(
-                e => new LoadedItemTypeData()
+                e => new LoadedTypeItem()
                 {
                     fullyQualifiedName = GetFullyQualifiedName(e),
                     contextData = e.CreationName,
                     iconUrl = string.Format("{0}{1}.png", IconUrlServiceEndpoint, e.IconName),
-                    itemType = GetItemType(e.Group)
+                    itemType = e.Group.ToString().ToLower(),
+                    keywords = e.SearchKeywords.Any() ? e.SearchKeywords.Aggregate((x, y) => string.Format("{0}, {1}", x, y)) : string.Empty
                 }).ToList();
 
             var ms = new MemoryStream();
@@ -73,24 +81,6 @@ namespace Dynamo.LibraryUI.Handlers
         {
             var pkgprefix = element.ElementType.HasFlag(ElementTypes.Packaged) ? "pkg://" : "";
             return string.Format("{0}{1}", pkgprefix, element.FullName);
-        }
-
-        private string GetItemType(SearchElementGroup group)
-        {
-            switch (group)
-            {
-                case SearchElementGroup.None:
-                    return "none";
-                case SearchElementGroup.Create:
-                    return "creation";
-                case SearchElementGroup.Action:
-                    return "action";
-                case SearchElementGroup.Query:
-                    return "query";
-                default:
-                    break;
-            }
-            return "none";
         }
     }
 }
