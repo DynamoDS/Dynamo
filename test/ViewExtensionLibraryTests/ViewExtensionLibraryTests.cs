18de9ca22401ff7230ee7f3d4b9b0dc5f77f7429
monikaprabhu <monika.prabhu@autodesk.com>
2017-04-28 18:41:06 +0800

    Add moq test for LibraryViewController

---
+++ test/ViewExtensionLibraryTests/ViewExtensionLibraryTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using CefSharp;
+using Dynamo.LibraryUI;
+using Dynamo.Extensions;
+using System.Windows;
+using SystemTestServices;
+
+namespace ViewExtensionLibraryTests
+{
+    public class ViewExtensionLibraryTests
+    {
+        [Test]
+        [Category("UnitTests")]
+        public void ViewExtensionMockCallbacktest()
+        {
+            var cmd = new Mock<ICommandExecutive>();
+            var callback = new Mock<IJavascriptCallback>();
+            callback.Setup(c => c.CanExecute).Returns(true);
+            var controller = new EventController();
+
+            controller.On("detailsViewContextDataChanged", callback.Object);
+            controller.DetailsViewContextData = 5;
+
+            callback.Verify(c => c.ExecuteAsync(5));
+        }
+    }
+}
