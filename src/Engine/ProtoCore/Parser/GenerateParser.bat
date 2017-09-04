9ce47b231aa4bd555679454cafb19a5dc346b51b
Yu Ke <Yu.Ke@autodesk.com>
2017-09-04 14:22:52 +0800

    Remove temporary file generated by parser

--- src/Engine/ProtoCore/Parser/GenerateParser.bat
+++ src/Engine/ProtoCore/Parser/GenerateParser.bat
@@ -2,4 +2,7 @@ copy/b atg\Start.atg+atg\Associative.atg+atg\Imperative.atg+atg\End.atg DesignSc
 Coco.exe -namespace ProtoCore.DesignScriptParser DesignScript.atg
 copy DesignScript.atg DS.DesignScript.atg.temp
 del DesignScript.atg
+del DS.DesignScript.atg.temp
+del Parser.cs.old
+del Scanner.cs.old
 PAUSE
