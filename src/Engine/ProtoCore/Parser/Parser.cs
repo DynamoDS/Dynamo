1b98799555ff026ade68040cd51b0792a15d5a9e
Yu Ke <Yu.Ke@autodesk.com>
2017-09-05 12:25:42 +0800

    Update parser

--- src/Engine/ProtoCore/Parser/Parser.cs
+++ src/Engine/ProtoCore/Parser/Parser.cs
@@ -985,10 +985,9 @@ public Node root { get; set; }
 		
 		if (la.kind == 10) {
 			Associative_LanguageBlock(out rhs);
-		} else if (IsFunctionCallStatement()) {
-			Associative_FunctionCallStatement(out rhs, false);
 		} else if (StartOf(4)) {
-			Associative_NonAssignmentStatement(out rhs, false);
+			Associative_Expression(out rhs);
+			Expect(23);
 		} else SynErr(72);
 		bNode.RightNode = rhs;
 		bNode.Optr = Operator.assign;
@@ -997,15 +996,12 @@ public Node root { get; set; }
 		
 	}
 
-	void Associative_NonAssignmentStatement(out ProtoCore.AST.AssociativeAST.AssociativeNode node, bool generateTempLhs = true) {
+	void Associative_NonAssignmentStatement(out ProtoCore.AST.AssociativeAST.AssociativeNode node) {
 		while (!(StartOf(7))) {SynErr(73); Get();}
 		node = null; 
 		ProtoCore.AST.AssociativeAST.AssociativeNode rightNode = null;
 		 
 		Associative_Expression(out rightNode);
-		if (generateTempLhs)
-		{
-		//Try to make a false binary expression node.					    
 		ProtoCore.AST.AssociativeAST.BinaryExpressionNode expressionNode = new ProtoCore.AST.AssociativeAST.BinaryExpressionNode();
 		ProtoCore.AST.AssociativeAST.IdentifierNode leftNode = new ProtoCore.AST.AssociativeAST.IdentifierNode();
 		leftNode.Value = leftNode.Name = Constants.kTempProcLeftVar;
@@ -1022,11 +1018,6 @@ public Node root { get; set; }
 		expressionNode.Optr = Operator.assign;
 		NodeUtils.UpdateBinaryExpressionLocation(expressionNode);
 		node = expressionNode;
-		}
-		else
-		{
-		node = rightNode;
-		}
 		
 		if (la.val != ";")
 		   SynErr(Resources.SemiColonExpected);  
@@ -1035,7 +1026,7 @@ public Node root { get; set; }
 		NodeUtils.SetNodeEndLocation(node, t); 
 	}
 
-	void Associative_FunctionCallStatement(out ProtoCore.AST.AssociativeAST.AssociativeNode node, bool generateTempLhs = true) {
+	void Associative_FunctionCallStatement(out ProtoCore.AST.AssociativeAST.AssociativeNode node) {
 		while (!(StartOf(7))) {SynErr(74); Get();}
 		node = null; 
 		ProtoCore.AST.AssociativeAST.AssociativeNode rightNode = null;
@@ -1048,8 +1039,6 @@ public Node root { get; set; }
 		   || rightNode is ProtoCore.AST.AssociativeAST.FunctionCallNode
 		   || allowIdentList)
 		{
-		if (generateTempLhs)
-		{
 		ProtoCore.AST.AssociativeAST.BinaryExpressionNode expressionNode = new ProtoCore.AST.AssociativeAST.BinaryExpressionNode();
 		ProtoCore.AST.AssociativeAST.IdentifierNode leftNode = new ProtoCore.AST.AssociativeAST.IdentifierNode();
 		leftNode.Value = leftNode.Name = Constants.kTempProcLeftVar;
@@ -1066,11 +1055,6 @@ public Node root { get; set; }
 		expressionNode.Optr = Operator.assign;
 		NodeUtils.UpdateBinaryExpressionLocation(expressionNode);
 		node = expressionNode;
-		}
-		else
-		{
-		node = rightNode;
-		}
 		}
 		
 		if (la.val != ";")
