ecd6a1037b9372bb83e4be757bd6cdee021db952
ramramps <sriram.pollachisubburaman@autodesk.com>
2017-09-26 07:52:16 -0400

    Remove Presets

--- src/DynamoCore/Graph/Presets/PresetModel.cs
+++ src/DynamoCore/Graph/Presets/PresetModel.cs
@@ -10,6 +10,7 @@ namespace Dynamo.Graph.Presets
     /// This class references a set of nodemodels, and a set of serialized versions of those nodemodels
     /// a client can use this class to store the state of a set of nodes from a graph
     /// </summary>
+    [Obsolete("Presets are not used anymore", false)]
     public class PresetModel : ModelBase
     {
         private List<NodeModel> nodes;
