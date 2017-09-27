d16941a7bca431880abb9dbe2ee5b72cb18c4ac4
Peter <peter.b.boyer@gmail.com>
2017-09-27 13:27:38 -0400

    Dictionary library

---
+++ src/Libraries/CoreNodes/Dictionary.cs
@@ -0,0 +1,50 @@
+﻿#region
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using Autodesk.DesignScript.Runtime;
+using DSCore.Properties;
+
+#endregion
+
+namespace DSCore
+{
+    #region public methods
+    /// <summary>
+    ///     Methods for creating and manipulating Lists.
+    /// </summary>
+    [IsVisibleInDynamoLibrary(false)]
+    public static class Dictionary
+    {
+        /// <summary>
+        ///     Makes a dictionary using the supplied list of keys and values. The number of entries will be equal to 
+        ///     the shorter of keys or values.
+        /// </summary>
+        /// <returns name="keys">The keys of the dictionary</returns>
+        /// <returns name="values">The values of the dictionary</returns>
+        /// <search>dictionary, {}</search>
+        [IsVisibleInDynamoLibrary(true)]
+        public static IDictionary ByKeysValues(IList keys, IList values)
+        {
+            return keys.Cast<object>().Zip(values.Cast<object>(), (a, b) =>
+            {
+                if (a is double)
+                {
+                    a = (int) Math.Floor((double) a);
+                }
+
+                if (a is string || a is int || a is String)
+                {
+                    return new KeyValuePair<object, object>(a, b);
+                }
+
+                throw new Exception("All keys must be strings or numbers");
+            }).ToDictionary(a => a.Key, a => a.Value);
+        }
+    }
+    #endregion
+}
