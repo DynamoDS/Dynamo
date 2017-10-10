e73cfd27f8628e9e0499c256f9b5704628edebbd
QilongTang <173288704@qq.com>
2017-10-10 01:58:48 -0400

    Address comments

--- src/DynamoCore/Graph/Workspaces/WorkspaceInfo.cs
+++ src/DynamoCore/Graph/Workspaces/WorkspaceInfo.cs
@@ -111,6 +111,7 @@ namespace Dynamo.Graph.Workspaces
                 // we have a dyf and it lacks an ID field, we need to assign it
                 // a deterministic guid based on its name.  By doing it deterministically,
                 // files remain compatible
+                if (string.IsNullOrEmpty(id) && !string.IsNullOrEmpty(funName) && funName != Properties.Resources.DefaultHomeWorkspaceName)
                 {
                     id = GuidUtility.Create(GuidUtility.UrlNamespace, funName).ToString();
                 }
