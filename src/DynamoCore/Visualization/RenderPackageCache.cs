910c1a093b3bcdceb6a0650c3a65fcbbcc7a89ca
ColinDayOrg <colin.day@autodesk.com>
2017-11-27 00:24:13 -0500

    Add some comments

--- src/DynamoCore/Visualization/RenderPackageCache.cs
+++ src/DynamoCore/Visualization/RenderPackageCache.cs
@@ -4,6 +4,9 @@ using Autodesk.DesignScript.Interfaces;
 
 namespace Dynamo.Visualization
 {
+    /// <summary>
+    /// This class controls the collection and distribution of render packages
+    /// </summary>
     public class RenderPackageCache
     {
         private List<IRenderPackage> packages;
@@ -24,18 +27,28 @@ namespace Dynamo.Visualization
             portMap[outputPortId].Add(package);
         }
 
+        /// <summary>
+        /// Create an empty RenderPackageCache object
+        /// </summary>
         public RenderPackageCache()
         {
             packages = new List<IRenderPackage>();
             portMap = null;
         }
 
+        /// <summary>
+        /// Create a RenderPackageCache object containing the given render packages
+        /// </summary>
+        /// <param name="otherPackages">The packages to fill the new cache object.</param>
         public RenderPackageCache(IEnumerable<IRenderPackage> otherPackages)
         : this()
         {
             packages.AddRange(otherPackages);
         }
 
+        /// <summary>
+        /// Get the render packages in this cache
+        /// </summary>
         public IEnumerable<IRenderPackage> Packages
         {
             get
@@ -44,6 +57,10 @@ namespace Dynamo.Visualization
             }
         }
 
+        /// <summary>
+        /// Get the RenderPackageCache object for the given port ID
+        /// </summary>
+        /// <param name="portId">The port ID used to get the sub-cache.</param>
         public RenderPackageCache GetPortPackages(Guid portId)
         {
             if (portMap == null)
@@ -59,11 +76,18 @@ namespace Dynamo.Visualization
             return portPackages;
         }
 
+        /// <summary>
+        /// Returns true if the cache is empty
+        /// </summary>
         public bool IsEmpty()
         {
             return packages.Count == 0;
         }
 
+        /// <summary>
+        /// Concatenates the other cache into this cache
+        /// </summary>
+        /// <param name="other">The cache to add to this cache.</param>
         public void Add(RenderPackageCache other)
         {
             packages.AddRange(other.packages);
@@ -80,11 +104,21 @@ namespace Dynamo.Visualization
             }
         }
 
+        /// <summary>
+        /// Adds a render package to this cache
+        /// </summary>
+        /// <param name="package">The package to add to this cache.</param>
         public void Add(IRenderPackage package)
         {
             packages.Add(package);
         }
 
+        /// <summary>
+        /// Adds a render package to this cache, including a reference to 
+        /// the output port that the package originated from
+        /// </summary>
+        /// <param name="package">The package to add to this cache.</param>
+        /// <param name="outputPortId">The output port to associate the package with.</param>
         public void Add(IRenderPackage package, Guid outputPortId)
         {
             Add(package);
