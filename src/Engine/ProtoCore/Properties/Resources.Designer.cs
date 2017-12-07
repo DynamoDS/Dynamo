dd276be51af729e70c838e284610d9698762751f
aparajit-pratap <aparajit.pratap@autodesk.com>
2017-12-07 16:53:32 -0500

    fix warning message

--- src/Engine/ProtoCore/Properties/Resources.Designer.cs
+++ src/Engine/ProtoCore/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ProtoCore.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -953,7 +953,7 @@ namespace ProtoCore.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Array indices must be numeric..
+        ///   Looks up a localized string similar to List indices must be numeric..
         /// </summary>
         public static string InvalidArrayIndexType {
             get {
