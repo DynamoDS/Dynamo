02fa77e5738e2f098a51f0335e8d2120fb778821
Craig Long <craig.long@autodesk.com>
2018-01-31 10:06:29 -0500

    Add NodeOutputData class and helper methods

---
+++ src/DynamoCore/Graph/Nodes/NodeOutputData.cs
@@ -0,0 +1,105 @@
+﻿using Dynamo.Graph.Workspaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.Graph.Nodes
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum NodeOutputTypes
+    {
+        [EnumMember(Value = "integer")]
+        integerOutput,
+        [EnumMember(Value = "float")]
+        floatOutput,
+        [EnumMember(Value = "boolean")]
+        booleanOutput,
+        [EnumMember(Value = "string")]
+        stringOutput,
+        [EnumMember(Value = "unknown")]
+        unknownOutput
+    };
+
+
+    /// <summary>
+    /// Represents a node which acts as a UI output for the graph
+    /// - may also hold a value for that output
+    /// </summary>
+    public class NodeOutputData
+    {
+        /// <summary>
+        /// The id of the node.
+        /// </summary>
+        [JsonConverter(typeof(IdToGuidConverter))]
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Display name of the output node.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The type of output this node is.
+        /// </summary>
+        public NodeOutputTypes Type { get; set; }
+        /// <summary>
+        /// The value of the output when the graph was saved.
+        /// This should always be a string for all types.
+        /// </summary>
+        public string IntitialValue { get; set; }
+        /// <summary>
+        /// Description displayed to user of this output node.
+        /// </summary>
+        public string Description { get; set; }
+
+        private static Dictionary<Type, NodeOutputTypes> dotNetTypeToNodeOutputType = new Dictionary<Type, NodeOutputTypes>
+        {
+            { typeof(String),NodeOutputTypes.stringOutput},
+            { typeof(Boolean),NodeOutputTypes.booleanOutput},
+            { typeof(double),NodeOutputTypes.floatOutput},
+            { typeof(int),NodeOutputTypes.integerOutput},
+            { typeof(float),NodeOutputTypes.floatOutput},
+        };
+        public static NodeOutputTypes getNodeOutputTypeFromType(Type type)
+        {
+            NodeOutputTypes output;
+            if (dotNetTypeToNodeOutputType.TryGetValue(type, out output))
+            {
+                return output;
+            }
+            else
+            {
+                return NodeOutputTypes.unknownOutput;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var converted = obj as NodeOutputData;
+
+            var valNumberComparison = false;
+            try
+            {
+                valNumberComparison = Math.Abs(Convert.ToDouble(this.IntitialValue) - Convert.ToDouble(converted.IntitialValue)) < .000001;
+            }
+            catch (Exception e)
+            {
+                //this just stays false.
+                valNumberComparison = false;
+            }
+
+            return obj is NodeOutputData && this.Id == converted.Id &&
+                this.Description == converted.Description &&
+                //TODO don't check name for now as this requires a VIew.
+                //and we only have model level tests.
+                //this.Name == converted.Name &&
+                this.Type == converted.Type &&
+                //check if the value is the same or if the value is a number check is it similar
+                ((this.IntitialValue == converted.IntitialValue) || valNumberComparison || this.IntitialValue.ToString() == converted.IntitialValue.ToString());
+        }
+    }
+
+}
