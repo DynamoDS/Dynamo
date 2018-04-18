2ed4b0eb44d699558a55a0fa76e14ba600fcafa1
Jorgen Dahl <jorgen.dahl@autodesk.com>
2018-04-18 11:07:46 -0400

    Add color support

--- src/DynamoCLI/GeometryHolder.cs
+++ src/DynamoCLI/GeometryHolder.cs
@@ -61,16 +61,22 @@ namespace DynamoCLI
                 if (renderPackages.Packages.Count() > 0)
                 {
                     List<double[]> verts = new List<double[]>();
+                    List<List<byte>> vertColors = new List<List<byte>>();
                     List<double[]> normals = new List<double[]>();
                     List<List<double>> points = new List<List<double>>();
+                    List<List<byte>> pointColors = new List<List<byte>>();
                     List<List<double>> lines = new List<List<double>>();
+                    List<List<byte>> lineColors = new List<List<byte>>();
 
                     foreach (IRenderPackage p in renderPackages.Packages)
                     {
                         verts.Add(p.MeshVertices.ToArray());
+                        vertColors.Add(p.MeshVertexColors.ToList());
                         normals.Add(p.MeshNormals.ToArray());
                         points.Add(p.PointVertices.ToList());
+                        pointColors.Add(p.PointVertexColors.ToList());
                         lines.Add(p.LineStripVertices.ToList());
+                        lineColors.Add(p.LineStripVertexColors.ToList());
                     }
 
                     Dictionary<string, Object> groupData = new Dictionary<string, object>();
@@ -78,9 +84,12 @@ namespace DynamoCLI
                     groupData.Add("transactionType", "update");
                     groupData.Add("displayPreview", node.ShouldDisplayPreview);
                     groupData.Add("vertices", verts);
+                    groupData.Add("verticeColors", vertColors);
                     groupData.Add("normals", normals);
                     groupData.Add("points", points);
+                    groupData.Add("pointColors", pointColors);
                     groupData.Add("lines", lines);
+                    groupData.Add("lineColors", lineColors);
 
                     GeometryJson = JsonConvert.SerializeObject(groupData);
                     HasGeometry = true;
