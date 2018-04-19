9e73ada0939d06df53699ccec7397c350747b552
Jorgen Dahl <jorgen.dahl@autodesk.com>
2018-04-19 10:58:20 -0400

    Add unit test for CLI geometry option.

--- test/Libraries/CommandLineTests/CommandLineTests.cs
+++ test/Libraries/CommandLineTests/CommandLineTests.cs
@@ -10,6 +10,8 @@ using System.Xml;
 using Dynamo;
 using Dynamo.Applications;
 using DynamoCLI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Dynamo.Tests
 {
@@ -104,5 +106,45 @@ namespace Dynamo.Tests
             AssertOutputValuesForGuid("6a5bcff0-ce40-4773-aee6-88d99104b4a7", new List<Tuple<int, string>> { Tuple.Create(0, "{a,b,c,d,e}"), Tuple.Create(1, "{1,2,{bar : 999, foo : 99},{4,5},6}") }, output);
 
         }
+        [Test]
+        public void CanOpenAndRunFileWithCustomNodeAndOutputGeometryFromDynamoCLIexe()
+        {
+            string openpath = Path.Combine(TestDirectory, @"core\commandline\GeometryTest.dyn");
+            var newpath = GetNewFileNameOnTempPath("json");
+            string geometrypath = Path.Combine(TestDirectory, @"core\commandline\GeometryTest.json");
+            string commandstring = "/o" + " " + openpath + " " + "/g" + " " + newpath;
+
+            DynamoCLI.Program.Main(CommandStringToStringArray(commandstring));
+
+            using (StreamReader geometryFile = new StreamReader(geometrypath))
+            {
+                using (StreamReader newFile = new StreamReader(newpath))
+                {
+                    Assert.IsNotNull(geometryFile);
+                    var geometry = geometryFile.ReadToEnd();
+
+                    Assert.IsNotNull(newFile);
+                    var newGeometry = newFile.ReadToEnd();
+
+                    Assert.IsNotNullOrEmpty(geometry);
+                    var geometryJson = JsonConvert.DeserializeObject(geometry) as JArray;
+
+                    Assert.IsNotNullOrEmpty(newGeometry);
+                    var newGeometryJson = JsonConvert.DeserializeObject(newGeometry) as JArray;
+
+                    Assert.IsNotNull(geometryJson);
+                    Assert.IsNotNull(newGeometryJson);
+
+                    Assert.IsTrue(JToken.DeepEquals(geometryJson, newGeometryJson));
+                }
+            }
+
+            //            var output = new XmlDocument();
+            //            output.Load(newpath);
+            //            AssertOutputValuesForGuid("36c30251-c867-4d73-9a3b-24f3e9ab00e5", new List<Tuple<int, string>> { Tuple.Create(0, "{e : 6, d : {4, 5}, a : 1, c : {bar : 999, foo : 99}, b : 2}") }, output);
+            //            AssertOutputValuesForGuid("6a5bcff0-ce40-4773-aee6-88d99104b4a7", new List<Tuple<int, string>> { Tuple.Create(0, "{a,b,c,d,e}"), Tuple.Create(1, "{1,2,{bar : 999, foo : 99},{4,5},6}") }, output);
+
+        }
+
     }
 }
