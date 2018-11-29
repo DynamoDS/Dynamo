90cd788a8d33854647d401a40c63f8b0fbcf2a30
Michael Kirschner <mjk.kirschner@gmail.com>
2018-11-29 10:22:01 -0500

    rename notification stings so  bindings update (#9275)
    rename converter (spelling error)
    rename private fields for consistencey
    add tests for color and text

--- src/DynamoCore/Graph/Nodes/PortModel.cs
+++ src/DynamoCore/Graph/Nodes/PortModel.cs
@@ -29,7 +29,7 @@ namespace Dynamo.Graph.Nodes
         private bool usingDefaultValue;
         private bool isEnabled = true;
         private bool useLevels = false;
-        private bool shouldKeepListStructure = false;
+        private bool keepListStructure = false;
         private int level = 1;
         private string toolTip;
         #endregion
@@ -271,14 +271,14 @@ namespace Dynamo.Graph.Nodes
         {
             get
             {
-                return shouldKeepListStructure;
+                return keepListStructure;
             }
             set
             {
-                if (shouldKeepListStructure != value)
+                if (keepListStructure != value)
                 {
-                    shouldKeepListStructure = value;
-                    RaisePropertyChanged("ShouldKeepListStructure");
+                    keepListStructure = value;
+                    RaisePropertyChanged(nameof(KeepListStructure));
                 }
             }
         }
