2f36c6bb9ace1e89dd51f1178209d1ae6d8fc74e
Aaron (Qilong) <aaron.tang@autodesk.com>
2018-11-30 15:29:06 -0500

    Add Build and Revision Number to distinguish RC1.3.4

--- tools/NuGet/BuildPackages.bat
+++ tools/NuGet/BuildPackages.bat
@@ -20,7 +20,7 @@ for /f %%f in ('cscript //Nologo ..\install\GetFileVersion.vbs %harvestPath%\Dyn
   set /a count=!count!+1
 )
 setlocal DisableDelayedExpansion
-set version=%Major%.%Minor%.0
+set version=%Major%.%Minor%.%Build%.%Revision%
 
 :: Clean files generated from the previous run
 del *.nupkg
