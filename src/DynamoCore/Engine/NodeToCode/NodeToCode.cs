ced9b8d62481b11a5ab0c780596161f45a959e45
Michael Kirschner <mjk.kirschner@gmail.com>
2019-01-02 13:01:51 -0500

    Avoid namespace collisions when collapsing graph to custom node (#9330)
    * this works - but I am hesitant to modify this replacer.
    Not sure how this effects node2code. - may create rewriter directly.
    
    * add a new visitor method for typedIdentifierNode
    use it in custmNodeManager collapse method
    
    * move shortQualifiedNameReplacer
    update references
    add comments to node2code mutation tests
    
    * add more tests
    add extra logic to deserialization for symbol node element resolver cache
    
    * revert public API break, keep rewriter in dynamoCore but in new file
    
    * remove extra usings added previously.
    
    * remove debugging code

--- src/DynamoCore/Engine/NodeToCode/NodeToCode.cs
+++ src/DynamoCore/Engine/NodeToCode/NodeToCode.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Dynamo.Core;
+﻿using Dynamo.Core;
 using Dynamo.Engine.CodeGeneration;
 using Dynamo.Graph;
 using Dynamo.Graph.Nodes;
@@ -10,7 +7,9 @@ using ProtoCore.AST.AssociativeAST;
 using ProtoCore.DSASM;
 using ProtoCore.Namespace;
 using ProtoCore.SyntaxAnalysis;
-using ProtoCore.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Dynamo.Engine.NodeToCode
 {
@@ -299,142 +298,6 @@ namespace Dynamo.Engine.NodeToCode
                 node.RightNode = newRightNode;
 
             return node;
-        }
-    }
-
-    /// <summary>
-    /// Replace a fully qualified function call with short name. 
-    /// </summary>
-    internal class ShortestQualifiedNameReplacer : AstReplacer
-    {
-        private readonly ClassTable classTable;
-        private readonly ElementResolver resolver;
-
-        public ShortestQualifiedNameReplacer(ClassTable classTable, ElementResolver resolver)
-        {
-            this.classTable = classTable;
-            this.resolver = resolver;
-        }
-
-        public override AssociativeNode VisitIdentifierListNode(IdentifierListNode node)
-        {
-            if (node == null)
-                return null;
-
-            // First pass attempt to resolve the node class name 
-            // and shorten it before traversing it deeper
-            AssociativeNode shortNameNode;
-            if (TryShortenClassName(node, out shortNameNode))
-                return shortNameNode;
-
-            var rightNode = node.RightNode;
-            var leftNode = node.LeftNode;
-
-            rightNode = rightNode.Accept(this);
-            var newLeftNode = leftNode.Accept(this);
-
-            node = new IdentifierListNode
-            {
-                LeftNode = newLeftNode,
-                RightNode = rightNode,
-                Optr = Operator.dot
-            };
-            return leftNode != newLeftNode ? node : RewriteNodeWithShortName(node);
-        }
-
-        private bool TryShortenClassName(IdentifierListNode node, out AssociativeNode shortNameNode)
-        {
-            shortNameNode = null;
-
-            string qualifiedName = CoreUtils.GetIdentifierExceptMethodName(node);
-
-            // if it is a global method with no class
-            if (string.IsNullOrEmpty(qualifiedName))
-                return false;
-
-            // Make sure qualifiedName is not a property
-            var matchingClasses = classTable.GetAllMatchingClasses(qualifiedName);
-            if (matchingClasses.Length == 0)
-                return false;
-
-            string className = qualifiedName.Split('.').Last();
-
-            var symbol = new ProtoCore.Namespace.Symbol(qualifiedName);
-            if (!symbol.Matches(node.ToString()))
-                return false;
-
-            shortNameNode = CreateNodeFromShortName(className, qualifiedName);
-            return shortNameNode != null;
-        }
-
-        private IdentifierListNode RewriteNodeWithShortName(IdentifierListNode node)
-        {
-            // Get class name from AST
-            string qualifiedName = CoreUtils.GetIdentifierExceptMethodName(node);
-
-            // if it is a global method
-            if (string.IsNullOrEmpty(qualifiedName))
-                return node;
-
-            // Make sure qualifiedName is not a property
-            var lNode = node.LeftNode;
-            var matchingClasses = classTable.GetAllMatchingClasses(qualifiedName);
-            while (matchingClasses.Length == 0 && lNode is IdentifierListNode)
-            {
-                qualifiedName = lNode.ToString();
-                matchingClasses = classTable.GetAllMatchingClasses(qualifiedName);
-                lNode = ((IdentifierListNode)lNode).LeftNode;
-            }
-            qualifiedName = lNode.ToString();
-            string className = qualifiedName.Split('.').Last();
-
-            var newIdentList = CreateNodeFromShortName(className, qualifiedName);
-            if (newIdentList == null)
-                return node;
-
-            // Replace class name in input node with short name (newIdentList)
-            node = new IdentifierListNode
-            {
-                LeftNode = newIdentList,
-                RightNode = node.RightNode,
-                Optr = Operator.dot
-            };
-            return node;
-        }
-
-        private AssociativeNode CreateNodeFromShortName(string className, string qualifiedName)
-        {
-            // Get the list of conflicting namespaces that contain the same class name
-            var matchingClasses = CoreUtils.GetResolvedClassName(classTable, AstFactory.BuildIdentifier(className));
-            if (matchingClasses.Length == 0)
-                return null;
-
-            string shortName;
-            // if there is no class conflict simply use the class name as the shortest name
-            if (matchingClasses.Length == 1)
-            {
-                shortName = className;
-            }
-            else
-            {
-                shortName = resolver != null ? resolver.LookupShortName(qualifiedName) : null;
-
-                if (string.IsNullOrEmpty(shortName))
-                {
-                    // Use the namespace list as input to derive the list of shortest unique names
-                    var symbolList =
-                        matchingClasses.Select(matchingClass => new ProtoCore.Namespace.Symbol(matchingClass))
-                            .ToList();
-                    var shortNames = ProtoCore.Namespace.Symbol.GetShortestUniqueNames(symbolList);
-
-                    // Get the shortest name corresponding to the fully qualified name
-                    shortName = shortNames[new ProtoCore.Namespace.Symbol(qualifiedName)];
-                }
-            }
-            // Rewrite the AST using the shortest name
-            var newIdentList = CoreUtils.CreateNodeFromString(shortName);
-            return newIdentList;
-
         }
     }
 
@@ -1194,6 +1057,7 @@ namespace Dynamo.Engine.NodeToCode
                 ast.Accept(replacer);
             }
         }
+
 
         /// <summary>
         /// Compile a set of nodes to ASTs. 
