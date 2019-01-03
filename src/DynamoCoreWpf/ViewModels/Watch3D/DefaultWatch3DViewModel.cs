792ea81e6c5122504571642f74b776befa883096
Keith Alfaro <keith4895@gmail.com>
2019-01-03 11:02:48 -0500

    use is instead of typeof/GetType

--- src/DynamoCoreWpf/ViewModels/Watch3D/DefaultWatch3DViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Watch3D/DefaultWatch3DViewModel.cs
@@ -95,7 +95,7 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
 
                 OnActiveStateChanged();
 
-                if (active && this.GetType() == typeof(Wpf.ViewModels.Watch3D.HelixWatch3DViewModel))
+                if (active && this is Wpf.ViewModels.Watch3D.HelixWatch3DViewModel)
                 {
                     RegenerateAllPackages();
                 }
