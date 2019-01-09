037c2ae5bea696e2559f124345b5e8d190f7c12a
Radu Gidei <radugidei@gmail.com>
2019-01-09 12:41:31 +0000

    scaffold unit tests for CrashReporting

---
+++ test/DynamoCoreWpfTests/CrashReportingTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Dynamo.ViewModels;
+using NUnit.Framework;
+
+namespace Dynamo.Tests
+{
+    public class CrashReportingTests : DynamoViewModelUnitTest
+    {
+        public List<string> TargetWords = new List<string> { "New Issue", "DynamoDS" };
+
+        [Test]
+        public void CanReportBugWithNoContent()
+        {
+            // report a bug with no details
+            Assert.DoesNotThrow( () => DynamoViewModel.ReportABug());
+
+            // give the system time to launch a browser & open the page
+            Thread.Sleep(3000);
+
+            // check browser is open on correct page
+            Assert.True(BrowserIsOpenOnPageWithTitleMatching(TargetWords));
+        }
+
+        [Test]
+        public void CanReportBugWithContent()
+        {
+            // report a bug with details
+            var details = "Exception thrown somewhere";
+            Assert.IsNotNull(details);
+            Assert.DoesNotThrow(() => DynamoViewModel.ReportABug(details));
+
+            // give the system time to launch a browser & open the page
+            Thread.Sleep(3000);
+
+            // check browser is open on correct page
+            Assert.True(BrowserIsOpenOnPageWithTitleMatching(TargetWords));
+        }
+
+        private bool BrowserIsOpenOnPageWithTitleMatching(List<string> pageTitleWords)
+        {
+            // get list of all processes on the system
+            var allProcesses = Process.GetProcesses();
+
+            // find processes whose main window title matches our keywords
+            var matchingProcesses = allProcesses
+                                        .Select(x => x.MainWindowTitle)
+                                        .Where(title => ContainsAllTargetWords(title))
+                                        .ToList();
+
+            // check that at least one process matches our search
+            return matchingProcesses.Any();
+            }
+
+        private bool ContainsAllTargetWords(string source)
+        {
+            return TargetWords.TrueForAll(x => source.Contains(x));
+        }
+    }
+}
