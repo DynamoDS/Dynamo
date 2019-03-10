40f70cc830588ad3480f89c715330570df2a9d2b
aparajit-pratap <aparajit.pratap@autodesk.com>
2019-03-10 15:50:21 -0400

    DYN-1639: Add performance test framework  (#9539)
    * add performance test framework v1
    
    * add .net standard reference
    
    * nunit perf test fx to compare times with benchmarkdotnet
    
    * revert VisualStudioVersion to version 14
    
    * set AutoGenerateBindingRedirects property to true in csproj file to resolve conflicts with dependencies
    
    * revert unnecessary changes
    
    * uninstall BenchmarkDotNet v0.11.4 and replace with v0.11.3
    
    * Replace GlobalSetup with IterationSetup
    
    * config changes
    
    * enclose Console.Writeline statements within debug flags to improve VM performance in release mode
    
    * remove NUnitPerformance project, add commandline argument for test directory
    
    * copy core dependencies and build project in its own bin directory
    
    * revert debug flags surrounding console.writeline statements
    
    * add assemblysharedinfo to project, add API docs

---
+++ test/Performance/DynamoPerformanceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DynamoPerformanceTests")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b19c56fe-48c0-445e-9200-2cbf9b6ac77f")]
+
