DynamoDS/Dynamo

commit 0f4be7d2d5102f7b443246c4d5ff72282343086f

Duplicate connectors using `ctrl + left click` on input ports with testing (#9567)
* Add recorded command tests for ctrl + click duplicate input port PR

* add access modifiers where not present in DynamoModelCommands.cs

* use var

* RequiresSTA not required

* address API breaking changes

* more API preserving cleanup

* add Jira task number for removing API breaking changes

* update recorded tests after modifying enum

* internal to private accessors

src/DynamoCore/Models/DynamoModelCommands.cs
@@ -52,19 +52,19 @@ namespace Dynamo.Models
             //ClipBoard.Clear();
         }
 
-        void RunCancelImpl(RunCancelCommand command)
+        private void RunCancelImpl(RunCancelCommand command)
         {
             var model = CurrentWorkspace as HomeWorkspaceModel;
             if (model != null)
                 model.Run();
         }
 
-        void ForceRunCancelImpl(ForceRunCancelCommand command)
+        private void ForceRunCancelImpl(ForceRunCancelCommand command)
         {
             ForceRun();
         }
 
-        void CreateNodeImpl(CreateNodeCommand command)
+        private void CreateNodeImpl(CreateNodeCommand command)
         {
             var node = GetNodeFromCommand(command);
             if (node == null)
@@ -79,7 +79,7 @@ namespace Dynamo.Models
             CurrentWorkspace.RecordCreatedModel(node);
         }
 
-        void CreateAndConnectNodeImpl(CreateAndConnectNodeCommand command)
+        private void CreateAndConnectNodeImpl(CreateAndConnectNodeCommand command)
         {
             using (CurrentWorkspace.UndoRecorder.BeginActionGroup())
             {
@@ -124,7 +124,7 @@ namespace Dynamo.Models
             }
         }
 
-        NodeModel GetNodeFromCommand(CreateNodeCommand command)
+        private NodeModel GetNodeFromCommand(CreateNodeCommand command)
         {
             if (command.Node != null)
             {
@@ -202,7 +202,7 @@ namespace Dynamo.Models
             return node;
         }
 
-        void CreateNoteImpl(CreateNoteCommand command)
+        private void CreateNoteImpl(CreateNoteCommand command)
         {
             NoteModel noteModel = CurrentWorkspace.AddNote(
                 command.DefaultPosition,
@@ -214,14 +214,14 @@ namespace Dynamo.Models
             CurrentWorkspace.RecordCreatedModel(noteModel);
         }
 
-        void CreateAnnotationImpl(CreateAnnotationCommand command)
+        private void CreateAnnotationImpl(CreateAnnotationCommand command)
         {
             AnnotationModel annotationModel = currentWorkspace.AddAnnotation(command.AnnotationText, command.ModelGuid);
             
             CurrentWorkspace.RecordCreatedModel(annotationModel);
         }
 
-        void SelectModelImpl(SelectModelCommand command)
+        private void SelectModelImpl(SelectModelCommand command)
         {
             // Empty ModelGuid means clear selection.
             if (command.ModelGuid == Guid.Empty)
@@ -249,7 +249,7 @@ namespace Dynamo.Models
             }
         }
 
-        void MakeConnectionImpl(MakeConnectionCommand command)
+        private void MakeConnectionImpl(MakeConnectionCommand command)
         {
             Guid nodeId = command.ModelGuid;
 
@@ -271,8 +271,17 @@ namespace Dynamo.Models
                     EndShiftReconnections(nodeId, command.PortIndex, command.Type);
                     break;
 
+                // TODO - can be removed in Dynamo 3.0 - DYN-1729
                 case MakeConnectionCommand.Mode.EndAndStartCtrlConnection:
-                    EndAndStartCtrlConnection(nodeId, command.PortIndex, command.Type);
+                    BeginDuplicateConnection(nodeId, command.PortIndex, command.Type);
+                    break;
+
+                case MakeConnectionCommand.Mode.BeginDuplicateConnection:
+                    BeginDuplicateConnection(nodeId, command.PortIndex, command.Type);
+                    break;
+
+                case MakeConnectionCommand.Mode.BeginCreateConnections:
+                    BeginCreateConnections(nodeId, command.PortIndex, command.Type);
                     break;
 
                 case MakeConnectionCommand.Mode.Cancel:
@@ -281,7 +290,7 @@ namespace Dynamo.Models
             }
         }
 
-        void BeginConnection(Guid nodeId, int portIndex, PortType portType)
+        private void BeginConnection(Guid nodeId, int portIndex, PortType portType)
         {
             bool isInPort = portType == PortType.Input;
             activeStartPorts = null;
@@ -315,7 +324,36 @@ namespace Dynamo.Models
             }
         }
 
-        void BeginShiftReconnections(Guid nodeId, int portIndex, PortType portType)
+        private void BeginDuplicateConnection(Guid nodeId, int portIndex, PortType portType)
+        {
+            // If the port clicked is an output port, begin connection as per normal
+            if (portType == PortType.Output)
+            {
+                BeginConnection(nodeId, portIndex, portType);
+                return;
+            }
+
+            // Otherwise, if the port is an input port, check if the port already has a connection.
+            // If it does, duplicate the existing connection.
+            var node = CurrentWorkspace.GetModelInternal(nodeId) as NodeModel;
+            if (node == null)
+            {
+                return;
+            }
+
+            var portModel = node.InPorts[portIndex];
+            if (portModel.Connectors.Count == 0)
+            {
+                // If the port doesn't have any existing connections, begin connection as per normal
+                BeginConnection(nodeId, portIndex, portType);
+                return;
+            }
+            // Duplicate the existing connection
+            activeStartPorts = new PortModel[] { portModel.Connectors[0].Start };
+            firstStartPort = portModel.Connectors[0].Start;
+        }
+
+        private void BeginShiftReconnections(Guid nodeId, int portIndex, PortType portType)
         {
             if (portType == PortType.Input) return; //only handle multiple connections when the port selected is an output port
             var node = CurrentWorkspace.GetModelInternal(nodeId) as NodeModel;
@@ -343,7 +381,7 @@ namespace Dynamo.Models
             return;
         }
 
-        void EndConnection(Guid nodeId, int portIndex, PortType portType)
+        private void EndConnection(Guid nodeId, int portIndex, PortType portType)
         {
             // Check if the node from which the connector starts is valid and has not been deleted
             if (activeStartPorts == null || activeStartPorts.Count() <= 0 || activeStartPorts[0].Owner == null) return;
@@ -365,7 +403,7 @@ namespace Dynamo.Models
             firstStartPort = null;
         }
 
-        void EndShiftReconnections(Guid nodeId, int portIndex, PortType portType)
+        private void EndShiftReconnections(Guid nodeId, int portIndex, PortType portType)
         {
             if (portType == PortType.Input) return; //only handle multiple connections when the port selected is an output port
             if (activeStartPorts == null || activeStartPorts.Count() <= 0) return;
@@ -388,7 +426,7 @@ namespace Dynamo.Models
             return;
         }
 
-        void EndAndStartCtrlConnection(Guid nodeId, int portIndex, PortType portType)
+        private void BeginCreateConnections(Guid nodeId, int portIndex, PortType portType)
         {
             if (portType == PortType.Output) return; // Only handle ctrl connections if selected port is an input port
             if (firstStartPort == null || activeStartPorts == null || activeStartPorts.Count() <= 0) return;
@@ -403,7 +441,7 @@ namespace Dynamo.Models
             activeStartPorts = new PortModel[] { firstStartPort };
         }
 
-        void CancelConnections()
+        private void CancelConnections()
         {
             CurrentWorkspace.DeleteSavedModels();
             activeStartPorts = null;
@@ -411,7 +449,7 @@ namespace Dynamo.Models
             return;
         }
 
-        static Dictionary<ModelBase, UndoRedoRecorder.UserAction> GetConnectorsToAddAndDelete(
+        private static Dictionary<ModelBase, UndoRedoRecorder.UserAction> GetConnectorsToAddAndDelete(
             PortModel endPort, PortModel startPort)
         {
             ConnectorModel connectorToRemove = null;
@@ -457,7 +495,7 @@ namespace Dynamo.Models
             return models;
         }
 
-        void DeleteModelImpl(DeleteModelCommand command)
+        private void DeleteModelImpl(DeleteModelCommand command)
         {
             var modelsToDelete = new List<ModelBase>();
             if (command.ModelGuid == Guid.Empty)
@@ -473,7 +511,7 @@ namespace Dynamo.Models
             DeleteModelInternal(modelsToDelete);
         }
 
-        void UngroupModelImpl(UngroupModelCommand command)
+        private void UngroupModelImpl(UngroupModelCommand command)
         {
             if (command.ModelGuid == Guid.Empty)
                 return;
@@ -483,7 +521,7 @@ namespace Dynamo.Models
             UngroupModel(modelsToUngroup);
         }
 
-        void AddToGroupImpl(AddModelToGroupCommand command)
+        private void AddToGroupImpl(AddModelToGroupCommand command)
         {
             if (command.ModelGuid == Guid.Empty)
                 return;
@@ -493,7 +531,7 @@ namespace Dynamo.Models
             AddToGroup(modelsToGroup);
         }
 
-        void UndoRedoImpl(UndoRedoCommand command)
+        private void UndoRedoImpl(UndoRedoCommand command)
         {
             switch (command.CmdOperation)
             {
@@ -506,7 +544,7 @@ namespace Dynamo.Models
             }
         }
 
-        void SendModelEventImpl(ModelEventCommand command)
+        private void SendModelEventImpl(ModelEventCommand command)
         {
             foreach (var guid in command.ModelGuids)
             {
@@ -514,7 +552,7 @@ namespace Dynamo.Models
             }
         }
 
-        void UpdateModelValueImpl(UpdateModelValueCommand command)
+        private void UpdateModelValueImpl(UpdateModelValueCommand command)
         {
             WorkspaceModel targetWorkspace = CurrentWorkspace;
             if (!command.WorkspaceGuid.Equals(Guid.Empty))
@@ -533,7 +571,7 @@ namespace Dynamo.Models
             CurrentWorkspace.HasUnsavedChanges = true;
         }
 
-        void CreateCustomNodeImpl(CreateCustomNodeCommand command)
+        private void CreateCustomNodeImpl(CreateCustomNodeCommand command)
         {
             var workspace = CustomNodeManager.CreateCustomNode(
                 command.Name,
@@ -545,19 +583,19 @@ namespace Dynamo.Models
             CurrentWorkspace = workspace;
         }
 
-        void SwitchWorkspaceImpl(SwitchTabCommand command)
+        private void SwitchWorkspaceImpl(SwitchTabCommand command)
         {
             // We don't attempt to null-check here, we need it to fail fast.
             CurrentWorkspace = Workspaces.ElementAt(command.WorkspaceModelIndex);
         }
 
-        void CreatePresetStateImpl(AddPresetCommand command)
+        private void CreatePresetStateImpl(AddPresetCommand command)
         {
             var preset = this.CurrentWorkspace.AddPreset(command.PresetStateName,command.PresetStateDescription,command.ModelGuids);
 
             CurrentWorkspace.RecordCreatedModel(preset);
         }
-        void SetWorkSpaceToStateImpl(ApplyPresetCommand command)
+        private void SetWorkSpaceToStateImpl(ApplyPresetCommand command)
         {
             var workspaceToSet = this.Workspaces.Where(x => x.Guid == command.WorkSpaceID).First();
             if (workspaceToSet == null)

src/DynamoCoreWpf/Commands/DynamoCommands.cs
@@ -222,8 +222,19 @@ namespace Dynamo.ViewModels
                         nodeId, command.PortIndex, command.Type);
                     break;
 
+                // TODO - can be removed in Dynamo 3.0 - DYN-1729
                 case DynamoModel.MakeConnectionCommand.Mode.EndAndStartCtrlConnection:
-                    CurrentSpaceViewModel.EndAndStartCtrlConnection(
+                    CurrentSpaceViewModel.BeginConnection(
+                        nodeId, command.PortIndex, command.Type);
+                    break;
+
+                case DynamoModel.MakeConnectionCommand.Mode.BeginDuplicateConnection:
+                    CurrentSpaceViewModel.BeginConnection(
+                        nodeId, command.PortIndex, command.Type);
+                    break;
+
+                case DynamoModel.MakeConnectionCommand.Mode.BeginCreateConnections:
+                    CurrentSpaceViewModel.BeginCreateConnections(
                         nodeId, command.PortIndex, command.Type);
                     break;
 

src/DynamoCore/Models/RecordableCommands.cs
@@ -1360,11 +1360,20 @@ namespace Dynamo.Models
                 /// <summary>
                 /// End and start control connections.
                 /// </summary>
+                [Obsolete("Replaced with BeginDuplicateConnection")]
                 EndAndStartCtrlConnection,
                 /// <summary>
                 /// Cancel connection.
                 /// </summary>
-                Cancel
+                Cancel,
+                /// <summary>
+                /// Begin duplicate connection.
+                /// </summary>
+                BeginDuplicateConnection,
+                /// <summary>
+                /// End current connection and create new connections.
+                /// </summary>
+                BeginCreateConnections
             }
 
             void setProperties(int portIndex, PortType portType, Mode mode)

src/DynamoCoreWpf/ViewModels/Core/StateMachine.cs
@@ -225,7 +225,7 @@ namespace Dynamo.ViewModels
             this.SetActiveConnectors(null);
         }
 
-        internal void EndAndStartCtrlConnection(Guid nodeId, int portIndex, PortType portType)
+        internal void BeginCreateConnections(Guid nodeId, int portIndex, PortType portType)
         {
             // Only handle ctrl connections if selected port is an input port
             if (firstStartPort == null || portType == PortType.Output) return; 
@@ -790,6 +790,11 @@ namespace Dynamo.ViewModels
                         multipleConnections = true;
                         mode = DynamoModel.MakeConnectionCommand.Mode.BeginShiftReconnections;
                     }
+                    else if (Keyboard.Modifiers == ModifierKeys.Control)
+                    {
+                        // If the control key is held down, check if there is a need to duplicate connections
+                        mode = DynamoModel.MakeConnectionCommand.Mode.BeginDuplicateConnection;
+                    }
 
                     var command = new DynamoModel.MakeConnectionCommand(nodeId, portIndex, portModel.PortType, mode);
                     owningWorkspace.DynamoViewModel.ExecuteCommand(command);
@@ -819,7 +824,7 @@ namespace Dynamo.ViewModels
                         }
                         else if (Keyboard.Modifiers == ModifierKeys.Control) // If the control key is held down
                         {
-                            mode = DynamoModel.MakeConnectionCommand.Mode.EndAndStartCtrlConnection;
+                            mode = DynamoModel.MakeConnectionCommand.Mode.BeginCreateConnections;
                             this.currentState = State.Connection; // Start a new connection
                             owningWorkspace.CurrentCursor = CursorLibrary.GetCursor(CursorSet.ArcSelect); // Reassign the cursor
                         }

test/DynamoCoreWpfTests/RecordedTests.cs
@@ -1155,6 +1155,62 @@ namespace DynamoCoreWpfTests
             Assert.AreEqual(2, workspace.Connectors.Count()); 
         }
 
+        /// <summary>
+        /// The following recorded test manually adds a connection between 2 nodes,
+        /// followed by ctrl + clicking to copy this connection to 2 additional nodes.
+        /// The final assertion will only be true if all connections were successfully established.
+        /// </summary>
+        [Test]
+        public void TestCtrlClickInputConnections()
+        {
+            RunCommandsFromFile("CtrlClickRecordedTest_Recording.xml");
+
+            Assert.AreEqual(6, workspace.Nodes.Count());
+            Assert.AreEqual(7, workspace.Connectors.Count()); // 2 of which should be executed in the recording using ctrl + click
+
+            // Sum of values which is only true if connections are valid downstream
+            AssertPreviewValue("995b61e54a7d4c0fae4973f74132bd1b", 12);
+        }
+
+        /// <summary>
+        /// The following recorded test manually adds a connecting between 2 nodes,
+        /// followed by ctrl + clicking to copy this connection to 2 additional nodes.
+        /// We then call 2 undo commands and verify only our original connection exists.
+        /// </summary>
+        [Test]
+        public void TestCtrlClickInputConnectionsUndo()
+        {
+            RunCommandsFromFile("CtrlClickRecordedTestUndo_Recording.xml");
+
+            Assert.AreEqual(4, workspace.Nodes.Count());
+            Assert.AreEqual(1, workspace.Connectors.Count()); // Only 1 connection should remain after undos
+
+            // A value of 25 is passed to all 3 downstream nodes however 2 undos will disconnect the trailing 2 nodes
+            AssertPreviewValue("4975adccfc57433aa915e67c03ff6503", 25);
+            AssertPreviewValue("0ddbbdf502344bb2ab5c75e6d6ac7b13", null);
+            AssertPreviewValue("eb839c03d115492d98a6e0d562d44d73", null);
+        }
+
+        /// <summary>
+        /// The following recorded test manually adds a connecting between 2 nodes,
+        /// followed by ctrl + clicking to copy this connection to 2 additional nodes.
+        /// We then call 2 undo commands followed by 2 redo commands and verify all 3 
+        /// connections exist.
+        /// </summary>
+        [Test]
+        public void TestCtrlClickInputConnectionsUndoRedo()
+        {
+            RunCommandsFromFile("CtrlClickRecordedTestUndoRedo_Recording.xml");
+
+            Assert.AreEqual(4, workspace.Nodes.Count());
+            Assert.AreEqual(3, workspace.Connectors.Count()); // All 3 connections should remain after 3 undos and 3 redos
+
+            // A value of 10 is passed to all 3 downstream nodes
+            AssertPreviewValue("3a998ea7ef284e47901db6cd13e77022", 10);
+            AssertPreviewValue("40c505cae1a14a3a9d7043c36c62b013", 10);
+            AssertPreviewValue("5a42348373b94a11920feeaa680834fd", 10);
+        }
+
         #endregion
 
         #region General Node Operations Test Cases

test/core/recorded/CtrlClickRecordedTestUndoRedo_Recording.xml
@@ -1,0 +1,90 @@
+<Commands ExitAfterPlayback="true" PauseAfterPlaybackInMs="10" CommandIntervalInMs="20">
+  <SelectModelCommand Modifiers="0">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </SelectModelCommand>
+  <CreateNodeCommand X="216.5" Y="226" DefaultPosition="false" TransformCoordinates="true">
+    <ModelGuid>29afdfb9-e0cb-45ab-b5c0-3a046f3ded1a</ModelGuid>
+    <Dynamo.Graph.Nodes.CodeBlockNodeModel guid="29afdfb9-e0cb-45ab-b5c0-3a046f3ded1a" type="Dynamo.Graph.Nodes.CodeBlockNodeModel" nickname="Code Block" x="124" y="195" isVisible="true" lacing="Disabled" isSelectedInput="False" isSelectedOutput="False" IsFrozen="false" isPinned="false" CodeText="10;" ShouldFocus="false">
+      <OutPortInfo LineIndex="0" />
+    </Dynamo.Graph.Nodes.CodeBlockNodeModel>
+  </CreateNodeCommand>
+  <SelectModelCommand Modifiers="0">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </SelectModelCommand>
+  <UpdateModelValueCommand Name="Code" Value="10" WorkspaceGuid="b16efd26-fc75-4829-b782-72d27614612c">
+    <ModelGuid>29afdfb9-e0cb-45ab-b5c0-3a046f3ded1a</ModelGuid>
+  </UpdateModelValueCommand>
+  <SelectModelCommand Modifiers="0">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </SelectModelCommand>
+  <CreateNodeCommand X="377.5" Y="93" DefaultPosition="false" TransformCoordinates="true">
+    <ModelGuid>3a998ea7-ef28-4e47-901d-b6cd13e77022</ModelGuid>
+    <Dynamo.Graph.Nodes.CodeBlockNodeModel guid="3a998ea7-ef28-4e47-901d-b6cd13e77022" type="Dynamo.Graph.Nodes.CodeBlockNodeModel" nickname="Code Block" x="285" y="62" isVisible="true" lacing="Disabled" isSelectedInput="False" isSelectedOutput="False" IsFrozen="false" isPinned="false" CodeText="a;" ShouldFocus="false">
+      <PortInfo index="0" default="False" name="a" />
+      <OutPortInfo LineIndex="0" />
+    </Dynamo.Graph.Nodes.CodeBlockNodeModel>
+  </CreateNodeCommand>
+  <SelectModelCommand Modifiers="0">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </SelectModelCommand>
+  <UpdateModelValueCommand Name="Code" Value="a" WorkspaceGuid="b16efd26-fc75-4829-b782-72d27614612c">
+    <ModelGuid>3a998ea7-ef28-4e47-901d-b6cd13e77022</ModelGuid>
+  </UpdateModelValueCommand>
+  <SelectModelCommand Modifiers="0">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </SelectModelCommand>
+  <CreateNodeCommand X="366.5" Y="222" DefaultPosition="false" TransformCoordinates="true">
+    <ModelGuid>40c505ca-e1a1-4a3a-9d70-43c36c62b013</ModelGuid>
+    <Dynamo.Graph.Nodes.CodeBlockNodeModel guid="40c505ca-e1a1-4a3a-9d70-43c36c62b013" type="Dynamo.Graph.Nodes.CodeBlockNodeModel" nickname="Code Block" x="274" y="191" isVisible="true" lacing="Disabled" isSelectedInput="False" isSelectedOutput="False" IsFrozen="false" isPinned="false" CodeText="b;" ShouldFocus="false">
+      <PortInfo index="0" default="False" name="b" />
+      <OutPortInfo LineIndex="0" />
+    </Dynamo.Graph.Nodes.CodeBlockNodeModel>
+  </CreateNodeCommand>
+  <SelectModelCommand Modifiers="0">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </SelectModelCommand>
+  <UpdateModelValueCommand Name="Code" Value="b" WorkspaceGuid="b16efd26-fc75-4829-b782-72d27614612c">
+    <ModelGuid>40c505ca-e1a1-4a3a-9d70-43c36c62b013</ModelGuid>
+  </UpdateModelValueCommand>
+  <SelectInRegionCommand X="597.5" Y="175" Width="0" Height="0" IsCrossSelection="false" />
+  <SelectModelCommand Modifiers="0">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </SelectModelCommand>
+  <CreateNodeCommand X="342.5" Y="356" DefaultPosition="false" TransformCoordinates="true">
+    <ModelGuid>5a423483-73b9-4a11-920f-eeaa680834fd</ModelGuid>
+    <Dynamo.Graph.Nodes.CodeBlockNodeModel guid="5a423483-73b9-4a11-920f-eeaa680834fd" type="Dynamo.Graph.Nodes.CodeBlockNodeModel" nickname="Code Block" x="250" y="325" isVisible="true" lacing="Disabled" isSelectedInput="False" isSelectedOutput="False" IsFrozen="false" isPinned="false" CodeText="c;" ShouldFocus="false">
+      <PortInfo index="0" default="False" name="c" />
+      <OutPortInfo LineIndex="0" />
+    </Dynamo.Graph.Nodes.CodeBlockNodeModel>
+  </CreateNodeCommand>
+  <SelectModelCommand Modifiers="0">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </SelectModelCommand>
+  <UpdateModelValueCommand Name="Code" Value="c" WorkspaceGuid="b16efd26-fc75-4829-b782-72d27614612c">
+    <ModelGuid>5a423483-73b9-4a11-920f-eeaa680834fd</ModelGuid>
+  </UpdateModelValueCommand>
+  <MakeConnectionCommand PortIndex="0" Type="1" ConnectionMode="0">
+    <ModelGuid>29afdfb9-e0cb-45ab-b5c0-3a046f3ded1a</ModelGuid>
+  </MakeConnectionCommand>
+  <MakeConnectionCommand PortIndex="0" Type="0" ConnectionMode="1">
+    <ModelGuid>3a998ea7-ef28-4e47-901d-b6cd13e77022</ModelGuid>
+  </MakeConnectionCommand>
+  <MakeConnectionCommand PortIndex="0" Type="0" ConnectionMode="6">
+    <ModelGuid>3a998ea7-ef28-4e47-901d-b6cd13e77022</ModelGuid>
+  </MakeConnectionCommand>
+  <MakeConnectionCommand PortIndex="0" Type="0" ConnectionMode="7">
+    <ModelGuid>40c505ca-e1a1-4a3a-9d70-43c36c62b013</ModelGuid>
+  </MakeConnectionCommand>
+  <MakeConnectionCommand PortIndex="0" Type="0" ConnectionMode="7">
+    <ModelGuid>5a423483-73b9-4a11-920f-eeaa680834fd</ModelGuid>
+  </MakeConnectionCommand>
+  <MakeConnectionCommand PortIndex="-1" Type="0" ConnectionMode="5">
+    <ModelGuid>00000000-0000-0000-0000-000000000000</ModelGuid>
+  </MakeConnectionCommand>
+  <UndoRedoCommand CmdOperation="0" />
+  <UndoRedoCommand CmdOperation="0" />
+  <UndoRedoCommand CmdOperation="0" />
+  <UndoRedoCommand CmdOperation="1" />
+  <UndoRedoCommand CmdOperation="1" />
+  <UndoRedoCommand CmdOperation="1" />
+</Commands>

commit 93f87c0d4d0d632df4f28b343981051e9c8189fc

address bug introduced in #9567

src/DynamoCore/Models/DynamoModelCommands.cs
@@ -273,7 +273,7 @@ namespace Dynamo.Models
 
                 // TODO - can be removed in Dynamo 3.0 - DYN-1729
                 case MakeConnectionCommand.Mode.EndAndStartCtrlConnection:
-                    BeginDuplicateConnection(nodeId, command.PortIndex, command.Type);
+                    BeginCreateConnections(nodeId, command.PortIndex, command.Type);
                     break;
 
                 case MakeConnectionCommand.Mode.BeginDuplicateConnection:

src/DynamoCoreWpf/Commands/DynamoCommands.cs
@@ -224,7 +224,7 @@ namespace Dynamo.ViewModels
 
                 // TODO - can be removed in Dynamo 3.0 - DYN-1729
                 case DynamoModel.MakeConnectionCommand.Mode.EndAndStartCtrlConnection:
-                    CurrentSpaceViewModel.BeginConnection(
+                    CurrentSpaceViewModel.BeginCreateConnections(
                         nodeId, command.PortIndex, command.Type);
                     break;
 