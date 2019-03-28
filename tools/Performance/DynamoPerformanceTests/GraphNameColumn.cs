ef533cd0eef7c7805647c7ce08af0188f89636a8
Scott Mitchell <scott.a.mitchell13@gmail.com>
2019-03-28 15:23:10 -0400

    Documentation for GraphNameColumn

--- tools/Performance/DynamoPerformanceTests/GraphNameColumn.cs
+++ tools/Performance/DynamoPerformanceTests/GraphNameColumn.cs
@@ -11,12 +11,22 @@ using BenchmarkDotNet.Running;
 
 namespace DynamoPerformanceTests
 {
+    /// <summary>
+    /// This column displays the file name (without extension) of performacnce test Dynamo graphs.
+    /// </summary>
     public class GraphNameColumn : IColumn
     {
-        public GraphNameColumn() { }
-        
+        /// <summary>
+        /// The name of this column -- becomes column header in results table.
+        /// </summary>
         public string ColumnName => "Graph";
 
+        /// <summary>
+        /// Get the file name of the Dynamo graph used in a benchmark.
+        /// </summary>
+        /// <param name="summary"></param>
+        /// <param name="benchmarkCase"></param>
+        /// <returns>File name of the Dynamo graph used in benchmark</returns>
         public string GetValue(Summary summary, BenchmarkCase benchmarkCase)
         {
             var path = benchmarkCase.Parameters["DynamoFilePath"].ToString();
@@ -24,17 +34,38 @@ namespace DynamoPerformanceTests
             return Path.GetFileNameWithoutExtension(fi.Name);
         }
 
+        /// <summary>
+        /// Get the file name of the Dynamo graph used in a benchmark.
+        /// </summary>
+        /// <param name="summary"></param>
+        /// <param name="benchmarkCase"></param>
+        /// <param name="style"></param>
+        /// <returns>File name of the Dynamo graph used in benchmark</returns>
         public string GetValue(Summary summary, BenchmarkCase benchmarkCase, SummaryStyle style) => GetValue(summary, benchmarkCase);
-        
+
+        /// <summary>
+        /// Explanation of column to appear in the results table legend.
+        /// </summary>
+        public string Legend => $"Name of test graph";
+
+        /// <summary>
+        /// Column Category for this column. 
+        /// Determines column order in results table.
+        /// </summary>
+        public ColumnCategory Category => ColumnCategory.Job;
+
+        /// <summary>
+        /// Column Category Priority for this column. 
+        /// Secondary determination of column order in results table.
+        /// </summary>
+        public int PriorityInCategory => 0;
+
         public string Id => nameof(GraphNameColumn);
         public bool IsDefault(Summary summary, BenchmarkCase benchmarkCase) => true;
         public bool IsAvailable(Summary summary) => true;
         public bool AlwaysShow => true;
-        public ColumnCategory Category => ColumnCategory.Job;
-        public int PriorityInCategory => 0;
         public bool IsNumeric => false;
         public UnitType UnitType => UnitType.Dimensionless;
-        public string Legend => $"Name of test graph";
         public override string ToString() => ColumnName;
     }
 }
