efbfe18b6207aaa5ba09980401bb2c65e16b1495
Scott Mitchell <scott.a.mitchell13@gmail.com>
2019-04-07 22:37:45 -0400

    Save v1

--- tools/Performance/DynamoPerformanceTests/ResultsComparer.cs
+++ tools/Performance/DynamoPerformanceTests/ResultsComparer.cs
@@ -17,6 +17,17 @@ namespace DynamoPerformanceTests
     /// </summary>
     public class ResultsComparer
     {
+        private int numColumns = 6;
+
+        private string[] ColumnNames = new string[] { "Method", "Graph", "Version", "Mean", "Error", "StdDev" };
+
+        private class ComparisonData
+        {
+            public Dictionary<string, object> Base;
+
+            public Dictionary<string, object> Diff;
+        }
+
         private class BenchmarkResult
         {
             /// <summary>
@@ -62,6 +73,7 @@ namespace DynamoPerformanceTests
             DiffResults = ImportResultsCSV(diffPath);
 
             Compare();
+            LogResults();
         }
 
         /// <summary>
@@ -78,31 +90,84 @@ namespace DynamoPerformanceTests
                 {
                     Method = result.Method,
                     Graph = result.Graph,
-                    Mean = result.Mean / baseResult.Mean,
-                    Error = result.Error / baseResult.Error,
-                    StdDev = result.StdDev / baseResult.StdDev,
+                    Mean = Math.Round(100 * result.Mean / baseResult.Mean, 2),
+                    Error = Math.Round(100 * result.Error / baseResult.Error, 2),
+                    StdDev = Math.Round(100 * result.StdDev / baseResult.StdDev, 2),
                 };
             }
         }
 
-        public void WriteResultsCSV(string filePath)
+        private List<string[]> GetComparisonDataRows()
         {
-            var csv = new StringBuilder();
-            csv.AppendLine("Method,Graph,Version,Mean,Error,StdDev");
-            foreach(var result in DiffResults.Values)
+            var rows = new List<string[]>();
+            rows.Add(ColumnNames);
+
+            foreach (var result in DiffResults.Values)
             {
                 var baseResult = GetBaseResult(result);
                 var baseLine = new string[] { result.Method, result.Graph, "Base", baseResult.Mean.ToString(), baseResult.Error.ToString(), baseResult.StdDev.ToString() };
-                csv.AppendLine(string.Join(",", baseLine));
+                rows.Add(baseLine);
 
                 var diffLine = new string[] { "", "", "Diff", result.Mean.ToString(), result.Error.ToString(), result.StdDev.ToString() };
-                csv.AppendLine(string.Join(",", diffLine));
+                rows.Add(diffLine);
 
                 var compResult = Comparison[result.Method + result.Graph];
-                var compLine = new string[] { "", "", "", compResult.Mean.ToString(), compResult.Error.ToString(), compResult.StdDev.ToString() };
-                csv.AppendLine(string.Join(",", compLine));
+                var compLine = new string[] { "", "", compResult.Mean < 100 ? "+" : "-", compResult.Mean.ToString() + "%", compResult.Error.ToString() + "%", compResult.StdDev.ToString() + "%" };
+                rows.Add(compLine);
+            }
+            return rows;
+        }
+
+        private List<string>[] GetComparisonDataColumns()
+        {
+            var columns = new List<string>[numColumns];
+            for (int i = 0; i < numColumns; i++)
+            {
+                columns[i] = new List<string>();
             }
 
+            var rows = GetComparisonDataRows();
+            foreach(var row in rows)
+            {
+                for (int i = 0; i < numColumns; i++)
+                {
+                    columns[i].Add(row[i]);
+                }
+            }
+            return columns;
+        }
+
+        private void LogResults()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            var columns = GetComparisonDataColumns();
+            var columnWidths = columns.Select(c => c.OrderByDescending(s => s.Length).First().Length + 1).ToList();
+            for (int i = 0; i < columns[0].Count; i++)
+            {
+                if ( i % 3 == 1)
+                {
+                    //Console.WriteLine("".PadLeft(columnWidths.Sum()+ 13, '-'));
+                }
+                Console.Write("|");
+                for (int j = 0; j < numColumns; j++)
+                {
+                    var dataItem = columns[j][i].PadLeft(columnWidths[j], ' ');
+                    dataItem += " |";
+                    Console.Write(dataItem);
+                }
+                Console.Write("\n");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+
+        public void WriteResultsCSV(string filePath)
+        {
+            var rows = GetComparisonDataRows().Select(r => string.Join(",", r));
+            var csv = new StringBuilder();
+            foreach (var row in rows) csv.AppendLine(row);
+            
             filePath = GetFullPath(filePath);
             filePath += "/comparison.csv";
             File.WriteAllText(filePath, csv.ToString());
@@ -126,6 +191,21 @@ namespace DynamoPerformanceTests
             parser.SetDelimiters(",");
 
             var header = parser.ReadFields();
+            var columnIndices = ColumnNames.Select(c => Array.IndexOf(header, c)).ToList();
+            if (columnIndices.Where(i => i == -1).Count() > 0)
+            {
+                var missingColumns = new List<string>();
+                for(int i = 0; i < ColumnNames.Count(); i++)
+                {
+                    if (columnIndices[i] == -1)
+                    {
+                        missingColumns.Add(ColumnNames[i]);
+                    }
+                }
+                var message = string.Format("The csv file at {0} does not contain the following required columns: {1}.", csvPath, string.Join(", ", missingColumns));
+                throw new Exception(message);
+            }
+
             var iMethod = Array.IndexOf(header, "Method");
             var iGraph = Array.IndexOf(header, "Graph");
             var iMean = Array.IndexOf(header, "Mean");
@@ -133,9 +213,20 @@ namespace DynamoPerformanceTests
             var iStdDev = Array.IndexOf(header, "StdDev");
 
             var benchmarkResults = new Dictionary<string, BenchmarkResult>();
+
+            var data = new Dictionary<string, IEnumerable<string>>();
             while (!parser.EndOfData)
             {
+                // Get full benchmark data from csv
                 var brLine = parser.ReadFields();
+
+                // Get only the values that we care about
+                var valuesToKeep = new List<string>();
+                foreach (var i in columnIndices) valuesToKeep.Add(brLine[i]);
+
+                // Store the benchmark based on its Method and Graph values
+                data[valuesToKeep[0] + valuesToKeep[1]] = valuesToKeep;
+
                 var method = brLine[iMethod];
                 var graph = brLine[iGraph];
                 var mean = Convert.ToDouble(Regex.Replace(brLine[iMean], "[^0-9|.|,]", ""), CultureInfo.InvariantCulture);
