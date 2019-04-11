95e93d208451a60ca6f33dbb92df63b65c28648d
reddyashish <43763136+reddyashish@users.noreply.github.com>
2019-04-11 13:29:09 -0400

    Removing the irrelevant test

--- test/DynamoCoreWpfTests/RunSettingsTests.cs
+++ test/DynamoCoreWpfTests/RunSettingsTests.cs
@@ -149,26 +149,6 @@ namespace DynamoCoreWpfTests
             Assert.AreEqual(homeSpace.RunSettings.RunPeriod, 10);
         }
 
-        // This test is now irrelevant due to the follow fix:
-        //   https://github.com/DynamoDS/Dynamo/pull/4674
-        // 
-        [Test, Ignore]
-        [Category("Failure")]
-        public void RunSettingsDisableRun()
-        {
-            string openPath = Path.Combine(workingDirectory, @"..\..\..\test\core\math\Add.dyn");
-
-            Model.OpenFileFromPath(openPath);
-            var homeSpace = GetHomeSpace();
-            homeSpace.RunSettings.RunEnabled = false;
-            homeSpace.Run();
-
-            string varname = GetVarName("4c5889ac-7b91-4fb5-aaad-a2128b533279");
-            var mirror = GetRuntimeMirror(varname);
-
-            Assert.IsNull(mirror);
-        }
-
         private RoutedEventArgs GetKeyboardEnterEventArgs(Visual visual)
         {
             var routedEvent = Keyboard.KeyDownEvent;
