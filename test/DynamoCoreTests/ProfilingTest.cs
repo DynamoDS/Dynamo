3fc8b742f8083b31caf9e2ef1f4d66374e6dc4ca
ColinDayOrg <colin.day@autodesk.com>
2019-04-19 14:44:19 -0400

    Add test for pre-run, post-run, and node deletion post-run

---
+++ test/DynamoCoreTests/ProfilingTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Dynamo.Engine.CodeGeneration;
+using Dynamo.Graph;
+using Dynamo.Graph.Nodes;
+using Dynamo.Models;
+using NUnit.Framework;
+
+namespace Dynamo.Tests
+{
+    [Category("DSExecution")]
+    class ProfilingTest : DynamoModelTestBase
+    {
+        protected override void GetLibrariesToPreload(List<string> libraries)
+        {
+            libraries.Add("ProtoGeometry.dll");
+            libraries.Add("DSCoreNodes.dll");
+            libraries.Add("VMDataBridge.dll");
+            base.GetLibrariesToPreload(libraries);
+        }
+
+        [Test]
+        public void TestProfilingSingleNode()
+        {
+            // Note: This test file is saved in manual run mode
+            string openPath = Path.Combine(TestDirectory, @"core\profiling\SingleNode.dyn");
+            OpenModel(openPath);
+
+            var profilingData = CurrentDynamoModel.EngineController.ProfilingData;
+            var node = CurrentDynamoModel.CurrentWorkspace.Nodes.FirstOrDefault();
+
+            Assert.IsNull(profilingData.TotalExecutionTime);
+            Assert.IsNull(profilingData.NodeExecutionTime(node));
+
+            BeginRun();
+
+            Assert.IsNotNull(profilingData.TotalExecutionTime);
+            Assert.Greater(profilingData.TotalExecutionTime?.Ticks, 0);
+            Assert.IsNotNull(profilingData.NodeExecutionTime(node));
+            Assert.Greater(profilingData.NodeExecutionTime(node)?.Ticks, 0);
+
+            this.CurrentDynamoModel.ExecuteCommand(
+                new DynamoModel.DeleteModelCommand(node.GUID));
+            BeginRun();
+
+            Assert.IsNotNull(profilingData.TotalExecutionTime);
+            Assert.Greater(profilingData.TotalExecutionTime?.Ticks, 0);
+            Assert.IsNull(profilingData.NodeExecutionTime(node));
+        }
+    }
+}
