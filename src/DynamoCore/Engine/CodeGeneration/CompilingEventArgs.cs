6eb5cc5b4bdad13dd2fb660b19c7bb3d2e685ec2
ColinDayOrg <colin.day@autodesk.com>
2019-05-03 08:43:36 -0400

    Obsolete Node property getter and add NodeId property getter

--- src/DynamoCore/Engine/CodeGeneration/CompilingEventArgs.cs
+++ src/DynamoCore/Engine/CodeGeneration/CompilingEventArgs.cs
@@ -7,15 +7,35 @@ namespace Dynamo.Engine.CodeGeneration
     /// </summary>
     public class CompilingEventArgs : EventArgs
     {
+        private Guid nodeId;
+
         /// Construct ASTBuildingEventArgs with NodeModel.
         public CompilingEventArgs(Guid node)
         {
-            Node = node;
+            nodeId = node;
         }
 
         /// <summary>
         /// Guid of NodeModel that is being compiled to AST.
         /// </summary>
-        public Guid Node { get; private set; }
+        [Obsolete("This item is being obsoleted due to the incorrect namimg, the new property to use is NodeId")]
+        public Guid Node
+        {
+            get
+            {
+                return nodeId;
+            }
+        }
+
+        /// <summary>
+        /// Guid of NodeModel that is being compiled to AST.
+        /// </summary>
+        public Guid NodeId
+        {
+            get
+            {
+                return nodeId;
+            }
+        }
     }
 }
