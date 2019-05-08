9caccd6377a52dff4aa817fc8db82483efcf0db6
ColinDayOrg <colin.day@autodesk.com>
2019-05-08 11:56:27 -0400

    Add the abillity to switch profiling on/off (off by default), including tests

--- test/DynamoCoreTests/ProfilingTest.cs
+++ test/DynamoCoreTests/ProfilingTest.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-using Dynamo.Engine.CodeGeneration;
-using Dynamo.Graph;
-using Dynamo.Graph.Nodes;
+using Dynamo.Graph.Workspaces;
 using Dynamo.Models;
 using NUnit.Framework;
 
@@ -29,15 +26,30 @@ namespace Dynamo.Tests
             string openPath = Path.Combine(TestDirectory, @"core\profiling\SingleNode.dyn");
             OpenModel(openPath);
 
-            Assert.IsNotNull(CurrentDynamoModel.EngineController.profilingSession);
-            var profilingData = CurrentDynamoModel.EngineController.profilingSession.ProfilingData;
-            var node = CurrentDynamoModel.CurrentWorkspace.Nodes.FirstOrDefault();
+            // Assert that profiling is disabled by default
+            var engineController = CurrentDynamoModel.EngineController;
+            Assert.IsNull(engineController.profilingSession);
 
+            // Assert that no profiling data is created after a run when profiling is disabled
+            BeginRun();
+            Assert.IsNull(engineController.profilingSession);
+
+            var homeWorkspace = CurrentDynamoModel.Workspaces.OfType<HomeWorkspaceModel>().FirstOrDefault();
+            var nodes = CurrentDynamoModel.CurrentWorkspace.Nodes;
+
+            // Assert that profiling can be enabled
+            engineController.EnableProfiling(true, homeWorkspace, nodes);
+            Assert.IsNotNull(engineController.profilingSession);
+
+            // Assert that there is no profiling data before the graph is run
+            var profilingData = engineController.profilingSession.ProfilingData;
+            var node = nodes.FirstOrDefault();
             Assert.IsNull(profilingData.TotalExecutionTime);
             Assert.IsNull(profilingData.NodeExecutionTime(node));
 
             BeginRun();
 
+            // Assert that profiling data exists after a run occurs
             Assert.IsNotNull(profilingData.TotalExecutionTime);
             Assert.Greater(profilingData.TotalExecutionTime?.Ticks, 0);
             Assert.IsNotNull(profilingData.NodeExecutionTime(node));
@@ -47,9 +59,14 @@ namespace Dynamo.Tests
                 new DynamoModel.DeleteModelCommand(node.GUID));
             BeginRun();
 
+            // Assert that per-node profiling data is deleted after a node is deleted
             Assert.IsNotNull(profilingData.TotalExecutionTime);
             Assert.Greater(profilingData.TotalExecutionTime?.Ticks, 0);
             Assert.IsNull(profilingData.NodeExecutionTime(node));
+
+            // Assert that profiling can be disabled
+            engineController.EnableProfiling(false, homeWorkspace, nodes);
+            Assert.IsNull(engineController.profilingSession);
         }
     }
 }
