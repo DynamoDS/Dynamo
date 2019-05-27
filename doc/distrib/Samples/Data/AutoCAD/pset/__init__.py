810f3c2d2c2e5ea075c887db03cfeb256573aad8
Angelo Wang <angelo.wang@autodesk.com>
2019-05-27 16:35:13 +0800

    [Samples] Add dynamo for civil 3d samples
    Both Data and en-US folders are orgnanized in subfolders AutoCAD and Civil 3D. For Data, this helps resolving potential future file naming conflicts.

---
+++ doc/distrib/Samples/Data/AutoCAD/pset/__init__.py
@@ -0,0 +1,9 @@
+"""
+Copyright 2018 Autodesk, Inc. All rights reserved.
+
+This file is part of the Civil 3D Python Module.
+
+"""
+__author__ = 'Paolo Emilio Serra - paolo.serra@autodesk.com'
+__copyright__ = '2018'
+__version__ = '1.0.0'
