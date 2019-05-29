75cd36e561cb8abff17bc39cb19a927da301c33a
aparajit-pratap <aparajit.pratap@autodesk.com>
2019-05-29 13:29:40 -0400

    update tests

--- test/Engine/ProtoTest/Associative/MicroFeatureTests.cs
+++ test/Engine/ProtoTest/Associative/MicroFeatureTests.cs
@@ -3675,11 +3675,11 @@ r = foo(3);
         public void TestDoubleEquality()
         {
             string code = @"
-a = 1/3 == 0.333333333;
-b = 1/3 == 0.33333333;
+a = 1/3 == 0.33333;
+b = 1/3 == 0.3333;
 c = 3.14159265358979;
-d = c == c + 0.0000000001;
-e = c == c + 0.000000001;
+d = c == c + 0.000001;
+e = c == c + 0.00001;
 ";
             thisTest.RunScriptSource(code);
             thisTest.VerifyBuildWarningCount(0);
