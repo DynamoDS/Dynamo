bbde5d94becd28ce042511f6d40731779e48f38d
manuel saldivar <manuel.saldivar@globant.com>
2019-06-06 18:00:49 -0500

    Add changes that mike request.

--- src/Tools/TransformXMLToJSONUsingXSLT/Program.cs
+++ src/Tools/TransformXMLToJSONUsingXSLT/Program.cs
@@ -15,7 +15,7 @@ namespace TransformXMLToJSONUsingXSLT
         static string DirectoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName);
         static string xmlFilePathResult = ConfigurationManager.AppSettings["xmlFilePathResult"];
         static string xsltFile = ConfigurationManager.AppSettings["xsltFile"];
-        static string textFile = ConfigurationManager.AppSettings["textFile"];
+        static string textFileComplete = ConfigurationManager.AppSettings["textFileComplete"];
         static string textFileWithFilters = ConfigurationManager.AppSettings["textFileWithFilters"];
         static int applyFilters = int.Parse(ConfigurationManager.AppSettings["applyFilters"].ToString());
 
@@ -27,36 +27,44 @@ namespace TransformXMLToJSONUsingXSLT
         }
         public static void XSLTToAnyType()
         {
-            XslCompiledTransform xslCompiledTransform = new XslCompiledTransform();
-            xslCompiledTransform.Load(String.Format(@"{0}\XSLT\{1}", DirectoryPath, xsltFile));
-            StringBuilder stringBuilder = new StringBuilder();
-            StringWriter stringWriter = new StringWriter(stringBuilder);
-            xslCompiledTransform.Transform(String.Format(@"{0}\XML\{1}", DirectoryPath, xmlFilePathResult), null, stringWriter);
-            stringWriter.Close();
-            List<String> lstTestFixtureName = new List<String>();
-
-            if (!Directory.Exists(String.Format(@"{0}\Result", DirectoryPath)))
-                Directory.CreateDirectory(String.Format(@"{0}\Result", DirectoryPath));
-
-            if (File.Exists(String.Format(@"{0}\Result\{1}", DirectoryPath, textFile)))
-                File.Delete(String.Format(@"{0}\Result\{1}", DirectoryPath, textFile));
-
-            File.AppendAllText(String.Format(@"{0}\Result\{1}", DirectoryPath, textFile), stringBuilder.ToString());
-
-            if (File.Exists(String.Format(@"{0}\Result\{1}", DirectoryPath, textFile)) && applyFilters == 1)
+            try
             {
-                NameValueCollection section = (NameValueCollection)ConfigurationManager.GetSection("Filters");
-                var allLines = File.ReadAllLines(String.Format(@"{0}\Result\{1}", DirectoryPath, textFile));
+                XslCompiledTransform xslCompiledTransform = new XslCompiledTransform();
+                xslCompiledTransform.Load(String.Format(@"{0}\XSLT\{1}", DirectoryPath, xsltFile));
+                StringBuilder stringBuilder = new StringBuilder();
+                StringWriter stringWriter = new StringWriter(stringBuilder);
+                xslCompiledTransform.Transform(String.Format(@"{0}\XML\{1}", DirectoryPath, xmlFilePathResult), null, stringWriter);
+                stringWriter.Close();
+                List<String> lstTestFixtureName = new List<String>();
 
-                foreach (String filter in section)
+                if (!Directory.Exists(String.Format(@"{0}\Result", DirectoryPath)))
+                    Directory.CreateDirectory(String.Format(@"{0}\Result", DirectoryPath));
+
+                if (File.Exists(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileComplete)))
+                    File.Delete(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileComplete));
+
+                File.AppendAllText(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileComplete), stringBuilder.ToString());
+
+                if (File.Exists(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileComplete)) && applyFilters == 1)
                 {
-                    lstTestFixtureName.AddRange(allLines.Where(l => l.Split(new char[] { ',' })[0].ToString() == filter));
+                    NameValueCollection section = (NameValueCollection)ConfigurationManager.GetSection("Filters");
+                    var allLines = File.ReadAllLines(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileComplete));
+
+                    foreach (String filter in section)
+                    {
+                        lstTestFixtureName.AddRange(allLines.Where(l => l.Split(new char[] { ',' })[0].ToString() == filter));
+                    }
+                    if (File.Exists(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileWithFilters)))
+                        File.Delete(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileWithFilters));
+
+
+                    File.AppendAllLines(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileWithFilters), lstTestFixtureName);
                 }
-                if (File.Exists(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileWithFilters)))
-                    File.Delete(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileWithFilters));
-
-
-                File.AppendAllLines(String.Format(@"{0}\Result\{1}", DirectoryPath, textFileWithFilters), lstTestFixtureName);
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
             }
 
         }
