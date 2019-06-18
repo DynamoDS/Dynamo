43e501ed3b924bfc545c26c7e690a9e208b3fce2
Aaron (Qilong) <aaron.tang@autodesk.com>
2019-06-18 02:05:45 -0400

    Dynamo Package Dependency View Extension (#9787)
    * Initial Commit
    
    * Update csproj to use project config imports
    
    * Add Data bind
    
    * Workspace Open with package info
    
    * adjust styling and make cells read only
    
    * adjust styling
    
    * reduce package cell width
    
    * Code clean Up
    
    1. Using ready param instead of data context
    2. Exposed workspace cleared event for extension to watch for and clear dependency data
    
    * Add Property watch for Package Dependencies
    
    * use name of
    
    * Some UI tunning
    
    * Styling changes
    
    * Using View injection API
    
    * grid styling
    
    * Highlight packages not installed
    
    * Add Download Button and view injection Menu Item
    
    * Some comments missing
    
    * code clean up
    
    * Address Comments
    
    * UI Adjustment
    
    * Update unsubscribe logic
    
    * More comments
    
    * Fix Unit Tests

--- src/DynamoCore/Graph/Workspaces/WorkspaceModel.cs
+++ src/DynamoCore/Graph/Workspaces/WorkspaceModel.cs
@@ -33,6 +33,10 @@ using ProtoCore.Namespace;
 // For workspace package dependency collection
 [assembly: InternalsVisibleTo("DynamoPackages")]
 
+// Internals are visible to the Package Manager extension
+// For workspace package dependency collection
+[assembly: InternalsVisibleTo("PackageDependencyViewExtension")]
+
 namespace Dynamo.Graph.Workspaces
 {
     /// <summary>
@@ -649,6 +653,7 @@ namespace Dynamo.Graph.Workspaces
             set
             {
                 packageDependencies = value;
+                RaisePropertyChanged(nameof(PackageDependencies));
             }
         }
 
---
+++ src/PackageDependencyViewExtension/PackageDependencyView.xaml.cs
@@ -0,0 +1,238 @@
+﻿using Dynamo.Graph.Workspaces;
+using Dynamo.Wpf.Extensions;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Dynamo.PackageDependency
+{
+    /// <summary>
+    /// Interaction logic for PackageDependencyView.xaml
+    /// </summary>
+    public partial class PackageDependencyView : UserControl
+    {
+        private DependencyTable table = new DependencyTable();
+
+        private WorkspaceModel currentWorkspace;
+
+        /// <summary>
+        /// Event handler for workspaceAdded event
+        /// </summary>
+        /// <param name="obj">workspace model</param>
+        internal void OnWorkspaceChanged(IWorkspaceModel obj)
+        {
+            if (obj is WorkspaceModel)
+            {
+                // Unsubscribe
+                if (currentWorkspace != null)
+                {
+                    currentWorkspace.PropertyChanged -= OnWorkspacePropertyChanged;
+                }
+                DependencyRegen(obj as WorkspaceModel);
+                // Update current workspace
+                currentWorkspace = obj as WorkspaceModel;
+                currentWorkspace.PropertyChanged += OnWorkspacePropertyChanged;
+            }
+        }
+
+        /// <summary>
+        /// Event handler for workspaceRemoved event
+        /// </summary>
+        /// <param name="obj">workspace model</param>
+        internal void OnWorkspaceCleared(IWorkspaceModel obj)
+        {
+            if (obj is WorkspaceModel)
+            {
+                // Clear the dependency table.
+                table.Columns.Clear();
+            }
+        }
+
+        private void OnWorkspacePropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == nameof(currentWorkspace.PackageDependencies))
+                DependencyRegen(currentWorkspace);
+        }
+
+        /// <summary>
+        /// Regenerate dependency table
+        /// </summary>
+        /// <param name="ws">workspace model</param>
+        internal void DependencyRegen(WorkspaceModel ws)
+        {
+            // Clear the dependency table.
+            table.Columns.Clear();
+            foreach (var package in ws.PackageDependencies)
+            {
+                bool matchFound = false;
+                // Check if the target package is installed and loaded.
+                foreach (var loadedPackage in ws.LoadedPackageDependencies)
+                {
+                    if (package.Equals(loadedPackage))
+                    {
+                        table.Columns.Add(new Column()
+                        {
+                            ColumnsData = new ObservableCollection<ColumnData>()
+                            {
+                                new ColumnData(package.Name),
+                                new ColumnData(package.Version.ToString(), 100)
+                            }
+                        });
+                        matchFound = true;
+                        continue;
+                    }
+                }
+                // TODO: Not ideal! O(N * M) complexicty, would like LoadedPackageDependencies to be a dictionary or something with constant package name search time
+                if (!matchFound)
+                {
+                    table.Columns.Add(new Column()
+                    {
+                        ColumnsData = new ObservableCollection<ColumnData>()
+                        {
+                            new ColumnData(package.Name, ColumnData.WarningColor),
+                            new ColumnData(package.Version.ToString(), 100)
+                        }
+                    });
+                }
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="p">ViewLoadedParams</param>
+        public PackageDependencyView(ViewLoadedParams p)
+        {
+            InitializeComponent();
+            DataContext = table;
+            currentWorkspace = p.CurrentWorkspaceModel as WorkspaceModel;
+            p.CurrentWorkspaceChanged += OnWorkspaceChanged;
+            p.CurrentWorkspaceCleared += OnWorkspaceCleared;
+            currentWorkspace.PropertyChanged += OnWorkspacePropertyChanged;
+        }
+    }
+
+    /// <summary>
+    /// Every table line
+    /// </summary>
+    public class Column
+    {
+        public ObservableCollection<ColumnData> ColumnsData
+        {
+            get; set;
+        }
+    }
+
+    /// <summary>
+    /// Class defining data for each column
+    /// </summary>
+    public class ColumnData
+    {
+        static int DefaultWidth = 200;
+        static SolidColorBrush DefaultColor = (SolidColorBrush)(new BrushConverter().ConvertFrom("#aaaaaa"));
+        public static Brush WarningColor = Brushes.Red;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="data"></param>
+        public ColumnData(string data)
+        {
+            Data = data;
+            Width = DefaultWidth;
+            Color = DefaultColor;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="width"></param>
+        public ColumnData(string data, int width)
+        {
+            Data = data;
+            Width = width;
+            Color = DefaultColor;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="color"></param>
+        public ColumnData(string data, Brush color)
+        {
+            Data = data;
+            Width = DefaultWidth;
+            Color = color;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="width"></param>
+        /// <param name="brush"></param>
+        public ColumnData(string data, int width, Brush brush)
+        {
+            Data = data;
+            Width = width;
+            Color = brush;
+        }
+
+        /// <summary>
+        /// Data in each cell
+        /// </summary>
+        public string Data
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Width of each cell
+        /// </summary>
+        public int Width
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Foreground color of each cell
+        /// </summary>
+        public Brush Color
+        {
+            get; set;
+        }
+    }
+
+    /// <summary>
+    /// The data binding table holding all the dependency info
+    /// </summary>
+    public class DependencyTable
+    {
+        public ObservableCollection<Column> Columns
+        { get; set; }
+
+        public ObservableCollection<ColumnData> Headers
+        { get; set; }
+
+        public DependencyTable()
+        {
+            Columns = new ObservableCollection<Column>();
+            Headers = new ObservableCollection<ColumnData>();
+
+            Columns.Add(new Column()
+            {
+                ColumnsData = new ObservableCollection<ColumnData>()
+                    {
+                        new ColumnData("DummyPackage"),
+                        new ColumnData("1.0.0")
+                    }
+            });
+
+            Headers.Add(new ColumnData("Package Name"));
+            Headers.Add(new ColumnData("Version"));
+        }
+    }
+}
