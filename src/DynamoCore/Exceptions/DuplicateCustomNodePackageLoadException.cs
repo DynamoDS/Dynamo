eacd25ca72f82194ff6ef4a7167dc384e5d81554
aparajit-pratap <aparajit.pratap@autodesk.com>
2019-06-25 00:50:11 -0400

    prevent duplicate custom node package from loading

---
+++ src/DynamoCore/Exceptions/DuplicateCustomNodePackageLoadException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.Exceptions
+{
+    public class DuplicateCustomNodePackageLoadException : Exception
+    {
+        /// <summary>
+        /// File path of failing custom node package.
+        /// </summary>
+        public string Path;
+
+        /// <summary>
+        /// Failure reason.
+        /// </summary>
+        public string Reason;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="reason"></param>
+        public DuplicateCustomNodePackageLoadException(string path, string reason)
+            : base(String.Format(Properties.Resources.FailedToLoad, path, reason))
+        {
+            Path = path;
+            Reason = reason;
+        }
+    }
+}
