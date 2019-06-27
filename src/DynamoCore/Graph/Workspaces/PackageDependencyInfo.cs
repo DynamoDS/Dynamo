ffed17082724cd4404f4dabfe3622486f577eb6c
Scott Mitchell <scott.a.mitchell13@gmail.com>
2019-06-27 08:52:05 -0400

    Refactor PackageDependencies property (#9803)
    * Refactor PackageDependencies property
    
    * Add test to verify multiple packages not listed as dependencies for one node
    
    * Keep original package dependency by default and add API to void original package dependency
    
    * Add comment
    
    * Move helper methods out of properties region
    
    * Fix IsLoaded logic
    
    * Update test .dyn to avoid serialization test failures
    
    * Exclude new dyn from serialization tests
    
    * Use preexisting method to exclude test dyn from serialization tests

--- src/DynamoCore/Graph/Workspaces/PackageDependencyInfo.cs
+++ src/DynamoCore/Graph/Workspaces/PackageDependencyInfo.cs
@@ -4,10 +4,9 @@ using System.Collections.Generic;
 namespace Dynamo.Graph.Workspaces
 {
     /// <summary>
-    /// Class containing info about a Dynamo package. 
-    /// Used for serialization.
+    /// Class containing info about a package
     /// </summary>
-    internal class PackageDependencyInfo
+    internal class PackageInfo
     {
         /// <summary>
         /// Name of the package
@@ -20,6 +19,86 @@ namespace Dynamo.Graph.Workspaces
         internal Version Version { get; set; }
 
         /// <summary>
+        /// Create a package info object from the package name and version
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="version"></param>
+        internal PackageInfo(string name, Version version)
+        {
+            Name = name;
+            Version = version;
+        }
+
+        /// <summary>
+        /// Checks whether two PackageInfos are equal
+        /// They are equal if their Name and Versions are equal
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is PackageInfo))
+            {
+                return false;
+            }
+
+            var other = obj as PackageInfo;
+            if (other.Name == this.Name && other.Version == this.Version)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the hashcode for this PackageInfo
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Version.GetHashCode();
+        }
+
+        /// <summary>
+        /// Get the string representing this PackageInfo
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name + ", Version=" + Version.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Class containing info about a workspace package dependency
+    /// </summary>
+    internal class PackageDependencyInfo
+    {
+        /// <summary>
+        /// PackageInfo for this package
+        /// </summary>
+        internal PackageInfo PackageInfo { get; set; }
+
+        /// <summary>
+        /// Name of the package
+        /// </summary>
+        internal string Name => PackageInfo.Name;
+
+        /// <summary>
+        /// Version of the package
+        /// </summary>
+        internal Version Version => PackageInfo.Version;
+
+        /// <summary>
+        /// Indicates whether this package is loaded in the current session
+        /// </summary>
+        internal bool IsLoaded { get; set; }
+
+        /// <summary>
         /// Guids of nodes in the workspace that are dependent on this package
         /// </summary>
         internal HashSet<Guid> Nodes
@@ -29,14 +108,23 @@ namespace Dynamo.Graph.Workspaces
         private HashSet<Guid> nodes;
         
         /// <summary>
-        /// Create a package info object from the package name and version
+        /// Create a package dependency from the package name and version
         /// </summary>
         /// <param name="name"></param>
         /// <param name="version"></param>
         internal PackageDependencyInfo(string name, Version version)
         {
-            Name = name;
-            Version = version;
+            PackageInfo = new PackageInfo(name, version);
+            nodes = new HashSet<Guid>();
+        }
+
+        /// <summary>
+        /// Create a package dependency from its package info
+        /// </summary>
+        /// <param name="packageInfo"></param>
+        internal PackageDependencyInfo(PackageInfo packageInfo)
+        {
+            PackageInfo = packageInfo;
             nodes = new HashSet<Guid>();
         }
 
@@ -71,7 +159,7 @@ namespace Dynamo.Graph.Workspaces
         }
 
         /// <summary>
-        /// Checks whether two PackageDependencyInfos are equal
+        /// Checks whether two PackageDependencyInfo instances are equal
         /// They are equal if their Name and Versions are equal
         /// </summary>
         /// <param name="obj"></param>
@@ -102,6 +190,15 @@ namespace Dynamo.Graph.Workspaces
         public override int GetHashCode()
         {
             return Name.GetHashCode() ^ Version.GetHashCode();
+        }
+
+        /// <summary>
+        /// Get the string representing this PackageDependencyInfo
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name + ", Version=" + Version.ToString();
         }
     }
 }
