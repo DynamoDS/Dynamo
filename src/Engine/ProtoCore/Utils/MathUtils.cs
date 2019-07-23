986a714fbf0d2ea6dbccfa1b98b1c79479601b82
aparajit-pratap <aparajit.pratap@autodesk.com>
2019-07-22 21:30:02 -0400

    add tests, restore deleted methods as obsolete

--- src/Engine/ProtoCore/Utils/MathUtils.cs
+++ src/Engine/ProtoCore/Utils/MathUtils.cs
@@ -6,6 +6,30 @@ namespace ProtoCore.Utils
     {
         public static double Tolerance = 1e-5;
 
+        [Obsolete("This method is no longer used. Remove in Dynamo 3.0")]
+        public static bool IsLessThan(double lhs, double rhs)
+        {
+            return (lhs<rhs) && !Equals(lhs, rhs);
+        }
+
+        [Obsolete("This method is no longer used. Remove in Dynamo 3.0")]
+        public static bool IsGreaterThan(double lhs, double rhs)
+        {
+            return (lhs > rhs) && !Equals(lhs, rhs);
+        }
+
+        [Obsolete("This method is no longer used. Remove in Dynamo 3.0")]
+        public static bool IsGreaterThanOrEquals(double lhs, double rhs)
+        {
+            return (lhs > rhs) || Equals(lhs, rhs);
+        }
+
+        [Obsolete("This method is no longer used. Remove in Dynamo 3.0")]
+        public static bool IsLessThanOrEquals(double lhs, double rhs)
+        {
+            return (lhs < rhs) || Equals(lhs, rhs);
+        }
+
         public static bool Equals(double lhs, double rhs)
         {
             if (double.IsPositiveInfinity(lhs) && double.IsPositiveInfinity(rhs))
