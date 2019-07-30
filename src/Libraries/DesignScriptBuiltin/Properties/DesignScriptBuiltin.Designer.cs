68790282a7b2515c9edfc16a58bd2dfa06fadc44
aparajit-pratap <aparajit.pratap@autodesk.com>
2019-07-30 10:25:28 -0400

    Cherry pick #9861: Fix warning message for indexing into lists, display fully qualified … (#9865)
    * Fix warning message for indexing into lists, display fully qualified classname in warnings (#9861)
    
    * fix warning message for indexing into lists, display fully qualified classname in warnings
    
    * display fully qualified class names in type mismatch and method resolution warnings
    
    * add resource strings for null reference exceptions with lists, strings and dictionaries
    
    * add comments
    
    * update test cases

--- src/Libraries/DesignScriptBuiltin/Properties/DesignScriptBuiltin.Designer.cs
+++ src/Libraries/DesignScriptBuiltin/Properties/DesignScriptBuiltin.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Builtin.Properties
-{
-
-
+namespace Builtin.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -66,6 +66,15 @@ namespace Builtin.Properties
         internal static string IndexOutOfRangeExceptionMessage {
             get {
                 return ResourceManager.GetString("IndexOutOfRangeExceptionMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Cannot index into a null list, dictionary or string..
+        /// </summary>
+        internal static string NullReferenceExceptionMessage {
+            get {
+                return ResourceManager.GetString("NullReferenceExceptionMessage", resourceCulture);
             }
         }
         
