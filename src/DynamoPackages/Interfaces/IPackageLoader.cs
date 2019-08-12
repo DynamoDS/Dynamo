1da5e9c5dd117927e2564feffa3185950275474a
tanga <aaron.tang@autodesk.com>
2019-08-12 01:58:41 -0400

    Add comments

--- src/DynamoPackages/Interfaces/IPackageLoader.cs
+++ src/DynamoPackages/Interfaces/IPackageLoader.cs
@@ -3,8 +3,15 @@ using Dynamo.PackageManager;
 
 namespace DynamoPackages.Interfaces
 {
+    /// <summary>
+    /// This interface is for PackageLoader. It can be expanded if needed,
+    /// currently only exposed inViewLoadedParams and referenced in WorkspaceDependencyViewExtension
+    /// </summary>
     public interface IPackageLoader
     {
+        /// <summary>
+        /// List of local packages
+        /// </summary>
         IEnumerable<Package> LocalPackages { get; }
     }
 }
