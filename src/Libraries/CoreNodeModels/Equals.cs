89374ba622a4ca70d747a9044977e968426de985
aparajit-pratap <aparajit.pratap@autodesk.com>
2019-09-23 23:11:49 -0400

    added lacing, supported disabling default value

--- src/Libraries/CoreNodeModels/Equals.cs
+++ src/Libraries/CoreNodeModels/Equals.cs
@@ -18,18 +18,23 @@ namespace CoreNodeModels
     [IsDesignScriptCompatible]
     public class Equals : NodeModel
     {
-        private static readonly string functionName = nameof(DSCore.List.Equals);
+        private static readonly DoubleNode tolerancePortDefaultValue = new DoubleNode(MathUtils.Tolerance);
 
         [JsonConstructor]
         private Equals(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base(inPorts, outPorts)
         {
+            ArgumentLacing = LacingStrategy.Auto;
+            inPorts.ElementAt(2).DefaultValue = tolerancePortDefaultValue;
         }
 
         public Equals()
         {
+            ArgumentLacing = LacingStrategy.Auto;
+
             InPorts.Add(new PortModel(PortType.Input, this, new PortData("lhs", "integer or double value")));
             InPorts.Add(new PortModel(PortType.Input, this, new PortData("rhs", "integer or double value")));
-            InPorts.Add(new PortModel(PortType.Input, this, new PortData("tolerance", "tolerance = 0.00001")));
+            InPorts.Add(new PortModel(PortType.Input, this,
+                new PortData("tolerance", "tolerance", tolerancePortDefaultValue)));
             OutPorts.Add(new PortModel(PortType.Output, this, new PortData("bool", "result of equality check")));
             RegisterAllPorts();
         }
@@ -41,60 +46,39 @@ namespace CoreNodeModels
                 return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode()) };
             }
 
-            AssociativeNode rhs = null;
+            AssociativeNode rhs;
             if (IsPartiallyApplied)
             {
-                if (inputAstNodes[0].Kind != AstKind.Null && inputAstNodes[1].Kind != AstKind.Null && inputAstNodes[2].Kind == AstKind.Null)
+                var connectedInputs = Enumerable.Range(0, InPorts.Count)
+                    .Where(index => InPorts[index].IsConnected)
+                    .Select(x => new IntNode(x) as AssociativeNode)
+                    .ToList();
+                var arguments = AstFactory.BuildExprList(inputAstNodes);
+                
+
+                var functionNode = new IdentifierListNode
                 {
-                    rhs = AstFactory.BuildFunctionCall(new Func<double, double, double, bool>(DSCore.Math.Equals),
-                        new List<AssociativeNode> {inputAstNodes[0], inputAstNodes[1]});
-                }
-                else
+                    LeftNode = new IdentifierNode("DSCore.Math"),
+                    RightNode = new IdentifierNode("Equals")
+                };
+                IntNode paramNumNode = new IntNode(3);
+                var positionNode = AstFactory.BuildExprList(connectedInputs);
+                
+                var inputParams = new List<AssociativeNode>
                 {
-                    List<AssociativeNode> connectedInputs = null;
-                    ExprListNode arguments = null;
-                    if (inputAstNodes[2].Kind == AstKind.Null)
-                    {
-                        connectedInputs = Enumerable.Range(0, InPorts.Count - 1)
-                            .Where(index => InPorts[index].IsConnected)
-                            .Select(x => new IntNode(x) as AssociativeNode)
-                            .ToList();
-                        connectedInputs.Add(new IntNode(2));
-                        arguments = AstFactory.BuildExprList(new List<AssociativeNode>
-                            {inputAstNodes[0], inputAstNodes[1], new DoubleNode(MathUtils.Tolerance)});
-                    }
-                    else
-                    {
-                        connectedInputs = Enumerable.Range(0, InPorts.Count)
-                            .Where(index => InPorts[index].IsConnected)
-                            .Select(x => new IntNode(x) as AssociativeNode)
-                            .ToList();
-                        arguments = AstFactory.BuildExprList(inputAstNodes);
-                    }
+                    functionNode,
+                    paramNumNode,
+                    positionNode,
+                    arguments,
+                    AstFactory.BuildBooleanNode(true)
+                };
 
-                    var functionNode = new IdentifierListNode
-                    {
-                        LeftNode = new IdentifierNode("DSCore.Math"),
-                        RightNode = new IdentifierNode("Equals")
-                    };
-                    IntNode paramNumNode = new IntNode(3);
-                    var positionNode = AstFactory.BuildExprList(connectedInputs);
-                    
-                    var inputParams = new List<AssociativeNode>
-                    {
-                        functionNode,
-                        paramNumNode,
-                        positionNode,
-                        arguments,
-                        AstFactory.BuildBooleanNode(true)
-                    };
-
-                    rhs = AstFactory.BuildFunctionCall("__CreateFunctionObject", inputParams);
-
-                }
+                rhs = AstFactory.BuildFunctionCall("__CreateFunctionObject", inputParams);
             }
             else
             {
+                UseLevelAndReplicationGuide(inputAstNodes);
+
                 rhs = AstFactory.BuildFunctionCall(new Func<double, double, double, bool>(DSCore.Math.Equals),
                     inputAstNodes);
             }
