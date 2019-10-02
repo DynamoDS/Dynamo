1f868399b78ec9712c95afddc8446539fa499aa4
Siddhartha Mangarole <sid.mangarole@gmail.com>
2019-10-02 09:34:26 -0400

    Revert "DockerUtilities"
    This reverts commit 501cfff6190c261c2fda511777346bfeba019d22.

--- DockerUtilities/RestartDockerDesktop.ps1
+++
@@ -1,7 +0,0 @@
-$processes = Get-Process "*docker desktop*"
-if ($processes.Count -gt 0)
-{
-    $processes[0].Kill()
-    $processes[0].WaitForExit()
-}
-Start-Process "$env:ProgramFiles\Docker\Docker\Docker Desktop.exe"
