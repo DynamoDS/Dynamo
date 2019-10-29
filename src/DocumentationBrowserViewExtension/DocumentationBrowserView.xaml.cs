69afe5f17ce02fc208f222ef30e988a26db96f69
SHKnudsen <sylvesterknudsen@gmail.com>
2019-10-29 16:11:54 +0000

    creating documentation view extension files

---
+++ src/DocumentationBrowserViewExtension/DocumentationBrowserView.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Dynamo.Graph.Workspaces;
+using Dynamo.Logging;
+using Dynamo.Utilities;
+using Dynamo.ViewModels;
+using Dynamo.Wpf.Extensions;
+
+namespace Dynamo.DocumentationBrowser
+{
+    /// <summary>
+    /// Interaction logic for WorkspaceDependencyView.xaml
+    /// </summary>
+    public partial class DocumentationBrowserView : Window, IDisposable
+    {
+
+        private WorkspaceModel currentWorkspace;
+
+        /// <summary>
+        /// The hyper link where Dynamo user will be forwarded to for submitting comments.
+        /// </summary>
+        private readonly string FeedbackLink = "https://forum.dynamobim.com/t/call-for-feedback-on-dynamo-graph-package-dependency-display/37229";
+
+        private ViewLoadedParams loadedParams;
+        private DocumentationBrowserViewExtension browserViewExtension;
+
+        /// <summary>
+        /// Re-directs to a web link to get the feedback from the user. 
+        /// </summary>
+        private void ProvideFeedback(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(FeedbackLink);
+            }
+            catch (Exception ex)
+            {
+                String message = Dynamo.DocumentationBrowser.Properties.Resources.ProvideFeedbackError + "\n\n" + ex.Message;
+                MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// Event handler for workspaceAdded event
+        /// </summary>
+        /// <param name="obj">workspace model</param>
+        internal void OnWorkspaceChanged(IWorkspaceModel obj)
+        {
+            
+        }
+
+        /// <summary>
+        /// Event handler for workspaceRemoved event
+        /// </summary>
+        /// <param name="obj">workspace model</param>
+        internal void OnWorkspaceCleared(IWorkspaceModel obj)
+        {
+
+        }
+
+        private void OnWorkspacePropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="p">ViewLoadedParams</param>
+        public DocumentationBrowserView(DocumentationBrowserViewExtension viewExtension, ViewLoadedParams p)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            currentWorkspace = p.CurrentWorkspaceModel as WorkspaceModel;
+            p.CurrentWorkspaceChanged += OnWorkspaceChanged;
+            p.CurrentWorkspaceCleared += OnWorkspaceCleared;
+            currentWorkspace.PropertyChanged += OnWorkspacePropertyChanged;
+            loadedParams = p;
+            browserViewExtension = viewExtension;
+        }
+
+
+
+        /// <summary>
+        /// Dispose function for WorkspaceDependencyView
+        /// </summary>
+        public void Dispose()
+        {
+            loadedParams.CurrentWorkspaceChanged -= OnWorkspaceChanged;
+            loadedParams.CurrentWorkspaceCleared -= OnWorkspaceCleared;
+        }
+    }
+}
---
+++ src/DocumentationBrowserViewExtension/DocumentationBrowserViewExtension.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using Dynamo.Extensions;
+using Dynamo.Graph.Workspaces;
+using Dynamo.Logging;
+using Dynamo.PackageManager;
+using Dynamo.DocumentationBrowser.Properties;
+using Dynamo.Wpf.Extensions;
+
+namespace Dynamo.DocumentationBrowser
+{
+    /// <summary>
+    /// This sample view extension demonstrates a sample IViewExtension 
+    /// which tracks graph dependencies (currently only packages) on the Dynamo right panel.
+    /// It reacts to workspace modified/ cleared events to refresh.
+    /// </summary>
+    public class DocumentationBrowserViewExtension : IViewExtension, ILogSource
+    {
+        private MenuItem documentationBrowserMenuItem;
+
+        internal DocumentationBrowserView BrowserView
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Extension Name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return "Documentation Browser ViewExtension";
+            }
+        }
+
+        /// <summary>
+        /// GUID of the extension
+        /// </summary>
+        public string UniqueId
+        {
+            get
+            {
+                return "011ec935-fcd6-43f0-ab32-1c5c0f913b33";
+            }
+        }
+
+        /// <summary>
+        /// Dispose function after extension is closed
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+
+        [Obsolete("This method is not implemented and will be removed.")]
+        public void Ready(ReadyParams readyParams)
+        {
+        }
+
+        public void Shutdown()
+        {
+            BrowserView.Dispose();
+            this.Dispose();
+        }
+
+        public void Startup(ViewStartupParams viewLoadedParams)
+        {
+        }
+
+        public event Action<ILogMessage> MessageLogged;
+        internal void OnMessageLogged(ILogMessage msg)
+        {
+            this.MessageLogged?.Invoke(msg);
+        }
+
+        public void Loaded(ViewLoadedParams viewLoadedParams)
+        {
+            BrowserView = new DocumentationBrowserView(this, viewLoadedParams);
+            // when a package is loaded update the DependencyView 
+            // as we may have installed a missing package.
+
+
+            // Adding a button in view menu to refresh and show manually
+            documentationBrowserMenuItem = new MenuItem { Header = Resources.MenuItemString };
+            documentationBrowserMenuItem.Click += (sender, args) =>
+            {
+                // Refresh dependency data
+                viewLoadedParams.AddToExtensionsSideBar(this, BrowserView);
+            };
+            viewLoadedParams.AddMenuItem(MenuBarType.View, documentationBrowserMenuItem);
+        }
+
+    }
+}
