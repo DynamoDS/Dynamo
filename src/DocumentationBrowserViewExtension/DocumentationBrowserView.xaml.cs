97df5745df35fe0bd427ac5d23437bb1c7bf942f
SHKnudsen <sylvesterknudsen@gmail.com>
2019-10-30 14:46:18 +0000

    doucmentation browser view

--- src/DocumentationBrowserViewExtension/DocumentationBrowserView.xaml.cs
+++ src/DocumentationBrowserViewExtension/DocumentationBrowserView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Windows;
@@ -15,60 +16,10 @@ using Dynamo.Wpf.Extensions;
 namespace Dynamo.DocumentationBrowser
 {
     /// <summary>
-    /// Interaction logic for WorkspaceDependencyView.xaml
+    /// Interaction logic for DocumentationBrowserView.xaml
     /// </summary>
     public partial class DocumentationBrowserView : Window, IDisposable
     {
-
-        private WorkspaceModel currentWorkspace;
-
-        /// <summary>
-        /// The hyper link where Dynamo user will be forwarded to for submitting comments.
-        /// </summary>
-        private readonly string FeedbackLink = "https://forum.dynamobim.com/t/call-for-feedback-on-dynamo-graph-package-dependency-display/37229";
-
-        private ViewLoadedParams loadedParams;
-        private DocumentationBrowserViewExtension browserViewExtension;
-
-        /// <summary>
-        /// Re-directs to a web link to get the feedback from the user. 
-        /// </summary>
-        private void ProvideFeedback(object sender, EventArgs e)
-        {
-            try
-            {
-                System.Diagnostics.Process.Start(FeedbackLink);
-            }
-            catch (Exception ex)
-            {
-                String message = Dynamo.DocumentationBrowser.Properties.Resources.ProvideFeedbackError + "\n\n" + ex.Message;
-                MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        /// <summary>
-        /// Event handler for workspaceAdded event
-        /// </summary>
-        /// <param name="obj">workspace model</param>
-        internal void OnWorkspaceChanged(IWorkspaceModel obj)
-        {
-            
-        }
-
-        /// <summary>
-        /// Event handler for workspaceRemoved event
-        /// </summary>
-        /// <param name="obj">workspace model</param>
-        internal void OnWorkspaceCleared(IWorkspaceModel obj)
-        {
-
-        }
-
-        private void OnWorkspacePropertyChanged(object sender, PropertyChangedEventArgs args)
-        {
-
-        }
-
 
         /// <summary>
         /// Constructor
@@ -77,24 +28,31 @@ namespace Dynamo.DocumentationBrowser
         public DocumentationBrowserView(DocumentationBrowserViewExtension viewExtension, ViewLoadedParams p)
         {
             InitializeComponent();
-            this.DataContext = this;
-            currentWorkspace = p.CurrentWorkspaceModel as WorkspaceModel;
-            p.CurrentWorkspaceChanged += OnWorkspaceChanged;
-            p.CurrentWorkspaceCleared += OnWorkspaceCleared;
-            currentWorkspace.PropertyChanged += OnWorkspacePropertyChanged;
-            loadedParams = p;
-            browserViewExtension = viewExtension;
+            DocumentationBrowserViewModel.LinkChanged += this.NavigateToPage;
+            documentationBrowser.NavigationStarting += DocumentationBrowser_NavigationStarting;
         }
 
+        private void DocumentationBrowser_NavigationStarting(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationStartingEventArgs e)
+        {
+            if (e.Uri == null) return;
+            if (e.Uri.Scheme != Uri.UriSchemeHttp && e.Uri.Scheme != Uri.UriSchemeHttps) return;
 
+            e.Cancel = true;
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));       
+        }
+
+        public void NavigateToPage(string link)
+        {
+            documentationBrowser.NavigateToString(link);
+        }
 
         /// <summary>
-        /// Dispose function for WorkspaceDependencyView
+        /// Dispose function for DocumentationBrowser
         /// </summary>
         public void Dispose()
         {
-            loadedParams.CurrentWorkspaceChanged -= OnWorkspaceChanged;
-            loadedParams.CurrentWorkspaceCleared -= OnWorkspaceCleared;
+            DocumentationBrowserViewModel.LinkChanged -= this.NavigateToPage;
+            documentationBrowser.Dispose();
         }
     }
 }
