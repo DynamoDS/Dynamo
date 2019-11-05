87d063778305d60ca730aa47b32f063f51c54b16
Radu Gidei <radumg@users.noreply.github.com>
2019-11-05 18:20:00 +0000

    remove IsRemoteResource from documentation link open event args

--- src/DynamoCoreWpf/ViewModels/Core/DynamoViewModelEventArgs.cs
+++ src/DynamoCoreWpf/ViewModels/Core/DynamoViewModelEventArgs.cs
@@ -130,11 +130,9 @@ namespace Dynamo.ViewModels
     public class OpenDocumentationLinkEventArgs : EventArgs
     {
         public Uri Link { get; set; }
-        public bool IsRemoteResource { get; private set; }
         public OpenDocumentationLinkEventArgs(Uri link)
         {
             Link = link ?? throw new ArgumentNullException(nameof(link));
-            IsRemoteResource = !link.IsFile;
         }
     }
 }
