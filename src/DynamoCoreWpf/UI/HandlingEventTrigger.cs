ef17698ffe26ad624010c33bef6e869bf73bb7f1
michael kirschner <mjk.kirschner@gmail.com>
2019-11-25 21:43:33 -0500

    this works - but requires more work for mem and perf profiling

--- src/DynamoCoreWpf/UI/HandlingEventTrigger.cs
+++ src/DynamoCoreWpf/UI/HandlingEventTrigger.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Dynamo.UI.Views
 {
@@ -16,4 +19,113 @@ namespace Dynamo.UI.Views
             base.OnEvent(eventArgs);
         }
     }
+
+    //forked from: http://www.mobilemotion.eu/?p=1822
+
+
+    public class EventToCommand
+    {
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached("Commands", typeof(object), typeof(EventToCommand));
+
+        public static ICommand[] GetCommands(DependencyObject obj)
+        {
+            return (ICommand[])obj.GetValue(CommandProperty);
+        }
+
+        public static void SetCommands(DependencyObject obj, string value)
+        {
+            var commands = parseCommandString(obj as FrameworkElement, value);
+            obj.SetValue(CommandProperty, commands.ToArray());
+        }
+
+        public static readonly DependencyProperty EventProperty =
+            DependencyProperty.RegisterAttached("Events", typeof(object), typeof(EventToCommand),
+                new PropertyMetadata(EventPropertyChangedCallback));
+
+        public static string[] GetEvents(DependencyObject obj)
+        {
+            return (string[])obj.GetValue(EventProperty);
+        }
+
+        public static void SetEvents(DependencyObject obj, string value)
+        {
+            //parse string "eventName1,eventName2";
+
+            var eventNames = parseEventNames(value);
+            obj.SetValue(EventProperty, eventNames);
+        }
+
+        public static void EventPropertyChangedCallback(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            string[] @events = (args.NewValue as string).Split(',');
+            var eventInfos = @events.Select(x => obj.GetType().GetEvent(x));
+            foreach (var eventInfo in eventInfos)
+            {
+                if (eventInfo != null)
+                {
+                    var methodInfo = typeof(EventToCommand).GetMethod("OnEvent", BindingFlags.Static | BindingFlags.NonPublic);
+                    //TODO clear previous subscriptions?
+                    eventInfo.GetAddMethod().Invoke(obj,
+                        new object[] { Delegate.CreateDelegate(eventInfo.EventHandlerType, methodInfo) });
+                }
+            }
+        }
+
+        //TODO handle the events.
+        private static void OnEvent(object sender, RoutedEventArgs e)
+        {
+            if (e != null)
+            {
+                e.Handled = true;
+
+            }
+            UIElement element = (UIElement)sender;
+            if (element != null)
+            {
+                ICommand[] commands = parseCommandString(element as FrameworkElement,element.GetValue(CommandProperty) as string);
+                var events =parseEventNames(element.GetValue(EventProperty) as string);
+                //find which command to execute.
+                //this is slow - there should be a better way, but unclear yet where to cache this data.
+
+                var index = Array.IndexOf(events, e.RoutedEvent.Name);
+                if (index > -1 && commands.Length > index)
+                {
+                    var command = commands[index];
+                    if (command != null && command.CanExecute(null))
+                    {
+                        //need to pass the PortViewModel in some cases...
+                        //TODO need to expose commandParamter dep prop
+                        command.Execute((element as FrameworkElement).DataContext);
+
+                       
+                    }
+                }
+                
+            }
+        }
+
+        private static ICommand[] parseCommandString(FrameworkElement obj, string commandString)
+        {
+            var commandNames = commandString.Split(',');
+            var commands = commandNames.Select(x =>
+            {
+                //TODO this requires that the datacontext of the bound element is a portViewModel.
+                //add assert/test/
+                var dataContext = (obj as FrameworkElement).DataContext;
+                return dataContext.GetType().GetProperty(x).GetValue(dataContext) as ICommand;
+            });
+
+            return commands.ToArray();
+
+        }
+        private static string[] parseEventNames(string events)
+        {
+            return events.Split(',');
+        }
+
+        //TODO dispose dep properties?
+
+    }
+
 }
