27fb9fd05d4ffe8de87fec9d68ef541ad3a7887e
michael kirschner <mjk.kirschner@gmail.com>
2019-12-02 19:00:32 -0500

    raise event when importing zt node via LoadNodeLibrary method to restore behavior

--- src/DynamoCore/Models/DynamoModel.cs
+++ src/DynamoCore/Models/DynamoModel.cs
@@ -1257,6 +1257,8 @@ namespace Dynamo.Models
             if (!NodeModelAssemblyLoader.ContainsNodeModelSubType(assem))
             {
                 LibraryServices.LoadNodeLibrary(assem.Location, false);
+                LibraryServices.OnLibrariesImported(new LibraryServices.LibraryLoadedEventArgs(new List<string> { assem.Location }));
+
                 return;
             }
 
