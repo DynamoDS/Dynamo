2cdbca7dac611ca1c18c47bb0de53476b5c17c8b
Craig Long <craig.long@autodesk.com>
2019-12-10 08:34:57 -0500

    Refactor .Net assembly cetificate verication to DynamoCrypto

--- src/DynamoCrypto/DynamoCrypto.cs
+++ src/DynamoCrypto/DynamoCrypto.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
@@ -195,6 +197,47 @@ namespace DynamoCrypto
             var cert = new X509Certificate2(certPath);
             store.Add(cert);
             return cert;
+        }
+
+        /// <summary>
+        /// Check if a .NET assembly can be loaded and has a valid certificate
+        /// </summary>
+        /// <param name="assemblyPath">Path of the assembly file</param>
+        /// <returns></returns>
+        public static bool CheckAssemblyForValidCertificate(string assemblyPath)
+        {
+            //Verify the assembly exists
+            if (!File.Exists(assemblyPath))
+            {
+                throw new Exception(String.Format(
+                    "A dll file was not found at {0}. No certificate was able to be verified.", assemblyPath));
+            }
+
+            //Verify that you can load the assembly into a Reflection only context
+            Assembly asm;
+            try
+            {
+                asm = Assembly.ReflectionOnlyLoadFrom(assemblyPath);
+            }
+            catch
+            {
+                throw new Exception(String.Format(
+                    "A dll file found at {0} could not be loaded.", assemblyPath));
+            }
+
+            //Verify the node library has a verified signed certificate
+            var cert = asm.Modules.FirstOrDefault()?.GetSignerCertificate();
+            if (cert != null)
+            {
+                var cert2 = new System.Security.Cryptography.X509Certificates.X509Certificate2(cert);
+                if (cert2.Verify())
+                {
+                    return true;
+                }
+            }
+
+            throw new Exception(String.Format(
+                "A dll file found at {0} did not have a signed certificate.", assemblyPath));
         }
     }
 }
