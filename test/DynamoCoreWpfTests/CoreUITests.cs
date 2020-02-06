8311b5a9b0b0e575f078b09fa7992bb8b480522d
Radu Gidei <radumg@users.noreply.github.com>
2020-02-06 16:50:55 +0000

    add behaviour UI test for InfoBubbleViewModel

--- test/DynamoCoreWpfTests/CoreUITests.cs
+++ test/DynamoCoreWpfTests/CoreUITests.cs
@@ -723,7 +723,42 @@ namespace DynamoCoreWpfTests
                 Assert.AreEqual(content, infoBubble.Content);
                 Assert.AreEqual(InfoBubbleViewModel.Style.Error, infoBubble.InfoBubbleStyle);
                 Assert.AreEqual(InfoBubbleViewModel.Direction.Bottom, infoBubble.ConnectingDirection);
+                Assert.IsNull(infoBubble.DocumentationLink);
             }
+        }
+
+        [Test]
+        [Category("DynamoUI")]
+        public void UpdateInfoBubble_DocumentationLink()
+        {
+            // Arrange
+            InfoBubbleViewModel infoBubble = new InfoBubbleViewModel(this.ViewModel);
+            const string documentationFile = "dynamo.html";
+            string content = $"This is the test infoBubble href={documentationFile}";
+            InfoBubbleDataPacket infoBubbleDataPacket = new InfoBubbleDataPacket(InfoBubbleViewModel.Style.Error,
+                new Point(0, 0), new Point(0, 0), content, InfoBubbleViewModel.Direction.Bottom);
+            var docEventArgs = new OpenDocumentationLinkEventArgs(infoBubbleDataPacket.Link);
+
+            // register an extra handler for documentation events so we can confirm
+            // the Dynamo ViewModel event gets triggered from the InfoBubble viewmodel
+            RequestOpenDocumentationLinkHandler handler = (OpenDocumentationLinkEventArgs e) => 
+            {
+                Assert.IsNotNull(e);
+                Assert.AreEqual(documentationFile,e.Link.ToString());
+            };
+            this.ViewModel.RequestOpenDocumentationLink += handler;
+
+            // Act
+            // test we can pass in content with a link and it gets set
+            if (infoBubble.UpdateContentCommand.CanExecute(null))
+                infoBubble.UpdateContentCommand.Execute(infoBubbleDataPacket);
+            // test the InfoBubbleViewModel raises the right DynamoViewModel event
+            if (infoBubble.OpenDocumentationLinkCommand.CanExecute(null))
+                infoBubble.OpenDocumentationLinkCommand.Execute(docEventArgs.Link);
+
+            // Assert
+            Assert.IsNotNull(infoBubble.DocumentationLink);
+            Assert.AreEqual(documentationFile, infoBubble.DocumentationLink.ToString());
         }
 
         [Test, Ignore]
