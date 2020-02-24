26b1aed686f407966385ee8659a945ae8981cf95
Aaron (Qilong) <173288704@qq.com>
2020-02-24 14:59:31 -0500

    GA client not launching when terms not agreed (#10407)
    * GA client not launching when terms not agreed
    
    * Comments
    
    (cherry picked from commit 89c0038754dac54eac7beab90d1fcb56c69fdaac)

--- test/DynamoCoreWpfTests/CoreUITests.cs
+++ test/DynamoCoreWpfTests/CoreUITests.cs
@@ -502,7 +502,7 @@ namespace DynamoCoreWpfTests
 
         [Test]
         [Category("DynamoUI")]
-        public void PreferenceSetting_AgreeAnalyticSharing()
+        public void PreferenceSetting_NotAgreeAnalyticsSharing()
         {
             // Test deserialization of analytics setting 
             // Test loading old settings file without agreement 
@@ -515,9 +515,9 @@ namespace DynamoCoreWpfTests
 
         [Test]
         [Category("DynamoUI")]
-        public void PreferenceSetting_NotAgreeAnalyticsSharing()
+        public void PreferenceSetting_AgreeAnalyticsSharing()
         {
-            // Test loading old settings file without render precision attribute
+            // Test loading old settings file with agreement 
             var filePath = Path.Combine(GetTestDirectory(ExecutingDirectory), @"settings\DynamoSettings-AnalyticsTurnedOn.xml");
             var resultSetting = PreferenceSettings.Load(filePath);
             Assert.AreEqual(true, resultSetting.IsAnalyticsReportingApproved);
