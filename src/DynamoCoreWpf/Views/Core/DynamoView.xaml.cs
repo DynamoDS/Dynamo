e12b9859cf992d36aafa166fb2ffcb7cd04dbb38
tanga <aaron.tang@autodesk.com>
2020-02-26 01:36:54 -0500

    Adding the ability to dock python window

--- src/DynamoCoreWpf/Views/Core/DynamoView.xaml.cs
+++ src/DynamoCoreWpf/Views/Core/DynamoView.xaml.cs
@@ -204,11 +204,11 @@ namespace Dynamo.Controls
         /// sets the extension window as the tab content.
         /// </summary>
         /// <param name="viewExtension">target view extension</param>
-        /// <param name="contentControl">target control</param>
+        /// <param name="targetControl">target control</param>
         /// <returns></returns>
-        internal TabItem AddTabItem(IViewExtension viewExtension, ContentControl contentControl)
+        internal TabItem AddTabItem(IViewExtension viewExtension, Control targetControl)
         {
-            return AddTabItem(viewExtension.Name, contentControl);
+            return AddTabItem(viewExtension.Name, targetControl);
         }
 
         /// <summary>
@@ -216,31 +216,35 @@ namespace Dynamo.Controls
         /// sets the input content control as the tab content.
         /// </summary>
         /// <param name="name">target control name</param>
-        /// <param name="contentControl">target control</param>
+        /// <param name="targetControl">target control</param>
         /// <returns></returns>
-        internal TabItem AddTabItem(string name, ContentControl contentControl)
+        internal TabItem AddTabItem(string name, object targetControl)
         {
             int count = TabItems.Count;
 
-            if (!IsAddedToRightSideBar(contentControl))
+            if (!IsAddedToRightSideBar(targetControl))
             {
                 tabDynamic.DataContext = null;
 
                 // creates a new tab item
                 TabItem tab = new TabItem();
                 tab.Header = name;
-                tab.Tag = contentControl.GetType();
+                tab.Tag = targetControl.GetType();
                 tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
 
                 // setting the extension UI to the current tab content 
                 // based on whether it is a UserControl element or window element. 
-                if (contentControl is UserControl)
+                if (targetControl is UserControl)
                 {
-                    tab.Content = contentControl;
+                    tab.Content = targetControl;
+                }
+                else if (targetControl is ContentControl)
+                {
+                    tab.Content = (targetControl as ContentControl)?.Content;
                 }
                 else
                 {
-                    tab.Content = contentControl.Content;
+                    tab.Content = targetControl;
                 }
 
                 //Insert the tab at the end
@@ -290,11 +294,11 @@ namespace Dynamo.Controls
             this.HideOrShowRightSideBar();
         }
 
-        private Boolean IsAddedToRightSideBar(ContentControl contentControl)
+        private Boolean IsAddedToRightSideBar(object targetControl)
         {
             foreach (TabItem tabItem in TabItems)
             {
-                if (tabItem.Tag.Equals(contentControl.GetType()))
+                if (tabItem.Tag.Equals(targetControl.GetType()))
                 {
                     return true;
                 }
