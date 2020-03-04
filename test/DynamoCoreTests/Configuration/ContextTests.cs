606b74c8372e032ef4957ff84de21e32052d4a39
t-hernjes <jesus.hc@globant.com>
2020-03-04 12:18:22 -0600

    Cover Test for Configuration Folder

---
+++ test/DynamoCoreTests/Configuration/ContextTests.cs
@@ -0,0 +1,16 @@
+﻿using Dynamo.Configuration;
+using NUnit.Framework;
+
+namespace Dynamo.Tests.Configuration
+{
+    [TestFixture]
+    class ContextTests : DynamoModelTestBase
+    {
+        [Test]
+        [Category("UnitTests")]
+        public void TestIsUnix()
+        {
+            Assert.IsFalse(Context.IsUnix);   
+        }
+    }
+}
