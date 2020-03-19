56ebeed08835ff86d0a3e21d715ed182b4946f05
michael kirschner <mjk.kirschner@gmail.com>
2020-03-19 15:57:57 -0400

    update comments

--- src/DynamoCoreWpf/ViewModels/Watch3D/shaderSource/psDynamoMesh.hlsl
+++ src/DynamoCoreWpf/ViewModels/Watch3D/shaderSource/psDynamoMesh.hlsl
@@ -42,7 +42,7 @@ float4 main(PSInputCustom input, bool isFrontFacing : SV_IsFrontFace) : SV_Targe
 	  IsFrozen = 1,
 	  IsSelected = 2,
 	  IsIsolated = 4,
-	  IsSpecialRenderPackage = 8, //not used
+	  IsSpecialRenderPackage = 8,
 	  HasTransparency = 16, //not used
 	  RequiresPerVertexColoration = 32
 	  FlatShade = 64 //not used
