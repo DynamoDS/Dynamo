d0d9b641947403ecac478ec48186dd20cf359efa
gantaa <43763136+reddyashish@users.noreply.github.com>
2020-03-23 09:42:12 -0400

    comments

--- src/DynamoCoreWpf/ViewModels/PackageManager/InstalledPackagesViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/PackageManager/InstalledPackagesViewModel.cs
@@ -32,7 +32,7 @@ namespace Dynamo.ViewModels
         {
             foreach (var pkg in Model.LocalPackages)
             {
-                // If the pkg is null due to invalid package structure, then dont load that package. 
+                // If the pkg is null due to invalid package structure, then don't load that package. 
                 if (pkg != null)
                 {
                     LocalPackages.Add(new PackageViewModel(this.dynamoViewModel, pkg));
