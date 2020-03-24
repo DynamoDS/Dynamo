e59e3073ade5a1208cf27f03d7751592cb0adb30
michael kirschner <mjk.kirschner@gmail.com>
2020-03-24 18:16:09 -0400

    unused usings

--- src/VisualizationTests/HelixImageComparisonTests.cs
+++ src/VisualizationTests/HelixImageComparisonTests.cs
@@ -8,13 +8,10 @@ using DynamoCoreWpfTests.Utility;
 using HelixToolkit.Wpf.SharpDX;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 
 namespace WpfVisualizationTests
