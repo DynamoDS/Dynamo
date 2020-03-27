36a2ed597bb1508ddf73a91b6ce9bf4a7712db9c
reddyashish <43763136+reddyashish@users.noreply.github.com>
2020-03-27 01:42:10 -0400

    [DYN-1815] Dynamo package install invalid package (sad path) error and crash (#10493)
    * Package manager crash due to loading of an invalid package.
    
    * comments
    
    * Adding the fix to the PackageManagerClientViewModel, tests and comments.
    
    * Moving the test package to test/pkgs folder

--- src/DynamoCoreWpf/Properties/Resources.Designer.cs
+++ src/DynamoCoreWpf/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Dynamo.Wpf.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -3179,6 +3179,15 @@ namespace Dynamo.Wpf.Properties {
         public static string MessageGettingNodeError {
             get {
                 return ResourceManager.GetString("MessageGettingNodeError", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Failed to load an invalid package..
+        /// </summary>
+        public static string MessageInvalidPackage {
+            get {
+                return ResourceManager.GetString("MessageInvalidPackage", resourceCulture);
             }
         }
         
