c7d133f80d2519d1d73f1640405144eb4a456658
mjkkirschner <mjk.kirschner@gmail.com>
2020-04-01 17:57:57 -0400

    add micro test ignore all expressions which are not assigments in the nullifyBinaryExpression code

--- src/Engine/ProtoScript/Runners/LiveRunner.cs
+++ src/Engine/ProtoScript/Runners/LiveRunner.cs
@@ -959,19 +959,9 @@ namespace ProtoScript.Runners
             return true;
         }
 
-        //TODO move to DEFS
-        //TODO what binary expressions should actually be converted to null assignments...
-        private bool IsLogicalOP(Operator op)
-        {
-            if(op is Operator.and || op is Operator.or)
-            {
-                return true;
-            }
-            return false;
-        }
-
         /// <summary>
-        /// Creates a list of null assignment statements where the lhs is retrieved from an ast list
+        /// Creates a list of null assignment statements where the lhs is retrieved from an ast list.
+        /// Any expressions which are not assignments are not modified.
         /// </summary>
         /// <param name="astList"></param>
         /// <returns></returns>
@@ -993,7 +983,7 @@ namespace ProtoScript.Runners
                 }
 
                 IdentifierNode leftNode = bNode.LeftNode as IdentifierNode;
-                if (leftNode == null || leftNode.ArrayDimensions != null  || IsLogicalOP(bNode.Optr))
+                if (leftNode == null || leftNode.ArrayDimensions != null  || bNode.Optr != Operator.assign)
                 {
                     continue;
                 }
