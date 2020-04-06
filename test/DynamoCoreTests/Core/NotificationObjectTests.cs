9a2d730ee74b3be487c4cf2e399d17c34d114ad7
Bruno Yorda <bruno.yorda@globant.com>
2020-04-06 16:28:39 -0500

    Added NotificationObject tests

---
+++ test/DynamoCoreTests/Core/NotificationObjectTests.cs
@@ -0,0 +1,49 @@
+﻿using Dynamo.Core;
+using Dynamo.Models;
+using Dynamo.Scheduler;
+using NUnit.Framework;
+using System.ComponentModel;
+
+namespace Dynamo.Tests.Core
+{
+    [TestFixture]
+    public class NotificationObjectTests : DynamoModelTestBase
+    {
+        private class NotificationObjectTestingSubclass : NotificationObject
+        {
+            public bool TestProperty { get; set; }
+
+            public NotificationObjectTestingSubclass()
+            {
+                PropertyChanged += Test_PropertyChanged;
+            }
+
+            
+            private void Test_PropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName.Equals("TestProperty"))
+                {
+                    RaisePropertyChanged("TestProperty");
+
+                    var manyProps = new string[] { "TestProperty", "TestProperty" };
+                    RaisePropertyChanged(manyProps);
+                }
+            }
+        }
+
+        NotificationObjectTestingSubclass notificationObject;
+
+        [SetUp]
+        public void Init()
+        {
+            notificationObject = new NotificationObjectTestingSubclass();
+        }
+
+        [Test]
+        [NUnit.Framework.Category("UnitTests")]
+        public void RaisePropertyChangedTest()
+        {
+            notificationObject.TestProperty = !notificationObject.TestProperty;
+        }
+    }
+}
