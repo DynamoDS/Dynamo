a5aa1581a1b40db5c57a0036607208d97b9dc2e7
Roberto Tellez <roberto.tellez@globant.com>
2020-04-07 10:07:01 -0500

    DYN-2555 - Increase the code coverage: DynamoCore/Search Folder
    I just added the folder to the project and the main class for the BrowserItem

---
+++ test/DynamoCoreTests/Search/BrowserItemTest.cs
@@ -0,0 +1,24 @@
+﻿using Dynamo.Search;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.Tests.Search
+{
+    /// <summary>
+    /// In this class we are including the test cases missing from the abstract class BrowserItem,I mean, the ones without coverage
+    /// </summary>
+    [TestFixture]
+    class BrowserItemTest
+    {
+        [Test]
+        [Category("UnitTests")]
+        public void TestInternalMigration()
+        {
+            var browserItem = new BrowserInternalElement();
+        }
+    }
+}
