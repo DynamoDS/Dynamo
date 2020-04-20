8a40cc03ca2a4c6768e09c1a81e237ae3313a133
Roberto T <61755417+RobertGlobant20@users.noreply.github.com>
2020-04-20 13:57:55 -0500

    DYN-2555 - Increase the code coverage: DynamoCore/Search Folder 2 (#10582)
    I created several unit tests using nunit and AxoCover (code coverage) for the next classes (some of them already had code coverage) inside Search and SearchElements folders:
    
    - BrowserInternalElement
    - SearchDictionary
    - SearchLibrary
    - SearchElements/CodeBlockNodeSearchElement
    - SearchElements/CustomNodeSearchElement
    - SearchElements/NodeModelSearchElement
    - SearchElements/NodeSearchElement
    - SearchElements/SearchElementBase
    
    Thanks

---
+++ test/DynamoCoreTests/Search/SearchElements/CodeBlockNodeSearchElementTest.cs
@@ -0,0 +1,36 @@
+﻿using Dynamo.Graph.Nodes;
+using Dynamo.Search.SearchElements;
+using NUnit.Framework;
+
+namespace Dynamo.Tests.Search.SearchElements
+{
+    
+    [TestFixture]
+    class CodeBlockNodeSearchElementTest : DynamoModelTestBase
+    {
+        /// <summary>
+        /// This test method will execute the next method in CodeBlockNodeSearchElement 
+        /// protected override NodeModel ConstructNewNodeModel()
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void TestCodeBlockNodeSearchNewNodeModel()
+        {
+            //Arrange
+            var cbnData = new TypeLoadData(typeof(CodeBlockNodeModel));
+            var codeBlockSearch = new CodeBlockNodeSearchElement(cbnData, CurrentDynamoModel.LibraryServices);
+            int initialCount = CurrentDynamoModel.SearchModel.NumElements;
+
+            CurrentDynamoModel.SearchModel.Add(codeBlockSearch);
+
+            //Act
+            //This will call the method CodeBlockNodeSearchElement.ConstructNewNodeModel()
+            codeBlockSearch.ProduceNode();
+
+            //Assert
+            //Just validate that the new CodeBlockNodeSearchElement was added correctly
+            Assert.AreEqual(initialCount + 1, CurrentDynamoModel.SearchModel.NumElements);
+        }
+
+    }
+}
