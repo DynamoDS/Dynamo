027cccdd3c3c93ee91f8970d1f2d5ab71bf84b15
Roberto Tellez <roberto.tellez@globant.com>
2020-04-21 09:29:28 -0500

    DYN-2560 - Increase the code coverage: DynamoCore/Models Folder
    I started adding just one test method TestOnRequestDispatcherBeginInvoke() for testing the DynamoModelEvents class.

---
+++ test/DynamoCoreTests/Models/DynamoModelEventsTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Dynamo.Tests.ModelsTest
+{
+    [TestFixture]
+    class DynamoModelEventsTest : DynamoModelTestBase
+    {
+        
+        [Test]
+        [Category("UnitTests")]
+        public void TestOnRequestDispatcherBeginInvoke()
+        {
+            /*var viewModel = DynamoViewModel.Start(
+            new DynamoViewModel.StartConfiguration()
+            {
+                DynamoModel = this.CurrentDynamoModel,
+                Watch3DViewModel =
+                    new DefaultWatch3DViewModel(null, new Watch3DViewModelStartupParams(this.CurrentDynamoModel))
+                    {
+                        Active = false,
+                        CanBeActivated = false
+                    }
+            });*/
+
+            Assert.Pass();
+        }
+    }
+}
