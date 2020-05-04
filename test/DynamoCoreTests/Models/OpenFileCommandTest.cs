9d70a185c74b3b38204a96231d6692c35263d038
Roberto Tellez <roberto.tellez@globant.com>
2020-05-04 12:37:19 -0500

    DYN-2560 - Increase the code coverage: DynamoCore/Models 2
    DYN-2560 - Increase the code coverage: DynamoCore/Models
    I created several test cases for the next clases inside the RecordableCommands.cs file:
    
    RecordableCommands.
    OpenFileCommand
    PausePlaybackCommand

---
+++ test/DynamoCoreTests/Models/OpenFileCommandTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Dynamo.Models;
+using NUnit.Framework;
+using static Dynamo.Models.DynamoModel;
+
+namespace Dynamo.Tests.ModelsTest
+{
+    [TestFixture]
+    class OpenFileCommandTest : DynamoModelTestBase
+    {
+        [Test]
+        [Category("UnitTests")]
+        public void TestTryFindFile()
+        {
+            string wspath = Path.Combine(TestDirectory, @"core\callsite\RebindingSingleDimension.dyn");
+
+            var fileCommand = new DynamoModel.OpenFileCommand(wspath);          
+
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlElement elemTest = xmlDocument.CreateElement("TestCommand");
+
+            var command = new OpenFileCommandDerivedTest(wspath);
+            command.SerializeCoreTest(elemTest);
+
+            var deserializedCommand = OpenFileCommand.DeserializeCore(elemTest);
+
+            Assert.IsNotNull(deserializedCommand);
+        }
+    }
+
+    /// <summary>
+    /// This test class was created with the purpose of execute the protected method ExecuteCore(DynamoModel dynamoModel)
+    /// </summary>
+    class OpenFileCommandDerivedTest : OpenFileCommand
+    {
+        public OpenFileCommandDerivedTest(string filePath) : base(filePath)
+        {
+        }
+
+        public void ExecuteTest(DynamoModel dynamoModel)
+        {
+            ExecuteCore(dynamoModel);
+        }
+
+        public void SerializeCoreTest(System.Xml.XmlElement element)
+        {
+            SerializeCore(element);
+        }
+
+        protected override void ExecuteCore(DynamoModel dynamoModel)
+        {
+            try
+            {
+                //This will raise a NotImplementedException since ExecuteCore should not be implemented for a PausePlaybackCommand 
+                base.ExecuteCore(dynamoModel);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        protected override void SerializeCore(System.Xml.XmlElement element)
+        {
+            base.SerializeCore(element);
+        }
+    }
+}
--- test/DynamoCoreTests/Models/RecordableCommandsTest.cs
+++ test/DynamoCoreTests/Models/RecordableCommandsTest.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
+using CoreNodeModels.Input;
 using Dynamo.Extensions;
+using Dynamo.Graph.Nodes;
 using Dynamo.Models;
+using Dynamo.Selection;
+using Dynamo.Utilities;
 using Moq;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
@@ -16,6 +21,10 @@ namespace Dynamo.Tests.ModelsTest
         ICommandExecutive executive;
         int cmdExecutionState = -1;
 
+        /// <summary>
+        /// This method will be executed as callback when Dynamo is Ready
+        /// </summary>
+        /// <param name="ready"></param>
         private void ExtensionReadyCallback(ReadyParams ready)
         {
             executive = ready.CommandExecutive;
@@ -29,49 +38,94 @@ namespace Dynamo.Tests.ModelsTest
             extMock = new Mock<IExtension>();
             extMock.Setup(ext => ext.Ready(It.IsAny<ReadyParams>())).Callback((ReadyParams r) => ExtensionReadyCallback(r));
             cmdExecutionState = -1;
-
         }
 
+        /// <summary>
+        /// This Test Method will execute the next methods in the RecordableCommands class
+        /// protected RecordableCommand(string tag)
+        /// internal string Serialize()
         /// </summary>
         [Test]
         [Category("UnitTests")]
         public void TestRecordableCommand()
         {
+            //Arrange
+            //The tag passed as parameter is null, then it will raise an exception later
             var cmd = new Mock<DynamoModel.RecordableCommand>(null);
-
             var pausePlaybackCommand = new DynamoModel.PausePlaybackCommand(20);
 
+            //Act
             var xmlElements = pausePlaybackCommand.Serialize();
     
-            Assert.IsNotNull(JToken.Parse(xmlElements));
+            //Assert
+            Assert.IsNotNull(JToken.Parse(xmlElements));//Verify that the json serialized is valid
 
+            //This will execute the exception section in RecordableCommand(string tag), because tag is null
             Assert.Throws<System.Reflection.TargetInvocationException>(() => executive.ExecuteCommand(cmd.Object, "TestRecordable", "ExtensionTests"));
-            Assert.AreEqual(-1, cmdExecutionState);
-
+            Assert.AreEqual(-1, cmdExecutionState);//It means that the command was not executed successfully
         }
 
+        /// <summary>
+        /// This Test Method will execute the neext methods in RecordableCommands class
+        /// static RecordableCommand Deserialize(string jsonString)
+        /// internal virtual void TrackAnalytics() 
+        /// </summary>
         [Test]
         [Category("UnitTests")]
         public void TestRecordableCommandDeserialize()
         {
-            var cmd = new Mock<DynamoModel.RecordableCommand>(null);
+            //Arrange
+            var numberNode1 = new DoubleInput();
+            numberNode1.Value = "1";
+            var numberNode2 = new DoubleInput();
+            numberNode2.Value = "2";
 
-            var pausePlaybackCommand = new DynamoModel.AddPresetCommand("PresetName","Description");
+            DynamoSelection.Instance.ClearSelection();
+            //create the first state with the numbers selected
+            DynamoSelection.Instance.Selection.Add(numberNode1);
+            DynamoSelection.Instance.Selection.Add(numberNode2);
 
-            var xmlElements = pausePlaybackCommand.Serialize();
-
+            var ids = DynamoSelection.Instance.Selection.OfType<NodeModel>().Select(x => x.GUID).ToList();
+            var AddPresetCommand = new DynamoModel.AddPresetCommand("PresetName","Description",ids);
+            
             // Serialize the command into an XmlElement.
             XmlDocument xmlDocument = new XmlDocument();
-            XmlElement element = pausePlaybackCommand.Serialize(xmlDocument);
-            Assert.IsNotNull(element);
+            XmlElement elementAddPresetCommand = AddPresetCommand.Serialize(xmlDocument);
 
+            var jsonAddPresetCommand = AddPresetCommand.Serialize();
+
+
+            var helper = new XmlElementHelper(elementAddPresetCommand);
+            Guid gWorkspace = Guid.NewGuid();
+            Guid gState = Guid.NewGuid();
+            var ApplyPresetCommand = new DynamoModel.ApplyPresetCommand(gWorkspace, gState);
+
+            //This will execute the empty method TrackAnalytics()
+            ApplyPresetCommand.TrackAnalytics();
+            XmlElement elementApplyPresetCommand = ApplyPresetCommand.Serialize(xmlDocument);
+
+            //Act
             // Deserialized the XmlElement into a new instance of the command.
-            var duplicate = DynamoModel.RecordableCommand.Deserialize(element);
+            var deserializedAddPresetCommand = DynamoModel.RecordableCommand.Deserialize(elementAddPresetCommand);
+            var deserializedApplyPresetCommand = DynamoModel.RecordableCommand.Deserialize(elementApplyPresetCommand);
 
-            Assert.IsNotNull(JToken.Parse(xmlElements));
+            //This will execute the overloaded Deserialize method (the one receiving a string as parameter)
+            var deserializedFromJson = DynamoModel.RecordableCommand.Deserialize(jsonAddPresetCommand);
 
-            Assert.Throws<System.Reflection.TargetInvocationException>(() => executive.ExecuteCommand(cmd.Object, "TestRecordable", "ExtensionTests"));
-            Assert.AreEqual(-1, cmdExecutionState);
+            //This will generate a invalid json string, so when it's deserialized will raise an exception
+            jsonAddPresetCommand = jsonAddPresetCommand.Replace('{', '<');
+
+            //This is a fake command so when it's deserialized will raise an exception
+            XmlElement elemTest = xmlDocument.CreateElement("TestCommand");
+            Assert.Throws<ArgumentException>(() => DynamoModel.RecordableCommand.Deserialize(elemTest));
+
+            //Assert
+            //This will check that the Deserialized commands are valid
+            Assert.IsNotNull(deserializedAddPresetCommand);
+            Assert.IsNotNull(deserializedApplyPresetCommand);
+            Assert.IsNotNull(deserializedFromJson);
+            Assert.Throws<ApplicationException>(() => DynamoModel.RecordableCommand.Deserialize(jsonAddPresetCommand));
+
 
         }
     }
