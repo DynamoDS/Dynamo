0cdd1908687f8e81c26fc15856e69cd307c3d2bc
Aaron (Qilong) <173288704@qq.com>
2020-05-06 12:17:23 -0400

    Python3 Selection Under Debug Modes (#10629)
    * Cherrypick Python3 changes
    
    * Cleanup
    
    * Use Debug Modes
    
    * CleanUp
    
    * Rename Function
    
    * Clean Up
    
    * Do not use anouymous function as handler
    
    * Revert newer language change

--- src/DynamoCore/Properties/AssemblyInfo.cs
+++ src/DynamoCore/Properties/AssemblyInfo.cs
@@ -30,9 +30,6 @@ using System.Runtime.CompilerServices;
 // Internals are visible to the Package Manager extension
 // For workspace package dependency collection
 [assembly: InternalsVisibleTo("DynamoPackages")]
-
-// Internals are visible to the Package Manager extension
-// For workspace package dependency collection
 [assembly: InternalsVisibleTo("WorkspaceDependencyViewExtension")]
-
+[assembly: InternalsVisibleTo("PythonNodeModelsWpf")]
 [assembly: InternalsVisibleTo("LibraryViewExtensionMSWebBrowser")]
