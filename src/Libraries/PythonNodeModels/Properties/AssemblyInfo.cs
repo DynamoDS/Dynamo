0136ee61119ef68e9404bd64479204a51664b8d8
gantaa <43763136+reddyashish@users.noreply.github.com>
2020-05-08 10:52:18 -0400

    Update AssemblyInfo.cs

--- src/Libraries/PythonNodeModels/Properties/AssemblyInfo.cs
+++ src/Libraries/PythonNodeModels/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PythonNodeModels")]
 [assembly: AssemblyCulture("")]
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a0debf47-0cf2-46e8-90b9-0c3d9b808fd7")] 
+[assembly: Guid("a0debf47-0cf2-46e8-90b9-0c3d9b808fd7")]
