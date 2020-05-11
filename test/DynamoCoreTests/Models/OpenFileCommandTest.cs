9d3f90ab6f46c4e7795dbd4af12c6dd329a71308
Roberto Tellez <roberto.tellez@globant.com>
2020-05-11 12:21:32 -0500

    DYN-2560 - Increase the code coverage: DynamoCore/Models 2
    I added nunit test method for the next classes:
    
    - CreateAndConnectNodeCommand
    - CreateCustomNodeCommand
    - CreateNodeCommand
    - CreateNoteCommand
    - CreateProxyNodeCommand
    - ForceRunCancelCommand
    - GroupModelCommand
    - ModelEventCommand
    - MutateTestCommand
    - OpenFileCommand
    - PausePlaybackCommand
    - PresetCommand
    - RecordableCommands
    - SelectModelCommand
    - UndoRedoCommand
    - UpdateModelValueCommand

--- test/DynamoCoreTests/Models/OpenFileCommandTest.cs
+++ test/DynamoCoreTests/Models/OpenFileCommandTest.cs
@@ -10,28 +10,85 @@ namespace Dynamo.Tests.ModelsTest
     [TestFixture]
     class OpenFileCommandTest : DynamoModelTestBase
     {
+        /// <summary>
+        /// This test method will execute the next methods:
+        /// static string TryFindFile(string xmlFilePath, string uriString = null)
+        /// protected override void SerializeCore(XmlElement element)
+        /// void ExecuteCore(DynamoModel dynamoModel)
+        /// void TrackAnalytics()
+        /// </summary>
         [Test]
         [Category("UnitTests")]
         public void TestTryFindFile()
         {
+            //Arrange
             string wspath = Path.Combine(TestDirectory, @"core\callsite\RebindingSingleDimension.dyn");
-
             var fileCommand = new DynamoModel.OpenFileCommand(wspath);          
 
+            //We need a XmlDocument instance to create a new custom XmlElement
             XmlDocument xmlDocument = new XmlDocument();
             XmlElement elemTest = xmlDocument.CreateElement("TestCommand");
 
+            //Act
+            var command = new OpenFileCommandDerivedTest(wspath);
+            command.SerializeCoreTest(elemTest);
+            var deserializedCommand = OpenFileCommand.DeserializeCore(elemTest);
+
+            //We need to set up the DynamoModel inside the OpenFileCommandDerivedTest class before calling TrackAnalytics()
+            command.ExecuteTest(CurrentDynamoModel);
+            command.TrackAnalytics();
+
+            //Assert
+            //It will validate that the Deserialized element is valid
+            Assert.IsNotNull(deserializedCommand);
+        }
+
+        /// <summary>
+        ///  This test method will execute the TryFindFile (the final part of the function)
+        ///  Then the dyn file passed as parameter doesn't exists
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void TestTryFindFileUri()
+        {
+            //Arrange
+            //The dyn file passed as parameter doesn't exists, so a different part in TryFindFile method will be executed
+            string wspath = Path.Combine(TestDirectory, @"core\callsite\RebindingSingleDimension1.dyn");
+
+            var fileCommand = new DynamoModel.OpenFileCommand(wspath);
+
+            CustomXmlDocument xmlDocument = new CustomXmlDocument();
+
+            //We set the URI to be fake so when it's split on TryFindFile it will execute a different code section
+            xmlDocument.CustomBaseURL = "http://localhost/test";
+            XmlElement elemTest = xmlDocument.CreateElement("TestCommand");
+
+            //Act
             var command = new OpenFileCommandDerivedTest(wspath);
             command.SerializeCoreTest(elemTest);
 
-            var deserializedCommand = OpenFileCommand.DeserializeCore(elemTest);
+            //Assert
+            Assert.Throws<FileNotFoundException>(() => OpenFileCommand.DeserializeCore(elemTest));
+        }
+    }
+    
+    /// <summary>
+    /// This test class was created due that we need to set the baseURL but the property is private, then one way is using a derived class
+    /// </summary>
+    public class CustomXmlDocument : XmlDocument
+    {
+        public string CustomBaseURL { get; set; }
 
-            Assert.IsNotNull(deserializedCommand);
+        public override string BaseURI
+        {
+            get { return this.CustomBaseURL; }
         }
     }
 
     /// <summary>
-    /// This test class was created with the purpose of execute the protected method ExecuteCore(DynamoModel dynamoModel)
+    /// This test class was created with the purpose of execute in the Derived class the next protected methods 
+    /// void ExecuteCore(DynamoModel dynamoModel)
+    /// void SerializeCore(XmlElement element)
     /// </summary>
     class OpenFileCommandDerivedTest : OpenFileCommand
     {
@@ -58,7 +115,7 @@ namespace Dynamo.Tests.ModelsTest
             }
             catch (Exception ex)
             {
-                throw;
+                throw;//re-
             }
         }
 
