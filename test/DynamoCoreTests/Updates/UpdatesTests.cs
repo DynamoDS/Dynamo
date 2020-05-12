15d94cfe0d7f59b0f492bd89a1199b9db6e89676
Bruno Yorda <bruno.yorda@globant.com>
2020-05-12 14:54:42 -0500

    Added more tests for UpdateManager.cs

--- test/DynamoCoreTests/Updates/UpdatesTests.cs
+++ test/DynamoCoreTests/Updates/UpdatesTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Dynamo.Updates;
 using NUnit.Framework;
@@ -20,6 +21,7 @@ namespace Dynamo.Tests
             updateManager = new UpdateManager(config);
         }
 
+        #region UpdateDownloadedEventArgs
         [Test]
         [Category("UnitTests")]
         public void UpdateDownloadedEventArgsConstructorTest()
@@ -37,7 +39,9 @@ namespace Dynamo.Tests
 
             Assert.IsFalse(eventArgs.UpdateAvailable);
         }
+        #endregion
 
+        #region UpdateManagerConfiguration
         [Test]
         [Category("UnitTests")]
         public void UpdateManagerConfiguration_LoadTest()
@@ -47,5 +51,88 @@ namespace Dynamo.Tests
             Assert.IsNull(UpdateManagerConfiguration.Load("filePath", updateManager));
             Assert.IsNull(UpdateManagerConfiguration.Load(Path.GetTempFileName(), updateManager));
         }
+
+        [Test]
+        [Category("UnitTests")]
+        public void UpdateManagerConfiguration_SaveTest()
+        {
+            var config = new UpdateManagerConfiguration();
+            Assert.DoesNotThrow(() => config.Save(Path.GetTempFileName(), updateManager));
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void CheckNewerDailyBuildTest()
+        {
+            var config = new UpdateManagerConfiguration();
+            var oldValue = config.CheckNewerDailyBuild;
+
+            config.CheckNewerDailyBuild = !config.CheckNewerDailyBuild;
+            Assert.AreEqual(!oldValue, config.CheckNewerDailyBuild);
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void ForceUpdateTest()
+        {
+            var config = new UpdateManagerConfiguration();
+            var oldValue = config.ForceUpdate;
+
+            config.ForceUpdate = !config.ForceUpdate;
+            Assert.AreEqual(!oldValue, config.ForceUpdate);
+        }
+        #endregion
+
+        #region UpdateManager
+        [Test]
+        [Category("UnitTests")]
+        public void QuitAndInstallUpdateTest()
+        {
+            Assert.DoesNotThrow(() => updateManager.QuitAndInstallUpdate());
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void HostApplicationBeginQuitTest()
+        {
+            Assert.DoesNotThrow(() => updateManager.HostApplicationBeginQuit());
+        }
+        #endregion
+
+        #region DynamoLookup
+        private class DynamoLookupChildTest : DynamoLookUp
+        {
+            public override IEnumerable<string> GetDynamoInstallLocations()
+            {
+                return new List<string>();
+            }
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void DynamoLookupTests()
+        {
+            var lookup = new DynamoLookupChildTest();
+
+            Assert.IsNull(lookup.LatestProduct);
+            Assert.IsNull(lookup.GetDynamoVersion(Path.GetTempPath()));
+            Assert.IsNotNull(lookup.GetDynamoUserDataLocations());
+        }
+        #endregion
+
+        #region UpdateRequest
+        [Test]
+        [Category("UnitTests")]
+        public void UpdateRequest_ConstructorTest()
+        {
+            var path = new Uri(Path.GetTempPath());
+            var updateRequest = new UpdateRequest(path, updateManager);
+
+            Assert.IsNotNull(updateRequest.OnRequestCompleted);
+            Assert.IsNotNull(updateRequest.Data);
+            Assert.IsNotNull(updateRequest.Error);
+            Assert.IsNotNull(updateRequest.Path);
+        }
+        #endregion
     }
 }
