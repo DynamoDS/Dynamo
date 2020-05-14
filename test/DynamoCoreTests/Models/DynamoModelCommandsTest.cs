d07f9ca894494ef2575ca54bc3037a9803bcda21
Roberto Tellez <roberto.tellez@globant.com>
2020-05-14 11:36:25 -0500

    DYN-2560 - Models - Folder3
    Renamed some test methods to avoid using "_" in the name.
    Fixed some test method comments.
    Also I added more test cases for the DynamoModel class.

--- test/DynamoCoreTests/Models/DynamoModelCommandsTest.cs
+++ test/DynamoCoreTests/Models/DynamoModelCommandsTest.cs
@@ -2,31 +2,50 @@
 using System.IO;
 using System.Linq;
 using System.Xml;
+using CoreNodeModels;
 using Dynamo.Graph.Nodes;
 using Dynamo.Graph.Nodes.ZeroTouch;
 using Dynamo.Models;
 using Dynamo.Selection;
 using Dynamo.Utilities;
 using NUnit.Framework;
-using DynCmd = Dynamo.Models.DynamoModel;
 
 namespace Dynamo.Tests.ModelsTest
 {
     /// <summary>
-    /// This test class will contain test method for executing methods/properties in the DynamoModel class
+    /// This test class will contain test method for executing methods in the DynamoModel class
     /// </summary>
     [TestFixture]
     class DynamoModelCommandsTest : DynamoModelTestBase
     {
         /// <summary>
-        /// This test method will execute the DynamoModel_CreateAndConnectNodeImpl method from the DynamoModel class
+        /// This test method will execute the ForceRunCancelImpl method from the DynamoModel class
         /// </summary>
         [Test]
         [Category("UnitTests")]
-        public void DynamoModel_CreateAndConnectNodeImpl()
+        public void ForceRunCancelImplTest()
         {
             //Arrange
             Guid newNodeGuid = Guid.NewGuid();
+            var command = new DynamoModel.ForceRunCancelCommand(true, true);
+   
+            //Act
+            CurrentDynamoModel.ExecuteCommand(command);
+
+            //Assert
+            Assert.IsNotNull(command);
+        }
+
+        /// <summary>
+        /// This test method will execute the CreateAndConnectNodeImpl method from the DynamoModel class
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void CreateAndConnectNodeImplTest()
+        {
+            //Arrange
+            Guid newNodeGuid = Guid.NewGuid();
+            //This guid belongs to a CoreNodeModels.CreateList node inside the .dyn file
             Guid existingNodeGuid = new Guid("81c94fd0-35a0-4680-8535-00aff41192d3");
             double x = 100;
             double y = 190;
@@ -58,18 +77,18 @@ namespace Dynamo.Tests.ModelsTest
         /// </summary>
         [Test]
         [Category("UnitTests")]
-        public void DynamoModel_GetNodeFromCommand()
+        public void GetNodeFromCommandTest()
         {
             //Arrange
             Guid newNodeGuid = Guid.NewGuid();
 
-            var command = new DynCmd.CreateNodeCommand(newNodeGuid.ToString(), "NodeName1", 0, 0, true, true);
-            var command2 = new DynCmd.CreateNodeCommand(newNodeGuid.ToString(), "CoreNodeModels.Watch", 0, 0, true, true);
+            var command = new DynamoModel.CreateNodeCommand(newNodeGuid.ToString(), "NodeName1", 0, 0, true, true);
+            var command2 = new DynamoModel.CreateNodeCommand(newNodeGuid.ToString(), "CoreNodeModels.Watch", 0, 0, true, true);
 
             //This will execute the proxy section code in the GetNodeFromCommand() method
-            var commandProxy = new DynCmd.CreateProxyNodeCommand(newNodeGuid.ToString(), "CoreNodeModels.Watch", 0, 0, true, true,"ProxyCommand1",0,0);
+            var commandProxy = new DynamoModel.CreateProxyNodeCommand(newNodeGuid.ToString(), "CoreNodeModels.Watch", 0, 0, true, true,"ProxyCommand1",0,0);
 
-            //Act//Assert
+            //Assert
             //Calling the ExecuteCommand internally executes the GetNodeFromCommand() method
             Assert.Throws<Exception>( () => CurrentDynamoModel.ExecuteCommand(command));
             CurrentDynamoModel.ExecuteCommand(command2);
@@ -83,16 +102,16 @@ namespace Dynamo.Tests.ModelsTest
         /// </summary> 
         [Test]
         [Category("UnitTests")]
-        public void DynamoModel_GetNodeFromCommand_DuplicatedGuids()
+        public void GetNodeFromCommand_DuplicatedGuidsTest()
         {
             //Arrange
-            //This guid already exist in the .dyn file (List.Create), then it will execute a specific code in GetNodeFromCommand method
+            //This guid belongs to a CoreNodeModels.CreateList node inside the .dyn file
             Guid existingNodeGuid = new Guid("81c94fd0-35a0-4680-8535-00aff41192d3");
 
             string openPath = Path.Combine(TestDirectory, @"core\DetailedPreviewMargin_Test.dyn");
             RunModel(openPath);
 
-            var command = new DynCmd.CreateNodeCommand(existingNodeGuid.ToString(), "List.Create", 0, 0, true, true);  
+            var command = new DynamoModel.CreateNodeCommand(existingNodeGuid.ToString(), "List.Create", 0, 0, true, true);  
 
             //Act
             CurrentDynamoModel.ExecuteCommand(command);
@@ -110,7 +129,7 @@ namespace Dynamo.Tests.ModelsTest
         /// </summary> 
         [Test]
         [Category("UnitTests")]
-        public void DynamoModel_GetNodeFromCommand_XML()
+        public void GetNodeFromCommand_XMLTest()
         {
             //Arrange
             XmlDocument xmlDocument = new XmlDocument();
@@ -133,7 +152,7 @@ namespace Dynamo.Tests.ModelsTest
             elemTest.AppendChild(elemTestChild);
 
             //Act
-            var createNodeCommand = DynCmd.CreateNodeCommand.DeserializeCore(elemTest);
+            var createNodeCommand = DynamoModel.CreateNodeCommand.DeserializeCore(elemTest);
             //Calling Execute method will call internally the GetNodeFromCommand method
             createNodeCommand.Execute(CurrentDynamoModel);
 
@@ -143,67 +162,96 @@ namespace Dynamo.Tests.ModelsTest
             Assert.AreEqual(createNodeCommand.NodeXml.Name, "TestCommandChild");
         }
 
-        
+        /// <summary>
+        /// This test method will execute the SelectModelImpl method from the DynamoModel class
+        /// </summary>
         [Test]
         [Category("UnitTests")]
-        public void DynamoModel_SelectModelImpl()
+        public void SelectModelImplTest()
         {
+            //Arrange
             string openPath = Path.Combine(TestDirectory, @"core\DetailedPreviewMargin_Test.dyn");
             RunModel(openPath);
 
-            Guid id = Guid.NewGuid();
+            //This will add a new DSFunction node to the current workspace
             var addNode = new DSFunction(CurrentDynamoModel.LibraryServices.GetFunctionDescriptor("+"));
             CurrentDynamoModel.CurrentWorkspace.AddAndRegisterNode(addNode, false);
 
             //Select the node and notes
             DynamoSelection.Instance.Selection.Add(addNode);
-
             var ids = DynamoSelection.Instance.Selection.OfType<NodeModel>().Select(x => x.GUID).ToList();
             var selectCommand = new DynamoModel.SelectModelCommand(ids,ModifierKeys.Shift);
 
+            //Act
             CurrentDynamoModel.ExecuteCommand(selectCommand);
+
+            //Assert
+            Assert.Greater(ids.Count, 0); //At least one guid was found
+            Assert.IsNotNull(selectCommand);
         }
 
-        
+        /// <summary>
+        /// This test method will execute the BeginDuplicateConnection method from the DynamoModel class
+        /// </summary>
         [Test]
         [Category("UnitTests")]
-        public void DynamoModel_BeginDuplicateConnection()
+        public void BeginDuplicateConnectionTest()
         {
+            //Arrange
             string openPath = Path.Combine(TestDirectory, @"core\DetailedPreviewMargin_Test.dyn");
             RunModel(openPath);
 
             Guid id = Guid.NewGuid();
+            //This guid belongs to a CoreNodeModels.CreateList node inside the .dyn file
             Guid existingNodeGuid = new Guid("81c94fd0-35a0-4680-8535-00aff41192d3");
 
-            var connectionCommand = new DynamoModel.MakeConnectionCommand(id,0,PortType.Input, DynamoModel.MakeConnectionCommand.Mode.BeginDuplicateConnection);
+            //Connection Command with one Input port using a new guid
+            var connectionCommand = new DynamoModel.MakeConnectionCommand(id, 0,PortType.Input, DynamoModel.MakeConnectionCommand.Mode.BeginDuplicateConnection);
 
+            //Connection Command with one Output port using a new guid
             var connectionCommand2 = new DynamoModel.MakeConnectionCommand(id, 0, PortType.Output, DynamoModel.MakeConnectionCommand.Mode.BeginDuplicateConnection);
 
-            var codeBlockNode1 = CreateCodeBlockNode();
-            UpdateCodeBlockNodeContent(codeBlockNode1, "3;");
+            var watch1 = new Watch { X = 100, Y = 300 };
+            CurrentDynamoModel.CurrentWorkspace.AddAndRegisterNode(watch1, false);
+            //Connection Command using a guid from a Watch node addded previosuly
+            var connectionCommand3 = new DynamoModel.MakeConnectionCommand(watch1.GUID, 0, PortType.Input, DynamoModel.MakeConnectionCommand.Mode.BeginDuplicateConnection);
 
-            var connectionCommand3 = new DynamoModel.MakeConnectionCommand(existingNodeGuid, 0, PortType.Output, DynamoModel.MakeConnectionCommand.Mode.BeginDuplicateConnection);
-
+            //Act
+            //The ExecuteCommand method internally will execute the BeginDuplicateConnectionTest method
             CurrentDynamoModel.ExecuteCommand(connectionCommand);
             CurrentDynamoModel.ExecuteCommand(connectionCommand2);
             CurrentDynamoModel.ExecuteCommand(connectionCommand3);
+
+            //Assert
+            //Validating that the commands were created successfully
+            Assert.IsNotNull(connectionCommand);
+            Assert.IsNotNull(connectionCommand2);
+            Assert.IsNotNull(connectionCommand3);
         }
 
-        private CodeBlockNodeModel CreateCodeBlockNode()
+        [Test]
+        [Category("UnitTests")]
+        public void UngroupGrouplImplTest()
         {
-            var cbn = new CodeBlockNodeModel(CurrentDynamoModel.LibraryServices);
-            var command = new DynCmd.CreateNodeCommand(cbn, 0, 0, true, false);
+            //Arrange
+            string openPath = Path.Combine(TestDirectory, @"core\DetailedPreviewMargin_Test.dyn");
+            RunModel(openPath);
 
-            CurrentDynamoModel.ExecuteCommand(command);
+            //This will add a new DSFunction node to the current workspace
+            var addNode = new DSFunction(CurrentDynamoModel.LibraryServices.GetFunctionDescriptor("+"));
+            CurrentDynamoModel.CurrentWorkspace.AddAndRegisterNode(addNode, false);
 
-            Assert.IsNotNull(cbn);
-            return cbn;
-        }
+            //Select the node and notes
+            DynamoSelection.Instance.Selection.Add(addNode);
+            var ids = DynamoSelection.Instance.Selection.OfType<NodeModel>().Select(x => x.GUID).ToList();
+            var selectCommand = new DynamoModel.SelectModelCommand(ids, ModifierKeys.Shift);
 
-        private void UpdateCodeBlockNodeContent(CodeBlockNodeModel cbn, string value)
-        {
-            var command = new DynCmd.UpdateModelValueCommand(Guid.Empty, cbn.GUID, "Code", value);
-            CurrentDynamoModel.ExecuteCommand(command);
+            //Act
+            CurrentDynamoModel.ExecuteCommand(selectCommand);
+
+            //Assert
+            Assert.Greater(ids.Count, 0); //At least one guid was found
+            Assert.IsNotNull(selectCommand);
         }
     }
 }
