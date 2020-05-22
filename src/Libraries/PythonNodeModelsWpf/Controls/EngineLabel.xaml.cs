e3d60fbe3a9aa009308fa2233bdd0091eb30caf1
Sylvester Knudsen <sylvesterknudsen@gmail.com>
2020-05-22 16:14:18 +0100

    Python ux improvements (#10607)
    * Add Engine property to PythonNodeBase
    
    * Add engine selector dropdown to python editor
    
    * Add notification API to nodemodel
    
    * add warnings to resources
    
    * Add IronPython dialog on open saved workspace
    
    * Add Engine label on node
    
    * make LogNotification internal
    
    * python engine enum
    
    * Python migration extension DRAFT (#1)
    
    * Python migration extension DRAFT
    
    * update extension
    
    * small cleanup
    
    * move dialog and notification strings to extension resource
    
    * Update PythonMigrationViewExtension.cs
    
    * added python engine label to From String Node
    
    * add engine selector to python script from string node in context menu
    
    * Update EngineLabel.xaml
    
    * comment updates
    
    * Update Dynamo.All.sln
    
    * Update PythonMigrationViewExtension.cs
    
    * Update PythonMigrationViewExtension.cs
    
    * comment updates
    
    * Update Resources.Designer.cs
    
    * update csproj + small cleanup
    
    * added more information btn to IronPythonDialog
    
    * comment updates
    
    * tests added
    
    * tests
    
    * comment updates
    
    * Updates dialog, notification and documentationbrowser text
    
    * update tests
    
    * fix dialog box and small clean up
    
    * warning html update
    
    * updates to warning messages
    
    * Update PythonStringNode.cs
    
    Co-authored-by: Michael Dewberry <michael.dewberry@autodesk.com>

---
+++ src/Libraries/PythonNodeModelsWpf/Controls/EngineLabel.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PythonNodeModels;
+
+namespace PythonNodeModelsWpf.Controls
+{
+    /// <summary>
+    /// Interaction logic for EngineLabel.xaml
+    /// </summary>
+    public partial class EngineLabel : UserControl
+    {
+        private PythonNodeBase NodeModel { get; set; }
+        public EngineLabel(PythonNodeModels.PythonNodeBase nodeModel)
+        {
+            InitializeComponent();
+            NodeModel = nodeModel;
+        }
+    }
+}
---
+++ src/Libraries/PythonNodeModelsWpf/PythonStringNode.cs
@@ -0,0 +1,86 @@
+﻿using Dynamo.Controls;
+using Dynamo.Graph.Workspaces;
+using Dynamo.ViewModels;
+using Dynamo.Wpf;
+using PythonNodeModels;
+using PythonNodeModelsWpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PythonNodeModelsWpf
+{
+    class PythonStringNodeViewCustomization : VariableInputNodeViewCustomization, INodeViewCustomization<PythonStringNode>
+    {
+        private PythonStringNode pythonStringNodeModel;
+        private NodeView pythonStringNodeView;
+        private MenuItem pythonEngine2Item = new MenuItem { Header = PythonNodeModels.Properties.Resources.PythonNodeContextMenuEngineVersionTwo, IsCheckable = true };
+        private MenuItem pythonEngine3Item = new MenuItem { Header = PythonNodeModels.Properties.Resources.PythonNodeContextMenuEngineVersionThree, IsCheckable = true };
+
+        public void CustomizeView(PythonStringNode nodeModel, NodeView nodeView)
+        {
+            base.CustomizeView(nodeModel, nodeView);
+
+            pythonStringNodeModel = nodeModel;
+            pythonStringNodeView = nodeView;
+
+            // If it is a Debug build, display a python engine switcher
+            if (Dynamo.Configuration.DebugModes.IsEnabled("Python3DebugMode"))
+            {
+                var pythonEngineVersionMenu = new MenuItem { Header = PythonNodeModels.Properties.Resources.PythonNodeContextMenuEngineSwitcher, IsCheckable = false };
+                nodeView.MainContextMenu.Items.Add(pythonEngineVersionMenu);
+                pythonEngine2Item.Click += UpdateToPython2Engine;
+                pythonEngine3Item.Click += UpdateToPython3Engine;
+                pythonEngineVersionMenu.Items.Add(pythonEngine2Item);
+                pythonEngineVersionMenu.Items.Add(pythonEngine3Item);
+
+                // Check the correct item based on NodeModel engine version
+                if (pythonStringNodeModel.Engine == PythonEngineVersion.IronPython2)
+                {
+                    pythonEngine2Item.IsChecked = true;
+                    pythonEngine3Item.IsChecked = false;
+                }
+                else
+                {
+                    pythonEngine2Item.IsChecked = false;
+                    pythonEngine3Item.IsChecked = true;
+                }
+            }
+
+            nodeModel.Disposed += NodeModel_Disposed;
+
+            nodeView.PresentationGrid.Visibility = Visibility.Visible;
+            nodeView.PresentationGrid.Children.Add(new EngineLabel(pythonStringNodeModel));
+        }
+
+        private void NodeModel_Disposed(Dynamo.Graph.ModelBase obj)
+        {
+            pythonEngine2Item.Click -= UpdateToPython2Engine;
+            pythonEngine3Item.Click -= UpdateToPython3Engine;
+        }
+
+        /// <summary>
+        /// MenuItem click handler
+        /// </summary>
+        private void UpdateToPython2Engine(object sender, EventArgs e)
+        {
+            pythonStringNodeModel.Engine = PythonEngineVersion.IronPython2;
+            pythonEngine2Item.IsChecked = true;
+            pythonEngine3Item.IsChecked = false;
+        }
+
+        /// <summary>
+        /// MenuItem click handler
+        /// </summary>
+        private void UpdateToPython3Engine(object sender, EventArgs e)
+        {
+            pythonStringNodeModel.Engine = PythonEngineVersion.CPython3;
+            pythonEngine2Item.IsChecked = false;
+            pythonEngine3Item.IsChecked = true;
+        }
+    }
+}
