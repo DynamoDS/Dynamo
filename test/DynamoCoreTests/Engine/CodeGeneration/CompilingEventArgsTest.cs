3aef5fc43bba5d68fcaa6d10dbd6dbe1f332f75b
Roberto Tellez <roberto.tellez@globant.com>
2020-05-27 17:17:24 -0500

    DYN-2562-Coverage-Engine2
    I created nUnit test methods for the next classes:
    
    - CompilingEventArgs
    - ProfilingData
    - NodeProfilingData

---
+++ test/DynamoCoreTests/Engine/CodeGeneration/CompilingEventArgsTest.cs
@@ -0,0 +1,28 @@
+﻿using Dynamo.Engine.CodeGeneration;
+using NUnit.Framework;
+using ProtoCore.AST.AssociativeAST;
+using System;
+using System.Collections.Generic;
+
+namespace Dynamo.Tests.Engine.CodeGeneration
+{
+    [TestFixture]
+    class CompilingEventArgsTest
+    {
+        [Test]
+        [Category("UnitTests")]
+        public void TestInternalMigration()
+        {
+            //Arrange
+            var guid = Guid.NewGuid();
+            var associativeNodes = new List<AssociativeNode> { new IntNode(1), new IntNode(2) };
+            var eventArgsCompiling = new CompilingEventArgs(guid);
+            var eventArgsCompiled = new CompiledEventArgs(guid, associativeNodes);
+
+            //Assert
+            Assert.AreEqual(eventArgsCompiling.NodeId, guid);
+            Assert.AreEqual(eventArgsCompiled.NodeId, guid);
+            Assert.AreEqual((eventArgsCompiled.AstNodes as List<AssociativeNode>).Count, associativeNodes.Count);
+        }
+    }
+}
---
+++ test/DynamoCoreTests/Engine/Profiling/ProfilingDataTest.cs
@@ -0,0 +1,81 @@
+﻿using Dynamo.Engine.Profiling;
+using Dynamo.Graph.Nodes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DynCmd = Dynamo.Models.DynamoModel;
+
+namespace Dynamo.Tests.Engine.Profiling
+{
+    [TestFixture]
+    class ProfilingDataTest : DynamoModelTestBase
+    {
+        [Test]
+        [Category("UnitTests")]
+        public void ProfilingDataRegisterUnRegisterNodesTest()
+        {
+            //Arrange
+            var nodesList = new List<NodeModel>();
+            var profiling = new ProfilingData();
+            var cbn = CreateCodeBlockNode();
+
+            //Act
+            profiling.RegisterNode(cbn);
+            var nodeData = profiling.NodeProfilingData;
+
+            //Assert
+            Assert.IsNotNull(nodeData);
+            Assert.AreEqual(nodeData.Count, 1);
+
+            //Act
+            profiling.UnregisterNode(nodeData.First().Key);
+
+            //Assert
+            Assert.AreEqual(nodeData.Count, 0);
+
+            var cbn2 = CreateCodeBlockNode();
+            nodesList.Add(cbn2);
+           
+            //Act
+            profiling.UnregisterDeletedNodes(nodesList);
+            nodeData = profiling.NodeProfilingData;
+
+            //Assert
+            Assert.AreEqual(nodeData.Count, 0);
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void NodeProfilingDataDispose()
+        {
+            //Arrange
+            var profiling = new ProfilingData();
+            var cbn = CreateCodeBlockNode();
+            profiling.RegisterNode(cbn);
+            var nodeData = profiling.NodeProfilingData;
+
+            //Act
+            var timeSpan = nodeData.First().Value.ExecutionTime;
+            nodeData.First().Value.Dispose();
+
+            //Assert
+            Assert.IsNull(timeSpan);
+            Assert.IsNotNull(nodeData);
+            Assert.AreEqual(nodeData.Count, 1);
+
+          
+        }
+
+        private CodeBlockNodeModel CreateCodeBlockNode()
+        {
+            var cbn = new CodeBlockNodeModel(CurrentDynamoModel.LibraryServices);
+            var command = new DynCmd.CreateNodeCommand(cbn, 0, 0, true, false);
+
+            CurrentDynamoModel.ExecuteCommand(command);
+
+            Assert.IsNotNull(cbn);
+            return cbn;
+        }
+    }
+}
