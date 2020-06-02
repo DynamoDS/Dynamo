11754dd667526fbdc00499154383753b8503f5b9
aparajit-pratap <aparajit.pratap@autodesk.com>
2020-06-02 15:45:58 -0400

    resolve merge conflicts with mike's commit

--- src/DynamoCoreWpf/ViewModels/Watch3D/DynamoLineGeometryModel3D.cs
+++ src/DynamoCoreWpf/ViewModels/Watch3D/DynamoLineGeometryModel3D.cs
@@ -11,7 +11,7 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
     {
         protected override SceneNode OnCreateSceneNode()
         {
-            return new DynamoLineNode();
+            return new DynamoLineNode() {Material = material};
         }
 
         /// <summary>
