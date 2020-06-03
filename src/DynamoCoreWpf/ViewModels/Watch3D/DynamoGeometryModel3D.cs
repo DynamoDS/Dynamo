7b06dc47c4fd6e454518c82a10142c0d686eaf29
aparajit-pratap <aparajit.pratap@autodesk.com>
2020-06-03 18:17:10 -0400

    mark property obsolete

--- src/DynamoCoreWpf/ViewModels/Watch3D/DynamoGeometryModel3D.cs
+++ src/DynamoCoreWpf/ViewModels/Watch3D/DynamoGeometryModel3D.cs
@@ -51,7 +51,7 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
             return new DynamoMeshNode();
         }
 
-        // TODO: Make private in 3.0
+        [Obsolete("This property will be deprecated and made internal in Dynamo 3.0.")]
         public static readonly DependencyProperty RequiresPerVertexColorationProperty =
             DependencyProperty.Register("RequiresPerVertexColoration", typeof(bool), 
                 typeof(GeometryModel3D), new UIPropertyMetadata(false, RequirePerVertexColorationChanged));
