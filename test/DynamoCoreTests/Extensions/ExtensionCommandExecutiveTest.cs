f58da19fa6dd7b3b2726b385fb47c0b17da796a5
Roberto Tellez <roberto.tellez@globant.com>
2020-06-05 09:28:04 -0500

    DYN-2363-CodeCoverage-Entension-Folder
    I created nUnit Test Methods for the next classes inside the Entensions folder:
    
    - ExtensionCommandExecutive
    - ExtensionLoader
    - ExtensionManager
    
    Also I added the a invalid xml due that in a test method I needed to cover a section that is validating that the xml tags are right.
    Thanks

---
+++ test/DynamoCoreTests/Extensions/ExtensionCommandExecutiveTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dynamo.Extensions;
+using Dynamo.Models;
+using Dynamo.Scheduler;
+using Moq;
+using NUnit.Framework;
+
+namespace Dynamo.Tests.Extensions
+{
+    [TestFixture]
+    class ExtensionCommandExecutiveTest
+    {
+        int cmdExecutionState = -1;
+        ICommandExecutive executive;
+        Mock<IExtension> extMock;
+        private Logging.NotificationMessage message;
+        DynamoModel model;
+
+        //This callback method will be used in the Mocked Extension
+        private void ExtensionReadyCallback(ReadyParams ready)
+        {
+            executive = ready.CommandExecutive;
+            Assert.IsTrue(ready.WorkspaceModels.Any());
+            Assert.IsNotNull(ready.CurrentWorkspaceModel);
+            ready.NotificationRecieved += (Logging.NotificationMessage obj) => message = obj;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            extMock = new Mock<IExtension>();
+            extMock.Setup(ext => ext.Ready(It.IsAny<ReadyParams>())).Callback((ReadyParams r) => ExtensionReadyCallback(r));
+            cmdExecutionState = -1;
+
+            model = DynamoModel.Start(
+                new DynamoModel.DefaultStartConfiguration()
+                {
+                    StartInTestMode = true,
+                    Extensions = new List<IExtension> { extMock.Object },
+                    ProcessMode = TaskProcessMode.Synchronous,
+                });
+            model.DebugSettings.VerboseLogging = true;//Setting this flag to true will enable to execute a specific section in the ExecuteCommand() method
+        }
+
+        /// <summary>
+        /// This test method will execute the ublic void ExecuteCommand() method from the ExtensionCommandExecutive class
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void ExtensionsCommandExecutiveExecuteCommand()
+        {
+            //Arrange
+            var cmd = new Mock<DynamoModel.RecordableCommand>();
+            model.CommandStarting += OnCommandStarting;
+            model.CommandCompleted += OnCommandCompleted;
+
+            //Assert
+            //Check that the commands hasn't be executed
+            Assert.AreEqual(-1, cmdExecutionState);
+
+            //Act
+            //Due that VerboseLogging = true then when executing the ExecuteCommand method will reach the Logger section 
+            Assert.DoesNotThrow(() => executive.ExecuteCommand(cmd.Object, "TestRecordable", "ExtensionTests"));
+
+            //Assert
+            //Check that the command was already executed
+            Assert.AreEqual(0, cmdExecutionState);
+
+            //Unsubcribe from the command events
+            model.CommandStarting -= OnCommandStarting;
+            model.CommandCompleted -= OnCommandCompleted;
+        }
+
+        void OnCommandCompleted(DynamoModel.RecordableCommand command)
+        {
+            cmdExecutionState--;
+        }
+
+        void OnCommandStarting(DynamoModel.RecordableCommand command)
+        {
+            cmdExecutionState = 1;
+        }
+
+    }
+}
---
+++ test/DynamoCoreTests/Extensions/ExtensionLoaderTest.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Linq;
+using Dynamo.Extensions;
+using NUnit.Framework;
+
+namespace Dynamo.Tests.Extensions
+{
+    [TestFixture]
+    class ExtensionLoaderTest : DynamoModelTestBase
+    {
+        /// <summary>
+        /// This test method will execute the private IExtension Load(ExtensionDefinition extension) method from the ExtensionLoader class
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void ExtensionsLoadExtensionInvalidCertificate()
+        {
+            //Arrange
+            string extensionsPath = Path.Combine(TestDirectory, "pkgs\\sampleExtension\\extra");         
+            var extensionManager = new ExtensionManager(new[] { extensionsPath });
+
+            //Act
+            //The extension definition is located in a folder without certificate, then when calling the CheckAssemblyForValidCertificate() method it will raise an exception
+            var extensions = extensionManager.ExtensionLoader.LoadDirectory(extensionsPath);
+
+            //Assert
+            //Just checks that the extension was not loaded due to a invalid certificate (when trying to check the certificate, it will raise an exception handled internally)
+            Assert.AreEqual(extensions.Count(), 0);
+        }
+
+        /// <summary>
+        /// This test method will execute the  public IExtension Load(string extensionPath) method from the ExtensionLoader class, specifically the Log section
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void ExtensionsLoadExtensionWrongDefinition()
+        {
+            //Arrange
+            //The xml inside the wrong_extra is malformed.
+            string extensionsPath = Path.Combine(TestDirectory, "pkgs\\sampleExtension\\wrong_extra");
+            var extensionManager = new ExtensionManager();
+
+            //Act
+            //This will execute the Load(string extensionPath) method and due that the xml is malformed will execute the Log section
+            var extensions = extensionManager.ExtensionLoader.LoadDirectory(extensionsPath);
+
+            //Assert
+            //Just checks that NO extension was loaded in the local ExtensionManager
+            Assert.AreEqual(extensions.Count(), 0);
+        }
+    }
+}
