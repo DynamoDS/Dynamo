5c8e0c6ccce237e50547595f118d34d5a8143b2d
mjkkirschner <mjk.kirschner@gmail.com>
2020-06-12 16:15:25 -0400

    missing comments extra usings

--- src/DynamoCoreWpf/ViewModels/Core/DynamoViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Core/DynamoViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Configuration;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
