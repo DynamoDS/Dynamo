53ada95834ddeac58203d16ffe9b7d569eaf29cb
Bruno Yorda <bruno.yorda@globant.com>
2020-06-22 09:09:01 -0500

    Revert "Added tests for NodeModel"
    This reverts commit 4434d5fcbd1f452ac17d9555d73c6a185a37d5c8.

--- test/DynamoCoreTests/Graph/Nodes/NodeModelTests.cs
+++ test/DynamoCoreTests/Graph/Nodes/NodeModelTests.cs
@@ -1,70 +1,8 @@
-﻿using System.Collections.ObjectModel;
-using Dynamo.Graph.Nodes;
-using NUnit.Framework;
-using ProtoCore.AST;
-using Revit.Elements;
-
-namespace Dynamo.Tests
-{
-    /// <summary>
-    /// Class containing tests for the NodeModel class
-    /// </summary>
-    [TestFixture]
-    class NodeModelTests : DynamoModelTestBase
-    {
-        NodeModel testNodeModel;
-
-        [SetUp]
-        public void Init()
-        {
-            CurrentDynamoModel.NodeFactory.CreateNodeFromTypeName("CoreNodeModels.Input.DoubleInput", out testNodeModel);
-        }
 
         [Test]
         [Category("UnitTests")]
-        public void CreationNameTest()
+        public void Test()
         {
-            testNodeModel.Category = "Category";
-
-            var result = testNodeModel.ConstructDictionaryLinkFromLibrary(CurrentDynamoModel.LibraryServices);
-            Assert.AreEqual("http://dictionary.dynamobim.com/2/#/Category/Action/Number", result);
-        }
-        
-        [Test]
-        [Category("UnitTests")]
-        public void InPortsOutPortsTest()
-        {
-            var input = new PortModel(PortType.Input, testNodeModel, new PortData("input A", "This is input A."));
-            var inPorts = new ObservableCollection<PortModel>() { input };
-            testNodeModel.InPorts = inPorts;
-
-            Assert.Contains(input, testNodeModel.InPorts);
-
-            var output = new PortModel(PortType.Output, testNodeModel, new PortData("output A", "This is output A."));
-            var outPorts = new ObservableCollection<PortModel>() { output };
-            testNodeModel.OutPorts = outPorts;
-
-            Assert.Contains(output, testNodeModel.OutPorts);
-        }
-
-        [Test]
-        [Category("UnitTests")]
-        public void CategoryTest()
-        {
-            testNodeModel.Category = null; //Set Category to null so that GetCategoryStringFromAttributes() gets called in the property getter
-            var category = testNodeModel.Category;
-
-            Assert.AreEqual("Core.Input", category);
-        }
-
-        [Test]
-        [Category("UnitTests")]
-        public void DictionaryLinkTest()
-        {
-            testNodeModel.DictionaryLink = null; //Set DictionaryLink to null to cover the setter and to test the Configuration property
-            var dictLink = testNodeModel.DictionaryLink;
-
-            Assert.AreEqual("http://dictionary.dynamobim.com/2/", dictLink);
+            
         }
     }
-}
