4f3587f97ea792d73780371dc7dcb2ad980429e3
Bruno Yorda <bruno.yorda@globant.com>
2020-07-01 14:00:17 -0500

    Deprecated PrintExpression and removed the associated test.

--- test/DynamoCoreTests/Graph/Nodes/NodeModelTests.cs
+++ test/DynamoCoreTests/Graph/Nodes/NodeModelTests.cs
@@ -69,26 +69,5 @@ namespace Dynamo.Tests
             var countAfter = DynamoSelection.Instance.Selection.Count;
             Assert.AreEqual(2, countAfter);
         }
-
-        [Test]
-        [Category("UnitTests")]
-        public void PrintExpressionTest()
-        {
-            string listTestFolder = Path.Combine(TestDirectory, "core");
-            string angleFilePath = Path.Combine(listTestFolder, "Angle.dyn");
-
-            RunModel(angleFilePath);
-
-            NodeModel degreesToRadiansNode = CurrentDynamoModel.CurrentWorkspace.NodeFromWorkspace("dcd9c6c6-6350-4292-a553-c57a764504b4");
-            Assert.AreEqual("(Math.DegreesToRadians Number)", degreesToRadiansNode.PrintExpression());
-
-            //Cover the case when a node has one or more inputs not connected.
-            string nodeStatesFilePath = Path.Combine(listTestFolder, "NodeStates.dyn");
-            
-            RunModel(nodeStatesFilePath);
-
-            NodeModel createListNode = CurrentDynamoModel.CurrentWorkspace.NodeFromWorkspace("50219c24-e583-4b85-887c-409fb062da6e");
-            Assert.AreEqual("(lambda (colors value) (Color_Range colors Code_Block value))", createListNode.PrintExpression());
-        }
     }
 }
