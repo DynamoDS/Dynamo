edf927d6ac2485c8a7191269642739a17fae7931
Astul Betizagasti <astul.betizagasti@globant.com>
2020-07-09 17:45:19 -0300

    Tests1

---
+++ test/DynamoCoreTests/Graph/Workspaces/CustomNodeWorkspaceModelTests.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Linq;
+using Dynamo.Graph.Workspaces;
+using NUnit.Framework;
+
+namespace Dynamo.Tests
+{
+    /// <summary>
+    /// Class containing tests for the CustomNodeWorkspaceModel class
+    /// </summary>
+    [TestFixture]
+    class CustomNodeWorkspaceModelTests : DynamoModelTestBase
+    {
+        public void OpenTestFile(string folder, string fileName)
+        {
+            var examplePath = Path.Combine(TestDirectory, folder, fileName);
+            OpenModel(examplePath);
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void OnPropertyNameChangedTest()
+        {
+            OpenTestFile(@"core\CustomNodes", "add_Read_only.dyf");
+            var nodeWorkspace = CurrentDynamoModel.Workspaces.FirstOrDefault(x => x is CustomNodeWorkspaceModel);
+            Assert.IsNotNull(nodeWorkspace);
+
+            OpenTestFile(@"core\CustomNodes", "TestAdd.dyn");
+            var homeWorkspace = CurrentDynamoModel.CurrentWorkspace as HomeWorkspaceModel;
+            Assert.NotNull(homeWorkspace);
+
+            //Changes the Name property so that the event is raised
+            nodeWorkspace.Name = "NewNodeName";
+
+            var resultNode = homeWorkspace.Nodes.First(x => x.Name == "NewNodeName");
+            Assert.IsNotNull(resultNode);
+            Assert.IsTrue(resultNode.Name == "NewNodeName");
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void GetSharedNameTest()
+        {
+            var fileName = "add_Read_only";
+            OpenTestFile(@"core\CustomNodes", fileName+".dyf");
+            var nodeWorkspace = CurrentDynamoModel.Workspaces.FirstOrDefault(x => x is CustomNodeWorkspaceModel);
+            Assert.IsNotNull(nodeWorkspace);
+
+            var result = nodeWorkspace.GetSharedName();
+            Assert.AreEqual(fileName,result);
+
+            //Set fileName to null to simulate it is not a saved file
+            nodeWorkspace.FileName = null;
+            var expected = nodeWorkspace.Name;
+
+            result = nodeWorkspace.GetSharedName();
+
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
---
+++ test/DynamoCoreTests/Graph/Workspaces/HomeWorkspaceModelTests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Dynamo.Graph.Workspaces;
+using Dynamo.Models;
+using NUnit.Framework;
+
+namespace Dynamo.Tests
+{
+    /// <summary>
+    /// Class containing tests for the HomeWorkspaceModel class
+    /// </summary>
+    [TestFixture]
+    class HomeWorkspaceModelTests : DynamoModelTestBase
+    {
+        public void OpenTestFile(string folder, string fileName)
+        {
+            var examplePath = Path.Combine(TestDirectory, folder, fileName);
+            OpenModel(examplePath);
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void OnPreviewGraphCompletedTest()
+        {
+            //Loads a file with a valid graph on the Home workspace
+            OpenTestFile(@"core\number", "TestBigNumber.dyn");
+            var homeWorkspace = CurrentDynamoModel.CurrentWorkspace as HomeWorkspaceModel;
+            Assert.NotNull(homeWorkspace);
+
+            //Gets results from raised event
+            bool eventWasRaised = false;
+            string objectFileName = "";
+            homeWorkspace.SetNodeDeltaState += delegate(object workspace, DeltaComputeStateEventArgs d)
+            {
+                eventWasRaised = true;
+                var eventParamWorkspace = workspace as HomeWorkspaceModel;
+                objectFileName = eventParamWorkspace.FileName;
+            };
+
+            homeWorkspace.Run();
+            homeWorkspace.GetExecutingNodes(true);
+
+            Assert.IsTrue(eventWasRaised);
+            Assert.AreEqual(homeWorkspace.FileName, objectFileName);
+        }
+    }
+}
