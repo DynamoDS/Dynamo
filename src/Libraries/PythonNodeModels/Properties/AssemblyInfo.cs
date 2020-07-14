3f264dd6d877e8b354926f2e72cd38987d233dfc
SHKnudsen <sylvesterknudsen@gmail.com>
2020-07-14 17:12:15 +0100

    fix build failure

--- src/Libraries/PythonNodeModels/Properties/AssemblyInfo.cs
+++ src/Libraries/PythonNodeModels/Properties/AssemblyInfo.cs
@@ -11,8 +11,10 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a0debf47-0cf2-46e8-90b9-0c3d9b808fd7")]
 
-// Visible for PythonNodeModelsWpf 
-[assembly: InternalsVisibleTo("PythonNodeModelsWpf")]
 // Remove this when PythonEngineSelector become public
 [assembly: InternalsVisibleTo("DynamoPythonTests")]
 [assembly: InternalsVisibleTo("DynamoCoreWpfTests")]
+
+// Needed to use internal MigrationAssistantRequested event 
+[assembly: InternalsVisibleTo("PythonNodeModelsWpf")]
+[assembly: InternalsVisibleTo("PythonMigrationViewExtension")]
