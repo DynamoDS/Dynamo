34ce869f83691f48b59bbd1a09fbeb5bf3a3c023
Astul Betizagasti <astul.betizagasti@globant.com>
2020-07-23 16:22:10 -0300

    IdReferenceResolver Tests

---
+++ test/DynamoCoreTests/Graph/Workspaces/IdReferenceResolverTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using Dynamo.Graph.Nodes;
+using Dynamo.Graph.Workspaces;
+using Dynamo.Selection;
+using NUnit.Framework;
+using Revit.Elements;
+
+namespace Dynamo.Tests
+{
+    /// <summary>
+    /// Class containing tests for the IdReferenceResolver class
+    /// </summary>
+    [TestFixture]
+    class IdReferenceResolverTests : DynamoModelTestBase
+    {
+        private IdReferenceResolver idRefResolver;
+        private object testingObject;
+
+        [SetUp]
+        public void Init()
+        {
+            idRefResolver = new IdReferenceResolver();
+            testingObject = new object();
+        }
+
+        #region ReferenceModelMap Tests
+
+        [Test]
+        [Category("UnitTests")]
+        public void AddAndResolveReferenceFromMapTest()
+        {
+            //Guid to use as reference
+            Guid guid = Guid.NewGuid();
+            var reference = guid.ToString();
+
+            //Adds new reference to the map
+            idRefResolver.AddToReferenceMap(guid, testingObject);
+
+            //Resolves the reference
+            var result = idRefResolver.ResolveReferenceFromMap(null, reference);
+
+            Assert.AreEqual(testingObject, result);
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void AddReferenceFromMapTest_CaseDuplicatedGuid()
+        {
+            //Guid to use as reference
+            Guid guid = Guid.NewGuid();
+            var reference = guid.ToString();
+
+            //Adds new reference to the map
+            idRefResolver.AddToReferenceMap(guid, testingObject);
+
+            //Adds new object with same guid
+            var newObj = new object();
+            var ex = Assert.Throws<InvalidOperationException>(() => idRefResolver.AddToReferenceMap(guid, newObj));
+
+            //Checks that not only the exception type is correct but also the message
+            Assert.AreEqual(@"the map already contains a model with this id, the id must
+                    be unique for the workspace that is currently being deserialized: " + guid, ex.Message);
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void ResolveReferenceFromMapTest_CaseInvalidGuid()
+        {
+            //Invalid Guid to use as reference
+            string reference = "Invalid Guid";
+
+            //Resolves the reference
+            var result = idRefResolver.ResolveReferenceFromMap(null, reference);
+
+            Assert.IsNull(result);
+        }
+
+        #endregion
+
+        #region ReferenceModel Tests
+
+        [Test]
+        [Category("UnitTests")]
+        public void AddAndResolveReferenceTest()
+        {
+            //Guid to use as reference
+            Guid guid = Guid.NewGuid();
+            var reference = guid.ToString();
+
+            //Adds new reference to the map
+            idRefResolver.AddReference(null, reference, testingObject);
+
+            //Resolves the reference
+            var result = idRefResolver.ResolveReference(null, reference);
+
+            Assert.AreEqual(testingObject, result);
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void AddReference_CaseDuplicatedGuid()
+        {
+            //Guid to use as reference
+            Guid guid = Guid.NewGuid();
+            var reference = guid.ToString();
+
+            //Adds new reference to the map
+            idRefResolver.AddReference(null, reference, testingObject);
+
+            //Adds new object with same guid
+            var newObj = new object();
+            var ex = Assert.Throws<InvalidOperationException>(() => idRefResolver.AddReference(null, reference, newObj));
+
+            //Checks that not only the exception type is correct but also the message
+            Assert.AreEqual(@"the map already contains a model with this id, the id must
+                    be unique for the workspace that is currently being deserialized :" + guid, ex.Message);
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void ResolveReferenceTest_CaseInvalidGuid()
+        {
+            //Invalid Guid to use as reference
+            string reference = "Invalid Guid";
+
+            //Resolves the reference
+            var result = idRefResolver.ResolveReference(null, reference);
+
+            Assert.IsNull(result);
+        }
+
+        #endregion
+    }
+}
