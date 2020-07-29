02b9e7db17597c1753e18b366e2233c974d74ec5
Radu Gidei <radumg@users.noreply.github.com>
2020-07-29 21:01:56 +0200

    add note to obsolete MigrateCode method

--- src/Libraries/PythonNodeModels/PythonNode.cs
+++ src/Libraries/PythonNodeModels/PythonNode.cs
@@ -216,7 +216,7 @@ namespace PythonNodeModels
             return base.UpdateValueCore(updateValueParams);
         }
 
-        [Obsolete]
+        [Obsolete("This method is part of the temporary IronPython to CPython3 migration feature and will be removed in future versions of Dynamo.")]
         /// <summary>
         /// Updates the Script property of the node and raise the migration event notifications.
         /// NOTE: This is a temporary method used during the Python 2 to Python 3 transistion period,
