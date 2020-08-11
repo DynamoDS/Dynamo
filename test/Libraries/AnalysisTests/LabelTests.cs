a0f136d4baedede356ee29c4be48967bed9394fb
t-tellro <roberto.tellez@globant.com>
2020-08-11 12:21:30 -0500

    DYN-3026-CodeCoverage-AnalysisFolder
    I created several test methods for increasing the code coverage in the next classes (Analysis project).
    - Label
    - SurfaceData
    - AnalysisExtensions
    - Utils
    
    Also I removed two methods from the SurfaceData.cs file since both have cero references and both are private.

---
+++ test/Libraries/AnalysisTests/LabelTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Analysis;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Interfaces;
+using Dynamo.Visualization;
+using NUnit.Framework;
+
+namespace AnalysisTests
+{
+    [TestFixture]
+    class LabelTests
+    {
+        /// <summary>
+        /// This test method will execute the next methods from the Label class:
+        /// private Label(Point point, string label)
+        /// public static Label ByPointAndString(Point point, string label)
+        /// public void Tessellate(IRenderPackage package, TessellationParameters parameters)
+        /// </summary>
+        [Test, Category("UnitTests")]
+        public void LabelByPointAndStringTest()
+        {
+            //Arrange
+            var labelTest = Label.ByPointAndString(Point.ByCoordinates(100,100), "TestingLabel");
+            var factory = new DefaultRenderPackageFactory();
+            var package = factory.CreateRenderPackage();
+
+            //Act
+            labelTest.Tessellate(package, new TessellationParameters());
+
+            //Assert
+            Assert.IsNotNull(labelTest);
+            //Validates when Point is null
+            Assert.Throws<ArgumentNullException>( () => Label.ByPointAndString(null, "TestingLabel"));
+            //Validates when the label string is null
+            Assert.Throws<ArgumentNullException>(() => Label.ByPointAndString(Point.ByCoordinates(100, 100), null));
+        }
+    }
+}
