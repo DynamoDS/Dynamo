4d316afaafadbcbc3ea6495d3282605f70ce1014
t-tellro <roberto.tellez@globant.com>
2020-08-18 10:38:11 -0500

    DYN-3026-CodeCoverage-AnalysisFolder-CodeReview2
    I re-wrote the test for the Label class since Michael asked me to use a Label inside a Code Block node, then I created 3 dyn files with one CodeBlock creating a Label, one compiling and other two with errors (on purpose) when passing parameters to the Label.ByPointAndString method.
    
    Also by Michael response I undid the changes done by Aaron in the PR https://github.com/DynamoDS/Dynamo/pull/8189.
    
    Finally I applied some changes that the developer Ian Keough did like 5 years ago (HelixRenderPackage.cs -> CleanTag) but were discarded in the PR below, with the code as it is now in the Dynamo repo  when creating a Label inside a Code Block it shows a strange symbol in the Background Preview. The only way to show the label text correctly was using the syntax "label:text", then Ian's discarded change was re-applied by me and now we don't need to use the weird Label syntax.
    https://github.com/DynamoDS/Dynamo/pull/4637

--- test/Libraries/AnalysisTests/LabelTests.cs
+++ test/Libraries/AnalysisTests/LabelTests.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using Analysis;
-using Autodesk.DesignScript.Geometry;
-using Autodesk.DesignScript.Interfaces;
-using Dynamo.Visualization;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dynamo.Graph.Nodes;
+using HelixToolkit.Wpf.SharpDX;
 using NUnit.Framework;
 
 namespace AnalysisTests
 {
-    [TestFixture]
-    class LabelTests
+    [TestFixture, RequiresSTA]
+    class LabelTests : WpfVisualizationTests.VisualizationTest
     {
+        protected override void GetLibrariesToPreload(List<string> libraries)
+        {
+            libraries.Add("ProtoGeometry.dll");
+            libraries.Add("DSIronPython.dll");
+            libraries.Add("DesignScriptBuiltin.dll");
+            libraries.Add("DSCoreNodes.dll");
+            libraries.Add("GeometryColor.dll");
+            libraries.Add("VMDataBridge.dll");
+            libraries.Add("Analysis.dll");
+            base.GetLibrariesToPreload(libraries);
+        }
+
         /// <summary>
         /// This test method will execute the next methods from the Label class:
         /// private Label(Point point, string label)
@@ -17,22 +28,76 @@ namespace AnalysisTests
         /// public void Tessellate(IRenderPackage package, TessellationParameters parameters)
         /// </summary>
         [Test, Category("UnitTests")]
-        public void LabelByPointAndStringTest()
+        public void ByPointAndString_ShowLabel_Test()
         {
             //Arrange
-            var labelTest = Label.ByPointAndString(Point.ByCoordinates(100,100), "TestingLabel");
-            var factory = new DefaultRenderPackageFactory();
-            var package = factory.CreateRenderPackage();
+            OpenVisualizationTest("Test_CBN_Label.dyn");
+            Assert.IsTrue(ViewModel.HomeSpace.Nodes.All(x => x.DisplayLabels != true));
+
+            var cbn =
+              Model.CurrentWorkspace.Nodes.FirstOrDefault(
+                  x => x.GUID.ToString() == "6087ac20-e784-4ffb-8391-35b627625c9f") as
+                  CodeBlockNodeModel;
+
+            //Check that the Code Block node was found
+            Assert.IsNotNull(cbn);
+            //This will make visible the labels in the Background Preview
+            cbn.DisplayLabels = true;
 
             //Act
-            Assert.IsNotNull(labelTest);
-            labelTest.Tessellate(package, new TessellationParameters());
+            //Internally it will call the Label.ByPointAndString method located inside the CodeBlock node
+            ViewModel.HomeSpace.Run();
+
+            //It won't have any warnings then it will be false
+            bool hasWarnings = ViewModel.HomeSpace.Nodes.Any(n => n.State == ElementState.Warning || n.State == ElementState.PersistentWarning);
+
+            //This will get the text elements in the Background Preview
+            var textElements = BackgroundPreviewGeometry
+                               .Where(g => g is BillboardTextModel3D)
+                               .Cast<BillboardTextModel3D>()
+                               .Select(bb => bb.Geometry).Cast<BillboardText3D>()
+                               .ToArray();
 
             //Assert
-            //Validates when Point is null
-            Assert.Throws<ArgumentNullException>( () => Label.ByPointAndString(null, "TestingLabel"));
-            //Validates when the label string is null
-            Assert.Throws<ArgumentNullException>(() => Label.ByPointAndString(Point.ByCoordinates(100, 100), null));
+            //Be Sure that we have no warning
+            Assert.IsFalse(hasWarnings);
+
+            //Check that the Label contains the right text
+            Assert.That(textElements.First().TextInfo.First().Text, Is.EqualTo("test"));
+        }
+
+
+        /// <summary>
+        /// This test method will execute the Label.ByPointAndString method and will raise the exceptions due to the parameters passed
+        /// </summary>
+        [Test, Category("UnitTests")]
+        [TestCase("Test_CBN_Label_InvalidPoint.dyn", Description = "Passing a NULL Point to the Label.ByPointAndString method")]
+        [TestCase("Test_CBN_Label_EmptyText.dyn", Description = "Passing an empty text to the Label.ByPointAndString method")]
+        public void ByPointAndString_EmptyLabel_ThrowArgumentNullException(string dynFileName)
+        {
+            //Arrange
+            OpenVisualizationTest(dynFileName);
+            Assert.IsTrue(ViewModel.HomeSpace.Nodes.All(x => x.DisplayLabels != true));
+
+            var cbn =
+              Model.CurrentWorkspace.Nodes.FirstOrDefault(
+                  x => x.GUID.ToString() == "6087ac20-e784-4ffb-8391-35b627625c9f") as
+                  CodeBlockNodeModel;
+
+            //Check that the Code Block node was found
+            Assert.IsNotNull(cbn);
+            cbn.DisplayLabels = true;
+
+            //Act
+            //Internally it will call the Label.ByPointAndString method located inside the CodeBlock node
+            ViewModel.HomeSpace.Run();
+            bool hasWarnings = ViewModel.HomeSpace.Nodes.Any(n => n.State == ElementState.Warning || n.State == ElementState.PersistentWarning);
+
+            //Assert
+            //It will have warnings due that the graph is not compiling
+            Assert.IsTrue(hasWarnings);
+            //Due that the parameters passed to the Label.ByPointAndString method are invalid it will raise an exception (that cannot be catched) but is recorded in the ToolTipText parameter
+            Assert.That(cbn.ToolTipText, Is.StringContaining("ArgumentNullException"));
         }
     }
 }
