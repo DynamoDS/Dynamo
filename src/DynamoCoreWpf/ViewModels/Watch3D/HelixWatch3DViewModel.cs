1a1addf31985c9ea9b30aea3ba12e64ad7384f76
Martin Misol Monzo <mmisol@users.noreply.github.com>
2020-08-24 11:11:50 -0400

    Avoid Helix crash due to edge double values (#11031)
    This prevents the display of geometry located in positions considered
    invalid by Helix. This includes the special values PositiveInfinity,
    NegativeInfinity and NaN.
    
    Also NaN is checked as a special value when unmarhsalling data from an
    FFI. Special values PositiveInfinity and NegativeInfinity were already
    handled. This allows to generate a warning over the node with an
    appropriate message.

--- src/DynamoCoreWpf/ViewModels/Watch3D/HelixWatch3DViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Watch3D/HelixWatch3DViewModel.cs
@@ -1607,6 +1607,8 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
         /// <param name="packages">An <see cref="IEnumerable"/> of <see cref="HelixRenderPackage"/>.</param>
         internal virtual void AggregateRenderPackages(IEnumerable<HelixRenderPackage> packages)
         {
+            packages = FilterOutInvalidPackages(packages);
+
             IEnumerable<string> customNodeIdents = null;
             if (InCustomNode())
             {
@@ -1807,6 +1809,29 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
                 }
 
             }
+        }
+
+        /// <summary>
+        /// Filters out packages that are considered invalid by Helix. This includes any package
+        /// that has a coordinate with a special value like NaN or Infinite.
+        /// </summary>
+        /// <param name="packages">Original packages to render</param>
+        /// <returns>List of packages considered valid</returns>
+        private IEnumerable<HelixRenderPackage> FilterOutInvalidPackages(IEnumerable<HelixRenderPackage> packages)
+        {
+            List<HelixRenderPackage> result = new List<HelixRenderPackage>();
+
+            foreach (var package in packages)
+            {
+                if (!package.Points.Positions.Any(v => v.IsInvalid()) &&
+                    !package.Lines.Positions.Any(v => v.IsInvalid()) &&
+                    !package.Mesh.Positions.Any(v => v.IsInvalid()))
+                {
+                    result.Add(package);
+                }
+            }
+
+            return result;
         }
 
         private void AddLabelPlace(string nodeId, Vector3 pos, IRenderPackage rp)
@@ -2480,6 +2505,13 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
         internal static double DistanceToPlane(this Vector3 point, Vector3 planeOrigin, Vector3 planeNormal)
         {
             return Vector3.Dot(planeNormal, (point - planeOrigin));
+        }
+
+        internal static bool IsInvalid(this Vector3 point)
+        {
+            return float.IsNaN(point.X) || float.IsInfinity(point.X) ||
+                float.IsNaN(point.Y) || float.IsInfinity(point.Y) ||
+                float.IsNaN(point.Z) || float.IsInfinity(point.Z);
         }
     }
 
---
+++ src/VisualizationTests/PerformanceVisualizationTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace WpfVisualizationTests
+{
+    /// <summary>
+    /// Runs the performance graphs as visualization tests. Their purpose is to provide rendering timings only.
+    /// For that, you should make sure the renderTimer Stopwatch in HelixWatch3DViewModel is enabled and logging to Console.
+    /// You should run test including only the PerformanceVisualization category. Output should be redirected to a file.
+    /// You can extract the timings using grep and output them to a CSV file for ease of postprocessing.
+    /// </summary>
+    [TestFixture]
+    [Category("PerformanceVisualization")]
+    [Category("Failure")] // Not really a failure, we just don't want them to run on a normal build.
+    public class PerformanceVisualizationTests : VisualizationTest
+    {
+        // Determines how many times each performance graph will be run.
+        const int SamplingAmount = 10;
+
+        [Test]
+        [TestCaseSource("PerformanceTestSource")]
+        public void TestVisualizationPerformance(string filePath)
+        {
+            ViewModel.OpenCommand.Execute(filePath);
+            RunCurrentModel();
+        }
+
+        public IEnumerable<string> PerformanceTestSource
+        {
+            get
+            {
+                var path = Path.Combine(GetTestDirectory(ExecutingDirectory), @"..\tools\Performance\DynamoPerformanceTests\graphs");
+                return new DirectoryInfo(path).GetFiles("*.dyn").SelectMany(fileInfo => Enumerable.Repeat(fileInfo.FullName, SamplingAmount));
+            }
+        }
+    }
+}
