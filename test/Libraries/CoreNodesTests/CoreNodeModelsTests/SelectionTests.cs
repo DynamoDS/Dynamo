763b639315f5d1d2da3186c5ca43baaeb60d137f
Astul Betizagasti <astul.betizagasti@globant.com>
2020-09-01 18:23:09 -0300

    CoreNodeModels Reference issue fix

--- test/Libraries/CoreNodesTests/CoreNodeModelsTests/SelectionTests.cs
+++ test/Libraries/CoreNodesTests/CoreNodeModelsTests/SelectionTests.cs
@@ -7,11 +7,11 @@ using CoreNodeModels.Properties;
 using Dynamo;
 using Dynamo.Graph;
 using Dynamo.Graph.Nodes;
-using Dynamo.Models;
 using NUnit.Framework;
 using Moq;
+using TestUINodes;
 
-namespace CoreNodeModelsTests
+namespace DSCoreNodesTests
 {
     [TestFixture]
     public class SelectionTests : DynamoModelTestBase
@@ -26,7 +26,7 @@ namespace CoreNodeModelsTests
             selectionHelperMock = new Mock<IModelSelectionHelper<ModelBase>>(MockBehavior.Strict);
             selection = new SelectionConcrete(SelectionType.Many, SelectionObjectType.Element, "testMessage", "testPrefix", selectionHelperMock.Object);
             selection.Name = "selectionTestName";
-            testNode = CreateCodeBlockNode();
+            testNode = new DummyNode();
         }
 
         [Test]
@@ -85,26 +85,58 @@ namespace CoreNodeModelsTests
 
             //Checks ToString result for the selected element
             toStringResult = selection.ToString();
-            Assert.AreEqual("testPrefix : Dynamo.Graph.Nodes.CodeBlockNodeModel", toStringResult);
+            Assert.AreEqual("testPrefix : Dynamo.Graph.Nodes.DummyNode", toStringResult);
 
             selectionHelperMock.VerifyAll();
         }
 
-        [TestCase(SelectionObjectType.Edge, SelectionType.One, "Curve")]
-        [TestCase(SelectionObjectType.Edge, SelectionType.Many, "Curves")]
-        [TestCase(SelectionObjectType.Face, SelectionType.One, "Surface")]
-        [TestCase(SelectionObjectType.Face, SelectionType.Many, "Surfaces")]
-        [TestCase(SelectionObjectType.PointOnFace, SelectionType.One, "Point")]
-        [TestCase(SelectionObjectType.PointOnFace, SelectionType.Many, "Points")]
-        [TestCase(SelectionObjectType.None, SelectionType.One, "Element")]
-        [TestCase(SelectionObjectType.None, SelectionType.Many, "Elements")]
-        public void GetSelectionOutputTest(SelectionObjectType selectionObjectType, SelectionType selectinType, string expectedResult)
+        [Test]
+        public void GetSelectionOutputTest()
         {
-            selection = new SelectionConcrete(selectinType, selectionObjectType, "testMessage", "testPrefix");
-
+            //Case ObjectType: Edge
+            selection = new SelectionConcrete(SelectionType.One, SelectionObjectType.Edge, "testMessage", "testPrefix");
+            var expectedResult = "Curve";
             var result = selection.GetOutputPortName();
-
             Assert.AreEqual(expectedResult, result);
+
+            selection = new SelectionConcrete(SelectionType.Many, SelectionObjectType.Edge, "testMessage", "testPrefix");
+            expectedResult = "Curves"; 
+            result = selection.GetOutputPortName();
+            Assert.AreEqual(expectedResult, result);
+
+            //Case ObjectType: Face
+            selection = new SelectionConcrete(SelectionType.One, SelectionObjectType.Face, "testMessage", "testPrefix");
+            expectedResult = "Surface";
+            result = selection.GetOutputPortName();
+            Assert.AreEqual(expectedResult, result);
+
+            selection = new SelectionConcrete(SelectionType.Many, SelectionObjectType.Face, "testMessage", "testPrefix");
+            expectedResult = "Surfaces";
+            result = selection.GetOutputPortName();
+            Assert.AreEqual(expectedResult, result);
+
+            //Case ObjectType: PointOnFace
+            selection = new SelectionConcrete(SelectionType.One, SelectionObjectType.PointOnFace, "testMessage", "testPrefix");
+            expectedResult = "Point";
+            result = selection.GetOutputPortName();
+            Assert.AreEqual(expectedResult, result);
+
+            selection = new SelectionConcrete(SelectionType.Many, SelectionObjectType.PointOnFace, "testMessage", "testPrefix");
+            expectedResult = "Points";
+            result = selection.GetOutputPortName();
+            Assert.AreEqual(expectedResult, result);
+
+            //Case ObjectType: None
+            selection = new SelectionConcrete(SelectionType.One, SelectionObjectType.None, "testMessage", "testPrefix");
+            expectedResult = "Element";
+            result = selection.GetOutputPortName();
+            Assert.AreEqual(expectedResult, result);
+
+            selection = new SelectionConcrete(SelectionType.Many, SelectionObjectType.None, "testMessage", "testPrefix");
+            expectedResult = "Elements";
+            result = selection.GetOutputPortName();
+            Assert.AreEqual(expectedResult, result);
+
         }
 
         [Test]
@@ -185,104 +217,6 @@ namespace CoreNodeModelsTests
             Assert.AreEqual(1, count);
             var selectedNode = selection.SelectionResults.FirstOrDefault();
             Assert.AreEqual(testNode.GUID, selectedNode.GUID);
-        }
-
-        /// <summary>
-        /// Class created in order to test protected methods in SelectionBase parent
-        /// </summary>
-        private class SelectionConcrete : SelectionBase<ModelBase, ModelBase>
-        {
-            //Property used to allow testing with implementation specific method
-            public NodeModel testNode { get; set; }
-            public override IModelSelectionHelper<ModelBase> SelectionHelper { get; }
-
-            public void SerializeCore(XmlElement nodeElement, SaveContext context) =>
-                base.SerializeCore(nodeElement, context);
-
-            public void DeserializeCore(XmlElement nodeElement, SaveContext context) =>
-                base.DeserializeCore(nodeElement, context);
-
-            public bool UpdateValueCore(UpdateValueParams updateValueParams) =>
-                base.UpdateValueCore(updateValueParams);
-
-            public string GetOutputPortName() =>
-                 base.GetOutputPortName();
-
-            //Implemented this way for testing so the selection suffers no modifications
-            protected override IEnumerable<ModelBase> ExtractSelectionResults(ModelBase selections)
-            {
-                return new List<ModelBase> { selections };
-            }
-
-            protected override string GetIdentifierFromModelObject(ModelBase modelObject)
-            {
-                return modelObject.GUID.ToString();
-            }
-
-            protected override ModelBase GetModelObjectFromIdentifer(string id)
-            {
-                return testNode;
-            }
-
-            //This constructor is only used during testing to allow the initialization of the SelectionHelper property
-            public SelectionConcrete(
-                SelectionType selectionType,
-                SelectionObjectType selectionObjectType,
-                string message,
-                string prefix,
-                IModelSelectionHelper<ModelBase> selectionHelper)
-                : base(
-                    selectionType,
-                    selectionObjectType,
-                    message,
-                    prefix)
-            {
-                SelectionHelper = selectionHelper;
-            }
-
-            public SelectionConcrete(
-                SelectionType selectionType,
-                SelectionObjectType selectionObjectType,
-                string message,
-                string prefix)
-                    : base(
-                        selectionType,
-                        selectionObjectType,
-                        message,
-                        prefix)
-            {
-            }
-
-            public SelectionConcrete(
-                SelectionType selectionType,
-                SelectionObjectType selectionObjectType,
-                string message,
-                string prefix,
-                IEnumerable<string> selectionIdentifier,
-                IEnumerable<PortModel> inPorts,
-                IEnumerable<PortModel> outPorts)
-                    : base(
-                        selectionType,
-                        selectionObjectType,
-                        message,
-                        prefix,
-                        selectionIdentifier,
-                        inPorts,
-                        outPorts)
-            {
-            }
-        }
-
-        //Creates a NodeModel to use for testing
-        private CodeBlockNodeModel CreateCodeBlockNode()
-        {
-            var cbn = new CodeBlockNodeModel(CurrentDynamoModel.LibraryServices);
-            var command = new DynamoModel.CreateNodeCommand(cbn, 0, 0, true, false);
-
-            CurrentDynamoModel.ExecuteCommand(command);
-
-            Assert.IsNotNull(cbn);
-            return cbn;
         }
     }
 }
---
+++ test/TestUINodes/SelectionConcrete.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using CoreNodeModels;
+using Dynamo.Graph;
+using Dynamo.Graph.Nodes;
+
+namespace TestUINodes
+{
+    /// <summary>
+    /// Class created in order to test protected methods in SelectionBase parent
+    /// </summary>
+    public class SelectionConcrete : SelectionBase<ModelBase, ModelBase>
+    {
+        //Property used to allow testing with implementation specific method
+        public NodeModel testNode { get; set; }
+        public override IModelSelectionHelper<ModelBase> SelectionHelper { get; }
+
+        public void SerializeCore(XmlElement nodeElement, SaveContext context) =>
+            base.SerializeCore(nodeElement, context);
+
+        public void DeserializeCore(XmlElement nodeElement, SaveContext context) =>
+            base.DeserializeCore(nodeElement, context);
+
+        public bool UpdateValueCore(UpdateValueParams updateValueParams) =>
+            base.UpdateValueCore(updateValueParams);
+
+        public string GetOutputPortName() =>
+             base.GetOutputPortName();
+
+        //Implemented this way for testing so the selection suffers no modifications
+        protected override IEnumerable<ModelBase> ExtractSelectionResults(ModelBase selections)
+        {
+            return new List<ModelBase> { selections };
+        }
+
+        protected override string GetIdentifierFromModelObject(ModelBase modelObject)
+        {
+            return modelObject.GUID.ToString();
+        }
+
+        protected override ModelBase GetModelObjectFromIdentifer(string id)
+        {
+            return testNode;
+        }
+
+        //This constructor is only used during testing to allow the initialization of the SelectionHelper property
+        public SelectionConcrete(
+            SelectionType selectionType,
+            SelectionObjectType selectionObjectType,
+            string message,
+            string prefix,
+            IModelSelectionHelper<ModelBase> selectionHelper)
+            : base(
+                selectionType,
+                selectionObjectType,
+                message,
+                prefix)
+        {
+            SelectionHelper = selectionHelper;
+        }
+
+        public SelectionConcrete(
+            SelectionType selectionType,
+            SelectionObjectType selectionObjectType,
+            string message,
+            string prefix)
+                : base(
+                    selectionType,
+                    selectionObjectType,
+                    message,
+                    prefix)
+        {
+        }
+
+        public SelectionConcrete(
+            SelectionType selectionType,
+            SelectionObjectType selectionObjectType,
+            string message,
+            string prefix,
+            IEnumerable<string> selectionIdentifier,
+            IEnumerable<PortModel> inPorts,
+            IEnumerable<PortModel> outPorts)
+                : base(
+                    selectionType,
+                    selectionObjectType,
+                    message,
+                    prefix,
+                    selectionIdentifier,
+                    inPorts,
+                    outPorts)
+        {
+        }
+    }
+}
