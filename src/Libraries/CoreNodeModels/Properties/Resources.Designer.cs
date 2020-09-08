a913da4ba5ac20e52f697f5feed00edc62b241e0
Martin Stacey <martin__stacey@hotmail.com>
2020-09-08 13:42:17 +0100

    Update path from directory
     **NODE: Directory From Path**
    -CHANGES:
    Output Description : Path to the directory. (string)

--- src/Libraries/CoreNodeModels/Properties/Resources.Designer.cs
+++ src/Libraries/CoreNodeModels/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace CoreNodeModels.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -286,7 +286,7 @@ namespace CoreNodeModels.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Path to the directory..
+        ///   Looks up a localized string similar to Path to the directory. (string).
         /// </summary>
         public static string DirectoryObjectPortDataPathToolTip {
             get {
