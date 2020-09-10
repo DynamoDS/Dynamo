17a452e0185cee3afda2ca09cb35f33694b11fe4
Astul Betizagasti <astul.betizagasti@globant.com>
2020-09-10 18:22:42 -0300

    Tests 2

---
+++ test/TestUINodes/EnumAsStringConcrete.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using CoreNodeModels;
+using Dynamo.Graph;
+using Dynamo.Graph.Nodes;
+
+namespace TestUINodes
+{
+    /// <summary>
+    /// Class created in order to test methods in EnumAsString and parent classes
+    /// </summary>
+    public class EnumAsStringConcrete : EnumAsString<testEnum>
+    {
+        public EnumAsStringConcrete() : base() {}
+
+        public string PopulateItemsCore(string currentSelection) => 
+            base.PopulateItemsCore(currentSelection).ToString();
+
+        public void SerializeCore(XmlElement nodeElement, SaveContext context) =>
+            base.SerializeCore(nodeElement, context);
+
+        public void DesrializeCore(XmlElement nodeElement, SaveContext context) =>
+            base.DeserializeCore(nodeElement, context);
+
+        public bool UpdateValueCore(UpdateValueParams updateValueParams) => 
+            base.UpdateValue(updateValueParams);
+    }
+
+    /// <summary>
+    /// Enum created for testing
+    /// </summary>
+    public enum testEnum
+    {
+        A,
+        B,
+        C
+    }
+}
