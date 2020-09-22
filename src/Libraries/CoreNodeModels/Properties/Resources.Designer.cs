01eb3ddf292ce1b7fa3765274c52022de4752a95
Martin Stacey <martin__stacey@hotmail.com>
2020-09-22 17:42:52 +0100

    Directory from Path input description updated
    new description: type: string (Path to the directory)

--- src/Libraries/CoreNodeModels/Properties/Resources.Designer.cs
+++ src/Libraries/CoreNodeModels/Properties/Resources.Designer.cs
@@ -286,7 +286,7 @@ namespace CoreNodeModels.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Path to the directory..
+        ///   Looks up a localized string similar to type: string (Path to the directory).
         /// </summary>
         public static string DirectoryObjectPortDataPathToolTip {
             get {
