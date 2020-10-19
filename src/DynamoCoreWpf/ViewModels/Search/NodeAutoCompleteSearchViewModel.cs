1f0d7b49192ff1a5199c0fabf349aade3bd10fd7
mjkkirschner <mjk.kirschner@gmail.com>
2020-10-19 14:29:09 -0400

    for types that include rank, strip off the rank using the parser handle comparing nodes with multiple return types (not zt dictionaries)

--- src/DynamoCoreWpf/ViewModels/Search/NodeAutoCompleteSearchViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Search/NodeAutoCompleteSearchViewModel.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using DSCore;
-using Dynamo.Controls;
+using Dynamo.Properties;
 using Dynamo.Search.SearchElements;
 using Dynamo.Wpf.ViewModels;
-using ProtoCore;
+using ProtoCore.AST.AssociativeAST;
 using ProtoCore.Mirror;
+using ProtoCore.Utils;
 
 namespace Dynamo.ViewModels
 {
     /// <summary>
-    /// Search View Model for Node AutoComplate Search Bar
+    /// Search View Model for Node AutoComplete Search Bar
     /// </summary>
     public class NodeAutoCompleteSearchViewModel : SearchViewModel
     {
@@ -43,39 +42,40 @@ namespace Dynamo.ViewModels
         /// that output a type compatible with the port type if it's an input port.
         /// These search elements can belong to either zero touch, NodeModel or Builtin nodes.
         /// This method returns an empty collection if the input port type cannot be inferred or
-        /// there are no matching nodes found for the type. Currently the match is an exact match
-        /// done including the rank information in the type, e.g. Point[] or var[]..[].
-        /// The search elements can be made to appear in the node autocomplete search dialog.
+        /// there are no matching nodes found for the type. Currently the match does not include
+        /// rank information. For example Curve[] is matched as Curve. The results include types
+        /// that would be valid using normal class inheritance rules.
+        /// The resulting compatible search elements can be made to appear in the node autocomplete search dialog.
         /// </summary>
         /// <returns>collection of node search elements</returns>
         internal IEnumerable<NodeSearchElement> GetMatchingNodes()
         {
             var elements = new List<NodeSearchElement>();
-
             var inputPortType = PortViewModel.PortModel.GetInputPortType();
+
             if (inputPortType == null) return elements;
+            //if inputPortType is an array, use just the typename
+            var cbn = ParserUtils.ParseWithCore($"dummyName:{ inputPortType};", dynamoViewModel.Model.LibraryServices.LibraryManagementCore);
+            var ast = cbn.CodeBlockNode.Children().FirstOrDefault() as IdentifierNode;
+            //if parsing the type failed, revert to original string.
+            inputPortType = ast != null ? ast.datatype.Name : inputPortType;
 
-            var libraryServices = dynamoViewModel.Model.LibraryServices;
-
-            // Builtin functions and zero-touch functions
-            // Multi-return ports for these nodes do not contain type information
-            // and are therefore skipped.
-            var functionGroups = libraryServices.GetAllFunctionGroups();
-            var functionDescriptors = functionGroups.SelectMany(fg => fg.Functions).Where(fd => fd.IsVisibleInLibrary);
-
-            var ztSearchElements = Model.SearchEntries.OfType<ZeroTouchSearchElement>();
-
+            //gather all ztsearchelements that are visible in search and filter using inputPortType and zt return type name.
+            var ztSearchElements = Model.SearchEntries.OfType<ZeroTouchSearchElement>().Where(x => x.IsVisibleInSearch);
             foreach (var ztSearchElement in ztSearchElements)
             {
+                //for now, remove rank from descriptors
+                var returnTypeName = ztSearchElement.Descriptor.ReturnType.Name;
+
                 var descriptor = ztSearchElement.Descriptor;
-                if ((descriptor.ReturnType.ToString() == inputPortType)
-                    || DerivesFrom(inputPortType, descriptor.ReturnType.ToString(), dynamoViewModel.Model.LibraryServices.LibraryManagementCore))
+                if ((returnTypeName == inputPortType)
+                    || DerivesFrom(inputPortType, returnTypeName, dynamoViewModel.Model.LibraryServices.LibraryManagementCore))
                 {
                     elements.Add(ztSearchElement);
                 }
             }
 
-            // NodeModel nodes
+            // NodeModel nodes, match any output return type to inputport type name
             foreach (var element in Model.SearchEntries.OfType<NodeModelSearchElement>())
             {
                 if (element.OutputParameters.Any(op => op == inputPortType))
@@ -83,13 +83,15 @@ namespace Dynamo.ViewModels
                     elements.Add(element);
                 }
             }
-            //TODO if we intend to cache data instantiate this comparer at construction time
-            //and update the typeNameToCompare.
+
             var comparer = new NodeSearchElementComparer(inputPortType, dynamoViewModel.Model.LibraryServices.LibraryManagementCore);
+
             //first sort by type distance to input port type
             elements.Sort(comparer);
             //then sort by node library group (create, action, or query node)
-            return elements.OrderBy(x =>x.Group);
+            return elements.OrderBy(x => x.Group);
+
+            //this results in a list of elements with 3 major groups(create,action,query), each group is sub sorted into types.
         }
 
         /// <summary>
@@ -107,7 +109,6 @@ namespace Dynamo.ViewModels
                 var mirror1 = new ClassMirror(typea, core);
                 var mirror2 = new ClassMirror(typeb, core);
 
-
                 //TODO as we do this check we can cache the type distance...
                 if (mirror2.GetClassHierarchy().Any(x => x.ClassName == mirror1.ClassName))
                 {
@@ -118,12 +119,14 @@ namespace Dynamo.ViewModels
             }
             catch
             {
-                // TODO need to deal with the fact that type[] and type[]..[] are not classes - trying to create these up as ClassMirrors will yield an exception.
-                Debug.WriteLine($"failed to create class mirror for {typea} or {typeb} during node autocomplete operation ");
+                Debug.WriteLine($"failed to create class mirror for either {typea} or {typeb} during node autocomplete operation ");
                 return false;
             }
         }
 
+        /// <summary>
+        /// Compares NodeSearchElements based on their typeDistance from a given type 'typeNameToCompareTo'
+        /// </summary>
         internal class NodeSearchElementComparer : IComparer<NodeSearchElement>
         {
             private string typeNameToCompareTo;
@@ -137,65 +140,103 @@ namespace Dynamo.ViewModels
 
             public int Compare(NodeSearchElement x, NodeSearchElement y)
             {
-               return CompareNodeSearchElementByTypeDistance(x, y, typeNameToCompareTo, core);
+                return CompareNodeSearchElementByTypeDistance(x, y, typeNameToCompareTo, core);
             }
 
+
+            /// <summary>
+            /// Compares two nodeSearchElements - general rules of the sort as follows:
+            /// If all return types of the two searchElements are the same, they are equal.
+            /// If any return type of both searchElements is an exact match for our input type, they are equal.
+            /// If a searchElement is null, it is larger than the other element.
+            /// If a single searchElement's return type list contains an exact match it is smaller.
+            /// If the minimum type distance between a searchElements return types and our inputType than this searchElement is smaller. (closer)
+            /// If the minimim type distances are the same the searchElements are equal.
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            /// <param name="typeNameToCompareTo"></param>
+            /// <param name="core"></param>
+            /// <returns></returns>
             private int CompareNodeSearchElementByTypeDistance(NodeSearchElement x, NodeSearchElement y, string typeNameToCompareTo, ProtoCore.Core core)
             {
                 /*
-                 * -1	x proceeds y.
+                 * -1	x preceeds y.
                  0 x and y are equal
                  1	x is after y.
                  */
+                var none = new string[] { Resources.NoneString };
+                var xTypeNames = x.OutputParameters;
+                var yTypeNames = y.OutputParameters;
 
-                // TODO - for now using OutputParameters.FirstOrDefault() - but this is not ideal. What about
-                // nodes which have multiple outputs - might need to iterate over each output param
-                // and take the closest match as mechanism for sorting.
+                //if either element is a ztsearchElement then just use the type name (this strips off the rank)
+                if (x is ZeroTouchSearchElement xzt)
+                {
+                    xTypeNames = new string[] { xzt.Descriptor.ReturnType.Name };
+                }
+                else
+                {
+                    //if inputPortType is an array, lets just use the class name to match 
+                    xTypeNames = xTypeNames.Select(type => ParserUtils.ParseWithCore($"dummyName:{ type};", core).CodeBlockNode.Children().ElementAt(0).Name);
+                }
+                if (y is ZeroTouchSearchElement yzt)
+                {
+                    yTypeNames = new string[] { yzt.Descriptor.ReturnType.Name };
+                }
+                else
+                {
+                    yTypeNames = yTypeNames.Select(type => ParserUtils.ParseWithCore($"dummyName:{ type};", core).CodeBlockNode.Children().ElementAt(0).Name);
 
-                var xTypeName = x.OutputParameters.FirstOrDefault();
-                var yTypeName = y.OutputParameters.FirstOrDefault();
+                }
 
-                if (xTypeName == yTypeName)
+                if (xTypeNames.SequenceEqual(yTypeNames))
                 {
                     return 0;
                 }
+
+                // x and y are equal because both typeLists contain an exact match
+                if (xTypeNames.Any(xType => xType == typeNameToCompareTo) && (yTypeNames.Any(yType => yType == typeNameToCompareTo)))
+                {
+                    return 0;
+                }
+
                 // null is further away, so x is at the end of list.
-                if (xTypeName == null)
+                if (xTypeNames.SequenceEqual(none))
                 {
                     return 1;
                 }
                 // null is further away, so y is at the end of the list.
-                if (yTypeName == null)
+                if (yTypeNames.SequenceEqual(none))
                 {
                     return -1;
                 }
 
-                // x proceeds y because it's an exact match
-                if (xTypeName == typeNameToCompareTo)
+                // x preceeds y because it contains an exact match
+                if (xTypeNames.Any(xType => xType == typeNameToCompareTo))
                 {
                     return -1;
                 }
-                // y proceeds x because it's an exact match
-                if (yTypeName == typeNameToCompareTo)
+                //  y preceeds x because it contains an exact match
+                if (yTypeNames.Any(yType => yType == typeNameToCompareTo))
                 {
                     return 1;
                 }
 
-                var xdistance = GetTypeDistance(typeNameToCompareTo, xTypeName, core);
-                var ydistance = GetTypeDistance(typeNameToCompareTo, yTypeName, core);
+                var xminDistance = xTypeNames.Select(name => GetTypeDistance(typeNameToCompareTo, name, core)).Min();
+                var yminDistance = yTypeNames.Select(name => GetTypeDistance(typeNameToCompareTo, name, core)).Min();
 
                 //if distance of x to currentSelectedType is greater than y distance
                 //then x is further away
-                if (xdistance > ydistance)
+                if (xminDistance > yminDistance)
                 {
                     return 1;
                 }
-                if (xdistance == ydistance)
+                if (xminDistance == yminDistance)
                 {
                     return 0;
                 }
                 // distance2 < distance 1
-                // x proceeds y
+                // x preceeds y
                 return -1;
             }
 
