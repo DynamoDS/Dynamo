bbefc36de5e5984ee4541048b9e43866bab48454
Jorgen Dahl <jorgen.dahl@autodesk.com>
2020-11-17 10:50:34 -0500

    First pass - singleton version

--- src/DocumentationBrowserViewExtension/MarkdownHandler.cs
+++ src/DocumentationBrowserViewExtension/MarkdownHandler.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using Dynamo.PackageManager;
-using Markdig;
-using Markdig.Parsers;
-using Markdig.Renderers;
-using Markdig.Syntax;
-using Markdig.Syntax.Inlines;
+using Dynamo.Utilities;
 
 namespace Dynamo.DocumentationBrowser
 {
@@ -17,7 +11,7 @@ namespace Dynamo.DocumentationBrowser
     {
         private const string NODE_ANNOTATION_NOT_FOUND = "Dynamo.DocumentationBrowser.Docs.NodeAnnotationNotFound.md";
         private const string SYNTAX_HIGHLIGHTING = "Dynamo.DocumentationBrowser.Docs.syntaxHighlight.html";
-        private readonly MarkdownPipeline pipeline;
+        private readonly MD2HTML converter = new MD2HTML();
 
 
         private static MarkdownHandler instance;
@@ -32,10 +26,6 @@ namespace Dynamo.DocumentationBrowser
 
         private MarkdownHandler()
         {
-            var pipelineBuilder = new MarkdownPipelineBuilder();
-            pipeline = pipelineBuilder
-                .UseAdvancedExtensions()
-                .Build();
         }
 
         /// <summary>
@@ -86,17 +76,11 @@ namespace Dynamo.DocumentationBrowser
             }
             scriptTagsRemoved = false;
 
-            var renderer = new HtmlRenderer(writer);
-            pipeline.Setup(renderer);
+            converter.ParseToHtml(ref writer, mdString, mdFilePath);
 
-            var document = MarkdownParser.Parse(mdString, pipeline);
-            ConvertRelativeLocalImagePathsToAbsolute(mdFilePath, document);
-
-            renderer.Render(document);
             // inject the syntax highlighting script at the bottom at the document.
             writer.WriteLine(DocumentationBrowserUtils.GetDPIScript());
             writer.WriteLine(GetSyntaxHighlighting());
-
             return scriptTagsRemoved;
         }
 
@@ -111,33 +95,6 @@ namespace Dynamo.DocumentationBrowser
                 return string.Empty;
 
             return syntaxHighlightingContent;
-        }
-
-        /// <summary>
-        /// For markdown local images needs to be in the same folder as the md file
-        /// referencing it with a relative path "./image.png", when we convert to html
-        /// we need the full path. This method finds relative image paths and converts them to absolute paths.
-        /// </summary>
-        private static void ConvertRelativeLocalImagePathsToAbsolute(string mdFilePath, MarkdownDocument document)
-        {
-            var imageLinks = document.Descendants<ParagraphBlock>()
-                .SelectMany(x => x.Inline.Descendants<LinkInline>())
-                .Where(x => x.IsImage)
-                .Select(x => x).ToList();
-
-            foreach (var image in imageLinks)
-            {
-                if (!image.Url.StartsWith("./"))
-                    continue;
-
-                var imageName = image.Url.Split(new string[] { "./" }, StringSplitOptions.None);
-                var dir = Path.GetDirectoryName(mdFilePath);
-
-                var htmlImagePathPrefix = @"file:///";
-                var absoluteImagePath = Path.Combine(dir, imageName.Last());
-
-                image.Url = $"{htmlImagePathPrefix}{absoluteImagePath}";
-            }
         }
     }
 }
---
+++ src/DynamoUtilities/MD2HTML.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Dynamo.Utilities
+{
+    public class MD2HTML
+    {
+        private readonly Process process = new Process();
+        public MD2HTML()
+        {
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(ProcessExit);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+
+                UseShellExecute = false,
+                Arguments = @"",
+                FileName = ToolPath()
+            };
+
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        ~MD2HTML()
+        {
+            KillProcess();
+        }
+
+        public void ParseToHtml(ref StringWriter writer, string mdString, string mdPath)
+        {
+            process.StandardInput.WriteLine("<<<<<Convert>>>>>");
+            process.StandardInput.WriteLine(mdPath);
+            process.StandardInput.WriteLine(mdString);
+            process.StandardInput.WriteLine("<<<<<Eod>>>>>");
+            var done = false;
+            while(!done)
+            {
+                var line = process.StandardOutput.ReadLine();
+                if (line == null || line == "<<<<<Eod>>>>>")
+                {
+                    done = true;
+                }
+                else
+                {
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            } 
+        }
+
+        private static string ToolPath ()
+        {
+            var rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new ArgumentNullException("Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)");
+            var toolPath = Path.Combine(rootPath, @"MD2HTML\MD2HTML.exe");
+            return toolPath;
+        }
+
+        private void KillProcess()
+        {
+            if (!process.HasExited)
+            {
+                process.Kill();
+            }
+        }
+        private void ProcessExit(object sender, EventArgs e)
+        {
+            KillProcess();
+        }
+    }
+}
