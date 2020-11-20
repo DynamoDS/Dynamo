3f5b9ded4c5178f11c5e5e51aac0e3b38703875b
Astul Betizagasti <astul.betizagasti@globant.com>
2020-11-20 18:00:39 -0300

    Refactor

--- .github/scripts/CheckIssueContent.ps1
+++ .github/scripts/CheckIssueContent.ps1
@@ -3,6 +3,10 @@ param([string]$issueContent="")
 
 #Processing
 function Check-For-Missing-Information([string]$issueContent){
+    #Compares the issue content against the given pattern. 
+    #(The '*' represent any amount of any characters)
+    #True: if the content has 2 instances of "(Fill in here)"
+    #False: if the pattern is not matched
     return $issueContent -like '*(Fill in here)*(Fill in here)*'
 }
 
