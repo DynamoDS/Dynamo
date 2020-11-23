dfa60bb2821ead0717bb2a61152c8e4b61198ab7
Martin Misol Monzo <mmisol@users.noreply.github.com>
2020-11-23 16:25:56 -0500

    Cleanup after verifying this works

--- src/DynamoCoreWpf/Views/Core/DynamoView.xaml.cs
+++ src/DynamoCoreWpf/Views/Core/DynamoView.xaml.cs
@@ -290,13 +290,8 @@ namespace Dynamo.Controls
                 window = new ExtensionWindow(this, ref windowRect);
                 if (windowSettings.Status == WindowStatus.Maximized)
                 {
-                    //var screen = System.Windows.Forms.Screen.AllScreens.FirstOrDefault(s => s.DeviceName == windowSettings.Screen);
-                    //if (screen != null)
-                    //{
-                    //    windowRect.Left = screen.WorkingArea.Left + 10;
-                    //    windowRect.Top = screen.WorkingArea.Top + 10;
-                    //}
-                    //window.WindowState = WindowState.Maximized;
+                    // Rather than setting the WindowState here, this is delayed to the Loaded event.
+                    // This helps overcome a bug which makes the window appear always on the primary screen.
                     window.ShouldMaximize = true;
                 }
             }
@@ -337,13 +332,6 @@ namespace Dynamo.Controls
             settings.WindowSettings.Top = (int)window.SavedWindowRect.Top;
             settings.WindowSettings.Width = (int)window.SavedWindowRect.Width;
             settings.WindowSettings.Height = (int)window.SavedWindowRect.Height;
-            // Find screen currently showing the extension
-            //var screens = System.Windows.Forms.Screen.AllScreens;
-            //var currentScreen = screens.FirstOrDefault(s => window.Left >= s.WorkingArea.Left && window.Left < s.WorkingArea.Right);
-            //if (currentScreen != null)
-            //{
-            //    settings.WindowSettings.Screen = currentScreen.DeviceName;
-            //}
         }
 
         private TabItem AddExtensionTab(IViewExtension viewExtension, ContentControl contentControl)
--- src/DynamoCoreWpf/Windows/ExtensionWindow.xaml.cs
+++ src/DynamoCoreWpf/Windows/ExtensionWindow.xaml.cs
@@ -18,11 +18,20 @@ namespace Dynamo.Wpf.Windows
         /// </summary>
         public bool ShouldMaximize { get; set; }
 
+        /// <summary>
+        /// Creates a window with an initially empty window rectangle.
+        /// Note: This constructor assumes Owner is set externally.
+        /// </summary>
         public ExtensionWindow()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Creates a window with the specified owner and window rectangle.
+        /// </summary>
+        /// <param name="viewParent">A UI object in the Dynamo visual tree.</param>
+        /// <param name="rect">A reference to the Rect object that will store the window's position during this session.</param>
         public ExtensionWindow(DependencyObject viewParent, ref WindowRect rect)
             : base(viewParent, ref rect)
         {
@@ -75,6 +84,7 @@ namespace Dynamo.Wpf.Windows
         {
             // This can't be done using markup, so we do it here.
             iconImage.Source = Icon;
+            // The window is maximized at this point to make sure it appears on the right screen.
             if (ShouldMaximize)
             {
                 WindowState = WindowState.Maximized;
