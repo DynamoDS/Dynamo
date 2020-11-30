8bfad9b28a052c9b599c0354045ae44f6d3b82bc
Martin Misol Monzo <mmisol@users.noreply.github.com>
2020-11-30 11:11:24 -0500

    Save/Load view extension size and location (#11278)
    Information of where an extension control was last shown is saved and
    retrieved as preference settings. These are actually not directly
    visible for the user and are updating automatically, just like the main
    Dynamo window size. These settings are used when showing the extension
    control, to restore it to how it used to be before closing it.
    
    The available information for each view extension is:
    
    1. How it is shown. For now this can be either as a floating window or
    docked to the right-side panel.
    
    2. In case it is shown as a floating window, the following information
    about the window is saved:
    - Location by Top/Left coordinates
    - Size by Width/Height
    - Whether the window is maximized

--- src/DynamoCoreWpf/Windows/ExtensionWindow.xaml.cs
+++ src/DynamoCoreWpf/Windows/ExtensionWindow.xaml.cs
@@ -13,8 +13,27 @@ namespace Dynamo.Wpf.Windows
         /// Note: Setter is internal for testing purposes only.
         /// </summary>
         public bool DockRequested { get; internal set; }
+        /// <summary>
+        /// Indicates if the window should be maximized when it is loaded.
+        /// </summary>
+        public bool ShouldMaximize { get; set; }
 
+        /// <summary>
+        /// Creates a window with an initially empty window rectangle.
+        /// Note: This constructor assumes Owner is set externally.
+        /// </summary>
         public ExtensionWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Creates a window with the specified owner and window rectangle.
+        /// </summary>
+        /// <param name="viewParent">A UI object in the Dynamo visual tree.</param>
+        /// <param name="rect">A reference to the Rect object that will store the window's position during this session.</param>
+        public ExtensionWindow(DependencyObject viewParent, ref WindowRect rect)
+            : base(viewParent, ref rect)
         {
             InitializeComponent();
         }
@@ -65,6 +84,12 @@ namespace Dynamo.Wpf.Windows
         {
             // This can't be done using markup, so we do it here.
             iconImage.Source = Icon;
+            // The window is maximized at this point to make sure it appears on the right screen.
+            if (ShouldMaximize)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            RefreshMaximizeRestoreButton();
         }
     }
 }
