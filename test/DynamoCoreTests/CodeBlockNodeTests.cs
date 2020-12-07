b9d522e83dcd0fc7d16b7a6a4ce6ce5b15b82cec
aparajit-pratap <aparajit.pratap@autodesk.com>
2020-12-07 14:53:46 -0500

    update tests to reflect property completion additionally

--- test/DynamoCoreTests/CodeBlockNodeTests.cs
+++ test/DynamoCoreTests/CodeBlockNodeTests.cs
@@ -1942,7 +1942,7 @@ var06 = g;
 
             var members = type.GetMembers();
 
-            var expected = new[] { "AddWithValueContainer", "CodeCompletionClass", "IsEqualTo", "OverloadedAdd", "StaticFunction", "StaticProp"};
+            var expected = new[] { "AddWithValueContainer", "ClassProperty", "CodeCompletionClass", "IntVal", "IsEqualTo", "OverloadedAdd", "StaticFunction", "StaticProp"};
             AssertCompletions(members, expected);
         }
 
@@ -2272,7 +2272,7 @@ var06 = g;
         {
             var codeCompletionServices = new CodeCompletionServices(libraryServicesCore);
             var completions = codeCompletionServices.GetCompletionsOnType("", "CodeCompletionClass");
-            Assert.AreEqual(6, completions.Count());
+            Assert.AreEqual(8, completions.Count());
         }
 
         [Test]
@@ -2290,10 +2290,11 @@ var06 = g;
         {
             var codeCompletionServices = new CodeCompletionServices(libraryServicesCore);
             var completions = codeCompletionServices.GetCompletionsOnType("", "DupTargetTest").ToList();
-            Assert.AreEqual(3, completions.Count);
+            Assert.AreEqual(4, completions.Count);
             Assert.AreEqual("DupTargetTest", completions[0].Text);
             Assert.AreEqual("Bar", completions[1].Text);
             Assert.AreEqual("Foo", completions[2].Text);
+            Assert.AreEqual("Prop", completions[3].Text);
         }
 
         [Test]
@@ -2315,9 +2316,10 @@ var06 = g;
         {
             var codeCompletionServices = new CodeCompletionServices(libraryServicesCore);
             var completions = codeCompletionServices.GetCompletionsOnType("", "FFITarget.C.B.DupTargetTest").ToList();
-            Assert.AreEqual(2, completions.Count);
+            Assert.AreEqual(3, completions.Count);
             Assert.AreEqual("DupTargetTest", completions[0].Text);
             Assert.AreEqual("Foo", completions[1].Text);
+            Assert.AreEqual("Prop", completions[2].Text);
         }
 
         [Test]
