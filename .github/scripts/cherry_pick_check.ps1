f0e5d0c1f976371b6b63622440c6bde151efbad6
Astul Betizagasti <astul.betizagasti@globant.com>
2021-01-21 14:18:12 -0300

    change variable name

--- .github/scripts/cherry_pick_check.ps1
+++ .github/scripts/cherry_pick_check.ps1
@@ -2,11 +2,11 @@ param([string]$commitMessage)
 
 #Looks for the cherry-pick command on the commit message
 #Format: Cherry-pick to: 'target-branch'
-$commitData = $commitMessage | Select-String -Pattern "Cherry-pick to:\s?'*'"
+$cherrypickCommand = $commitMessage | Select-String -Pattern "Cherry-pick to:\s?'*'"
 
 #Extracts the branch name
-if($commitData){
-    $splitedData = $commitData.Line.split("'")
+if($cherrypickCommand){
+    $splitedData = $cherrypickCommand.Line.split("'")
     $branchName = $splitedData[1]
     if($branchName){
         Write-Output $branchName
