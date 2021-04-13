b1fe891aaa841a472ae669e06ebf10ed8b1d99b6
Aaron (Qilong) <173288704@qq.com>
2021-04-13 07:54:04 -0700

    Address Unsaved Changes Edge Case (#11601)
    * Adress Unsaved Changes Edge Case
    
    * Add unit test

--- test/DynamoCoreTests/CoreTests.cs
+++ test/DynamoCoreTests/CoreTests.cs
@@ -699,6 +699,23 @@ namespace Dynamo.Tests
             Assert.IsFalse(sumNode.IsModified);
         }
 
+        /// <summary>
+        /// This test is added to test if the HasUnsavedChanges can be set 
+        /// correctly after node removal using an edge case
+        /// </summary>
+        [Test]
+        public void TestHasUnsavedChangesOnNodeRemovalEdgeCase()
+        {
+            string openPath = Path.Combine(TestDirectory, @"core\logic\comparison\testToleranceEquals_defaultTolerance.dyn");
+            OpenModel(openPath);
+            // Default state after graph open
+            Assert.AreEqual(false, CurrentDynamoModel.CurrentWorkspace.HasUnsavedChanges);
+            CurrentDynamoModel.CurrentWorkspace.RecordAndDeleteModels(
+                CurrentDynamoModel.CurrentWorkspace.Nodes.Where(x=>x.GUID.Equals(new Guid("c2f69bf434be47fa8f0a4a0ceca5910b"))).ToList<ModelBase>());
+            // Assert HasUnsavedChanges is true after a downstream node removal
+            Assert.AreEqual(true, CurrentDynamoModel.CurrentWorkspace.HasUnsavedChanges);
+        }
+
         [Test]
         public void TestFileDirtyOnLacingChange()
         {
