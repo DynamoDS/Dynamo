eb821824f1f95bf15924bbd3bef6709cb03eea5d
reddyashish <43763136+reddyashish@users.noreply.github.com>
2021-04-29 08:02:04 -0400

    Update AutoCompletionNodeSearchElement.cs

--- src/DynamoCore/Search/SearchElements/AutoCompletionNodeSearchElement.cs
+++ src/DynamoCore/Search/SearchElements/AutoCompletionNodeSearchElement.cs
@@ -8,6 +8,7 @@ namespace Dynamo.Search.SearchElements
 {
     /// <summary>
     ///      Class for all Auto-Completion Node search elements.
+    ///      This class will contain the info related to Auto-completion functionality of the matching node elements.
     /// </summary>
     public abstract class AutoCompletionNodeSearchElement
     {
