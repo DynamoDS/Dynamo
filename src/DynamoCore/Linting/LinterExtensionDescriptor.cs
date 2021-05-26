a7b9fecf621e2447bf2bd81fec6fcde1b1b355bc
Craig Long <craig.long@autodesk.com>
2021-05-25 21:25:32 -0400

    Add equals override to LinterExtensionDescriptor

--- src/DynamoCore/Linting/LinterExtensionDescriptor.cs
+++ src/DynamoCore/Linting/LinterExtensionDescriptor.cs
@@ -20,5 +20,39 @@ namespace Dynamo.Linting
             Id = id;
             Name = name;
         }
+
+        /// <summary>
+        /// Checks whether two LinterExtensionDescriptor are equal
+        /// They are equal if their Name and Id are equal
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is LinterExtensionDescriptor))
+            {
+                return false;
+            }
+
+            var other = obj as LinterExtensionDescriptor;
+            if (other.Name == this.Name && other.Id == this.Id)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the hashcode for this LinterExtensionDescriptor
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Id.GetHashCode();
+        }
     }
 }
