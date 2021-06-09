3c861d2b3041733f007b6718e21644487379f2b4
Sylvester Knudsen <sylvesterknudsen@gmail.com>
2021-06-09 14:49:48 +0100

    Graph properties view extension (#11675)
    * initial commit
    
    * TestCommit
    
    * Basic adding/editing of custom properties implemented. Removing custom properties not implemented yet.
    
    * CustomPropertyControl element removal implemented.
    
    * Revert "TestCommit"
    
    * Various style (xaml) fixes.
    
    * PR comments SHK picked up largely.
    
    * Additional cleanup.
    
    * Corrected changes from latest PR.
    
    * Added validation logic to Uri textbox.
    
    -Validation Rule
    -ValidationErrorTemplate
    -Fixed empty textbox error.
    
    * Commented code cleanup.
    
    * Dispose methods added to  ViewModel and to Extension.
    
    * Quick correction to override Loaded method.
    
    * Removed redundant  interface.
    
    * Update .gitignore
    
    * Various edits around handling Custom Property behaviour correctly.
    
    * Update .gitignore
    
    * Styling updates
    
    * Update GraphMetadataView.xaml
    
    * add extension tests
    
    * Clean up
    
    * Update PackageManagerExtensionLoadingTests.cs
    
    * comment updates
    
    * fix margin and add new icons
    
    * add summaries
    
    * Dont show extension in CustomNode ws + add CustomProp name textbox highlight
    
    * Mark graph dirty on props changed
    
    * use absolute URI
    
    Co-authored-by: Marco Juliani <marcotjuliani@gmail.com>
    Co-authored-by: Craig Long <craig.alan.long@gmail.com>

---
+++ src/GraphMetadataViewExtension/ValidationRules/UriValidationRule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Dynamo.GraphMetadata.ValidationRules
+{
+    public class UriValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            string input = value as string;
+
+            if (String.IsNullOrEmpty(input)) // Valid input, converts to null.
+            {
+                return new ValidationResult(true, null);
+            }
+
+            if (Uri.TryCreate(input, UriKind.Absolute, out Uri outUri))
+            {
+                return new ValidationResult(true, null);
+            }
+
+            return new ValidationResult(false, Properties.Resources.URIValidationRule_Fail_Message);
+
+        }
+    }
+}
