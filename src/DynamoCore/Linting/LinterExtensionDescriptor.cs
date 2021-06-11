56d2bcfc3f397acb3ee5b18c77516f241466686e
pinzart <tiberiu.pinzariu@autodesk.com>
2021-06-11 16:08:58 -0400

    Revert "revert linting change"
    This reverts commit 51117ca0ff693edfad84d78f5c47ccfb51bf0c2d.

--- src/DynamoCore/Linting/LinterExtensionDescriptor.cs
+++ src/DynamoCore/Linting/LinterExtensionDescriptor.cs
@@ -20,5 +20,44 @@ namespace Dynamo.Linting
             Id = id;
             Name = name;
         }
+
+        /// <summary>
+        /// Checks whether two LinterExtensionDescriptor are equal
+        /// They are equal if their Name and Id are equal
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            
+            if (!(obj is LinterExtensionDescriptor other))
+            {
+                return false;
+            }
+
+            if (other.Name == this.Name && other.Id == this.Id)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the hashcode for this LinterExtensionDescriptor
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Id.GetHashCode();
+        }
+
+        private const string NONE_DESCRIPTOR_GUID = "7b75fb44-43fd-4631-a878-29f4d5d8399a";
+        
+        internal static LinterExtensionDescriptor DefaultDescriptor => new LinterExtensionDescriptor(NONE_DESCRIPTOR_GUID, Properties.Resources.NoneLinterDescriptorName);
     }
 }
