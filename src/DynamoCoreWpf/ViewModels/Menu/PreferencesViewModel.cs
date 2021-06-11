048b973e18ed062cf226f1abfa5de78ddcfc96bf
Aparajit Pratap <aparajit.pratap@autodesk.com>
2021-06-11 12:36:21 -0400

    filter programdata path from dropdown list

--- src/DynamoCoreWpf/ViewModels/Menu/PreferencesViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Menu/PreferencesViewModel.cs
@@ -251,8 +251,12 @@ namespace Dynamo.ViewModels
                 if (packagePathsForInstall == null || !packagePathsForInstall.Any())
                 {
                     packagePathsForInstall = new ObservableCollection<string>();
+                    var programDataPath = new Uri(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData)
+                                                 + "\\");
+                    
                     var customPaths = preferenceSettings.CustomPackageFolders.Where(
-                        x => x != DynamoModel.StandardLibraryToken);
+                        x => x != DynamoModel.StandardLibraryToken && !programDataPath.IsBaseOf(new Uri(x)));
+                    
 
                     foreach (var path in customPaths)
                     {
