24fa56080611ceac4d30703fef1be9d6a165e6b8
Martin Stacey <martin__stacey@hotmail.com>
2021-06-16 09:37:49 +0100

    Add bullet point support

--- src/DynamoCore/Graph/Notes/NoteModel.cs
+++ src/DynamoCore/Graph/Notes/NoteModel.cs
@@ -9,6 +9,13 @@ namespace Dynamo.Graph.Notes
     /// </summary>
     public class NoteModel : ModelBase
     {
+        private const string ROUND_FILLED_BULLET = "\u2022";
+        private const string ROUND_EMPTY_BULLET = "\u25E6";
+        private const string SQUARE_FILLED_BULLET = "\u2023";
+        private string LEFT_INDENT = new String(' ', 1);
+        private string TAB_INDENT = new String(' ', 2);
+        private string RIGHT_INDENT = new String(' ', 1);
+
         private string text;
       
         /// <summary>
@@ -19,7 +26,7 @@ namespace Dynamo.Graph.Notes
             get { return text; }
             set
             {
-                text = value;
+                text = AddBulletPointSupport(value);
                 RaisePropertyChanged("Text");
             }
         }
@@ -76,6 +83,119 @@ namespace Dynamo.Graph.Notes
             // Notify listeners that the position of the note has changed, 
             // then parent group will also redraw itself.
             ReportPosition();
+        }
+
+        #endregion
+
+        #region BulletSupport
+
+        private string AddBulletPointSupport(string text)
+        {
+            if (text.Length == 0)
+                return text;
+
+            string firstTabIndent = LEFT_INDENT + TAB_INDENT;
+            string secondTabIndent = LEFT_INDENT + TAB_INDENT + TAB_INDENT;
+            string leftIndentedBullet = LEFT_INDENT + ROUND_FILLED_BULLET + RIGHT_INDENT;
+            string firstTabIndentedBullet = firstTabIndent + ROUND_EMPTY_BULLET + RIGHT_INDENT;
+            string secondTabIndentedBullet = secondTabIndent + SQUARE_FILLED_BULLET + RIGHT_INDENT;
+
+            text = TurnTabsToSpaces(
+                text,
+                leftIndentedBullet,
+                firstTabIndent, firstTabIndentedBullet,
+                secondTabIndentedBullet);
+
+            text = ConvertDashToBullet(
+                text,
+                leftIndentedBullet,
+                firstTabIndent, firstTabIndentedBullet,
+                secondTabIndent, secondTabIndentedBullet);
+            text = ConvertIndentedBullets(
+                text,
+                firstTabIndent, firstTabIndentedBullet,
+                secondTabIndent, secondTabIndentedBullet);
+
+            return text;
+        }
+
+        private string TurnTabsToSpaces(
+            string text,
+            string leftIndentedBullet,
+            string firstTabIndent,
+            string firstTabIndentedBullet,
+            string secondTabIndentedBullet)
+        {
+            text = ReplaceEachLineStart(text, leftIndentedBullet + "\t", firstTabIndentedBullet);
+            text = ReplaceEachLineStart(text, firstTabIndentedBullet + "\t", secondTabIndentedBullet);
+            text = ReplaceEachLineStart(text, "\t" + LEFT_INDENT, firstTabIndent);
+            text = ReplaceEachLineStart(text, "\t", firstTabIndent);
+            text = text.Replace("\t", TAB_INDENT);
+            return text;
+        }
+
+        private string ConvertDashToBullet(
+            string text,
+            string leftIndentedBullet,
+            string firstTabIndent,
+            string firstTabIndentedBullet,
+            string secondTabIndent,
+            string secondTabIndentedBullet
+            )
+        {
+            text = ReplaceEachLineStart(text, "-", leftIndentedBullet);
+            text = ReplaceEachLineStart(text, firstTabIndent + "-", firstTabIndentedBullet);
+            text = ReplaceEachLineStart(text, secondTabIndent + "-", secondTabIndentedBullet);
+
+            return text;
+        }
+
+        private string ConvertIndentedBullets(
+            string text,
+            string firstTabIndent,
+            string firstTabIndentedBullet,
+            string secondTabIndent,
+            string secondTabIndentedBullet
+            )
+        {
+            text = ReplaceEachLineStart(text, firstTabIndent + ROUND_FILLED_BULLET, firstTabIndentedBullet);
+            text = ReplaceEachLineStart(text, secondTabIndent + ROUND_EMPTY_BULLET, secondTabIndentedBullet);
+            return text;
+        }
+
+        private string AddNextBullet(
+            string text,
+            string leftIndentedBullet,
+            string firstTabIndentedBullet,
+            string secondTabIndentedBullet)
+        {
+
+            if (!text.EndsWith("\n"))
+                return text;
+            var lines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            var previousLine = lines[lines.Length - 2];
+
+            if (previousLine.StartsWith(leftIndentedBullet))
+                text = text + leftIndentedBullet;
+
+            if (previousLine.StartsWith(firstTabIndentedBullet))
+                text = text + firstTabIndentedBullet;
+
+            if (previousLine.StartsWith(secondTabIndentedBullet))
+                text = text + secondTabIndentedBullet;
+
+            return text;
+        }
+
+        private string ReplaceEachLineStart(string text, string oldChars, string newChars)
+        {
+            if (text.StartsWith(oldChars))
+            {
+                text = text.Remove(0, oldChars.Length);
+                text = newChars + text;
+            }
+            text = text.Replace("\n" + oldChars, "\n" + newChars);
+            return text;
         }
 
         #endregion
