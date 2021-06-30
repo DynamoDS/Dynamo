b14fd265424c1c6c7a5616a23e1a33283f10cc73
t-tellro <roberto.tellez@globant.com>
2021-06-30 17:37:59 -0500

    DYN-3767 PopupWindow
    The changes are basically the creation of the PopupWindow UI, also I included some classes that will be needed later for supporting the Step hierarchy in which the child classes are Survey, Tooltip, Welcome and ExitTour.
    
    One of the functionalities included is that when you press the close button the popup is closed (as expected).
    
    Three buttons were added in the PopupWindow but for Welcome screen just the "Start Tour" button is shown, the popup pointer is hidden. For Tooltips the navigation controls and the pointer are shown.
    Also the CustomRichTextBox class was added to enable adding a TextBox in a Tooltip/Welcome screen with functionalities like showing hyperlinks, text in bold, bullet point and images.

---
+++ src/DynamoCoreWpf/UI/GuidedTour/ExitTour.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using Dynamo.Wpf.ViewModels.GuidedTour;
+using Dynamo.Wpf.Views.GuidedTour;
+
+namespace Dynamo.Wpf.UI.GuidedTour
+{
+    /// <summary>
+    /// This class represent the Notification Toast popup that will be shown when a Dynamo Guided Tour is closed
+    /// </summary>
+    public class ExitTour : Step
+    {
+        public ExitTour(HostInfo host, double width, double height)
+            : base(host, width, height)
+        {
+
+        }
+
+        protected override void CreatePopup()
+        {
+            //In this place will go the code for creating a ExitTourWindow window (Notification Toast)
+        }
+    }
+}
---
+++ src/DynamoCoreWpf/UI/GuidedTour/Survey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Dynamo.Wpf.UI.GuidedTour
+{
+    public class Survey : Step
+    {
+        public Survey(HostInfo host, double width, double height)
+            :base(host, width, height)
+        {
+
+        }
+
+        protected override void CreatePopup()
+        {
+            //In this place will go the code for creating a SurveyWindow
+        }
+    }
+}
