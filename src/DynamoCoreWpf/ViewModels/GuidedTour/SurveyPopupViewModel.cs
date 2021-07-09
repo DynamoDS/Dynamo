e1fb82c379f421a955f120cd7850d3d5f11efddb
t-tellro <roberto.tellez@globant.com>
2021-07-09 10:28:08 -0500

    DYN-3768-SurveyWindowUI
    I created 3 resources for the two titles of the SurveyWindow and one for the text content.
    I modified the CustomRichTextBox because hyperlink names separated by spaces were not supported then I did some changes and now are working.
    I added the RatingControl UI and the SurveyPopup UI and some used styles in the DynamoModern.xaml.
    Finally I added a instance of the Survey in the DynamoView.xaml.cs and now a survey popup will be displayed just for testing purposes.

---
+++ src/DynamoCoreWpf/ViewModels/GuidedTour/SurveyPopupViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Wpf.UI.GuidedTour;
+
+namespace Dynamo.Wpf.ViewModels.GuidedTour
+{
+    public class SurveyPopupViewModel
+    {
+        #region Private Properties
+        private Survey step;
+        #endregion
+
+        /// <summary>
+        /// This property hold a reference to the Step that was created (can be Welcome, Survey, Tooltip, ExitTour). 
+        /// </summary>
+        public Survey Step
+        {
+            get
+            {
+                return step;
+            }
+            set
+            {
+                step = value;
+            }
+        }
+
+        /// <summary>
+        /// For creating a SurveyPopupViewModel we need to pass the step to be used 
+        /// </summary>
+        /// <param name="popupType">Step is a abstract class then the parameter can be any of the child clases like Welcome, Tooltip, Survey....</param>
+        public SurveyPopupViewModel(Survey popupType)
+        {
+            Step = popupType;
+        }
+    }
+}
