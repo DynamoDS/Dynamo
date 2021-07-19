699036291fb12bdd05c06883b1ccf67a5f9d6b2e
t-tellro <roberto.tellez@globant.com>
2021-07-19 14:24:57 -0500

    DYN-3834-GeoScaling-UnitTest Code Review
    I had to split the tests in two (one for Automatic and other for Manual) due that the Resources strings can not be used as a parameters in nUnit Tests parameters (since parameters expected should be constant values).

--- test/DynamoCoreWpfTests/PreferencesGeometryScalingTests.cs
+++ test/DynamoCoreWpfTests/PreferencesGeometryScalingTests.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using Dynamo.Graph.Nodes;
-using Dynamo.Tests;
-using Dynamo.ViewModels;
 using Dynamo.Wpf.Views;
 using DynamoCoreWpfTests.Utility;
 using NUnit.Framework;
@@ -13,7 +11,7 @@ using NUnit.Framework;
 namespace DynamoCoreWpfTests
 {
     [TestFixture]
-    class PreferencesGeometryScalingTests : DynamoTestUIBase
+    public class PreferencesGeometryScalingTests : DynamoTestUIBase
     {
         public override void Open(string path)
         {
@@ -25,28 +23,21 @@ namespace DynamoCoreWpfTests
         {
             libraries.Add("VMDataBridge.dll");
             libraries.Add("ProtoGeometry.dll");
-            libraries.Add("DesignScriptBuiltin.dll");
             base.GetLibrariesToPreload(libraries);
         }
 
         /// <summary>
         /// The test validates that when RunType=Automatic and the MarkNodesAsModifiedAndRequestRun() method is called the graph execution is kicked off
-        /// The test validates that when RunType=Manual and the MarkNodesAsModifiedAndRequestRun() method is called the graph execution is NOT kicked off
         /// </summary>
         [Test]
-        [TestCase("Automatic", ElementState.Warning)]
-        [TestCase("Manual", ElementState.Active)]
-        public void PreferencesGeoScaling_RunGraph_Automatic_Mode(string runMode, ElementState expectedNodeState)
+        public void PreferencesGeoScaling_RunGraph_Automatic()
         {
             //The GeometryScalingCodeBlock.dyn contains a CodeBlock with a large number that needs ScaleFactor > Medium
             Open(@"core\GeometryScalingCodeBlock.dyn");
 
-            if (runMode.Equals("Automatic"))
-            {
-                //Change the RunType to Automatic, so when the MarkNodesAsModifiedAndRequestRun() is called a graph execution will be kicked off 
-                View.RunSettingsControl.RunTypesComboBox.SelectedItem = View.RunSettingsControl.RunTypesComboBox.Items[1];
-                DispatcherUtil.DoEvents();
-            }
+            //Change the RunType to Automatic, so when the MarkNodesAsModifiedAndRequestRun() is called a graph execution will be kicked off 
+            View.RunSettingsControl.RunTypesComboBox.SelectedItem = View.RunSettingsControl.RunTypesComboBox.Items[1];
+            DispatcherUtil.DoEvents();
 
             //Creates the Preferences dialog and the ScaleFactor = 2 ( Medium)
             var preferencesWindow = new PreferencesView(View);
@@ -65,8 +56,36 @@ namespace DynamoCoreWpfTests
             var nodeView = NodeViewWithGuid("bcf1c893-5311-4dff-9a04-203ffb9d426c");
 
             //When RunType = Automatic when the graph is executed the ByCenterPointRadius node change status to Warning due that ScaleFactor = Small
-            //When RunType = Manual when the graph is NOT executed then the ByCenterPointRadius status will be Active
-            Assert.AreEqual(nodeView.ViewModel.State, expectedNodeState);
+            Assert.AreEqual(nodeView.ViewModel.State, ElementState.Warning);
+        }
+
+        /// <summary>
+        /// The test validates that when RunType=Manual and the MarkNodesAsModifiedAndRequestRun() method is called the graph execution is NOT kicked off
+        /// </summary>
+        [Test]
+        public void PreferencesGeoScaling_RunGraph_Manual_Mode()
+        {
+            //The GeometryScalingCodeBlock.dyn contains a CodeBlock with a large number that needs ScaleFactor > Medium
+            Open(@"core\GeometryScalingCodeBlock.dyn");
+
+            //Creates the Preferences dialog and the ScaleFactor = 2 ( Medium)
+            var preferencesWindow = new PreferencesView(View);
+            preferencesWindow.Show();
+            DispatcherUtil.DoEvents();
+
+            //Change the RadioButton checked so the ScaleFactor is updated to -2 (Small)
+            preferencesWindow.RadioSmall.IsChecked = true;
+            DispatcherUtil.DoEvents();
+
+            //Close the Preferences Dialog and due that the ScaleFactor was updated the MarkNodesAsModifiedAndRequestRun() method will be called
+            preferencesWindow.CloseButton.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+            DispatcherUtil.DoEvents();
+
+            //This will get the Circle.ByCenterPointRadius node (connected to the CodeBlock)
+            var nodeView = NodeViewWithGuid("bcf1c893-5311-4dff-9a04-203ffb9d426c");
+
+            //When RunType = Automatic when the graph is executed the ByCenterPointRadius node change status to Warning due that ScaleFactor = Small
+            Assert.AreEqual(nodeView.ViewModel.State, ElementState.Active);
         }
     }
 }
