b6e650f4e8e8062994368cd32dca74c12c54c356
reddyashish <43763136+reddyashish@users.noreply.github.com>
2021-08-04 11:03:43 -0400

    Local Definitions in workspace references

--- src/WorkspaceDependencyViewExtension/Properties/Resources.Designer.cs
+++ src/WorkspaceDependencyViewExtension/Properties/Resources.Designer.cs
@@ -142,11 +142,29 @@ namespace Dynamo.WorkspaceDependency.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Local Definitions.
+        /// </summary>
+        public static string LocalDefinitionsHeaderText {
+            get {
+                return ResourceManager.GetString("LocalDefinitionsHeaderText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Show Workspace References.
         /// </summary>
         public static string MenuItemString {
             get {
                 return ResourceManager.GetString("MenuItemString", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Name.
+        /// </summary>
+        public static string NameHeader {
+            get {
+                return ResourceManager.GetString("NameHeader", resourceCulture);
             }
         }
         
@@ -181,15 +199,6 @@ namespace Dynamo.WorkspaceDependency.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Name.
-        /// </summary>
-        public static string PackageHeaderName {
-            get {
-                return ResourceManager.GetString("PackageHeaderName", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Packages.
         /// </summary>
         public static string PackageHeaderText {
@@ -201,9 +210,9 @@ namespace Dynamo.WorkspaceDependency.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Path.
         /// </summary>
-        public static string PackagePathText {
+        public static string PathHeader {
             get {
-                return ResourceManager.GetString("PackagePathText", resourceCulture);
+                return ResourceManager.GetString("PathHeader", resourceCulture);
             }
         }
         
@@ -244,11 +253,20 @@ namespace Dynamo.WorkspaceDependency.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Size.
+        /// </summary>
+        public static string SizeHeader {
+            get {
+                return ResourceManager.GetString("SizeHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Version.
         /// </summary>
-        public static string VersionHeaderText {
+        public static string VersionHeader {
             get {
-                return ResourceManager.GetString("VersionHeaderText", resourceCulture);
+                return ResourceManager.GetString("VersionHeader", resourceCulture);
             }
         }
     }
