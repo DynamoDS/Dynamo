524a468a742542df046067b73432aaf5803fae38
michael kirschner <michael.kirschner@autodesk.com>
2021-08-04 14:56:21 -0400

    obsolete public field

--- src/DynamoCore/Core/NodeModelAssemblyLoader.cs
+++ src/DynamoCore/Core/NodeModelAssemblyLoader.cs
@@ -22,8 +22,10 @@ namespace Dynamo.Models
         #region Properties/Fields
 
         /// <summary>
-        /// Loaded Assemblies.
+        /// Used at startup to avoid reloading NodeModels from assemblies that have already been loaded.
+        /// Is NOT kept in sync with latest loaded assemblies - use LoadedAssemblies Property for that.
         /// </summary>
+        [Obsolete("Will be made internal, please use LoadedAssemblies Property.")]
         public readonly HashSet<string> LoadedAssemblyNames = new HashSet<string>();
         private readonly HashSet<Assembly> loadedAssemblies = new HashSet<Assembly>();
 
