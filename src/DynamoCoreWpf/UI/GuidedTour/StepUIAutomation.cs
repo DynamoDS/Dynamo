449c4d87f487ae3dace7f2a96b175e75a3a6899d
Roberto T <61755417+RobertGlobant20@users.noreply.github.com>
2021-08-19 00:57:47 -0500

    Dyn 3908 open controls by step (#11937)
    * DYN-3908-OpenControls-ByStep
    
    I modified the json file to include the UI Automation part
    Also I created a new class called StepUIAutomation that will be used by a Step to fetch the information from the json file.
    Finally I modified the GuidesManager class for including a method that will create the Automation information and the Guide class for UIAutomation action when the Next or Back button are pressed in the current Popup.
    
    * DYN-3908-OpenControls-ByStep
    
    Added functionality that when a Tour is closed and the ExitTour popup is shown if a new Tour is opened the previous ExitTour will be closed.
    
    * DYN-3908-OpenControls-ByStep
    
    I modified the guides json file to include the UI Automation part
    Also I created a new class called StepUIAutomation that will be used by a Step to fetch the information from the json file.
    I modified the GuidesManager class for including a method that will create the Automation information and the Guide class for UIAutomation action that will be used when the Next or Back button are pressed in the current Popup.
    Finally I added some event handlers for the RealTimeInfoWindow, (this popup will appear just when the current Tour is closed without finishing it). This event handlers will prevent to have a pile of RealTimeInfoWindow if several tours are not finished.
    
    * DYN-3908-OpenControls-ByStep Code Review
    
    I updated all the contents for each step following the wiki entry Content for Dynamo First Time Use Guide.
    Also updated the Height for the Run Status popup
    Also moved the functions for executing the UI Automation from the Guide class to the Step class.
    
    * DYN-3908-OpenControls-ByStep Code Review
    
    Missing to update the text content for the ExitTour popup

---
+++ src/DynamoCoreWpf/UI/GuidedTour/StepUIAutomation.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Dynamo.Wpf.UI.GuidedTour
+{
+    /// <summary>
+    /// This class will be used as a container for the UI Automation information read from the json file
+    /// </summary>
+    public class StepUIAutomation
+    {
+        /// <summary>
+        /// This Sequence will be unique for each automation step
+        /// </summary>
+        public double Sequence { get; set; }
+
+        /// <summary>
+        /// The Control type represent the WPF Control type that will be Automated, like MenuItem, Window, Dropdown
+        /// </summary>
+        public string ControlType { get; set; }
+
+        /// <summary>
+        /// This contain a string ID of the UI Automation 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// This will represent the action that will be executed as part of the UI Automation
+        /// </summary>
+        public string Action { get; set; }
+
+        /// <summary>
+        /// This will be the WPF UI Element in which the Action will be executed
+        /// </summary>
+        public UIElement UIElementAutomation { get; set; }
+    }
+}
