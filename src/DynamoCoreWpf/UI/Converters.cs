7dd5011d39093b28b56ef35c4f7b3001e608fb23
Marco Juliani <marcotjuliani@gmail.com>
2021-08-22 10:11:26 +0100

    Wire Behaviour UI fixes (#11954)
    * Wire UI Fixes
    
     1. Made wire tooltip highest ZIndex.
     2. Made first line of tooltip bold, the rest is regular
     3. Added a clickable item under View-> Connectors to turn tooltip on/off.
    
    * Added comments.

--- src/DynamoCoreWpf/UI/Converters.cs
+++ src/DynamoCoreWpf/UI/Converters.cs
@@ -32,6 +32,34 @@ using Thickness = System.Windows.Thickness;
 
 namespace Dynamo.Controls
 {
+    public class ToolTipFirstLineOnly : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string incomingString = value as string;
+            return incomingString.Split(new[] { '\r', '\n' }, 2)[0].Trim();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ToolTipAllLinesButFirst : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string incomingString = value as string;
+            return incomingString.Split(new[] { '\r', '\n' }, 2)[1].Trim();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class TooltipLengthTruncater : IValueConverter
     {
         private const int MaxChars = 100;
