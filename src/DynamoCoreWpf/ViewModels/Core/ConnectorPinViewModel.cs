d0111ad748ea1f694fc4f6eee0896aba19b13f19
Marco Juliani <marcotjuliani@gmail.com>
2021-09-02 18:00:22 +0100

    (Updated) Wires: New icon resources added. (#12006)
    * Re-added refs.
    
    * Updated reference calls.
    
    * PR fixes.
    
    * PR fixes.
    
    * PR fixes.

--- src/DynamoCoreWpf/ViewModels/Core/ConnectorPinViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Core/ConnectorPinViewModel.cs
@@ -87,16 +87,17 @@ namespace Dynamo.ViewModels
                 RaisePropertyChanged(nameof(Model));
             }
         }
-        /// <summary>
-        /// In this case width == height for connectorPinModels, 
-        /// so we only need one property that can be used for both.
-        /// </summary>
+      /// <summary>
+      /// This property is used to center the ConnectorPinIcon in views
+      /// as well as to offset the coordinate used for drawing
+      /// bezier connectors through.
+      /// </summary>
         [JsonIgnore]
-        public double HalfWidth
+        public double OneThirdWidth
         {
             get
             {
-                return Model.Width * 0.5;
+                return Model.Width * 0.33333;
             }
         }
 
