340935158383e2798cf3b413f973c00345523d85
filipeotero <89042471+filipeotero@users.noreply.github.com>
2021-10-01 14:32:08 -0300

    Loading html from resources

--- src/DynamoCoreWpf/Views/GuidedTour/WebBrowserWindow.xaml.cs
+++ src/DynamoCoreWpf/Views/GuidedTour/WebBrowserWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
@@ -19,12 +21,11 @@ namespace Dynamo.Wpf.Views.GuidedTour
         private const int tooltipOffset = 10;
         //The headers size of the main popup to add an offset and adjust this popup
         private const int headerOffset = 50;
-        //Directory where the html files are located
-        private const string htmlFileDirectory = "file:///{0}/Views/GuidedTour/HtmlPages/{1}.html";
+        
         public WebBrowserWindow(PopupWindowViewModel viewModel, HostControlInfo hInfo)
         {
             InitializeComponent();
-            
+
             webBrowser.Width = viewModel.Width;
             //The height is subtracted by a const that sums the height of the header and footer of the popup
             webBrowser.Height = viewModel.Height - headerAndFooterCompensation;
@@ -38,9 +39,9 @@ namespace Dynamo.Wpf.Views.GuidedTour
             //Vertical offset plus 50 is to compensate the header size
             VerticalOffset = hInfo.VerticalPopupOffSet + headerOffset;
 
-            string curDir = Directory.GetCurrentDirectory();
-            Uri webBrowserUri = new Uri(string.Format(htmlFileDirectory, curDir, hInfo.HtmlPage));
-            webBrowser.Navigate(webBrowserUri);
+            var bodyHtmlPage = GuidedTourResources.LoadContentFromResources(hInfo.HtmlPage, GetType().Assembly);
+
+            webBrowser.NavigateToString(bodyHtmlPage);
         }
     }
 }
