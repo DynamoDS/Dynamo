74736639604d48eeb4bc70453051fef3d3def938
filipeotero <89042471+filipeotero@users.noreply.github.com>
2021-10-21 17:57:46 -0300

    Exit guide modal layout

---
+++ src/DynamoCoreWpf/Views/GuidedTour/ExitGuideWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dynamo.Wpf.Views.GuidedTour
+{
+    /// <summary>
+    /// Interaction logic for ExitGuideWindow.xaml
+    /// </summary>
+    public partial class ExitGuideWindow : Popup
+    {       
+
+        public ExitGuideWindow(FrameworkElement mainRootElement)
+        {
+            InitializeComponent();
+
+            ContentRichTextBox.Width = Width;
+
+            VerticalOffset = (mainRootElement.ActualHeight / 2) - (Height / 2);
+            HorizontalOffset = (mainRootElement.ActualWidth / 2) - (Width / 2);
+        }
+
+        private void HideRichTextBox()
+        {
+            Height -= ContentRichTextBox.ActualHeight;
+            ContentRichTextBox.Visibility = Visibility.Collapsed;
+        }
+    }
+}
