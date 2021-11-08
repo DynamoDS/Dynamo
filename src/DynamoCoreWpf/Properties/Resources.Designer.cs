b592a08442be9c8c50971d339c0ecf34ff47b9ad
Ollie Green <oliveredwardgreen@gmail.com>
2021-11-08 15:52:54 +0000

    Updates groups context menu style

--- src/DynamoCoreWpf/Properties/Resources.Designer.cs
+++ src/DynamoCoreWpf/Properties/Resources.Designer.cs
@@ -2885,7 +2885,7 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Add group to group.
+        ///   Looks up a localized string similar to Add Group to Group.
         /// </summary>
         public static string GroupContextMenuAddGroupToGroup {
             get {
