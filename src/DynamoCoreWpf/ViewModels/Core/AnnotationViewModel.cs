eb49f78a0e94fd492db87e51e1da826b6240c648
Ollie Green <oliveredwardgreen@gmail.com>
2021-12-06 13:14:03 +0000

    Update AnnotationViewModel.cs

--- src/DynamoCoreWpf/ViewModels/Core/AnnotationViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Core/AnnotationViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.ComponentModel;
 using System.Linq;
 using System.Windows.Input;
 using System.Windows.Media;
