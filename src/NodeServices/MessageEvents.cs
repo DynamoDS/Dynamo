3a13b0675803c6287472ed7b001c02c5483350e4
aparajit-pratap <aparajit.pratap@autodesk.com>
2021-12-05 22:04:18 -0500

    Report loading of blocked assemblies to user and exclude cases from impacting MTBF results (#12357)
    * show message box to user if there are any blocked assemblies that prevent startup
    
    * catch file load exception at top level and skip reporting to analytics
    
    * cleanup
    
    * cleanup
    
    * show same msg box for importing blocked assemblies without crashing Dynamo
    
    * cleanup
    
    * rethrow explicit load library exception for blocked assembly and handle at top level
    
    * cleanup
    
    * cleanup
    
    * add code comments
    
    * edit user facing message
    
    * use new message box style, add message box for package loading of blocked assemblies
    
    * add comment
    
    * add new exception type
    
    * add code comments

--- src/NodeServices/MessageEvents.cs
+++ src/NodeServices/MessageEvents.cs
@@ -22,7 +22,18 @@ namespace DynamoServices
             if (LogWarningMessage != null)
                 LogWarningMessage(new LogWarningMessageEventArgs(message));
         }
+    }
 
+    internal static class LoadLibraryEvents
+    {
+        /// <summary>
+        /// Raised when loading of a library fails. A failure message is passed as a parameter.
+        /// </summary>
+        internal static event Action<string, string> LoadLibraryFailure;
 
+        internal static void OnLoadLibraryFailure(string failureMessage, string messageBoxTitle)
+        {
+            LoadLibraryFailure?.Invoke(failureMessage, messageBoxTitle);
+        }
     }
 }
