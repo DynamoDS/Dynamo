720f094928aa82117325bbf5818e0f3708c70957
pinzart <tiberiu.pinzariu@autodesk.com>
2022-01-24 12:54:48 -0500

    Update PreferenceSettings.cs

--- src/DynamoCore/Configuration/PreferenceSettings.cs
+++ src/DynamoCore/Configuration/PreferenceSettings.cs
@@ -468,7 +468,7 @@ namespace Dynamo.Configuration
         /// This static property is not serialized and is assigned IronPythonResolveTargetVersion's value 
         /// if found at deserialize time.
         /// </summary>
-        internal static Version IronPythonResolveVersion = new Version(2, 1, 0);
+        internal static Version IronPythonResolveVersion = new Version(2, 2, 0);
 
         /// <summary>
         /// The Version of the IronPython package that Dynamo will download when it is found as missing in graphs.
