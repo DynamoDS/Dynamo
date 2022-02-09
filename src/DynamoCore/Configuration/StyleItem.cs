31f38a834285ee0c4eeabfe144ede97f1c942f13
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-02-09 14:48:49 -0300

    Dyn 4542 default group settings (#12604)
    * Saving group style settings
    
    * Unit Test
    
    * Adding some comments
    
    * retrigger checks
    
    * Fixing comment spell
    
    * Minor style fixes
    
    * Changing class name
    
    * Changing to groupstyle class
    
    * Renaming group style property and uni test fix

---
+++ src/DynamoCore/Configuration/StyleItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.Configuration
+{
+    /// <summary>
+    /// This class stores the group styles added by the user
+    /// </summary>
+    public class StyleItem
+    {
+        /// This property will contain the Group Name of the stored style
+        public string Name { get; set; }
+        /// This property will contain the color in hexadecimal
+        public string HexColorString { get; set; }
+    }
+}
