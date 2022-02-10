027673a7871494c1dfa7c9edb2e08e4bb7ea9171
pinzart <tiberiu.pinzariu@autodesk.com>
2022-02-10 14:22:51 -0500

    update

---
+++ test/Engine/ProtoTest/WarningCompatibilityTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ProtoTest
+{
+    class WarningCompatibilityTest : ProtoTestBase
+    {
+        [Test]
+        [Description("Tests the backwards compatibility of the RuntimeStatus.Warnings property")]
+        public void TestWarningCompatibility()
+        {
+            TestWarningsBackwardComp.Test.TestWarningBackwardsCompatibility();
+        }
+    }
+}
---
+++ test/Tools/TestWarningsBackwardComp/Test.cs
@@ -0,0 +1,29 @@
+﻿using ProtoCore;
+using ProtoCore.DSASM;
+using ProtoCore.Runtime;
+using System;
+using System.Collections.Generic;
+
+namespace TestWarningsBackwardComp
+{
+    public class Test
+    {
+        // This assembly references ProtoCore @ Dynamo 2.13
+        public static void TestWarningBackwardsCompatibility()
+        {
+            RuntimeCore runtimeCore = new RuntimeCore(new Heap(), new Options());
+            RuntimeStatus st = new RuntimeStatus(runtimeCore);
+            st.LogWarning(WarningID.Default, "Test", "Test", 0, 0);
+
+
+            IEnumerable<WarningEntry> warnings = st.Warnings;
+            foreach (var item in warnings)
+            {
+                if (item.ID != WarningID.Default || item.Message != "Test")
+                {
+                    throw new Exception("Warning not found");
+                }
+            }
+        } 
+    }
+}
