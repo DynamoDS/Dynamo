75d467c8de668f6615c637d43e37f69e908e6049
Aparajit Pratap <aparajit.pratap@autodesk.com>
2022-02-11 16:11:32 -0500

    remove migration popup

--- src/DynamoCore/Models/DynamoModelDelegates.cs
+++ src/DynamoCore/Models/DynamoModelDelegates.cs
@@ -40,6 +40,4 @@ namespace Dynamo.Models
     /// <param name="sender">Workspace</param>
     /// <param name="e"><see cref="EventArgs"/></param>
     public delegate void NodeEventHandler(object sender, EventArgs e);
-
-    internal delegate void SettingsMigrationHandler(SettingsMigrationEventArgs args);
 }
