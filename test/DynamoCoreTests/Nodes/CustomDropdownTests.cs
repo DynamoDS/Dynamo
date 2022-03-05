5a4ac2c058ff196e28b4302a95342b3800ad4ecd
Long Nguyen <longnguyen.connect@gmail.com>
2022-03-05 11:59:52 +0200

    DYN file test completed

--- test/DynamoCoreTests/Nodes/CustomDropdownTests.cs
+++ test/DynamoCoreTests/Nodes/CustomDropdownTests.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using CoreNodeModels;
+using CoreNodeModels.Input;
 using Dynamo.PackageManager;
 using NUnit.Framework;
 using SystemTestServices;
@@ -35,13 +38,16 @@ namespace Dynamo.Tests.Nodes
             Assert.AreEqual(pkg.Name, "Dynamo Samples");
 
             // Run the graph with correct info serialized, node should deserialize to correct selection
-            string path = Path.Combine(TestDirectory, "pkgs", "Dynamo Samples", "extra", "DropDownSample.dyn");
+            string path = Path.Combine(TestDirectory, "pkgs", "Dynamo Samples", "extra", "CustomDropdownMenuNodeSample.dyn");
             RunModel(path);
 
             var node = CurrentDynamoModel.CurrentWorkspace.Nodes.FirstOrDefault();
-            // Second selection selected
-            Assert.AreEqual(1, node.GetType().GetProperty("SelectedIndex").GetValue(node));
-            Assert.AreEqual("Cersei", node.GetType().GetProperty("SelectedString").GetValue(node));
+
+            object itemsAsObject = node.GetType().GetProperty(nameof(CustomSelectionNodeModel.Items), typeof(ObservableCollection<CustomSelectionItemViewModel>)).GetValue(node);
+            Assert.NotNull(itemsAsObject);
+            var items = (ObservableCollection<CustomSelectionItemViewModel>)(itemsAsObject);
+            Assert.AreEqual(3, items.Count);
+            Assert.AreEqual("One", items[0].Name);
         }
 
 
