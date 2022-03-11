f1b8ab86c575cb3f1589210df2aaf0a2acbba96f
tanga <aaron.tang@autodesk.com>
2022-03-10 23:48:23 -0500

    Adjust unit tests since we are changing how default styles serialized

--- test/DynamoCoreWpfTests/CoreUITests.cs
+++ test/DynamoCoreWpfTests/CoreUITests.cs
@@ -535,16 +535,16 @@ namespace DynamoCoreWpfTests
             resultSetting = PreferenceSettings.Load(tempPath);
 
             // Test if the fields are being saved
-            Assert.AreEqual(1, initalSetting.GroupStyleItemsList.Count);
-            Assert.AreEqual(resultSetting.GroupStyleItemsList[0].Name, initalSetting.GroupStyleItemsList[0].Name);
-            Assert.AreEqual(resultSetting.GroupStyleItemsList[0].HexColorString, initalSetting.GroupStyleItemsList[0].HexColorString);
+            Assert.AreEqual(5, initalSetting.GroupStyleItemsList.Count);
+            Assert.AreEqual(resultSetting.GroupStyleItemsList[4].Name, initalSetting.GroupStyleItemsList[4].Name);
+            Assert.AreEqual(resultSetting.GroupStyleItemsList[4].HexColorString, initalSetting.GroupStyleItemsList[4].HexColorString);
 
             // Test loading the settings defined in the xml configuration file
             var filePath = Path.Combine(GetTestDirectory(ExecutingDirectory), @"settings\DynamoSettings-OneGroupStyle.xml");
             PreferenceSettings OneGroupStyle = PreferenceSettings.Load(filePath);
-            Assert.AreEqual(1, OneGroupStyle.GroupStyleItemsList.Count);
-            Assert.AreEqual(OneGroupStyle.GroupStyleItemsList[0].Name, initalSetting.GroupStyleItemsList[0].Name);
-            Assert.AreEqual(OneGroupStyle.GroupStyleItemsList[0].HexColorString, initalSetting.GroupStyleItemsList[0].HexColorString);
+            Assert.AreEqual(5, OneGroupStyle.GroupStyleItemsList.Count);
+            Assert.AreEqual(OneGroupStyle.GroupStyleItemsList[4].Name, initalSetting.GroupStyleItemsList[4].Name);
+            Assert.AreEqual(OneGroupStyle.GroupStyleItemsList[4].HexColorString, initalSetting.GroupStyleItemsList[4].HexColorString);
         }
 
         [Test]
