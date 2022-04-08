9f6e0596afac09c9c2af6b04cd183a61c34f80b3
Craig Long <craig.long@autodesk.com>
2022-04-07 20:01:09 -0400

    Fix test

--- src/DynamoCLI/CommandLineRunner.cs
+++ src/DynamoCLI/CommandLineRunner.cs
@@ -45,6 +45,8 @@ namespace DynamoCLI
                 Console.WriteLine("geometryFilePath option is only available when running DynamoWPFCLI, not DynamoCLI");
             }
 
+            model.HostAnalyticsInfo = cmdLineArgs.AnalyticsInfo;
+
             cmdLineArgs.ImportedPaths.ToList().ForEach(path =>
             {
                 ImportAssembly(model, path);
