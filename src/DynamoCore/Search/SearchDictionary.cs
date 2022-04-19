5df10f12158feb657782e14fad589fba97787350
jesusalvino <jesus.alvino@globant.com>
2022-04-19 10:30:14 -0500

    Refactoring the SpecialAndInvalidCharacters to single line

--- src/DynamoCore/Search/SearchDictionary.cs
+++ src/DynamoCore/Search/SearchDictionary.cs
@@ -325,23 +325,8 @@ namespace Dynamo.Search
 
         public static Char[] SpecialAndInvalidCharacters()
         {
-            char[] invalidFileChars = System.IO.Path.GetInvalidFileNameChars();
-
-            List<string> filteredChars = new List<string>();
-            foreach (char someChar in invalidFileChars)
-            {   // Excluding white spaces and uncommon characters, only keeping the displayed in the Windows alert
-                if (!Char.IsWhiteSpace(someChar) && (int)someChar > 31)
-                {
-                    filteredChars.Add(someChar.ToString());
-                }
-            }
-
-            char[] result = new char[filteredChars.Count];
-            for (int i = 0; i < filteredChars.Count; i++)
-            {
-                result[i] = char.Parse(filteredChars[i]);
-            }
-            return result;
+            // Excluding white spaces and uncommon characters, only keeping the displayed in the Windows alert
+            return System.IO.Path.GetInvalidFileNameChars().Where(x => !char.IsWhiteSpace(x) && (int)x > 31).ToArray();
         }
 
         public static bool ContainsSpecialCharacters(string element)
