1d5b1dbac266f12d8f86f3d3ff2e3323ee5cfec2
Ashish Aggarwal <ashish.aggarwal@autodesk.com>
2022-05-31 17:11:23 -0400

    Fix Publish package remove item bug (#12941)
    * update remove item
    
    * cvb

--- src/DynamoCoreWpf/ViewModels/PackageManager/PublishPackageViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/PackageManager/PublishPackageViewModel.cs
@@ -1335,15 +1335,28 @@ namespace Dynamo.PackageManager
         {
             if (!(parameter is PackageItemRootViewModel packageItemRootViewModel)) return;
 
-            if (packageItemRootViewModel.FileInfo == null || packageItemRootViewModel.FileInfo == null)
-            {
-                PackageContents.Remove(PackageContents
-                    .First(x => x.Name == packageItemRootViewModel.Name));
-                return;
-            }
+            string fileName = packageItemRootViewModel.FileInfo == null ? packageItemRootViewModel.Name : packageItemRootViewModel.FileInfo.FullName;
+            string fileType = packageItemRootViewModel.DependencyType.ToString();
 
-            PackageContents.Remove(PackageContents
-                .First(x => x.FileInfo?.FullName == packageItemRootViewModel.FileInfo.FullName));
+            if (fileName.ToLower().EndsWith(".dll") || fileType.Equals(DependencyType.Assembly))
+            {
+                Assemblies.Remove(Assemblies
+                    .First(x => x.Name == Path.GetFileNameWithoutExtension(fileName)));
+            }
+            else if (fileType.Equals(DependencyType.CustomNode))
+            {
+                CustomNodeDefinitions.Remove(CustomNodeDefinitions
+                    .First(x => x.DisplayName == fileName));
+            }
+            else
+            {
+                AdditionalFiles.Remove(AdditionalFiles
+                    .First(x => x == fileName));
+            }
+                        
+            RefreshPackageContents();
+            return;
+
         }
 
         private bool CanShowAddFileDialogAndAdd()
