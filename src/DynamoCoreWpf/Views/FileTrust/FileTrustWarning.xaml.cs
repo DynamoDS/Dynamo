98d7033f70467c52ab7fabec2a5f1134b60e7e89
Roberto T <61755417+RobertGlobant20@users.noreply.github.com>
2022-06-01 14:43:57 -0500

    DYN-4975-FileTrustUI-PreferencesSettings (#12940)
    * DYN-4975-FileTrustUI-PreferencesSettings
    
    I've removed the Debug mode for Preferences settings and also I've added the needed code for execute the Preferences panel in the Security tab and expand the TrustedPaths section.
    
    * DYN-4975-FileTrustUI-PreferencesSettings CodeReview
    
    A new class for preference panel utilities was added also a function that opesn the Preferences panel in a specific tab and with a specific expander expanded was added.
    The function previously described was used for opening the Preferences panel when the FileTrust UI Settings button is pressed.
    
    * DYN-4975-FileTrustUI-PreferencesSettings CodeReview
    
    Adding the Preference panel initial location as parameter in the utility method.
    
    * DYN-4975-FileTrustUI-PreferencesSettings CodeReview
    
    Removed unused usings
    Instead of assigning the namespace I used it directly to get the resources.

--- src/DynamoCoreWpf/Views/FileTrust/FileTrustWarning.xaml.cs
+++ src/DynamoCoreWpf/Views/FileTrust/FileTrustWarning.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Threading;
 using Dynamo.Controls;
 using Dynamo.Models;
 using Dynamo.ViewModels;
-using Dynamo.Wpf.Controls;
+using Dynamo.Wpf.Utilities;
 using Dynamo.Wpf.ViewModels;
 using Dynamo.Wpf.ViewModels.FileTrust;
 
@@ -107,7 +105,9 @@ namespace Dynamo.Wpf.Views.FileTrust
 
         private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
-            //Launch the Preference panel in the Security tab
+            var tabName = Properties.Resources.PreferencesSecuritySettingsTab;
+            var expanderName = Properties.Resources.TrustedPathsExpanderName;
+            PreferencesPanelUtilities.OpenPreferencesPanel(mainWindow, WindowStartupLocation.CenterOwner, tabName, expanderName);
         }
 
         private void CloseFileButton_Click(object sender, RoutedEventArgs e)
