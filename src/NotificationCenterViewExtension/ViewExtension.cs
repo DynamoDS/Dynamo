1a9105e24c48a682f4b32ec8f73c4c583ef549b7
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-06-29 17:16:00 -0300

    ViewExtension project

---
+++ src/NotificationCenterViewExtension/ViewExtension.cs
@@ -0,0 +1,44 @@
+﻿using Dynamo.Models;
+using Dynamo.Wpf.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationCenterViewExtension
+{
+    public class ViewExtension : IViewExtension
+    {
+        public static readonly string ExtensionName = "NotificationUI - WebView2";
+
+        public string UniqueId { 
+            get { return "2c24fadf-38c0-47fb-8b7d-18e6c3053754"; } 
+        }
+
+        public string Name => ExtensionName;
+
+        public void Dispose()
+        {
+            System.GC.SuppressFinalize(this);
+        }
+
+        public void Loaded(ViewLoadedParams viewLoadedParams)
+        {
+            if (!DynamoModel.IsTestMode)
+            {
+
+            }
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Startup(ViewStartupParams viewStartupParams)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
