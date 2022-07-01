7c08274172bf66c4b41196624cb8d0e936f65791
Jorgen Dahl <jorgen.dahl@autodesk.com>
2022-07-01 10:05:19 -0400

    Initial implementation

---
+++ test/DynamoCoreTests/Performance/PerformanceTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Dynamo.Events;
+using Dynamo.Graph.Nodes;
+using Dynamo.Graph.Nodes.CustomNodes;
+using Dynamo.Graph.Workspaces;
+using NUnit.Framework;
+
+namespace Dynamo.Tests
+{
+    [TestFixture, Category("Performance")]
+    public class PerformanceTests : DynamoModelTestBase
+    {
+        private TimeSpan lastExecutionDuration = new TimeSpan();
+
+        protected override void GetLibrariesToPreload(List<string> libraries)
+        {
+            libraries.Add("VMDataBridge.dll");
+            libraries.Add("ProtoGeometry.dll");
+            libraries.Add("DesignScriptBuiltin.dll");
+            libraries.Add("DSCoreNodes.dll");
+            libraries.Add("GeometryColor.dll");
+            base.GetLibrariesToPreload(libraries);
+        }
+        public object[] FindWorkspaces()
+        {
+            var di = new DirectoryInfo(Path.Combine(TestDirectory, "core", "performance"));
+            var fis = di.GetFiles("*.dyn", SearchOption.AllDirectories);
+            return fis.Select(fi => fi.FullName).ToArray();
+        }
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            ExecutionEvents.GraphPostExecution += ExecutionEvents_GraphPostExecution;
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            ExecutionEvents.GraphPostExecution -= ExecutionEvents_GraphPostExecution;
+        }
+
+        private void ExecutionEvents_GraphPostExecution(Session.IExecutionSession session)
+        {
+            lastExecutionDuration = (TimeSpan)session.GetParameterValue(Session.ParameterKeys.LastExecutionDuration);
+        }
+
+        [Test, TestCaseSource("FindWorkspaces"), Category("Performance")]
+        public void PerformanceTest(string filePath)
+        {
+            DoWorkspaceOpenAndCompare(filePath);
+        }
+
+        private void DoWorkspaceOpenAndCompare(string filePath)
+        {
+            OpenModel(filePath);
+
+            var model = CurrentDynamoModel;
+            var ws1 = model.CurrentWorkspace;
+            ws1.Description = "TestDescription";
+
+            var dummyNodes = ws1.Nodes.Where(n => n is DummyNode);
+            if (dummyNodes.Any())
+            {
+                Assert.Inconclusive("The Workspace contains dummy nodes for: " + string.Join(",", dummyNodes.Select(n => n.Name).ToArray()));
+            }
+
+            var cbnErrorNodes = ws1.Nodes.Where(n => n is CodeBlockNodeModel && n.State == ElementState.Error);
+            if (cbnErrorNodes.Any())
+            {
+                Assert.Inconclusive("The Workspace contains code block nodes in error state due to which rest " +
+                                    "of the graph will not execute; skipping test ...");
+            }
+
+            if (((HomeWorkspaceModel)ws1).RunSettings.RunType == Dynamo.Models.RunType.Manual)
+            {
+                RunCurrentModel();
+            }
+
+            var wcd1 = new serializationTestUtils.WorkspaceComparisonData(ws1, CurrentDynamoModel.EngineController);
+
+            // The big hammer, maybe not needed
+            Cleanup();
+
+            // TODO, Switch to the new VM
+
+            Setup();
+
+            OpenModel(filePath);
+            model = CurrentDynamoModel;
+            var ws2 = model.CurrentWorkspace;
+            ws2.Description = "TestDescription";
+
+            if (((HomeWorkspaceModel)ws2).RunSettings.RunType == Dynamo.Models.RunType.Manual)
+            {
+                RunCurrentModel();
+            }
+
+            Assert.NotNull(ws2);
+
+            dummyNodes = ws2.Nodes.Where(n => n is DummyNode);
+            if (dummyNodes.Any())
+            {
+                Assert.Inconclusive("The Workspace contains dummy nodes for: " + string.Join(",", dummyNodes.Select(n => n.Name).ToArray()));
+            }
+
+            var wcd2 = new serializationTestUtils.WorkspaceComparisonData(ws2, CurrentDynamoModel.EngineController);
+
+            serializationTestUtils.CompareWorkspaceModels(wcd1, wcd2, null);
+        }
+    }
+}
