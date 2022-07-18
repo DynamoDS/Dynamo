75e79b28cd66758bdf0359d1596ddf8d6bc066bc
pinzart <tiberiu.pinzariu@autodesk.com>
2022-07-18 10:21:42 -0400

    Update AssemblyInfo.cs

--- src/Engine/EmitMSIL/Properties/AssemblyInfo.cs
+++ src/Engine/EmitMSIL/Properties/AssemblyInfo.cs
@@ -10,3 +10,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2e7be1fb-7038-4277-9dae-fd0faabf5768")]
+[assembly: InternalsVisibleTo("CodeGenILTests")] 
