468d60401d1f5bbd7aa5037d07f77422214eb948
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-07-19 14:29:55 -0300

    Notifications UI web view2 (#13096)
    * Create the notifications button
    
    * Notification ui popup
    
    * fit webview2 into the popup
    
    * pointer tooltip and shadow
    
    * ViewExtension project
    
    * creation of viewextension
    
    * Inserting the notification ui into the NotificationsExtension
    
    * Load local files
    
    * Unit tests
    
    * Notifications Icon
    
    * Removing old created notifications center
    
    * Code refactor
    
    * Reverting dynamo sln
    
    * Assigning comments
    
    * PR comments
    
    * Addressing comments
    
    * updates
    
    * Addressing comments
    
    * inserting npm install notifications package
    
    * misspelling fix
    
    * commenting npm build command
    
    * undoing npm command comment
    
    * Commenting npm command and adding the js bundle file
    
    Co-authored-by: tanga <aaron.tang@autodesk.com>

---
+++ src/Notifications/View/NotificationUI.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dynamo.Notifications.View
+{
+    /// <summary>
+    /// Interaction logic for NotificationUI.xaml
+    /// </summary>
+    public partial class NotificationUI : Popup
+    {
+        private NotificationsUIViewModel notificationsUIViewModel;
+
+        public NotificationUI()
+        {
+            InitializeComponent();
+
+            if (notificationsUIViewModel == null)
+            {
+                notificationsUIViewModel = new NotificationsUIViewModel();
+            }
+
+            DataContext = notificationsUIViewModel;
+
+            //The BackgroundRectangle represent the tooltip background rectangle that is drawn over a Canvas
+            //Needs to be moved 10 pixels over the X axis to show the direction pointers (Height was already increased above)
+            //Needs to be moved 10 pixels over the Y axis to show the shadow at top and bottom.
+            BackgroundRectangle.Rect = new Rect(notificationsUIViewModel.PopupBordersOffSet, notificationsUIViewModel.PopupBordersOffSet, notificationsUIViewModel.PopupRectangleWidth, notificationsUIViewModel.PopupRectangleHeight);
+
+            //The CustomRichTextBox has a margin of 10 in left and 10 in right, also there is a indentation for drawing the PointerDirection of 10 of each side so that's why we are subtracting 40.
+            mainPopupGrid.Width = notificationsUIViewModel.PopupRectangleWidth;
+            mainPopupGrid.Height = notificationsUIViewModel.PopupRectangleHeight;
+            mainPopupGrid.Margin = new Thickness(notificationsUIViewModel.PopupBordersOffSet, notificationsUIViewModel.PopupBordersOffSet, 0, 0);
+        }
+
+        internal void UpdatePopupLocation()
+        {
+            if (IsOpen)
+            {
+                var positionMethod = typeof(Popup).GetMethod("UpdatePosition", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                positionMethod.Invoke(this, null);
+            }
+        }
+    }
+}
