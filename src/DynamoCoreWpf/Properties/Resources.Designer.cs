c88bbf845389262bc8fe7f96447ee79a3ac60cb3
Deyan Nenov <dnenov@archilizer.com>
2022-07-19 15:24:13 +0100

    Dnenov restyle visual restyle (#13075)
    * Terms of Use Restyle
    
    - part of the overall visual restyle in Q3 2022
    - style matched to the existing DynamoMessageBox
    - interim commit
    
    * Icon Change
    
    - swapped window icons
    
    * Edit Text Window Restyle
    
    - restyle Edit text pop-up window
    
    * About Window Restyle
    
    - restyle to the Dynamo About window
    
    * Edit Node Name Dark Theme
    
    - changed Edit Node to dark theme as per Figma
    
    * Small Visual Changes
    
    - small tweaks to UI of Edit, About and TermsOfUse Windows
    
    * UsageReportingAgreement Restyle
    
    - visual update to the Usage Reporting Agreement Window
    - changed the fontsize for all RichTextFiles (this is a limitation)
    
    * Crash Window Restyle (White)
    
    - restyle to Dynamo crash window
    
    * Generic Task Dialog Restyle
    
    - the Generic Task Dialog is the base class that is used to display the 'Graph Contains Unresolved Nodes and Cannot be Saved dialog.' Restyled to match the new Theme.
    
    * Zoomed-in Node Rename
    
    -contextually rename Node directly when under the ZoomThreshold
    
    * About Dialog Fix
    
    - small visual fix to the About Dialog
    - added documentation for the new methods incorporated in the NodeView
    
    * Restyle Comments
    
    - Graph contains unresolved nodes dialog: - blue button, image change
    - Dynamo has crashed - image change
    - About Dynamo - Dynamo icon text needs to be changed to darker color. Same for version text
    
    * Remove Old Icons
    
    - removed png files that are no longer needed
    
    * Text Update
    
    - multiple dialog texts have been updated
      - crash dialog
      - unresolved nodes
    - minor alignment to control buttons of dialogs
    
    * About Dialog Comments
    
    - updated 'About' dialog following team meeting
    - green color of (Up-to-date) changed
    - (Up-to-date) changed to (Up to date)
    - the dialog is now resizable with grip (slow performance noted, not sure how to optimize or if it is possible)
    - minimize, maximize buttons added
    
    * Edit Window Adjustments
    
    - implemented a version of the high-density design style - generally less margins all around
    
    * Escape Closes Dialog Window
    
    - added the ability to close down a dialog window by hitting the Escape key
    
    * Crash Prompt Detail Text Box Fix
    
    - fixed the style of the text box displaying the details of a crash, if expanded
    - a new style created, old style is still in use so remains unchanged
    
    * Crash Dialog Title Text Change
    
    - fixed the double 'Dynamo' text of the Crash Dialog Title inside the US Recourses
    
    * BugFix
    
    - causing a crash when opening Custom Nodes
    
    * TextFix
    
    - introducing a TextBox to allow for inline rename was causing a test to fail

--- src/DynamoCoreWpf/Properties/Resources.Designer.cs
+++ src/DynamoCoreWpf/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to (Up-to-date).
+        ///   Looks up a localized string similar to (Up to date).
         /// </summary>
         public static string AboutWindowUpToDate {
             get {
@@ -880,9 +880,9 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Uh oh... something went wrong and {0} has crashed, sorry about that.
+        ///   Looks up a localized string similar to Something went wrong and Dynamo has closed unexpectedly.
         ///
-        ///You will get a chance to save your work..
+        ///Don&apos;t worry, you&apos;ll have the option to save your work..
         /// </summary>
         public static string CrashPromptDialogCrashMessage {
             get {
@@ -909,7 +909,7 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Submit Bug To Github.
+        ///   Looks up a localized string similar to Submit bug to GitHub.
         /// </summary>
         public static string CrashPromptDialogSubmitBugButton {
             get {
@@ -918,7 +918,7 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {0} has crashed.
+        ///   Looks up a localized string similar to {0} has closed unexpectedly.
         /// </summary>
         public static string CrashPromptDialogTitle {
             get {
@@ -8209,7 +8209,7 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Agreement to collect usability data on your use of {0}.
+        ///   Looks up a localized string similar to Agreement to Collect Usability Data.
         /// </summary>
         public static string UsageReportPromptDialogTitle {
             get {
