6a97e7352a9fb1032dc7a6e41714ca996e9b50bf
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-07-22 16:10:35 -0300

    DYN-5084  Checking if the ports are empty or null (#13122)
    * Checking if the ports are empty or null
    
    * disconsider the watch node as a function

--- src/DynamoCore/Graph/Nodes/NodeModel.cs
+++ src/DynamoCore/Graph/Nodes/NodeModel.cs
@@ -917,7 +917,12 @@ namespace Dynamo.Graph.Nodes
         [JsonIgnore]
         public bool AreAllOutputsConnected
         {
-            get { return outPorts.All(p => p.IsConnected); }
+            get { 
+                return 
+                    outPorts != null &&
+                    outPorts.Count > 0 &&
+                    outPorts.All(p => p.IsConnected); 
+            }
         }
 
         /// <summary>
@@ -926,7 +931,12 @@ namespace Dynamo.Graph.Nodes
         [JsonIgnore]
         internal bool AreAllInputsDisconnected
         {
-            get { return inPorts.All(p => !p.IsConnected); }
+            get { 
+                return 
+                    inPorts != null &&
+                    inPorts.Count > 0 &&
+                    inPorts.All(p => !p.IsConnected); 
+            }
         }
 
         /// <summary>
