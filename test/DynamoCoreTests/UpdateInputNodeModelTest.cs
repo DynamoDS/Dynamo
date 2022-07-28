b90860edc782ec246e8b267821fe681e8350cec4
Aparajit Pratap <aparajit.pratap@autodesk.com>
2022-07-28 00:00:40 -0400

    cleanup

--- test/DynamoCoreTests/UpdateInputNodeModelTest.cs
+++ test/DynamoCoreTests/UpdateInputNodeModelTest.cs
@@ -26,9 +26,13 @@ namespace Dynamo.Tests
             var guid = "313bc594-8879-492b-aa99-8efc61c5707a";
             AssertPreviewValue(guid, 91);
 
+            // Create backup of original file
+            var originalFile = Path.Combine(TestDirectory, @"core\astbuilder\hardcoded_image_file.jpg");
+            var backupFile = Path.Combine(TestDirectory, @"core\astbuilder\hardcoded_image_file - Copy.jpg");
+            File.Copy(originalFile, backupFile);
+
             // Overwrite the file "harcoded_image_file.jpg" with the file "harcoded_image_file2.jpg"
             var path = Path.Combine(TestDirectory, @"core\astbuilder\hardcoded_image_file2.jpg");
-            var originalFile = Path.Combine(TestDirectory, @"core\astbuilder\hardcoded_image_file.jpg");
             File.Copy(path, originalFile, true);
 
             // Force re-execute the nodemodel node 
@@ -36,9 +40,9 @@ namespace Dynamo.Tests
             var nodeModel = CurrentDynamoModel.CurrentWorkspace.NodeFromWorkspace(guid2);
             nodeModel.OnNodeModified(true);
 
+            File.Delete(originalFile);
             // restore the file, "harcoded_image_file.jpg"
-            var backupFile = Path.Combine(TestDirectory, @"core\astbuilder\hardcoded_image_file - Copy.jpg");
-            File.Copy(backupFile, originalFile, true);
+            File.Move(backupFile, originalFile);
 
             // Assert that the same color value has changed as the file is now pointing to a different image
             AssertPreviewValue(guid, 41);
