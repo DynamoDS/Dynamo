27f906ba7a876bd0babe8414b4c7cb567a75ea4b
Aparajit Pratap <aparajit.pratap@autodesk.com>
2022-07-27 23:40:54 -0400

    add test

---
+++ test/DynamoCoreTests/UpdateInputNodeModelTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NUnit.Framework;
+using DSCore.IO;
+using Dynamo.Models;
+
+namespace Dynamo.Tests
+{
+    [TestFixture]
+    internal class UpdateInputNodeModelTest : DynamoModelTestBase
+    {
+        protected override void GetLibrariesToPreload(List<string> libraries)
+        {
+            libraries.Add("DesignScriptBuiltin.dll");
+            libraries.Add("DSCoreNodes.dll");
+
+            base.GetLibrariesToPreload(libraries);
+        }
+
+        [Test]
+        public void TestUpdateInputNodeModel()
+        {
+            // DYN file contains nodemodel node that reads an image from hardcoded file name and returns a bitmap
+            string openPath = Path.Combine(TestDirectory, @"core\astbuilder\updateInputNodeModel.dyn");
+            RunModel(openPath);
+
+            // Assert on one of the color values belonging to the image read
+            var guid = "313bc594-8879-492b-aa99-8efc61c5707a";
+            AssertPreviewValue(guid, 91);
+
+            // Overwrite the file "harcoded_image_file.jpg" with the file "harcoded_image_file2.jpg"
+            var path = Path.Combine(TestDirectory, @"core\astbuilder\hardcoded_image_file2.jpg");
+            var originalFile = Path.Combine(TestDirectory, @"core\astbuilder\hardcoded_image_file.jpg");
+            File.Copy(path, originalFile, true);
+
+            // Force re-execute the nodemodel node 
+            var guid2 = "a787e45f-f6ed-439f-9eb3-60008b4c8a72";
+            var nodeModel = CurrentDynamoModel.CurrentWorkspace.NodeFromWorkspace(guid2);
+            nodeModel.OnNodeModified(true);
+
+            // restore the file, "harcoded_image_file.jpg"
+            var backupFile = Path.Combine(TestDirectory, @"core\astbuilder\hardcoded_image_file - Copy.jpg");
+            File.Copy(backupFile, originalFile, true);
+
+            // Assert that the same color value has changed as the file is now pointing to a different image
+            AssertPreviewValue(guid, 41);
+        }
+    }
+}
--- test/TestUINodes/TestUINodes.cs
+++ test/TestUINodes/TestUINodes.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Drawing;
 using Autodesk.DesignScript.Runtime;
 using Dynamo.Graph.Nodes;
 using Newtonsoft.Json;
 using ProtoCore.AST.AssociativeAST;
+using DSCore.IO;
 
 namespace TestUINodes
 {
@@ -26,6 +30,67 @@ namespace TestUINodes
         public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
         {
             throw new Exception("Dummy error message.");
+        }
+    }
+
+    [NodeName("Test Selection Node")]
+    [NodeCategory("TestUINodes")]
+    [NodeDescription("A test selection node.")]
+    [OutPortTypes("var")]
+    [IsDesignScriptCompatible]
+    //[IsVisibleInDynamoLibrary(false)]
+    public class TestSelectionNode : NodeModel
+    {
+        public TestSelectionNode()
+        {
+            // Nodes can have an arbitrary number of inputs and outputs.
+            // If you want more ports, just create more PortData objects.
+            OutPorts.Add(new PortModel(PortType.Output, this, new PortData("File", "The selected file.")));
+
+            // This call is required to ensure that your ports are
+            // properly created.
+            RegisterAllPorts();
+
+            // The arugment lacing is the way in which Dynamo handles
+            // inputs of lists. If you don't want your node to
+            // support argument lacing, you can set this to LacingStrategy.Disabled.
+            ArgumentLacing = LacingStrategy.Disabled;
+
+            IsSetAsInput = true;
+        }
+
+        // Starting with Dynamo v2.0 you must add Json constructors for all nodeModel
+        // dervived nodes to support the move from an Xml to Json file format.  Failing to
+        // do so will result in incorrect ports being generated upon serialization/deserialization.
+        // This constructor is called when opening a Json graph.
+        [JsonConstructor]
+        TestSelectionNode(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base(inPorts, outPorts)
+        {
+        }
+
+        /// <param name="inputAstNodes"></param>
+        /// <returns></returns>
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var directory = new DirectoryInfo(executingDirectory);
+            var testDirectory = Path.Combine(directory.Parent.Parent.Parent.Parent.FullName, "test/core/astbuilder");
+
+            string imagePath = Path.Combine(testDirectory, "hardcoded_image_file.jpg");
+            var func1 =
+                AstFactory.BuildFunctionCall(
+                    new Func<string, FileInfo>(FileSystem.FileFromPath),
+                    new List<AssociativeNode> { AstFactory.BuildStringNode(imagePath) });
+
+            var func2 = AstFactory.BuildFunctionCall(
+                new Func<FileInfo, Bitmap>(DSCore.IO.Image.ReadFromFile), new List<AssociativeNode> { func1 });
+
+            // returns an identifier that is assigned to a bitmap
+            return new[]
+            {
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), func2),
+
+            };
         }
     }
 }
