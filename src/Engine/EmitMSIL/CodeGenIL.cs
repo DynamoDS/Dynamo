0ba1baaa17980137a372af806a39050706dfa071
Jorgen Dahl <jorgen.dahl@autodesk.com>
2022-08-05 10:12:07 -0400

    Add Diagnostics

--- src/Engine/EmitMSIL/CodeGenIL.cs
+++ src/Engine/EmitMSIL/CodeGenIL.cs
@@ -5,6 +5,7 @@ using ProtoCore.Utils;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
