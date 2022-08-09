d0a5147e07cae393f1a13cfd418e140998308c50
Ashish Aggarwal <ashish.aggarwal@autodesk.com>
2022-08-09 16:44:53 -0400

    POC

--- src/Notifications/NotificationCenterController.cs
+++ src/Notifications/NotificationCenterController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using Dynamo.Controls;
+using Dynamo.Logging;
 using Dynamo.Notifications.View;
 
 namespace Dynamo.Notifications
@@ -20,6 +22,8 @@ namespace Dynamo.Notifications
 
         private static readonly string htmlEmbeddedFile = "Dynamo.Notifications.node_modules._dynamods.notifications_center.build.index.html";
         private static readonly string jsEmbeddedFile = "Dynamo.Notifications.node_modules._dynamods.notifications_center.build.index.bundle.js";
+
+        private event Action<ILogMessage> MessageLogged;
 
         internal NotificationCenterController(DynamoView dynamoView)
         {
@@ -55,10 +59,14 @@ namespace Dynamo.Notifications
                 htmlString = reader.ReadToEnd();
             }
 
+            //Fetch notification URL from config
+            string notificationURL = FetchNotificationURL();
+
             using (Stream stream = assembly.GetManifestResourceStream(jsEmbeddedFile))
             using (StreamReader reader = new StreamReader(stream))
             {
                 var jsString = reader.ReadToEnd();
+                jsString = jsString.Replace("http://demo9540080.mockable.io/notifications", notificationURL);
                 htmlString = htmlString.Replace("mainJs", jsString);
             }
 
@@ -91,6 +99,35 @@ namespace Dynamo.Notifications
             notificationUIPopup.IsOpen = !notificationUIPopup.IsOpen;
             if (notificationUIPopup.IsOpen)
                 notificationUIPopup.webView.Focus();
-        }        
+        }
+
+        private string FetchNotificationURL()
+        {
+            var path = this.GetType().Assembly.Location;
+            var config = ConfigurationManager.OpenExeConfiguration(path);
+            var key = config.AppSettings.Settings["notificationAddress"];
+            string url = null;
+            if (key != null)
+            {
+                url = key.Value;
+            }
+
+            OnMessageLogged(LogMessage.Info("Dynamo will use the notifications service at : "+ url));
+
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                throw new ArgumentException("Incorrectly formatted URL provided for Notification service.", "url");
+            }
+
+            return url;
+        }
+
+        private void OnMessageLogged(ILogMessage msg)
+        {
+            if (this.MessageLogged != null)
+            {
+                this.MessageLogged(msg);
+            }
+        }
     }
 }
