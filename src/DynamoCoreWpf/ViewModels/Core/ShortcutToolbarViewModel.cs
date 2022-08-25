5ef411de349442fc7518891af2539bf8f9b99e48
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-08-25 14:36:37 -0300

    DYN-5189 update notifications number (#13232)
    * fetching the notifications and counting the number
    
    * setting shortcut viewmodel to internal

---
+++ src/DynamoCoreWpf/ViewModels/Core/ShortcutToolbarViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Dynamo.ViewModels;
+
+namespace Dynamo.Wpf.ViewModels.Core
+{
+    internal class ShortcutToolbarViewModel : ViewModelBase
+    {
+        public ShortcutToolbarViewModel()
+        {
+            NotificationsNumber = 0;
+        }
+
+        /// <summary>
+        /// This property represents the number of new notifications 
+        /// </summary>
+        public int NotificationsNumber { get; set; }
+    }
+}
