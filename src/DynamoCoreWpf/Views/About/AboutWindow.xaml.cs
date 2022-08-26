4e1ad4f1dedec7aaf43c2f3214ddc65de86e0db6
mjkkirschner <mjk.kirschner@gmail.com>
2022-08-25 21:18:06 -0400

    add new webview to about box for showing html add html lic files

--- src/DynamoCoreWpf/Views/About/AboutWindow.xaml.cs
+++ src/DynamoCoreWpf/Views/About/AboutWindow.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using Dynamo.ViewModels;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
 
 namespace Dynamo.UI.Views
 {
@@ -16,6 +19,9 @@ namespace Dynamo.UI.Views
     public partial class AboutWindow : Window
     {
         bool ignoreClose;
+        private WebView2 htmlLicenseView;
+        static readonly string HTML_IMAGE_PATH_PREFIX = @"http://";
+        private const string ABOUT_BLANK_URI = "about:blank";
 
         public AboutWindow(DynamoViewModel dynamoViewModel)
         {
@@ -27,6 +33,14 @@ namespace Dynamo.UI.Views
             TitleTextBlock.Text = string.Format(Dynamo.Wpf.Properties.Resources.AboutWindowTitle, dynamoViewModel.BrandingResourceProvider.ProductName);
             Title = string.Format(Dynamo.Wpf.Properties.Resources.AboutWindowTitle, dynamoViewModel.BrandingResourceProvider.ProductName);
             DynamoWebsiteButton.Content = string.Format(Dynamo.Wpf.Properties.Resources.AboutWindowDynamoWebsiteButton, dynamoViewModel.BrandingResourceProvider.ProductName);
+
+            this.Loaded += AboutWindow_Loaded;
+
+        }
+
+        private void AboutWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadHTMLAboutBoxContent();
         }
 
         public bool InstallNewUpdate { get; private set; }
@@ -35,6 +49,130 @@ namespace Dynamo.UI.Views
         {
             if (e.Key == Key.Escape)
                 Close();
+        }
+
+        private void LoadHTMLAboutBoxContent()
+        {
+            string executingAssemblyPathName = Assembly.GetExecutingAssembly().Location;
+            string rootModuleDirectory = Path.GetDirectoryName(executingAssemblyPathName);
+            var htmlLicensePath = Path.Combine(rootModuleDirectory, "third_party_licenses_html");
+            var files = Directory.GetFiles(htmlLicensePath, "*.html");
+            var head = @"<!DOCTYPE html>
+<html lang=""en"">
+<head>
+<meta charset=""utf-8"">
+<title>title</title>
+</head>
+<style>
+/* total width */
+body::-webkit-scrollbar {
+    background-color: #fff;
+    width: 16px;
+}
+
+/* background of the scrollbar except button or resizer */
+body::-webkit-scrollbar-track {
+    background-color: #fff;
+}
+
+/* scrollbar itself */
+body::-webkit-scrollbar-thumb {
+    background-color: #babac0;
+    border-radius: 16px;
+    border: 4px solid #fff;
+}
+
+/* set button(top and bottom of the scrollbar) */
+body::-webkit-scrollbar-button {
+    display:none;
+}
+</style>
+<body style=""padding-left:15px; background-color: #F0F0F0; font-family: 'Artifakt Element', 'Open Sans';font-size: small; "">
+";
+            var footer = @"
+</body>
+</html>";
+
+            var finalHtml = head;
+
+             foreach (var file in files)
+             {
+                 var licContent = File.ReadAllText(file);
+                //TODO range check.
+                var licName = new FileInfo(file).Name.Split('_')[0];
+                var licWrapped = $@"<details> <summary>{licName}</summary> {licContent} </details>";
+                finalHtml += licWrapped;
+            }
+            finalHtml += footer;
+            htmlLicenseView = ((browserparent as Border)?.Child as WebView2);
+            if(htmlLicenseView != null)
+            {
+                InitializeAsync(htmlLicenseView, finalHtml);
+            }
+        }
+
+        async void InitializeAsync(WebView2 browser, string htmlContent)
+        {
+            //Initialize the CoreWebView2 component otherwise we can't navigate to a web page
+            await browser.EnsureCoreWebView2Async();
+
+            // Context menu disabled
+            browser.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            browser.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+            browser.CoreWebView2.NavigationStarting += ShouldAllowNavigation;
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                browser.NavigateToString(htmlContent);
+            }));
+        }
+
+        private void CoreWebView2_NewWindowRequested(object sender, global::Microsoft.Web.WebView2.Core.CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            try
+            {
+                Process.Start(e.Uri);
+            }
+            catch(Exception ex)
+            {
+                if (DataContext is DynamoViewModel dvm)
+                {
+                    dvm.Model.Logger.Log(ex);
+                }
+            }
+            e.Handled = true;
+        }
+
+        //TODO refactor extensions to share this.
+        /// <summary>
+        /// Redirect the user to the browser if they press a link in the browser.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShouldAllowNavigation(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            // if is not an URL then we should return otherwise it will crash when trying to open the URL in the default Web Browser
+            if (!e.Uri.StartsWith(HTML_IMAGE_PATH_PREFIX.Substring(0, 4)))
+            {
+                return;
+            }
+
+            // we never set the uri if navigating to a local document, so safe to navigate
+            if (e.Uri == null)
+                return;
+
+            // we want to cancel navigation when a clicked link would navigate 
+            // away from the page the ViewModel wants to display
+            var isAboutBlankLink = e.Uri.ToString().Equals(ABOUT_BLANK_URI);
+            var isRemoteLinkFromLocalDocument = e.Uri.ToLower().StartsWith("@https://") || e.Uri.ToLower().StartsWith(HTML_IMAGE_PATH_PREFIX);
+
+            if (isAboutBlankLink || isRemoteLinkFromLocalDocument)
+            {
+                // in either of these two cases, cancel the navigation 
+                // and redirect it to a new process that starts the default OS browser
+                e.Cancel = true;
+                Process.Start(new ProcessStartInfo(e.Uri));
+            }
         }
 
         private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
@@ -77,6 +215,7 @@ namespace Dynamo.UI.Views
         /// <param name="toggle"></param>
         private void ToggleButtons(bool toggle)
         {
+            
             if (toggle)
             {
                 this.MaximizeButton.Visibility = Visibility.Collapsed;
@@ -87,6 +226,7 @@ namespace Dynamo.UI.Views
                 this.MaximizeButton.Visibility = Visibility.Visible;
                 this.NormalizeButton.Visibility = Visibility.Collapsed;
             }
+            
         }
 
         /// <summary>
@@ -103,6 +243,9 @@ namespace Dynamo.UI.Views
         // ESC Button pressed triggers Window close        
         private void OnCloseExecuted(object sender, ExecutedRoutedEventArgs e)
         {
+            htmlLicenseView.CoreWebView2.NewWindowRequested -= CoreWebView2_NewWindowRequested;
+            htmlLicenseView.CoreWebView2.NavigationStarting -= ShouldAllowNavigation;
+            htmlLicenseView.Dispose();
             this.Close();
         }
     }
