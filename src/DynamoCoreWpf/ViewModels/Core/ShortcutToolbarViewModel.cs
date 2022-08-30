3056f4af71fd7f5ba582d99c565fce9d613ff227
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-08-30 12:29:47 -0300

    DYN-5190 mark all as read (#13260)
    * mark all as read function
    
    * mark all as read functionallity
    
    * remove local settings
    
    * Addressing comments
    
    * updating the pointer position and add a margin to the notifcations button
    
    * Limit of months to filter the notifications

--- src/DynamoCoreWpf/ViewModels/Core/ShortcutToolbarViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Core/ShortcutToolbarViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Dynamo.ViewModels;
+using System.Windows;
 
 namespace Dynamo.Wpf.ViewModels.Core
 {
@@ -9,9 +10,31 @@ namespace Dynamo.Wpf.ViewModels.Core
             NotificationsNumber = 0;
         }
 
+        private int notificationsNumber;
+
         /// <summary>
         /// This property represents the number of new notifications 
         /// </summary>
-        public int NotificationsNumber { get; set; }
+        public int NotificationsNumber { 
+            get {
+                return notificationsNumber;
+            } 
+            set {
+                notificationsNumber = value;
+                RaisePropertyChanged(nameof(IsNotificationsCounterVisible));
+            }
+        }
+
+        public bool IsNotificationsCounterVisible
+        { 
+            get 
+            {
+                if (NotificationsNumber == 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
     }
 }
