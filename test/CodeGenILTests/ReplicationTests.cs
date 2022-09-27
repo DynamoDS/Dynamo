69f5e8a4f454d182c2888b413d2800b284730775
kirschm <michael.kirschner@autodesk.com>
2022-09-26 20:00:40 -0400

    add more failing tests

--- test/CodeGenILTests/ReplicationTests.cs
+++ test/CodeGenILTests/ReplicationTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using ProtoCore.Utils;
 using System;
 using System.Collections.Generic;
@@ -9,7 +9,7 @@ namespace CodeGenILTests
     class ReplicationTests : MicroTests
     {
         [Test]
-        public void MSIL_Arithmatic_List_And_List_Same_Length()
+        public void MSIL_Arithmetic_List_And_List_Same_Length()
         {
             string dscode = @"
 import(""DesignScriptBuiltin.dll"");
@@ -28,7 +28,66 @@ list3 = DSCore.Math.Max([ 1, 4, 7, 2], [ 5, 8, 3, 6 ]);
         }
 
         [Test]
-        public void MSIL_Arithmatic_No_Replication()
+        public void ZTLongestLacing_ShouldReturn4Results()
+        {
+            string dscode = @"
+import(""DesignScriptBuiltin.dll"");
+import(""DSCoreNodes.dll"");
+list3 = DSCore.Math.Max(10, [ 5, 8, 3, 6 ]<1L>);
+";
+            var ast = ParserUtils.Parse(dscode).Body;
+            var output = codeGen.EmitAndExecute(ast);
+            Assert.IsNotEmpty(output);
+
+            Assert.IsTrue(output.ContainsKey("list3"));
+
+            var expectedResult = new int[] { 10,10,10,10};
+            var result = output["list3"];
+            Assert.AreEqual(expectedResult, result);
+        }
+        //TODO this fails, it has different behavior than in dynamo master in a codeblock. 
+        [Test]
+        [Category("Failure")]
+        public void ZTLongestLacing_ShouldReturn4NestedResults()
+        {
+            string dscode = @"
+import(""DesignScriptBuiltin.dll"");
+import(""DSCoreNodes.dll"");
+list3 = DSCore.Math.Max([10], [ 5, 8, 3, 6 ]<1L>);
+";
+            var ast = ParserUtils.Parse(dscode).Body;
+            var output = codeGen.EmitAndExecute(ast);
+            Assert.IsNotEmpty(output);
+
+            Assert.IsTrue(output.ContainsKey("list3"));
+
+            var expectedResult = new object[] { new[] { 10 }, new[] { 10 }, new[] { 10 }, new[] { 10 } };
+            var result = output["list3"];
+            Assert.AreEqual(expectedResult, result);
+        }
+        //TODO this fails, it has different behavior than in dynamo master in a codeblock. 
+        [Test]
+        [Category("Failure")]
+        public void ZTShortestLacing_ShouldReturnSingleItem_AndBeDefault()
+        {
+            string dscode = @"
+import(""DesignScriptBuiltin.dll"");
+import(""DSCoreNodes.dll"");
+list3 = DSCore.Math.Max([10], [ 5, 8, 3, 6 ]);
+";
+            var ast = ParserUtils.Parse(dscode).Body;
+            var output = codeGen.EmitAndExecute(ast);
+            Assert.IsNotEmpty(output);
+
+            Assert.IsTrue(output.ContainsKey("list3"));
+
+            var expectedResult = new int[] { 10 };
+            var result = output["list3"];
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void MSIL_Arithmetic_No_Replication()
         {
             string dscode = @"
 import(""DesignScriptBuiltin.dll"");
