0ee6320b601fc8bf1976063b2db22dcf4855f6b2
t-tellro <roberto.tellez@globant.com>
2022-10-21 16:17:29 -0500

    UpdateLibrarieMinJs
    Fixed spelling and using TrimStart method.

--- src/LibraryViewExtensionWebView2/LibraryViewController.cs
+++ src/LibraryViewExtensionWebView2/LibraryViewController.cs
@@ -171,7 +171,7 @@ namespace Dynamo.LibraryViewExtensionWebView2
         {
             var ext = string.Empty;
             var magicstringprod = "__webpack_require__.p+";
-            var minfiedURLHtmlReplacement = minifiedURL;
+            var minifiedURLHtmlReplacement = minifiedURL;
             // this depends on librariejs minification producing the same results - 
             // longterm this is fragile. We should intercept these requests and handle them instead.
             if (magicreplace)
@@ -199,7 +199,7 @@ namespace Dynamo.LibraryViewExtensionWebView2
             }
 
             //In Libraryjs project, Webpack5 is removing the initial slash "/" when using resource files so for example in the html we have the string like "/resources/image.svg" then we need to remove the first slash (the first char) otherwise it won't be found and replaced by the base64 content.
-            html = html.Replace(minifiedURL.Replace(minfiedURLHtmlReplacement, minfiedURLHtmlReplacement.Remove(0, 1)), '"' + base64 + '"');
+            html = html.Replace(minifiedURL.Replace(minifiedURLHtmlReplacement, minifiedURLHtmlReplacement.TrimStart('/')), '"' + base64 + '"');
             return html;
         }
 
