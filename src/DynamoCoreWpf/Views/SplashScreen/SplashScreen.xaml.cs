366f63fac02b68f99b9c44372f715d7040b88be7
t-tellro <roberto.tellez@globant.com>
2022-11-22 12:32:14 -0600

    DYN-5388-SplashScreen-Revit
    When Dynamo is opened from Revit it was crashing due that pplication.Current is null, so I removed the line, we already validated that the MainWindow is already set when reaching this line, then is not needed.

--- src/DynamoCoreWpf/Views/SplashScreen/SplashScreen.xaml.cs
+++ src/DynamoCoreWpf/Views/SplashScreen/SplashScreen.xaml.cs
@@ -190,7 +190,6 @@ namespace Dynamo.UI.Views
             DynamoModel.RequestUpdateLoadBarStatus -= DynamoModel_RequestUpdateLoadBarStatus;
             StaticSplashScreenReady -= OnStaticScreenReady;
             Close();
-            Application.Current.MainWindow = dynamoView;
             dynamoView.Show();
             dynamoView.Activate();
         }
