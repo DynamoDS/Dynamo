736686bbbbec013f0cf8147902c4e8edce60d470
Deyan Nenov <dnenov@archilizer.com>
2023-01-04 13:11:33 +0000

    Embedded Artifakt Element as base64
    - to ensure the DocumentationBrowser always loads with the correct font, the Artifakt Element font is now included in the css style sheet as base64

--- src/Tools/Md2Html/Md2HtmlSanitizer.cs
+++ src/Tools/Md2Html/Md2HtmlSanitizer.cs
@@ -54,6 +54,8 @@ namespace Md2Html
             AllowedAtRules.Add(CssRuleType.Media);
             AllowedAtRules.Add(CssRuleType.Keyframe);
             AllowedAtRules.Add(CssRuleType.Keyframes);
+            AllowedAtRules.Add(CssRuleType.FontFace);
+            AllowedAtRules.Add(CssRuleType.FontFeatureValues);
 
             RemovingAtRule += ChangedEvent;
             RemovingAttribute += ChangedEvent;
