752979a258895b5e0b803dd04389f6f55e491c3a
Jorgen Dahl <jorgen.dahl@autodesk.com>
2023-01-05 11:16:29 -0500

    Add hard coded graphs

--- test/DynamoCoreTests/Performance/PerformanceTests.cs
+++ test/DynamoCoreTests/Performance/PerformanceTests.cs
@@ -1,17 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+using Autodesk.DesignScript.Geometry;
+using DocumentFormat.OpenXml.Presentation;
 using Dynamo.Graph.Nodes;
 using Dynamo.Graph.Workspaces;
+using Modifiers;
 using NUnit.Framework;
+using SharpDX;
+using Color = DSCore.Color;
+using Point = Autodesk.DesignScript.Geometry.Point;
+using TimeSpan = System.TimeSpan;
 
 namespace Dynamo.Tests
 {
     [TestFixture, Category("Performance")]
     public class PerformanceTests : DynamoModelTestBase
     {
-        private List<(string graph, TimeSpan oldEngineCompileTime, TimeSpan oldEngineExecutionTime, TimeSpan newEngineCompileTime, TimeSpan newEngineExecutionTime)> executionData;
+        private List<(string graph, TimeSpan oldEngineCompileTime, TimeSpan oldEngineExecutionTime, TimeSpan newEngineCompileTime, TimeSpan newEngineExecutionTime, TimeSpan hardcodedExecutionTime)> executionData;
+        private bool warmupDone;
 
         protected override void GetLibrariesToPreload(List<string> libraries)
         {
@@ -39,23 +50,38 @@ namespace Dynamo.Tests
         [TestFixtureSetUp]
         public void SetupPerformanceTests()
         {
-            executionData = new List<(string, TimeSpan, TimeSpan, TimeSpan, TimeSpan)>();
+            executionData = new List<(string, TimeSpan, TimeSpan, TimeSpan, TimeSpan, TimeSpan)>();
+
         }
 
         [TestFixtureTearDown]
         public void TeardownPerformanceTests()
         {
-            Console.WriteLine("{0,50}{1,9}{2,9}{3,11}{4,9}{5,9}{6,11}", "Graph", "Old C", "Old E", "Old C+E", "New C", "New E", "New C+E");
+            Console.WriteLine("{0,50}{1,9}{2,9}{3,11}{4,9}{5,9}{6,11}{7,9}", "Graph", "Old C", "Old E", "Old C+E", "New C", "New E", "New C+E", "HC");
             executionData.ForEach(item =>
             {
-                var (graph, oldEngineCompileTime, oldEngineExecutionTime, newEngineCompileTime, newEngineExecutionTime) = item;
-                Console.WriteLine("{0,50}{1,9:0.0}{2,9:0.0}{3,11:0.0}{4,9:0.0}{5,9:0.0}{6,11:0.0}", graph,
-                    oldEngineCompileTime.TotalMilliseconds, oldEngineExecutionTime.TotalMilliseconds,
-                    oldEngineCompileTime.TotalMilliseconds + oldEngineExecutionTime.TotalMilliseconds,
-                    newEngineCompileTime.TotalMilliseconds, newEngineExecutionTime.TotalMilliseconds,
-                    newEngineCompileTime.TotalMilliseconds + newEngineExecutionTime.TotalMilliseconds);
+                var (graph, oldEngineCompileTime, oldEngineExecutionTime, newEngineCompileTime, newEngineExecutionTime, hardcodedExecutionTime) = item;
+                if (hardcodedExecutionTime == TimeSpan.Zero)
+                {
+                    Console.WriteLine("{0,50}{1,9:0.0}{2,9:0.0}{3,11:0.0}{4,9:0.0}{5,9:0.0}{6,11:0.0}", graph,
+                        oldEngineCompileTime.TotalMilliseconds, oldEngineExecutionTime.TotalMilliseconds,
+                        oldEngineCompileTime.TotalMilliseconds + oldEngineExecutionTime.TotalMilliseconds,
+                        newEngineCompileTime.TotalMilliseconds, newEngineExecutionTime.TotalMilliseconds,
+                        newEngineCompileTime.TotalMilliseconds + newEngineExecutionTime.TotalMilliseconds);
+                }
+                else
+                {
+                    Console.WriteLine("{0,50}{1,9:0.0}{2,9:0.0}{3,11:0.0}{4,9:0.0}{5,9:0.0}{6,11:0.0}{7,9:0.0}", graph,
+                        oldEngineCompileTime.TotalMilliseconds, oldEngineExecutionTime.TotalMilliseconds,
+                        oldEngineCompileTime.TotalMilliseconds + oldEngineExecutionTime.TotalMilliseconds,
+                        newEngineCompileTime.TotalMilliseconds, newEngineExecutionTime.TotalMilliseconds,
+                        newEngineCompileTime.TotalMilliseconds + newEngineExecutionTime.TotalMilliseconds,
+                        hardcodedExecutionTime.TotalMilliseconds);
+                }
             });
             executionData.Clear();
+
+            warmupDone = false;
         }
 
         [Test, TestCaseSource("FindWorkspaces"), Category("Performance")]
@@ -66,6 +92,11 @@ namespace Dynamo.Tests
 
         private void DoWorkspaceOpenAndCompare(string filePath)
         {
+
+            WarmUp();
+
+            var hardCodedExecutionTime = RunHardcodedTest(filePath);
+
             OpenModel(filePath);
 
             var model = CurrentDynamoModel;
@@ -117,13 +148,77 @@ namespace Dynamo.Tests
 
             var newEngineCompileAndExecutionTime = model.EngineController.CompileAndExecutionTime;
 
-            Console.WriteLine("Compile and Execution time old Engine={0:0.0}+{1:0.0} ms, new Engine={2:0.0}+{3:0.0} ms",
+            Console.WriteLine("Compile and Execution time old Engine={0:0.0}+{1:0.0} ms, new Engine={2:0.0}+{3:0.0} ms, hardCoded={4:0.0}",
                 oldEngineCompileAndExecutionTime.compileTime.TotalMilliseconds, oldEngineCompileAndExecutionTime.executionTime.TotalMilliseconds,
-                newEngineCompileAndExecutionTime.compileTime.TotalMilliseconds, newEngineCompileAndExecutionTime.executionTime.TotalMilliseconds);
+                newEngineCompileAndExecutionTime.compileTime.TotalMilliseconds, newEngineCompileAndExecutionTime.executionTime.TotalMilliseconds,
+                hardCodedExecutionTime.TotalMilliseconds);
             var execution = (Path.GetFileName(filePath),
                 oldEngineCompileAndExecutionTime.compileTime, oldEngineCompileAndExecutionTime.executionTime,
-                newEngineCompileAndExecutionTime.compileTime, newEngineCompileAndExecutionTime.executionTime);
+                newEngineCompileAndExecutionTime.compileTime, newEngineCompileAndExecutionTime.executionTime,
+                hardCodedExecutionTime);
             executionData.Add(execution);
+        }
+
+        private void WarmUp()
+        {
+            if (!warmupDone)
+            {
+                var warmupFile = Path.Combine(TestDirectory, "core", "performance", "simple_point.dyn");
+                OpenModel(warmupFile);
+
+                Cleanup();
+
+                Setup();
+
+                OpenModel(warmupFile, dsExecution: false);
+
+                Cleanup();
+
+                Setup();
+
+                warmupDone = true;
+            }
+        }
+
+        private TimeSpan RunHardcodedTest(string fileName)
+        {
+            TimeSpan hardCodedExecutionTime = TimeSpan.Zero;
+            var basename = Path.GetFileNameWithoutExtension(fileName);
+            var cleanUp = false;
+
+            if (string.Compare(basename, "lotsofcoloredstuff-simplified", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                var timer = new Stopwatch();
+                timer.Start();
+
+                LotsOfColoredStuffSimplified();
+
+                timer.Stop();
+                hardCodedExecutionTime = timer.Elapsed;
+
+                cleanUp = true;
+            }
+
+            if (string.Compare(basename, "cross_product_lacing_arrays", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                var timer = new Stopwatch();
+                timer.Start();
+
+                CrossProductLacingArrays();
+
+                timer.Stop();
+                hardCodedExecutionTime = timer.Elapsed;
+
+                cleanUp = true;
+            }
+
+            if (cleanUp)
+            {
+                Cleanup();
+                Setup();
+            }
+
+            return hardCodedExecutionTime;
         }
 
         private void CheckForDummyNodes(WorkspaceModel ws)
@@ -133,6 +228,108 @@ namespace Dynamo.Tests
             {
                 Assert.Inconclusive("The Workspace contains dummy nodes for: " + string.Join(",", dummyNodes.Select(n => n.Name).ToArray()));
             }
+        }
+
+        private void LotsOfColoredStuffSimplified()
+        {
+            var seeds = Enumerable.Range(0, 26).Select(x => x * 2).ToList();
+
+            var points = seeds.Select(x => seeds.Select(y => seeds.Select(z => Point.ByCoordinates(x, y, z)).ToList()).ToList()).ToList();
+
+            var cuboids = points.Select(a => a.Select(b => b.Select(p => Cuboid.ByLengths(p)).ToList()).ToList()).ToList();
+
+            var circles = points.Select(a => a.Select(b => b.Select(p => Circle.ByCenterPointRadius(p, 0.7)).ToList()).ToList()).ToList();
+
+            var color1 = Color.ByARGB(255, 75);
+
+            var spheres = points.Select(a => a.Select(b => b.Select(p => Sphere.ByCenterPointRadius(p, 0.7)).ToList()).ToList()).ToList();
+
+            var color2 = Color.ByARGB(255, 0, 255, 75);
+
+            var color3 = Color.ByARGB(255, 255, 0, 255);
+
+            var edges = cuboids.Select(a => a.Select(b => b.Select(c => c.Edges)).ToList()).ToList();
+
+            var curves = edges.Select(a => a.Select(b => b.Select(l => l.Select(e => e.CurveGeometry).ToList()).ToList()).ToList()).ToList();
+
+            var color4 = Color.ByARGB(255, 255, 255);
+
+            var geometrycolorcircles = circles.Select(a => a.Select(b => b.Select(c => Modifiers.GeometryColor.ByGeometryColor(c, color1)).ToList()).ToList()).ToList();
+
+            var geometrycolorspheres = spheres.Select(a => a.Select(b => b.Select(s => Modifiers.GeometryColor.ByGeometryColor(s, color2)).ToList()).ToList()).ToList();
+
+            var geometrycolorcuboids = cuboids.Select(a => a.Select(b => b.Select(c => Modifiers.GeometryColor.ByGeometryColor(c, color3)).ToList()).ToList()).ToList();
+
+            var geometrycolorcurves = curves.Select(a => a.Select(b => b.Select(c => c.Select(e => Modifiers.GeometryColor.ByGeometryColor(e, color4)).ToList()).ToList()).ToList()).ToList();
+
+            var geometrycolorcirclescount = geometrycolorcircles.Select(a => a.Select(b => b.Count).Sum()).Sum();
+
+            var geometrycolorspherescount = geometrycolorspheres.Select(a => a.Select(b => b.Count).Sum()).Sum();
+
+            var geometrycolorcuboidscount = geometrycolorcuboids.Select(a => a.Select(b => b.Count).Sum()).Sum();
+
+            var geometrycolorcurvescount = geometrycolorcurves.Select(a => a.Select(b => b.Select(c => c.Count).Sum()).Sum()).Sum();
+
+            Assert.AreEqual(17576, geometrycolorcirclescount);
+            Assert.AreEqual(17576, geometrycolorspherescount);
+            Assert.AreEqual(17576, geometrycolorcuboidscount);
+            Assert.AreEqual(210912, geometrycolorcurvescount);
+        }
+
+        private void CrossProductLacingArrays()
+        {
+            var list1 = Enumerable.Range(5, 5).ToList();
+            var list2 = Enumerable.Range(0, 130001).ToList();
+            var list3 = Enumerable.Range(0, 551).ToList();
+
+            var listoflists1 = list2.Select((x, i) => new { Value = x, Index = i }).GroupBy(x => x.Index / 5).Select(x => x.Select(v => v.Value).ToList()).ToList();
+
+
+            var indexOf1 = list1.Select(a => listoflists1.Select(b => b.IndexOf(a)).ToList()).ToList();
+            var indexof1Count = indexOf1.Select(a => a.Count).Sum();
+            Assert.AreEqual(130005, indexof1Count);
+            Assert.AreEqual(5, indexOf1.Count);
+            Assert.AreEqual(0, indexOf1[0][1]);
+            Assert.AreEqual(1, indexOf1[1][1]);
+            Assert.AreEqual(2, indexOf1[2][1]);
+            Assert.AreEqual(3, indexOf1[3][1]);
+            Assert.AreEqual(4, indexOf1[4][1]);
+
+
+            var indexOf2 = list1.Select(a => listoflists1.Select(b => b.IndexOf(a)).ToList()).ToList();
+            var indexof2Count = indexOf2.Select(a => a.Count).Sum();
+            Assert.AreEqual(130005, indexof2Count);
+            Assert.AreEqual(5, indexOf2.Count);
+            Assert.AreEqual(0, indexOf2[0][1]);
+            Assert.AreEqual(1, indexOf2[1][1]);
+            Assert.AreEqual(2, indexOf2[2][1]);
+            Assert.AreEqual(3, indexOf2[3][1]);
+            Assert.AreEqual(4, indexOf2[4][1]);
+
+            var plus = list3.Select(a => list3.Select(b => a + b).ToList()).ToList();
+            var pluscount = plus.Select(a => a.Count).Sum();
+            Assert.AreEqual(303601, pluscount);
+
+            var sum = list3.Select(a => a).ToList();
+            Assert.AreEqual(551, sum.Count);
+
+            var max = list3.Select(a => list3.Select(b => Math.Max(a, b)).ToList()).ToList();
+            var maxcount = plus.Select(a => a.Count).Sum();
+            Assert.AreEqual(303601, maxcount);
+
+            var listOfList2 = list3.Select((x, i) => new { Value = x, Index = i }).GroupBy(x => x.Index / 5).Select(x => x.Select(v => v.Value).ToList()).ToList();
+
+            var plus2 = listOfList2.Select(a => listOfList2.Select(b => a.Zip(b, (x,y) => x + y).ToList()).ToList().ToList()).ToList();
+            var plus2count = plus2.Select(a => a.Select(b => b.Count).Sum()).Sum();
+            Assert.AreEqual(60721, plus2count);
+
+            var sum2 = listOfList2.Select(a => a.Sum()).ToList();
+            var sum2count = sum2.Count;
+            Assert.AreEqual(111, sum2count);
+
+            var max2 = listOfList2.Select(a => listOfList2.Select(b => a.Zip(b, Math.Max).ToList()).ToList().ToList()).ToList();
+            var max2count = max2.Select(a => a.Select(b => b.Count).Sum()).Sum();
+            Assert.AreEqual(60721, max2count);
         }
     }
 }
