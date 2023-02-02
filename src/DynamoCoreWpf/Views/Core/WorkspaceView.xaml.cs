35005c356fc06fbb392708d71c8bb5cae0495f55
Roberto T <61755417+RobertGlobant20@users.noreply.github.com>
2023-02-02 10:48:28 -0600

    Dyn 4964 popup geo scaling bugs (#13717)
    * DYN-4964-PopupGeoScaling-Bugs
    
    Fixing the next bugs reported by Aabi:
    - The Popup was not closing when clicking in the Dynamo workspace (adding a similar fix like the Workspace ContextMenu).
    - Adding a shadow over the new Geometry Scaling popup icon.
    
    * DYN-4964-WorkingRange-Popup Bug fixes
    
    There was a missing case that when the user click a Dynamo menu the Popup was not closing then I added some extra code to close it.
    
    * DYN-4964-WorkingRange-Popup Bug Code Review1
    
    Removing extra trailing spaces.

--- src/DynamoCoreWpf/Views/Core/WorkspaceView.xaml.cs
+++ src/DynamoCoreWpf/Views/Core/WorkspaceView.xaml.cs
@@ -20,6 +20,7 @@ using Dynamo.Models;
 using Dynamo.Search.SearchElements;
 using Dynamo.Selection;
 using Dynamo.UI;
+using Dynamo.UI.Controls;
 using Dynamo.Utilities;
 using Dynamo.ViewModels;
 using Dynamo.Wpf.UI;
@@ -196,6 +197,12 @@ namespace Dynamo.Views
             ShowHidePopup(flag, ContextMenuPopup);
         }
 
+        private void ShowHideGeoScalingPopup(ShowHideFlags flag)
+        {
+            if (GeoScalingPopup != null)
+                ShowHidePopup(flag, GeoScalingPopup);
+        }
+
         private void ShowHidePopup(ShowHideFlags flag, Popup popup)
         {
             // Reset popup display state
@@ -267,6 +274,13 @@ namespace Dynamo.Views
             {
                 ShowHideContextMenu(ShowHideFlags.Hide);
                 ShowHideInCanvasControl(ShowHideFlags.Hide);
+            }
+
+            var imageButtonGeoScalingPopup = Mouse.DirectlyOver as ImageRepeatButton;
+            //When imageButtonGeoScalingPopup is null means that the user is clicking the Geometry Scaling button so we should not close the Popup
+            if (imageButtonGeoScalingPopup == null && GeoScalingPopup != null && GeoScalingPopup.IsOpen)
+            {
+                ShowHideGeoScalingPopup(ShowHideFlags.Hide);
             }
             // If triggered on node level, make sure node popups are also hidden
             if(sender is NodeView && (PortContextMenu.IsOpen || NodeAutoCompleteSearchBar.IsOpen) )
@@ -712,6 +726,8 @@ namespace Dynamo.Views
         {
             ContextMenuPopup.IsOpen = false;
             InCanvasSearchBar.IsOpen = false;
+            if (GeoScalingPopup != null)
+                GeoScalingPopup.IsOpen = false;
             
             if(PortContextMenu.IsOpen) DestroyPortContextMenu();
         }
@@ -734,6 +750,8 @@ namespace Dynamo.Views
 
             ViewModel.HandleMouseRelease(workBench, e);
             ContextMenuPopup.IsOpen = false;
+            if (GeoScalingPopup != null)
+                GeoScalingPopup.IsOpen = false;
             if (returnToSearch)
             {
                 ViewModel.DynamoViewModel.CurrentSpaceViewModel.InCanvasSearchViewModel.OnRequestFocusSearch();
