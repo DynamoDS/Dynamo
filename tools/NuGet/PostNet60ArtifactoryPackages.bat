cc08b3a8056bbb88753515614e8d6df25f001aad
mjkkirschner <mjk.kirschner@gmail.com>
2023-02-15 15:52:37 -0500

    remove publish script

--- tools/NuGet/PostNet60ArtifactoryPackages.bat
+++
@@ -1,3 +0,0 @@
-:: Use credentials in config file for pushing package to Artifactory
-set configPath=%~dp0..\..\dynamo-nuget.config
-nuget push *.nupkg -source team-dynamo-nuget -configfile %configPath%
