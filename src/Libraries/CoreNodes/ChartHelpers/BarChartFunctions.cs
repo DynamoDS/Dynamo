7b740f5c8113f97bc52ce3ce5c0697f7d8eb9926
Deyan Nenov <dnenov@archilizer.com>
2023-03-10 18:13:29 +0000

    Default Values Part 1
    - wip for default values

--- src/Libraries/CoreNodes/ChartHelpers/BarChartFunctions.cs
+++ src/Libraries/CoreNodes/ChartHelpers/BarChartFunctions.cs
@@ -9,12 +9,31 @@ namespace CoreNodes.ChartHelpers
 {
     public class BarChartFunctions
     {
-        private BarChartFunctions() { }
+        private static List<string> defaultLabels = new List<string> { "2019", "2020", "2021" };
 
+        private static List<double> defaultValues = new List<double> { 5.0, 6.0, 7.0, 8.0 };
+
+        private static List<List<double>> defaultNestedValues = new List<List<double>> {
+            new List<double> { 5.0, 6.0, 7.0, 8.0 },
+            new List<double> { 10.0, 12.0, 14.0, 16.0 },
+            new List<double> { 15.0, 18.0, 12.0, 24.0 } };
+
+        private BarChartFunctions() { }
+            
         [IsVisibleInDynamoLibrary(false)]
         public static Dictionary<string, List<double>> GetNodeInput(List<string> labels, List<List<double>> values, List<DSCore.Color> colors)
         {
             var output = new Dictionary<string, List<double>>();
+
+            if (labels == null && values == null)
+            {
+                for (var i = 0; i < defaultLabels.Count; i++)
+                {
+                    output.Add(defaultLabels[i], defaultNestedValues[i]);
+                }
+
+                return output;
+            }
 
             if (labels.Count != values.Count)
             {
@@ -33,6 +52,16 @@ namespace CoreNodes.ChartHelpers
         public static Dictionary<string, double> GetNodeInput(List<string> labels, List<double> values, List<DSCore.Color> colors)
         {
             var output = new Dictionary<string, double>();
+
+            if (labels == null && values == null)
+            {
+                for (var i = 0; i < defaultLabels.Count; i++)
+                {
+                    output.Add(defaultLabels[i], defaultValues[i]);
+                }
+
+                return output;
+            }
 
             if (labels.Count != values.Count)
             {
--- src/Libraries/CoreNodes/ChartHelpers/HeatSeriesFunctions.cs
+++ src/Libraries/CoreNodes/ChartHelpers/HeatSeriesFunctions.cs
@@ -9,21 +9,61 @@ namespace CoreNodes.ChartHelpers
 {
     public class HeatSeriesFunctions
     {
+        private static List<string> defaultLabelsX = new List<string> { "Item-1", "Item-2", "Item-3", "Item-4", "Item-5" };
+        private static List<string> defaultLabelsY = new List<string>
+        {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        };
+
+        private static List<List<double>> defaultNestedValues = new List<List<double>> {
+            new List<double> { 5, 6, 6, 7, 8, 9, 3 },
+            new List<double> { 5, 6, 6, 7, 8, 9, 3 },
+            new List<double> { 5, 6, 6, 7, 8, 9, 3 },
+            new List<double> { 5, 6, 6, 7, 8, 9, 3 },
+            new List<double> { 5, 6, 6, 7, 8, 9, 3 } };
+
         private HeatSeriesFunctions() { }
 
         [IsVisibleInDynamoLibrary(false)]
-        public static Dictionary<string, List<double>> GetNodeInput(List<string> xLabels, List<string> yLabels, List<List<double>> values, List<DSCore.Color> colors)
+        public static Dictionary<Dictionary<string, string>, List<double>> GetNodeInput(List<string> xLabels, List<string> yLabels, List<List<double>> values, List<DSCore.Color> colors)
         {
+            var output = new Dictionary<Dictionary<string, string>, List<double>>();
+
+            if (xLabels == null && yLabels == null && values == null)
+            {
+                for (var i = 0; i < defaultLabelsX.Count; i++)
+                {
+                    var labelsDict = new Dictionary<string, string>();
+                    for (var j = 0; j < defaultLabelsY.Count; j++)
+                    {
+                        labelsDict.Add(defaultLabelsY[j], defaultLabelsX[i]);
+                    }
+                    output.Add(labelsDict, defaultNestedValues[i]);
+                }
+
+                return output;
+            }
+
             if (xLabels.Count != values.Count && xLabels.Count > 0)
             {
                 return null;
             }
 
-            var output = new Dictionary<string, List<double>>();
 
             for (var i = 0; i < xLabels.Count; i++)
             {
-                output.Add(xLabels[i], values[i]);
+                var labelsDict = new Dictionary<string, string>();
+                for (var j = 0; j < yLabels.Count; j++)
+                {
+                    labelsDict.Add(yLabels[j], xLabels[i]);
+                }
+                output.Add(labelsDict, values[i]);
             }
 
             return output;
