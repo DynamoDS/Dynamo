2eab68528843c09c2b02de7c00e048540003f1fd
JimmySanchezGlobant <jimmy.sanchez@globant.com>
2023-06-06 13:54:55 -0500

    First version of XML comments warnings fixed

--- test/Engine/FFITarget/AbstractDisposeTracert.cs
+++ test/Engine/FFITarget/AbstractDisposeTracert.cs
@@ -1,34 +1,57 @@
-﻿using System;
+using System;
 
 namespace FFITarget
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public abstract class AbstractDisposeTracert : IDisposable
     {
+        /// <summary>
+        /// 
+        /// </summary>
         static public int DisposeCount { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public virtual void Dispose()
         {
             DisposeCount++;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public int I { get; set; }
     }
 
 
-
+    /// <summary>
+    /// 
+    /// </summary>
     public class AbstractDerivedDisposeTracer2 : AbstractDisposeTracert
     {
-
+        /// <summary>
+        /// 
+        /// </summary>
         public AbstractDerivedDisposeTracer2()
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="i"></param>
         public AbstractDerivedDisposeTracer2(int i)
         {
             I = i;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void StupidPlaceHolderMethod()
         {
 
