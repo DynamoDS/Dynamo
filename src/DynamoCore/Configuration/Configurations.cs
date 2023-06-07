226d41b298663a89eff64980655ec9105bcd001f
Roberto T <61755417+RobertGlobant20@users.noreply.github.com>
2023-06-07 14:16:14 -0600

    DYN-5795 Lucene Search Weights
    The hard-coded values for the field names were moved to the Configurations class and all the places in which this names were used were replaced.
    Also in the SearchViewModel.Search() method I've done minor changes to consider the wildcard expression * keyword *

--- src/DynamoCore/Configuration/Configurations.cs
+++ src/DynamoCore/Configuration/Configurations.cs
@@ -56,10 +56,47 @@ namespace Dynamo.Configuration
         /// </summary>
         public static LuceneVersion LuceneNetVersion = LuceneVersion.LUCENE_48;
 
+
+        /// <summary>
+        /// This represent the fields that will be indexed when initializing Lucene Search
+        /// </summary>
+        public enum IndexFieldsEnum
+        {
+            /// <summary>
+            /// Name - The name of the node
+            /// </summary>
+            Name,
+            /// <summary>
+            /// FullCategoryName - The category of the node
+            /// </summary>
+            FullCategoryName,
+            /// <summary>
+            /// Description - The description of the node
+            /// </summary>
+            Description,
+            /// <summary>
+            /// SearchKeywords - Several keywords that will be used when searching any word (this values are coming from xml files like BuiltIn.xml, DesignScriptBuiltin.xml or ProtoGeometry.xml)
+            /// </summary>
+            SearchKeywords,
+            /// <summary>
+            /// DocName - Name of the Document
+            /// </summary>
+            DocName,
+            /// <summary>
+            /// Documentation - Documentation of the node
+            /// </summary>
+            Documentation
+        }
+
         /// <summary>
         /// Fields to be indexed by Lucene Search
         /// </summary>
-        public static string[] IndexFields = { "Name", "FullCategoryName", "Description", "SearchKeywords", "InputParameters", "OutputParameters", "DocName", "Documentation", "PackageName", "PackageVersion" };
+        public static string[] IndexFields = { nameof(IndexFieldsEnum.Name),
+                                               nameof(IndexFieldsEnum.FullCategoryName),
+                                               nameof(IndexFieldsEnum.Description),
+                                               nameof(IndexFieldsEnum.SearchKeywords),
+                                               nameof(IndexFieldsEnum.DocName),
+                                               nameof(IndexFieldsEnum.Documentation)};
 
         #endregion
 
