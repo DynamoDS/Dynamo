74867c3466ae8ece777323b66192edc3a2fc3f1b
marbelarrietaGlobant <134449953+marbelarrietaGlobant@users.noreply.github.com>
2023-06-07 09:16:07 -0500

    remove requiresSTA from assemblyInfo

--- test/Libraries/DynamoMSOfficeTests/Properties/AssemblyInfo.cs
+++ test/Libraries/DynamoMSOfficeTests/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Threading;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynamoMSOfficeTests")]
-[assembly: AssemblyCulture("")]
-
-#if NET6_0_OR_GREATER
-[assembly: Apartment(ApartmentState.STA)]
-#elif NETFRAMEWORK
-        [assembly: RequiresSTA]
-#endif
+[assembly: AssemblyCulture("")] 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("45c011cc-53f5-477e-b6b0-ff4e494f6674")]
