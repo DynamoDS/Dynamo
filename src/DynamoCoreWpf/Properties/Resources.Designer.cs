6ef1f9169e258392dde6ad8838c3769d38316cba
Aaron (Qilong) <aaron.tang@autodesk.com>
2023-06-21 15:42:44 -0400

    revert setting name because it would be too long

--- src/DynamoCoreWpf/Properties/Resources.Designer.cs
+++ src/DynamoCoreWpf/Properties/Resources.Designer.cs
@@ -7134,7 +7134,7 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Include Timestamp in Image Export File Name.
+        ///   Looks up a localized string similar to Include Timestamp in Export Image Path.
         /// </summary>
         public static string PreferencesViewSettingIncludeTimestampExportPath {
             get {
