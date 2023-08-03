4dfdef490185d49267d9ccaa32b37906ca75bef4
Jesus Alfredo Alviño <jesus.alfredo.alvino@autodesk.com>
2023-08-02 23:59:17 -0500

    Adding Preferences to the config Settings Test

--- test/DynamoCoreTests/Settings.cs
+++ test/DynamoCoreTests/Settings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -91,7 +91,8 @@ namespace Dynamo.Tests
 
             var config = new DynamoModel.DefaultStartConfiguration()
             {
-                PythonTemplatePath = templatePath
+                PythonTemplatePath = templatePath,
+                Preferences = PreferenceSettings.Instance
             };
 
             var model = DynamoModel.Start(config);
@@ -106,7 +107,8 @@ namespace Dynamo.Tests
 
             var config = new DynamoModel.DefaultStartConfiguration()
             {
-                PythonTemplatePath = templatePath
+                PythonTemplatePath = templatePath,
+                Preferences = PreferenceSettings.Instance
             };
 
             var model = DynamoModel.Start(config);
