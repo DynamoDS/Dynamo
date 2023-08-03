001392f59b6ad3cf56a9e072ed112fccd035ad44
pinzart <tiberiu.pinzariu@autodesk.com>
2023-08-03 11:32:05 -0400

    remove net48 dsoffice

--- src/Libraries/DSOffice/Properties/AssemblyInfo.cs
+++ src/Libraries/DSOffice/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,7 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DSOffice")]
 [assembly: AssemblyCulture("")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4b9906ad-ac4d-469c-9f6e-0373b8c7a12a")]
-[assembly: InternalsVisibleTo("DynamoCoreTests")]
 [assembly: InternalsVisibleTo("DynamoMSOfficeTests")]
+[assembly: TypeForwardedTo(typeof(DSOffice.WorkBook))]
+[assembly: TypeForwardedTo(typeof(DSOffice.WorkSheet))]
