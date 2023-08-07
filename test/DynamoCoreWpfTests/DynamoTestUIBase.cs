865b6bfd74a270408d5e8097f40136fead7765ee
Aparajit Pratap <aparajit.pratap@autodesk.com>
2023-08-07 13:54:00 -0400

    update

--- test/DynamoCoreWpfTests/DynamoTestUIBase.cs
+++ test/DynamoCoreWpfTests/DynamoTestUIBase.cs
@@ -36,7 +36,7 @@ namespace DynamoCoreWpfTests
 
         protected string TempFolder { get; private set; }
 
-        [SetUp]
+        [OneTimeSetUp]
         public virtual void Start()
         {
             var assemblyPath = Assembly.GetExecutingAssembly().Location;
@@ -105,7 +105,7 @@ namespace DynamoCoreWpfTests
             };
         }
 
-        [TearDown]
+        [OneTimeTearDown]
         public void Exit()
         {
             //Ensure that we leave the workspace marked as
