130d3c3510ef376182bae38ddb3dcd78a67bc9c5
aparajit-pratap <aparajit.pratap@autodesk.com>
2023-08-06 22:21:38 -0400

    Migrating formula node deserialization to code block nodes (#14196)
    * attempt at migrating formula node deserialization to code block nodes
    
    * use DS parser to migrate formula node code, remove ncalc.
    
    * delete formula nodemodel and view customization files
    
    * review comments
    
    * add migration for more builtin methods
    
    * fix net6 build by removing unused usings
    
    * review comments, add ncalc to remove_me
    
    * review comment
    
    * cleanup, add migration test
    
    * add xml migration for formula node
    
    * cleanup
    
    * cleanup, merge master
    
    * update tests, warning message
    
    * remove unused usings

--- src/Libraries/CoreNodeModels/Formula.cs
+++ src/Libraries/CoreNodeModels/Formula.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Xml;
+using Dynamo.Engine;
 using Dynamo.Graph;
 using Dynamo.Graph.Nodes;
-using NCalc;
-using Newtonsoft.Json;
+using Dynamo.Graph.Nodes.NodeLoaders;
+using Dynamo.Migration;
 using ProtoCore;
-using ProtoCore.AST.AssociativeAST;
-using Expression = NCalc.Expression;
 
 namespace CoreNodeModels
 {
@@ -20,262 +16,40 @@ namespace CoreNodeModels
     [AlsoKnownAs("DSCoreNodesUI.Formula")]
     public class Formula : NodeModel
     {
-        private string formulaString = "";
-
-        [JsonProperty("Formula")]
-        public string FormulaString
+        [NodeMigration(version: "3.0.0.0")]
+        public static NodeMigrationData MigrateToCodeBlockNode(NodeMigrationData data)
         {
-            get
+            var migrationData = new NodeMigrationData(data.Document);
+            var node = data.MigratedNodes.ElementAt(0);
+
+            var child = node.FirstChild;
+            if (child == null)
             {
-                return formulaString;
+                return migrationData;
             }
+            var formula = child.InnerText;
+            string convertedCode = string.Empty;
+            bool conversionFailed = false;
 
-            set
-            {
-                if (formulaString == null || !formulaString.Equals(value))
-                {
-                    formulaString = value;
-                    if (value != null)
-                    {
-                        ElementState oldState = State;
-                        {
-                            ProcessFormula();
-                            RaisePropertyChanged("FormulaString");
-
-                            OnNodeModified();
-                        }
-
-                        if (oldState != State)
-                            RaisePropertyChanged("State");
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// The NodeType property provides a name which maps to the 
-        /// server type for the node. This property should only be
-        /// used for serialization. 
-        /// </summary>
-        public override string NodeType
-        {
-            get
-            {
-                return "FormulaNode";
-            }
-        }
-
-        [JsonConstructor]
-        private Formula(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base(inPorts, outPorts)
-        {
-            ArgumentLacing = LacingStrategy.Auto;
-        }
-
-        public Formula()
-        {
-            ArgumentLacing = LacingStrategy.Auto;
-            OutPorts.Add(new PortModel(PortType.Output, this, new PortData("", Properties.Resources.FormulaPortDataResultToolTip)));
-            RegisterAllPorts();
-        }
-
-        protected override bool UpdateValueCore(UpdateValueParams updateValueParams)
-        {
-            string name = updateValueParams.PropertyName;
-            string value = updateValueParams.PropertyValue;
-
-            if (name == "FormulaString")
-            {
-                FormulaString = value;
-                return true; // UpdateValueCore handled.
-            }
-
-            return base.UpdateValueCore(updateValueParams);
-        }
-
-        #region Serialization/Deserialization methods
-
-        protected override void SerializeCore(XmlElement element, SaveContext context)
-        {
-            base.SerializeCore(element, context); //Base implementation must be called
-            var formStringNode = element.OwnerDocument.CreateElement("FormulaText");
-            formStringNode.InnerText = FormulaString;
-            element.AppendChild(formStringNode);
-        }
-
-        protected override void DeserializeCore(XmlElement nodeElement, SaveContext context)
-        {
-            base.DeserializeCore(nodeElement, context); //Base implementation must be called
-
-            if (nodeElement.Attributes != null)
-            {
-                var formulaAttr = nodeElement.Attributes["formula"];
-                if (formulaAttr != null)
-                {
-                    FormulaString = formulaAttr.Value;
-                    return;
-                }
-            }
-
-            var formStringNode = nodeElement.ChildNodes.Cast<XmlNode>().FirstOrDefault(childNode => childNode.Name == "FormulaText");
-            FormulaString = formStringNode != null
-                ? formStringNode.InnerText
-                : nodeElement.InnerText;
-        }
-
-        #endregion
-
-/*
-        private static readonly HashSet<string> reservedFuncNames = new HashSet<string> { 
-            "abs", "acos", "asin", "atan", "ceiling", "cos",
-            "exp", "floor", "ieeeremainder", "log", "log10",
-            "max", "min", "pow", "round", "sign", "sin", "sqrt",
-            "tan", "truncate", "in", "if"
-        };
-*/
-
-        private static readonly HashSet<string> reservedParamNames = new HashSet<string> {
-            "pi", "π"
-        };
-
-        private void ProcessFormula()
-        {
-            Expression e;
+            var newNode = MigrationManager.CreateCodeBlockNodeFrom(node);
+            newNode.SetAttribute("guid", node.Attributes["guid"].Value);
             try
             {
-                e = new Expression(
-                    FormulaString.ToLower()
-                        .Replace(" and ", "+").Replace("&&", "+")
-                        .Replace(" or ", "+").Replace("||", "+"), 
-                    EvaluateOptions.IgnoreCase);
+                var formulaConverter = new MigrateFormulaToDS();
+                convertedCode = formulaConverter.ConvertFormulaToDS(formula);
             }
-            catch (Exception ex)
+            catch (BuildHaltException)
             {
-                Error(ex.Message);
-                return;
+                newNode.Attributes["CodeText"].Value = formula;
+
+                conversionFailed = true;
             }
-
-            if (e.HasErrors())
+            if (!conversionFailed)
             {
-                Error(e.Error);
-                return;
+                newNode.Attributes["CodeText"].Value = convertedCode;
             }
-
-            var parameters = new List<string>();
-            var paramSet = new HashSet<string>();
-
-            e.EvaluateParameter += delegate(string name, ParameterArgs args)
-            {
-                if (!paramSet.Contains(name) && !reservedParamNames.Contains(name))
-                {
-                    paramSet.Add(name);
-                    parameters.Add(name);
-                }
-
-                args.Result = 0;
-            };
-
-            e.EvaluateFunction += delegate(string name, FunctionArgs args)
-            {
-                foreach (var p in args.Parameters)
-                {
-                    p.Evaluate();
-                }
-
-                args.Result = 0;
-            };
-
-            try
-            {
-                e.Evaluate();
-            }
-            catch { }
-
-            InPorts.Clear();
-
-            foreach (var p in parameters)
-            {
-                InPorts.Add(new PortModel(PortType.Input, this, new PortData(p, "variable")));
-            }
-
-            ClearErrorsAndWarnings();
-        }
-
-        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
-        {
-            Func<string, string[], object[], object> backingMethod = DSCore.Math.EvaluateFormula;
-
-            // Format input names to be used as function parameters
-            var inputs = InPorts.Select(x => x.Name.Replace(' ', '_')).ToList();
-
-
-            /*  def formula_partial(<params>) {
-             *    return = DSCore.Formula.Evaluate(<FormulaString>, <InPortData Names>, <params>);
-             *  }
-             */
-
-            var functionDef = new FunctionDefinitionNode
-            {
-                Name = "__formula_" + AstIdentifierGuid, 
-                Signature =
-                    new ArgumentSignatureNode
-                    {
-                        Arguments = inputs.Select(AstFactory.BuildParamNode).ToList()
-                    },
-                ReturnType = TypeSystem.BuildPrimitiveTypeObject(PrimitiveType.Var),
-                FunctionBody =
-                    new CodeBlockNode
-                    {
-                        Body =
-                            new List<AssociativeNode>
-                                {
-                                    AstFactory.BuildReturnStatement(
-                                        AstFactory.BuildFunctionCall(
-                                            backingMethod,
-                                            new List<AssociativeNode>
-                                            {
-                                                AstFactory.BuildStringNode(FormulaString),
-                                                AstFactory.BuildExprList(
-                                                    InPorts.Select(
-                                                        x =>
-                                                            AstFactory.BuildStringNode(x.Name) as
-                                                            AssociativeNode).ToList()),
-                                                AstFactory.BuildExprList(
-                                                    inputs.Select(AstFactory.BuildIdentifier)
-                                                        .Cast<AssociativeNode>()
-                                                        .ToList())
-                                            }))
-                                }
-                    }
-            };
-
-            if (IsPartiallyApplied)
-            {
-                return new AssociativeNode[]
-                {
-                    functionDef,
-                    AstFactory.BuildAssignment(
-                        GetAstIdentifierForOutputIndex(0),
-                        AstFactory.BuildFunctionObject(
-                            functionDef.Name,
-                            InPorts.Count,
-                            Enumerable.Range(0, InPorts.Count).Where(index=>InPorts[index].IsConnected),
-                            inputAstNodes))
-                };
-            }
-            else
-            {
-                UseLevelAndReplicationGuide(inputAstNodes);
-
-                return new AssociativeNode[]
-                {
-                    functionDef,
-                    AstFactory.BuildAssignment(
-                        GetAstIdentifierForOutputIndex(0),
-                        AstFactory.BuildFunctionCall(
-                            functionDef.Name,
-                            inputAstNodes))
-                };
-            }
+            migrationData.AppendNode(newNode);
+            return migrationData;
         }
     }
 }
