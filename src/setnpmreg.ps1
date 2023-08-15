52507154377b01d74ff3a0b0082c81f819f5b54e
mjk.kirschner <mjk.kirschner@gmail.com>
2023-08-14 22:43:27 -0400

    review comments

--- src/setnpmreg.ps1
+++ src/setnpmreg.ps1
@@ -1,8 +1,8 @@
 If ((Test-Connection git.autodesk.com).PingSucceeded){
-    echo host reachable
-    npm set registry https://npm.autodesk.com/artifactory/api/npm/autodesk-npm-virtual/
+    echo adsk reachable
+    New-Item -Path . -Name ".npmrc" -ItemType "file" -Value "registry=https://npm.autodesk.com/artifactory/api/npm/autodesk-npm-virtual/" -Force
 }
 else{
-    echo host not reachable
-    npm set registry https://registry.npmjs.org
+    echo adsk not reachable
+    New-Item -Path . -Name ".npmrc" -ItemType "file" -Value "registry=https://registry.npmjs.org" -Force
 }
