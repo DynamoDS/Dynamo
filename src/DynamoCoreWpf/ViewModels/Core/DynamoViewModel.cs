0780568d0e9119f926dc4f995a0c5ae8365189d5
jesusalvino <96534278+jesusalvino@users.noreply.github.com>
2023-08-25 13:34:58 -0500

    DYN-6130 remove the toast notification (#14317)
    * Make sure close the Toast notification when Dynamo closes
    
    * Removing extra file
    
    * Removing extra file
    
    * Relocate the responsibility about the toast notification to the DynamoViewModel
    
    * removing unnecessary using
    
    * take advantage of already implementation to apply minimal changes
    
    * Moving the code to a proper place
    
    ---------
    
    Co-authored-by: Jesus Alfredo Alviño <jesus.alfredo.alvino@autodesk.com>

--- src/DynamoCoreWpf/ViewModels/Core/DynamoViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Core/DynamoViewModel.cs
@@ -3421,6 +3421,7 @@ namespace Dynamo.ViewModels
 
 
             BackgroundPreviewViewModel.Dispose();
+            MainGuideManager?.CloseRealTimeInfoWindow();
 
             model.ShutDown(shutdownParams.ShutdownHost);
             if (shutdownParams.ShutdownHost)
@@ -3434,7 +3435,7 @@ namespace Dynamo.ViewModels
             BackgroundPreviewViewModel.PropertyChanged -= Watch3DViewModelPropertyChanged;
             WatchHandler.RequestSelectGeometry -= BackgroundPreviewViewModel.AddLabelForPath;
             model.ComputeModelDeserialized -= model_ComputeModelDeserialized;
-            model.RequestNotification -= model_RequestNotification;
+            model.RequestNotification -= model_RequestNotification;            
 
             return true;
         }
