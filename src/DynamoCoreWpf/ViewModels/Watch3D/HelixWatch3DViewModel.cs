e4ef88c0be4c26ac5ab52e7680647ce14870bb8b
Bogdan Zavu <BogdanZavu@users.noreply.github.com>
2023-08-31 10:48:22 -0400

    fix cast crash (#14351)
    Co-authored-by: Bogdan Zavu <bogdan.zavu@autodesk.com>

--- src/DynamoCoreWpf/ViewModels/Watch3D/HelixWatch3DViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/Watch3D/HelixWatch3DViewModel.cs
@@ -909,7 +909,9 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
             renderTimer.Start();
 #endif
             var packages = taskPackages.Packages;
-            var meshPackages = packages.Cast<HelixRenderPackage>().Where(rp => rp.MeshVertexCount % 3 == 0);
+
+            var meshPackages = packages.Where(renderPackage => (renderPackage as HelixRenderPackage)?.MeshVertexCount % 3 == 0)
+                .Select(renderPackage => renderPackage as HelixRenderPackage);
 
             RemoveGeometryForUpdatedPackages(meshPackages);
             try
