7a1a34412b3ed1c62db87576083f042a44c2fcf3
mjk.kirschner <mjk.kirschner@gmail.com>
2023-09-05 15:46:33 -0400

    try to get tests passing

--- src/Libraries/CoreNodeModels/String.cs
+++ src/Libraries/CoreNodeModels/String.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Autodesk.DesignScript.Runtime;
 using CoreNodeModels.Properties;
 using Dynamo.Graph.Nodes;
 using Newtonsoft.Json;
@@ -30,6 +32,15 @@ namespace CoreNodeModels
         {
             AssociativeNode rhs = null;
 
+            //to avoid needing to add a different builtin function for stringFromObject conversion
+            //we add a default value of false to old node calls, which will only have 1 parameter, the string.
+            //new calls will have 2 parameters. the string and the numeric format option.
+            if (inputAstNodes.Count < 2)
+            {
+                //force to false for use numeric to keep old behavior of this node.
+                inputAstNodes.Add(AstFactory.BuildBooleanNode(false));
+            }
+
             if (IsPartiallyApplied)
             {
                 var connectedInputs = new List<AssociativeNode>();
@@ -49,7 +60,7 @@ namespace CoreNodeModels
                 rhs = AstFactory.BuildFunctionCall("__CreateFunctionObject", inputParams);
             }
             else
-            {
+            {  
                 rhs = AstFactory.BuildFunctionCall(functionName, inputAstNodes);
             }
 
@@ -60,6 +71,34 @@ namespace CoreNodeModels
         }
     }
 
+    [NodeName("String from Object2")]
+    [NodeDescription("StringfromObjectDescription", typeof(Resources))]
+    [NodeCategory("Core.String.Actions")]
+    [NodeSearchTags("FromObjectSearchTags", typeof(Resources))]
+    [OutPortTypes("string")]
+    [IsDesignScriptCompatible]
+    public class StringFromObject : ToStringNodeBase
+    {
+        [JsonConstructor]
+        private StringFromObject(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) :
+            base("__ToStringFromObject", inPorts, outPorts)
+        {
+            ArgumentLacing = LacingStrategy.Disabled;
+        }
+
+        public StringFromObject() : base("__ToStringFromObject")
+        {
+            ArgumentLacing = LacingStrategy.Disabled;
+            InPorts.Add(new PortModel(PortType.Input, this, new PortData("object", Resources.FromObjectPortDataObjToolTip)));
+            InPorts.Add(new PortModel(PortType.Input, this,
+                    new PortData("useNumericFormat",
+                        "should the numeric precision format be used when converting doubles",
+                        AstFactory.BuildBooleanNode(false))));
+            OutPorts.Add(new PortModel(PortType.Output, this, new PortData("string", Resources.FromObjectPortDataResultToolTip)));
+            RegisterAllPorts();
+        }
+    }
+
     [NodeName("String from Object")]
     [NodeDescription("StringfromObjectDescription", typeof(Resources))]
     [NodeCategory("Core.String.Actions")]
@@ -67,25 +106,45 @@ namespace CoreNodeModels
     [OutPortTypes("string")]
     [IsDesignScriptCompatible]
     [AlsoKnownAs("DSCoreNodesUI.StringNodes.FromObject", "DSCoreNodesUI.FromObject")]
+    [Obsolete("TODO update to resx - please use string from object overload with numeric format option")]
     public class FromObject: ToStringNodeBase 
     {
         [JsonConstructor]
         private FromObject(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : 
             base("__ToStringFromObject",inPorts, outPorts)
         {
-            if (inPorts.Count() < 2)
-            {
-                InPorts.Add(new PortModel(PortType.Input, this, new PortData("useNumericFormat", "should the numeric precision format be used when converting doubles", AstFactory.BuildBooleanNode(false))));
-            }
             ArgumentLacing = LacingStrategy.Disabled;
         }
-
         public FromObject() : base("__ToStringFromObject")
         {
             ArgumentLacing = LacingStrategy.Disabled;
             InPorts.Add(new PortModel(PortType.Input, this, new PortData("object", Resources.FromObjectPortDataObjToolTip)));
-            InPorts.Add(new PortModel(PortType.Input, this, new PortData("useNumericFormat", "should the numeric precision format be used when converting doubles",AstFactory.BuildBooleanNode(false))));
             OutPorts.Add(new PortModel(PortType.Output, this, new PortData("string", Resources.FromObjectPortDataResultToolTip)));
+            RegisterAllPorts();
+        }
+    }
+
+    [NodeName("String from Array2")]
+    [NodeDescription("StringfromArrayDescription", typeof(Resources))]
+    [NodeCategory("Core.String.Actions")]
+    [NodeSearchTags("FromArraySearchTags", typeof(Resources))]
+    [OutPortTypes("string")]
+    [IsDesignScriptCompatible]
+    [AlsoKnownAs("DSCoreNodesUI.StringNodes.FromArray", "DSCoreNodesUI.FromArray")]
+    public class StringFromArray : ToStringNodeBase
+    {
+        [JsonConstructor]
+        private StringFromArray(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) :
+            base("__ToStringFromArray", inPorts, outPorts)
+        {
+            ArgumentLacing = LacingStrategy.Disabled;
+        }
+
+        public StringFromArray() : base("__ToStringFromArray")
+        {
+            ArgumentLacing = LacingStrategy.Disabled;
+            InPorts.Add(new PortModel(PortType.Input, this, new PortData("array", Resources.FromArrayPortDataArrayToolTip)));
+            OutPorts.Add(new PortModel(PortType.Output, this, new PortData("string", Resources.FromArrayPortDataResultToolTip)));
             RegisterAllPorts();
         }
     }
@@ -110,6 +169,10 @@ namespace CoreNodeModels
         {
             ArgumentLacing = LacingStrategy.Disabled;
             InPorts.Add(new PortModel(PortType.Input, this, new PortData("array", Resources.FromArrayPortDataArrayToolTip)));
+            InPorts.Add(new PortModel(PortType.Input, this,
+                new PortData("useNumericFormat",
+                    "should the numeric precision format be used when converting doubles",
+                    AstFactory.BuildBooleanNode(false))));
             OutPorts.Add(new PortModel(PortType.Output, this, new PortData("string", Resources.FromArrayPortDataResultToolTip)));
             RegisterAllPorts();
         }
