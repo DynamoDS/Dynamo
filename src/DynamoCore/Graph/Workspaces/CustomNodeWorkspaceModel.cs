cf65b2db93719d58b2c469cb176ed9dfb70a1eb7
Jorgen Dahl <jorgen.dahl@autodesk.com>
2023-10-24 10:17:53 -0400

    CustomNodeWorkspaceModel.cs

--- src/DynamoCore/Graph/Workspaces/CustomNodeWorkspaceModel.cs
+++ src/DynamoCore/Graph/Workspaces/CustomNodeWorkspaceModel.cs
@@ -315,23 +315,5 @@ namespace Dynamo.Graph.Workspaces
 
             base.Save(newPath, isBackup, engine);
         }
-
-        [Obsolete("Method will be deprecated in Dynamo 3.0.")]
-        protected override bool PopulateXmlDocument(XmlDocument document)
-        {
-            if (!base.PopulateXmlDocument(document))
-                return false;
-
-            var root = document.DocumentElement;
-            if (root == null)
-                return false;
-            
-            var guid = CustomNodeDefinition != null ? CustomNodeDefinition.FunctionId : Guid.NewGuid();
-            root.SetAttribute("ID", guid.ToString());
-            root.SetAttribute("Description", Description);
-            root.SetAttribute("Category", Category);
-            
-            return true;
-        }
     }
 }
