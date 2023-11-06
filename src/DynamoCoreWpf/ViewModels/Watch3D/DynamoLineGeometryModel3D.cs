d790e1186acd37c4c3fc16556cdff6c2eabbbdf3
Jorgen Dahl <jorgen.dahl@autodesk.com>
2023-11-06 09:12:56 -0500

    DynamoLineGeometryModel3D.cs

--- src/DynamoCoreWpf/ViewModels/Watch3D/DynamoLineGeometryModel3D.cs
+++ src/DynamoCoreWpf/ViewModels/Watch3D/DynamoLineGeometryModel3D.cs
@@ -11,7 +11,7 @@ namespace Dynamo.Wpf.ViewModels.Watch3D
     /// A Dynamo line class which supports the RenderCustom technique.
     /// </summary>
     [Obsolete("Do not use! This will be moved to a new project in a future version of Dynamo.")]
-    public class DynamoLineGeometryModel3D : LineGeometryModel3D
+    internal class DynamoLineGeometryModel3D : LineGeometryModel3D
     {
         protected override SceneNode OnCreateSceneNode()
         {
