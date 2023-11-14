bcb39716a0d34b54525b75d5d29766c0742c4a9c
Deyan Nenov <dnenov@archilizer.com>
2023-11-14 18:13:45 +0000

    restored deleted namespaces
    - not sure how the namespaces got wiped with the previous commit

--- src/DynamoCoreWpf/ViewModels/PackageManager/PackageItemRootViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/PackageManager/PackageItemRootViewModel.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using Dynamo.Utilities;
+
 namespace Dynamo.PackageManager.UI
 {
     public enum DependencyType
