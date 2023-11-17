eceb9e5d9a006d0eee13079e582438b1f4b5c7fe
Aaron (Qilong) <aaron.tang@autodesk.com>
2023-11-17 17:15:13 -0500

    Revert "DYN-6329 DYN-6396 Pm publishpackage packagecontents (#14496)"
    This reverts commit 803d8e1091cc86bc1830f0ba16d90f40370bdbb7.

--- src/DynamoCoreWpf/Views/PackageManager/PackageManagerView.xaml.cs
+++ src/DynamoCoreWpf/Views/PackageManager/PackageManagerView.xaml.cs
@@ -5,7 +5,6 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using Dynamo.Controls;
 using Dynamo.Logging;
-using Dynamo.Models;
 using Dynamo.UI;
 using Dynamo.Utilities;
 using Dynamo.ViewModels;
@@ -128,7 +127,6 @@ namespace Dynamo.PackageManager.UI
         private void WindowClosed(object sender, EventArgs e)
         {
             this.packageManagerPublish.Dispose();
-            this.PackageManagerViewModel.PublishPackageViewModel.CancelCommand.Execute();
             this.PackageManagerViewModel.PackageSearchViewModel.RequestShowFileDialog -= OnRequestShowFileDialog;
             this.PackageManagerViewModel.PackageSearchViewModel.PackageManagerViewClose();
         }
@@ -179,44 +177,6 @@ namespace Dynamo.PackageManager.UI
         {
             this.loadingSearchWarningBar.Visibility = Visibility.Collapsed;
             this.loadingMyPackagesWarningBar.Visibility = Visibility.Collapsed;
-        }
-
-        private void tab_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            var selectedTab = sender as TabItem;
-            if (selectedTab == null) return;
-            var tabControl = selectedTab.Parent as TabControl;
-            if (tabControl == null) return;
-            var prevTab = tabControl.SelectedItem as TabItem;
-            if (prevTab == null) return;
-
-            if (prevTab.Name.Equals("publishTab") && !selectedTab.Name.Equals("publishTab"))
-            {
-
-                if (!PackageManagerViewModel.PublishPackageViewModel.AnyUserChanges())
-                {
-                    selectedTab.IsSelected = true;
-                    return;
-                }
-
-                MessageBoxResult response = DynamoModel.IsTestMode ? MessageBoxResult.OK :
-                        MessageBoxService.Show(
-                            this,
-                            Dynamo.Wpf.Properties.Resources.DiscardChangesWarningPopupMessage,
-                            Dynamo.Wpf.Properties.Resources.DiscardChangesWarningPopupCaption,
-                            MessageBoxButton.OKCancel,
-                            MessageBoxImage.Warning);
-
-                if (response == MessageBoxResult.OK)
-                {
-                    PackageManagerViewModel.PublishPackageViewModel.CancelCommand.Execute();
-                    selectedTab.IsSelected = true;
-                }
-                else
-                {
-                    e.Handled = true;
-                }
-            }
         }
     }
 }
