4e0871e736775a825177c101d656b5748091d704
Craig Long <craig.long@autodesk.com>
2023-12-02 17:41:11 -0500

    Why was this ever this way

--- src/DynamoCoreWpf/Views/Core/DynamoView.xaml.cs
+++ src/DynamoCoreWpf/Views/Core/DynamoView.xaml.cs
@@ -1360,7 +1360,7 @@ namespace Dynamo.Controls
             BackgroundPreview = new Watch3DView { Name = BackgroundPreviewName };
             background_grid.Children.Add(BackgroundPreview);
             BackgroundPreview.DataContext = dynamoViewModel.BackgroundPreviewViewModel;
-            BackgroundPreview.Margin = new System.Windows.Thickness(0, 20, 0, 0);
+            BackgroundPreview.Margin = new System.Windows.Thickness(0, 0, 0, 0);
             var vizBinding = new Binding
             {
                 Source = dynamoViewModel.BackgroundPreviewViewModel,
