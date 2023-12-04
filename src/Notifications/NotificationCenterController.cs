69f72483391f6c65d54237595852f5c578e8f3a3
Enzo Batista <enzo.batista@globant.com>
2023-12-04 16:20:28 -0300

    feat(NotificationCenterController): add PreviewMouseDown to dismiss notifications panel by clicking outside the panel

--- src/Notifications/NotificationCenterController.cs
+++ src/Notifications/NotificationCenterController.cs
@@ -83,6 +83,7 @@ namespace Dynamo.Notifications
             dynamoView.LocationChanged += DynamoView_LocationChanged;
             notificationsButton.Click += NotificationsButton_Click;
             dynamoView.Closing += View_Closing;
+            dynamoView.PreviewMouseDown += DynamoView_PreviewMouseDown;
 
             notificationUIPopup = new NotificationUI
             {
@@ -274,6 +275,17 @@ namespace Dynamo.Notifications
         {
             notificationUIPopup.Placement = PlacementMode.Bottom;
             notificationUIPopup.UpdatePopupLocation();
+        }
+
+        /// <summary>
+        /// Dismiss notifications panel by clicking outside the panel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DynamoView_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!notificationUIPopup.IsOpen) return;
+            notificationUIPopup.IsOpen = false;
         }
 
         /// <summary>
