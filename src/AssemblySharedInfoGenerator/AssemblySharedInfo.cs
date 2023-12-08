d7b1f5e85ae0afe05304673c7dce51a3636c1811
Jorgen Dahl <jorgen.dahl@autodesk.com>
2023-12-07 22:08:25 -0500

    Bump year in algorithm to 2023.

--- src/AssemblySharedInfoGenerator/AssemblySharedInfo.cs
+++ src/AssemblySharedInfoGenerator/AssemblySharedInfo.cs
@@ -45,7 +45,7 @@ using System.Runtime.InteropServices;
 // to distinguish one build from another. AssemblyFileVersion is specified
 // in AssemblyVersionInfo.cs so that it can be easily incremented by the
 // automated build process.
-[assembly: AssemblyVersion("3.1.0.7060")]
+[assembly: AssemblyVersion("3.1.0.3411")]
 
 
 // By default, the "Product version" shown in the file properties window is
@@ -54,12 +54,14 @@ using System.Runtime.InteropServices;
 // AssemblyVersionAttribute so that the "Product version" in the file
 // properties window matches the version displayed in the GAC shell extension.
 //[assembly: AssemblyInformationalVersion("1.0.0.0")] // a.k.a. "Product version"
-// Version information for an assembly consists of the following four valu
-//      Majo//Minor Version
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
 //      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("3.1.0.7060")]
+[assembly: AssemblyFileVersion("3.1.0.3411")]
