557452fe4696b177f9c96828fac308ca2cb30c82
pinzart90 <46732933+pinzart90@users.noreply.github.com>
2024-01-24 14:28:36 -0500

    Test crash fixes (#14789)
    * update
    
    * Update NodeModelAssemblyLoader.cs
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update AnalyticsTests.cs
    
    * Update SerializationTests.cs
    
    * Update Setup.cs
    
    * update
    
    * update
    
    * Update SplashScreen.xaml.cs
    
    * Update PublishPackageViewModelTests.cs
    
    * Update PublishPackageViewModelTests.cs
    
    * update
    
    * Update AssemblyInfo.cs
    
    * Update TestUtilities.cs
    
    * update
    
    * upate
    
    * update
    
    * update
    
    * update
    
    * Update AnalyticsTests.cs
    
    * Update WebView2Utilities.cs
    
    * update
    
    * update
    
    * Update Setup.cs
    
    * update
    
    * update
    
    * Update PeriodicEvaluationTests.cs
    
    ---------
    
    Co-authored-by: pinzart <tiberiu.pinzariu@autodesk.com>

--- src/DynamoCoreWpf/Utilities/WebView2Utilities.cs
+++ src/DynamoCoreWpf/Utilities/WebView2Utilities.cs
@@ -1,10 +1,49 @@
 using System.Windows;
 using Dynamo.Wpf.Properties;
-using Dynamo.Wpf.Utilities;
+using DynamoUtilities;
 using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
 
-namespace Dynamo.Utilities
+namespace Dynamo.Wpf.Utilities
 {
+    /// <summary>
+    /// Custom Webview2 class designed to have a safer cleanup logic and give better debugging capabilities   
+    /// </summary>
+    public class DynamoWebView2 : WebView2
+    {
+        #region API/Data used for debugging/testing
+        private string tag;
+        #endregion
+
+        public DynamoWebView2() : base()
+        {
+            tag = TestUtilities.WebView2Tag;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (System.Environment.CurrentManagedThreadId != Dispatcher.Thread.ManagedThreadId)
+            {
+                System.Console.WriteLine($"WebView2 instance with stamp {tag} is being disposed of on non-UI thread");
+            }
+            // We should dispose of webview2 only in the UI thread.
+            // Dispose can be called from the Finalizer (which can run on a non UI thread)
+            if (Dispatcher != null)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    base.Dispose(disposing);
+                });
+            }
+            else
+            {
+                System.Console.WriteLine($"WebView2 instance with stamp {tag} is being disposed of but has no valid Dispatcher");
+                // Should we still try to dispose ? (might crash if not on UI thread)
+                base.Dispose(disposing);
+            }
+        }
+    }
+
     /// <summary>
     /// This class will contain several utility functions that will be used for the WebView2 component
     /// </summary>
@@ -25,7 +64,7 @@ namespace Dynamo.Utilities
             catch (WebView2RuntimeNotFoundException)
             {
                 var messageStr = Resources.ResourceManager.GetString("WebView2RequiredMessage");
-                if(messageStr.IndexOf("\\n") >= 0)
+                if (messageStr.IndexOf("\\n") >= 0)
                     messageStr = messageStr.Replace("\\n", "\n");
 
                 MessageBoxService.Show(messageStr,
