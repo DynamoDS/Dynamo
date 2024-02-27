664a2a1de850cbc85a205c4f16125faa5277f3cf
Michael Kirschner <mjk.kirschner@gmail.com>
2024-02-27 08:04:33 -0500

    DYN-6728 feature flags should be controlled by no network mode when creating a dynamo model directly. (#14975)
    * check network mode
    
    * align disable and networkmode
    
    * add small tests
    
    * remove todo
    
    * review comments
    
    * another test

--- test/DynamoCoreTests/Logging/AnalyticsServiceTest.cs
+++ test/DynamoCoreTests/Logging/AnalyticsServiceTest.cs
@@ -80,5 +80,72 @@ namespace Dynamo.Tests.Loggings
                 dynamoCLI?.Kill();
             }
         }
+
+        [Test]
+        [Platform("win")]//nunit attribute for now only run on windows until we know it's useful on linux.
+        public void DisableAnalyticsViaNoNetWorkMode()
+        {
+            var versions = new List<Version>(){
+
+                    new Version(230, 0,0),
+            };
+
+            var directory = new DirectoryInfo(Assembly.GetExecutingAssembly().Location);
+            var testDirectory = Path.Combine(directory.Parent.Parent.Parent.FullName, "test");
+            string openPath = Path.Combine(testDirectory, @"core\Angle.dyn");
+            //go get a valid asm path.
+            var locatedPath = string.Empty;
+            var coreDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Process dynamoCLI = null;
+            //TODO an approach we could take to get this running on linux.
+            //unclear if this needs to be compiled with an ifdef or runtime is ok.
+            //related to https://jira.autodesk.com/browse/DYN-5705
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                DynamoShapeManager.Utilities.SearchForASMInLibGFallback(versions, ref locatedPath, coreDirectory, out _);
+            }
+            else
+            {
+                DynamoShapeManager.Utilities.GetInstalledAsmVersion2(versions, ref locatedPath, coreDirectory);
+            }
+            try
+            {
+                Assert.DoesNotThrow(() =>
+                {
+
+                    dynamoCLI = Process.Start(new ProcessStartInfo(Path.Combine(coreDirectory, "DynamoCLI.exe"), $"--GeometryPath \"{locatedPath}\" -k --NoNetworkMode -o \"{openPath}\" ") { UseShellExecute = true });
+
+                    Thread.Sleep(5000);// Wait 5 seconds to open the dyn
+                    Assert.IsFalse(dynamoCLI.HasExited);
+                    var dt = DataTarget.AttachToProcess(dynamoCLI.Id, false);
+                    var assemblies = dt
+                          .ClrVersions
+                          .Select(dtClrVersion => dtClrVersion.CreateRuntime())
+                          .SelectMany(runtime => runtime.AppDomains.SelectMany(runtimeAppDomain => runtimeAppDomain.Modules))
+                          .Select(clrModule => clrModule.AssemblyName)
+                          .Distinct()
+                          .Where(x => x != null)
+                          .ToList();
+
+                    var firstASMmodulePath = string.Empty;
+                    foreach (string module in assemblies)
+                    {
+                        if (module.IndexOf("Analytics", StringComparison.OrdinalIgnoreCase) != -1)
+                        {
+                            Assert.Fail("Analytics module was loaded");
+                        }
+                        if (module.IndexOf("AdpSDKCSharpWrapper", StringComparison.OrdinalIgnoreCase) != -1)
+                        {
+                            Assert.Fail("ADP module was loaded");
+                        }
+                    }
+                });
+            }
+            finally
+            {
+
+                dynamoCLI?.Kill();
+            }
+        }
     }
 }
