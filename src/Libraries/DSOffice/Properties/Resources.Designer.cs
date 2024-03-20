48f1fe2b48dad597b7b0c3c8a9e251c0a5d245c9
Roberto T <roberto.tellez@globant.com>
2024-03-20 14:52:25 -0600

    DYN-6619-Localizing-StreamWriter-Exception
    Localizing System StreamWriter exception

--- src/Libraries/DSOffice/Properties/Resources.Designer.cs
+++ src/Libraries/DSOffice/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace DSOffice.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace DSOffice.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
+        internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DSOffice.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace DSOffice.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
+        internal static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace DSOffice.Properties {
         /// <summary>
         ///   Looks up a localized string similar to The file or directory cannot be found.
         /// </summary>
-        public static string StreamWriterNotFoundException {
+        internal static string StreamWriterNotFoundException {
             get {
                 return ResourceManager.GetString("StreamWriterNotFoundException", resourceCulture);
             }
