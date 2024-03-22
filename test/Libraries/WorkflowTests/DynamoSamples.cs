6e4a99eed18c88a2c0baf703b75b69066c0312c4
Aaron (Qilong) <173288704@qq.com>
2024-03-21 20:52:38 -0400

    Update DynamoSamples.cs (#15044)

--- test/Libraries/WorkflowTests/DynamoSamples.cs
+++ test/Libraries/WorkflowTests/DynamoSamples.cs
@@ -557,6 +557,7 @@ namespace Dynamo.Tests
         [Test, Category("ExcelTest")]
             //Todo Ritesh: Locally passing but failing on CI.
             //After fixing issue with this test case add Smoke Test Category.
+        [Category("Failure")]
         public void ImportExport_Excel_to_Dynamo()
         {
             OpenSampleModel(@"en-US\ImportExport\ImportExport_Excel to Dynamo.dyn");
