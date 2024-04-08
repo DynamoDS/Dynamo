28ac2e45590882cf80b36c0e4dc35f99eff1a527
Aaron (Qilong) <173288704@qq.com>
2024-04-08 14:48:54 -0400

    Update MarkdownGeneratorCommandTests.cs (#15099)

--- test/Tools/NodeDocumentationMarkdownGeneratorTests/MarkdownGeneratorCommandTests.cs
+++ test/Tools/NodeDocumentationMarkdownGeneratorTests/MarkdownGeneratorCommandTests.cs
@@ -164,7 +164,7 @@ namespace NodeDocumentationMarkdownGeneratorTests
             FromDirectoryCommand.HandleDocumentationFromDirectory(opts);
 
             var generatedFileNames = tempDirectory.GetFiles().Select(x => x.Name);
-            Assert.AreEqual(701, generatedFileNames.Count());
+            Assert.AreEqual(707, generatedFileNames.Count());
         }
 
         [Test]
