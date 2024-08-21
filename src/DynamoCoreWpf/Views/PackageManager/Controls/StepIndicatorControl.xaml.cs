83fecf7fc2dcf5fd8e705d6ce7cdf55de30e9dc4
Deyan Nenov <dnenov@archilizer.com>
2024-08-21 18:28:43 +0100

    new wizzard control
    - new wizard-style custom user control

---
+++ src/DynamoCoreWpf/Views/PackageManager/Controls/StepIndicatorControl.xaml.cs
@@ -0,0 +1,134 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Dynamo.UI;
+
+namespace Dynamo.PackageManager.UI
+{
+    /// <summary>
+    /// Interaction logic for StepIndicatorControl.xaml
+    /// </summary>
+    public partial class StepIndicatorControl : UserControl
+    {
+        private StepState _previousState;
+        private bool _clickedDuringMouseOver;
+
+        public StepIndicatorControl()
+        {
+            InitializeComponent();
+            this.UpdateVisualState(State);
+            this._clickedDuringMouseOver = false;
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+         DependencyProperty.Register("Title", typeof(string), typeof(StepIndicatorControl), new PropertyMetadata("Step"));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty StepNumberProperty =
+        DependencyProperty.Register("StepNumber", typeof(string), typeof(StepIndicatorControl),
+            new PropertyMetadata("1", OnStepNumberChanged));
+
+        public string StepNumber
+        {
+            get { return (string)GetValue(StepNumberProperty); }
+            set { SetValue(StepNumberProperty, value); }
+        }
+
+        private static void OnStepNumberChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (StepIndicatorControl)d;
+            control.StepNumberText.Text = (string)e.NewValue;
+        }
+
+        public enum StepState
+        {
+            Inactive,
+            Active,
+            Ok
+        }
+
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.Register("State", typeof(StepState), typeof(StepIndicatorControl),
+                new PropertyMetadata(StepState.Inactive, OnStateChanged));
+
+        public StepState State
+        {
+            get { return (StepState)GetValue(StateProperty); }
+            set { SetValue(StateProperty, value); }
+        }
+
+        private static void OnStateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (StepIndicatorControl)d;
+            var state = (StepState)e.NewValue;
+            control.UpdateVisualState(state);
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            _previousState = State; // Save the current state
+            VisualStateManager.GoToState(this, "MouseOver", true);
+        }
+
+        private void Ellipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (!_clickedDuringMouseOver)
+            {
+                UpdateVisualState(_previousState);
+            }
+            _clickedDuringMouseOver = false;
+        }
+
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _clickedDuringMouseOver = true;
+
+            if (State == StepState.Inactive)
+            {
+                State = StepState.Active; // Change to Active on click
+            }
+            else if (State == StepState.Active)
+            {
+                State = StepState.Ok; // Change to Ok on click
+            }
+
+            UpdateVisualState(State); // Update to the new state immediately
+        }
+
+        private void UpdateVisualState(StepState state)
+        {
+            switch (state)
+            {
+                case StepState.Active:
+                    StepNumberText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#38abdf"));
+                    StepNumberText.Visibility = Visibility.Visible;
+                    OkIcon.Visibility = Visibility.Collapsed;
+                    StepTitle.FontFamily = SharedDictionaryManager.DynamoModernDictionary["ArtifaktElementBold"] as FontFamily;
+                    break;
+                    
+                case StepState.Inactive:
+                    StepNumberText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#979797"));
+                    StepNumberText.Visibility = Visibility.Visible;
+                    OkIcon.Visibility = Visibility.Collapsed;
+                    StepTitle.FontFamily = SharedDictionaryManager.DynamoModernDictionary["ArtifaktElementRegular"] as FontFamily;
+                    break;
+
+                case StepState.Ok:  
+                    StepNumberText.Visibility = Visibility.Collapsed;
+                    OkIcon.Visibility = Visibility.Visible;
+                    StepTitle.FontFamily = SharedDictionaryManager.DynamoModernDictionary["ArtifaktElementRegular"] as FontFamily;
+                    break;
+
+            }
+
+            string stateName = state.ToString();
+            VisualStateManager.GoToState(this, stateName, true);
+        }
+    }
+}
