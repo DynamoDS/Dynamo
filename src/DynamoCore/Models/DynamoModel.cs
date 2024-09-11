ff3d73326507943a4a6597bed3dc19fb3f9ea13a
Aaron (Qilong) <173288704@qq.com>
2024-09-11 12:14:38 -0400

    DYN-7312 Introducing new properties to HostAnalyticsInfo (#15474)

--- src/DynamoCore/Models/DynamoModel.cs
+++ src/DynamoCore/Models/DynamoModel.cs
@@ -98,10 +98,14 @@ namespace Dynamo.Models
     /// </summary>
     public struct HostAnalyticsInfo
     {
-        // Dynamo variation identified by host.
+        // Dynamo variation identified by host, e.g. Dynamo Revit
         public string HostName;
         // Dynamo variation version specific to host
         public Version HostVersion;
+        // Dynamo host application name, e.g. Revit
+        public string HostProductName;
+        // Dynamo host application version, e.g. 2025.2.0
+        public Version HostProductVersion;
         // Dynamo host parent id for analytics purpose.
         public string ParentId;
         // Dynamo host session id for analytics purpose.
