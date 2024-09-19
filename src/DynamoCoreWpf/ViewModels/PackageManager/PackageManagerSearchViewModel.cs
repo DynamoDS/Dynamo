cef1ec2b9a3a280ce03988eca52038175677af7e
Aaron (Qilong) <173288704@qq.com>
2024-09-19 16:49:42 -0400

    Fix crash edge case about failure fetching hosts

--- src/DynamoCoreWpf/ViewModels/PackageManager/PackageManagerSearchViewModel.cs
+++ src/DynamoCoreWpf/ViewModels/PackageManager/PackageManagerSearchViewModel.cs
@@ -741,7 +741,7 @@ namespace Dynamo.PackageManager
             var hostFilter = new List<FilterEntry>();
             try
             {
-                foreach (var host in PackageManagerClientViewModel.Model.GetKnownHosts())
+                foreach (var host in PackageManagerClientViewModel.Model?.GetKnownHosts())
                 {
                     hostFilter.Add(new FilterEntry(host, Resources.PackageFilterByHost, Resources.PackageHostDependencyFilterContextItem, this));
                 }
