ccac4952d49a0d08e764bd388668ecbfeffe6ae1
Craig Long <craig.alan.long@gmail.com>
2024-11-05 13:38:50 -0500

    DYN-7728 Add Wild Card Syntax (#15591)
    Co-authored-by: Deyan Nenov <dnenov@archilizer.com>

--- test/DynamoCoreTests/VersionUtilitiesTests.cs
+++ test/DynamoCoreTests/VersionUtilitiesTests.cs
@@ -63,6 +63,20 @@ namespace Dynamo.Tests
             Assert.IsNull(result, "Expected null when input is an invalid version string.");
         }
 
+
+        [Test]
+        public void ParseVersionSafely_TooManyComponents_ReturnsNull()
+        {
+            // Arrange
+            string version = "1.2.3.4.5";
+
+            // Act
+            Version result = VersionUtilities.Parse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input has too many components '1.2.3.4.5'.");
+        }
+
         [Test]
         public void ParseVersionSafely_ShortVersion_ReturnsPaddedVersion()
         {
@@ -87,6 +101,161 @@ namespace Dynamo.Tests
 
             // Assert
             Assert.AreEqual(new Version(2019, 1, 2), result, "Expected version '2019.1.2' when input is '2019.1.2'.");
+        }
+
+        [Test]
+        public void ParseVersionSafely_LongVersion_ReturnsParsedVersion()
+        {
+            // Arrange
+            string version = "2019.1.2.0";
+
+            // Act
+            Version result = VersionUtilities.Parse(version);
+
+            // Assert
+            Assert.AreEqual(new Version(2019, 1, 2, 0), result, "Expected version '2019.1.2.0' when input is '2019.1.2.0'.");
+        }
+
+        [Test]
+        public void ParseWildCard_ValidVersion_ReturnsNull()
+        {
+            // Arrange
+            string version = "2019.1.2";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input does not end with a wildcard symbol.");
+        }
+
+        [Test]
+        public void ParseWildCard_InvalidWildcardVersion1_ReturnsNull()
+        {
+            // Arrange
+            string version = "2019.1.2.*";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input is an invalid version string.");
+        }
+
+        [Test]
+        public void ParseWildCard_InvalidWildcardVersion2_ReturnsNull()
+        {
+            // Arrange
+            string version = "2019.1*";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input is an invalid version string.");
+        }
+
+        [Test]
+        public void ParseWildCard_InvalidWildcardVersion3_ReturnsNull()
+        {
+            // Arrange
+            string version = "2019.*.1";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input is an invalid version string.");
+        }
+
+        [Test]
+        public void ParseWildCard_InvalidCharacters1_ReturnsNull()
+        {
+            // Arrange
+            string version = "a.*";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input is an invalid version string.");
+        }
+
+        [Test]
+        public void ParseWildCard_InvalidCharacters2_ReturnsNull()
+        {
+            // Arrange
+            string version = "a.1.*";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input is an invalid version string.");
+        }
+
+        [Test]
+        public void ParseWildCard_InvalidCharacters3_ReturnsNull()
+        {
+            // Arrange
+            string version = "2016.a.*";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input is an invalid version string.");
+        }
+
+        [Test]
+        public void ParseWildCard_ValidString1_ReturnsParsedVersion()
+        {
+            // Arrange
+            string version = "2019.1.*";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.AreEqual(new Version(2019, 1, 2147483647), result, "Expected version '2019.1.2147483647' when input is '2019.1.*'.");
+        }
+
+        [Test]
+        public void ParseWildCard_ValidString2_ReturnsParsedVersion()
+        {
+            // Arrange
+            string version = "2019.*";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.AreEqual(new Version(2019, 2147483647, 0), result, "Expected version '2019.2147483647.0' when input is '2019.*'.");
+        }
+
+        [Test]
+        public void ParseWildCard_TwoAndThreePartVersionConsistency_ReturnsParsedVersions()
+        {
+            // Arrange & Act
+            Version twoPartResult = VersionUtilities.WildCardParse("2021.*");
+            Version threePartResult = VersionUtilities.WildCardParse("2021.2.*");
+
+            // Assert
+            Assert.AreEqual(new Version(2021, 2147483647, 0), twoPartResult, "Expected version '2021.2147483647.0' for input '2021.*'.");
+            Assert.AreEqual(new Version(2021, 2, 2147483647), threePartResult, "Expected version '2021.2.2147483647' for input '2021.2.*'.");
+        }
+
+        [Test]
+        public void ParseWildCard_TooManyComponents_ReturnsNull()
+        {
+            // Arrange
+            string version = "1.2.3.4.*";
+
+            // Act
+            Version result = VersionUtilities.WildCardParse(version);
+
+            // Assert
+            Assert.IsNull(result, "Expected null when input has too many components '1.2.3.4.*'.");
         }
 
     }
