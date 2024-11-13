59308e239e5f6bf2bb829b665b847f5d8fba7a67
Aparajit Pratap <aparajit.pratap@autodesk.com>
2024-11-13 16:20:56 -0500

    add comments

--- test/Engine/TestCodeBlockNodeSecurityIssue/TestCodeBlockNodeSecurityIssue.cs
+++ test/Engine/TestCodeBlockNodeSecurityIssue/TestCodeBlockNodeSecurityIssue.cs
@@ -1,12 +1,13 @@
 namespace TestCodeBlockNodeSecurityIssue
 {
     /// <summary>
-    /// This class is used in
+    /// This project assembly is used in
     /// Dynamo.Tests.CodeBlockNodeTests.ImportStatementInCodeBlock_DoesNotLoadAssemblyIntoProcess
     /// to test a security issue in CodeBlockNodes where an import statement
     /// would unexpectedly load the assembly into the process.
     /// This should be prevented as import statements are not permitted in CBNs
     /// and therefore should not be loaded into the process in the first place.
+    /// NOTE: This assembly shouldn't be used in any other tests.
     /// </summary>
     public class TestCodeBlockNodeSecurityIssue
     {
