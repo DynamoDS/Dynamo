a83946e1966d946a28c3aa41a3a4992c98035622
pinzart90 <tiberiu.pinzariu@gmail.com>
2024-12-31 23:44:25 -0500

    update

--- test/DynamoCoreTests/Logging/AnalyticsServiceTest.cs
+++ test/DynamoCoreTests/Logging/AnalyticsServiceTest.cs
@@ -2,13 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Microsoft.Diagnostics.Runtime;
 using NUnit.Framework;
-using System.Runtime.Versioning;
 
 namespace Dynamo.Tests.Loggings
 {
@@ -41,42 +38,29 @@ namespace Dynamo.Tests.Loggings
             {
                 DynamoShapeManager.Utilities.GetInstalledAsmVersion2(versions, ref locatedPath, coreDirectory);
             }
+
             try
             {
-                Assert.DoesNotThrow(() =>
+                dynamoCLI = Process.Start(new ProcessStartInfo(Path.Combine(coreDirectory, "DynamoCLI.exe"), $"--GeometryPath \"{locatedPath}\" -k --DisableAnalytics -o \"{openPath}\" ") { UseShellExecute = true });
+
+                Thread.Sleep(5000);// Wait 5 seconds to open the dyn
+                Assert.IsFalse(dynamoCLI.HasExited);
+
+                var firstASMmodulePath = string.Empty;
+                foreach (ProcessModule module in dynamoCLI.Modules)
                 {
-
-                    dynamoCLI = Process.Start(new ProcessStartInfo(Path.Combine(coreDirectory, "DynamoCLI.exe"), $"--GeometryPath \"{locatedPath}\" -k --DisableAnalytics -o \"{openPath}\" ") { UseShellExecute = true });
-
-                    Thread.Sleep(5000);// Wait 5 seconds to open the dyn
-                    Assert.IsFalse(dynamoCLI.HasExited);
-                    var dt = DataTarget.AttachToProcess(dynamoCLI.Id, false);
-                    var assemblies = dt
-                          .ClrVersions
-                          .Select(dtClrVersion => dtClrVersion.CreateRuntime())
-                          .SelectMany(runtime => runtime.AppDomains.SelectMany(runtimeAppDomain => runtimeAppDomain.Modules))
-                          .Select(clrModule => clrModule.AssemblyName)
-                          .Distinct()
-                          .Where(x => x != null)
-                          .ToList();
-
-                    var firstASMmodulePath = string.Empty;
-                    foreach (string module in assemblies)
+                    if (module.ModuleName.IndexOf("Analytics.dll", StringComparison.OrdinalIgnoreCase) != -1)
                     {
-                        if (module.IndexOf("Analytics", StringComparison.OrdinalIgnoreCase) != -1)
-                        {
-                            Assert.Fail("Analytics module was loaded");
-                        }
-                        if (module.IndexOf("AdpSDKCSharpWrapper", StringComparison.OrdinalIgnoreCase) != -1)
-                        {
-                            Assert.Fail("ADP module was loaded");
-                        }
+                        Assert.Fail("Analytics module was loaded");
                     }
-                });
+                    if (module.ModuleName.IndexOf("AdpSDKCSharpWrapper.dll", StringComparison.OrdinalIgnoreCase) != -1)
+                    {
+                        Assert.Fail("ADP module was loaded");
+                    }
+                }
             }
             finally
             {
-
                 dynamoCLI?.Kill();
             }
         }
@@ -116,25 +100,17 @@ namespace Dynamo.Tests.Loggings
                     dynamoCLI = Process.Start(new ProcessStartInfo(Path.Combine(coreDirectory, "DynamoCLI.exe"), $"--GeometryPath \"{locatedPath}\" -k --NoNetworkMode -o \"{openPath}\" ") { UseShellExecute = true });
 
                     Thread.Sleep(5000);// Wait 5 seconds to open the dyn
+
                     Assert.IsFalse(dynamoCLI.HasExited);
-                    var dt = DataTarget.AttachToProcess(dynamoCLI.Id, false);
-                    var assemblies = dt
-                          .ClrVersions
-                          .Select(dtClrVersion => dtClrVersion.CreateRuntime())
-                          .SelectMany(runtime => runtime.AppDomains.SelectMany(runtimeAppDomain => runtimeAppDomain.Modules))
-                          .Select(clrModule => clrModule.AssemblyName)
-                          .Distinct()
-                          .Where(x => x != null)
-                          .ToList();
 
                     var firstASMmodulePath = string.Empty;
-                    foreach (string module in assemblies)
+                    foreach (ProcessModule module in dynamoCLI.Modules)
                     {
-                        if (module.IndexOf("Analytics", StringComparison.OrdinalIgnoreCase) != -1)
+                        if (module.ModuleName.IndexOf("Analytics.dll", StringComparison.OrdinalIgnoreCase) != -1)
                         {
                             Assert.Fail("Analytics module was loaded");
                         }
-                        if (module.IndexOf("AdpSDKCSharpWrapper", StringComparison.OrdinalIgnoreCase) != -1)
+                        if (module.ModuleName.IndexOf("AdpSDKCSharpWrapper.dll", StringComparison.OrdinalIgnoreCase) != -1)
                         {
                             Assert.Fail("ADP module was loaded");
                         }
