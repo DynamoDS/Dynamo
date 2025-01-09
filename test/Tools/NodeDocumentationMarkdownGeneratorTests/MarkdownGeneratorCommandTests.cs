c766c7e523e93c629f2c1a0bfda0948b9aafaa54
Roberto T <roberto.tellez@globant.com>
2025-01-09 17:53:43 -0600

    DYN-7963 Obsolote Excel Interop Node - Regression
    2 commits were reverted (they were used to get more info about why the test was failing).
    
    In the  ProducesCorrectOutputFromCoreDirectory_preloadedbinaries() test the generated md files were reduced from 756 to 754 due that 2 excel nodes were obsoleted with a warning message so the md files are not generated for those 2 nodes.

--- test/Tools/NodeDocumentationMarkdownGeneratorTests/MarkdownGeneratorCommandTests.cs
+++ test/Tools/NodeDocumentationMarkdownGeneratorTests/MarkdownGeneratorCommandTests.cs
@@ -164,7 +164,7 @@ namespace NodeDocumentationMarkdownGeneratorTests
             FromDirectoryCommand.HandleDocumentationFromDirectory(opts);
 
             var generatedFileNames = tempDirectory.GetFiles().Select(x => x.Name);
-            Assert.AreEqual(756, generatedFileNames.Count());
+            Assert.AreEqual(754, generatedFileNames.Count());
         }
 
         [Test]
