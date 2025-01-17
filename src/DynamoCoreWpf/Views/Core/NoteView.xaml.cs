75f37f1f3c2e84edfb6797fd9d17458c057f1d7f
john pierson <sixtysecondrevit@gmail.com>
2025-01-17 08:51:07 -0700

    Allow for "-" on numpad to add bullets to notes

--- src/DynamoCoreWpf/Views/Core/NoteView.xaml.cs
+++ src/DynamoCoreWpf/Views/Core/NoteView.xaml.cs
@@ -23,7 +23,7 @@ namespace Dynamo.Nodes
         /// <summary>
         /// Special keys definition in note
         /// </summary>
-        internal Key[] specialKeys = { Key.OemMinus, Key.Tab, Key.Enter };
+        internal Key[] specialKeys = { Key.OemMinus, Key.Subtract, Key.Tab, Key.Enter };
 
         public NoteViewModel ViewModel { get; private set; }
 
@@ -230,6 +230,9 @@ namespace Dynamo.Nodes
                     switch (e.Key)
                     {
                         case Key.OemMinus:
+                            textBox.Text = BulletDashHandler(text, caretIndex);
+                            break;
+                        case Key.Subtract:
                             textBox.Text = BulletDashHandler(text, caretIndex);
                             break;
                         case Key.Tab:
