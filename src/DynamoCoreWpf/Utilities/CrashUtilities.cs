d1c75927c3c2237402add3e0561dfca2f6a577e4
john pierson <sixtysecondrevit@gmail.com>
2025-01-23 04:46:16 -0700

    Revert back to crash report for the crash handler

--- src/DynamoCoreWpf/Utilities/CrashUtilities.cs
+++ src/DynamoCoreWpf/Utilities/CrashUtilities.cs
@@ -12,7 +12,7 @@ namespace Dynamo.Wpf.Utilities
         {
             var baseUri = new UriBuilder(Configurations.GitHubBugReportingLink);
 
-            var issueTitle = "Issue report from Dynamo {0}";
+            var issueTitle = "Crash report from Dynamo {0}";
             var dynamoVersion = AssemblyHelper.GetDynamoVersion().ToString() ?? "2.1.0+";
 
             // append the title and body to the URL as query parameters
