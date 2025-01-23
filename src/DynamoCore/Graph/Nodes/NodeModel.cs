fdd147a77a25d25b5258efb47ce3b8079e056e4c
DimitarVen <dimitar.venkov@autodesk.com>
2025-01-23 23:34:21 +0200

    use protected modifier

--- src/DynamoCore/Graph/Nodes/NodeModel.cs
+++ src/DynamoCore/Graph/Nodes/NodeModel.cs
@@ -1224,7 +1224,7 @@ namespace Dynamo.Graph.Nodes
         }
 
 
-        internal AssemblyName NameOfAssemblyReferencedByNode = null;
+        protected AssemblyName NameOfAssemblyReferencedByNode = null;
 
         /// <summary>
         /// The method returns the assembly name from which the node originated.
