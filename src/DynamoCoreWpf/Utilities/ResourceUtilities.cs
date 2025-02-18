81580faaf5d12d7e7bbd03b50fce3f611241a3c3
Roberto T <roberto.tellez@globant.com>
2025-02-18 08:40:15 -0600

    DYN-8275 HTML Localization
    - I've copied the html used for guided tours to Dynamo\doc\distrib\html\en-US
    -I've modified the DynamoCoreWpf for copying the html files from \Dynamo\doc\distrib\html\.. to Dynamo\src\DynamoCoreWpf\Views\GuidedTour\HtmlPages.
    -I've modified the DynamoCoreWpf.csproj for including all the html files as embedded resources.
    -Finally I've modified the ResourceUtilities.LoadContentFromResources(), ResourceUtilities.LoadWebBrowser() methods for passing the locale and the method in PopupWindow.xaml.cs for passing the expected language.

--- src/DynamoCoreWpf/Utilities/ResourceUtilities.cs
+++ src/DynamoCoreWpf/Utilities/ResourceUtilities.cs
@@ -250,7 +250,7 @@ namespace Dynamo.Utilities
             return imageSource;
         }
 
-        internal static string LoadContentFromResources(string name, Assembly localAssembly = null, bool injectDPI = true, bool removeScriptTags = true)
+        internal static string LoadContentFromResources(string name, Assembly localAssembly = null, bool injectDPI = true, bool removeScriptTags = true, string locale = "")
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentNullException(nameof(name));
@@ -288,6 +288,12 @@ namespace Dynamo.Utilities
 
             var matchingResource = availableResources
                 .FirstOrDefault(str => str.EndsWith(name));
+
+            if(!string.IsNullOrEmpty(locale))
+            {
+                matchingResource = availableResources
+                .FirstOrDefault(str => str.EndsWith(name) && str.Contains(locale.Replace('-','_')));
+            }
 
             if (string.IsNullOrEmpty(matchingResource))
             {
@@ -585,11 +591,11 @@ namespace Dynamo.Utilities
         /// <param name="fontStylePath">Path to the Font Style that will be used in some part of the HTML page</param>
         /// <param name="localAssembly">Local Assembly in which the resource will be loaded</param>
         /// <param name="userDataFolder">the folder that WebView2 will use for storing cache info</param>
-        internal static async void LoadWebBrowser(HtmlPage htmlPage, DynamoWebView2 webBrowserComponent, string resourcesPath, string fontStylePath, Assembly localAssembly, string userDataFolder = default(string))
+        internal static async void LoadWebBrowser(HtmlPage htmlPage, DynamoWebView2 webBrowserComponent, string resourcesPath, string fontStylePath, Assembly localAssembly, string userDataFolder = default(string), string locale = "")
         {
             try
             {
-                var bodyHtmlPage = ResourceUtilities.LoadContentFromResources(htmlPage.FileName, localAssembly, false, false);
+                var bodyHtmlPage = ResourceUtilities.LoadContentFromResources(htmlPage.FileName, localAssembly, false, false, locale);
 
                 bodyHtmlPage = LoadResouces(bodyHtmlPage, htmlPage.Resources, resourcesPath);
                 bodyHtmlPage = LoadResourceAndReplaceByKey(bodyHtmlPage, "#fontStyle", fontStylePath);
