1d7bcea5d930a51f6fe210b9d2f1b859489dc8f6
john pierson <sixtysecondrevit@gmail.com>
2025-03-31 08:33:19 -0600

    Update preference tests

--- test/DynamoCoreTests/Configuration/PreferenceSettingsTests.cs
+++ test/DynamoCoreTests/Configuration/PreferenceSettingsTests.cs
@@ -81,7 +81,7 @@ namespace Dynamo.Tests.Configuration
             Assert.AreEqual(settings.GetIsBackgroundPreviewActive("MyBackgroundPreview"), true);
             Assert.AreEqual(settings.ShowCodeBlockLineNumber, true);
             Assert.AreEqual(settings.ShowTabsAndSpacesInScriptEditor, false);
-            Assert.AreEqual(settings.EnableNodeAutoComplete, true);
+            Assert.AreEqual(settings.EnableNodeAutoComplete, false);
             Assert.AreEqual(settings.EnableNotificationCenter, true);
             Assert.AreEqual(settings.DefaultPythonEngine, string.Empty);
             Assert.AreEqual(settings.MaxNumRecentFiles, PreferenceSettings.DefaultMaxNumRecentFiles);
