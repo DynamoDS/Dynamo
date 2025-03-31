9103ec2b522c5576a5af63e86ae30e6930c35202
john pierson <sixtysecondrevit@gmail.com>
2025-03-31 10:31:33 -0600

    Update NodeViewCustomizationTests.cs

--- test/DynamoCoreWpf3Tests/NodeViewCustomizationTests.cs
+++ test/DynamoCoreWpf3Tests/NodeViewCustomizationTests.cs
@@ -311,15 +311,7 @@ namespace DynamoCoreWpfTests
             // As of Nov 2024, we have 8 images per NodeView
             // Images are named for ease of use
             // As of Dynamo 3.5, the number of images in a node view is 9 after the addition of the TransientImage
-            // With Dynamo 3.6, we have the introduction of the Node AutoComplete Marker.
-            // This contains an image on the input and output ports that appears on hover.
-            // Resulting in a total of 11 images in a watch node view as of 3.6
-            Assert.AreEqual(11, imgs.Count());
-
-            var img = imgs.First(x => x.Name == "DotsImage");
-
-            Assert.Greater(img.ActualWidth, 10);
-            Assert.Greater(img.ActualHeight, 10);
+            Assert.AreEqual(9, imgs.Count());
         }
 
         [Test]
