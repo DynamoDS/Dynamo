393fa2a6bd89b1d862431065d3c9c78f1e85a2e1
Ashish Aggarwal <ashish.aggarwal@autodesk.com>
2025-05-01 17:00:14 -0400

    DYN-8305: Implement Bitmap Cache for nodes when zoomed out (#16174)
    Co-authored-by: DimitarVen <dimitar.venkov@autodesk.com>

--- src/DynamoUtilities/DynamoFeatureFlagsManager.cs
+++ src/DynamoUtilities/DynamoFeatureFlagsManager.cs
@@ -88,13 +88,13 @@ namespace DynamoUtilities
         /// <summary>
         /// Check feature flag value, if it does not exist, return the defaultval.
         /// </summary>
-        /// <typeparam name="T">Must be a bool or string, only bool or string flags should be created unless this implementation is improved.</typeparam>
+        /// <typeparam name="T">Must be a bool, string or double, flags of only these types should be created unless this implementation is improved.</typeparam>
         /// <param name="featureFlagKey">feature flag name</param>
-        /// <param name="defaultval">Currently the flag and default val MUST be a bool, or long(int64).</param>
+        /// <param name="defaultval">Currently the flag and default val MUST be a bool, or long(int64) or double.</param>
         /// <returns></returns>
         internal T CheckFeatureFlag<T>(string featureFlagKey, T defaultval)
         {
-            if(!(defaultval is bool || defaultval is string || defaultval is long)){
+            if(!(defaultval is bool || defaultval is string || defaultval is long || defaultval is double)){
                 throw new ArgumentException("unsupported flag type", defaultval.GetType().ToString());
             }
             // if we have not retrieved flags from the cli return empty
