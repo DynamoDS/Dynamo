f2a469f9f5b7c20ccb76b5774a99994f9a2d4a4d
john pierson <sixtysecondrevit@gmail.com>
2025-06-10 11:36:38 -0600

    Add ? tooltip to preferences view for new ui

--- src/DynamoCoreWpf/Properties/Resources.Designer.cs
+++ src/DynamoCoreWpf/Properties/Resources.Designer.cs
@@ -8028,11 +8028,20 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Enable New Node Autocomplete UI.
+        ///   Looks up a localized string similar to Enable New UI.
         /// </summary>
         public static string PreferencesViewEnableNodeAutoCompleteNewUI {
             get {
                 return ResourceManager.GetString("PreferencesViewEnableNodeAutoCompleteNewUI", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Enable the new floating menu for node suggestions for a more streamlined workflow. Turn off to return to the traditional pop-up dialog..
+        /// </summary>
+        public static string PreferencesViewEnableNodeAutoCompleteNewUITooltipText {
+            get {
+                return ResourceManager.GetString("PreferencesViewEnableNodeAutoCompleteNewUITooltipText", resourceCulture);
             }
         }
         
