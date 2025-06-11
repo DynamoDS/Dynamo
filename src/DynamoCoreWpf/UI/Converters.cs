71846e9550c96cc6969ccaeb053d26f7ad29e01d
Bogdan Zavu <BogdanZavu@users.noreply.github.com>
2025-06-11 09:57:15 -0400

    DYN-9999 : dna filtering improvements (#16281)
    Co-authored-by: Bogdan Zavu <bogdan.zavu@autodesk.com>

--- src/DynamoCoreWpf/UI/Converters.cs
+++ src/DynamoCoreWpf/UI/Converters.cs
@@ -4310,4 +4310,20 @@ namespace Dynamo.Controls
             return Binding.DoNothing;
         }
     }
+
+    public class LengthToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int length)
+                return length > 0 ? Visibility.Collapsed : Visibility.Visible;
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
