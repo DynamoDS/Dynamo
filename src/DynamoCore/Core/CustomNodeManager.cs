1e6f5d977e8e97a3133cf91a083d6a6015625c45
Aparajit Pratap <aparajit.pratap@autodesk.com>
2025-07-09 10:58:16 -0400

    sort usings

--- src/DynamoCore/Core/CustomNodeManager.cs
+++ src/DynamoCore/Core/CustomNodeManager.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
 using Dynamo.Engine;
 using Dynamo.Engine.NodeToCode;
 using Dynamo.Exceptions;
@@ -19,18 +28,7 @@ using Dynamo.Properties;
 using Dynamo.Selection;
 using Dynamo.Utilities;
 using ProtoCore.AST.AssociativeAST;
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
-using System.Xml;
 using Symbol = Dynamo.Graph.Nodes.CustomNodes.Symbol;
-using System.Threading.Tasks;
-using Autodesk.DesignScript.Geometry;
-using System.Security.Policy;
 
 namespace Dynamo.Core
 {
