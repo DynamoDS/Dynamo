15a6aa698299bed3e5f37dfcd25707c785bb9f44
Ivo Petrov <ivaylo.matov@gmail.com>
2025-09-24 07:43:06 +0100

    dscpython commented out

--- src/Libraries/DSCPython/Encoders/DictionaryDecoder.cs
+++ src/Libraries/DSCPython/Encoders/DictionaryDecoder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,42 +7,42 @@ using Python.Runtime;
 
 namespace DSCPython.Encoders
 {
-    internal class DictionaryDecoder : IPyObjectDecoder
+    internal class DictionaryDecoder //: IPyObjectDecoder
     {
-        private static readonly Type[] decodableTypes = new Type[]
-        {
-            typeof(IDictionary<,>), typeof(Dictionary<,>),
-            typeof(IDictionary), typeof(Hashtable)
-        };
+        //private static readonly Type[] decodableTypes = new Type[]
+        //{
+        //    typeof(IDictionary<,>), typeof(Dictionary<,>),
+        //    typeof(IDictionary), typeof(Hashtable)
+        //};
 
-        public bool CanDecode(PyObject objectType, Type targetType)
-        {
-            if (targetType.IsGenericType)
-            {
-                targetType = targetType.GetGenericTypeDefinition();
-            }
-            return decodableTypes.IndexOf(targetType) >= 0;
-        }
+        //public bool CanDecode(PyObject objectType, Type targetType)
+        //{
+        //    if (targetType.IsGenericType)
+        //    {
+        //        targetType = targetType.GetGenericTypeDefinition();
+        //    }
+        //    return decodableTypes.IndexOf(targetType) >= 0;
+        //}
 
-        public bool TryDecode<T>(PyObject pyObj, out T value)
-        {
-            if (!PyDict.IsDictType(pyObj))
-            {
-                value = default;
-                return false;
-            }
-            using (var pyDict = new PyDict(pyObj))
-            {
-                if (typeof(T).IsGenericType)
-                {
-                    value = pyDict.ToDictionary<T>();
-                }
-                else
-                {
-                    value = (T)pyDict.ToDictionary();
-                }
-                return true;
-            }
-        }
+        //public bool TryDecode<T>(PyObject pyObj, out T value)
+        //{
+        //    if (!PyDict.IsDictType(pyObj))
+        //    {
+        //        value = default;
+        //        return false;
+        //    }
+        //    using (var pyDict = new PyDict(pyObj))
+        //    {
+        //        if (typeof(T).IsGenericType)
+        //        {
+        //            value = pyDict.ToDictionary<T>();
+        //        }
+        //        else
+        //        {
+        //            value = (T)pyDict.ToDictionary();
+        //        }
+        //        return true;
+        //    }
+        //}
     }
 }
