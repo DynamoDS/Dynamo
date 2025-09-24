9865f6ec032b03b98ca466ebe9955f4df3f40d37
Ivo Petrov <ivaylo.matov@gmail.com>
2025-09-24 07:34:40 +0100

    Revert "dscpython removed"
    This reverts commit bcd5ab85ff00aec8a3f5a14d088e6ac65900b029.

---
+++ src/Libraries/DSCPython/Encoders/DictionaryDecoder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Dynamo.Utilities;
+using Python.Runtime;
+
+namespace DSCPython.Encoders
+{
+    internal class DictionaryDecoder : IPyObjectDecoder
+    {
+        private static readonly Type[] decodableTypes = new Type[]
+        {
+            typeof(IDictionary<,>), typeof(Dictionary<,>),
+            typeof(IDictionary), typeof(Hashtable)
+        };
+
+        public bool CanDecode(PyObject objectType, Type targetType)
+        {
+            if (targetType.IsGenericType)
+            {
+                targetType = targetType.GetGenericTypeDefinition();
+            }
+            return decodableTypes.IndexOf(targetType) >= 0;
+        }
+
+        public bool TryDecode<T>(PyObject pyObj, out T value)
+        {
+            if (!PyDict.IsDictType(pyObj))
+            {
+                value = default;
+                return false;
+            }
+            using (var pyDict = new PyDict(pyObj))
+            {
+                if (typeof(T).IsGenericType)
+                {
+                    value = pyDict.ToDictionary<T>();
+                }
+                else
+                {
+                    value = (T)pyDict.ToDictionary();
+                }
+                return true;
+            }
+        }
+    }
+}
