b9de26702c264e79caf9b8aed31a22d8df7bd1ed
gohb <beng-lin.goh@autodesk.com>
2025-09-26 17:20:31 +0800

    Added 'DynamoUnits' to 'InternalsVisibleTo'

--- src/Tools/DynamoInstallDetective/Properties/AssemblyInfo.cs
+++ src/Tools/DynamoInstallDetective/Properties/AssemblyInfo.cs
@@ -9,3 +9,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("DynamoUtilitiesTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("DynamoUnits")]
