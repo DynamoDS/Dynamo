73198989975507e35f53dbe8bd92c3967a063e97
Ivo Petrov <ivaylo.matov@gmail.com>
2025-09-30 19:21:53 +0100

    Update check_file_version.ps1

--- .github/scripts/check_file_version.ps1
+++ .github/scripts/check_file_version.ps1
@@ -72,7 +72,7 @@ $excludedFiles = @(
     "DSPythonNet3.dll",
     "DSPythonNet3.resources.dll",
     "DSPythonNet3Extension.dll",
-    "DSPythonNet3Wheels.dll",
+    "DSPythonNet3Wheels.dll"
 )
 $noVersion = @()
 $wrongVersion = @()
