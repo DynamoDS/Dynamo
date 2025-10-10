be666e18656de6ef91ecbebdd0ecb0d993d51522
chubakueno <chubakueno@gmail.com>
2025-10-10 08:56:08 -0500

    DYN-9211: DNA inside a group should add the node in the group (#16577)

--- src/NodeAutoCompleteViewExtension/ViewModels/NodeAutoCompleteBarViewModel.cs
+++ src/NodeAutoCompleteViewExtension/ViewModels/NodeAutoCompleteBarViewModel.cs
@@ -324,6 +324,15 @@ namespace Dynamo.NodeAutoComplete.ViewModels
 
             NodeAutoCompleteUtilities.PostAutoLayoutNodes(dynamoViewModel, node.WorkspaceViewModel.Model, node.NodeModel, transientNodes.Select(x => x.NodeModel), true, true, PortViewModel.PortType, null);
 
+            var parentGroup = node.WorkspaceViewModel.Annotations.FirstOrDefault(x=>x.Nodes.Any(x=>x.GUID==node.NodeModel.GUID));
+            if(parentGroup != null)
+            {
+                foreach(var newNode in transientNodes.Select(x => x.NodeModel))
+                {
+                    parentGroup.AnnotationModel.AddToTargetAnnotationModel(newNode);
+                }
+            }
+
             if (PortViewModel.PortType == PortType.Input)
             {
                 node.NodeModel.MarkNodeAsModified();
