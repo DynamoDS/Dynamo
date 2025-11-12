ca58c96d63c490be286a07f03317bfaeaa6ba488
Ivo Petrov <ivaylo.matov@gmail.com>
2025-11-12 17:10:25 +0000

    Revert "toast timer now set to 5 sec"
    This reverts commit 8246b14bc4d00ed68510d386f05bc43a3515ca6c.

--- src/DynamoCoreWpf/UI/ToastManager.cs
+++ src/DynamoCoreWpf/UI/ToastManager.cs
@@ -15,7 +15,7 @@ namespace Dynamo.Wpf.UI
         private const double VerticalOffset = 10;
         //The HorizontalOffset is used to move horizontally the popup (using as a reference the statusBarPanel position)
         private const double HorizontalOffset = 110;
-        internal const int AutoCloseSeconds = 5;
+        internal const int AutoCloseSeconds = 3;
 
         private RealTimeInfoWindow toastPopup;
         private UIElement mainRootElement;
