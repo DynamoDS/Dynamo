31cf7929c0312328b60262051d16225fce542349
gohb <beng-lin.goh@autodesk.com>
2025-11-27 08:11:56 -0800

    Rename interface to 'IWebComponentEntryPoint'

--- src/DynamoCoreWpf/ViewModels/ProxyServer/IWebComponentEntryPoint.cs
+++ src/DynamoCoreWpf/ViewModels/ProxyServer/IWebComponentEntryPoint.cs
@@ -5,24 +5,24 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Dynamo.Wpf.ViewModels.ProxyServer
 {
     /// <summary>
-    /// Entry point interface for view DLLs to register controllers, services,
+    /// Entry point interface for web component DLLs to register controllers, services,
     /// and static files with the proxy server.
     /// </summary>
-    public interface IWebServiceEntryPoint
+    public interface IWebComponentEntryPoint
     {
         /// <summary>
-        /// Gets the assembly containing controllers for this view.
+        /// Gets the assembly containing controllers for this web component.
         /// </summary>
         Assembly Assembly { get; }
 
         /// <summary>
-        /// Registers services for this view with the dependency injection container.
+        /// Registers services for this web component with the dependency injection container.
         /// </summary>
         /// <param name="initializationParams">The initialization parameters.</param>
         void Initialize(InitializationParams initializationParams);
 
         /// <summary>
-        /// Configures static file serving for this view's web assets.
+        /// Configures static file serving for this web component's web assets.
         /// </summary>
         /// <param name="app">The web application to configure static files for.</param>
         /// <param name="wwwrootPath">The base path to the wwwroot directory containing
