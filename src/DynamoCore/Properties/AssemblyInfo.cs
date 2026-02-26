3410ac07d2d0bc486455d0ea385c81604f493fcf
Aparajit Pratap <aparajit.pratap@autodesk.com>
2026-02-26 13:33:44 -0500

    unrelated cleanup

--- src/DynamoCore/Properties/AssemblyInfo.cs
+++ src/DynamoCore/Properties/AssemblyInfo.cs
@@ -63,9 +63,6 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("MCPViewExtension")]
 [assembly: InternalsVisibleTo("MCPServer")]
 
-// Disable PublicAPIAnalyzer errors for this type as they're already added to the public API text file
-#pragma warning disable RS0016
 [assembly: TypeForwardedTo(typeof(Dynamo.Scheduler.Disposable))]
 // NodeCategoryAttribute moved to DynamoServices so libraries that reference only DynamoServices (e.g. LibG) can use it.
 [assembly: TypeForwardedTo(typeof(Dynamo.Graph.Nodes.NodeCategoryAttribute))]
-#pragma warning restore RS0016
