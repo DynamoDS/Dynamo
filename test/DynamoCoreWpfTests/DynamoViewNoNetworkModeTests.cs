6cd46862d2260931fd127caf653b0c6eee8333be
Edwin Vasquez <edwin.vasquez@autodesk.com>
2026-06-05 11:05:53 -0500

    Including more scenarios for unit testing

--- test/DynamoCoreWpfTests/DynamoViewNoNetworkModeTests.cs
+++ test/DynamoCoreWpfTests/DynamoViewNoNetworkModeTests.cs
@@ -1,6 +1,8 @@
 using Dynamo.Controls;
 using Dynamo.Models;
+using Dynamo.ViewModels;
 using NUnit.Framework;
+using TestServices;
 
 namespace DynamoCoreWpfTests
 {
@@ -38,6 +40,65 @@ namespace DynamoCoreWpfTests
                 "added");
 
             Assert.IsTrue(shouldDisable);
+        }
+
+        [Test]
+        public void AutodeskAssistantExtensionIsNotDisabledWhenNoNetworkModeIsFalse()
+        {
+            var pathResolver = new TestPathResolver();
+            DynamoModel modelWithoutNoNetworkMode = null;
+            DynamoViewModel viewModelWithoutNoNetworkMode = null;
+            DynamoView viewWithoutNoNetworkMode = null;
+
+            try
+            {
+                modelWithoutNoNetworkMode = DynamoModel.Start(new DynamoModel.DefaultStartConfiguration()
+                {
+                    PathResolver = pathResolver,
+                    StartInTestMode = true,
+                    GeometryFactoryPath = preloader.GeometryFactoryPath,
+                    ProcessMode = Dynamo.Scheduler.TaskProcessMode.Synchronous,
+                    NoNetworkMode = false
+                });
+
+                viewModelWithoutNoNetworkMode = DynamoViewModel.Start(new DynamoViewModel.StartConfiguration()
+                {
+                    DynamoModel = modelWithoutNoNetworkMode
+                });
+
+                viewWithoutNoNetworkMode = new DynamoView(viewModelWithoutNoNetworkMode);
+
+                var shouldDisable = viewWithoutNoNetworkMode.DisableExtensionWhenNoNetworkMode(
+                    DynamoView.AutodeskAssistantExtensionId,
+                    "Autodesk Assistant",
+                    "added");
+
+                Assert.IsFalse(shouldDisable);
+            }
+            finally
+            {
+                if (viewWithoutNoNetworkMode != null && viewWithoutNoNetworkMode.IsLoaded)
+                {
+                    viewWithoutNoNetworkMode.Close();
+                }
+
+                if (viewModelWithoutNoNetworkMode != null)
+                {
+                    var shutdownParams = new DynamoViewModel.ShutdownParams(shutdownHost: false, allowCancellation: false);
+                    viewModelWithoutNoNetworkMode.PerformShutdownSequence(shutdownParams);
+                }
+            }
+        }
+
+        [Test]
+        public void UnrecognizedExtensionIsNotDisabledWhenNoNetworkModeIsEnabled()
+        {
+            var shouldDisable = View.DisableExtensionWhenNoNetworkMode(
+                "11111111-1111-1111-1111-111111111111",
+                "Some Other Extension",
+                "added");
+
+            Assert.IsFalse(shouldDisable);
         }
     }
 }
