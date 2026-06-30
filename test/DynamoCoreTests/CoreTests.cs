c9cdfef90426d67789bae87a93808ef6cb422824
Chloepeg <cpegeot@archilizer.com>
2026-06-30 11:13:23 +0100

    vararg port type lookup for string nodes
    Ensures ZeroTouch vararg nodes like String.Concat and String.Join report input and output port types correctly in the documentation browser by resolving types through DSFunctionBase and mapping expanded variadic ports back to the params parameter.
    Added a regression test for DSVarArgFunction port type reporting, covering String.Concat, String.Join, and an added variadic input port.

--- test/DynamoCoreTests/CoreTests.cs
+++ test/DynamoCoreTests/CoreTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -703,6 +703,24 @@ namespace Dynamo.Tests
             {
                 CurrentDynamoModel.Paste(); // Nope, paste should not crash Dynamo.
             });
+        }
+
+        [Test]
+        [Category("UnitTests")]
+        public void DSVarArgFunctionPortsReportInputAndOutputTypes()
+        {
+            var concatNode = new DSVarArgFunction(CurrentDynamoModel.LibraryServices.GetFunctionDescriptor("DSCore.String.Concat@string[]"));
+
+            Assert.AreEqual("string[]", concatNode.InPorts[0].GetInputPortType());
+            Assert.AreEqual("string", concatNode.OutPorts[0].GetOutPortType());
+
+            var joinNode = new DSVarArgFunction(CurrentDynamoModel.LibraryServices.GetFunctionDescriptor("DSCore.String.Join@string,string[]"));
+            joinNode.VarInputController.AddInputToModel();
+
+            Assert.AreEqual("string", joinNode.InPorts[0].GetInputPortType());
+            Assert.AreEqual("string[]", joinNode.InPorts[1].GetInputPortType());
+            Assert.AreEqual("string[]", joinNode.InPorts[2].GetInputPortType());
+            Assert.AreEqual("string", joinNode.OutPorts[0].GetOutPortType());
         }
 
         /// <summary>
