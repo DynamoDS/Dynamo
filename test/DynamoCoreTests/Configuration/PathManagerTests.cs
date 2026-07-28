DynamoDS/Dynamo

commit ffbc1812e6b638e91cc14ebecd4aa633f029d817

Open Dynamo Template as new workspace (#14871)
* Open Template UI change

* Add more functions for template opening

* add comments

* reuse open function

* Open Template with FileName as empty

* Give user notification

* Add message confirmation

* Unit test

* Unit test

* comments

* comments

* reuse ShowOpenDialogAndOpenResult code

* clean up

src/DynamoCore/Configuration/PathManager.cs
@@ -66,6 +66,7 @@ namespace Dynamo.Core
         public const string ViewExtensionsDirectoryName = "viewExtensions";
         public const string DefinitionsDirectoryName = "definitions";
         public const string SamplesDirectoryName = "samples";
+        public const string TemplateDirectoryName = "templates";
         public const string BackupDirectoryName = "backup";
         public const string PreferenceSettingsFileName = "DynamoSettings.xml";
         public const string PythonTemplateFileName = "PythonTemplate.py";
@@ -82,6 +83,7 @@ namespace Dynamo.Core
         private string commonPackages;
         private string logDirectory;
         private string samplesDirectory;
+        private string templatesDirectory;
         private string backupDirectory;
         private string defaultBackupDirectory;
         private string preferenceFilePath;
@@ -238,6 +240,14 @@ namespace Dynamo.Core
         public string SamplesDirectory
         {
             get { return samplesDirectory; }
+        }
+
+        /// <summary>
+        /// Dynamo Templates folder
+        /// </summary>
+        public string TemplatesDirectory
+        {
+            get { return templatesDirectory; }
         }
 
         public string BackupDirectory
@@ -572,6 +582,7 @@ namespace Dynamo.Core
             commonDefinitions = Path.Combine(commonDataDir, DefinitionsDirectoryName);
             commonPackages = Path.Combine(commonDataDir, PackagesDirectoryName);
             samplesDirectory = GetSamplesFolder(commonDataDir);
+            templatesDirectory = GetTemplateFolder(commonDataDir);
 
             rootDirectories = new List<string> { userDataDir };
 
@@ -715,7 +726,51 @@ namespace Dynamo.Core
 
             return sampleDirectory;
         }
-        
+
+        /// <summary>
+        /// Get template folder path from common data directory
+        /// </summary>
+        /// <param name="dataRootDirectory"></param>
+        /// <returns></returns>
+        private string GetTemplateFolder(string dataRootDirectory)
+        {
+            var versionedDirectory = dataRootDirectory;
+            if (!Directory.Exists(versionedDirectory))
+            {
+                // Try to see if folder "%ProgramData%\{...}\{major}.{minor}" exists, if it
+                // does not, then root directory would be "%ProgramData%\{...}".
+                //
+                dataRootDirectory = Directory.GetParent(versionedDirectory).FullName;
+            }
+            else if (!Directory.Exists(Path.Combine(versionedDirectory, TemplateDirectoryName)))
+            {
+                // If the folder "%ProgramData%\{...}\{major}.{minor}" exists, then try to see
+                // if the folder "%ProgramData%\{...}\{major}.{minor}\templates" exists. If it
+                // doesn't exist, then root directory would be "%ProgramData%\{...}".
+                //
+                dataRootDirectory = Directory.GetParent(versionedDirectory).FullName;
+            }
+
+            var uiCulture = CultureInfo.CurrentUICulture.Name;
+            var templateDirectory = Path.Combine(dataRootDirectory, TemplateDirectoryName, uiCulture);
+
+            // If the localized template directory does not exist then fall back 
+            // to using the en-US template folder. Do an additional check to see 
+            // if the localized folder is available but is empty.
+            // 
+            var di = new DirectoryInfo(templateDirectory);
+            if (!Directory.Exists(templateDirectory) ||
+                !di.GetDirectories().Any() ||
+                !di.GetFiles("*.dyn", SearchOption.AllDirectories).Any())
+            {
+                var neturalCommonTemplates = Path.Combine(dataRootDirectory, TemplateDirectoryName, "en-US");
+                if (Directory.Exists(neturalCommonTemplates))
+                    templateDirectory = neturalCommonTemplates;
+            }
+
+            return templateDirectory;
+        }
+
         private IEnumerable<string> LibrarySearchPaths(string library)
         {
             // Strip out possible directory from library path.

src/DynamoCore/Models/DynamoModel.cs
@@ -1958,6 +1958,29 @@ namespace Dynamo.Models
         }
 
         /// <summary>
+        /// Opens a Dynamo workspace from a path to a template on disk.
+        /// </summary>
+        /// <param name="filePath">Path to file</param>
+        /// <param name="forceManualExecutionMode">Set this to true to discard
+        /// execution mode specified in the file and set manual mode</param>
+        public void OpenTemplateFromPath(string filePath, bool forceManualExecutionMode = false)
+        {
+
+            if (DynamoUtilities.PathHelper.isValidJson(filePath, out string fileContents, out Exception ex))
+            {
+                OpenJsonFileFromPath(fileContents, filePath, forceManualExecutionMode, true);
+            }
+            else
+            {
+                // These kind of exceptions indicate that file is not accessible 
+                if (ex is IOException || ex is UnauthorizedAccessException || ex is JsonReaderException)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
         /// Inserts a Dynamo graph or Custom Node inside the current workspace from a file path
         /// </summary>
         /// <param name="filePath"></param>
@@ -2028,8 +2051,9 @@ namespace Dynamo.Models
         /// <param name="filePath">Path to file</param>
         /// <param name="forceManualExecutionMode">Set this to true to discard
         /// execution mode specified in the file and set manual mode</param>
+        /// <param name="isTemplate">Set this to true to indicate that the file is a template</param>
         /// <returns>True if workspace was opened successfully</returns>
-        private bool OpenJsonFileFromPath(string fileContents, string filePath, bool forceManualExecutionMode)
+        private bool OpenJsonFileFromPath(string fileContents, string filePath, bool forceManualExecutionMode, bool isTemplate = false)
         {
             try
             {
@@ -2040,7 +2064,7 @@ namespace Dynamo.Models
                     if (true) //MigrationManager.ProcessWorkspace(dynamoPreferences.Version, xmlDoc, IsTestMode, NodeFactory))
                     {
                         WorkspaceModel ws;
-                        if (OpenJsonFile(filePath, fileContents, dynamoPreferences, forceManualExecutionMode, out ws))
+                        if (OpenJsonFile(filePath, fileContents, dynamoPreferences, forceManualExecutionMode, isTemplate, out ws))
                         {
                             OpenWorkspace(ws);
                             //Raise an event to deserialize the view parameters before
@@ -2076,7 +2100,7 @@ namespace Dynamo.Models
                 {
                     if (true) //MigrationManager.ProcessWorkspace(dynamoPreferences.Version, xmlDoc, IsTestMode, NodeFactory))
                     {
-                        if (OpenJsonFile(filePath, fileContents, dynamoPreferences, forceManualExecutionMode, out WorkspaceModel ws))
+                        if (OpenJsonFile(filePath, fileContents, dynamoPreferences, forceManualExecutionMode, false, out WorkspaceModel ws))
                         {
                             ExtraWorkspaceViewInfo viewInfo = ExtraWorkspaceViewInfo.ExtraWorkspaceViewInfoFromJson(fileContents);
 
@@ -2268,6 +2292,7 @@ namespace Dynamo.Models
           string fileContents,
           DynamoPreferencesData dynamoPreferences,
           bool forceManualExecutionMode,
+          bool isTemplate,
           out WorkspaceModel workspace)
         {
             if (!string.IsNullOrEmpty(filePath))
@@ -2295,8 +2320,8 @@ namespace Dynamo.Models
                 CustomNodeManager,
                 this.LinterManager);
 
-            workspace.FileName = string.IsNullOrEmpty(filePath) ? "" : filePath;
-            workspace.FromJsonGraphId = string.IsNullOrEmpty(filePath) ? WorkspaceModel.ComputeGraphIdFromJson(fileContents) : "";
+            workspace.FileName = string.IsNullOrEmpty(filePath) || isTemplate? string.Empty : filePath;
+            workspace.FromJsonGraphId = string.IsNullOrEmpty(filePath) ? WorkspaceModel.ComputeGraphIdFromJson(fileContents) : string.Empty;
             workspace.ScaleFactor = dynamoPreferences.ScaleFactor;
             
             if (!IsTestMode && !IsHeadless)

test/DynamoCoreWpfTests/WorkspaceSaving.cs
@@ -551,6 +551,27 @@ namespace Dynamo.Tests
 
         [Test]
         [Category("UnitTests")]
+        public void CanOpenTemplateAsNewWorkspace()
+        {
+            // get empty workspace
+            var dynamoModel = ViewModel.Model;
+            Assert.IsNotNull(dynamoModel.CurrentWorkspace);
+
+            // set description
+            dynamoModel.CurrentWorkspace.Description = "dummy description";
+
+            // save
+            var newPath = GetNewFileNameOnTempPath("dyn");
+            dynamoModel.CurrentWorkspace.Save(newPath);
+
+            // load as template
+            ViewModel.Model.OpenTemplateFromPath(newPath);
+            Assert.AreEqual(string.Empty, ViewModel.Model.CurrentWorkspace.FileName);
+            Assert.AreEqual("dummy description", ViewModel.Model.CurrentWorkspace.Description);
+        }
+
+        [Test]
+        [Category("UnitTests")]
         public void CanSaveAndReadWorkspaceName()
         {
             // get empty workspace

src/DynamoCore/Configuration/IPathResolver.cs
@@ -157,6 +157,11 @@ namespace Dynamo.Interfaces
         string SamplesDirectory { get; }
 
         /// <summary>
+        /// The root directory where all template files are stored
+        /// </summary>
+        string TemplatesDirectory { get; }
+
+        /// <summary>
         /// The directory where the automatically saved files will be stored.
         /// </summary>
         string BackupDirectory { get; }

src/DynamoCore/Models/DynamoModelCommands.cs
@@ -46,10 +46,18 @@ namespace Dynamo.Models
         {
             string filePath = command.FilePath;
             bool forceManualMode = command.ForceManualExecutionMode;
+            bool isTemplate = command.IsTemplate;
             OpenFileFromPath(filePath, forceManualMode);
 
             //clear the clipboard to avoid copying between dyns
             //ClipBoard.Clear();
+        }
+
+        protected virtual void OpenTemplateImpl(OpenFileCommand command)
+        {
+            string filePath = command.FilePath;
+            bool forceManualMode = command.ForceManualExecutionMode;
+            OpenTemplateFromPath(filePath, forceManualMode);
         }
 
         protected virtual void OpenFileFromJsonImpl(OpenFileFromJsonCommand command)

src/DynamoCore/Models/RecordableCommands.cs
@@ -458,14 +458,16 @@ namespace Dynamo.Models
             #region Public Class Methods
 
             /// <summary>
-            ///
+            /// Constructor
             /// </summary>
             /// <param name="filePath">The path to the file.</param>
             /// <param name="forceManualExecutionMode">Should the file be opened in manual execution mode?</param>
-            public OpenFileCommand(string filePath, bool forceManualExecutionMode = false)
+            /// <param name="isTemplate">Is Dynamo opening a template file?</param>
+            public OpenFileCommand(string filePath, bool forceManualExecutionMode = false, bool isTemplate = false)
             {
                 FilePath = filePath;
                 ForceManualExecutionMode = forceManualExecutionMode;
+                IsTemplate = isTemplate;
             }
 
             private static string TryFindFile(string xmlFilePath, string uriString = null)
@@ -507,6 +509,7 @@ namespace Dynamo.Models
             [DataMember]
             internal string FilePath { get; private set; }
             internal bool ForceManualExecutionMode { get; private set; }
+            internal bool IsTemplate { get; private set; }
             private DynamoModel dynamoModel;
 
             #endregion
@@ -516,7 +519,14 @@ namespace Dynamo.Models
             protected override void ExecuteCore(DynamoModel dynamoModel)
             {
                 this.dynamoModel = dynamoModel;
-                dynamoModel.OpenFileImpl(this);
+                if (IsTemplate)
+                {
+                    dynamoModel.OpenTemplateImpl(this);
+                }
+                else
+                {
+                    dynamoModel.OpenFileImpl(this);
+                }
             }
 
             protected override void SerializeCore(XmlElement element)

src/DynamoCoreWpf/Properties/Resources.Designer.cs
@@ -1802,6 +1802,24 @@ namespace Dynamo.Wpf.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to _File.
+        /// </summary>
+        public static string DynamoViewFileMenuOpenFile {
+            get {
+                return ResourceManager.GetString("DynamoViewFileMenuOpenFile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to _Template.
+        /// </summary>
+        public static string DynamoViewFileMenuOpenTemplate {
+            get {
+                return ResourceManager.GetString("DynamoViewFileMenuOpenTemplate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Open _Recent Files.
         /// </summary>
         public static string DynamoViewFileMenuRecentFiles {
@@ -10140,6 +10158,24 @@ namespace Dynamo.Wpf.Properties {
         public static string WebView2RequiredTitle {
             get {
                 return ResourceManager.GetString("WebView2RequiredTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Workspaces cannot be saved to the Templates folder. Please choose a different folder to save your file..
+        /// </summary>
+        public static string WorkspaceSaveTemplateDirectoryBlockMsg {
+            get {
+                return ResourceManager.GetString("WorkspaceSaveTemplateDirectoryBlockMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Invalid Save Path.
+        /// </summary>
+        public static string WorkspaceSaveTemplateDirectoryBlockTitle {
+            get {
+                return ResourceManager.GetString("WorkspaceSaveTemplateDirectoryBlockTitle", resourceCulture);
             }
         }
         

src/DynamoCoreWpf/Properties/Resources.en-US.resx
@@ -3900,4 +3900,16 @@ In certain complex graphs or host program scenarios, Automatic mode may cause in
   <data name="ResetChangesWarningPopupMessage" xml:space="preserve">
     <value>Your changes will be lost if you proceed.</value>
   </data>
+  <data name="DynamoViewFileMenuOpenFile" xml:space="preserve">
+    <value>_File</value>
+  </data>
+  <data name="DynamoViewFileMenuOpenTemplate" xml:space="preserve">
+    <value>_Template</value>
+  </data>
+  <data name="WorkspaceSaveTemplateDirectoryBlockMsg" xml:space="preserve">
+    <value>Workspaces cannot be saved to the Templates folder. Please choose a different folder to save your file.</value>
+  </data>
+  <data name="WorkspaceSaveTemplateDirectoryBlockTitle" xml:space="preserve">
+    <value>Invalid Save Path</value>
+  </data>
 </root>

commit 07e06ea2ad3b8413e3f5d586a493128e918039a7

DYN-10661: never return a null templates directory
PathManager.templatesDirectory had no initializer. Its only assignment
was inside UpdatePreferenceItemPath, which returns early without
assigning when PathHelper.CreateFolderIfNotExist rejects the location
(unwritable %ProgramData%, unavailable network share, invalid path).
A single failed call at startup therefore left PathManager
.TemplatesDirectory null for the remainder of the session.

In 3.6.2 that null flowed into path.Contains(TemplatesDirectory) in
DynamoViewModel.InternalSaveAs, so String.Contains threw
ArgumentNullException and Save, Save As and Ctrl+S all failed. PR
#17190 replaced that call with the null-guarded
IsPathInTemplateDirectoryTree, which stopped the exception but left
the null in place - so the template write-protection added by that PR
silently does nothing for affected users.

Seed templatesDirectory with defaultTemplatesDirectory when common
directories are built, so the property always reports a usable path
and a rejected preference falls back instead of nulling out. Log the
rejection to the Dynamo log as well; the silent failure is why this
went unnoticed since the check was introduced in #14871.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

src/DynamoCore/Configuration/PathManager.cs
@@ -869,6 +869,13 @@ namespace Dynamo.Core
             commonDataDir = GetCommonDataFolder();
 
             defaultTemplatesDirectory = GetTemplateFolder(commonDataDir);
+
+            // Seed the active templates directory with the default so it is never null.
+            // A preferred location supplied later through UpdatePreferenceItemPath may be
+            // rejected (unwritable %ProgramData%, unavailable network share, invalid path),
+            // and callers must still get a usable path rather than null. See DYN-10661.
+            templatesDirectory = defaultTemplatesDirectory;
+
             rootDirectories = new List<string> { userDataDir };
 
             nodeDirectories = new HashSet<string>

src/DynamoCore/Models/DynamoModel.cs
@@ -1909,7 +1909,14 @@ namespace Dynamo.Models
 
 
             UpdatePreferenceItemLocation(PreferenceItem.Backup, PreferenceSettings.BackupLocation);
-            UpdatePreferenceItemLocation(PreferenceItem.Templates, PreferenceSettings.TemplateFilePath);
+            if (!UpdatePreferenceItemLocation(PreferenceItem.Templates, PreferenceSettings.TemplateFilePath))
+            {
+                // The preferred templates location could not be used, so the default stays
+                // in effect. This previously failed silently and only surfaced downstream as
+                // a null templates directory. See DYN-10661.
+                Logger?.Log("Could not use templates location '" + PreferenceSettings.TemplateFilePath +
+                    "'. Falling back to '" + pathManager.TemplatesDirectory + "'.", LogLevel.File);
+            }
         }
         internal bool UpdatePreferenceItemLocation(PreferenceItem item, string newLocation)
         {

test/DynamoCoreTests/Configuration/PathManagerTests.cs
@@ -1,0 +1,61 @@
+using System.IO;
+using Dynamo.Core;
+using NUnit.Framework;
+
+namespace Dynamo.Tests.Configuration
+{
+    [TestFixture]
+    class PathManagerTests : UnitTestBase
+    {
+        private static PathManager MakePathManager()
+        {
+            return new PathManager(new PathManagerParams
+            {
+                CorePath = Path.GetDirectoryName(typeof(PathManager).Assembly.Location)
+            });
+        }
+
+        /// <summary>
+        /// DYN-10661: TemplatesDirectory has no default value. It is only ever assigned by
+        /// UpdatePreferenceItemPath, so a PathManager that has not had that call succeed
+        /// reports a null templates directory even though DefaultTemplatesDirectory is valid.
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void WhenPathManagerIsConstructedThenTemplatesDirectoryIsNotNull()
+        {
+            var pathManager = MakePathManager();
+
+            Assert.That(pathManager.DefaultTemplatesDirectory, Is.Not.Null.And.Not.Empty,
+                "DefaultTemplatesDirectory is built by the constructor and should always be set.");
+            Assert.That(pathManager.TemplatesDirectory, Is.Not.Null.And.Not.Empty,
+                "TemplatesDirectory must never be null - callers use it in string and path operations.");
+        }
+
+        /// <summary>
+        /// DYN-10661: when the preferred templates location cannot be created (locked-down
+        /// %ProgramData%, unavailable network share, invalid path), UpdatePreferenceItemPath
+        /// returns false without assigning templatesDirectory, permanently leaving the
+        /// property null for the rest of the session. It should fall back to the default.
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void WhenTemplateLocationCannotBeCreatedThenTemplatesDirectoryFallsBackToDefault()
+        {
+            var pathManager = MakePathManager();
+
+            // A file occupying the target path makes Directory.CreateDirectory throw
+            // IOException, which is how PathHelper.CreateFolderIfNotExist reports an
+            // unusable location without surfacing an error to the user.
+            var blockedTemplateLocation = Path.Combine(TempFolder, "blockedTemplates");
+            File.WriteAllText(blockedTemplateLocation, string.Empty);
+
+            var updated = pathManager.UpdatePreferenceItemPath(
+                PathManager.PreferenceItem.Templates, blockedTemplateLocation);
+
+            Assert.That(updated, Is.False, "An unusable location should be rejected.");
+            Assert.That(pathManager.TemplatesDirectory, Is.Not.Null.And.Not.Empty,
+                "A rejected templates location must leave a usable fallback, not null.");
+        }
+    }
+}

test/DynamoCoreWpf3Tests/WorkspaceSaving.cs
@@ -588,6 +588,21 @@ namespace Dynamo.Tests
                 Path.Combine(templateRoot, "fr-FR", "Template.dyn"), localizedTemplateDirectory));
         }
 
+        /// <summary>
+        /// DYN-10661: Save/SaveAs threw ArgumentNullException when the templates directory
+        /// was null, because the check was a bare path.Contains(templatesDirectory).
+        /// The check must treat an unknown templates directory as "not a template path".
+        /// </summary>
+        [Test]
+        [Category("UnitTests")]
+        public void TemplateSavePathCheckDoesNotThrowWhenTemplateDirectoryIsUnknown()
+        {
+            var savePath = Path.Combine(TempFolder, "Workspace.dyn");
+
+            Assert.IsFalse(DynamoViewModel.IsPathInTemplateDirectoryTree(savePath, null));
+            Assert.IsFalse(DynamoViewModel.IsPathInTemplateDirectoryTree(savePath, string.Empty));
+        }
+
         [Test]
         [Category("UnitTests")]
         public void TemplateSavePathCheckAllowsPathsOutsideTemplateRoot()